1d0e62642154ecaece661190d83b48ac4f5b8e0e
Astralchroma <astralchroma@proton.me>
2022-02-18 14:22:28 +0000

    Implement /starships #4

---
+++ src/main/kotlin/net/horizonsend/ion/core/commands/Starships.kt
@@ -0,0 +1,30 @@
+package net.horizonsend.ion.core.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.Default
+import net.horizonsend.ion.sendMiniMessage
+import net.starlegacy.database.schema.misc.SLPlayer
+import net.starlegacy.database.schema.starships.PlayerStarshipData.Companion.findByPilot
+import net.starlegacy.database.slPlayerId
+import net.starlegacy.util.blockKeyX
+import net.starlegacy.util.blockKeyY
+import net.starlegacy.util.blockKeyZ
+import org.bukkit.entity.Player
+
+@CommandAlias("starships")
+internal class Starships: BaseCommand() {
+	@Default
+	fun starships(sender: Player) {
+		sender.sendMiniMessage(
+			findByPilot(sender.slPlayerId).map {
+				val x = blockKeyX(it.blockKey)
+				val y = blockKeyY(it.blockKey)
+				val z = blockKeyZ(it.blockKey)
+
+				"${it.type.formatted} in world <gold>${it.world}</gold> at <green>$x</green>, <green>$y</green>, <green>$z</green> owned by <aqua>${SLPlayer[it.captain]?.lastKnownName}</aqua>"
+
+			}.joinToString("\n", "<bold>Starships:</bold><gray>")
+		)
+	}
+}
