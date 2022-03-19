c1fadbc1049fca1542ef9b76d12b76ede6188ba9
Peter Crawley <peterzcrawley@gmail.com>
2022-03-19 14:53:56 +0000

    Reflection Fixes

--- src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
+++ src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
@@ -13,49 +13,54 @@ import org.bukkit.Location
 import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
+import org.litote.kmongo.newId
+
+// Updated version based on https://www.spigotmc.org/threads/teleport-player-smoothly.317416/page-2#post-4186516
 
 object ConnectionUtils {
 	private val OFFSET_DIRECTION = setOf(X_ROT, Y_ROT)
 	private val OFFSET_ALL = setOf(X_ROT, Y_ROT, X, Y, Z)
 
 	private var justTeleportedField: Field = getField("justTeleported")
+	private var awaitingPositionFromClientField: Field = getField("y") // y = awaitingPositionFromClient
 	private var teleportPosField: Field = getField("teleportPos")
 	private var lastPosXField: Field = getField("lastPosX")
 	private var lastPosYField: Field = getField("lastPosY")
 	private var lastPosZField: Field = getField("lastPosZ")
-	private var teleportAwaitField: Field = getField("teleportAwait")
-	private var AField: Field = getField("A")
-	private var eField: Field = getField("e")
+	private var awaitingTeleportField: Field = getField("z") // z = awaitingTeleport
+	private var awaitingTeleportTimeField: Field = getField("A") // A = awaitingTeleportTimeField
+	private var aboveGroundVehicleTickCountField: Field = getField("e") // E = aboveGroundVehicleTickCountField
 
 	private fun getField(name: String): Field =
 		ServerGamePacketListenerImpl::class.java.getDeclaredField(name).apply { isAccessible = true }
 
 	fun move(player: Player, loc: Location, theta: Float = 0.0f, offsetPos: Vector? = null) {
-		val handle = (player as CraftPlayer).handle
-
-		if (handle.containerMenu !== handle.inventoryMenu) handle.closeContainer()
-
 		val x = loc.x
 		val y = loc.y
 		val z = loc.z
 
+		val handle = (player as CraftPlayer).handle
+
+		if (handle.containerMenu !== handle.inventoryMenu) handle.closeContainer()
+
 		handle.absMoveTo(x, y, z, handle.yRot + theta, handle.xRot)
 
 		val connection = handle.connection
+
 		var teleportAwait: Int
 
 		justTeleportedField.set(connection, true)
-		teleportPosField.set(connection, Vec3(x, y, z))
+		awaitingPositionFromClientField.set(connection, Vec3(x, y, z))
 		lastPosXField.set(connection, x)
 		lastPosYField.set(connection, y)
 		lastPosZField.set(connection, z)
 
-		teleportAwait = teleportAwaitField.getInt(connection).plus(1)
+		teleportAwait = awaitingTeleportField.getInt(connection) + 1
 
 		if (teleportAwait == 2147483647) teleportAwait = 0
 
-		teleportAwaitField.set(connection, teleportAwait)
-		AField.set(connection, eField.get(connection))
+		awaitingTeleportField.set(connection, teleportAwait)
+		awaitingTeleportTimeField.set(connection, aboveGroundVehicleTickCountField.get(connection))
 
 		val px: Double
 		val py: Double
