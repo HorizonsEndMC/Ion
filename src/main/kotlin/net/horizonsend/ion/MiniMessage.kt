01efbaae619ff1ad2cc11d0fae1db8192ebfd56b
Astralchroma <astralchroma@proton.me>
2022-03-25 17:52:38 +0000

    Internal

--- src/main/kotlin/net/horizonsend/ion/MiniMessage.kt
+++ src/main/kotlin/net/horizonsend/ion/MiniMessage.kt
@@ -3,6 +3,6 @@ package net.horizonsend.ion
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
-val String.asMiniMessage get() = miniMessage().deserialize(this)
+internal val String.asMiniMessage get() = miniMessage().deserialize(this)
 
-fun Audience.sendMiniMessage(message: String) = sendMessage(message.asMiniMessage)
+internal fun Audience.sendMiniMessage(message: String) = sendMessage(message.asMiniMessage)
