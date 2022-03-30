d46b0caec775fec9f46b8e1e5ed35d3b338f6610
Peter Crawley <peterzcrawley@gmail.com>
2022-03-30 23:24:12 +0100

    Disable enchanting

---
+++ src/main/kotlin/net/horizonsend/ion/enchantment/EnchantmentListener.kt
@@ -0,0 +1,38 @@
+package net.horizonsend.ion.enchantment
+
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment.SILK_TOUCH
+import org.bukkit.enchantments.EnchantmentOffer
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent
+import org.bukkit.event.inventory.PrepareAnvilEvent
+
+internal class EnchantmentListener: Listener {
+	@EventHandler
+	fun onPrepareItemEnchantEvent(event: PrepareItemEnchantEvent) {
+		// All of these suppressions are because Spigot don't know how nullability annotations work.
+		@Suppress("UNCHECKED_CAST")
+		(event.offers as Array<EnchantmentOffer?>)[0] = if (SILK_TOUCH.canEnchantItem(event.item)) EnchantmentOffer(SILK_TOUCH, 120, 0) else null
+
+		@Suppress("UNCHECKED_CAST")
+		(event.offers as Array<EnchantmentOffer?>)[1] = null
+
+		@Suppress("UNCHECKED_CAST")
+		(event.offers as Array<EnchantmentOffer?>)[2] = null
+	}
+
+	@EventHandler
+	fun onPrepareAnvilEvent(event: PrepareAnvilEvent) {
+		if (event.inventory.firstItem == null) return
+		if (event.inventory.secondItem == null) return
+
+		if (event.inventory.secondItem!!.type == Material.ENCHANTED_BOOK) {
+			if (!event.inventory.secondItem!!.enchantments.containsKey(SILK_TOUCH)) event.result = null
+			else {
+				event.result = event.inventory.firstItem!!.clone()
+				event.result!!.enchantments[SILK_TOUCH] = 1
+			}
+		}
+	}
+}
