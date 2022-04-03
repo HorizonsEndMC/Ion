fb37b615381b8d6aac0e4bd76cefde9083bc77d8
Peter-Crawley <peterzcrawley@gmail.com>
2022-04-03 01:46:01 +0100

    Use plugin libraries loader to reduce Jar size

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,19 +11,20 @@ repositories {
 	mavenCentral()
 }
 
-val configurateVersion = "4.1.2"
-
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT") // Paper
 
 	compileOnly(project(":IonCore")) // IonCore
 
-	implementation("org.spongepowered:configurate-hocon:$configurateVersion") // Configurate (HOCON)
-	implementation("org.spongepowered:configurate-extra-kotlin:$configurateVersion") // Configurate (Kotlin Additions)
-
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.20") // Required by Configurate (Kotlin Additions)
-
 	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT") // Annotation Command Framework
+
+	// Provided by Server Library Loader
+	compileOnly("org.spongepowered:configurate-hocon:4.1.2") // Configurate (HOCON)
+	compileOnly("org.spongepowered:configurate-extra-kotlin:4.1.2") // Configurate (Kotlin Additions)
+
+	compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.20") // Required by Configurate (Kotlin Additions)
+
+	compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
 }
 
 tasks {
