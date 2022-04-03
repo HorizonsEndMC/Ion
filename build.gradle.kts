cd3f40cea9ec65c94c610daad0acaceead4ac4d2
Peter Crawley <peterzcrawley@gmail.com>
2022-04-03 02:13:50 +0100

    Use plugin libraries loader to reduce Jar size

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,29 +25,35 @@ dependencies {
 	compileOnly("io.papermc.paper:paper-api:$minecraftVersion")
 	paperDevBundle(minecraftVersion)
 
+	// Provided by us
+	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+	// TODO: Remove
+	implementation("com.github.jkcclemens:khttp:0.1.0") {
+		exclude("org.jetbrains.kotlin")
+		exclude("org.json")
+	}
+	implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT")
+
+	// Provided by other plugins
 	compileOnly("net.luckperms:api:5.4")
 	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 	compileOnly("net.citizensnpcs:citizens:2.0.27-SNAPSHOT")
 	compileOnly("com.github.webbukkit.dynmap:spigot:3.1")
 	compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
-
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.20")
-	implementation("org.litote.kmongo:kmongo:4.5.1")
-	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
-
-	// TODO: Remove these
+	// TODO: Remove
 	compileOnly("com.discordsrv:discordsrv:1.25.1")
 
-	implementation("com.github.jkcclemens:khttp:0.1.0")
-	implementation("io.github.config4k:config4k:0.4.2")
-	implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT")
-	implementation("com.googlecode.cqengine:cqengine:3.6.0")
-	implementation("com.daveanthonythomas.moshipack:moshipack:1.0.1")
-
-	// Older versions need to be used for compatibility, I won't bother fixing it for now because were going to drop these
-	// dependencies in the future anyway - Peter
-	implementation("redis.clients:jedis:3.7.1")
-	implementation("com.github.stefvanschie.inventoryframework:IF:0.5.8")
+	// Provided by Server Library Loader
+	compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.20")
+	compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
+	compileOnly("org.litote.kmongo:kmongo:4.5.1")
+	// TODO: Remove
+	compileOnly("io.github.config4k:config4k:0.4.2")
+	compileOnly("com.googlecode.cqengine:cqengine:3.6.0")
+	compileOnly("com.daveanthonythomas.moshipack:moshipack:1.0.1")
+	// Older versions
+	compileOnly("redis.clients:jedis:3.7.1")
+	compileOnly("com.github.stefvanschie.inventoryframework:IF:0.5.8")
 }
 
 tasks {
