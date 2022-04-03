ed1022419cf0c3b3b186bce188b8f55a0f426abf
Astralchroma <astralchroma@proton.me>
2022-04-03 23:27:35 +0100

    Change Packages

--- src/main/kotlin/net/horizonsend/ion/commands/IonReloadCommand.kt
+++ src/main/kotlin/net/horizonsend/ion/commands/IonReloadCommand.kt
@@ -1,10 +1,12 @@
-package net.horizonsend.ion
+package net.horizonsend.ion.commands
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Description
+import net.horizonsend.ion.Ion
+import net.horizonsend.ion.extensions.sendMiniMessage
 import org.bukkit.command.CommandSender
 import org.spongepowered.configurate.ConfigurateException
 
