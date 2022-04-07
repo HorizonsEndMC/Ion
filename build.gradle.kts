148af5f094f28bbd89c18e3c6011e754ac618147
Peter Crawley <peterzcrawley@gmail.com>
2022-04-07 19:30:22 +0100

    Fix Annotation Command Framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,9 +58,24 @@ dependencies {
 }
 
 tasks {
-	compileKotlin { kotlinOptions { jvmTarget = "17" } }
-	reobfJar { outputJar.set(file(rootProject.projectDir.absolutePath + "/build/IonCore.jar")) }
-	shadowJar { minimize() }
+	compileJava {
+		options.compilerArgs.add("-parameters")
+		options.isFork = true
+		options.forkOptions.executable = "javac"
+	}
+
+	compileKotlin {
+		kotlinOptions.javaParameters = true
+		kotlinOptions.jvmTarget = "17"
+	}
+
+	reobfJar {
+		outputJar.set(file(rootProject.projectDir.absolutePath + "/build/IonCore.jar"))
+	}
+
+	shadowJar {
+		minimize()
+	}
 }
 
 java.toolchain.languageVersion.set(of(17))
