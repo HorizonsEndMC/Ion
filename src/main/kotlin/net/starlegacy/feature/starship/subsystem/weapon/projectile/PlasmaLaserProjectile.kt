41516158d2ba8b7039e177a45ad3603f2645aefb
Peter Crawley <peterzcrawley@gmail.com>
2022-04-14 18:12:56 +0100

    Revert Sciath's Low Effort, Unbalanced Weapon Changes
    This reverts commit 8c52b26be17b6e2bc1dea60dfeca9a95430cff26.

--- src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/PlasmaLaserProjectile.kt
+++ src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/PlasmaLaserProjectile.kt
@@ -14,11 +14,11 @@ class PlasmaLaserProjectile(
 ) : LaserProjectile(starship, loc, dir, shooter) {
 	override val range: Double = 160.0
 	override val speed: Double = 400.0
-	override val shieldDamageMultiplier: Int = 2
+	override val shieldDamageMultiplier: Int = 3
 	override val color: Color = starship.weaponColor
 	override val thickness: Double = 0.3
 	override val particleThickness: Double = .5
-	override val explosionPower: Float = 2.5f
+	override val explosionPower: Float = 4.0f
 	override val volume: Int = 10
 	override val soundName: String = "starship.weapon.plasma_cannon.shoot"
 }
