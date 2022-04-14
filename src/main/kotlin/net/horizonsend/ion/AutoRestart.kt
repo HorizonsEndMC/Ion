420c879601d01c885e5bb3b984255178f7573f4b
Astralchroma <astralchroma@proton.me>
2022-04-14 03:16:40 +0100

    Butcher the auto restart to allow for restart votes

--- src/main/kotlin/net/horizonsend/ion/AutoRestart.kt
+++ src/main/kotlin/net/horizonsend/ion/AutoRestart.kt
@@ -5,11 +5,13 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Description
+import java.lang.Math.floorDiv
 import java.lang.System.currentTimeMillis
 import java.lang.Thread.sleep
 import net.horizonsend.ion.extensions.asMiniMessage
 import net.horizonsend.ion.extensions.sendMiniMessage
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority.MONITOR
 import org.bukkit.event.Listener
@@ -25,58 +27,86 @@ import org.bukkit.event.player.PlayerQuitEvent
  * If restartAsSoonAsPossible is set to true, then rule 1 is ignored, and rule 3 will not wait 5 minutes.
  */
 
-@CommandAlias("restartASAP")
-@CommandPermission("ion.restartASAP")
+@CommandAlias("restartvote")
 @Description("Causes the server to restart as soon as it can.")
 internal class AutoRestart(private val plugin: Ion): BaseCommand(), Listener {
+	private val requiredRestartVotes get() = floorDiv(plugin.server.onlinePlayers.size, 2) + 1
+
+	private var isRestartVoting = false
+	private var restartVotes = mutableSetOf<Player>()
+
 	private val serverStart = currentTimeMillis()
-	private var restartAsSoonAsPossible = false
 
 	// This class is constructed on plugin load, so we can do our init logic here.
 	init {
 		// Forced 24h restart, the only one we need to give a warning for.
-		plugin.server.scheduler.runTaskLaterAsynchronously(plugin, Runnable {
-			plugin.server.sendMiniMessage("<aqua>Server restart in 1 minute!")
+		plugin.server.scheduler.runTaskLaterAsynchronously(plugin, Runnable { restartCountdown() }, 1728000) // 24h
+	}
 
-			sleep(50000) // 50s
+	fun restartCountdown() {
+		plugin.server.sendMiniMessage("<aqua>Server restart in 1 minute!")
 
-			for (i in 10 downTo 2) {
-				plugin.server.sendMiniMessage("<red>Server restart in $i seconds!")
-				sleep(1000) // 1s
-			}
+		sleep(50000) // 50s
 
-			plugin.server.sendMiniMessage("<red>Server restart in 1 second!")
+		for (i in 10 downTo 2) {
+			plugin.server.sendMiniMessage("<red>Server restart in $i seconds!")
 			sleep(1000) // 1s
+		}
 
-			plugin.server.scheduler.runTask(plugin, Runnable {
-				plugin.server.onlinePlayers.forEach {
-					it.kick("<aqua>Server is restarting.".asMiniMessage, RESTART_COMMAND)
-				}
-				plugin.server.shutdown()
-			})
-		}, 1728000) // 24h
+		plugin.server.sendMiniMessage("<red>Server restart in 1 second!")
+		sleep(1000) // 1s
+
+		plugin.server.scheduler.runTask(plugin, Runnable {
+			plugin.server.onlinePlayers.forEach {
+				it.kick("<aqua>Server is restarting.".asMiniMessage, RESTART_COMMAND)
+			}
+			plugin.server.shutdown()
+		})
 	}
 
 	@EventHandler(priority = MONITOR)
 	fun onPlayerDisconnect(@Suppress("unused") event: PlayerQuitEvent) { // Event
-		if (!restartAsSoonAsPossible && currentTimeMillis() - serverStart < 21600000) return // 6h
+		if (currentTimeMillis() - serverStart < 21600000) return // 6h
 
 		// The player that just disconnected will still be included in this list
 		if (plugin.server.onlinePlayers.size == 1) {
 			plugin.server.scheduler.runTaskLaterAsynchronously(plugin, Runnable {
 				if (plugin.server.onlinePlayers.isEmpty()) plugin.server.shutdown()
-			}, if (restartAsSoonAsPossible) 0 else 6000) // 5m
+			}, 6000) // 5m
 		}
 	}
 
 	@Default
+	@CommandPermission("ion.restartvote")
 	@Suppress("unused") // Entrypoint (Command)
-	fun onRestartASAP(sender: CommandSender) {
+	fun onRestartVote(sender: CommandSender) {
 		if (plugin.server.onlinePlayers.isEmpty()) plugin.server.shutdown()
 
-		if (!restartAsSoonAsPossible) {
-			plugin.server.sendMiniMessage("<aqua>The server has been set to restart as soon as possible in order to apply changes, a restart will occur when all players have disconnected.")
-			restartAsSoonAsPossible = true
+		plugin.server.sendMiniMessage("<aqua><b>The server needs to restart to apply changes!</b></aqua> Click <blue><underlined><click:run_command:'/restartvoteyes'>here</click></underlined></blue> to vote yes to the restart. Restart will take place after $requiredRestartVotes votes within the next 2 minutes.")
+		isRestartVoting = true
+
+		plugin.server.scheduler.runTaskLater(plugin, Runnable {
+			restartVotes.clear()
+			isRestartVoting = false
+		}, 20*60*2)
+	}
+
+	@CommandAlias("restartvoteyes")
+	@Suppress("unused") // Entrypoint (Command)
+	fun onRestartVoteYet(sender: Player) {
+		if (!isRestartVoting) {
+			sender.sendMiniMessage("<yellow>There is not currently a server restart vote.")
+			return
 		}
+
+		if (restartVotes.size >= requiredRestartVotes) {
+			plugin.server.sendMiniMessage("<aqua>Required votes met, server will restart.")
+			restartCountdown()
+			return
+		}
+
+		restartVotes.add(sender)
+
+		plugin.server.sendMiniMessage("<aqua>There are now ${restartVotes.size} restart votes. $requiredRestartVotes are needed.")
 	}
 }
