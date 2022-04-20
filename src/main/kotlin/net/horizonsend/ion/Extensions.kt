dfda9bfcc7ea2215e500b6565978077a107702df
Peter Crawley <peterzcrawley@gmail.com>
2022-04-20 14:16:16 +0100

    enumSetOf<Enum>()

--- src/main/kotlin/net/horizonsend/ion/Extensions.kt
+++ src/main/kotlin/net/horizonsend/ion/Extensions.kt
@@ -6,3 +6,8 @@ import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 internal val String.asMiniMessage get() = miniMessage().deserialize(this)
 
 internal fun Audience.sendMiniMessage(message: String) = sendMessage(message.asMiniMessage)
+
+inline fun <reified T : Enum<T>> enumSetOf(vararg elems: T) =
+	java.util.EnumSet.noneOf(T::class.java).apply {
+		addAll(elems)
+	}
