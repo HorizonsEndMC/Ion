0d89ce1d7c3b6fa7fac4f2b80c2db609f055fa85
Peter Crawley <peterzcrawley@gmail.com>
2022-04-20 14:18:24 +0100

    Specify Type + Screw with Imports

--- src/main/kotlin/net/horizonsend/ion/Extensions.kt
+++ src/main/kotlin/net/horizonsend/ion/Extensions.kt
@@ -1,5 +1,7 @@
 package net.horizonsend.ion
 
+import java.util.EnumSet
+import java.util.EnumSet.noneOf
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
@@ -7,7 +9,7 @@ internal val String.asMiniMessage get() = miniMessage().deserialize(this)
 
 internal fun Audience.sendMiniMessage(message: String) = sendMessage(message.asMiniMessage)
 
-inline fun <reified T : Enum<T>> enumSetOf(vararg elems: T) =
-	java.util.EnumSet.noneOf(T::class.java).apply {
+inline fun <reified T : Enum<T>> enumSetOf(vararg elems: T): EnumSet<T> =
+	noneOf(T::class.java).apply {
 		addAll(elems)
 	}
