d742a55b759a4b801ad0eeda8f995ee9e6695900
Astralchroma <astralchroma@proton.me>
2022-04-28 20:29:50 +0100

    Fancy API for Feedback Messages

--- src/main/kotlin/net/horizonsend/ion/core/extensions/Extensions.kt
+++ src/main/kotlin/net/horizonsend/ion/core/extensions/Extensions.kt
@@ -3,6 +3,9 @@ package net.horizonsend.ion.core.extensions
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
-fun Audience.sendMiniAction(message: String) = sendActionBar(miniMessage().deserialize(message))
+@Deprecated(
+	"Do not use MiniMessage Directly",
+	ReplaceWith("sendFeedbackMessage(?, message, ?)", "net.horizonsend.ion.core.feedback.sendFeedbackMessage")
+)
 
 fun Audience.sendMiniMessage(message: String) = sendMessage(miniMessage().deserialize(message))
