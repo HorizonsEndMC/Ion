20d5c1138ca4f7f5ea8a3e84192b24bdfe6f66be
Peter Crawley <peterzcrawley@gmail.com>
2022-05-04 12:52:50 +0100

    Compile Fixes

--- src/main/kotlin/net/horizonsend/ion/Extensions.kt
+++ src/main/kotlin/net/horizonsend/ion/Extensions.kt
@@ -3,5 +3,6 @@ package net.horizonsend.ion
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
+internal val String.asMiniMessage get() = miniMessage().deserialize(this)
 @Deprecated("Avoid using MiniMessage directly.", replaceWith = ReplaceWith("sendFeedbackMessage(?, message, ?)", "net.horizonsend.ion.feedback.sendFeedbackMessage"))
-internal fun Audience.sendMiniMessage(message: String) = sendMessage(miniMessage().deserialize(message.asMiniMessage))
+internal fun Audience.sendMiniMessage(message: String) = sendMessage(message.asMiniMessage)
