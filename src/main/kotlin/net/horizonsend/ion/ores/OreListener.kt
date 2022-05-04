ad62a9279c35ec1073eab11a96da4adf2d557a19
PeterCrawley1 <peterzcrawley@gmail.com>
2022-05-04 15:12:17 +0100

    Ore Changes

--- src/main/kotlin/net/horizonsend/ion/ores/OreListener.kt
+++ src/main/kotlin/net/horizonsend/ion/ores/OreListener.kt
@@ -2,59 +2,103 @@ package net.horizonsend.ion.ores
 
 import kotlin.random.Random
 import net.horizonsend.ion.Ion
-import net.horizonsend.ion.ores.OrePlacementConfig.valueOf
 import org.bukkit.Bukkit
+import org.bukkit.Chunk
+import org.bukkit.Material
 import org.bukkit.NamespacedKey
+import org.bukkit.block.data.BlockData
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.world.ChunkLoadEvent
-import org.bukkit.persistence.PersistentDataType.INTEGER
+import org.bukkit.persistence.PersistentDataType
 
-internal class OreListener(private val plugin: Ion): Listener {
-	init { plugin.server.pluginManager.registerEvents(this, plugin) }
+internal class OreListener(private val plugin: Ion) : Listener {
+	init {
+		plugin.server.pluginManager.registerEvents(this, plugin)
+	}
 
 	private val oreCheckNamespace = NamespacedKey(plugin, "oreCheck")
 
 	@EventHandler
 	fun onChunkLoad(event: ChunkLoadEvent) {
-		if (event.chunk.persistentDataContainer.get(oreCheckNamespace, INTEGER) != null) return
+		when (event.chunk.persistentDataContainer.get(oreCheckNamespace, PersistentDataType.INTEGER)) {
+			2 -> return // Ores are up-to-date.
+			null -> placeOres(event.chunk) // Ores have not been placed.
+			else -> placeOres(event.chunk, true) // Ores are out of date.
+		}
+	}
 
+	private class BlockLocation(var x: Int, var y: Int, var z: Int)
+
+	private fun placeOres(chunk: Chunk, removeExisting: Boolean = false) {
 		Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
-			val chunkSnapshot = event.chunk.getChunkSnapshot(true, false, false)
-			val placementConfig = try { valueOf(event.world.name) } catch (exception: IllegalArgumentException) { return@Runnable }
-			val random = Random(event.chunk.chunkKey)
+			val chunkSnapshot = chunk.getChunkSnapshot(false, false, false)
+			val placementConfiguration =
+				OrePlacementConfig.values().find { it.name == chunkSnapshot.worldName } ?: return@Runnable
+			val random = Random(chunk.chunkKey)
 
-			val placedOres = mutableMapOf<Triple<Int, Int, Int>, Ore>()
+			val placedBlocks = mutableMapOf<BlockLocation, BlockData>()
+			val placedOres = mutableMapOf<BlockLocation, Ore>()
 
-			for (x in 0 .. 15) {
-				for (z in 0 .. 15) {
-					for (y in event.world.minHeight .. chunkSnapshot.getHighestBlockYAt(x, z)) {
-						if (!placementConfig.groundMaterial.contains(chunkSnapshot.getBlockType(x, y, z))) continue
+			if (removeExisting) {
+				val file =
+					plugin.dataFolder.resolve("ores/${chunkSnapshot.worldName}/${chunkSnapshot.x}_${chunkSnapshot.z}.ores.csv")
 
-						placementConfig.options.forEach { (ore, chance) ->
-							if (random.nextDouble(0.0, 100.0) > chance) return@forEach
-							placedOres[Triple(x, y, z)] = ore
-						}
+				if (file.exists()) {
+					file.readText().split("\n").forEach { oreLine ->
+						val oreData = oreLine.split(",")
+
+						val x = oreData[0].toInt()
+						val y = oreData[1].toInt()
+						val z = oreData[2].toInt()
+						val original = Material.valueOf(oreData[3])
+						val placedOre = Ore.valueOf(oreData[4])
+
+						if (chunkSnapshot.getBlockType(x, y, z) == original)
+							placedBlocks[BlockLocation(x, y, z)] = placedOre.blockData
 					}
 				}
 			}
 
+			for (sectionY in chunk.world.minHeight.shr(16)..chunk.world.maxHeight.shr(16)) {
+				if (chunkSnapshot.isSectionEmpty(sectionY)) continue
+
+				for (x in 0..15) for (y in 0..15) for (z in 0..15) {
+					val blockData = chunkSnapshot.getBlockData(x, y + sectionY.shl(16), z)
+
+					if (blockData.material.isAir) continue
+					if (blockData.material.isInteractable) continue
+
+					if (chunkSnapshot.getBlockType(x + 1, y, z).isAir) continue
+					if (chunkSnapshot.getBlockType(x - 1, y, z).isAir) continue
+					if (chunkSnapshot.getBlockType(x, y + 1, z).isAir) continue
+					if (chunkSnapshot.getBlockType(x, y - 1, z).isAir) continue
+					if (chunkSnapshot.getBlockType(x, y, z + 1).isAir) continue
+					if (chunkSnapshot.getBlockType(x, y, z - 1).isAir) continue
+
+					placementConfiguration.options.forEach { (ore, chance) ->
+						if (random.nextDouble(0.0, 100.0) > 0.3 * (1 / chance)) return@forEach
+						placedOres[BlockLocation(x, y, z)] = ore
+					}
+				}
+			}
+
+			placedBlocks.putAll(placedOres.map { Pair(it.key, it.value.blockData) })
+
 			Bukkit.getScheduler().runTask(plugin, Runnable {
 				placedOres.forEach { (position, ore) ->
-					event.chunk.getBlock(position.first, position.second, position.third).setBlockData(ore.blockData, false)
+					chunk.getBlock(position.x, position.y, position.z).setBlockData(ore.blockData, false)
 				}
 
-				event.chunk.persistentDataContainer.set(oreCheckNamespace, INTEGER, 1)
+				chunk.persistentDataContainer.set(oreCheckNamespace, PersistentDataType.INTEGER, 2)
 			})
 
-			if (placedOres.isEmpty()) return@Runnable
-
-			plugin.dataFolder.resolve("ores/${event.world.name}")
+			plugin.dataFolder.resolve("ores/${chunkSnapshot.worldName}")
 				.apply { mkdirs() }
 				.resolve("${chunkSnapshot.x}_${chunkSnapshot.z}.ores.csv")
 				.writeText(placedOres.map {
-					"${it.key.first},${it.key.second},${it.key.third},${chunkSnapshot.getBlockType(it.key.first, it.key.second, it.key.third)},${it.value}"
-				}.joinToString("\n","",""))
+					"${it.key.x},${it.key.y},${it.key.z},${chunkSnapshot.getBlockType(it.key.x, it.key.y, it.key.z)},${it.value}"
+				}.joinToString("\n", "", ""))
 		})
 	}
 }
