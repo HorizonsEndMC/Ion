c43dcf21e8f3219bacf9f3d92404e55316131e82
Peter Crawley <peterzcrawley@gmail.com>
2022-05-13 22:43:31 +0100

    Split FeedbackType into its own file

--- src/main/kotlin/net/horizonsend/ion/utilities/feedback/Feedback.kt
+++ src/main/kotlin/net/horizonsend/ion/utilities/feedback/Feedback.kt
@@ -1,36 +1,8 @@
-@file:Suppress("unused") // API
-
 package net.horizonsend.ion.utilities.feedback
 
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
-
-/**
- * Represents a type of feedback that can be sent to a player
- * @property colour The colour used when displaying feedback
- */
-enum class FeedbackType(val colour: String) {
-	/**
-	 * Something failed as a result of an issue on the server, this is generally rare
-	 */
-	SERVER_ERROR("#ff8888"),
-
-	/**
-	 * Something failed as a result of the player, such as an invalid command parameter, or lacking permission
-	 */
-	USER_ERROR("#ff8844"),
-
-	/**
-	 * Information the player should be aware of
-	 */
-	INFORMATION("#8888ff"),
-
-	/**
-	 * Used when confirming a command succeeded
-	 */
-	SUCCESS("#88ff88")
-}
 
 /**
  * @param type The type of feedback
