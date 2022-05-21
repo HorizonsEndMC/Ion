f9fac50a1b01828a33ee91711d9286085536108b
Astralchroma <astralchroma@proton.me>
2022-05-21 16:12:07 +0100

    Prevent Fishing

---
+++ src/main/kotlin/net/horizonsend/ion/miscellaneous/listeners/PlayerFishListener.kt
@@ -0,0 +1,12 @@
+package net.horizonsend.ion.miscellaneous.listeners
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerFishEvent
+
+class PlayerFishListener : Listener {
+	@EventHandler
+	fun onPlayerFishEvent(event: PlayerFishEvent) {
+		event.isCancelled = true
+	}
+}
