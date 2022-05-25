401a650a47bb0026661e55bde7d3ca0cd3e56708
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-05-26 00:31:40 +0100

    Station Create Command

---
+++ src/main/kotlin/net/horizonsend/ion/core/commands/AdminCommands.kt
@@ -0,0 +1,23 @@
+package net.horizonsend.ion.core.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.core.FeedbackType.SUCCESS
+import net.horizonsend.ion.core.sendFeedbackMessage
+import net.starlegacy.database.schema.nations.CapturableStation
+
+import org.bukkit.entity.Player
+import org.litote.kmongo.dayOfWeek
+import java.time.DayOfWeek
+
+@CommandAlias("admin")
+object AdminCommands : BaseCommand(){
+	@CommandPermission("ion.core.capturablestation.create")
+	@Subcommand("capturablestation")
+	fun captuablestationcreation(sender: Player, stationname: String, x: Int, z: Int, siegehour: Int){
+		CapturableStation.create(stationname,sender.world.toString(), x, z, siegehour, DayOfWeek.values().toSet())
+		sender.sendFeedbackMessage(SUCCESS, "Successfully created Capturable Station ({0}), At {1}, {2}, SiegeHour is {3}", stationname, x, z, siegehour)
+	}
+}
