4a64588c9492bc395092cc3dc435bb6b66e0f019
Peter Crawley <peterzcrawley@gmail.com>
2022-05-28 19:27:51 +0100

    Actually Remove Autocrafters

--- src/main/kotlin/net/starlegacy/feature/multiblock/autocrafter/AutoCrafterMultiblockTier2.kt
+++
@@ -1,7 +0,0 @@
-package net.starlegacy.feature.multiblock.autocrafter
-
-import org.bukkit.Material
-
-object AutoCrafterMultiblockTier2 : AutoCrafterMultiblock("&eTier 2", Material.GOLD_BLOCK, iterations = 4) {
-	override val maxPower: Int = 400_000
-}
