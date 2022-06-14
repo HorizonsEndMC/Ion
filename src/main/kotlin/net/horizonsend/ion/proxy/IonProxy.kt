a448eb48fc2b9c702b39ba8d5389def9cf272063
Peter Crawley <peterzcrawley@gmail.com>
2022-06-14 22:45:17 +0100

    Basic Resource Pack Auto-Prompt

--- src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
+++ src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
@@ -1,8 +1,14 @@
 package net.horizonsend.ion.proxy
 
 import com.google.inject.Inject
+import com.velocitypowered.api.event.EventTask
+import com.velocitypowered.api.event.EventTask.async
+import com.velocitypowered.api.event.PostOrder
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.proxy.ProxyServer
+import net.horizonsend.ion.proxy.listeners.ServerPostConnectListener
 import org.slf4j.Logger
 
 @Plugin(
@@ -13,6 +19,12 @@ import org.slf4j.Logger
 )
 @Suppress("Unused")
 class IonProxy @Inject constructor(
-	@Suppress("Unused_Parameter") server: ProxyServer,
+	private val server: ProxyServer,
 	@Suppress("Unused_Parameter") slF4JLogger: Logger
-)
+) {
+	@Suppress("Unused_Parameter")
+	@Subscribe(order = PostOrder.LAST)
+	fun onProxyInitializeEvent(event: ProxyInitializeEvent): EventTask = async {
+		server.eventManager.register(this, ServerPostConnectListener(server))
+	}
+}
