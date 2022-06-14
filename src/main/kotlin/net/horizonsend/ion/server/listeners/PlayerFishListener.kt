30d74ae773821d8285fa30ed59346225948cb36c
Astralchroma <astralchroma@proton.me>
2022-06-14 16:37:19 +0100

    Suppress unused and specify event priority

--- src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerFishListener.kt
+++ src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerFishListener.kt
@@ -1,11 +1,13 @@
 package net.horizonsend.ion.server.listeners
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerFishEvent
 
 class PlayerFishListener : Listener {
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
+	@Suppress("Unused")
 	fun onPlayerFishEvent(event: PlayerFishEvent) {
 		event.isCancelled = true
 	}
--- src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerQuitListener.kt
+++ src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerQuitListener.kt
@@ -1,11 +1,13 @@
 package net.horizonsend.ion.server.listeners
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
 
 class PlayerQuitListener : Listener {
-	@EventHandler
+	@EventHandler(priority = EventPriority.NORMAL)
+	@Suppress("Unused")
 	fun onPlayerQuitEvent(event: PlayerQuitEvent) {
 		event.quitMessage(null)
 	}
