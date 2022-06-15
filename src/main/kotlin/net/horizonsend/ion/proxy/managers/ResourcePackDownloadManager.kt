7a581a1a2ff68a578185bb009aba01dd27115bf6
Peter Crawley <peterzcrawley@gmail.com>
2022-06-15 20:55:15 +0100

    Resource Pack Loading Fix

--- src/main/kotlin/net/horizonsend/ion/proxy/managers/ResourcePackDownloadManager.kt
+++ src/main/kotlin/net/horizonsend/ion/proxy/managers/ResourcePackDownloadManager.kt
@@ -10,7 +10,7 @@ object ResourcePackDownloadManager {
 		// Laziest JSON "Parsing" Ever
 		resourcePackTag = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
 			.readText()
-			.substringAfter("\",\n  \"tag_name\": \"")
-			.substringBefore("\",\n  ")
+			.substringAfter("\",\n\t\"tag_name\": \"")
+			.substringBefore("\",\n\t")
 	}
 }
