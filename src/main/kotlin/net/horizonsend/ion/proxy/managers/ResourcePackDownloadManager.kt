7498b4405a095041d5ccfe623979d478824b9bff
Peter Crawley <peterzcrawley@gmail.com>
2022-06-15 17:46:47 +0100

    Auto Updating Resource Pack Prompt

---
+++ src/main/kotlin/net/horizonsend/ion/proxy/managers/ResourcePackDownloadManager.kt
@@ -0,0 +1,16 @@
+package net.horizonsend.ion.proxy.managers
+
+import java.net.URL
+
+object ResourcePackDownloadManager {
+	lateinit var resourcePackTag: String
+		private set
+
+	fun update() {
+		// Laziest JSON "Parsing" Ever
+		resourcePackTag = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
+			.readText()
+			.substringAfter("\",\n  \"tag_name\": \"")
+			.substringBefore("\",\n  ")
+	}
+}
