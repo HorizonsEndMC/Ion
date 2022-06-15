e0704c8d6c97a70cc70f922284e2d2188b07758f
Peter Crawley <peterzcrawley@gmail.com>
2022-06-15 17:46:47 +0100

    Auto Updating Resource Pack Prompt

--- src/main/kotlin/net/horizonsend/ion/proxy/listeners/ServerPostConnectListener.kt
+++ src/main/kotlin/net/horizonsend/ion/proxy/listeners/ServerPostConnectListener.kt
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.player.ServerPostConnectEvent
 import com.velocitypowered.api.proxy.ProxyServer
+import net.horizonsend.ion.proxy.managers.ResourcePackDownloadManager
 
 class ServerPostConnectListener(val server: ProxyServer) {
 	@Subscribe(order = PostOrder.LAST)
@@ -12,7 +13,7 @@ class ServerPostConnectListener(val server: ProxyServer) {
 	fun onServerPostConnectEvent(event: ServerPostConnectEvent): EventTask = EventTask.async {
 		event.player.sendResourcePackOffer(
 			server.createResourcePackBuilder(
-				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/Latest/HorizonsEndResourcePack.zip"
+				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/${ResourcePackDownloadManager.resourcePackTag}/HorizonsEndResourcePack.zip"
 			).build()
 		)
 	}
