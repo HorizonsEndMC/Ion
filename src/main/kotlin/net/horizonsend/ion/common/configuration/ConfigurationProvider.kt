08acdbd34855fbce721c7fb20b1d4634c8130ab3
Peter Crawley <peterzcrawley@gmail.com>
2022-06-16 17:40:23 +0100

    Save Plugin Configuration

--- src/main/kotlin/net/horizonsend/ion/common/configuration/ConfigurationProvider.kt
+++ src/main/kotlin/net/horizonsend/ion/common/configuration/ConfigurationProvider.kt
@@ -3,19 +3,28 @@ package net.horizonsend.ion.common.configuration
 import java.nio.file.Path
 import net.horizonsend.ion.common.configuration.server.ServerConfiguration
 import net.horizonsend.ion.common.configuration.shared.SharedConfiguration
+import org.spongepowered.configurate.CommentedConfigurationNode
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader
 import org.spongepowered.configurate.kotlin.extensions.get
 import org.spongepowered.configurate.kotlin.objectMapperFactory
 
 object ConfigurationProvider {
+	private lateinit var sharedConfigurationLoader: HoconConfigurationLoader
+
+	private lateinit var sharedConfigurationNode: CommentedConfigurationNode
+
 	lateinit var sharedConfiguration: SharedConfiguration
 		private set
+
+	private lateinit var serverConfigurationLoader: HoconConfigurationLoader
+
+	private lateinit var serverConfigurationNode: CommentedConfigurationNode
 
 	lateinit var serverConfiguration: ServerConfiguration
 		private set
 
 	fun loadConfiguration(pluginDirectory: Path) {
-		sharedConfiguration = HoconConfigurationLoader.builder()
+		sharedConfigurationLoader = HoconConfigurationLoader.builder()
 			.path(pluginDirectory.resolve("shared.conf"))
 			.defaultOptions { options ->
 				options.serializers { builder ->
@@ -23,10 +32,14 @@ object ConfigurationProvider {
 				}
 			}
 			.build()
-			.load()
-			.get()!!
 
-		serverConfiguration = HoconConfigurationLoader.builder()
+		sharedConfigurationNode = sharedConfigurationLoader.load()
+		sharedConfiguration = sharedConfigurationNode.get()!!
+
+		sharedConfigurationNode.set(sharedConfiguration)
+		sharedConfigurationLoader.save(sharedConfigurationNode)
+
+		serverConfigurationLoader = HoconConfigurationLoader.builder()
 			.path(pluginDirectory.resolve("server.conf"))
 			.defaultOptions { options ->
 				options.serializers { builder ->
@@ -34,7 +47,11 @@ object ConfigurationProvider {
 				}
 			}
 			.build()
-			.load()
-			.get()!!
+
+		serverConfigurationNode = serverConfigurationLoader.load()
+		serverConfiguration = serverConfigurationNode.get()!!
+
+		serverConfigurationNode.set(serverConfiguration)
+		serverConfigurationLoader.save(serverConfigurationNode)
 	}
 }
