b51414ce40d99cb3153bd1215410b73b96f2081b
Peter Crawley <peterzcrawley@gmail.com>
2022-06-19 14:18:55 +0100

    Remove Unused Configuration Values

--- src/main/kotlin/net/horizonsend/ion/common/configuration/server/ServerConfiguration.kt
+++ src/main/kotlin/net/horizonsend/ion/common/configuration/server/ServerConfiguration.kt
@@ -3,6 +3,4 @@ package net.horizonsend.ion.common.configuration.server
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @ConfigSerializable
-data class ServerConfiguration(
-	val serverType: ServerType = ServerType.SURVIVAL
-)
+class ServerConfiguration
