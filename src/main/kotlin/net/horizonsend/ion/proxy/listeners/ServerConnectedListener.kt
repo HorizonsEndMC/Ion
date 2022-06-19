f02a26139ed8d51c435dc12193e4acb9c5d71c5a
Peter Crawley <peterzcrawley@gmail.com>
2022-06-19 12:02:58 +0100

    Use ServerConnectedEvent

--- src/main/kotlin/net/horizonsend/ion/proxy/listeners/ServerConnectedListener.kt
+++ src/main/kotlin/net/horizonsend/ion/proxy/listeners/ServerConnectedListener.kt
@@ -3,14 +3,14 @@ package net.horizonsend.ion.proxy.listeners
 import com.velocitypowered.api.event.EventTask
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
-import com.velocitypowered.api.event.player.ServerPostConnectEvent
+import com.velocitypowered.api.event.player.ServerConnectedEvent
 import com.velocitypowered.api.proxy.ProxyServer
 import net.horizonsend.ion.proxy.managers.ResourcePackDownloadManager
 
-class ServerPostConnectListener(val server: ProxyServer) {
+class ServerConnectedListener(val server: ProxyServer) {
 	@Subscribe(order = PostOrder.LAST)
-	@Suppress("Unused", "UnstableApiUsage")
-	fun onServerPostConnectEvent(event: ServerPostConnectEvent): EventTask = EventTask.async {
+	@Suppress("Unused")
+	fun onServerConnectedEvent(event: ServerConnectedEvent): EventTask = EventTask.async {
 		event.player.sendResourcePackOffer(
 			server.createResourcePackBuilder(
 				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/${ResourcePackDownloadManager.resourcePackTag}/HorizonsEndResourcePack.zip"
