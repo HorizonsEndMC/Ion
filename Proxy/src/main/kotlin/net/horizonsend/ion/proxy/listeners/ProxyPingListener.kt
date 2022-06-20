b205bb4f15dd6b703a22436e04125a3dd101b35c
Astralchroma <astralchroma@proton.me>
2022-06-20 17:57:57 +0100

    Nice Server List Entry

---
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/ProxyPingListener.kt
@@ -0,0 +1,45 @@
+package net.horizonsend.ion.proxy.listeners
+
+import com.velocitypowered.api.event.EventTask
+import com.velocitypowered.api.event.PostOrder
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyPingEvent
+import com.velocitypowered.api.proxy.ProxyServer
+import com.velocitypowered.api.proxy.server.ServerPing
+import com.velocitypowered.api.util.Favicon
+import java.net.URL
+import java.util.Base64
+import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
+
+class ProxyPingListener(private val server: ProxyServer) {
+	private val version = ServerPing.Version(759, "1.19")
+
+	private val messages =
+		URL("https://raw.githubusercontent.com/HorizonsEndMC/MOTDs/main/MOTD")
+			.readText()
+			.split('\n')
+			.filterNot { it.isEmpty() }
+
+	private val motdLine1 =
+		miniMessage().deserialize("<b><blue>Horizon's End</blue></b> / <grey>A space server with working ships.</grey>\n")
+
+	private val icon =
+		Favicon(
+			"data:image/png;base64,${
+				Base64.getEncoder().encodeToString(
+					URL("https://github.com/HorizonsEndMC/ResourcePack/raw/main/pack.png").readBytes()		
+				)
+			}"
+		)
+
+	@Subscribe(order = PostOrder.NORMAL)
+	@Suppress("unused")
+	fun onProxyPingEvent(event: ProxyPingEvent): EventTask = EventTask.async {
+		event.ping = ServerPing(
+			version,
+			ServerPing.Players(server.playerCount, server.playerCount, listOf()),
+			motdLine1.append(miniMessage().deserialize(messages.random())),
+			icon
+		)
+	}
+}
