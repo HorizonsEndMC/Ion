51ac15f7fa36ee1a6f701cae8190faea9c89dfbc
Astralchroma <astralchroma@proton.me>
2022-06-20 14:30:55 +0100

    Gradle Configuration Overhaul

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,79 +1,97 @@
-import org.gradle.jvm.toolchain.JavaLanguageVersion.of
-
 plugins {
+	id("com.github.johnrengelman.shadow") version "7.1.2"
+	id("io.papermc.paperweight.userdev") version "1.3.7"
+	id("org.jetbrains.kotlin.jvm") version "1.7.0"
   java
-	id("com.github.johnrengelman.shadow") version "7.1.2" // ShadowJar
-	id("io.papermc.paperweight.userdev") version "1.3.7"  // Paperweight
-	kotlin("jvm") version "1.7.0"                         // Kotlin
-	kotlin("plugin.serialization") version "1.7.0"        // Kotlin Serialization
 }
 
 repositories {
 	mavenCentral()
 
+	maven("https://repo.papermc.io/repository/maven-public/")
+	maven("https://repo.aikar.co/content/groups/aikar/") // Annotation Command Framework (Paper), WorldEdit API
+	maven("https://jitpack.io") // khttp, VaultAPI, Dynmap (Spigot)
 	maven("https://repo.codemc.io/repository/maven-snapshots/") // AnvilGUI
 	maven("https://nexus.scarsz.me/content/groups/public/") // DiscordSRV
-	maven("https://repo.aikar.co/content/groups/aikar/") // Annotation Command Framework (Paper), WorldEdit API
 	maven("https://m2.dv8tion.net/releases") // JDA (Required by DiscordSRV)
 	maven("https://repo.citizensnpcs.co/") // Citizens
-	maven("https://jitpack.io") // khttp, VaultAPI, Dynmap (Spigot)
 }
 
 dependencies {
 	paperDevBundle("1.19-R0.1-SNAPSHOT")
 
-	// Provided by us
-		implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+	compileOnly("net.luckperms:api:5.4")
+	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+	compileOnly("net.citizensnpcs:citizens:2.0.27-SNAPSHOT")
+	compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
+	compileOnly("com.discordsrv:discordsrv:1.25.1")
 
-		// TODO: Remove
-			implementation("com.github.jkcclemens:khttp:0.1.0") {
-				exclude("org.jetbrains.kotlin") // Old Version + Provided by Server Library Loader
-				exclude("org.json") // Server Library Loader
-			}
-			implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT")
-
-	// Provided by other plugins
-		compileOnly("net.luckperms:api:5.4")
-		compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-		compileOnly("net.citizensnpcs:citizens:2.0.27-SNAPSHOT")
-		compileOnly("com.github.webbukkit.dynmap:spigot:3.1") {
-			exclude("org.bukkit") // Old Version
-		}
-		compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
-
-		// TODO: Remove
-			compileOnly("com.discordsrv:discordsrv:1.25.1")
-
-	// Provided by Server Library Loader
-		compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-		compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
-		compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
-		compileOnly("org.litote.kmongo:kmongo:4.6.0")
-
-		// TODO: Remove
-			compileOnly("io.github.config4k:config4k:0.4.2")
-			compileOnly("com.googlecode.cqengine:cqengine:3.6.0")
-			compileOnly("com.daveanthonythomas.moshipack:moshipack:1.0.1")
-
-			// Older Versions
-				compileOnly("redis.clients:jedis:3.7.1")
-				compileOnly("com.github.stefvanschie.inventoryframework:IF:0.5.8")
+	implementation("org.litote.kmongo:kmongo:4.6.0")
+	implementation("io.github.config4k:config4k:0.4.2")
+	implementation("com.googlecode.cqengine:cqengine:3.6.0")
+	implementation("com.daveanthonythomas.moshipack:moshipack:1.0.1")
+	implementation("redis.clients:jedis:3.7.1")
+	implementation("com.github.stefvanschie.inventoryframework:IF:0.5.8")
+	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+	implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT")
+	implementation("com.github.jkcclemens:khttp:0.1.0") {
+		exclude("org.jetbrains.kotlin") // Old Version
+	}
+	compileOnly("com.github.webbukkit.dynmap:spigot:3.1") {
+		exclude("org.bukkit") // Old Version
+	}
 }
 
 tasks {
-	compileJava {
-		options.compilerArgs.add("-parameters")
-	}
-
-	compileKotlin {
-		kotlinOptions.javaParameters = true
-		kotlinOptions.jvmTarget = "17"
-	}
-
 	shadowJar {
-		relocate("co.aikar.commands", "net.horizonsend.ion.core.libraries.co.aikar.commands")
-		relocate("co.aikar.locales", "net.horizonsend.ion.core.libraries.co.aikar.locales")
-		relocate("net.wesjd.anvilgui", "net.horizonsend.ion.core.libraries.net.wesjd.anvilgui")
+		arrayOf(
+			"co.aikar.commands",
+			"co.aikar.locales",
+			"com.daveanthonythomas.moshipack",
+			"com.esotericsoftware.asm",
+			"com.esotericsoftware.kryo",
+			"com.esotericsoftware.minlog",
+			"com.esotericsoftware.reflectasm",
+			"com.fasterxml.jackson.annotation",
+			"com.fasterxml.jackson.core",
+			"com.fasterxml.jackson.databind",
+			"com.fasterxml.jackson.module.kotlin",
+			"com.github.stefvanschie.inventoryframework",
+			"com.googlecode.concurrenttrees.common",
+			"com.googlecode.concurrenttrees.radix",
+			"com.googlecode.concurrenttrees.radixinverted",
+			"com.googlecode.concurrenttrees.radixreversed",
+			"com.googlecode.concurrenttrees.solver",
+			"com.googlecode.concurrenttrees.suffix",
+			"com.googlecode.cqengine",
+			"com.mongodb",
+			"com.squareup.moshi",
+			"com.typesafe.config",
+			"de.javakaffee.kryoserializers",
+			"de.undercouch.bson4jackson",
+			"io.github.config4k",
+			"javassist",
+			"khttp",
+			"kotlin",
+			"net.jodah.typetools",
+			"net.wesjd.anvilgui",
+			"okio",
+			"org.antlr.v4.runtime",
+			"org.apache.commons.pool2",
+			"org.bson",
+			"org.intellij.lang.annotations",
+			"org.jetbrains.annotations",
+			"org.json",
+			"org.litote.jackson",
+			"org.litote.kmongo",
+			"org.litote.kreflect",
+			"org.objenesis",
+			"org.slf4j",
+			"org.sqlite",
+			"redis.clients.jedis"
+		).forEach { group ->
+			relocate(group, "net.starlegacy.libraries.$group")
+		}
 	}
 
 	reobfJar {
@@ -84,5 +102,3 @@ tasks {
 		dependsOn("reobfJar")
 	}
 }
-
-java.toolchain.languageVersion.set(of(17))
