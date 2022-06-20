f075333cd707606b25720b98aa51ac7ce667371f
Peter Crawley <peterzcrawley@gmail.com>
2022-06-20 18:20:26 +0100

    Disconnect users on old versions.

---
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/PreLoginListener.kt
@@ -0,0 +1,20 @@
+package net.horizonsend.ion.proxy.listeners
+
+import com.velocitypowered.api.event.EventTask
+import com.velocitypowered.api.event.PostOrder
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.connection.PreLoginEvent
+import net.horizonsend.ion.common.utilities.feedback.FeedbackType
+import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
+
+class PreLoginListener {
+	private val disconnectMessage = miniMessage().deserialize("<${FeedbackType.USER_ERROR.colour}>Only version 1.19 can be used on Horizon's End.")
+
+	@Subscribe(order = PostOrder.FIRST)
+	@Suppress("Unused")
+	fun onPreLoginEvent(event: PreLoginEvent): EventTask = EventTask.async {
+		if (event.connection.protocolVersion.protocol == 759) return@async
+
+		event.result = PreLoginEvent.PreLoginComponentResult.denied(disconnectMessage)
+	}
+}
