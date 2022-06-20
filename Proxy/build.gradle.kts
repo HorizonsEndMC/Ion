6427381267b734b0e2f77c482d134cd39ed5d903
Peter Crawley <peterzcrawley@gmail.com>
2022-06-20 21:01:20 +0100

    Add Cloud Command Framework as a Dependency

--- Proxy/build.gradle.kts
+++ Proxy/build.gradle.kts
@@ -12,5 +12,8 @@ dependencies {
 	compileOnly("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
 
 	implementation(project(":Common"))
+
+	// Cloud Command Framework
+	implementation("cloud.commandframework:cloud-velocity:1.7.0")
 }
 
