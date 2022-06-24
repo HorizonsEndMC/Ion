c5c374c4570ef343827383368068aa0b9ef28389
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-06-24 16:47:20 +0100

    if u got grace period you will be sent wiki link when u log in

--- src/main/kotlin/net/horizonsend/ion/core/listeners/PlayerJoinListener.kt
+++ src/main/kotlin/net/horizonsend/ion/core/listeners/PlayerJoinListener.kt
@@ -1,6 +1,6 @@
 package net.horizonsend.ion.core.listeners
 
-import net.horizonsend.ion.core.commands.GracePeriod
+import net.horizonsend.ion.core.NewPlayerProtection.hasProtection
 import net.horizonsend.ion.core.feedback.FeedbackType.INFORMATION
 import net.horizonsend.ion.core.feedback.sendFeedbackActionMessage
 import net.starlegacy.listener.SLEventListener
@@ -12,6 +12,6 @@ object PlayerJoinListener : SLEventListener() {
 	@Suppress("Unused")
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	fun onPlayerJoin(event: PlayerJoinEvent){
-		if (GracePeriod.isGracePeriod) event.player.sendFeedbackActionMessage(INFORMATION, "We strongly advise you to read this guide, and please ask questions in chat ( https://wiki.horizonsend.net/wiki/New_Player_Guide )")
+		if (event.player.hasProtection()) event.player.sendFeedbackActionMessage(INFORMATION, "We strongly advise you to read this guide, and please ask questions in chat ( https://wiki.horizonsend.net/wiki/New_Player_Guide )")
 	}
 }
