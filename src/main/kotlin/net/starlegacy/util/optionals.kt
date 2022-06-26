ca01561a02308f678e98d2e83f5d8c14739ec10b
Astralchroma <astralchroma@proton.me>
2022-06-26 22:02:11 +0100

    Yeet a few deprecation warnings

--- src/main/kotlin/net/starlegacy/util/optionals.kt
+++ src/main/kotlin/net/starlegacy/util/optionals.kt
@@ -4,4 +4,4 @@ import java.util.Optional
 
 fun <T> Optional<T>.orNull(): T? = orElse(null)
 
-fun <T> optional(t: T?): Optional<T> = Optional.ofNullable(t)
+fun <T : Any> optional(t: T?): Optional<T> = Optional.ofNullable(t)
