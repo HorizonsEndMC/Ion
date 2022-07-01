11033cbda228b746a475c6477763e007065934f2
Vandrayk <108155098+Vandrayk@users.noreply.github.com>
2022-07-01 15:49:50 -0400

    Update BowHitListener.kt
    Fixed

--- src/main/kotlin/net/horizonsend/ion/core/listeners/BowHitListener.kt
+++ src/main/kotlin/net/horizonsend/ion/core/listeners/BowHitListener.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent
 
 object BowHitListener : SLEventListener() {
 	@EventHandler(priority = EventPriority.LOW)
+	@Suppress("unused")
 	fun onBowHit(event: EntityDamageByEntityEvent){
 		if(event.damager is Projectile && ProtectionListener.isProtectedCity(event.entity.location)) event.isCancelled = true
 	}
