261d5e261b3cb96bf72e016653665baf7a20a106
Peter Crawley <peterzcrawley@gmail.com>
2022-07-01 14:28:19 +0100

    Attempt to fix out-of-order chat anti-kick

--- Server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerKickListener.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerKickListener.kt
@@ -2,6 +2,7 @@ package net.horizonsend.ion.server.listeners
 
 import net.horizonsend.ion.common.utilities.feedback.FeedbackType
 import net.horizonsend.ion.common.utilities.feedback.sendFeedbackMessage
+import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -12,7 +13,7 @@ class PlayerKickListener : Listener {
 	@Suppress("Unused")
 	fun onPlayerKickEvent(event: PlayerKickEvent) {
 		// Really dumb solution for players being kicked due to "out of order chat messages"
-		if (event.reason().toString().lowercase().contains("out-of-order")) {
+		if (event.reason() == Component.translatable("multiplayer.disconnect.out_of_order_chat")) {
 			event.player.sendFeedbackMessage(
 				FeedbackType.SERVER_ERROR,
 				"The server attempted to kick you for out-of-order chat messages. You may need to retry any recent commands."
