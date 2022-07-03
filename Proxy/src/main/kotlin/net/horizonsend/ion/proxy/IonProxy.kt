fccf0fc4e585c3128cb5024cc79fa3f895e81bf8
Peter Crawley <peterzcrawley@gmail.com>
2022-07-04 00:01:59 +0100

    Fix ACF Issues

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
@@ -6,6 +6,7 @@ import com.velocitypowered.api.event.EventTask
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
+import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.proxy.ProxyServer
 import net.horizonsend.ion.proxy.commands.LinksCommand
 import net.horizonsend.ion.proxy.listeners.PreLoginListener
@@ -14,6 +15,7 @@ import net.horizonsend.ion.proxy.listeners.ServerConnectedListener
 import org.slf4j.Logger
 
 @Suppress("Unused")
+@Plugin(id = "ion", name = "Ion") // While we do not use this for generating velocity-plugin.json, ACF requires it.
 class IonProxy @Inject constructor(
 	private val proxy: ProxyServer,
 	@Suppress("Unused_Parameter") slF4JLogger: Logger
