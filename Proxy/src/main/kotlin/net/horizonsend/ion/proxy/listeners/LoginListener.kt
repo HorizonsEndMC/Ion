bce1188f96048f6dc71b41aaef7236dd7821082a
Peter Crawley <peterzcrawley@gmail.com>
2022-07-04 15:01:59 +0100

    Better Configuration

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/LoginListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/LoginListener.kt
@@ -4,19 +4,19 @@ import com.velocitypowered.api.event.EventTask
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.connection.LoginEvent
-import net.horizonsend.ion.common.configuration.ConfigurationProvider
+import net.horizonsend.ion.proxy.IonProxy
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
 
-class LoginListener {
+class LoginListener(private val plugin: IonProxy) {
 	@Subscribe(order = PostOrder.LAST)
 	@Suppress("Unused")
 	fun onLoginEvent(event: LoginEvent): EventTask = EventTask.async {
 		var headerComponent = Component.text().append(Component.text("\nHorizon's End\n", TextColor.color(255, 127, 63), TextDecoration.BOLD))
 
-		val headerText = ConfigurationProvider.proxyConfiguration.tablistHeaderMessage
+		val headerText = plugin.proxyConfiguration.tablistHeaderMessage
 
 		if (headerText.isNotEmpty()) {
 			headerComponent = headerComponent
