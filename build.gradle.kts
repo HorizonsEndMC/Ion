f6ebf3e618ac116b1fa4faafcfa449e56f477e2c
Peter Crawley <peterzcrawley@gmail.com>
2022-07-06 15:44:34 +0100

    Bring back run-paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("com.github.johnrengelman.shadow") version "7.1.2"
 	id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 	id("org.jetbrains.kotlin.jvm") version "1.7.0"
+	id("xyz.jpenilla.run-paper") version "1.0.6"
 }
 
 repositories {
@@ -22,6 +23,7 @@ tasks {
 		archiveFileName.set("../Ion.jar")
 	}
 
+	runServer { minecraftVersion("1.19") }
 	ktlint { version.set("0.44.0") }
 
 	prepareKotlinBuildScriptModel { dependsOn("addKtlintFormatGitPreCommitHook") }
