d3e4291891608b059a96471732ec63b443f73f4e
Peter Crawley <peterzcrawley@gmail.com>
2022-07-11 10:32:20 +0100

    Use new MessageEmbed Util

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
@@ -1,11 +1,11 @@
 package net.horizonsend.ion.proxy.commands.discord
 
 import com.velocitypowered.api.proxy.ProxyServer
-import net.dv8tion.jda.api.entities.EmbedType
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.horizonsend.ion.common.annotations.CommandMeta
 import net.horizonsend.ion.common.annotations.Default
+import net.horizonsend.ion.proxy.utilities.messageEmbed
 
 @CommandMeta("playerlist", "List online players.")
 class PlayerListCommand(private val proxy: ProxyServer) {
@@ -13,15 +13,13 @@ class PlayerListCommand(private val proxy: ProxyServer) {
 	@Suppress("Unused")
 	fun onPlayerListCommand(event: SlashCommandInteractionEvent) {
 		event.replyEmbeds(
-			MessageEmbed(null, null, null, EmbedType.RICH, null, 0xff7f3f, null, null, null, null, null, null,
-				proxy.allServers.map { server ->
-					MessageEmbed.Field(
-						"${server.serverInfo.name.replaceFirstChar { it.uppercase() }} *(${server.playersConnected.size} online)*",
-						server.playersConnected.joinToString("\n", "", "") { it.username }.ifBlank { "*No players online*" },
-						true
-					)
-				}
-			)
+			messageEmbed(fields = proxy.allServers.map { server ->
+				MessageEmbed.Field(
+					"${server.serverInfo.name.replaceFirstChar { it.uppercase() }} *(${server.playersConnected.size} online)*",
+					server.playersConnected.joinToString("\n", "", "") { it.username }.ifBlank { "*No players online*" },
+					true
+				)
+			})
 		).setEphemeral(true).queue()
 	}
 }
