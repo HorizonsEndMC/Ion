af178b78a0125ed63debd96a094180c8efae5bd5
Peter Crawley <peterzcrawley@gmail.com>
2022-07-11 13:23:51 +0100

    Remove ResourcePackDownloadManager.kt

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerConnectedListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerConnectedListener.kt
@@ -4,16 +4,21 @@ import com.velocitypowered.api.event.EventTask
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.player.ServerConnectedEvent
-import net.horizonsend.ion.proxy.managers.ResourcePackDownloadManager
+import java.net.URL
 import net.horizonsend.ion.proxy.proxy
 
 class ServerConnectedListener {
+	private val resourcePackTag: String = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
+		.readText()
+		.substringAfter("\",\"tag_name\":\"")
+		.substringBefore("\",")
+
 	@Suppress("Unused")
 	@Subscribe(order = PostOrder.LAST)
 	fun onServerConnectedEvent(event: ServerConnectedEvent): EventTask = EventTask.async {
 		event.player.sendResourcePackOffer(
 			proxy.createResourcePackBuilder(
-				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/${ResourcePackDownloadManager.resourcePackTag}/HorizonsEndResourcePack.zip"
+				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/${resourcePackTag}/HorizonsEndResourcePack.zip"
 			).build()
 		)
 	}
