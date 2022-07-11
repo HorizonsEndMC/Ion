ce18d3ce311991d3c53c31228975b455358de392
Peter Crawley <peterzcrawley@gmail.com>
2022-07-11 10:32:20 +0100

    Use new MessageEmbed Util

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/DiscordInfoCommand.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/DiscordInfoCommand.kt
@@ -1,10 +1,9 @@
 package net.horizonsend.ion.proxy.commands.discord
 
-import net.dv8tion.jda.api.entities.EmbedType
-import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.horizonsend.ion.common.annotations.CommandMeta
 import net.horizonsend.ion.common.annotations.Default
+import net.horizonsend.ion.proxy.utilities.messageEmbed
 
 @CommandMeta("info", "List of useful links.")
 class DiscordInfoCommand {
@@ -12,13 +11,13 @@ class DiscordInfoCommand {
 	@Suppress("Unused")
 	fun onInfoCommand(event: SlashCommandInteractionEvent) {
 		event.replyEmbeds(
-			MessageEmbed(null, "Here are a few links of potential use:",
+			messageEmbed(title = "Here are a few links of potential use:", description =
 				"[Survival Web Map](https://survival.horizonsend.net)\n" +
 				"[Creative Web Map](https://creative.horizonsend.net)\n" +
 				"[Discord Server](https://discord.gg/RPvgQsGzKM)\n" +
 				"[Resource Pack](https://github.com/HorizonsEndMC/ResourcePack)\n" +
 				"[Wiki](https://wiki.horizonsend.net)"
-			, EmbedType.RICH, null, 0xff7f3f, null, null, null, null, null, null, null)
+			)
 		).queue()
 	}
 }
