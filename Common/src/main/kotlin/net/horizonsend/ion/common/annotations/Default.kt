c6e49694516e7d45aed55d8ebfbc9f3878f9a0fe
Astralchroma <astralchroma@proton.me>
2022-07-11 09:48:18 +0100

    JDACommandManager

---
+++ Common/src/main/kotlin/net/horizonsend/ion/common/annotations/Default.kt
@@ -0,0 +1,8 @@
+package net.horizonsend.ion.common.annotations
+
+/**
+ * This annotation indicates the default implementation of a command class.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Default
