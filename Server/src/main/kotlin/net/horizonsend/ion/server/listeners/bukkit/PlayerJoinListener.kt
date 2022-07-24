e02c03e9fbfe293c9dd7a83bc6a9d97b2e016f68
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-07-24 21:43:59 +0100

    startOnSpawn

--- Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/PlayerJoinListener.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/PlayerJoinListener.kt
@@ -1,16 +1,19 @@
 package net.horizonsend.ion.server.listeners.bukkit
 
+import net.horizonsend.ion.server.IonServer
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.GameMode
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
-class PlayerJoinListener : Listener {
+class PlayerJoinListener(private val plugin: IonServer) : Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	@Suppress("Unused")
 	fun onPlayerJoinEvent(event: PlayerJoinEvent) {
@@ -29,6 +32,7 @@ class PlayerJoinListener : Listener {
 			kitstarterasking.itemMeta.displayName(
 				MiniMessage.miniMessage().deserialize("Please do /kit starter if you run out of steak or need a new set!").color(
 					TextColor.color(0, 255, 255)))
+			kitstarterasking.itemMeta.persistentDataContainer.set(NamespacedKey(plugin, "SpawnPaper"), PersistentDataType.BYTE, 1)
 			event.player.inventory.addItem(ItemStack(Material.COOKED_BEEF, 32))
 			event.player.inventory.addItem(ItemStack(Material.CLOCK))
 			event.player.inventory.addItem(kitstarterasking)
