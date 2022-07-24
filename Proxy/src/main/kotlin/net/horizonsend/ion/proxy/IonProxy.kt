a57ad2566781679f68015fa7d5cc7f550789ab74
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-07-24 21:53:16 +0100

    Player request to download resource pack

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxy.kt
@@ -28,6 +28,7 @@ import net.horizonsend.ion.proxy.commands.velocity.VelocityAccountCommand
 import net.horizonsend.ion.proxy.commands.velocity.VelocityInfoCommand
 import net.horizonsend.ion.proxy.listeners.velocity.DisconnectListener
 import net.horizonsend.ion.proxy.listeners.velocity.LoginListener
+import net.horizonsend.ion.proxy.listeners.velocity.PlayerResourcePackStatusListener
 import net.horizonsend.ion.proxy.listeners.velocity.PreLoginListener
 import net.horizonsend.ion.proxy.listeners.velocity.ProxyPingListener
 import net.horizonsend.ion.proxy.listeners.velocity.ServerConnectedListener
@@ -63,7 +64,7 @@ class IonProxy @Inject constructor(proxy0: ProxyServer, logger0: Logger, @DataDi
 		CommonManager.init(dataDirectory)
 
 		arrayOf(
-			LoginListener(), PreLoginListener(), ProxyPingListener(), ServerConnectedListener(), DisconnectListener()
+			LoginListener(), PreLoginListener(), ProxyPingListener(), ServerConnectedListener(), DisconnectListener(), PlayerResourcePackStatusListener()
 		).forEach {
 			proxy.eventManager.register(this, it)
 		}
