44a92585b8b0b2b76cdc7a74e4a59954f59de501
Peter Crawley <peterzcrawley@gmail.com>
2022-07-26 13:49:04 +0100

    Fix Resource Pack Info Message

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/PlayerResourcePackStatusListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/PlayerResourcePackStatusListener.kt
@@ -11,12 +11,10 @@ class PlayerResourcePackStatusListener {
 	@Subscribe(order = PostOrder.LAST)
 	@Suppress("Unused")
 	fun onPlayerResourcePackStatusListener(event: PlayerResourcePackStatusEvent): EventTask = EventTask.async {
-		if (event.status == PlayerResourcePackStatusEvent.Status.ACCEPTED) {
-			event.player.sendMessage(
-				MiniMessage.miniMessage()
-					.deserialize("<${FeedbackType.USER_ERROR.colour}>Please consider downloading the resource pack for better login times!")
-			)
-			event.player.sendMessage(MiniMessage.miniMessage().deserialize("https://github.com/HorizonsEndMC/ResourcePack"))
-		}
+		if (event.status != PlayerResourcePackStatusEvent.Status.ACCEPTED) return@async
+
+		event.player.sendMessage(MiniMessage.miniMessage().deserialize(
+			"<${FeedbackType.USER_ERROR.colour}>Please consider downloading the resource pack for better login times! <click:open_url:'https://github.com/HorizonsEndMC/ResourcePack'>https://github.com/HorizonsEndMC/ResourcePack</click>"
+		))
 	}
 }
