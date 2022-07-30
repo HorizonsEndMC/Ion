67bf62c7f287edfedaa3a649a329e1fd3f96ed86
Peter Crawley <peterzcrawley@gmail.com>
2022-07-30 14:09:11 +0100

    Fix Anvil and Enchanting Table

--- Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/PrepareItemEnchantListener.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/PrepareItemEnchantListener.kt
@@ -1,5 +1,6 @@
 package net.horizonsend.ion.server.listeners.bukkit
 
+import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.enchantments.EnchantmentOffer
 import org.bukkit.event.EventHandler
@@ -11,7 +12,10 @@ import org.bukkit.event.enchantment.PrepareItemEnchantEvent
 class PrepareItemEnchantListener : Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	fun onPrepareItemEnchantEvent(event: PrepareItemEnchantEvent) {
-		event.offers[0] = EnchantmentOffer(Enchantment.SILK_TOUCH, 1, 120)
+		event.offers[0] =
+			if (Enchantment.SILK_TOUCH.canEnchantItem(event.item) || event.item.type == Material.BOOK)
+				EnchantmentOffer(Enchantment.SILK_TOUCH, 1, 120)
+			else null
 		event.offers[1] = null
 		event.offers[2] = null
 	}
