8d5284fd1e2a7971662f363d63b04e239cfc70ff
Peter Crawley <peterzcrawley@gmail.com>
2022-08-04 16:34:39 +0100

    Add achievements

--- Server/src/main/kotlin/net/horizonsend/ion/server/commands/AchievementsCommand.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/commands/AchievementsCommand.kt
@@ -7,8 +7,13 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Optional
 import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.common.database.Achievement
+import net.horizonsend.ion.common.database.PlayerData
+import net.horizonsend.ion.common.utilities.feedback.FeedbackType
+import net.horizonsend.ion.common.utilities.feedback.sendFeedbackMessage
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.jetbrains.exposed.sql.transactions.transaction
 
 @CommandAlias("achievements")
 class AchievementsCommand : BaseCommand() {
@@ -22,8 +27,29 @@ class AchievementsCommand : BaseCommand() {
 	@Subcommand("grant")
 	@CommandCompletion("@players")
 	@CommandPermission("ion.achievements.grant")
-	fun onAchievementGrant(sender: CommandSender, target: String) {
+	fun onAchievementGrant(sender: CommandSender, achievementString: String, target: String) {
+		val achievement = try {
+			Achievement.valueOf(achievementString)
+		} catch (_: IllegalArgumentException) {
+			sender.sendFeedbackMessage(FeedbackType.USER_ERROR, "Achievement {0} does not exist.", achievementString)
+			return
+		}
 
+		val playerData = transaction { PlayerData.getByUsername(target) }
+
+		if (playerData == null) {
+			sender.sendFeedbackMessage(FeedbackType.USER_ERROR, "Player {0} does not exist.", target)
+			return
+		}
+
+		transaction { playerData.addAchievement(achievement) }
+
+		sender.sendFeedbackMessage(
+			FeedbackType.SUCCESS,
+			"Gave achievement {0} to {1}. Please note, rewards are not given automatically.",
+			achievement.name,
+			target
+		)
 	}
 
 	@Suppress("Unused")
