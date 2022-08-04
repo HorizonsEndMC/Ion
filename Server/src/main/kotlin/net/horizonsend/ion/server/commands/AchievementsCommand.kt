77eea642f2cde6c0123911106960e3beedde5911
Peter Crawley <peterzcrawley@gmail.com>
2022-08-04 13:59:58 +0100

    Achievement Command Boilerplate

---
+++ Server/src/main/kotlin/net/horizonsend/ion/server/commands/AchievementsCommand.kt
@@ -0,0 +1,35 @@
+package net.horizonsend.ion.server.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Subcommand
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("achievements")
+class AchievementsCommand : BaseCommand() {
+	@Default
+	@Suppress("Unused")
+	fun onAchievementsList(sender: Player) {
+
+	}
+
+	@Suppress("Unused")
+	@Subcommand("grant")
+	@CommandCompletion("@players")
+	@CommandPermission("ion.achievements.grant")
+	fun onAchievementGrant(sender: CommandSender, target: String) {
+
+	}
+
+	@Suppress("Unused")
+	@Subcommand("revoke")
+	@CommandCompletion("@players")
+	@CommandPermission("ion.achievements.revoke")
+	fun onAchievementRevoke(sender: CommandSender, target: String) {
+
+	}
+}
