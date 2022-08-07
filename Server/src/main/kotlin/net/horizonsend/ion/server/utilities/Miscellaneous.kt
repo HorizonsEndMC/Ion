855aa9f416ab03f835a45aa0ae028e14d5dc3b94
Peter Crawley <peterzcrawley@gmail.com>
2022-08-07 12:13:42 +0100

    IonCore helper function

--- Server/src/main/kotlin/net/horizonsend/ion/server/utilities/Miscellaneous.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/utilities/Miscellaneous.kt
@@ -1,9 +1,15 @@
 package net.horizonsend.ion.server.utilities
 
 import net.horizonsend.ion.common.utilities.enumSetOf
+import org.bukkit.Bukkit
 import org.bukkit.Material
 
 val forbiddenCraftingItems = enumSetOf(
 	Material.WARPED_FUNGUS_ON_A_STICK, Material.NETHERITE_AXE, Material.NETHERITE_HOE, Material.NETHERITE_SWORD,
 	Material.NETHERITE_PICKAXE, Material.NETHERITE_SHOVEL
 )
+
+inline fun ionCore(execute: () -> Unit) {
+	if (!Bukkit.getPluginManager().isPluginEnabled("IonCore")) return
+	try { execute() } catch (_: NoClassDefFoundError) {}
+}
