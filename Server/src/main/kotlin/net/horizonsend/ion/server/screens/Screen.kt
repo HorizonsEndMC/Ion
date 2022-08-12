e572c0198e7767c5d10c72e6732f2cf1d96aed88
Peter Crawley <peterzcrawley@gmail.com>
2022-08-12 18:04:49 +0100

    Player.rewardAchievement(Achievement)

--- Server/src/main/kotlin/net/horizonsend/ion/server/screens/Screen.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/screens/Screen.kt
@@ -1,5 +1,8 @@
 package net.horizonsend.ion.server.screens
 
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
 
-abstract class Screen(val inventory: Inventory)
+abstract class Screen(val inventory: Inventory) {
+	open fun handleInventoryClick(event: InventoryClickEvent) {}
+}
