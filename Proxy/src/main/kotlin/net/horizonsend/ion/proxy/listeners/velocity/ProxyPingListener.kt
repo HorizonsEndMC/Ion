250819fc82f1fc222d8cb68fbe0d42f74a0b1034
Astralchroma <astralchroma@proton.me>
2022-08-17 15:19:22 +0100

    Cleanup ProxyPingListener

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ProxyPingListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ProxyPingListener.kt
@@ -8,35 +8,36 @@ import com.velocitypowered.api.proxy.server.ServerPing
 import com.velocitypowered.api.util.Favicon
 import java.net.URL
 import java.util.Base64
+import net.horizonsend.ion.proxy.IonProxy
 import net.horizonsend.ion.proxy.annotations.VelocityListener
-import net.horizonsend.ion.proxy.proxy
-import net.horizonsend.ion.proxy.proxyConfiguration
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
 @VelocityListener
 @Suppress("Unused")
-class ProxyPingListener {
+class ProxyPingListener(private val plugin: IonProxy) {
+	private val version = ServerPing.Version(760, "1.19.1/2")
+
 	private val messages =
 		URL("https://raw.githubusercontent.com/HorizonsEndMC/MOTDs/main/MOTD")
 			.readText()
 			.split('\n')
 			.filterNot { it.isEmpty() }
 
-	private val icon =
-		Favicon(
-			"data:image/png;base64,${
-				Base64.getEncoder().encodeToString(
-					URL("https://github.com/HorizonsEndMC/ResourcePack/raw/main/pack.png").readBytes()
-				)
-			}"
-		)
+	private val icon = kotlin.run {
+		val image = URL("https://github.com/HorizonsEndMC/ResourcePack/raw/main/pack.png").readBytes()
+		Favicon("data:image/png;base64,${Base64.getEncoder().encodeToString(image)}")
+	}
 
 	@Subscribe(order = PostOrder.NORMAL)
 	fun onProxyPingEvent(event: ProxyPingEvent): EventTask = EventTask.async {
 		event.ping = ServerPing(
-			ServerPing.Version(760, "1.19.1/2"),
-			ServerPing.Players(proxy.playerCount, proxy.playerCount + 1, listOf()),
-			miniMessage().deserialize("${proxyConfiguration.motdFirstLine}\n")
+			version,
+			ServerPing.Players(
+				plugin.velocity.playerCount,
+				plugin.velocity.playerCount + 1,
+				plugin.velocity.allPlayers.map {ServerPing.SamplePlayer(it.username, it.uniqueId) }
+			),
+			miniMessage().deserialize("${plugin.configuration.motdFirstLine}\n")
 				.append(miniMessage().deserialize(messages.random())),
 			icon
 		)
