a05be62e204291e8200e15a4ddd68c9f2c7e4652
Peter Crawley <peterzcrawley@gmail.com>
2022-08-17 15:19:33 +0100

    Cleanup ServerConnectedListener

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerConnectedListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerConnectedListener.kt
@@ -5,23 +5,38 @@ import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.player.ServerConnectedEvent
 import java.net.URL
+import java.security.MessageDigest
+import net.horizonsend.ion.proxy.IonProxy
 import net.horizonsend.ion.proxy.annotations.VelocityListener
-import net.horizonsend.ion.proxy.proxy
+import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
 @VelocityListener
 @Suppress("Unused")
-class ServerConnectedListener {
-	private val resourcePackTag: String = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
-		.readText()
-		.substringAfter("\",\"tag_name\":\"")
-		.substringBefore("\",")
+class ServerConnectedListener(private val plugin: IonProxy) {
+	private val resourcePackOffer = kotlin.run {
+		val tag = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
+			.readText()
+			.substringAfter("\",\"tag_name\":\"")
+			.substringBefore("\",")
+
+		val url = "https://github.com/HorizonsEndMC/ResourcePack/releases/download/$tag/HorizonsEndResourcePack.zip"
+
+		val fileHash = MessageDigest
+			.getInstance("SHA-1")
+			.digest(URL(url).readBytes())
+
+		plugin.velocity.createResourcePackBuilder(url)
+			.setHash(fileHash)
+			.setPrompt(miniMessage().deserialize("""
+				Horizon's End uses a Resource Pack to achieve many mod-like features. While you can play on the
+				server without the Resource Pack, it is not recommended to do so as it makes it harder to play on the
+				server.
+			""".trimIndent()))
+			.build()
+	}
 
 	@Subscribe(order = PostOrder.LAST)
 	fun onServerConnectedEvent(event: ServerConnectedEvent): EventTask = EventTask.async {
-		event.player.sendResourcePackOffer(
-			proxy.createResourcePackBuilder(
-				"https://github.com/HorizonsEndMC/ResourcePack/releases/download/${resourcePackTag}/HorizonsEndResourcePack.zip"
-			).build()
-		)
+		event.player.sendResourcePackOffer(resourcePackOffer)
 	}
 }
