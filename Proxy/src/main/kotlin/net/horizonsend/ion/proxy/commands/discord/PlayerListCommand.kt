7a68de81c69c8fb671746cc796cbe03c0c348364
Peter Crawley <peterzcrawley@gmail.com>
2022-08-17 15:41:36 +0100

    Refactor PlayerListCommand

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
@@ -3,26 +3,32 @@ package net.horizonsend.ion.proxy.commands.discord
 import co.aikar.commands.annotation.Default
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.horizonsend.ion.proxy.IonProxy
 import net.horizonsend.ion.proxy.annotations.CommandMeta
-import net.horizonsend.ion.proxy.proxy
 import net.horizonsend.ion.proxy.utilities.messageEmbed
 
 @CommandMeta("playerlist", "List online players.")
-class PlayerListCommand {
+class PlayerListCommand(private val plugin: IonProxy) {
 	@Default
 	@Suppress("Unused")
 	fun onPlayerListCommand(event: SlashCommandInteractionEvent) {
 		event.replyEmbeds(
-			messageEmbed(fields = proxy.allServers
+			messageEmbed(fields = plugin.velocity.allServers
 				.filter { it.playersConnected.isNotEmpty() }
 				.map { server ->
+					val serverName = server.serverInfo.name.replaceFirstChar { it.uppercase() }
+
 					MessageEmbed.Field(
-						"${server.serverInfo.name.replaceFirstChar { it.uppercase() }} *(${server.playersConnected.size} online)*",
-						server.playersConnected.joinToString("\n", "", "") { it.username.replace("_", "\\_") },
+						"$serverName *(${server.playersConnected.size} online)*",
+						server.playersConnected.joinToString("\n", "", "") {
+							it.username.replace("_", "\\_")
+						},
 						true
 					)
 				}
-				.ifEmpty { listOf(MessageEmbed.Field(null, "*No players online*", true)) }
+				.ifEmpty {
+					listOf(MessageEmbed.Field(null, "*No players online*", true))
+				}
 			)
 		).setEphemeral(true).queue()
 	}
