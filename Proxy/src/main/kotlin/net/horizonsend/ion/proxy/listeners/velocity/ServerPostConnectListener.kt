43d8ed62572f2315b5802ebbc470283a2aabd0e3
Astralchroma <astralchroma@proton.me>
2022-08-18 15:01:26 +0100

    Fix Resource Pack Auto-Prompt

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerPostConnectListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/velocity/ServerPostConnectListener.kt
@@ -3,16 +3,15 @@ package net.horizonsend.ion.proxy.listeners.velocity
 import com.velocitypowered.api.event.EventTask
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
-import com.velocitypowered.api.event.player.ServerConnectedEvent
+import com.velocitypowered.api.event.player.ServerPostConnectEvent
 import com.velocitypowered.api.proxy.ProxyServer
 import java.net.URL
 import java.security.MessageDigest
 import net.horizonsend.ion.proxy.annotations.VelocityListener
-import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
 @VelocityListener
 @Suppress("Unused")
-class ServerConnectedListener(private val velocity: ProxyServer) {
+class ServerPostConnectListener(private val velocity: ProxyServer) {
 	private val resourcePackOffer = kotlin.run {
 		val tag = URL("https://api.github.com/repos/HorizonsEndMC/ResourcePack/releases/latest")
 			.readText()
@@ -21,22 +20,16 @@ class ServerConnectedListener(private val velocity: ProxyServer) {
 
 		val url = "https://github.com/HorizonsEndMC/ResourcePack/releases/download/$tag/HorizonsEndResourcePack.zip"
 
-		val fileHash = MessageDigest
-			.getInstance("SHA-1")
-			.digest(URL(url).readBytes())
-
 		velocity.createResourcePackBuilder(url)
-			.setHash(fileHash)
-			.setPrompt(miniMessage().deserialize("""
-				Horizon's End uses a Resource Pack to achieve many mod-like features. While you can play on the
-				server without the Resource Pack, it is not recommended to do so as it makes it harder to play on the
-				server.
-			""".trimIndent()))
+			.setHash(
+				MessageDigest.getInstance("SHA-1")
+					.digest(URL(url).readBytes())
+			)
 			.build()
 	}
 
 	@Subscribe(order = PostOrder.LAST)
-	fun onServerConnectedEvent(event: ServerConnectedEvent): EventTask = EventTask.async {
+	fun onServerConnectedEvent(event: ServerPostConnectEvent): EventTask = EventTask.async {
 		event.player.sendResourcePackOffer(resourcePackOffer)
 	}
 }
