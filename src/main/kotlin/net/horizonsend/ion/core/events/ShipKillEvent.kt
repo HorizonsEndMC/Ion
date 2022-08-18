2ce8bbb8cc5d94c04fe7cdc00d4c1a875ad29728
Peter Crawley <peterzcrawley@gmail.com>
2022-08-18 01:35:02 +0100

    I really really really really really hate events

--- src/main/kotlin/net/horizonsend/ion/core/events/ShipKillEvent.kt
+++ src/main/kotlin/net/horizonsend/ion/core/events/ShipKillEvent.kt
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class ShipKillEvent(val player: Player, val killer: Player) : Event(true) {
+class ShipKillEvent(val player: Player, val killer: Player) : Event() {
 	override fun getHandlers(): HandlerList {
 		return handlerList
 	}
