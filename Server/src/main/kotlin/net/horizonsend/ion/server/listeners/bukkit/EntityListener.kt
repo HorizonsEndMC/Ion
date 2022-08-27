6d4636a4a6adf2129549bd7a668fb8cd884b63fe
Gutin1 <Claytonpinter@gmail.com>
2022-08-27 01:50:26 -0500

    disable end crystals

--- Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/EntityListener.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/listeners/bukkit/EntityListener.kt
@@ -2,7 +2,6 @@ package net.horizonsend.ion.server.listeners.bukkit
 
 import net.horizonsend.ion.common.utilities.feedback.FeedbackType
 import net.horizonsend.ion.common.utilities.feedback.sendFeedbackActionMessage
-import net.minecraft.world.entity.boss.enderdragon.EndCrystal
 import org.bukkit.entity.EnderCrystal
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
