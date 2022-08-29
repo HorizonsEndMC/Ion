f3752fa7099f4a5db3a617a046b5bef9cead4eb9
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 22:39:02 +0100

    Remove Unused LuckPerms Dependency

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
 
 	compileOnly(project(":IonCore"))
 	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-	compileOnly("net.luckperms:api:5.4")
 
 	implementation(project(":Common"))
 
