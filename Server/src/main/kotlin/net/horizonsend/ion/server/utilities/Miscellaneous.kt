aa193cfee9c2340ea947eb14df760e7d05634ebb
Peter Crawley <peterzcrawley@gmail.com>
2022-09-08 17:36:39 +0100

    Cleanup Miscellaneous.kt

--- Server/src/main/kotlin/net/horizonsend/ion/server/utilities/Miscellaneous.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/utilities/Miscellaneous.kt
@@ -4,13 +4,15 @@ import java.util.EnumSet
 import net.milkbowl.vault.economy.Economy
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 
 val forbiddenCraftingItems = enumSetOf(
 	Material.WARPED_FUNGUS_ON_A_STICK, Material.NETHERITE_AXE, Material.NETHERITE_HOE, Material.NETHERITE_SWORD,
 	Material.NETHERITE_PICKAXE, Material.NETHERITE_SHOVEL
 )
 
-@Deprecated("Safety wrapper for code using IonCore, should be replaced to not do so if possible.")
+@ScheduledForRemoval
+@Deprecated("IonCore Compatibility")
 inline fun ionCore(execute: () -> Unit) {
 	if (!Bukkit.getPluginManager().isPluginEnabled("IonCore")) return
 	try {
@@ -23,7 +25,6 @@ inline fun vaultEconomy(execute: (Economy) -> Unit) {
 	execute(Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider ?: return)
 }
 
+
 inline fun <reified T : Enum<T>> enumSetOf(vararg elems: T): EnumSet<T> =
-	EnumSet.noneOf(T::class.java).apply {
-		addAll(elems)
-	}
+	EnumSet.noneOf(T::class.java).apply { addAll(elems) }
