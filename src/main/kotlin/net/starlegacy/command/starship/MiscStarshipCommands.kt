78cbc591918e1e0847b364a8d2983ea574a083bf
trainb0y <trainb0ydev@gmail.com>
2022-09-17 19:35:13 -0600

    Add planet name autocompletion

--- src/main/kotlin/net/starlegacy/command/starship/MiscStarshipCommands.kt
+++ src/main/kotlin/net/starlegacy/command/starship/MiscStarshipCommands.kt
@@ -113,6 +113,7 @@ object MiscStarshipCommands : SLCommand() {
 	}
 
 	@CommandAlias("jump")
+	@CommandCompletion("@planets")
 	fun onJump(sender: Player, planet: String) {
 		val starship: ActivePlayerStarship = getStarshipPiloting(sender)
 
