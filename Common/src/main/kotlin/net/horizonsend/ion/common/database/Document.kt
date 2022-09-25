3a032dda32c2f04281cac0df1cbc1b031d760314
Peter Crawley <peterzcrawley@gmail.com>
2022-09-25 20:48:02 +0100

    better uuid handling

--- Common/src/main/kotlin/net/horizonsend/ion/common/database/Document.kt
+++ Common/src/main/kotlin/net/horizonsend/ion/common/database/Document.kt
@@ -1,11 +1,6 @@
 package net.horizonsend.ion.common.database
 
-import org.bson.codecs.pojo.annotations.BsonId
-import java.util.UUID
-
-abstract class Document(
-	@BsonId val uuid: UUID
-) {
+abstract class Document {
 	internal abstract fun update()
 }
 
