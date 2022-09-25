a02d6cbd87abe0eb21f8e6a6e37683be8534ef4b
Astralchroma <astralchroma@proton.me>
2022-09-25 20:48:02 +0100

    better uuid handling

--- Common/src/main/kotlin/net/horizonsend/ion/common/database/Collection.kt
+++ Common/src/main/kotlin/net/horizonsend/ion/common/database/Collection.kt
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoDatabase
 import org.litote.kmongo.findOneById
 import org.litote.kmongo.replaceOneById
 import org.litote.kmongo.util.KMongoUtil
+import org.litote.kmongo.util.idValue
 import java.util.UUID
 import kotlin.reflect.KClass
 
@@ -18,7 +19,7 @@ abstract class Collection<D : Document>(private val kClass: KClass<D>) {
 	protected abstract fun construct(id: UUID): D
 
 	protected fun update(document: D) {
-		collection.replaceOneById(document.uuid, document)
+		collection.replaceOneById(document.idValue!!, document)
 	}
 
 	operator fun get(id: UUID): D {
