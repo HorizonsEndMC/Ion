0396a583fafc1f35f84769bcc0b6448b130ce7a5
Peter Crawley <peterzcrawley@gmail.com>
2022-10-03 16:50:32 +0100

    LoginListener Cleanup

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/LoginListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/LoginListener.kt
@@ -1,26 +1,31 @@
 package net.horizonsend.ion.proxy.listeners
 
-import net.dv8tion.jda.api.JDA
 import net.horizonsend.ion.common.database.collections.PlayerData
 import net.horizonsend.ion.common.database.update
-import net.horizonsend.ion.proxy.ProxyConfiguration
+import net.horizonsend.ion.proxy.IonProxy
 import net.md_5.bungee.api.event.LoginEvent
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
 import net.md_5.bungee.event.EventPriority
 
-class LoginListener(private val configuration: ProxyConfiguration, private val jda: JDA) : Listener {
+class LoginListener : Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	fun onLoginEvent(event: LoginEvent) {
-		val memberId = PlayerData[event.connection.uniqueId]
-			.update { minecraftUsername = event.connection.name }
-			.discordId ?: return
+		val playerData = PlayerData[event.connection.uniqueId]
 
-		val guild = jda.getGuildById(configuration.discordServer) ?: return
+		if (playerData.minecraftUsername != event.connection.name) {
+			playerData.update { minecraftUsername = event.connection.name }
+		}
 
-		guild.addRoleToMember(
-			guild.getMemberById(memberId) ?: return,
-			guild.getRoleById(configuration.onlineRole) ?: return
-		).queue()
+		IonProxy.jda?.let { jda ->
+			val discordId = playerData.discordId ?: return
+
+			val guild = jda.getGuildById(IonProxy.configuration.discordServer) ?: return
+
+			guild.addRoleToMember(
+				guild.getMemberById(discordId) ?: return,
+				guild.getRoleById(IonProxy.configuration.onlineRole) ?: return
+			).queue()
+		}
 	}
 }
