a1208dddc0d08d8bb0c376ac0147218f6c1d46bc
Peter Crawley <peterzcrawley@gmail.com>
2022-10-25 15:12:38 +0100

    Cleanup some code + Fix Disconnect Event

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/VotifierListener.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/listeners/VotifierListener.kt
@@ -3,7 +3,7 @@ package net.horizonsend.ion.proxy.listeners
 import com.vexsoftware.votifier.bungee.events.VotifierEvent
 import net.horizonsend.ion.common.database.collections.PlayerData
 import net.horizonsend.ion.common.database.update
-import net.horizonsend.ion.proxy.IonProxy
+import net.horizonsend.ion.proxy.IonProxy.Companion.Ion
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
 import net.md_5.bungee.event.EventPriority
@@ -13,7 +13,7 @@ class VotifierListener : Listener {
 	fun onVotifierEvent(event: VotifierEvent) {
 		val site: String = event.vote.address
 
-		if (IonProxy.configuration.voteSites.contains(site)) {
+		if (Ion.configuration.voteSites.contains(site)) {
 			PlayerData[event.vote.username]?.update {
 				voteTimes[site] = System.currentTimeMillis()
 			}
