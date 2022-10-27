01ae09df20fd998a55a3f5ea2b4a944a18ccc7a1
Astralchroma <astralchroma@proton.me>
2022-10-27 19:01:07 +0100

    Configuration improvements

--- Common/src/main/kotlin/net/horizonsend/ion/common/Configuration.kt
+++ Common/src/main/kotlin/net/horizonsend/ion/common/Configuration.kt
@@ -1,16 +1,20 @@
 package net.horizonsend.ion.common
 
 import io.leangen.geantyref.TypeToken
-import java.io.File
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader
 import org.spongepowered.configurate.kotlin.objectMapperFactory
 import org.spongepowered.configurate.kotlin.toNode
+import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.createDirectories
 
-inline fun <reified T> loadConfiguration(directory: File, fileName: String): T {
-	directory.mkdirs()
+inline fun <reified T> loadConfiguration(file: File, fileName: String): T = loadConfiguration(file.toPath(), fileName)
+
+inline fun <reified T> loadConfiguration(path: Path, fileName: String): T {
+	path.createDirectories()
 
 	val loader = HoconConfigurationLoader.builder()
-		.path(directory.resolve(fileName).toPath())
+		.path(path.resolve(fileName))
 		.defaultOptions { options ->
 			options.serializers { builder ->
 				builder.registerAnnotatedObjects(objectMapperFactory())
