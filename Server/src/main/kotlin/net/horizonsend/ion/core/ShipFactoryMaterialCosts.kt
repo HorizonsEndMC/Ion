e14d94e32dd4e376fb1d3ab44b7720be92823b4b
Peter Crawley <peterzcrawley@gmail.com>
2022-11-05 17:34:38 +0000

    cleanup ShipFactoryMaterialCosts.kt

--- Server/src/main/kotlin/net/horizonsend/ion/core/ShipFactoryMaterialCosts.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/core/ShipFactoryMaterialCosts.kt
@@ -11,131 +11,133 @@ import net.starlegacy.util.isStainedTerracotta
 import net.starlegacy.util.isStairs
 import net.starlegacy.util.isWall
 import net.starlegacy.util.isWallSign
-import org.bukkit.Material
+import org.bukkit.Material.*
 import org.bukkit.block.data.BlockData
 
 object ShipFactoryMaterialCosts {
-	var blockprice = mapOf<Material, Double>(
-		Material.IRON_BLOCK to 50.0,
-		Material.DIAMOND_BLOCK to 50.0,
-		Material.COPPER_BLOCK to 175.0,
-		Material.GOLD_BLOCK to 50.0,
-		Material.EXPOSED_COPPER to 175.0,
-		Material.WEATHERED_COPPER to 175.0,
-		Material.OXIDIZED_COPPER to 175.0,
-		Material.WAXED_COPPER_BLOCK to 175.0,
-		Material.WAXED_EXPOSED_COPPER to 175.0,
-		Material.WAXED_WEATHERED_COPPER to 175.0,
-		Material.WAXED_OXIDIZED_COPPER to 175.0,
-		Material.REDSTONE_BLOCK to 100.0,
+	var blockprice = mapOf(
+		IRON_BLOCK to 50.0,
+		DIAMOND_BLOCK to 50.0,
+		COPPER_BLOCK to 175.0,
+		GOLD_BLOCK to 50.0,
+		EXPOSED_COPPER to 175.0,
+		WEATHERED_COPPER to 175.0,
+		OXIDIZED_COPPER to 175.0,
+		WAXED_COPPER_BLOCK to 175.0,
+		WAXED_EXPOSED_COPPER to 175.0,
+		WAXED_WEATHERED_COPPER to 175.0,
+		WAXED_OXIDIZED_COPPER to 175.0,
+		REDSTONE_BLOCK to 100.0,
 
-		Material.GRINDSTONE to 50.0,
-		Material.END_ROD to 5.0,
-		Material.BELL to 50.0,
-		Material.SPONGE to 2.0,
-		Material.END_PORTAL_FRAME to 500.0,
-		Material.PISTON to 20.0,
-		Material.STICKY_PISTON to 50.0,
-		Material.REDSTONE to 1.0,
-		Material.REDSTONE_LAMP to 25.0,
-		Material.JUKEBOX to 50.0,
-		Material.CRAFTING_TABLE to 30.0,
-		Material.DROPPER to 15.0,
-		Material.HOPPER to 50.0,
-		Material.LEVER to 1.0,
-		Material.CHEST to 5.0,
-		Material.FURNACE to 15.0,
-		Material.DISPENSER to 25.0,
-		Material.MAGMA_BLOCK to 35.0,
-		Material.SEA_LANTERN to 25.0,
-		Material.IRON_TRAPDOOR to 50.0,
-		Material.NOTE_BLOCK to 20.0,
-		Material.IRON_BARS to 10.0,
-		Material.OBSERVER to 5.0,
-		Material.TRAPPED_CHEST to 5.0,
-		Material.BARREL to 5.0
+		GRINDSTONE to 50.0,
+		END_ROD to 5.0,
+		BELL to 50.0,
+		SPONGE to 2.0,
+		END_PORTAL_FRAME to 500.0,
+		PISTON to 20.0,
+		STICKY_PISTON to 50.0,
+		REDSTONE to 1.0,
+		REDSTONE_LAMP to 25.0,
+		JUKEBOX to 50.0,
+		CRAFTING_TABLE to 30.0,
+		DROPPER to 15.0,
+		HOPPER to 50.0,
+		LEVER to 1.0,
+		CHEST to 5.0,
+		FURNACE to 15.0,
+		DISPENSER to 25.0,
+		MAGMA_BLOCK to 35.0,
+		SEA_LANTERN to 25.0,
+		IRON_TRAPDOOR to 50.0,
+		NOTE_BLOCK to 20.0,
+		IRON_BARS to 10.0,
+		OBSERVER to 5.0,
+		TRAPPED_CHEST to 5.0,
+		BARREL to 5.0,
 		
-		Material.OAK_PLANKS to 0.1
-		Material.SPRUCE_PLANKS to 0.1
-		Material.BIRCH_PLANKS to 0.1
-		Material.JUNGLE_PLANKS to 0.1
-		Material.ACACIA_PLANKS to 0.1
-		Material.DARK_OAK_PLANKS to 0.1
-		Material.MANGROVE_PLANKS to 0.1
-		Material.CRIMSON_PLANKS to 0.1
-		Material.WARPED_PLANKS to 0.1
-		Material.STONE to 0.1
-		Material.SMOOTH_STONE to 0.1
-		Material.SANDSTONE to 0.1
-		Material.CUT_SANDSTONE to 0.1
-		Material.COBBLESTONE to 0.1
-		Material.BRICKS to 0.1
-		Material.STONE_BRICKS to 0.1
-		Material.MUD_BRICKS to 0.1
-		Material.NETHER_BRICKS to 0.1
-		Material.QUARTZ_BLOCK to 0.1
-		Material.RED_SANDSTONE to 0.1
-		Material.CUT_RED_SANDSTONE to 0.1
-		Material.PURPUR_BLOCK to 0.1
-		Material.PRISMARINE to 0.1
-		Material.PRISMARINE_BRICKS to 0.1
-		Material.DARK_PRISMARINE to 0.1
-		Material.POLISHED_GRANITE to 0.1
-		Material.SMOOTH_RED_SANDSTONE to 0.1
-		Material.MOSSY_STONE_BRICKS to 0.1
-		Material.POLISHED_DIORITE to 0.1
-		Material.MOSSY_COBBLESTONE to 0.1
-		Material.END_STONE_BRICKS to 0.1
-		Material.SMOOTH_SANDSTONE to 0.1
-		Material.SMOOTH_QUARTZ to 0.1
-		Material.GRANITE to 0.1
-		Material.ANDESITE to 0.1
-		Material.RED_NETHER_BRICKS to 0.1
-		Material.POLISHED_ANDESITE to 0.1
-		Material.DIORITE to 0.1
-		Material.COBBLED_DEEPSLATE to 0.1
-		Material.POLISHED_DEEPSLATE to 0.1
-		Material.DEEPSLATE_BRICKS to 0.1
-		Material.DEEPSLATE_TILES to 0.1
-		Material.BLACKSTONE to 0.1
-		Material.POLISHED_BLACKSTONE to 0.1
-		Material.POLISHED_BLACKSTONE_BRICKS to 0.1
+		OAK_PLANKS to 0.1,
+		SPRUCE_PLANKS to 0.1,
+		BIRCH_PLANKS to 0.1,
+		JUNGLE_PLANKS to 0.1,
+		ACACIA_PLANKS to 0.1,
+		DARK_OAK_PLANKS to 0.1,
+		MANGROVE_PLANKS to 0.1,
+		CRIMSON_PLANKS to 0.1,
+		WARPED_PLANKS to 0.1,
+		STONE to 0.1,
+		SMOOTH_STONE to 0.1,
+		SANDSTONE to 0.1,
+		CUT_SANDSTONE to 0.1,
+		COBBLESTONE to 0.1,
+		BRICKS to 0.1,
+		STONE_BRICKS to 0.1,
+		MUD_BRICKS to 0.1,
+		NETHER_BRICKS to 0.1,
+		QUARTZ_BLOCK to 0.1,
+		RED_SANDSTONE to 0.1,
+		CUT_RED_SANDSTONE to 0.1,
+		PURPUR_BLOCK to 0.1,
+		PRISMARINE to 0.1,
+		PRISMARINE_BRICKS to 0.1,
+		DARK_PRISMARINE to 0.1,
+		POLISHED_GRANITE to 0.1,
+		SMOOTH_RED_SANDSTONE to 0.1,
+		MOSSY_STONE_BRICKS to 0.1,
+		POLISHED_DIORITE to 0.1,
+		MOSSY_COBBLESTONE to 0.1,
+		END_STONE_BRICKS to 0.1,
+		SMOOTH_SANDSTONE to 0.1,
+		SMOOTH_QUARTZ to 0.1,
+		GRANITE to 0.1,
+		ANDESITE to 0.1,
+		RED_NETHER_BRICKS to 0.1,
+		POLISHED_ANDESITE to 0.1,
+		DIORITE to 0.1,
+		COBBLED_DEEPSLATE to 0.1,
+		POLISHED_DEEPSLATE to 0.1,
+		DEEPSLATE_BRICKS to 0.1,
+		DEEPSLATE_TILES to 0.1,
+		BLACKSTONE to 0.1,
+		POLISHED_BLACKSTONE to 0.1,
+		POLISHED_BLACKSTONE_BRICKS to 0.1,
 		
-		Material.CHISELED_DEEPSLATE to 0.1
-		Material.CHISELED_NETHER_BRICKS to 0.1
-		Material.CHISELED_POLISHED_BLACKSTONE to 0.1
-		Material.CHISELED_QUARTZ_BLOCK to 0.1
-		Material.CHISELED_RED_SANDSTONE to 0.1
-		Material.CHISELED_SANDSTONE to 0.1
-		Material.CHISELED_STONE_BRICKS to 0.1
-		Material.QUARTZ_BRICKS to 0.1
-		Material.QUARTZ_PILLAR to 0.1
-		Material.PURPUR_PILLAR to 0.1
-		Material.CRACKED_STONE_BRICKS to 0.1
-		Material.CRACKED_DEEPSLATE_BRICKS to 0.1
-		Material.CRACKED_DEEPSLATE_TILES to 0.1
-		Material.CRACKED_NETHER_BRICKS to 0.1
-		Material.CRACKED_POLISHED_BLACKSTONE_BRICKS to 0.1	
+		CHISELED_DEEPSLATE to 0.1,
+		CHISELED_NETHER_BRICKS to 0.1,
+		CHISELED_POLISHED_BLACKSTONE to 0.1,
+		CHISELED_QUARTZ_BLOCK to 0.1,
+		CHISELED_RED_SANDSTONE to 0.1,
+		CHISELED_SANDSTONE to 0.1,
+		CHISELED_STONE_BRICKS to 0.1,
+		QUARTZ_BRICKS to 0.1,
+		QUARTZ_PILLAR to 0.1,
+		PURPUR_PILLAR to 0.1,
+		CRACKED_STONE_BRICKS to 0.1,
+		CRACKED_DEEPSLATE_BRICKS to 0.1,
+		CRACKED_DEEPSLATE_TILES to 0.1,
+		CRACKED_NETHER_BRICKS to 0.1,
+		CRACKED_POLISHED_BLACKSTONE_BRICKS to 0.1
 	)
+
 	fun getPrice(data: BlockData) : Double {
-		when(data){
-			CustomBlocks.MINERAL_TITANIUM.block.blockData -> return 200.0
-			CustomBlocks.MINERAL_URANIUM.block.blockData -> return 50.0
-			CustomBlocks.MINERAL_CHETHERITE.block.blockData -> return 100.0
-			CustomBlocks.MINERAL_ALUMINUM.block.blockData -> return 100.0
+		return when(data) {
+			CustomBlocks.MINERAL_TITANIUM.block.blockData -> 200.0
+			CustomBlocks.MINERAL_URANIUM.block.blockData -> 50.0
+			CustomBlocks.MINERAL_CHETHERITE.block.blockData -> 100.0
+			CustomBlocks.MINERAL_ALUMINUM.block.blockData -> 100.0
+			else ->
+				     if (data.material.isGlass) 0.1
+			  else if (data.material.isConcrete) 0.1
+			  else if (data.material.isGlassPane) 0.1
+			  else if (data.material.isSlab) 0.1
+			  else if (data.material.isStairs) 0.1
+			  else if (data.material.isStainedTerracotta) 0.1
+			  else if (data.material.isGlazedTerracotta) 0.1
+			  else if (data.material.isConcrete) 0.1
+			  else if (data.material.isSign) 25.0
+			  else if (data.material.isWallSign) 25.0
+			  else if (data.material.isWall) 0.1
+			  else if (blockprice.containsKey(data.material)) blockprice[data.material]!!.toDouble()
+			  else 3.0
 		}
-		if(data.material.isGlass) return 0.1
-		else if(data.material.isConcrete) return 0.1
-		else if(data.material.isGlassPane) return 0.1
-		else if(data.material.isSlab) return 0.1
-		else if(data.material.isStairs) return 0.1
-		else if(data.material.isStainedTerracotta) return 0.1
-		else if(data.material.isGlazedTerracotta) return 0.1
-		else if(data.material.isConcrete) return 0.1
-		else if(data.material.isSign) return 25.0
-		else if(data.material.isWallSign) return 25.0
-		else if(data.material.isWall) return 0.1
-		else return if (blockprice.containsKey(data.material)) blockprice[data.material]!!.toDouble()
-		else 3.0
 	}
 }
