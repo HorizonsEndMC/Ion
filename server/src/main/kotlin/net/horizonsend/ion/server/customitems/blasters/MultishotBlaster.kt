37b76e91241536acdccdd25daf9d4bb85178081b
Gutin1 <claytonpinter@gmail.com>
2022-11-22 01:31:15 -0600

    cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/MultishotBlaster.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/MultishotBlaster.kt
@@ -1,29 +1,7 @@
 package net.horizonsend.ion.server.customitems.blasters
 
 import net.horizonsend.ion.server.BalancingConfiguration
-import net.horizonsend.ion.server.managers.ProjectileManager
-import net.horizonsend.ion.server.projectiles.Projectile
-import org.bukkit.Particle
-import org.bukkit.entity.LivingEntity
-import org.bukkit.inventory.ItemStack
 
-abstract class multiShotBlaster : Blaster() {
+abstract class MultiShotBlaster : Blaster() {
 	abstract val multiShotWeaponBalancing: BalancingConfiguration.EnergyWeaponBalancing.MultiShotWeaponBalancing
-
-	override fun onPrimaryInteract(source: LivingEntity, item: ItemStack) {
-		ProjectileManager.addProjectile(
-			Projectile(
-				source.eyeLocation,
-				if (getParticleType(source) == Particle.REDSTONE){Particle.DustOptions(getParticleColour(source), multiShotWeaponBalancing.shotSize)} else null,
-				multiShotWeaponBalancing.iterationsPerTick,
-				multiShotWeaponBalancing.distancePerIteration,
-				getParticleType(source),
-				source,
-				multiShotWeaponBalancing.damage,
-				multiShotWeaponBalancing.shouldPassThroughEntities,
-				multiShotWeaponBalancing.shotSize.toDouble(),
-				multiShotWeaponBalancing.shouldBypassHitTicks
-			)
-		)
-	}
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/projectiles/Projectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/projectiles/Projectile.kt
@@ -1,100 +1,17 @@
 package net.horizonsend.ion.server.projectiles
 
-import org.bukkit.FluidCollisionMode
 import org.bukkit.Location
-import org.bukkit.Particle
-import org.bukkit.Particle.DustOptions
-import org.bukkit.Sound
-import org.bukkit.entity.Damageable
 import org.bukkit.entity.Entity
-import org.bukkit.entity.Flying
-import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
 
-class Projectile(
-	private val location: Location,
-	private val dustOptions: DustOptions?,
-	private val iterationsPerTick: Int,
-	distancePerIteration: Double,
-	val particle: Particle,
-	val shooter: Entity,
-	val damage: Double,
-	private val shouldPassThroughEntities: Boolean,
-	private val size: Double,
-	val shouldBypassHitTicks: Boolean
-) {
-	private var directionVector = location.direction.multiply(distancePerIteration)
+abstract class Projectile {
+	abstract val location: Location
+	abstract val iterationsPerTick: Int
+	abstract val shooter: Entity
+	abstract val damage: Double
+	abstract val shouldPassThroughEntities: Boolean
+	abstract val size: Double
+	abstract val shouldBypassHitTicks: Boolean
+	abstract val distancePerIteration: Double
 
-	fun tick(): Boolean {
-		/**
-		 * Every tick, this function will repeat the code below
-		 * for however many iterationsPerTick there are, which is
-		 * It will delete the projecile if it goes out of loaded chunks
-		 */
-		repeat(iterationsPerTick) {
-			location.add(directionVector)
-			if (!location.isChunkLoaded) return true
-			if (dustOptions != null) {
-				location.world.spawnParticle(particle, location, 1, 0.0, 0.0, 0.0, 0.0, dustOptions, true)
-			} else location.world.spawnParticle(particle, location, 1, 0.0, 0.0, 0.0, 0.0, null, true)
-
-		}
-		return false
-	}
-
-	fun rayCastTick(): Boolean {
-		val rayTraceResult = location.world.rayTrace(
-			location,
-			location.direction,
-			location.world.viewDistance.toDouble(),
-			FluidCollisionMode.NEVER,
-			true,
-			size,
-			null
-		)
-		val rayFlyingTraceResult = location.world.rayTrace(
-			location,
-			location.direction,
-			location.world.viewDistance.toDouble(),
-			FluidCollisionMode.NEVER,
-			true,
-			2.0,
-			null
-		)
-		if (rayTraceResult?.hitBlock != null || rayFlyingTraceResult?.hitBlock != null) {
-			rayTraceResult?.hitBlock?.blockSoundGroup?.breakSound?.let {
-				location.world.playSound(location,
-					it, 0.5f, 1f)
-			}
-			return true
-		}
-		if (rayFlyingTraceResult?.hitEntity != null && rayFlyingTraceResult.hitEntity is Flying) {
-			(rayFlyingTraceResult.hitEntity as? Damageable)?.damage(damage, shooter)
-			if (!shouldPassThroughEntities) {
-				return true
-			}
-			return false
-		}
-		if (rayTraceResult?.hitEntity != null) {
-			val playerHit = rayTraceResult.hitEntity
-			val hitLocation = rayTraceResult.hitPosition.toLocation(rayTraceResult.hitEntity!!.world)
-
-			val raycast = rayTraceResult.hitPosition
-			val playereye = (playerHit as? Player)?.eyeLocation?.toVector()
-			if (playereye != null && raycast.distance(playereye) < 0.5) {
-				if (shouldBypassHitTicks) (rayTraceResult.hitEntity as? LivingEntity)?.noDamageTicks = 0
-				(rayTraceResult.hitEntity as? Damageable)?.damage(damage * 1.5, shooter)
-				hitLocation.createExplosion(0.01f)
-				hitLocation.world.playSound(hitLocation, Sound.ENTITY_ARROW_HIT_PLAYER, 1f, 1f)
-				return true
-			}
-			if (shouldBypassHitTicks) (rayTraceResult.hitEntity as? LivingEntity)?.noDamageTicks = 0
-			(rayTraceResult.hitEntity as? Damageable)?.damage(damage, shooter)
-			if (!shouldPassThroughEntities) {
-				return true
-			}
-			return false
-		}
-		return false
-	}
+	abstract fun tick(): Boolean
 }
