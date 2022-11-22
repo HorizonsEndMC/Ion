9d6ed357340ca57262b069e850f2a243353c37e9
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-11-22 21:05:37 +0000

    Rebase, nullpointerfix, cleanup and reload attempt fix(didnt work)

--- server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/AmmoRequiringMultiShotBlaster.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/AmmoRequiringMultiShotBlaster.kt
@@ -4,6 +4,7 @@ import kotlin.math.absoluteValue
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.managers.ProjectileManager
 import net.horizonsend.ion.server.projectiles.RayTracedParticleProjectile
+import net.kyori.adventure.text.minimessage.MiniMessage
 import net.starlegacy.util.randomDouble
 import org.bukkit.NamespacedKey
 import org.bukkit.Particle
@@ -16,6 +17,20 @@ import org.bukkit.util.Vector
 abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 	abstract val requiredAmmo: ItemStack
 
+	override fun onPrimaryInteract(source: LivingEntity, item: ItemStack) {
+		if (!(source as? Player)?.inventory!!.contains(requiredAmmo)) return
+		(source as? Player)?.inventory
+		item.editMeta {
+			it.lore()?.clear()
+			it.lore(customItemlist.itemStack.lore())
+		}
+		item.editMeta { it.persistentDataContainer.remove(NamespacedKey(IonServer.Ion, "ammo")) }
+		item.editMeta {
+			it.persistentDataContainer[NamespacedKey(IonServer.Ion, "ammo"), PersistentDataType.INTEGER] =
+				multiShotWeaponBalancing.magazineSize
+		}
+		(source as? Player)?.setCooldown(item.type, this.multiShotWeaponBalancing.reload)
+	}
 	override fun onSecondaryInteract(entity: LivingEntity, item: ItemStack) {
 		if ((entity as? Player)?.hasCooldown(item.type) == true) return
 
@@ -24,9 +39,9 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 		entity.location.world.playSound(entity.location, "laser", 1f, multiShotWeaponBalancing.pitch)
 
 		for (i in 1..multiShotWeaponBalancing.shotCount) {
-			val offsetX = randomDouble(-1 * multiShotWeaponBalancing.offsetMax, multiShotWeaponBalancing.offsetMax)
-			val offsetY = randomDouble(-1 * multiShotWeaponBalancing.offsetMax, multiShotWeaponBalancing.offsetMax)
-			val offsetZ = randomDouble(-1 * multiShotWeaponBalancing.offsetMax, multiShotWeaponBalancing.offsetMax)
+			val offsetX = randomDouble(-1 * multiShotWeaponBalancing.offsetmax, multiShotWeaponBalancing.offsetmax)
+			val offsetY = randomDouble(-1 * multiShotWeaponBalancing.offsetmax, multiShotWeaponBalancing.offsetmax)
+			val offsetZ = randomDouble(-1 * multiShotWeaponBalancing.offsetmax, multiShotWeaponBalancing.offsetmax)
 
 			val location = entity.eyeLocation.clone()
 
@@ -53,41 +68,33 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 		}
 	}
 
-	fun reload(itemStack: ItemStack, source: LivingEntity): Boolean {
-		if ((source as? Player)!!.hasCooldown(itemStack.type)) return true
+	fun reload(item: ItemStack, source: LivingEntity): Boolean {
+		if ((source as? Player)!!.hasCooldown(item.type)) return true
 
-		if (itemStack.itemMeta.persistentDataContainer == null) {
-			itemStack.editMeta {
-				it.persistentDataContainer.set(
-					NamespacedKey(IonServer.Ion, "blaster"),
-					PersistentDataType.INTEGER,
-					this.multiShotWeaponBalancing.magazineSize.minus(1)
+		val pdc = item.itemMeta.persistentDataContainer.get(
+			NamespacedKey(IonServer.Ion, "ammo"),
+			PersistentDataType.INTEGER
+		)
+
+		if (pdc != 0 && pdc != null) {
+			val ammoValue: Int = pdc - 1
+
+			item.editMeta {
+				it.lore()?.clear()
+				it.lore(mutableListOf(
+					MiniMessage.miniMessage()
+					.deserialize("<bold><gray>Ammo:${ammoValue}/${multiShotWeaponBalancing.magazineSize}")))
+			}
+
+			(source as? Player)?.setCooldown(item.type, multiShotWeaponBalancing.timeBetweenShots)
+			item.editMeta { it.persistentDataContainer.remove(NamespacedKey(IonServer.Ion, "ammo")) }
+			item.editMeta {
+				it.persistentDataContainer.set(NamespacedKey(IonServer.Ion, "ammo"), PersistentDataType.INTEGER,
+					ammoValue
 				)
 			}
 
-			(source as? Player)?.setCooldown(itemStack.type, this.multiShotWeaponBalancing.timeBetweenShots)
 			return false
-		}
-
-		val pdc = itemStack.itemMeta.persistentDataContainer.get(NamespacedKey(IonServer.Ion, "blaster"),
-			PersistentDataType.INTEGER)
-
-		if (pdc?.absoluteValue != 0) {
-			itemStack.editMeta {
-				itemStack.itemMeta.persistentDataContainer.get(NamespacedKey(IonServer.Ion, "blaster"),
-					PersistentDataType.INTEGER)?.minus(1)
-			}
-
-			(source as? Player)?.setCooldown(itemStack.type, this.multiShotWeaponBalancing.timeBetweenShots)
-			return false
-		}
-
-		if (pdc.absoluteValue.absoluteValue == 0) {
-			(source as? Player)?.setCooldown(itemStack.type, this.multiShotWeaponBalancing.reload)
-
-			if (!(source as? Player)?.inventory!!.contains(this.requiredAmmo)) return true
-			(source as? Player)?.inventory?.remove(this.requiredAmmo)
-			return true
 		}
 
 		return true
