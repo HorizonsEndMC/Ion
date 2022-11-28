cc2910396ed07b82904ce0f2862540d06dd48c16
Astralchroma <astralchroma@proton.me>
2022-11-28 19:02:27 +0000

    Log potential combat logging incidents

--- Server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerQuitListener.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerQuitListener.kt
@@ -1,7 +1,7 @@
 package net.horizonsend.ion.server.listeners
 
+import net.horizonsend.ion.server.IonServer.Companion.Ion
 import org.bukkit.event.EventHandler
-import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -9,6 +9,18 @@ class PlayerQuitListener : Listener {
 	@EventHandler
 	@Suppress("Unused")
 	fun onPlayerQuitEvent(event: PlayerQuitEvent) {
+		val player = event.player
+
+		// Remove Disconnect Message
 		event.quitMessage(null)
+
+		// Log if player has potentially combat logged
+		for (otherPlayer in player.world.players) {
+			val distance = player.location.distance(otherPlayer.location)
+
+			if (distance <= 1_000) {
+				Ion.slF4JLogger.warn("\"${player.name}\" has potentially combat logged. \"${otherPlayer.name}\" is ${distance}m away.")
+			}
+		}
 	}
 }
