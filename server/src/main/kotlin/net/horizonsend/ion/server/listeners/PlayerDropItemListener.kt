861ac08768ad3d975c0390a81d6843363524dade
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-11-29 15:14:43 +0000

    Added some stuff, I forget what

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerDropItemListener.kt
@@ -0,0 +1,19 @@
+package net.horizonsend.ion.server.listeners
+
+import net.horizonsend.ion.server.customitems.getCustomItem
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerDropItemEvent
+
+class PlayerDropItemListener : Listener{
+	@EventHandler
+	@Suppress("unused")
+	fun onPlayerDropItem(event: PlayerDropItemEvent){
+		if (event.itemDrop.itemStack.getCustomItem() != null){
+			event.itemDrop.itemStack.getCustomItem().apply {
+				this?.onPrimaryInteract(event.player, event.itemDrop.itemStack)
+			}
+			event.isCancelled = true
+		}
+	}
+}
