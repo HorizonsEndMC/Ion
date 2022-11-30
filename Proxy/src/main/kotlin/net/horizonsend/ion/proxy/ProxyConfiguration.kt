67ee1829ae2932af52ab8e3858993fe273219c76
Gutin1 <Claytonpinter@gmail.com>
2022-11-29 23:51:11 -0600

    remove unused site IP

--- Proxy/src/main/kotlin/net/horizonsend/ion/proxy/ProxyConfiguration.kt
+++ Proxy/src/main/kotlin/net/horizonsend/ion/proxy/ProxyConfiguration.kt
@@ -12,15 +12,14 @@ data class ProxyConfiguration(
 	val unlinkedRole: Long = 0,
 	val linkedRole: Long = 0,
 	val voteSites: List<VoteSite> = listOf(
-		VoteSite("TESTNET", "1.0.0.1", "Test Server List", "https://Test.net/HorizonsEnd"),
-		VoteSite("TESTNET2", "1.0.0.2", "Test Server List2", "https://Test2.net/HorizonsEnd")
+		VoteSite("TESTNET",  "Test Server List", "https://Test.net/HorizonsEnd"),
+		VoteSite("TESTNET2",  "Test Server List2", "https://Test2.net/HorizonsEnd")
 	)
 )
 
 @ConfigSerializable
 data class VoteSite(
 	val serviceName: String,
-	val serviceAddress: String,
 	val displayName: String,
 	val displayAddress: String
 )
