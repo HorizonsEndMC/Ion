932b807da797e081c11e30f4032c94c1649e47fe
Gutin1 <Claytonpinter@gmail.com>
2022-12-05 04:30:51 -0600

    make regular guns use magazines
    unlock auto rifle's true potential
    
    small fixes

--- server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/AutoRifle.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/AutoRifle.kt
@@ -4,15 +4,25 @@ import net.horizonsend.ion.server.BalancingConfiguration
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.customitems.CustomItemList
 import net.horizonsend.ion.server.customitems.blasters.constructors.AmmoRequiringSingleShotBlaster
-import net.starlegacy.feature.misc.CustomItems
+import net.horizonsend.ion.server.customitems.blasters.constructors.Magazine
+import net.starlegacy.util.Tasks.syncDelay
+import org.bukkit.entity.LivingEntity
 import org.bukkit.inventory.ItemStack
 
 
 @Suppress("Unused")
 object AutoRifle : AmmoRequiringSingleShotBlaster() {
-	override val requiredAmmo: ItemStack = CustomItems.BATTERY_SMALL.singleItem()
+	override val requiredAmmo: Magazine = StandardMagazine
 	override val customItemlist: CustomItemList = CustomItemList.AUTO_RIFLE
 
 	override val singleShotWeaponBalancing: BalancingConfiguration.EnergyWeaponBalancing.SingleShotWeaponBalancing
 		get() = IonServer.Ion.balancing.energyWeapons.autoRifle
+
+	override fun onSecondaryInteract(entity: LivingEntity, item: ItemStack) { // Allows fire above 300 rpm
+		val repeatCount = if (singleShotWeaponBalancing.timeBetweenShots >= 4) { 1 } else { 4 / singleShotWeaponBalancing.timeBetweenShots }
+
+		for (count in 0..repeatCount) {
+			syncDelay(count.toLong()) { super.onSecondaryInteract(entity, item) }
+		}
+	}
 }
