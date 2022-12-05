1c3b61d2cc687a462f8b4f64443aeafb1d0295aa
Gutin1 <Claytonpinter@gmail.com>
2022-12-05 03:27:13 -0600

    get magazines working

--- server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/constructors/AmmoRequiringMultiShotBlaster.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/customitems/blasters/constructors/AmmoRequiringMultiShotBlaster.kt
@@ -2,6 +2,8 @@ package net.horizonsend.ion.server.customitems.blasters.constructors
 
 import io.papermc.paper.entity.RelativeTeleportFlag
 import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.customitems.blasters.StandardMagazine.getAmmo
+import net.horizonsend.ion.server.customitems.blasters.StandardMagazine.setAmmo
 import net.horizonsend.ion.server.customitems.getCustomItem
 import net.horizonsend.ion.server.managers.ProjectileManager
 import net.horizonsend.ion.server.projectiles.RayTracedParticleProjectile
@@ -19,13 +21,14 @@ import org.bukkit.persistence.PersistentDataType
 import org.bukkit.util.Vector
 
 abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
-	abstract val requiredAmmo: ItemStack
+	abstract val requiredAmmo: Magazine
 
+	// Fires the gun.
 	override fun onSecondaryInteract(entity: LivingEntity, item: ItemStack) {
 		val player = entity as? Player
 		if (player?.hasCooldown(item.type) == true) return
 
-		if (reload(item, entity)) return
+		if (canFire(item, entity)) return
 
 		entity.location.world.playSound(entity.location, "laser", 1f, multiShotWeaponBalancing.pitch)
 
@@ -81,7 +84,7 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 			 */
 			else if (multiShotWeaponBalancing.shouldAkimbo && itemPDC == offHandPDC) {
 				if (itemInOffHand.getCustomItem() is SingleShotBlaster) {
-					if (reload(item, entity)) return
+					if (canFire(item, entity)) return
 					(itemInOffHand.getCustomItem() as SingleShotBlaster).apply {
 						ProjectileManager.addProjectile(
 							RayTracedParticleProjectile(
@@ -100,11 +103,11 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 								} else null
 							)
 						)
-						reload(itemInOffHand, entity)
+						canFire(itemInOffHand, entity)
 					}
 				} else if (itemInOffHand.getCustomItem() is MultiShotBlaster) {
 					(itemInOffHand.getCustomItem() as MultiShotBlaster).apply {
-						if (reload(item, entity)) return
+						if (canFire(item, entity)) return
 						for (i in 1..multiShotWeaponBalancing.shotCount) {
 							val offsetX = randomDouble(-1 * multiShotWeaponBalancing.offsetMax,
 								multiShotWeaponBalancing.offsetMax)
@@ -143,53 +146,79 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 		}
 	}
 
-	override fun onTertiaryInteract(source: LivingEntity, item: ItemStack) {
-		val player = (source as? Player)
-		if (player?.hasCooldown(item.type) == true) return
-		val inventory = (source as? Player)?.inventory
-		if (item.itemMeta.persistentDataContainer.get(NamespacedKey(IonServer.Ion, "ammo"),
-				PersistentDataType.INTEGER) == multiShotWeaponBalancing.magazineSize
-		) return
-		if (!inventory!!.containsAtLeast(requiredAmmo, requiredAmmo.amount)) return
-		inventory.removeItemAnySlot(requiredAmmo.clone())
-		source.updateInventory()
+	// Reloads the gun from ammo. Returns the maximum value the gun could be filled to.
+	override fun onTertiaryInteract(entity: LivingEntity, item: ItemStack) {
+		val player = (entity as? Player) ?: return // Mobs don't need to reload
+
+		if (player.hasCooldown(item.type)) return // Can't reload while on cooldown
+
+		val ammoCount = item.itemMeta.persistentDataContainer.get(NamespacedKey(IonServer.Ion, "ammo"),
+			PersistentDataType.INTEGER) ?: return // Fail if PDC is broken
+
+		if (ammoCount == multiShotWeaponBalancing.magazineSize) return // Cancels reload if the magazine is full.
+
+		val magazine = player.inventory.find { inventoryItem -> inventoryItem.getCustomItem() is Magazine } ?: return // Fail if no ammo
+
+		val magAmmo = getAmmo(magazine)!! //Can't be null
+
+		// Calculate the maximum the gun can be filled to from the first available magazine.
+		val maxFilled: Int = if (magAmmo > multiShotWeaponBalancing.magazineSize - ammoCount) {
+			// If the new magazine capacity is above zero, set the magazine capacity to the new amount, and fill the gun to its full capacity
+			setAmmo(magazine, player, magAmmo - (multiShotWeaponBalancing.magazineSize - ammoCount))
+
+			multiShotWeaponBalancing.magazineSize
+		} else {
+			// If the new magazine capacity is below zero, fill the gun to what was left, and delete the magazine
+			player.inventory.removeItemAnySlot(magazine.clone())
+			player.updateInventory()
+
+			magAmmo
+		}
+
 		item.editMeta {
 			it.lore()?.clear()
-			it.lore(customItemlist.itemStack.lore())
+			it.lore(mutableListOf(MiniMessage.miniMessage().deserialize("<bold><gray>Ammo: $maxFilled/4")))
 		}
+
+		player.setCooldown(item.type, this.multiShotWeaponBalancing.reload)
+
 		item.editMeta { it.persistentDataContainer.remove(NamespacedKey(IonServer.Ion, "ammo")) }
+
 		item.editMeta {
-			it.persistentDataContainer[NamespacedKey(IonServer.Ion, "ammo"), PersistentDataType.INTEGER] =
-				multiShotWeaponBalancing.magazineSize
+			it.persistentDataContainer.set(NamespacedKey(IonServer.Ion, "ammo"), PersistentDataType.INTEGER,
+				maxFilled
+			)
 		}
-		(source as? Player)?.setCooldown(item.type, this.multiShotWeaponBalancing.reload)
-		player?.sendActionBar(MiniMessage.miniMessage()
-			.deserialize("<red>Ammo: ${multiShotWeaponBalancing.magazineSize}/${multiShotWeaponBalancing.magazineSize}"))
+
+		player.sendActionBar(MiniMessage.miniMessage().deserialize("<red>Ammo: $maxFilled/${multiShotWeaponBalancing.magazineSize}"))
 	}
 
-	fun reload(item: ItemStack, source: LivingEntity): Boolean {
+	// Checks if the gun is on cooldown, and ticks down the ammo in the gun
+	fun canFire(item: ItemStack, source: LivingEntity): Boolean {
 		val pdc = item.itemMeta.persistentDataContainer.get(
 			NamespacedKey(IonServer.Ion, "ammo"),
 			PersistentDataType.INTEGER
 		)
 
-		if (pdc != 0 && pdc != null) {
-			val ammoValue: Int = pdc - 1
+		if (pdc != 0 && pdc != null) { // Checks if the blaster has no ammo
+			val ammoValue: Int = pdc - 1 // If it has no ammo, lower the ammo count by one
 
-			item.editMeta {
+			item.editMeta {// Reflect the new ammo count in the item lore
 				it.lore()?.clear()
 				it.lore(mutableListOf(
 					MiniMessage.miniMessage()
 						.deserialize("<bold><gray>Ammo:${ammoValue}/${multiShotWeaponBalancing.magazineSize}")))
 			}
 
-			(source as? Player)?.setCooldown(item.type, multiShotWeaponBalancing.timeBetweenShots)
-			item.editMeta { it.persistentDataContainer.remove(NamespacedKey(IonServer.Ion, "ammo")) }
+			item.editMeta { it.persistentDataContainer.remove(NamespacedKey(IonServer.Ion, "ammo")) } // Delete the old ammo count
+
 			item.editMeta {
 				it.persistentDataContainer.set(NamespacedKey(IonServer.Ion, "ammo"), PersistentDataType.INTEGER,
 					ammoValue
-				)
+				) // Reflect the new ammo count in the item PDC
 			}
+
+			(source as? Player)?.setCooldown(item.type, multiShotWeaponBalancing.timeBetweenShots) // Set the cooldown for the next shot
 
 			return false
 		}
@@ -200,11 +229,15 @@ abstract class AmmoRequiringMultiShotBlaster : MultiShotBlaster() {
 	@Suppress("UnstableApiUsage")
 	private fun recoil(entity: LivingEntity) {
 		val recoil = multiShotWeaponBalancing.recoil / multiShotWeaponBalancing.packetsPerShot
+
 		for (i in 1..multiShotWeaponBalancing.packetsPerShot) {
 			if (entity is Flying) return
+
 			Tasks.syncDelay(i.toLong()) {
 				val loc = entity.location
+
 				loc.pitch -= recoil
+
 				(entity as? Player)?.teleport(
 					loc,
 					PlayerTeleportEvent.TeleportCause.PLUGIN,
