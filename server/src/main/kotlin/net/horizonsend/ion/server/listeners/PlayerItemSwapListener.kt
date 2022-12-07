cd7f6fac2dee91c7f67a0df09d67e16afbcd617f
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-12-07 20:47:39 +0000

    Renamed a file

--- server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerItemSwapListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerItemSwapListener.kt
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerSwapHandItemsEvent
 
-class PlayerDropItemListener : Listener{
+class PlayerItemSwapListener : Listener{
 	@EventHandler
 	@Suppress("unused")
 	fun onPlayerDropItem(event: PlayerSwapHandItemsEvent){
