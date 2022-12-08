b6a9587e01d79bb7705a7bfc6e6344e57895ba38
Gutin1 <Claytonpinter@gmail.com>
2022-12-07 19:55:18 -0600

    move reload to swap items, add magazine combination

--- server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerItemSwapListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listeners/PlayerItemSwapListener.kt
@@ -2,18 +2,24 @@ package net.horizonsend.ion.server.listeners
 
 import net.horizonsend.ion.server.customitems.getCustomItem
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerSwapHandItemsEvent
 
 class PlayerItemSwapListener : Listener{
-	@EventHandler
+	@EventHandler(priority = EventPriority.NORMAL)
 	@Suppress("unused")
-	fun onPlayerDropItem(event: PlayerSwapHandItemsEvent){
-		event.isCancelled = true
-		if (event.mainHandItem?.getCustomItem() != null){
-			event.mainHandItem?.getCustomItem().apply {
-				this?.onPrimaryInteract(event.player, event.mainHandItem!!)
-			}
+	fun onPlayerSwapItem(event: PlayerSwapHandItemsEvent) {
+		val item = event.offHandItem ?: return // The item swapped into the offhand
+
+		val customItem = item.getCustomItem() ?: return
+
+		event.isCancelled = true // Offhand item
+
+		val newItem = event.player.inventory.itemInMainHand
+
+		customItem.apply {
+			this.onTertiaryInteract(event.player, newItem)
 		}
 	}
 }
