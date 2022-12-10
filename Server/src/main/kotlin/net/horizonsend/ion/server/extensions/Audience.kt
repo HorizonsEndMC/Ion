adfee712dbc6336b62bdc83a6296f6b44fd5be53
Peter Crawley <peterzcrawley@gmail.com>
2022-12-10 20:23:08 +0000

    Improve Audience extensions

--- Server/src/main/kotlin/net/horizonsend/ion/server/extensions/Audience.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/extensions/Audience.kt
@@ -2,16 +2,25 @@ package net.horizonsend.ion.server.extensions
 
 import net.horizonsend.ion.server.IonServer.Companion.Ion
 import net.kyori.adventure.audience.Audience
-import net.kyori.adventure.text.Component.empty
+import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.TextColor.color
-import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 
+/** Does not support MiniMessage! */
+fun Audience.sendUserError(message: String) {
+	sendColouredMessage(message, 0xff7f3f)
+	Ion.slF4JLogger.warn(message)
+}
+
+/** Does not support MiniMessage! */
 fun Audience.sendServerError(message: String) {
-	sendColouredMiniMessage(message, 0xff3f3f)
+	sendColouredMessage(message, 0xff3f3f)
 	Ion.slF4JLogger.error(message)
 }
 
-fun Audience.sendUserError(message: String) = sendColouredMiniMessage(message, 0xff7f3f)
+/** Does not support MiniMessage! */
+fun Audience.sendInformation(message: String) {
+	sendColouredMessage(message, 0x3f3fff)
+	Ion.slF4JLogger.info(message)
+}
 
-private fun Audience.sendColouredMiniMessage(message: String, color: Int) =
-	sendMessage(empty().append(miniMessage().deserialize(message)).color(color(color)))
+private fun Audience.sendColouredMessage(message: String, color: Int) = sendMessage(text(message, color(color)))
