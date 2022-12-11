d81a13170f2ef0a471eeb015c3d8dc6d844767d0
Peter Crawley <peterzcrawley@gmail.com>
2022-12-11 03:30:46 +0000

    Clean up settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,10 @@
-pluginManagement {
-	repositories {
-		gradlePluginPortal()
-
-		maven("https://repo.papermc.io/repository/maven-public/")
-	}
-}
-
 rootProject.name = "Ion"
 
 include("Proxy")
 include("Server")
 include("Common")
+
+pluginManagement.repositories {
+	maven("https://repo.papermc.io/repository/maven-public/")
+	gradlePluginPortal()
+}
