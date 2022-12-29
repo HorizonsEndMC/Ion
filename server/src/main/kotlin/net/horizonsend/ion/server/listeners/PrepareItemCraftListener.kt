bb28e056ce84a46a34344d0bdc263ec3f775e294
Gutin1 <claytonpinter@gmail.com>
2022-12-29 01:55:00 -0600

    another fix

--- server/src/main/kotlin/net/horizonsend/ion/server/listeners/PrepareItemCraftListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listeners/PrepareItemCraftListener.kt
@@ -3,7 +3,6 @@ package net.horizonsend.ion.server.listeners
 import net.horizonsend.ion.server.items.CustomItems
 import net.horizonsend.ion.server.items.CustomItems.customItem
 import net.horizonsend.ion.server.items.objects.Magazine
-import net.horizonsend.ion.server.items.objects.Magazine.getAmmunition
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.PrepareItemCraftEvent
@@ -11,15 +10,19 @@ import org.bukkit.event.inventory.PrepareItemCraftEvent
 class PrepareItemCraftListener : Listener {
 	@EventHandler
 	fun onPrepareItemCraftEvent(event: PrepareItemCraftEvent) {
-		if (!event.isRepair) return
+		if (!event.isRepair) return // Will always be a combination of 2 items.
 
-		val magazines = event.inventory.matrix.filter { it?.customItem is Magazine }.filterNotNull()
+		val magazines = event.inventory.matrix.filter { it?.customItem is Magazine<*> }.filterNotNull()
+
 		if (magazines.isEmpty()) return
 
-		val totalAmmo = magazines.sumOf { getAmmunition(it) }.coerceIn(0..30)
+		if (magazines.first().customItem?.identifier != magazines.last().customItem?.identifier) return
 
-		val resultItemStack = CustomItems.MAGAZINE.constructItemStack()
-		CustomItems.MAGAZINE.setAmmunition(resultItemStack, event.inventory, totalAmmo)
+		val magazineType = magazines.first().customItem as Magazine<*>
+		val totalAmmo = magazines.sumOf { magazineType.getAmmunition(it) }.coerceIn(0..magazineType.balancing.capacity)
+		val resultItemStack = CustomItems.getByIdentifier(magazineType.identifier)!!.constructItemStack()
+
+		magazineType.setAmmunition(resultItemStack, event.inventory, totalAmmo)
 
 		event.inventory.result = resultItemStack
 	}
