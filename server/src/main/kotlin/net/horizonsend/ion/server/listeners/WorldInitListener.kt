c6e68bd943713f00a5354bf8f077fb2b2e2c97bd
Peter Crawley <peterzcrawley@gmail.com>
2023-01-01 08:40:50 +0000

    rebase

--- server/src/main/kotlin/net/horizonsend/ion/server/listeners/WorldInitListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listeners/WorldInitListener.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.listeners
 
 import net.horizonsend.ion.server.IonWorld
+import net.starlegacy.feature.machine.AreaShields
 import org.bukkit.craftbukkit.v1_19_R2.CraftWorld
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,5 +12,6 @@ class WorldInitListener : Listener {
 	@Suppress("Unused")
 	fun onWorldInitEvent(event: WorldInitEvent) {
 		IonWorld.register((event.world as CraftWorld).handle)
+		AreaShields.loadData()
 	}
 }
