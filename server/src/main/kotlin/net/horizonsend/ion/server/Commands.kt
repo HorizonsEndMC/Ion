550ef28cc6f8d785ec2e7c90704a8eedbd3cb761
Peter Crawley <peterzcrawley@gmail.com>
2023-01-06 01:24:08 +0000

    Move commands array to separate file

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
@@ -0,0 +1,10 @@
+package net.horizonsend.ion.server
+
+import net.horizonsend.ion.server.commands.BountyCommands
+import net.horizonsend.ion.server.legacy.commands.AchievementsCommand
+
+val commands = arrayOf(
+	BountyCommands(),
+
+	AchievementsCommand()
+)
