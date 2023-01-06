318cd6133a2e3a87a2aa7179eb31fc786aeda2f1
Astralchroma <astralchroma@proton.me>
2023-01-06 01:24:44 +0000

    Utility function to warn asynchronous usages

--- server/src/main/kotlin/net/horizonsend/ion/server/Miscellaneous.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/Miscellaneous.kt
@@ -1,6 +1,13 @@
 package net.horizonsend.ion.server
 
+import net.horizonsend.ion.server.IonServer.Companion.Ion
+import org.bukkit.Bukkit
 import java.util.EnumSet
 
 inline fun <reified T : Enum<T>> enumSetOf(vararg elems: T): EnumSet<T> =
 	EnumSet.noneOf(T::class.java).apply { addAll(elems) }
+
+/** Used for catching when a function that is not designed to be used async is being used async. */
+fun mainThreadCheck() {
+	if (!Bukkit.isPrimaryThread()) Ion.slF4JLogger.warn("This function may be unsafe to use asynchronously.", Throwable())
+}
