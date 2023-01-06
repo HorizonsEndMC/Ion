baa925f9ba19eae4d2749bf36e8f5a9a80d9586f
Peter Crawley <peterzcrawley@gmail.com>
2023-01-06 01:22:56 +0000

    Improvements to Audience extensions

--- server/src/main/kotlin/net/horizonsend/ion/server/extensions/Audience.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/extensions/Audience.kt
@@ -1,26 +1,55 @@
 package net.horizonsend.ion.server.extensions
 
 import net.horizonsend.ion.server.IonServer.Companion.Ion
+import net.horizonsend.ion.server.starships.control.PlayerController
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.TextColor.color
+import org.bukkit.entity.Player
 
-/** Does not support MiniMessage! */
+/** Returns the prefix that should be used when displaying server messages sent to this audience in the console. */
+private val Audience.prefix get() = when (this) {
+	is Player -> "to $name: "
+	is PlayerController -> "to ${serverPlayer.displayName}: "
+	else -> ""
+}
+
+/** Sends a message to the audience with the given colour. */
+private fun Audience.sendColouredMessage(message: String, color: Int) = sendMessage(text(message, color(color)))
+
+/**
+ * Used to provide potentially irrelevant information, sends a gray coloured message.
+ *
+ * Note: Does not support MiniMessage!
+ */
+fun Audience.sendHint(message: String) = sendColouredMessage(message, 0x7f7f7f)
+
+/**
+ * Used to provide relevant information, sends a blue coloured message, and logs it.
+ *
+ * Note: Does not support MiniMessage!
+ */
+fun Audience.sendInformation(message: String) {
+	sendColouredMessage(message, 0x7f7fff)
+	Ion.slF4JLogger.info("$prefix$message")
+}
+
+/**
+ * Used when the player is at fault, sends a orange coloured message, and logs it as a warning.
+ *
+ * Note: Does not support MiniMessage!
+ */
 fun Audience.sendUserError(message: String) {
 	sendColouredMessage(message, 0xff7f3f)
-	Ion.slF4JLogger.warn(message)
+	Ion.slF4JLogger.warn("$prefix$message")
 }
 
-/** Does not support MiniMessage! */
+/**
+ * Used when the server is at fault, sends a red coloured message, and logs it as a error.
+ *
+ * Note: Does not support MiniMessage!
+ */
 fun Audience.sendServerError(message: String) {
 	sendColouredMessage(message, 0xff3f3f)
-	Ion.slF4JLogger.error(message)
+	Ion.slF4JLogger.error("$prefix$message")
 }
-
-/** Does not support MiniMessage! */
-fun Audience.sendInformation(message: String) {
-	sendColouredMessage(message, 0x3f3fff)
-	Ion.slF4JLogger.info(message)
-}
-
-private fun Audience.sendColouredMessage(message: String, color: Int) = sendMessage(text(message, color(color)))
