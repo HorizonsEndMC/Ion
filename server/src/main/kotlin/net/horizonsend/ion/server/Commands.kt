9999ea97e8a6bd93ec8aafe8833eb6397c195594
Gutin1 <Claytonpinter@gmail.com>
2023-01-06 04:00:01 -0600

    mostly klint formatting

--- server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
@@ -1,10 +1,11 @@
 package net.horizonsend.ion.server
 
+import net.horizonsend.ion.server.commands.AsteroidCommand
 import net.horizonsend.ion.server.commands.BountyCommands
 import net.horizonsend.ion.server.legacy.commands.AchievementsCommand
 
 val commands = arrayOf(
 	BountyCommands(),
-
+	AsteroidCommand(),
 	AchievementsCommand()
 )
