d3b9ee2d77d87ac56179cff3024717cc4188a3eb
Astralchroma <astralchroma@proton.me>
2023-01-24 13:59:36 +0000

    Register ControlCommands

--- server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
@@ -2,6 +2,7 @@ package net.horizonsend.ion.server
 
 import net.horizonsend.ion.server.commands.BountyCommands
 import net.horizonsend.ion.server.commands.ConfigurationCommands
+import net.horizonsend.ion.server.commands.ControlCommands
 import net.horizonsend.ion.server.commands.ConvertCommand
 import net.horizonsend.ion.server.commands.CustomItemCommand
 import net.horizonsend.ion.server.commands.SettingsCommand
@@ -11,6 +12,7 @@ import net.horizonsend.ion.server.legacy.commands.AchievementsCommand
 val commands = arrayOf(
 	BountyCommands(),
 	ConfigurationCommands(),
+	ControlCommands(),
 	ConvertCommand(),
 	CustomItemCommand(),
 	SettingsCommand(),
