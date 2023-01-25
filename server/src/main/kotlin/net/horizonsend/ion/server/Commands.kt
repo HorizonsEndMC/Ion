8f592927ff455a00cedce45f51f7983f39c37f1c
Astralchroma <astralchroma@proton.me>
2023-01-25 22:11:01 +0000

    View/Simulation Distance Commands

--- server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/Commands.kt
@@ -1,10 +1,12 @@
 package net.horizonsend.ion.server
 
 import net.horizonsend.ion.server.commands.BountyCommands
+import net.horizonsend.ion.server.commands.UtilityCommands
 import net.horizonsend.ion.server.legacy.commands.AchievementsCommand
 
 val commands = arrayOf(
 	BountyCommands(),
+	UtilityCommands(),
 
 	AchievementsCommand()
 )
