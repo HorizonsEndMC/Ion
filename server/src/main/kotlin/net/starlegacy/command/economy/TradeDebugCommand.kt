c735b31a615457eac9dd6a7b6eadb1d183d8887d
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-30 11:54:46 -0800

    Removed deprecations for TradeDebugCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/economy/TradeDebugCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/economy/TradeDebugCommand.kt
@@ -6,6 +6,9 @@ import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Description
 import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.server.extensions.sendInformation
+import net.horizonsend.ion.server.legacy.feedback.FeedbackType
+import net.horizonsend.ion.server.legacy.feedback.sendFeedbackMessage
 import net.starlegacy.cache.trade.CargoCrates
 import net.starlegacy.command.SLCommand
 import net.starlegacy.database.schema.economy.CargoCrate
@@ -19,18 +22,7 @@ import net.starlegacy.feature.economy.city.TradeCityData
 import net.starlegacy.feature.economy.collectors.CollectionMissions
 import net.starlegacy.feature.nations.region.Regions
 import net.starlegacy.feature.nations.region.types.RegionTerritory
-import net.starlegacy.util.aqua
-import net.starlegacy.util.darkAqua
-import net.starlegacy.util.darkPurple
 import net.starlegacy.util.getNBTInt
-import net.starlegacy.util.gold
-import net.starlegacy.util.gray
-import net.starlegacy.util.green
-import net.starlegacy.util.lightPurple
-import net.starlegacy.util.msg
-import net.starlegacy.util.plus
-import net.starlegacy.util.text
-import net.starlegacy.util.yellow
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import kotlin.system.measureTimeMillis
@@ -38,32 +30,45 @@ import kotlin.system.measureTimeMillis
 @CommandAlias("tradedebug|tdebug|tbug")
 @CommandPermission("trade.debug")
 object TradeDebugCommand : SLCommand() {
+	@Suppress("Unused")
 	@Subcommand("rebalance")
 	@Description("Reload the balance config and regenerate shipments")
 	fun onRebalance(sender: CommandSender) {
 		ShipmentBalancing.reload()
 		onShipmentRegenerate(sender)
-		sender msg green("Reloaded the shipment balancing config & regenerated shipments.")
+		sender.sendFeedbackMessage(
+			FeedbackType.SUCCESS,
+			"Reloaded the shipment balancing config & regenerated shipments."
+		)
 
 		CollectionMissions.rebalance()
-		sender msg "Reloaded the collection mission balancing config & regenerated collection missions."
+		sender.sendFeedbackMessage(
+			FeedbackType.SUCCESS,
+			"Reloaded the collection mission balancing config & regenerated collection missions."
+		)
 	}
 
+	@Suppress("Unused")
 	@Subcommand("crate reload")
 	fun onCrateReload(sender: CommandSender) {
-		sender msg "Reloading data...".text().yellow()
+		sender.sendRichMessage("<yellow>Reloading data...")
 		CargoCrates.reloadData()
 		val crates = CargoCrates.crates
-		sender msg green("Reloaded. Crates (${crates.size}): ${crates.map { it.name }}")
+		sender.sendFeedbackMessage(
+			FeedbackType.SUCCESS,
+			"Reloaded. Crates (${crates.size}): ${crates.map { it.name }}"
+		)
 	}
 
+	@Suppress("Unused")
 	@Subcommand("crate give")
 	@CommandCompletion("@crates 1|9|27")
 	fun onCrateGive(sender: Player, crate: CargoCrate, amount: Int) {
 		repeat(amount) { sender.world.dropItemNaturally(sender.eyeLocation, CrateItems[crate]) }
-		sender msg "Gave $amount of ${crate.name}".text().green()
+		sender.sendRichMessage("<green>Gave $amount of ${crate.name}")
 	}
 
+	@Suppress("Unused")
 	@Subcommand("crate shipment")
 	fun onCrateShipment(sender: Player) = asyncCommand(sender) {
 		val item = sender.inventory.itemInMainHand
@@ -73,21 +78,20 @@ object TradeDebugCommand : SLCommand() {
 		val shipmentId = item.getNBTInt("shipment_id")
 			?: throw InvalidCommandArgument("That crate isn't part of a shipment!")
 
-		sender msg gold("Shipment ID: ") + yellow("$shipmentId")
+		sender.sendRichMessage("<gold>Shipment ID: <yellow>$shipmentId")
 	}
 
+	@Suppress("Unused")
 	@Subcommand("crate list")
-	fun onCrateList(sender: CommandSender) = sender msg gray(CargoCrates.crates.joinToString { it.name })
+	fun onCrateList(sender: CommandSender) = sender.sendRichMessage("<gold>${CargoCrates.crates.joinToString { it.name }}")
 
 	@Subcommand("shipment regenerate")
 	fun onShipmentRegenerate(sender: CommandSender) = asyncCommand(sender) {
 		ShipmentManager.regenerateShipmentsAsync {
-			sender msg "Regenerated shipments for ".text().green() +
-				"${TradeCities.getAll().size}".text().aqua() +
-				" cities.".text().green()
+			sender.sendRichMessage("<green>Regenerated shipments for <aqua>${TradeCities.getAll().size} <green>cities.")
 			asyncCommand(sender) {
-				sender msg darkPurple("Shipments (cities to importing cities): ") + lightPurple(
-					"(${
+				sender.sendRichMessage("<dark_purple>Shipments (cities to importing cities): " +
+					"(<light_purple>${
 					ShipmentManager.getShipmentMap().map { (territoryId, shipments) ->
 						val exporterName: String = TradeCities.getIfCity(Regions[territoryId])?.displayName
 							?: "[ERR:$territoryId]"
@@ -100,6 +104,7 @@ object TradeDebugCommand : SLCommand() {
 		}
 	}
 
+	@Suppress("Unused")
 	@Subcommand("shipment list")
 	fun onShipmentList(sender: CommandSender) = asyncCommand(sender) {
 		for (city: TradeCityData in TradeCities.getAll()) {
@@ -107,30 +112,32 @@ object TradeDebugCommand : SLCommand() {
 
 			val territory: RegionTerritory = Regions[city.territoryId]
 
-			sender msg "${city.displayName} on ${territory.world} (${shipments.size}):".text().darkAqua()
+			sender.sendRichMessage("<dark_aqua>${city.displayName} on ${territory.world} (${shipments.size}):")
 
 			shipments.sortedByDescending { it.routeValue }.forEach { shipment ->
-				sender msg "  Crate: ".text().gold() + CargoCrates[shipment.crate].name.text().aqua() +
-					" To: ".text().gold() + shipment.to.displayName.text().darkPurple() +
-					" Value: ".text().gold() + shipment.routeValue.toString().text().yellow()
+				sender.sendRichMessage("<gold>  Crate: " + "<aqua>${CargoCrates[shipment.crate].name}" +
+					"<gold> To: " + "<dark_purple>${shipment.to.displayName}" +
+					"<gold> Value: " + "<yellow>${shipment.routeValue}")
 			}
 		}
 	}
 
+	@Suppress("Unused")
 	@Subcommand("databasefreeze")
 	fun onDatabaseFreeze(sender: CommandSender, seconds: Int) = asyncCommand(sender) {
 		val elapsed = measureTimeMillis {
 			for (i in 1..seconds) {
 				Thread.sleep(1000)
-				sender msg "Froze a database thread for $i seconds..."
+				sender.sendInformation("Froze a database thread for $i seconds...")
 			}
 		}
 
-		sender msg "Done! Elapsed time: $elapsed"
+		sender.sendInformation("Done! Elapsed time: $elapsed")
 	}
 
+	@Suppress("Unused")
 	@Subcommand("npc fix")
 	fun onNpcFix(sender: CommandSender) {
-		CityNPCs.synchronizeNPCsAsync { sender msg "done" }
+		CityNPCs.synchronizeNPCsAsync { sender.sendInformation("done") }
 	}
 }
