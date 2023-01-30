5e603a2814e40723bf97ba593c23005e856f5bbd
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-30 00:08:53 -0800

    removed deprecations for PlayerInfoCommand.kt (#222)
    * removed deprecations for PlayerInfoCommand.kt
    
    * minor fix for PlayerInfoCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/misc/PlayerInfoCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/misc/PlayerInfoCommand.kt
@@ -5,9 +5,6 @@ import co.aikar.commands.InvalidCommandArgument
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.Default
-import net.md_5.bungee.api.ChatColor.GRAY
-import net.md_5.bungee.api.ChatColor.GREEN
-import net.md_5.bungee.api.ChatColor.RED
 import net.starlegacy.command.SLCommand
 import net.starlegacy.database.Oid
 import net.starlegacy.database.schema.misc.SLPlayer
@@ -16,25 +13,25 @@ import net.starlegacy.database.schema.nations.NationRelation
 import net.starlegacy.database.schema.nations.Settlement
 import net.starlegacy.database.uuid
 import net.starlegacy.util.getDurationBreakdown
-import net.starlegacy.util.msg
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 object PlayerInfoCommand : SLCommand() {
+	@Suppress("Unused")
 	@Default
 	@CommandAlias("playerinfo|pinfo|pi")
 	@CommandCompletion("@players")
 	fun onExecute(sender: CommandSender, player: String) = asyncCommand(sender) {
 		val slPlayer = SLPlayer[player] ?: throw InvalidCommandArgument("Player $player not found!")
 
-		sender msg "&ePlayer &6${slPlayer.lastKnownName}"
+		sender.sendRichMessage("<yellow>Player <gold>${slPlayer.lastKnownName}")
 
 		sendNationsInfo(sender, slPlayer)
 
 		sendAdvanceInfo(sender, slPlayer)
 
-		sender msg "&7Last Seen: ${getInactiveTimeText(slPlayer)}"
+		sender.sendRichMessage("<gray>Last Seen: ${getInactiveTimeText(slPlayer)}")
 	}
 
 	private fun sendNationsInfo(sender: CommandSender, slPlayer: SLPlayer) {
@@ -44,13 +41,13 @@ object PlayerInfoCommand : SLCommand() {
 			val settlementName: String = Settlement.getName(settlementId)
 				?: throw ConditionFailedException("Failed to get settlement data!")
 
-			sender msg "&3Settlement:&b $settlementName"
+			sender.sendRichMessage("<dark_aqua>Settlement: <aqua>$settlementName")
 
 			val nationId = slPlayer.nation
 
 			if (nationId != null) {
 				val nationName: String = Nation.findPropById(nationId, Nation::name)!!
-				sender msg "&2Nation:&a $nationName"
+				sender.sendRichMessage("<dark_green>Nation: <green>$nationName")
 
 				if (sender is Player) {
 					val senderSettlement: Oid<Settlement>? = SLPlayer[sender].settlement
@@ -61,7 +58,7 @@ object PlayerInfoCommand : SLCommand() {
 						if (senderNation != null) {
 							val relation: NationRelation.Level = NationRelation.getRelationActual(nationId, senderNation)
 
-							sender.sendRichMessage("<gray>Relation:<gray> ${relation.coloredName}")
+							sender.sendRichMessage("<gray>Relation: ${relation.coloredName}")
 						}
 					}
 				}
@@ -70,19 +67,19 @@ object PlayerInfoCommand : SLCommand() {
 	}
 
 	private fun sendAdvanceInfo(sender: CommandSender, slPlayer: SLPlayer) {
-		sender msg "&5SLXP:&d ${slPlayer.xp}"
-		sender msg "&cLevel:&e ${slPlayer.level}"
+		sender.sendRichMessage("<dark_purple>SLXP: <light_purple>${slPlayer.xp}")
+		sender.sendRichMessage("<red>Level: <yellow>${slPlayer.level}")
 	}
 
 	private fun getInactiveTimeText(player: SLPlayer): String {
 		val time: Long = System.currentTimeMillis() - player.lastSeen.time
 
 		val prefix: String = when {
-			Bukkit.getPlayer(player._id.uuid) != null -> "${GREEN}Online"
-			else -> "${RED}Offline"
+			Bukkit.getPlayer(player._id.uuid) != null -> "<green>Online"
+			else -> "<red>Offline"
 		}
 
-		return "$prefix$GRAY for ${getDurationBreakdown(time)}"
+		return "$prefix<gray> for ${getDurationBreakdown(time)}"
 	}
 
 	override fun supportsVanilla(): Boolean {
