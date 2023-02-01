2282fcbe94dab1db3c14a8b677c85e83c2618c7f
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-31 19:14:11 -0800

    Removed deprecations from StarshipDebugCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/starship/StarshipDebugCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/starship/StarshipDebugCommand.kt
@@ -6,24 +6,25 @@ import co.aikar.commands.annotation.Subcommand
 import net.starlegacy.command.SLCommand
 import net.starlegacy.feature.starship.movement.StarshipTeleportation
 import net.starlegacy.util.CARDINAL_BLOCK_FACES
-import net.starlegacy.util.msg
 import org.bukkit.Location
 import org.bukkit.entity.Player
 
 @CommandPermission("starlegacy.starshipdebug")
 @CommandAlias("starshipdebug|sbug")
 object StarshipDebugCommand : SLCommand() {
+	@Suppress("Unused")
 	@Subcommand("teleport")
 	fun onTeleport(sender: Player, x: Int, y: Int, z: Int) {
 		val riding = getStarshipRiding(sender)
 		StarshipTeleportation.teleportStarship(riding, Location(sender.world, x.toDouble(), y.toDouble(), z.toDouble()))
 	}
 
+	@Suppress("Unused")
 	@Subcommand("thrusters")
 	fun onThrusters(sender: Player) {
 		val starship = getStarshipRiding(sender)
 		for (dir in CARDINAL_BLOCK_FACES) {
-			sender msg starship.thrusterMap[dir].toString()
+			sender.sendRichMessage(starship.thrusterMap[dir].toString())
 		}
 	}
 }
