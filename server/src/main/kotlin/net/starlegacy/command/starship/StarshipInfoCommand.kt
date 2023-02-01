6410af30fd3429edfdb6b6f81a78770a159fddfe
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-31 19:36:13 -0800

    Removed deprecations from StarshipInfoCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/starship/StarshipInfoCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/starship/StarshipInfoCommand.kt
@@ -8,7 +8,6 @@ import net.starlegacy.feature.starship.factory.StarshipFactories
 import net.starlegacy.feature.starship.hyperspace.Hyperspace
 import net.starlegacy.util.Vec3i
 import net.starlegacy.util.isConcrete
-import net.starlegacy.util.msg
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -16,6 +15,7 @@ import kotlin.math.round
 import kotlin.math.roundToInt
 
 object StarshipInfoCommand : SLCommand() {
+	@Suppress("Unused")
 	@CommandAlias("starshipinfo|starship")
 	fun onExecute(p: Player) {
 		val ship = getStarshipPiloting(p)
@@ -24,40 +24,46 @@ object StarshipInfoCommand : SLCommand() {
 
 		val size = ship.initialBlockCount
 
-		p msg "&b${ship.data.starshipType.displayName} &f($size blocks)"
-		p msg "   &7Mass:&f ${ship.mass}"
-		p msg "   &7World:&f ${ship.world.name}"
-		p msg "   &7Pilot:&f ${ship.pilot?.name}"
+		p.sendRichMessage(
+			"<aqua>${ship.data.starshipType.displayName} <white>($size blocks)\n" +
+					"   <gray>Mass:<white> ${ship.mass}\n" +
+					"   <gray>World:<white> ${ship.world.name}\n" +
+					"   <gray>Pilot:<white> ${ship.pilot?.name}"
+		)
 
 		val passengers = ship.onlinePassengers.map { it.name }.joinToString()
 		if (passengers.any()) {
-			p msg "   &7Passengers:&f $passengers"
+			p.sendRichMessage("   <gray>Passengers: <white>$passengers")
 		}
 
-		p msg "   &7Carbyne Percent:&f ${createPercent(blocks.values.count { it.type.isConcrete }, size)}"
+		p.sendRichMessage(
+			"   <gray>Carbyne Percent: <white>${createPercent(blocks.values.count { it.type.isConcrete }, size)}"
+		)
 
 		val inventoryCount = blocks.values.count { StarshipDetection.isInventory(it.type) } +
 			blocks.values.count { it.type == Material.CHEST || it.type == Material.TRAPPED_CHEST } * 2
-		p msg "   &7Inventory Percent:&f ${createPercent(inventoryCount, size)}"
+		p.sendRichMessage("   <gray>Inventory Percent: <white>${createPercent(inventoryCount, size)}")
 		val hyperdrive = Hyperspace.findHyperdrive(ship)
 		if (hyperdrive != null) {
 			val hyperdriveClass = hyperdrive.multiblock.hyperdriveClass
 			val vector = hyperdrive.pos
-			p msg "   &7Hyperdrive:&f Class $hyperdriveClass at $vector"
+			p.sendRichMessage("   <gray>Hyperdrive: <white>Class $hyperdriveClass at $vector")
 		}
 		if (!ship.weaponSets.isEmpty) {
-			p msg "   &7Controlled Weapon Sets:"
+			p.sendRichMessage("   <gray>Controlled Weapon Sets:")
 			for (gunner in ship.weaponSetSelections.mapNotNull { Bukkit.getPlayer(it.key) }) {
 				val weaponSet = ship.weaponSetSelections[gunner.uniqueId]
-				p msg "         &6${gunner.name}:&c $weaponSet"
+				p.sendRichMessage("         <gold>${gunner.name}: <red>$weaponSet")
 			}
 		}
 		val powerOutput = ship.reactor.output
-		p msg "   &7Power Output:&f $powerOutput"
-		p msg "   &7Weapon Capacitor Capacity:&f ${ship.reactor.weaponCapacitor.capacity}"
-		p msg "   &7Heavy Weapon Booster Output:&f ${ship.reactor.heavyWeaponBooster.output}"
+		p.sendRichMessage(
+			"   <gray>Power Output: <white>$powerOutput\n" +
+					"   <gray>Weapon Capacitor Capacity: <white>${ship.reactor.weaponCapacitor.capacity}\n" +
+					"   <gray>Heavy Weapon Booster Output: <white>${ship.reactor.heavyWeaponBooster.output}"
+		)
 
-		p msg "   &7Power Division:"
+		p.sendRichMessage("   <gray>Power Division:")
 		val powerTypes = listOf(
 			"Shield" to ship.reactor.powerDistributor.thrusterPortion,
 			"Weapon" to ship.reactor.powerDistributor.weaponPortion,
@@ -66,36 +72,36 @@ object StarshipInfoCommand : SLCommand() {
 		for ((name, percent) in powerTypes) {
 			val percentRounded = (percent * 100).roundToInt()
 			val currentPower = percent * powerOutput
-			p msg "      &7$name:&e $percentRounded% ($currentPower)"
+			p.sendRichMessage("      <gray>$name: <yellow>$percentRounded% ($currentPower)")
 		}
 
 		if (ship.autoTurretTargets.isNotEmpty()) {
-			p msg "   &7Auto Turret Targets:"
+			p.sendRichMessage("   <gray>Auto Turret Targets:")
 			for ((set, targetId) in ship.autoTurretTargets) {
 				val targetName = Bukkit.getOfflinePlayer(targetId).name
-				p msg "      &6$set:&c $targetName"
+				p.sendRichMessage("      <gold>$set: <red>$targetName")
 			}
 		}
 
 		if (ship.shields.isNotEmpty()) {
-			p msg "   &7Shields:"
+			p.sendRichMessage("   <gray>Shields:")
 			for (shield in ship.shields) {
 				val percent = createPercent(shield.power, shield.maxPower)
 				val shieldClass = shield.multiblock.signText[3]?.let { legacyAmpersand().serialize(it) }
-				p msg "      &7${shield.name}:&b $percent ($shieldClass&b)"
+				p.sendRichMessage("      <gray>${shield.name}: <aqua>$percent ($shieldClass<aqua>)")
 			}
-			p msg "   &7Shield Regen Efficiency:&b ${ship.shieldEfficiency}"
-			p msg "    &7Maximum Shields the starship can handle:&b ${ship.maxShields}   "
+			p.sendRichMessage("	  <gray>Shield Regen Efficiency: <aqua>${ship.shieldEfficiency}")
+			p.sendRichMessage("   <gray>Maximum Shields the starship can handle:&b ${ship.maxShields}")
 		}
 
-		p msg "   &7Hull Integrity:&f ${ship.hullIntegrity().times(100).roundToInt()}%"
-		p msg "   &7Center of Mass:&f ${ship.centerOfMassVec3i}"
+		p.sendRichMessage("   <gray>Hull Integrity: <white>${ship.hullIntegrity().times(100).roundToInt()}%")
+		p.sendRichMessage("   <gray>Center of Mass: <white>${ship.centerOfMassVec3i}")
 
 		val worth = blocks.values
 			.sumOf { StarshipFactories.getPrice(it.blockData) ?: 0.0 }
 			.roundToInt()
 
-		p msg "   &7Worth:&f ~$worth"
+		p.sendRichMessage("   <gray>Worth: <white>~$worth")
 	}
 
 	// creates a percent that goes down to the tens place
