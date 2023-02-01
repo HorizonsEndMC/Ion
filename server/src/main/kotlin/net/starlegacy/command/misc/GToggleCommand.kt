08e2ef1b280e758f606aec8795ea881757d74fb0
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-31 17:56:16 -0800

    Commands misc deprecations (#228)
    * Removed deprecations from BatteryCommand.kt
    
    * Removed deprecations from CustomItemCommand.kt
    
    * Removed deprecations from DyeCommand.kt
    
    * Removed deprecations from GlobalGameRuleCommand.kt
    
    * Removed deprecations from GToggleCommand.kt
    
    * Removed deprecations from ListCommand.kt
    
    * Removed deprecations from ShuttleCommand.kt
    
    * Removed deprecations from TransportDebugCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/misc/GToggleCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/misc/GToggleCommand.kt
@@ -1,15 +1,17 @@
 package net.starlegacy.command.misc
 
 import co.aikar.commands.annotation.CommandAlias
+import net.horizonsend.ion.server.legacy.feedback.FeedbackType
+import net.horizonsend.ion.server.legacy.feedback.sendFeedbackActionMessage
 import net.luckperms.api.LuckPermsProvider
 import net.luckperms.api.model.group.Group
 import net.luckperms.api.model.user.User
 import net.luckperms.api.node.NodeEqualityPredicate
 import net.starlegacy.command.SLCommand
-import net.starlegacy.util.actionAndMsg
 import org.bukkit.entity.Player
 
 object GToggleCommand : SLCommand() {
+	@Suppress("Unused")
 	@CommandAlias("gtoggle")
 	fun onExecute(sender: Player) {
 		val api = LuckPermsProvider.get()
@@ -20,10 +22,10 @@ object GToggleCommand : SLCommand() {
 
 		if (user.data().contains(groupNode, NodeEqualityPredicate.IGNORE_EXPIRY_TIME).asBoolean()) {
 			user.data().remove(groupNode)
-			sender actionAndMsg "&aGlobal chat shown"
+			sender.sendFeedbackActionMessage(FeedbackType.SUCCESS, "Global chat shown")
 		} else {
 			user.data().add(groupNode)
-			sender actionAndMsg "&cGlobal chat hidden"
+			sender.sendFeedbackActionMessage(FeedbackType.SUCCESS, "&cGlobal chat hidden")
 		}
 	}
 
