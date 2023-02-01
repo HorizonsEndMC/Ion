1c1a664a4f089833ecb2809e601f5ed11211b511
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2023-01-31 16:40:44 -0800

    Removed deprecations from SpaceWorldCommand.kt

--- server/src/main/kotlin/net/starlegacy/command/space/SpaceWorldCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/space/SpaceWorldCommand.kt
@@ -4,10 +4,10 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.server.legacy.feedback.FeedbackType
+import net.horizonsend.ion.server.legacy.feedback.sendFeedbackMessage
 import net.starlegacy.command.SLCommand
 import net.starlegacy.feature.space.SpaceWorlds
-import net.starlegacy.util.green
-import net.starlegacy.util.msg
 import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.command.CommandSender
@@ -15,22 +15,24 @@ import org.bukkit.command.CommandSender
 @CommandAlias("spaceworld")
 @CommandPermission("space.spaceworld")
 object SpaceWorldCommand : SLCommand() {
+	@Suppress("Unused")
 	@Subcommand("set")
 	@CommandCompletion("@worlds true|false")
 	fun onSet(sender: CommandSender, world: World, spaceWorld: Boolean) {
 		SpaceWorlds.setSpaceWorld(world, spaceWorld)
-		sender msg green("Set ${world.name} to space world: $spaceWorld")
+		sender.sendFeedbackMessage(FeedbackType.SUCCESS, "Set {0} to space world: {1}", world.name, spaceWorld)
 		onList(sender)
 	}
 
 	@Subcommand("list")
 	fun onList(sender: CommandSender) {
-		sender msg "&bSpace Worlds: &d" + (
+		sender.sendFeedbackMessage(FeedbackType.INFORMATION, "Space Worlds: " + (
 			Bukkit.getWorlds()
 				.filter { SpaceWorlds.contains(it) }
 				.takeIf { it.isNotEmpty() }
 				?.joinToString { it.name }
 				?: "None"
 			)
+		)
 	}
 }
