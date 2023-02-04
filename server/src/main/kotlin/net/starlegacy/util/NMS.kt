b973032414de3b52e9318634b3d4f7f93cfcdde9
Gutin1 <claytonpinter@gmail.com>
2023-02-03 22:34:01 -0600

    rewrite (not working rn)

--- server/src/main/kotlin/net/starlegacy/util/NMS.kt
+++ server/src/main/kotlin/net/starlegacy/util/NMS.kt
@@ -127,4 +127,6 @@ fun MutableBlockPos.multiply(otherPos: BlockPos) {
 	this.y = this.y * otherPos.y
 	this.z = this.z * otherPos.z
 }
+
+fun Triple<Int, Int, Int>.toBlockPos() = BlockPos(this.first, this.second, this.third)
 //endregion
