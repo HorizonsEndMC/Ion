74893b759d38b230e0e4946cd6bf6ae1b6ad8ee6
Sciath <67932943+sciathgh@users.noreply.github.com>
2023-02-12 13:52:48 +0000

    QOL Commands

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/qol/RecipeCommand.kt
@@ -0,0 +1,90 @@
+package net.horizonsend.ion.server.features.qol
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.server.IonServer.Companion.Ion
+import net.horizonsend.ion.server.features.customItems.CustomItems
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.inventory.InventoryView
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.RecipeChoice.MaterialChoice
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ShapelessRecipe
+
+@CommandAlias("recipe")
+@Suppress("Unused")
+@CommandPermission("ion.recipe")
+class RecipeCommand : BaseCommand() {
+	companion object {
+		val invs = mutableListOf<InventoryView>()
+	}
+
+	@Default
+	@Suppress("Unused")
+	@CommandPermission("ion.recipe")
+	@CommandCompletion("@customItem")
+	fun onCustomItemCommand(
+		sender: Player,
+		customItem: String
+	) {
+		val itemStack = CustomItems.getByIdentifier(customItem) ?: return
+		val recipe = Bukkit.getRecipe(NamespacedKey(Ion, itemStack.identifier.lowercase()))
+
+		if (recipe is ShapedRecipe) {
+			shapedRecipe(sender, recipe)
+		} else if (recipe is ShapelessRecipe) {
+			shapelessRecipe(sender, recipe)
+		}
+	}
+
+	@Subcommand("legacy")
+	@CommandPermission("ion.recipe")
+	@CommandCompletion("@customitem")
+	fun onItemCommand(
+		sender: Player,
+		customItem: String
+	) {
+		val itemStack = net.starlegacy.feature.misc.CustomItems[customItem] ?: return
+		val recipe = Bukkit.getRecipe(NamespacedKey(Ion, itemStack.id))
+
+		if (recipe is ShapedRecipe) {
+			shapedRecipe(sender, recipe)
+		} else if (recipe is ShapelessRecipe) {
+			shapelessRecipe(sender, recipe)
+		}
+	}
+
+	fun shapedRecipe(sender: Player, recipe: ShapedRecipe) {
+		val view: InventoryView = sender.openWorkbench(null, true)!!
+		val recipeShape: Array<String> = recipe.shape
+		val ingredientMap: Map<Char, ItemStack> =
+			recipe.choiceMap.filter { it.value is MaterialChoice }.mapValues { (it.value as MaterialChoice).itemStack }
+
+		for (j in recipeShape.indices) {
+			for (k in 0 until recipeShape[j].length) {
+				val item: ItemStack = ingredientMap[recipeShape[j].toCharArray()[k]] ?: continue
+				view.setItem(j * 3 + k + 1, item)
+			}
+		}
+
+		invs.add(view)
+	}
+
+	fun shapelessRecipe(sender: Player, recipe: ShapelessRecipe) {
+		val ingredients: List<ItemStack> =
+			recipe.choiceList.filterIsInstance<MaterialChoice>().map { it.itemStack }
+
+		val view = sender.openWorkbench(null, true)!!
+		for (i in ingredients.indices) {
+			view.setItem(i + 1, ingredients[i])
+		}
+
+		invs.add(view)
+	}
+}
--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Commands.kt
@@ -7,6 +7,7 @@ import net.horizonsend.ion.server.features.blasters.SettingsCommand
 import net.horizonsend.ion.server.features.bounties.BountyCommands
 import net.horizonsend.ion.server.features.customItems.commands.ConvertCommand
 import net.horizonsend.ion.server.features.customItems.commands.CustomItemCommand
+import net.horizonsend.ion.server.miscellaneous.commands.CalcExpCommand
 
 val commands = arrayOf(
 	BountyCommands(),
@@ -15,6 +16,7 @@ val commands = arrayOf(
 	CustomItemCommand(),
 	SettingsCommand(),
 	IonCommand(),
+	CalcExpCommand(),
 
 	AchievementsCommand()
 )
