ddbba4a33571b24aa65b1a2b54fbd19c5a9e3ace
Gutin1 <claytonpinter@gmail.com>
2023-02-12 00:16:19 -0600

    move stuff around for rebase

--- server/src/main/kotlin/net/horizonsend/ion/server/features/generation/generators/SpaceChunkGenerator.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/generation/generators/SpaceChunkGenerator.kt
@@ -1,4 +1,4 @@
-package net.horizonsend.ion.server.generation
+package net.horizonsend.ion.server.features.generation.generators
 
 import org.bukkit.World
 import org.bukkit.generator.BlockPopulator
