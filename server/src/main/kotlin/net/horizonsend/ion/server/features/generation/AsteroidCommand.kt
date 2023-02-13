37e5da708ada48927bb1af538cfe33e3cb34c944
Gutin1 <claytonpinter@gmail.com>
2023-02-13 02:51:31 -0600

    fixed it tonight

--- server/src/main/kotlin/net/horizonsend/ion/server/features/generation/AsteroidCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/generation/AsteroidCommand.kt
@@ -1,4 +1,4 @@
-package net.horizonsend.ion.server.commands
+package net.horizonsend.ion.server.features.generation
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
@@ -7,11 +7,11 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Optional
 import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.server.IonServer.Companion.Ion
 import net.horizonsend.ion.server.configuration.ServerConfiguration
 import net.horizonsend.ion.server.extensions.sendInformation
 import net.horizonsend.ion.server.extensions.sendServerError
 import net.horizonsend.ion.server.extensions.sendUserError
-import net.horizonsend.ion.server.features.generation.SpaceGenerationManager
 import net.horizonsend.ion.server.features.generation.generators.AsteroidGenerator
 import net.horizonsend.ion.server.legacy.feedback.FeedbackType
 import net.horizonsend.ion.server.legacy.feedback.sendFeedbackMessage
@@ -29,16 +29,13 @@ class AsteroidCommand(val configuration: ServerConfiguration) : BaseCommand() {
 	@CommandCompletion("Range")
 	@Subcommand("regenerate")
 	fun onRegenerate(sender: Player, @Optional @Default("0") range: Int) {
-		val generator = SpaceGenerationManager.getGenerator((sender.world as CraftWorld).handle) ?: return sender
-			.sendUserError("No generator found for ${sender.world.name}")
-
 		sender.sendInformation("Regenerating")
 		for (x in sender.chunk.x - range..sender.chunk.x + range) {
 			for (z in sender.chunk.z - range..sender.chunk.z + range) {
 				val chunk2 = sender.world.getChunkAt(x, z)
 
 				try {
-					generator.rebuildChunkAsteroids(chunk2)
+					AsteroidGenerator.rebuildChunkAsteroids(chunk2)
 				} catch (error: java.lang.Error) {
 					error.printStackTrace()
 					error.message?.let { sender.sendServerError(it) }
@@ -54,6 +51,8 @@ class AsteroidCommand(val configuration: ServerConfiguration) : BaseCommand() {
 	@Subcommand("create custom")
 	@CommandCompletion("size index octaves")
 	fun onCreateCustom(sender: Player, size: Double, index: Int, octaves: Int) {
+		println(SpaceGenerationManager.worldGenerators)
+
 		val generator = SpaceGenerationManager.getGenerator((sender.world as CraftWorld).handle) ?: return sender
 			.sendUserError("No generator found for ${sender.world.name}")
 
@@ -86,6 +85,9 @@ class AsteroidCommand(val configuration: ServerConfiguration) : BaseCommand() {
 	@CommandPermission("ion.space.regenerate")
 	@Subcommand("create random")
 	fun onCreateRandom(sender: Player) {
+		println(SpaceGenerationManager.worldGenerators)
+		println(Ion.configuration.spaceGenConfig)
+
 		val generator = SpaceGenerationManager.getGenerator((sender.world as CraftWorld).handle) ?: return sender
 			.sendUserError("No generator found for ${sender.world.name}")
 
