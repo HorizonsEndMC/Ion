23bcdde803c537712fd3d0eee2b33684819730f9
Sciath <67932943+SciathGH@users.noreply.github.com>
2023-02-14 21:58:59 +0000

    Cthulu beam Balancing

--- server/src/main/kotlin/net/horizonsend/ion/server/legacy/starshipweapon/projectile/CthulhuBeamProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/legacy/starshipweapon/projectile/CthulhuBeamProjectile.kt
@@ -1,5 +1,6 @@
 package net.horizonsend.ion.server.legacy.starshipweapon.projectile
 
+import net.horizonsend.ion.server.IonServer
 import net.starlegacy.feature.starship.active.ActiveStarship
 import net.starlegacy.util.Tasks
 import org.bukkit.Location
@@ -14,14 +15,14 @@ class CthulhuBeamProjectile(
 	dir: Vector,
 	shooter: Player?
 ) : RayTracedProjectile(starship, loc, dir, shooter) {
-	override val range: Double = 64.0
-	override var speed: Double = 1.0
-	override val shieldDamageMultiplier: Int = 10
-	override val thickness: Double = 1.0
-	override val explosionPower: Float = 1.0f
-	override val volume: Float = 0.25f
-	override val pitch: Float = 2.0f
-	override val soundName: String = "minecraft:block.beacon.power_select"
+	override val range: Double = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.range
+	override var speed: Double = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.speed
+	override val shieldDamageMultiplier: Int = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.shieldDamageMultiplier
+	override val thickness: Double = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.thickness
+	override val explosionPower: Float = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.explosionPower
+	override val volume: Float = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.volume.toFloat()
+	override val pitch: Float = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.pitch
+	override val soundName: String = IonServer.Ion.balancing.starshipWeapons.CthulhuBeam.soundName
 
 	override fun visualize(loc: Location, targetLocation: Location) {
 		val crystal: EnderCrystal = (loc.world.spawnEntity(loc, EntityType.ENDER_CRYSTAL) as EnderCrystal)
