daf860f6109d9c93f3fdbf64b981edcfb1a5d389
Sciath <67932943+SciathGH@users.noreply.github.com>
2023-02-14 21:57:10 +0000

    Point Defense Balancing

--- server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/PointDefenseLaserProjectile.kt
+++ server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/PointDefenseLaserProjectile.kt
@@ -1,5 +1,6 @@
 package net.starlegacy.feature.starship.subsystem.weapon.projectile
 
+import net.horizonsend.ion.server.IonServer
 import net.starlegacy.feature.starship.active.ActiveStarship
 import net.starlegacy.util.mcName
 import org.bukkit.Color
@@ -15,13 +16,13 @@ class PointDefenseLaserProjectile(
 	override val range: Double,
 	shooter: Player?
 ) : LaserProjectile(starship, loc, dir, shooter) {
-	override val speed: Double = 150.0
-	override val shieldDamageMultiplier: Int = 0
+	override val speed: Double = IonServer.Ion.balancing.starshipWeapons.PointDefence.speed
+	override val shieldDamageMultiplier: Int = IonServer.Ion.balancing.starshipWeapons.PointDefence.shieldDamageMultiplier
 	override val color: Color = Color.BLUE
-	override val thickness: Double = 0.2
-	override val particleThickness: Double = 0.35
-	override val explosionPower: Float = 0.0f
-	override val volume: Int = 20
-	override val pitch: Float = 2.0f
-	override val soundName: String = Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST.mcName
+	override val thickness: Double = IonServer.Ion.balancing.starshipWeapons.PointDefence.thickness
+	override val particleThickness: Double = IonServer.Ion.balancing.starshipWeapons.PointDefence.particleThickness
+	override val explosionPower: Float = IonServer.Ion.balancing.starshipWeapons.PointDefence.explosionPower
+	override val volume: Int = IonServer.Ion.balancing.starshipWeapons.PointDefence.volume
+	override val pitch: Float = IonServer.Ion.balancing.starshipWeapons.PointDefence.pitch
+	override val soundName: String = IonServer.Ion.balancing.starshipWeapons.PointDefence.soundName
 }
