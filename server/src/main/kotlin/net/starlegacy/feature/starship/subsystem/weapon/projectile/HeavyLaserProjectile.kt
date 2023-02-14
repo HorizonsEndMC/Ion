565616250a696ef8d487042fc87c65b4334111a3
Sciath <67932943+SciathGH@users.noreply.github.com>
2023-02-14 21:29:04 +0000

    Heavy laser Balancing

--- server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/HeavyLaserProjectile.kt
+++ server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/projectile/HeavyLaserProjectile.kt
@@ -1,5 +1,6 @@
 package net.starlegacy.feature.starship.subsystem.weapon.projectile
 
+import net.horizonsend.ion.server.IonServer
 import net.starlegacy.feature.starship.active.ActiveStarship
 import org.bukkit.Color
 import org.bukkit.Location
@@ -15,13 +16,13 @@ class HeavyLaserProjectile(
 	baseAimDistance: Int,
 	sound: String
 ) : TrackingLaserProjectile(starship, loc, dir, shooter, originalTarget, baseAimDistance) {
-	override val shieldDamageMultiplier = 2
-	override val maxDegrees: Double = 25.0
-	override val range: Double = 200.0
-	override val speed: Double = 50.0
+	override val shieldDamageMultiplier = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.shieldDamageMultiplier
+	override val maxDegrees: Double = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.maxDegrees
+	override val range: Double = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.range
+	override val speed: Double = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.speed
 	override val color: Color = Color.RED
-	override val thickness: Double = 0.35
-	override val particleThickness: Double = 1.0
-	override val explosionPower: Float = 12.0f
+	override val thickness: Double = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.thickness
+	override val particleThickness: Double = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.particleThickness
+	override val explosionPower: Float = IonServer.Ion.balancing.starshipWeapons.HeavyLaser.explosionPower
 	override val soundName: String = sound
 }
