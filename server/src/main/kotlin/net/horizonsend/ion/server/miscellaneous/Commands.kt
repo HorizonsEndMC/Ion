74c8d0dcdcd5fa0227fb1a71815121f4d3ac3411
Gutin1 <Claytonpinter@gmail.com>
2023-02-19 02:53:04 -0600

    command cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Commands.kt
@@ -1,17 +1,16 @@
 package net.horizonsend.ion.server.miscellaneous
 
 import net.horizonsend.ion.server.IonCommand
-import net.horizonsend.ion.server.IonServer.Companion.Ion
 import net.horizonsend.ion.server.configuration.ConfigurationCommands
 import net.horizonsend.ion.server.features.achievements.AchievementsCommand
 import net.horizonsend.ion.server.features.blasters.SettingsCommand
 import net.horizonsend.ion.server.features.bounties.BountyCommands
 import net.horizonsend.ion.server.features.customItems.commands.ConvertCommand
 import net.horizonsend.ion.server.features.customItems.commands.CustomItemCommand
-import net.horizonsend.ion.server.features.space.generation.AsteroidCommand
+import net.horizonsend.ion.server.features.space.generation.SpaceGenCommand
 
 val commands = arrayOf(
-	AsteroidCommand(Ion.configuration),
+	SpaceGenCommand(),
 	BountyCommands(),
 	ConfigurationCommands(),
 	ConvertCommand(),
