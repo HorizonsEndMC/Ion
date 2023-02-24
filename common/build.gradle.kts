931a4c88019460979a013d2772ef9b342f721c59
Astralchroma <astralchroma@proton.me>
2023-02-24 09:51:46 +0000

    New Configuration Loader

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,14 @@
+plugins {
+	kotlin("plugin.serialization")
+}
+
+repositories {
+	mavenCentral()
+}
+
 dependencies {
-	// Included
+	api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
 	api(libs.bundles.configurate)
 	api(libs.bundles.kotlin)
 	api(libs.jedis)
