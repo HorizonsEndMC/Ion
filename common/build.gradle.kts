64d0e6879b26069141201abf9a25313d032b6e2b
Peter Crawley <peterzcrawley@gmail.com>
2023-02-24 10:51:56 +0000

    Remove old configuration

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 dependencies {
 	api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-	api(libs.bundles.configurate)
 	api(libs.bundles.kotlin)
 	api(libs.jedis)
 	api(libs.kmongo)
--- common/src/main/kotlin/net/horizonsend/ion/common/Configuration.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/Configuration.kt
@@ -1,17 +1,12 @@
 package net.horizonsend.ion.common
 
-import io.leangen.geantyref.TypeToken
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToStream
-import org.spongepowered.configurate.hocon.HoconConfigurationLoader
-import org.spongepowered.configurate.kotlin.objectMapperFactory
-import org.spongepowered.configurate.kotlin.toNode
 import java.io.File
 import java.io.IOException
-import java.nio.file.Path
 
 object Configuration {
 	@OptIn(ExperimentalSerializationApi::class)
@@ -35,30 +30,4 @@ object Configuration {
 
 		Json.encodeToStream(clazz, file.outputStream())
 	}
-}
-
-@Deprecated("Use KotlinX Serialization based configuration")
-inline fun <reified T> loadConfiguration(file: File, fileName: String): T = loadConfiguration(file.toPath(), fileName)
-
-@Deprecated("Use KotlinX Serialization based configuration")
-inline fun <reified T> loadConfiguration(path: Path, fileName: String): T {
-	path.toFile().mkdirs()
-
-	val loader = HoconConfigurationLoader.builder()
-		.path(path.resolve(fileName))
-		.defaultOptions { options ->
-			options.serializers { builder ->
-				builder.registerAnnotatedObjects(objectMapperFactory())
-			}
-		}
-		.build()
-
-	val node = loader.load()
-
-	val configuration = node.get(object : TypeToken<T>() {})!!
-
-	configuration.toNode(node)
-	loader.save(node)
-
-	return configuration
 }
