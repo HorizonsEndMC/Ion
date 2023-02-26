08d6d4d801421be35ca920af08e2cc8f07774a4f
Peter Crawley <peterzcrawley@gmail.com>
2023-02-26 13:42:13 +0000

    SQL Database Connectivity

--- common/src/main/kotlin/net/horizonsend/ion/common/Connectivity.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/Connectivity.kt
@@ -4,8 +4,11 @@ import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoDatabase
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import kotlinx.serialization.Serializable
 import org.bson.UuidRepresentation
+import org.jetbrains.exposed.sql.Database
 import org.litote.kmongo.KMongo.createClient
 import org.litote.kmongo.util.KMongoJacksonFeature
 import redis.clients.jedis.JedisPooled
@@ -13,6 +16,9 @@ import java.io.File
 import java.lang.System.setProperty
 
 object Connectivity {
+	private lateinit var database: Database
+	private lateinit var datasource: HikariDataSource
+
 	internal lateinit var mongoDatabase: MongoDatabase private set
 	private lateinit var mongoClient: MongoClient
 
@@ -20,6 +26,16 @@ object Connectivity {
 
 	fun open(dataDirectory: File) {
 		val configuration: DatabaseConfiguration = Configuration.load(dataDirectory, "database.json")
+
+		// Manually loaded because classloaders are weird
+		Class.forName("org.mariadb.jdbc.Driver")
+		Class.forName("org.sqlite.JDBC")
+
+		val hikariConfiguration = HikariConfig()
+		hikariConfiguration.jdbcUrl = configuration.connectionUri
+
+		datasource = HikariDataSource(hikariConfiguration)
+		database = Database.connect(datasource)
 
 		setProperty("org.litote.mongo.test.mapping.service", "org.litote.kmongo.jackson.JacksonClassMappingTypeService")
 
@@ -41,10 +57,12 @@ object Connectivity {
 	fun close() {
 		jedisPool.close()
 		mongoClient.close()
+		datasource.close()
 	}
 
 	@Serializable
 	internal data class DatabaseConfiguration(
+		internal val connectionUri: String = "jdbc:sqlite:plugins/Ion/database.db",
 		internal val mongoConnectionUri: String = "mongodb://test:test@mongo",
 		internal val redisConnectionUri: String = "redis",
 		internal val databaseName: String = "ion"
