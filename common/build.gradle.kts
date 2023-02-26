24e167c1a694c22a237e1163d22f26efbd9770de
Peter Crawley <peterzcrawley@gmail.com>
2023-02-26 13:29:05 +0000

    Dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,10 +7,20 @@ repositories {
 }
 
 dependencies {
+	api(libs.bundles.kotlin)
+
 	api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 	api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-	api(libs.bundles.kotlin)
+	api("org.jetbrains.exposed:exposed-core:0.40.1")
+	api("org.jetbrains.exposed:exposed-dao:0.40.1")
+	api("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+
+	api("com.zaxxer:HikariCP:5.0.1")
+
+	api("org.mariadb.jdbc:mariadb-java-client:3.1.2")
+	api("org.xerial:sqlite-jdbc:3.41.0.0")
+
 	api(libs.jedis)
 	api(libs.kmongo)
 }
