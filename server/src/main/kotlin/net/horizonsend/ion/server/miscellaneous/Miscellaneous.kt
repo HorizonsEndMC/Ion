e9041ab367a9568be020719fba3f8f0ce94bc332
Gutin1 <Claytonpinter@gmail.com>
2023-03-01 22:45:59 -0600

    rat stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Miscellaneous.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/Miscellaneous.kt
@@ -11,11 +11,13 @@ import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.monster.Shulker
 import net.starlegacy.util.Tasks
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.craftbukkit.v1_19_R2.CraftWorld
 import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
-import java.util.*
+import java.util.EnumSet
 
 val vaultEconomy = try {
 	Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider
@@ -35,6 +37,10 @@ fun mainThreadCheck() {
 		)
 	}
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T : LivingEntity> World.castSpawnEntity(location: Location, type: org.bukkit.entity.EntityType) =
+	this.spawnEntity(location, type) as T
 
 val Player.handle: ServerPlayer
 	get() = (this as CraftPlayer).handle
