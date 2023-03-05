5a5c05e7c15b1daeabda600cd6324099380480ed
Gutin1 <Claytonpinter@gmail.com>
2023-03-05 02:17:20 -0600

    now it works

--- server/src/main/kotlin/net/horizonsend/ion/server/legacy/multiblocks/DisposalMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/legacy/multiblocks/DisposalMultiblock.kt
@@ -1,32 +1,45 @@
 package net.horizonsend.ion.server.legacy.multiblocks
 
-import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.starlegacy.feature.machine.PowerMachines
 import net.starlegacy.feature.multiblock.FurnaceMultiblock
 import net.starlegacy.feature.multiblock.LegacyMultiblockShape
 import net.starlegacy.feature.multiblock.PowerStoringMultiblock
 import net.starlegacy.util.getFacing
+import net.starlegacy.util.leftFace
 import org.bukkit.Material
-import org.bukkit.block.Chest
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Furnace
 import org.bukkit.block.Sign
 import org.bukkit.event.inventory.FurnaceBurnEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
 
 object DisposalMultiblock : PowerStoringMultiblock(), FurnaceMultiblock {
-	var amountCleared = 0
+	override val name = "incinerator"
+
+	override var signText: Array<Component?> = arrayOf(
+		Component.text("Incinerator").color(NamedTextColor.RED),
+		null,
+		null,
+		null
+	)
 
 	override val maxPower: Int = 150_000
+
+	private const val powerConsumed = 3
 
 	override fun LegacyMultiblockShape.buildStructure() {
 		z(+0) {
 			y(-1) {
-				x(-1).type(Material.FURNACE)
-				x(+0).type(Material.CHEST)
-				x(+1).noteBlock()
+				x(-1).anyPipedInventory()
+				x(+0).wireInputComputer()
+				x(+1).ironBlock()
 			}
 			y(0) {
 				x(-1).anyStairs()
-				x(0).anyGlass()
+				x(+0).machineFurnace()
 				x(+1).anyStairs()
 			}
 			y(+1) {
@@ -67,14 +80,16 @@ object DisposalMultiblock : PowerStoringMultiblock(), FurnaceMultiblock {
 		}
 	}
 
-	override val name = "charger"
-
-	override var signText = createSignText(
-		line1 = "&14Disposal",
-		line2 = null,
-		line3 = null,
-		line4 = amountCleared.toString()
-	)
+	private fun getOutput(sign: Sign): Inventory {
+		val direction = sign.getFacing().oppositeFace
+		return (
+			sign.block.getRelative(direction)
+				.getRelative(direction.leftFace)
+				.getRelative(BlockFace.DOWN)
+				.getState(false) as InventoryHolder
+			)
+			.inventory
+	}
 
 	override fun onFurnaceTick(
 		event: FurnaceBurnEvent,
@@ -83,30 +98,25 @@ object DisposalMultiblock : PowerStoringMultiblock(), FurnaceMultiblock {
 	) {
 		event.isBurning = false
 		event.burnTime = 0
-		val chest = sign.block.getRelative(sign.getFacing().oppositeFace).getRelative(0, -1, 0)
-		val inventory = (chest as? Chest)?.inventory ?: return
-		val furnaceInventory = furnace.inventory
-		val smelting = furnaceInventory.smelting
+		val inventory = getOutput(sign)
 		val power = PowerMachines.getPower(sign)
-		if (smelting == null || smelting.type != Material.PRISMARINE_CRYSTALS) {
-			return
-		}
-		if (power == 0) {
-			return
-		}
+		if (power == 0) return
+
 		var amountToClear = 0
-		inventory.storageContents.forEach {
-			if (it != null){
-				amountToClear+=it.amount
-			}
+
+		// Clear while checking for power
+		for (i in 0 until inventory.size) {
+			val size = (inventory.getItem(i) ?: continue).amount
+			if ((size * powerConsumed) + (amountToClear * 3) >= power) continue
+			amountToClear += size
+			inventory.clear(i)
 		}
-		inventory.clear()
-		PowerMachines.setPower(sign, power - 3*amountToClear)
-		amountCleared += amountToClear
+
+		PowerMachines.setPower(sign, power - 3 * amountToClear)
 		furnace.cookTime = 20.toShort()
-		event.isCancelled = false
+
+		event.isCancelled = true
 		event.isBurning = false
 		event.burnTime = 20
-		signText[4] = MiniMessage.miniMessage().deserialize("<gray>$amountCleared")
 	}
 }
