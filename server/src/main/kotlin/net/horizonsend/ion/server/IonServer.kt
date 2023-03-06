c74c8484141313e7fa0044a098115f49b44b1fa9
Gutin1 <claytonpinter@gmail.com>
2023-03-06 03:33:04 -0600

    rewrite on the larger size

--- server/src/main/kotlin/net/horizonsend/ion/server/IonServer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/IonServer.kt
@@ -135,7 +135,6 @@ object IonServer : JavaPlugin() {
 
 					pluginManager.registerEvents(CityNPCs, this)
 
-					SpaceGenerationManager.primeGeneration()
 					commandManager.commandCompletions.registerCompletion("wreckSchematics") { context ->
 						SpaceGenerationManager.getGenerator(
 							(context.player.world as CraftWorld).handle
