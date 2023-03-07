85ae7f27fba34bbcab2ae8b8e6ec0dfd6232f1c8
Peter Crawley <peterzcrawley@gmail.com>
2023-03-07 19:08:16 +0000

    Warn when setting serverLevel asynchronously

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/Starship.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/Starship.kt
@@ -1,7 +1,12 @@
 package net.horizonsend.ion.server.features.starship
 
+import net.horizonsend.ion.server.miscellaneous.mainThreadCheck
 import net.minecraft.server.level.ServerLevel
 
-open class Starship(
-	open var serverLevel: ServerLevel
-)
+open class Starship(serverLevel: ServerLevel) {
+	open var serverLevel = serverLevel
+		set(value) {
+			mainThreadCheck()
+			field = value
+		}
+}
