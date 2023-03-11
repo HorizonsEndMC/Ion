39672bc438c0fb8e7ebc0e0893b7191c7698de12
Sciath <67932943+SciathGH@users.noreply.github.com>
2023-03-11 01:18:59 +0000

    Sounds

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/mininglaser/multiblock/MiningLaserMultiblockTier2.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/mininglaser/multiblock/MiningLaserMultiblockTier2.kt
@@ -27,6 +27,7 @@ sealed class MiningLaserMultiblockTier2 : MiningLaserMultiblock() {
 	override val mineRadius = 5
 	override val beamCount: Int = 3
 	override val maxBroken: Int = 7
+	override val sound: String = "starship.weapon.mining_laser.mining_laser_t2_loop"
 }
 
 object MiningLaserMultiblockTier2Top : MiningLaserMultiblockTier2() {
