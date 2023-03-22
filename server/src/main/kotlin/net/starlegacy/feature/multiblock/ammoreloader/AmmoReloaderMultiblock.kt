51f02b82a38ada207f861b6f722123e6eb2f98f8
kwazedilla <kwazedilla@gmail.com>
2023-03-22 16:53:54 -0700

    started ammo reloader multiblock

---
+++ server/src/main/kotlin/net/starlegacy/feature/multiblock/ammoreloader/AmmoReloaderMultiblock.kt
@@ -0,0 +1,9 @@
+package net.starlegacy.feature.multiblock.ammoreloader
+
+import net.starlegacy.feature.multiblock.FurnaceMultiblock
+import net.starlegacy.feature.multiblock.PowerStoringMultiblock
+import org.bukkit.Material
+
+abstract class AmmoReloaderMultiblock(val tierText: String) : PowerStoringMultiblock(), FurnaceMultiblock {
+	protected abstract val tierMaterial: Material
+}
