add1854f006e78365e1f6e309e45d96312c11423
Gutin1 <claytonpinter@gmail.com>
2023-03-26 21:16:17 -0500

    rebase

--- server/src/main/kotlin/net/starlegacy/util/NMS.kt
+++ server/src/main/kotlin/net/starlegacy/util/NMS.kt
@@ -127,6 +127,4 @@ fun MutableBlockPos.multiply(otherPos: BlockPos) {
 	this.y = this.y * otherPos.y
 	this.z = this.z * otherPos.z
 }
-
-fun Triple<Int, Int, Int>.toBlockPos() = BlockPos(this.first, this.second, this.third)
 //endregion
