c45ca8ea5aefb8bd5100606984d1e5ed78597233
kwazedilla <kwazedilla@gmail.com>
2023-03-29 21:52:13 -0700

    refilling functionality complete - ammo press now consumes refill items to reload blasters/magazines - bug: furnace does not turn off after finishing

--- server/src/main/kotlin/net/starlegacy/feature/multiblock/ammopress/AmmoPressMultiblock.kt
+++ server/src/main/kotlin/net/starlegacy/feature/multiblock/ammopress/AmmoPressMultiblock.kt
@@ -6,10 +6,13 @@ import net.starlegacy.feature.machine.PowerMachines
 import net.starlegacy.feature.multiblock.FurnaceMultiblock
 import net.starlegacy.feature.multiblock.LegacyMultiblockShape
 import net.starlegacy.feature.multiblock.PowerStoringMultiblock
+import net.starlegacy.util.getFacing
 import org.bukkit.Material
 import org.bukkit.block.Furnace
 import org.bukkit.block.Sign
 import org.bukkit.event.inventory.FurnaceBurnEvent
+import org.bukkit.inventory.InventoryHolder
+import org.bukkit.inventory.ItemStack
 import java.lang.Integer.min
 
 abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblock {
@@ -17,15 +20,13 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 	override fun LegacyMultiblockShape.buildStructure() {
 		z(+0) {
 			y(-1) {
-				x(-1).anySlab()
+				x(-1).anyStairs()
 				x(+0).wireInputComputer()
-				x(+1).anySlab()
+				x(+1).anyStairs()
 			}
 
 			y(0) {
-				x(-1).anySlab()
 				x(+0).machineFurnace()
-				x(+1).anySlab()
 			}
 		}
 
@@ -37,9 +38,9 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 			}
 
 			y(+0) {
-				x(-1).anyStairs()
+				x(-1).anySlab()
 				x(+0).type(Material.GRINDSTONE)
-				x(+1).anyStairs()
+				x(+1).anySlab()
 			}
 		}
 
@@ -93,22 +94,20 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 			}
 
 			y(+0) {
-				x(-1).anyStairs()
+				x(-1).anySlab()
 				x(+0).type(Material.GRINDSTONE)
-				x(+1).anyStairs()
+				x(+1).anySlab()
 			}
 		}
 		z(+6) {
 			y(-1) {
-				x(-1).anySlab()
+				x(-1).anyStairs()
 				x(+0).anyGlass()
-				x(+1).anySlab()
+				x(+1).anyStairs()
 			}
 
 			y(0) {
-				x(-1).anySlab()
 				x(+0).anyPipedInventory()
-				x(+1).anySlab()
 			}
 		}
 	}
@@ -127,7 +126,6 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 		furnace: Furnace,
 		sign: Sign
 	) {
-		event.isCancelled = true
 		val smelting = furnace.inventory.smelting
 		val fuel = furnace.inventory.fuel
 		val fuelCustomItem = fuel?.customItem
@@ -141,11 +139,7 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 			return
 		}
 
-		event.isBurning = false
-		event.burnTime = 20
-		furnace.cookTime = (-1000).toShort()
-		event.isCancelled = false
-
+		// deposit blaster/magazine into output if full
 		if ((fuelCustomItem as AmmunitionHoldingItem).getAmmunition(fuel) == fuelCustomItem.getMaximumAmmunition()) {
 			val result = furnace.inventory.result
 			if (result != null && result.type != Material.AIR) return
@@ -154,8 +148,25 @@ abstract class AmmoPressMultiblock() : PowerStoringMultiblock(), FurnaceMultiblo
 			return
 		}
 
-		val ammoToSet = min(fuelCustomItem.getMaximumAmmunition() - fuelCustomItem.getAmmunition(fuel), fuelCustomItem.getAmmoPerRefill())
+		// refill item check
+		val direction = sign.getFacing().oppositeFace
+		val state = sign.block.getRelative(direction, 7).getState(false)
+			as? InventoryHolder ?: return
+		val inventory = state.inventory
+		val typeRefill = Material.values().find { it.name == fuelCustomItem.getTypeRefill() } ?: return
+		if (!inventory.containsAtLeast(ItemStack(typeRefill), 1)) return
+
+		event.isBurning = false
+		event.burnTime = 200
+		furnace.cookTime = (-1000).toShort()
+		event.isCancelled = false
+
+		val ammoToSet = min(
+			fuelCustomItem.getMaximumAmmunition() - fuelCustomItem.getAmmunition(fuel),
+			fuelCustomItem.getAmmoPerRefill()
+		)
 		fuelCustomItem.setAmmunition(fuel, furnace.inventory, fuelCustomItem.getAmmunition(fuel) + ammoToSet)
+		inventory.removeItemAnySlot(ItemStack(typeRefill))
 		PowerMachines.removePower(sign, 250)
 	}
 }
