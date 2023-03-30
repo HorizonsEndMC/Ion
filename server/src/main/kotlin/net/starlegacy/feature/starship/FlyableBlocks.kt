9da226920e852cabcedc62000e308a0d4c97a610
Sciath <67932943+SciathGH@users.noreply.github.com>
2023-03-31 00:42:12 +0100

    Make terracotta flyable

--- server/src/main/kotlin/net/starlegacy/feature/starship/FlyableBlocks.kt
+++ server/src/main/kotlin/net/starlegacy/feature/starship/FlyableBlocks.kt
@@ -142,6 +142,7 @@ import org.bukkit.Material.SPONGE
 import org.bukkit.Material.SPRUCE_FENCE_GATE
 import org.bukkit.Material.STICKY_PISTON
 import org.bukkit.Material.STONECUTTER
+import org.bukkit.Material.TERRACOTTA
 import org.bukkit.Material.TORCH
 import org.bukkit.Material.TRAPPED_CHEST
 import org.bukkit.Material.VERDANT_FROGLIGHT
@@ -307,7 +308,9 @@ val FLYABLE_BLOCKS: EnumSet<Material> = mutableSetOf(
 	WARPED_FENCE_GATE,
 
 	LIGHTNING_ROD,
-	NETHERITE_BLOCK
+	NETHERITE_BLOCK,
+
+	TERRACOTTA
 
 ).also {
 	it.addAll(CONCRETE_TYPES)
