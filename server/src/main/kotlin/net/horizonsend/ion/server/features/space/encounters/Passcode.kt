7a0b7eb083b0f7e9deb975efa0598be51255e0c2
kwazedilla <kwazedilla@gmail.com>
2023-04-26 00:17:06 -0700

    passcode update (not finished, unable to test)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/space/encounters/Passcode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/space/encounters/Passcode.kt
@@ -3,13 +3,19 @@ package net.horizonsend.ion.server.features.space.encounters
 import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.extensions.userError
-import net.horizonsend.ion.server.miscellaneous.NamespacedKeys
-import net.kyori.adventure.text.minimessage.MiniMessage
+import net.horizonsend.ion.server.features.space.encounters.Encounters.createLootChest
+import net.horizonsend.ion.server.features.space.encounters.Encounters.getChestFlag
+import net.horizonsend.ion.server.features.space.encounters.Encounters.setChestFlag
+import net.horizonsend.ion.server.miscellaneous.NamespacedKeys.INACTIVE
+import net.horizonsend.ion.server.miscellaneous.NamespacedKeys.PASSCODE_CODE
+import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 import net.minecraft.nbt.CompoundTag
 import net.starlegacy.feature.nations.gui.skullItem
 import net.starlegacy.util.MenuHelper
 import net.starlegacy.util.randomInt
-import org.bukkit.Sound
+import org.bukkit.Sound.BLOCK_NOTE_BLOCK_BASS
+import org.bukkit.Sound.BLOCK_NOTE_BLOCK_HARP
+import org.bukkit.Sound.BLOCK_NOTE_BLOCK_SNARE
 import org.bukkit.World
 import org.bukkit.block.Chest
 import org.bukkit.entity.Player
@@ -18,7 +24,7 @@ import java.util.UUID
 
 object Passcode : Encounter(identifier = "passcode") {
 
-	val headID = mapOf(
+	private val headID = mapOf(
 		0 to UUID.fromString("cb34f183-2b0a-44a4-b87c-df957a63a4f0"),
 		1 to UUID.fromString("606d2a18-b91c-4f7f-9b0d-b3cd322e9214"),
 		2 to UUID.fromString("e4d886d5-331e-4558-8f69-4cca32085bc9"),
@@ -31,7 +37,7 @@ object Passcode : Encounter(identifier = "passcode") {
 		9 to UUID.fromString("a67e73f4-c989-4586-b571-561fc2dc069e")
 	)
 
-	val skinID = mapOf(
+	private val skinID = mapOf(
 		0 to "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmZhNDU5MTFiMTYyOThjZmNhNGIyMjkxZWVkYTY2NjExM2JjNmYyYTM3ZGNiMmVjZDhjMjc1NGQyNGVmNiJ9fX0=",
 		1 to "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2FmMWIyODBjYWI1OWY0NDY5ZGFiOWYxYTJhZjc5MjdlZDk2YTgxZGYxZTI0ZDUwYThlMzk4NGFiZmU0MDQ0In19fQ==",
 		2 to "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTRiMWUxZDQyNjEyM2NlNDBjZDZhNTRiMGY4NzZhZDMwYzA4NTM5Y2Y1YTZlYTYzZTg0N2RjNTA3OTUwZmYifX19",
@@ -57,7 +63,7 @@ object Passcode : Encounter(identifier = "passcode") {
 		9 to Pair(2, 0)
 	)
 
-	fun paneInteractCheck(player: Player, chest: Chest, passcode: String, currentCode: String) {
+	private fun paneInteractCheck(player: Player, chest: Chest, passcode: String, currentCode: String) {
 		when (compareCode(passcode, currentCode)) {
 			false -> playerFails(player)
 			true -> playerSucceeds(player, chest)
@@ -65,7 +71,7 @@ object Passcode : Encounter(identifier = "passcode") {
 		}
 	}
 
-	fun compareCode(passcode: String, currentCode: String): Boolean? {
+	private fun compareCode(passcode: String, currentCode: String): Boolean? {
 		return if (currentCode.isNotEmpty() && (currentCode.length > passcode.length || currentCode.last() != passcode[currentCode.length - 1])) {
 			false
 		} else if (currentCode == passcode) {
@@ -73,31 +79,27 @@ object Passcode : Encounter(identifier = "passcode") {
 		} else null
 	}
 
-	fun playerFails(player: Player) {
+	private fun playerFails(player: Player) {
 		player.closeInventory()
 		player.userError("You entered the wrong passcode!")
-		player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BASS, 5.0f, 1.0f)
+		player.playSound(player, BLOCK_NOTE_BLOCK_BASS, 5.0f, 1.0f)
 	}
 
-	fun playerSucceeds(player: Player, chest: Chest) {
+	private fun playerSucceeds(player: Player, chest: Chest) {
 		player.closeInventory()
 		player.success("You entered the correct passcode! The chest is now unlocked.")
-		chest.location.world.playSound(chest.location, Sound.BLOCK_NOTE_BLOCK_HARP, 5.0f, 2.0f)
-		Encounters.setChestFlag(chest, NamespacedKeys.INACTIVE, "true")
+		chest.location.world.playSound(chest.location, BLOCK_NOTE_BLOCK_HARP, 5.0f, 2.0f)
+		setChestFlag(chest, INACTIVE, "true")
 	}
 
-	fun playerContinues(chest: Chest) {
-		chest.location.world.playSound(chest.location, Sound.BLOCK_NOTE_BLOCK_SNARE, 5.0f, 1.0f)
+	private fun playerContinues(chest: Chest) {
+		chest.location.world.playSound(chest.location, BLOCK_NOTE_BLOCK_SNARE, 5.0f, 1.0f)
 	}
 
 	override fun generate(world: World, chestX: Int, chestY: Int, chestZ: Int) {
 		// Generate a new passcode unique to each wreck
 		val passcode = randomInt(0, 999999).toString().padStart(6, '0')
-		Encounters.setChestFlag(
-			world.getBlockAt(chestX, chestY, chestZ).state as Chest,
-			NamespacedKeys.PASSCODE_CODE,
-			passcode
-		)
+		setChestFlag(world.getBlockAt(chestX, chestY, chestZ).state as Chest, PASSCODE_CODE, passcode)
 	}
 
 	override fun onChestInteract(event: PlayerInteractEvent) {
@@ -105,10 +107,10 @@ object Passcode : Encounter(identifier = "passcode") {
 
 		event.isCancelled = true
 		val chest = (targetedBlock.state as? Chest) ?: return
-		val passcode = Encounters.getChestFlag(chest, NamespacedKeys.PASSCODE_CODE)
+		val passcode = getChestFlag(chest, PASSCODE_CODE)
 		var currentCode = ""
 
-		event.player.information("DEBUG: Passcode: $passcode")
+		event.player.information("Enter this passcode: $passcode")
 
 		MenuHelper.apply {
 			val pane = staticPane(3, 0, 3, 4)
@@ -117,9 +119,8 @@ object Passcode : Encounter(identifier = "passcode") {
 				pane.addItem(
 					guiButton(skullItem(i.toString(), headID[i]!!, skinID[i]!!)) {
 						currentCode += i.toString()
-						event.player.information("DEBUG: Current code: $currentCode")
 						paneInteractCheck(event.player, chest, passcode!!, currentCode)
-					}.setName(MiniMessage.miniMessage().deserialize(i.toString())),
+					}.setName(miniMessage().deserialize(i.toString())),
 					coordinates[i]!!.first, coordinates[i]!!.second
 				)
 			}
@@ -129,6 +130,6 @@ object Passcode : Encounter(identifier = "passcode") {
 	}
 
 	override fun constructChestNBT(): CompoundTag {
-		return Encounters.createLootChest("horizonsend:chests/guns")
+		return createLootChest("horizonsend:chests/guns")
 	}
 }
