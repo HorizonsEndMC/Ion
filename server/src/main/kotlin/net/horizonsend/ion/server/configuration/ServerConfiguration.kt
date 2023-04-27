7484b4c4247a3e5391e7c70960f0c67ac34d9b72
Gutin1 <Claytonpinter@gmail.com>
2023-04-26 21:09:36 -0500

    rewrite

--- server/src/main/kotlin/net/horizonsend/ion/server/configuration/ServerConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/configuration/ServerConfiguration.kt
@@ -1,15 +1,21 @@
 package net.horizonsend.ion.server.configuration
 
+import com.sk89q.worldedit.extent.clipboard.Clipboard
 import kotlinx.serialization.Serializable
+import net.horizonsend.ion.server.IonServer
 import net.minecraft.core.BlockPos
+import net.starlegacy.util.readSchematic
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.World
 
 @Serializable
 data class ServerConfiguration(
 	val serverName: String? = null,
-	val beacons: List<HyperspaceBeacon> = listOf()
+	val particleColourChoosingMoneyRequirement: Double? = 5.0,
+	val beacons: List<HyperspaceBeacon>,
+	val soldShips: List<Ship>
 ) {
 	@Serializable
 	data class HyperspaceBeacon(
@@ -35,5 +41,26 @@ data class ServerConfiguration(
 		fun toBlockPos(): BlockPos = BlockPos(x, y, z)
 
 		fun toLocation(): Location = Location(bukkitWorld(), x.toDouble(), y.toDouble(), z.toDouble())
+	}
+
+	/**
+	 * @param cooldown ticks
+	 **/
+	@Serializable
+	data class Ship(
+		val price: Double,
+		val displayName: String,
+		val name: String,
+		val guiMaterial: Material,
+		val cooldown: Long,
+		val teleportOffsetX: Double, // teleport offsets to teleport the player to after the ship is placed (away from schematic origin)
+		val teleportOffsetY: Double,
+		val teleportOffsetZ: Double,
+		val lore: List<String>
+	) {
+		@kotlinx.serialization.Transient
+		private val schematicFile = IonServer.dataFolder.resolve("sold_ships").resolve("$name.schem")
+
+		fun schematic(): Clipboard = readSchematic(schematicFile)!!
 	}
 }
