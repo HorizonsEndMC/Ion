3d1359293be02f39bbc3f2a5f4c504572755c13c
Gutin1 <Claytonpinter@gmail.com>
2023-04-28 05:10:30 -0500

    fix dynmap markers

--- server/src/main/kotlin/net/starlegacy/feature/hyperspace/HyperspaceBeacons.kt
+++ server/src/main/kotlin/net/starlegacy/feature/hyperspace/HyperspaceBeacons.kt
@@ -8,29 +8,20 @@ import org.dynmap.markers.MarkerSet
 object HyperspaceBeacons : SLComponent() {
 	fun reloadDynmap() {
 		val api = try { DynmapPlugin.plugin.markerAPI } catch (_: Error) { return } // dynmap not installed
-		var set: MarkerSet? = api.getMarkerSet("starships.hyperspace")
+		var set: MarkerSet? = api.getMarkerSet("beacons")
 
 		set?.deleteMarkerSet()
 
-		set = api.createMarkerSet("starships.hyperspace", "Hyperspace", null, false)
+		set = api.createMarkerSet("beacons", "Beacons", null, false)
 
 		for (beacon in IonServer.configuration.beacons) {
-			val split = beacon.name.split("_")
-
-			if (split.size != 2) continue
-
-			val name =
-				"${split[0].replaceFirstChar { it.uppercase() }} System -> ${split[1].replaceFirstChar { it.uppercase() }} System"
-
-			val x = beacon.spaceLocation.x.toDouble()
-			val z = beacon.spaceLocation.z.toDouble()
 			set.createMarker(
 				beacon.name,
 				beacon.name,
 				beacon.spaceLocation.world,
-				x,
+				beacon.spaceLocation.x.toDouble(),
 				128.0,
-				z,
+				beacon.spaceLocation.z.toDouble(),
 				api.getMarkerIcon("portal"),
 				false
 			)
