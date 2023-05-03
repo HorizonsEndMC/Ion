8a9d140c8c22589899510ddd8315fa6d255cb2ac
Astralchroma <astralchroma@proton.me>
2023-05-03 10:54:29 +0100

    Colors::calculateColorSafety

--- common/src/main/kotlin/net/horizonsend/ion/common/Colors.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/Colors.kt
@@ -1,5 +1,7 @@
 package net.horizonsend.ion.common
 
+import kotlin.math.pow
+
 object Colors {
 	const val SERVER_ERROR = 0xff3f3f
 	const val USER_ERROR = 0xff7f3f
@@ -8,4 +10,39 @@ object Colors {
 	const val SPECIAL = 0xffd700
 	const val SUCCESS = 0x3fff3f
 	const val HINT = 0x7f7f7f
+
+	enum class ColorSafety {
+		TOO_BRIGHT,
+		ACCEPTABLE,
+		TOO_DARK
+	}
+
+	fun calculateColorSafety(red: Int, green: Int, blue: Int): ColorSafety {
+		val perceivedLightness = calculatePerceivedLightness(red, green, blue)
+		if (perceivedLightness > 0.8) return ColorSafety.TOO_BRIGHT
+		if (perceivedLightness < 0.4) return ColorSafety.TOO_DARK
+		return ColorSafety.ACCEPTABLE
+	}
+
+	/**
+	 * Calculates the "perceived lightness" of the color, that being how bright the color appears to the human eye.
+	 *
+	 * Based on: https://stackoverflow.com/questions/56678483
+	 * @return Perceived lightness on a scale of 0 to 1, where 0 is pure black and 1 is pure white.
+	 */
+	private fun calculatePerceivedLightness(red: Int, green: Int, blue: Int): Double {
+		val decimalRed = red / 255.0
+		val decimalGreen = green / 255.0
+		val decimalBlue = blue / 255.0
+
+		val linearRed = if (decimalRed <= 0.04045) decimalRed / 12.92 else ((decimalRed + 0.055) / 1.055).pow(2.4)
+		val linearGreen = if (decimalGreen <= 0.04045) decimalGreen / 12.92 else ((decimalGreen + 0.055) / 1.055).pow(2.4)
+		val linearBlue = if (decimalBlue <= 0.04045) decimalBlue / 12.92 else ((decimalBlue + 0.055) / 1.055).pow(2.4)
+
+		val luminance = 0.2126 * linearRed + 0.7152 * linearGreen + 0.0722 * linearBlue
+
+		val perceivedLightness = if (luminance <= (216.0 / 24389.0)) luminance * (24389.0 / 27.0) else luminance.pow(1.0 / 3.0) * 116 - 16
+
+		return perceivedLightness / 100.0
+	}
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/blasters/SettingsCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/blasters/SettingsCommand.kt
@@ -6,12 +6,15 @@ import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
 import co.aikar.commands.annotation.Values
+import net.horizonsend.ion.common.Colors.ColorSafety.ACCEPTABLE
+import net.horizonsend.ion.common.Colors.ColorSafety.TOO_BRIGHT
+import net.horizonsend.ion.common.Colors.ColorSafety.TOO_DARK
+import net.horizonsend.ion.common.Colors.calculateColorSafety
 import net.horizonsend.ion.common.database.PlayerData
 import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.common.extensions.userError
 import org.bukkit.Color
 import org.bukkit.entity.Player
-import kotlin.math.pow
 import org.jetbrains.exposed.sql.transactions.transaction
 
 @CommandAlias("settings")
@@ -43,27 +46,14 @@ class SettingsCommand : BaseCommand() {
 
 		@Suppress("NAME_SHADOWING") val blue = blue.coerceIn(0..255)
 
-		// We get the perceived lightness to prevent people from selecting colors which are hard to see.
-		// https://stackoverflow.com/questions/56678483
-		val decimalRed = red / 255.0
-		val decimalGreen = green / 255.0
-		val decimalBlue = blue / 255.0
-
-		val linearRed = if (decimalRed <= 0.04045) decimalRed / 12.92 else ((decimalRed + 0.055) / 1.055).pow(2.4)
-		val linearGreen = if (decimalGreen <= 0.04045) decimalGreen / 12.92 else ((decimalGreen + 0.055) / 1.055).pow(2.4)
-		val linearBlue = if (decimalBlue <= 0.04045) decimalBlue / 12.92 else ((decimalBlue + 0.055) / 1.055).pow(2.4)
-
-		val luminance = 0.2126 * linearRed + 0.7152 * linearGreen + 0.0722 * linearBlue
-		val perceivedLightness =
-			if (luminance <= (216.0 / 24389.0)) luminance * (24389.0 / 27.0) else luminance.pow(1.0 / 3.0) * 116 - 16
-
-		if (perceivedLightness < 50.0) {
-			sender.userError("This color is too dark, please choose something lighter.")
-			return@transaction
+		when (calculateColorSafety(red, green, blue)) {
+			TOO_BRIGHT -> sender.userError("Chosen color is too bright!")
+			TOO_DARK -> sender.userError("Chosen color is too dark!")
+			ACCEPTABLE -> {
+				PlayerData[sender.uniqueId]?.color = Color.fromRGB(red, green, blue).asRGB()
+				sender.information("Set color to $red $green $blue")
+			}
 		}
-
-		PlayerData[sender.uniqueId]?.color = Color.fromRGB(red, green, blue).asRGB()
-		sender.information("Set color to $red $green $blue")
 	}
 
 	@Suppress("Unused")
