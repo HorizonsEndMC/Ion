3598b849ba8f19082abe2fbf5b10934b59222f8b
Rattlyy <giovannimariamazzone@gmail.com>
2023-06-10 15:30:07 +0200

    manual HTs initial work

--- server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/primary/HeavyTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/primary/HeavyTurretWeaponSubsystem.kt
@@ -4,7 +4,6 @@ import net.horizonsend.ion.server.IonServer
 import net.starlegacy.feature.multiblock.starshipweapon.turret.HeavyTurretMultiblock
 import net.starlegacy.feature.starship.active.ActiveStarship
 import net.starlegacy.feature.starship.subsystem.weapon.TurretWeaponSubsystem
-import net.starlegacy.feature.starship.subsystem.weapon.interfaces.AutoWeaponSubsystem
 import net.starlegacy.util.Vec3i
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
@@ -16,6 +15,10 @@ class HeavyTurretWeaponSubsystem(
 	face: BlockFace,
 	override val multiblock: HeavyTurretMultiblock
 ) : TurretWeaponSubsystem(ship, pos, face) {
-	override val inaccuracyRadians: Double get()  = Math.toRadians(IonServer.balancing.starshipWeapons.heavyTurret.inaccuracyRadians)
+	override val inaccuracyRadians: Double get() = Math.toRadians(IonServer.balancing.starshipWeapons.heavyTurret.inaccuracyRadians)
 	override val powerUsage: Int get() = IonServer.balancing.starshipWeapons.heavyTurret.powerUsage / 3
+
+	override fun autoFire(target: Player, dir: Vector) {
+		return
+	}
 }
