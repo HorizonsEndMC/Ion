c510f08b63cec6b2b9d4004253bc65030e4bbc5f
Gutin1 <Claytonpinter@gmail.com>
2023-06-10 21:15:20 -0500

    move auto-fire out of TurretWeaponSubsystem, delete cthulhu beam

--- server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/secondary/TriTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/starlegacy/feature/starship/subsystem/weapon/secondary/TriTurretWeaponSubsystem.kt
@@ -2,11 +2,15 @@ package net.starlegacy.feature.starship.subsystem.weapon.secondary
 
 import net.horizonsend.ion.server.IonServer
 import net.starlegacy.feature.multiblock.starshipweapon.turret.TriTurretMultiblock
+import net.starlegacy.feature.starship.active.ActivePlayerStarship
 import net.starlegacy.feature.starship.active.ActiveStarship
 import net.starlegacy.feature.starship.subsystem.weapon.TurretWeaponSubsystem
+import net.starlegacy.feature.starship.subsystem.weapon.interfaces.AutoWeaponSubsystem
 import net.starlegacy.feature.starship.subsystem.weapon.interfaces.HeavyWeaponSubsystem
 import net.starlegacy.util.Vec3i
 import org.bukkit.block.BlockFace
+import org.bukkit.entity.Player
+import org.bukkit.util.Vector
 import java.util.concurrent.TimeUnit
 
 class TriTurretWeaponSubsystem(
@@ -15,8 +19,15 @@ class TriTurretWeaponSubsystem(
 	face: BlockFace,
 	override val multiblock: TriTurretMultiblock
 ) : TurretWeaponSubsystem(ship, pos, face),
-	HeavyWeaponSubsystem {
+	HeavyWeaponSubsystem, AutoWeaponSubsystem {
 	override val inaccuracyRadians: Double = Math.toRadians(IonServer.balancing.starshipWeapons.triTurret.inaccuracyRadians)
 	override val powerUsage: Int = IonServer.balancing.starshipWeapons.triTurret.powerUsage
 	override val boostChargeNanos: Long = TimeUnit.SECONDS.toNanos(IonServer.balancing.starshipWeapons.triTurret.boostChargeNanos)
+
+	override val range: Double get() = multiblock.range
+
+	override fun autoFire(target: Player, dir: Vector) {
+		val shooter = (starship as? ActivePlayerStarship)?.pilot
+		multiblock.shoot(starship.serverLevel.world, pos, face, dir, starship, shooter)
+	}
 }
