3992e246db5847156784ddbb6b76869167f645bf
Gutin1 <Claytonpinter@gmail.com>
2023-06-14 13:07:19 -0500

    delete vote stuff

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/ProxyConfiguration.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/ProxyConfiguration.kt
@@ -10,9 +10,6 @@ data class ProxyConfiguration(
 	val discordServer: Long = 0,
 	val globalChannel: Long = 0,
 	val linkedRole: Long = 0,
-	val roleMap: Map<String, Long> = mapOf(),
-	val voteSites: List<VoteSite> = listOf()
-) {
-	@Serializable
-	data class VoteSite(val serviceName: String, val displayName: String, val displayAddress: String)
-}
+	val roleMap: Map<String, Long> = mapOf()
+)
+
