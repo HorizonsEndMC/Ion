27d0f96931b232916350b4cae19248468ad8cdd1
Gutin1 <Claytonpinter@gmail.com>
2023-06-17 22:41:16 -0500

    set tc color

--- server/src/main/kotlin/net/starlegacy/command/nations/admin/NPCOwnerCommand.kt
+++ server/src/main/kotlin/net/starlegacy/command/nations/admin/NPCOwnerCommand.kt
@@ -3,12 +3,15 @@ package net.starlegacy.command.nations.admin
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.common.extensions.success
 import net.starlegacy.command.SLCommand
 import net.horizonsend.ion.server.database.schema.nations.NPCTerritoryOwner
 import net.starlegacy.util.isAlphanumeric
 import net.starlegacy.util.msg
 import org.bukkit.Color
 import org.bukkit.entity.Player
+import org.litote.kmongo.eq
+import org.litote.kmongo.setValue
 
 @CommandPermission("nations.npcowner")
 @CommandAlias("npcowner")
@@ -47,11 +50,23 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("delete")
+	@Suppress("unused")
 	fun onDelete(sender: Player) = asyncCommand(sender) {
 		val territory = requireTerritoryIn(sender)
 		val npcOwner = territory.npcOwner ?: fail { "${territory.name} is not owned by an npc owner" }
 		val name = NPCTerritoryOwner.getName(npcOwner)
 		NPCTerritoryOwner.delete(npcOwner)
 		sender msg "&aDeleted $name"
+	}
+
+	@Subcommand("set color")
+	@Suppress("unused")
+	fun onSetColor(sender: Player, name: String, red: Int, green: Int, blue: Int) {
+		val city = NPCTerritoryOwner.findOne(NPCTerritoryOwner::name eq name) ?: fail { "City $name not found!" }
+
+		val color = validateColor(red, green, blue)
+
+		NPCTerritoryOwner.updateById(city._id, setValue(NPCTerritoryOwner::color, color))
+		sender.success("Set the color of $name!")
 	}
 }
