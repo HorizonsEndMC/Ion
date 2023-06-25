15052df92a3e1870b8cce3e1517a9c3956539c5f
Gutin1 <Claytonpinter@gmail.com>
2023-06-24 20:56:19 -0500

    some database stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/database/schema/nations/moonsieges/SiegeTerritory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/database/schema/nations/moonsieges/SiegeTerritory.kt
@@ -1,21 +1,27 @@
 package net.horizonsend.ion.server.database.schema.nations.moonsieges
 
-import com.mongodb.client.FindIterable
 import net.horizonsend.ion.server.database.DbObject
 import net.horizonsend.ion.server.database.Oid
 import net.horizonsend.ion.server.database.OidDbObjectCompanion
 import net.horizonsend.ion.server.database.schema.nations.Nation
-import net.horizonsend.ion.server.database.schema.space.Planet
 import org.bukkit.Bukkit
 import org.bukkit.World
+import org.litote.kmongo.and
 import org.litote.kmongo.ensureIndex
+import org.litote.kmongo.findOneById
+import org.litote.kmongo.setValue
+import org.litote.kmongo.updateOneById
+import java.util.Date
 
 data class SiegeTerritory(
 	override val _id: Oid<SiegeTerritory>,
 	val name: String,
 	val world: String,
 	val nation: Oid<Nation>?,
+	val previousOwner: Oid<Nation>?,
+	var lastChangedHands: Date,
 	var polygonData: ByteArray,
+	var siegeActive: Boolean,
 ) : DbObject {
 	companion object : OidDbObjectCompanion<SiegeTerritory>(
 		SiegeTerritory::class,
@@ -24,6 +30,16 @@ data class SiegeTerritory(
 			ensureIndex(SiegeTerritory::nation)
 		}
 	) {
+		fun changeOwner(territory: Oid<SiegeTerritory>, newOwner: Oid<Nation>) {
+			val data = col.findOneById(territory)
+
+			col.updateOneById(
+				territory, and(
+					setValue(SiegeTerritory::previousOwner, data?.nation),
+					setValue(SiegeTerritory::nation, newOwner)
+				)
+			)
+		}
 
 		fun create() = TODO("")
 	}
--- server/src/main/kotlin/net/horizonsend/ion/server/features/landsieges/MoonSieges.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/landsieges/MoonSieges.kt
@@ -1,41 +1,23 @@
 package net.horizonsend.ion.server.features.landsieges
 
 import fr.skytasul.guardianbeam.Laser.CrystalLaser
-import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.database.Oid
 import net.horizonsend.ion.server.database.schema.misc.SLPlayerId
 import net.horizonsend.ion.server.database.schema.nations.Nation
-import net.horizonsend.ion.server.database.schema.nations.NationRelation
 import net.horizonsend.ion.server.database.schema.nations.moonsieges.ForwardOperatingBase
 import net.horizonsend.ion.server.database.schema.nations.moonsieges.MoonSiege
 import net.horizonsend.ion.server.database.schema.nations.moonsieges.SiegeBeacon
 import net.horizonsend.ion.server.database.schema.nations.moonsieges.SiegeTerritory
+import net.horizonsend.ion.server.features.landsieges.BeaconSiege.updateBeaconSieges
 import net.horizonsend.ion.server.features.multiblock.moonsiege.SiegeBeaconMultiblock
-import net.kyori.adventure.audience.Audience
-import net.kyori.adventure.audience.ForwardingAudience
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextColor
 import net.starlegacy.SLComponent
-import net.starlegacy.cache.nations.PlayerCache
-import net.starlegacy.feature.multiblock.Multiblocks
-import net.starlegacy.feature.nations.region.Regions
-import net.starlegacy.feature.nations.region.types.RegionSiegeTerritory
-import net.starlegacy.feature.starship.event.StarshipExplodeEvent
 import net.starlegacy.util.Notify
 import net.starlegacy.util.Tasks
-import net.starlegacy.util.Vec3i
-import net.starlegacy.util.distance
-import net.starlegacy.util.toLocation
-import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.block.Sign
-import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.event.entity.PlayerDeathEvent
 import org.litote.kmongo.eq
 import org.litote.kmongo.setValue
 import java.time.DayOfWeek
@@ -49,9 +31,6 @@ object MoonSieges : SLComponent() {
 	val activeSieges = mutableMapOf<Oid<SiegeTerritory>, Oid<MoonSiege>>()
 
 	val activeBeaconBattles = mutableListOf<BeaconSiege>()
-
-	private const val BEACON_DETECTION_RADIUS = 100
-	private const val BEACON_SIEGE_MAX_TIME_MS: Long = 1000 * 60 * 60 * 2
 
 		/** Stores information for an individual battle for a beacon **/
 	data class BeaconSiege(
@@ -68,7 +47,7 @@ object MoonSieges : SLComponent() {
 		if (IonServer.configuration.serverName != "Survival") return
 
 		Tasks.syncRepeat(20, 20) {
-			updateSiegePeriod()
+			updateMoonSieges()
 			checkMoonSiegeWin()
 			updateBeaconSieges()
 		}
@@ -78,28 +57,55 @@ object MoonSieges : SLComponent() {
 		}
 	}
 
-	fun updateBeaconSieges() {
-		val finishedBeaconSieges = mutableListOf<BeaconSiege>()
+	fun updateMoonSieges() {
+		val dayOfTheWeek = ZonedDateTime.now().dayOfWeek
 
-		for (beaconSiege in activeBeaconBattles) {
-			if (System.currentTimeMillis() - beaconSiege.startTime < BEACON_SIEGE_MAX_TIME_MS) continue
-
-			finishedBeaconSieges.add(beaconSiege)
-
-			val beaconName = (beaconSiege.beacon.line(3) as TextComponent).content()
-
-			Notify online text(
-				"Siege Beacon Battle for $beaconName on ${beaconSiege.beacon.world} has ended.",
-				TextColor.fromHexString(" \t#EC5800")
-			)
+		if (!siegeDays.contains(dayOfTheWeek)) {
+			siegeActive = false
+			checkMoonSiegeWin()
+			return
 		}
 
-		activeBeaconBattles.removeAll(finishedBeaconSieges)
+		val isNew = !SiegeTerritory.all().any { it.siegeActive }
+
+		if (isNew) beginSiegePeriod()
+
+
+	}
+
+	fun setAllSiegeStates(active: Boolean) {
+		for (territory in SiegeTerritory.allIds()) {
+			setSiegeState(territory, active)
+		}
+	}
+
+	fun setSiegeState(territory: Oid<SiegeTerritory>, active: Boolean) = SiegeTerritory
+		.updateById(territory, setValue(SiegeTerritory::siegeActive, active))
+
+
+	fun beginSiegePeriod() {
+		setAllSiegeStates(true)
+
+		val notification = text().color(NamedTextColor.DARK_RED)
+			.append(text("The siege period has begun."))
+			.append(text("It will last for the next 48 hours", NamedTextColor.GRAY))
+			.build()
+
+		Notify.all(notification)
+	}
+
+	fun endSiegePeriod() {
+		setAllSiegeStates(false)
+
+		// List of changed hands
+		val notification = text().color(NamedTextColor.DARK_RED)
+			.append(text("The siege period has ended."))
+			.build()
+
+		Notify.all(notification)
 	}
 
 	fun updateBeacons() {
-
-
 		for (beacon in SiegeBeacon.all()) {
 			// Siege beacon period logic
 
@@ -135,40 +141,6 @@ object MoonSieges : SLComponent() {
 		}
 	}
 
-	fun updateSiegePeriod() {
-		val dayOfTheWeek = ZonedDateTime.now().dayOfWeek
-
-		if (!siegeDays.contains(dayOfTheWeek)) {
-			siegeActive = false
-			return
-		}
-
-		siegeActive = true
-
-		val notification = Component.text().color(NamedTextColor.DARK_RED)
-			.append(Component.text("The siege period has begun."))
-			.append(Component.text("It will last for the next 48 hours", NamedTextColor.GRAY))
-			.build()
-
-		Notify.all(notification)
-	}
-
-	fun tryBeginBeaconSiege(player: Player, beacon: Sign) {
-		val nation = PlayerCache[player].nationOid
-			?: return player.userError("You need to be in a nation to siege a station.")
-
-		val siegeTerritory = Regions.findFirstOf<RegionSiegeTerritory>(player.location)
-			?: return player.userError("You must be within a station's area to siege it.")
-
-		if (siegeTerritory.nation?.let { NationRelation.getRelationActual(nation, it).ordinal >= NationRelation.Level.ALLY.ordinal } == true) {
-			return player.userError("This station is owned by an ally of your nation.")
-		}
-
-		(Multiblocks[beacon] as? SiegeBeaconMultiblock)?.onSignInteract(beacon, player) ?: return
-
-		//TODO
-	}
-
 	fun endMoonSiege(siegeId: Oid<MoonSiege>) {
 		val siege = MoonSiege.findById(siegeId) ?: return
 		val territory = SiegeTerritory.findById(siege.siegeTerritory) ?: error("Siege territory ${siege.siegeTerritory} is not saved!")
@@ -177,46 +149,6 @@ object MoonSieges : SLComponent() {
 		for (forwardOperatingBase in ForwardOperatingBase.get(territory.bukkitWorld())) {
 			ForwardOperatingBase.updateById(forwardOperatingBase._id, setValue(ForwardOperatingBase::nation, null))
 		}
-	}
-
-	/** Used for detecting ship or player kills **/
-	fun getBeacon(location: Location): SiegeBeacon? {
-		val (x, y, z) = Vec3i(location)
-
-		return SiegeBeacon.all().asSequence()
-			.filter { it.world == location.world.name }
-			.filter { distance(it.x, it.y, it.z, x, y, z) <= BEACON_DETECTION_RADIUS }
-			.firstOrNull()
-	}
-
-	@EventHandler
-	fun onPlayerKill(event: PlayerDeathEvent) {
-		val beacon = getBeacon(event.player.location) ?: return
-		event.player.killer ?: return
-
-		// Determine the siege and the siege nations, do an ally check, determine points
-	}
-
-	@EventHandler
-	fun onShipKill(event: StarshipExplodeEvent) {
-		val beacon = getBeacon(event.starship.centerOfMass.toLocation(event.starship.serverLevel.world)) ?: return
-		val damagers = event.starship.damagers
-
-		val damagerPlayers = damagers.filter {
-			val slPlayer = PlayerCache[it.key.id]
-
-			// Only check relation if the beacon has an owner
-			val owner = beacon.owner?.let {
-				val relation = NationRelation.getRelationActual(beacon.owner, slPlayer.nationOid ?: return@filter false)
-
-				if (relation.ordinal >= NationRelation.Level.ALLY.ordinal) return@filter false
-			}
-
-			true
-		}.mapNotNull { Bukkit.getPlayer(it.key.id) }
-		// Determine the siege and the siege nations, do an ally check, determine points
-
-
 	}
 
 }
