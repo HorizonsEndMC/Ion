93d6b9ba1dfa2d6a9547aa5b87c7b2ecd4ed2a80
Gutin1 <Claytonpinter@gmail.com>
2023-07-01 01:57:27 -0500

    update multiblocks to interact multiblock

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/navigationcomputer/NavigationComputerMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/navigationcomputer/NavigationComputerMultiblock.kt
@@ -1,7 +1,8 @@
 package net.horizonsend.ion.server.features.multiblock.navigationcomputer
 
-import net.md_5.bungee.api.ChatColor
-import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor
+import net.starlegacy.feature.multiblock.Multiblock
 import org.bukkit.block.Sign
 import org.bukkit.entity.Player
 
@@ -9,7 +10,7 @@ abstract class NavigationComputerMultiblock : Multiblock() {
 	override val name = "navcomputer"
 
 	override fun onTransformSign(player: Player, sign: Sign) {
-		sign.setLine(3, ChatColor.WHITE.toString() + "[Standby]")
+		sign.line(3, text("[Standby]", NamedTextColor.WHITE))
 		sign.update()
 	}
 
