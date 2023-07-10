e0a70b8b2dba1ed61cb5b85b5a0106e2c38b285f
Gutin1 <Claytonpinter@gmail.com>
2023-07-09 23:30:13 -0500

    move

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/utils/VelocityDBUtils.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/utils/VelocityDBUtils.kt
@@ -1,1 +1,8 @@
 package net.horizonsend.ion.proxy.utils
+
+import com.velocitypowered.api.proxy.Player
+import net.horizonsend.ion.common.database.slPlayerId
+
+object VelocityDBUtils {
+	val Player.slPlayerId get() = uniqueId.slPlayerId
+}
