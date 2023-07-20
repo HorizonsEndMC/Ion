371afc552ff0cb45e6386f6c20b6cb224049b380
Gutin1 <Claytonpinter@gmail.com>
2023-07-19 23:30:51 -0500

    class

---
+++ server/src/main/kotlin/net/starlegacy/feature/multiblock/misc/ItemSplitterMultiblock.kt
@@ -0,0 +1,34 @@
+package net.starlegacy.feature.multiblock.misc
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.starlegacy.feature.multiblock.FurnaceMultiblock
+import net.starlegacy.feature.multiblock.LegacyMultiblockShape
+import net.starlegacy.feature.multiblock.Multiblock
+import org.bukkit.block.Furnace
+import org.bukkit.block.Sign
+import org.bukkit.event.inventory.FurnaceBurnEvent
+
+object ItemSplitterMultiblock : Multiblock(), FurnaceMultiblock {
+	override val name: String = "splitter"
+
+	val BLACKLIST = text("BLACKLIST", NamedTextColor.BLACK, TextDecoration.BOLD)
+	val WHITELIST = text("BLACKLIST", NamedTextColor.WHITE, TextDecoration.BOLD)
+
+	override val signText: Array<Component?> = arrayOf(
+		text("Item Splitter"),
+		BLACKLIST,
+		null,
+		null
+	)
+
+	override fun LegacyMultiblockShape.buildStructure() {
+
+	}
+
+	override fun onFurnaceTick(event: FurnaceBurnEvent, furnace: Furnace, sign: Sign) {
+
+	}
+}
