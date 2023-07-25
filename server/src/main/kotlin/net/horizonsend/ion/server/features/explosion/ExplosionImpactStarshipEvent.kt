966effdb20884a7e975115b1a5d9310e2d810467
Gutin1 <Claytonpinter@gmail.com>
2023-07-25 02:01:51 -0500

    implementation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/explosion/ExplosionImpactStarshipEvent.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/explosion/ExplosionImpactStarshipEvent.kt
@@ -1,18 +1,14 @@
-package net.horizonsend.ion.server.features.explosion
+package net.starlegacy.feature.starship.access.explosion
 
+import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.controllers.Controller
-import org.bukkit.event.Cancellable
-import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-/**
- * Event called when a starship causes any kind of explosion
- * @see StarshipExplosion
- **/
-open class StarshipCauseExplosionEvent(
-	val originator: Controller,
-	val explosion: StarshipExplosion
-) : Event(), Cancellable {
+class ExplosionImpactStarshipEvent(
+	originator: Controller,
+	explosion: StarshipExplosion,
+	val impactedStarship: Starship
+) : StarshipCauseExplosionEvent(originator, explosion) {
 	private var isCancelled: Boolean = false
 
 	override fun getHandlers(): HandlerList {
