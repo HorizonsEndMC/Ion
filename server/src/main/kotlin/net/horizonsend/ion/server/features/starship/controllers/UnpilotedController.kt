bf376206da62f0574e78eb888f7be40c91c294f3
Gutin1 <Claytonpinter@gmail.com>
2023-07-25 02:01:51 -0500

    implementation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/UnpilotedController.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/UnpilotedController.kt
@@ -3,5 +3,9 @@ package net.horizonsend.ion.server.features.starship.controllers
 import net.horizonsend.ion.server.features.starship.Starship
 import org.bukkit.entity.Player
 
+<<<<<<<< HEAD:server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/UnpilotedController.kt
 class UnpilotedController(player: Player, starship: Starship) : PlayerController(player, starship, "Legacy") {
 }
+========
+class LegacyController(player: Player, starship: Starship) : PlayerController(player, starship, "Legacy")
+>>>>>>>> b6fdd808 (implementation):server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/LegacyController.kt
