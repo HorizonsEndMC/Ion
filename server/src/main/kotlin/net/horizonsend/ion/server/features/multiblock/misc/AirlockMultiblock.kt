1f60b71b9df7b229392fd54c083e69e19227fcb6
Gutin1 <Claytonpinter@gmail.com>
2023-08-03 01:27:38 -0500

    fix docking tubes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/misc/AirlockMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/misc/AirlockMultiblock.kt
@@ -1,13 +1,22 @@
 package net.horizonsend.ion.server.features.multiblock.misc
 
-import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.InteractableMultiblock
 import net.horizonsend.ion.server.features.multiblock.Multiblock
-import org.bukkit.ChatColor
+import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.miscellaneous.utils.axis
+import net.horizonsend.ion.server.miscellaneous.utils.getFacing
+import net.horizonsend.ion.server.miscellaneous.utils.leftFace
+import net.horizonsend.ion.server.miscellaneous.utils.rightFace
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
 
-object AirlockMultiblock : Multiblock() {
+object AirlockMultiblock : Multiblock(), InteractableMultiblock {
 	override val name: String = "airlock"
 
 	override val signText = createSignText(
@@ -42,8 +51,38 @@ object AirlockMultiblock : Multiblock() {
 		}
 	}
 
-	override fun onTransformSign(player: Player, sign: Sign) = sign.setLine(1, OFF)
+	override fun onTransformSign(player: Player, sign: Sign) = sign.line(1, OFF)
 
-	val OFF = "${ChatColor.RED}-[OFF]-"
-	val ON = "${ChatColor.GREEN}-[ON]-"
+	private val OFF = text("-[OFF]-", NamedTextColor.RED)
+	private val ON = text("-[ON]-", NamedTextColor.GREEN)
+
+	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) {
+		if (event.hand != EquipmentSlot.HAND) return
+
+		val direction = sign.getFacing().oppositeFace
+		val right = direction.rightFace
+		val topPortal = sign.block.getRelative(direction).getRelative(right)
+		val bottomPortal = topPortal.getRelative(BlockFace.DOWN)
+
+		val enabled = topPortal.type == Material.IRON_BARS
+
+		val newData = if (enabled) {
+			Material.NETHER_PORTAL.createBlockData {
+				(it as org.bukkit.block.data.Orientable).axis = direction.rightFace.axis
+			}
+		} else {
+			Material.IRON_BARS.createBlockData {
+				(it as org.bukkit.block.data.MultipleFacing).setFace(direction.rightFace, true)
+				it.setFace(direction.leftFace, true)
+			}
+		}
+
+		topPortal.blockData = newData
+		bottomPortal.blockData = newData
+
+		val component = if (enabled) { ON } else { OFF }
+
+		sign.line(1, component)
+		sign.update()
+	}
 }
