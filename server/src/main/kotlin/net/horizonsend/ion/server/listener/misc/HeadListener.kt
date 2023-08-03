b03a991074bb48f848588dbf4d146b111f9f3c4d
Rattlyy <giovannimariamazzone@gmail.com>
2023-08-03 14:59:47 +0200

    head qol

--- server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/HeadListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/HeadListener.kt
@@ -1,9 +1,19 @@
 package net.horizonsend.ion.server.listener.misc
 
+import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
+import net.horizonsend.ion.common.database.schema.nations.NationRelation
+import net.horizonsend.ion.common.database.slPlayerId
+import net.horizonsend.ion.common.extensions.hintAction
+import net.horizonsend.ion.common.extensions.specialAction
 import net.horizonsend.ion.server.listener.SLEventListener
+import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
 import org.bukkit.Material
+import org.bukkit.block.Skull
 import org.bukkit.event.EventHandler
+import org.bukkit.event.block.Action
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import java.lang.System.currentTimeMillis
@@ -12,6 +22,24 @@ import java.util.UUID
 @Suppress("Unused")
 class HeadListener : SLEventListener() {
 	private val coolDowns = mutableMapOf<UUID, Long>()
+
+	@EventHandler
+	@Suppress("Unused")
+	fun onHeadClick(event: PlayerInteractEvent) {
+		if (event.action != Action.RIGHT_CLICK_BLOCK) return
+
+		val block = event.clickedBlock?.state as? Skull ?: return
+		val skullPlayer = SLPlayer[block.owningPlayer?.uniqueId?.slPlayerId ?: return] ?: return
+		val clickerPlayer = SLPlayer[event.player.slPlayerId] ?: return
+
+		val relationColor = if (skullPlayer.nation == null || clickerPlayer.nation == null) "" else
+			"<${NationRelation.getRelationActual(
+				clickerPlayer.nation!!,
+				skullPlayer.nation!!
+			).textStyle}>"
+
+		event.player.specialAction("This head belonged to $relationColor${skullPlayer.lastKnownName}.")
+	}
 
 	@EventHandler
 	@Suppress("Unused")
