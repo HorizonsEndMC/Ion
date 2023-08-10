fdeb43d7c9e4f3811439516725c57da7ad58f606
Gutin1 <Claytonpinter@gmail.com>
2023-08-10 01:20:42 -0500

    half integrate explosion reversal, needs to be heavily updated

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/explosion/reversal/Regeneration.kt
@@ -0,0 +1,57 @@
+package net.horizonsend.ion.server.features.explosion.reversal
+
+import net.horizonsend.ion.server.IonServer
+import org.bukkit.Bukkit
+import org.bukkit.World
+
+object Regeneration {
+	private val settings get() = IonServer.configuration.explosionRegenConfig
+
+	fun pulse() {
+		regenerateBlocks()
+	}
+
+	private fun regenerateBlocks(): Int {
+		val millisecondDelay: Long = settings.regenDelay.times( 60L * 1000L).toLong()
+		val maxNanos: Long = settings.placementIntensity.times( 1000000L).toLong()
+
+		val start = System.nanoTime()
+		var regenerated = 0
+
+		for (world in Bukkit.getWorlds()) {
+			val blocks = ExplosionReversal.worldData?.getBlocks(world) ?: return 0
+			val iterator = blocks.iterator()
+
+			while (iterator.hasNext()) {
+				val data = iterator.next()
+
+				if (System.nanoTime() - start > maxNanos) { // i.e. taking too long
+					return regenerated
+				}
+
+				if (System.currentTimeMillis() - data.explodedTime < millisecondDelay) {
+					continue
+				}
+
+				iterator.remove()
+				regenerateBlock(world, data)
+				regenerated++
+			}
+		}
+
+		return regenerated
+	}
+
+	private fun regenerateBlock(world: World, data: ExplodedBlockData) {
+		val block = world.getBlockAt(data.x, data.y, data.z)
+		val blockData = data.blockData
+
+		block.setBlockData(blockData, false)
+
+		val tileData = data.tileData
+
+		if (tileData != null) {
+			NMSUtils.setTileEntity(block, tileData)
+		}
+	}
+}
