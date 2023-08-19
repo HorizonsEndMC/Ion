4e76e9fffa832f13f948fe454e6a5fb4b6b35c54
Gutin1 <Claytonpinter@gmail.com>
2023-08-19 02:57:58 -0500

    buff trench loot chests

--- server/src/main/kotlin/net/horizonsend/ion/server/features/space/encounters/SecondaryChest.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/space/encounters/SecondaryChest.kt
@@ -3,8 +3,8 @@ package net.horizonsend.ion.server.features.space.encounters
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys.SECONDARY_CHEST_MONEY
-import net.minecraft.nbt.CompoundTag
 import net.horizonsend.ion.server.miscellaneous.utils.VAULT_ECO
+import net.minecraft.nbt.CompoundTag
 import org.bukkit.block.Chest
 import org.bukkit.entity.Player
 import org.bukkit.persistence.PersistentDataType
@@ -12,13 +12,13 @@ import org.bukkit.persistence.PersistentDataType.DOUBLE
 import java.util.Random
 
 enum class SecondaryChest(val NBT: CompoundTag, val money: (Double) -> Double) {
-	REPAIR_MATERIALS(Encounters.createLootChest("horizonsend:chests/starship_resource"), { input: Double -> input * 2750 + 250 }),
-	FOOD(Encounters.createLootChest("horizonsend:chests/food"), { input: Double -> input * 2750 + 250 }),
-	GUN_PARTS(Encounters.createLootChest("horizonsend:chests/gun_parts"), { input: Double -> input * 2750 + 250 }),
-	POWER_ARMOR_MODS(Encounters.createLootChest("horizonsend:chests/power_armor_mods"), { input: Double -> input * 2750 + 250 }),
-	ORES_LOW(Encounters.createLootChest("horizonsend:chests/low_tier_ores"), { input: Double -> input * 2750 + 250 }),
-	ORES_MEDIUM(Encounters.createLootChest("horizonsend:chests/mid_tier_ores"), { input: Double -> input * 2750 + 250 }),
-	ORES_HIGH(Encounters.createLootChest("horizonsend:chests/high_tier_ores"), { input: Double -> input * 2750 + 250 });
+	REPAIR_MATERIALS(Encounters.createLootChest("horizonsend:chests/starship_resource"), { input: Double -> input * 2750 + 2250 }),
+	FOOD(Encounters.createLootChest("horizonsend:chests/food"), { input: Double -> input * 2750 + 2250 }),
+	GUN_PARTS(Encounters.createLootChest("horizonsend:chests/gun_parts"), { input: Double -> input * 2750 + 2250 }),
+	POWER_ARMOR_MODS(Encounters.createLootChest("horizonsend:chests/power_armor_mods"), { input: Double -> input * 2750 + 2250 }),
+	ORES_LOW(Encounters.createLootChest("horizonsend:chests/low_tier_ores"), { input: Double -> input * 2750 + 2250 }),
+	ORES_MEDIUM(Encounters.createLootChest("horizonsend:chests/mid_tier_ores"), { input: Double -> input * 2750 + 2250 }),
+	ORES_HIGH(Encounters.createLootChest("horizonsend:chests/high_tier_ores"), { input: Double -> input * 2750 + 2250 });
 
 	companion object {
 		private val map = SecondaryChest.values().associateBy { it.name }
