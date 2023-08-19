911f855e5af13bf4fe3b8cf804d625365fbf9218
Gutin1 <Claytonpinter@gmail.com>
2023-08-19 18:44:22 -0500

    balancing config for gasses

--- server/src/main/kotlin/net/horizonsend/ion/server/configuration/GasConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/configuration/GasConfiguration.kt
@@ -6,20 +6,37 @@ import net.horizonsend.ion.server.features.gas.collectionfactors.CollectionFacto
 
 @Serializable
 data class GasConfiguration(
-	val hydrogen: Gas = Gas(listOf()),
-	val nitrogen: Gas = Gas(listOf()),
-	val methane: Gas = Gas(listOf()),
-	val oxygen: Gas = Gas(listOf()),
-	val chlorine: Gas = Gas(listOf()),
-	val fluorine: Gas = Gas(listOf()),
-	val carbonDioxide: Gas = Gas(listOf()),
-	val helium: Gas = Gas(listOf())
+	val hydrogen: Fuel = Fuel(2, 150, listOf()),
+	val nitrogen: Fuel = Fuel(1, 100, listOf()),
+	val methane: Fuel = Fuel(3, 200, listOf()),
+	val oxygen: Oxidizer = Oxidizer(1.0, listOf()),
+	val chlorine: Oxidizer = Oxidizer(1.5, listOf()),
+	val fluorine: Oxidizer = Oxidizer(2.0, listOf()),
+	val carbonDioxide: Gas = InertGas(listOf()),
+	val helium: Gas = InertGas(listOf())
 ) {
 	@Serializable
-	data class Gas(
+	data class InertGas(
+		override val factors: List<String>
+	) : Gas
+
+	@Serializable
+	data class Fuel(
+		val powerPerUnit: Int,
+		val cooldown: Int,
+		override val factors: List<String>,
+	) : Gas
+
+	@Serializable
+	data class Oxidizer(
+		val powerMultiplier: Double,
+		override val factors: List<String>,
+	) : Gas
+
+	interface Gas {
 		val factors: List<String>
-	) {
+
 		@Transient
-		val formattedFactors : List<CollectionFactor> = factors.map { CollectionFactor.valueOf(it) }
+		val formattedFactors : List<CollectionFactor> get() = factors.map { CollectionFactor.valueOf(it) }
 	}
 }
