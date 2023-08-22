1a4f9141858103234595982d164cb330324d25df
Gutin1 <Claytonpinter@gmail.com>
2023-08-22 01:00:25 -0500

    command, menu work, time gates, mostly finished

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/misc/BountyCommand.kt
@@ -0,0 +1,172 @@
+package net.horizonsend.ion.server.command.misc
+
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.Optional
+import co.aikar.commands.annotation.Subcommand
+import net.horizonsend.ion.common.database.schema.misc.ClaimedBounty
+import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.extensions.information
+import net.horizonsend.ion.common.extensions.success
+import net.horizonsend.ion.server.command.SLCommand
+import net.horizonsend.ion.server.features.bounties.Bounties
+import net.horizonsend.ion.server.features.bounties.BountiesMenu
+import net.horizonsend.ion.server.miscellaneous.utils.Notify
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
+import net.horizonsend.ion.server.miscellaneous.utils.VAULT_ECO
+import net.horizonsend.ion.server.miscellaneous.utils.toCreditsString
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
+import org.litote.kmongo.and
+import org.litote.kmongo.deleteOneById
+import org.litote.kmongo.eq
+import org.litote.kmongo.inc
+import org.litote.kmongo.setValue
+
+@CommandAlias("bounty")
+object BountyCommand : SLCommand() {
+	@Default
+	@Subcommand("menu")
+	@Description("Open the bounty menu")
+	@Suppress("unused")
+	fun menu(sender: Player) = BountiesMenu.openMenuAsync(sender)
+
+	@Subcommand("put")
+	@Description("Put a bounty on a player")
+	@CommandCompletion("@players")
+	@Suppress("unused")
+	fun put(sender: Player, targetName: String, amount: Double) = asyncCommand(sender) {
+		if (Bounties.isNotSurvival()) fail { "You can only do that on the Survival server!" }
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+		val bounty = target.bounty
+
+		Tasks.sync {
+			if (VAULT_ECO.getBalance(sender) < amount) fail { "Insufficient funds." }
+
+			VAULT_ECO.withdrawPlayer(sender, amount)
+
+			Notify.online(
+				text()
+					.append(text(sender.name, NamedTextColor.DARK_RED))
+					.append(text(" has placed a bounty of ", NamedTextColor.RED))
+					.append(text(amount.toCreditsString(), NamedTextColor.GOLD))
+					.append(text(" on ", NamedTextColor.RED))
+					.append(text(targetName, NamedTextColor.DARK_RED))
+					.append(text(". Their bounty is now ", NamedTextColor.RED))
+					.append(text((bounty + amount).toCreditsString(), NamedTextColor.GOLD))
+					.build()
+			)
+		}
+
+		SLPlayer.updateById(target._id, inc(SLPlayer::bounty, amount))
+	}
+
+	@Subcommand("claim")
+	@Description("Acquire a bounty")
+	@CommandCompletion("@players")
+	@Suppress("unused")
+	fun claim(sender: Player, targetName: String, @Optional amount: Double? = null) = asyncCommand(sender) {
+		if (Bounties.isNotSurvival()) fail { "You can only do that on the Survival server!" }
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+
+		if (target.bounty == 0.0) fail { "$targetName doesn't have a bounty!" }
+
+		if (amount != null) {
+			val message = text()
+				.append(text(targetName, NamedTextColor.DARK_RED))
+				.append(text(" has a bounty of ", NamedTextColor.RED))
+				.append(text(target.bounty.toCreditsString(), NamedTextColor.GOLD))
+				.append(text(". You must complete this bounty within 24 hours, you cannot claim a bounty on $targetName again until ${Bounties.nextClaim}\n", NamedTextColor.RED))
+				.append(text("If you wish to proceed, enter /bounty claim $targetName ${target.bounty}", NamedTextColor.RED))
+				.build()
+
+			sender.sendMessage(message)
+			return@asyncCommand
+		}
+
+		Bounties.claimBounty(sender, target._id, targetName, target.bounty)
+	}
+
+	@Subcommand("get")
+	@Description("Get a player's bounty")
+	@CommandCompletion("@players")
+	@Suppress("unused")
+	fun get(sender: Player, targetName: String) = asyncCommand(sender) {
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+
+		val bounty = target.bounty
+
+		if (target.bounty >= 0) {
+			sender.sendMessage(
+				text()
+					.append(text(targetName, NamedTextColor.DARK_RED))
+					.append(text("'s bounty is ", NamedTextColor.RED))
+					.append(text(bounty.toCreditsString(), NamedTextColor.GOLD))
+					.build()
+			)
+		} else {
+			sender.sendMessage(
+				text()
+					.append(text(targetName, NamedTextColor.DARK_RED))
+					.append(text(" does not have a bounty!", NamedTextColor.RED))
+					.build()
+			)
+		}
+	}
+
+	@Subcommand("put")
+	@CommandPermission("ion.bounty.modify")
+	@CommandCompletion("@players")
+	@Suppress("unused")
+	fun set(sender: Player, targetName: String, newBounty: Double) = asyncCommand(sender) {
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+
+		SLPlayer.updateById(target._id, setValue(SLPlayer::bounty, newBounty))
+		sender.success("$targetName's bounty was set to $newBounty")
+	}
+
+	@Subcommand("add")
+	@CommandPermission("ion.bounty.modify")
+	@CommandCompletion("@players")
+	@Suppress("unused")
+	fun add(sender: Player, name: String, newBounty: Double) = asyncCommand(sender) {
+		val player = SLPlayer[name] ?: fail { "Player $name not found!" }
+
+		SLPlayer.updateById(player._id, inc(SLPlayer::bounty, newBounty))
+		sender.success("$name's bounty was set to $newBounty")
+	}
+
+	@Subcommand("find")
+	@CommandPermission("ion.bounty.modify")
+	@Description("Find a past or current claimed bounty")
+	@CommandCompletion("@players @players")
+	@Suppress("unused")
+	fun find(sender: Player, hunterName: String, targetName: String) = asyncCommand(sender) {
+		val hunter = SLPlayer[hunterName] ?: fail { "Player $hunterName not found!" }
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+
+		val bounties = ClaimedBounty.find(and(ClaimedBounty::hunter eq hunter._id, ClaimedBounty::target eq target._id))
+
+		sender.information(bounties.joinToString { "$hunterName claimed bounty on $targetName on ${it.claimTime}. Completed: ${it.completed}" })
+	}
+
+	@Subcommand("clear")
+	@CommandPermission("ion.bounty.modify")
+	@Description("Clear a player's current claimed bounty")
+	@CommandCompletion("@players @players")
+	@Suppress("unused")
+	fun clear(sender: Player, hunterName: String, targetName: String) = asyncCommand(sender) {
+		val hunter = SLPlayer[hunterName] ?: fail { "Player $hunterName not found!" }
+		val target = SLPlayer[targetName] ?: fail { "Player $targetName not found!" }
+
+		val claimedBounty = ClaimedBounty.findOne(and(ClaimedBounty::hunter eq hunter._id, ClaimedBounty::target eq target._id)) ?: fail {
+			"Bounty claimed by $hunterName on $targetName not found!"
+		}
+
+		ClaimedBounty.col.deleteOneById(claimedBounty._id)
+	}
+}
--- server/src/main/kotlin/net/horizonsend/ion/server/features/bounties/Bounties.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/bounties/Bounties.kt
@@ -1,28 +1,78 @@
 package net.horizonsend.ion.server.features.bounties
 
+import net.horizonsend.ion.common.database.schema.misc.ClaimedBounty
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
+import net.horizonsend.ion.common.database.slPlayerId
+import net.horizonsend.ion.common.extensions.success
+import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.features.cache.PlayerCache
+import net.horizonsend.ion.server.features.starship.controllers.PlayerController
+import net.horizonsend.ion.server.features.starship.event.StarshipExplodeEvent
 import net.horizonsend.ion.server.miscellaneous.utils.Notify
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.VAULT_ECO
 import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
 import net.horizonsend.ion.server.miscellaneous.utils.toCreditsString
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
+import org.litote.kmongo.and
+import org.litote.kmongo.eq
+import org.litote.kmongo.gte
 import org.litote.kmongo.inc
 import org.litote.kmongo.setValue
+import java.util.Date
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
-class Bounties : IonServerComponent() {
-	override fun onEnable() {
+object Bounties : IonServerComponent() {
+	val coolDown get() = Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(2))
+	val nextClaim get() = Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2))
+	val lastActive get() = Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1))
 
+	data class BountyPlayer(
+		val name: String,
+		val uniqueId: UUID,
+		val skull: ItemStack,
+		val bounty: Double
+	)
+
+	fun claimBounty(hunter: Player, target: BountyPlayer) {
+		claimBounty(hunter, target.uniqueId.slPlayerId, target.name, target.bounty)
 	}
 
-	override fun onDisable() {
+	fun claimBounty(hunter: Player, targetId: SLPlayerId, targetName: String, targetBounty: Double) = Tasks.async {
+		if (hasActive(hunter.slPlayerId, targetId)) return@async hunter.userError("You already have an active bounty on $targetName!")
+		if (canClaim(hunter.slPlayerId, targetId)) return@async hunter.userError("You already claimed a bounty on $targetName in the last two days!")
 
+		ClaimedBounty.claim(hunter.slPlayerId, targetId)
+
+		hunter.success("You have claimed a bounty on $targetName for $targetBounty. \nYou have 1 day to collect this bounty before it expires.")
+	}
+
+	fun canClaim(hunter: SLPlayerId, target: SLPlayerId): Boolean {
+		val query = and(ClaimedBounty::hunter eq hunter, ClaimedBounty::target eq target, ClaimedBounty::claimTime gte coolDown)
+
+		return !SLPlayer.none(query)
+	}
+
+	fun hasActive(hunter: SLPlayerId, target: SLPlayerId): Boolean {
+		val query = and(
+			ClaimedBounty::target eq target,
+			ClaimedBounty::hunter eq hunter,
+			ClaimedBounty::claimTime gte lastActive,
+			ClaimedBounty::completed eq false
+		)
+
+		return !ClaimedBounty.none(query)
 	}
 
 	@EventHandler
@@ -32,32 +82,91 @@ class Bounties : IonServerComponent() {
 		val victim = event.player
 		val killer = event.entity.killer ?: return
 
-		collectBounty(victim, killer)
+		if (hasActive(killer.slPlayerId, victim.slPlayerId)) {
+			collectBounty(victim, killer)
+		} else {
+			val amount = 5000.0
+
+			val killerBounty = PlayerCache[killer].bounty
+
+			val increaseReason = text()
+				.append(text("For killing ", NamedTextColor.RED))
+				.append(text(victim.name, NamedTextColor.DARK_RED))
+				.append(text(", ", NamedTextColor.RED))
+				.append(text(killer.name, NamedTextColor.DARK_RED))
+				.append(text("'s bounty was increased by $amount! It is now ", NamedTextColor.RED))
+				.append(text((killerBounty + amount).toCreditsString(), NamedTextColor.GOLD))
+				.build()
+
+			increaseBounty(killer, amount, increaseReason)
+		}
 	}
 
-	fun collectBounty(killer: Player, killed: Player) {
-		val cached = PlayerCache[killed]
+
+	@EventHandler
+	fun onShipSink(event: StarshipExplodeEvent) {
+		if (isNotSurvival()) return
+		val victim = (event.starship.controller as? PlayerController)?.player ?: return
+
+		val isCargoShip = !event.starship.type.isWarship
+		val blockCountMultipler = 1.5
+
+		val damagers = event.starship.damagers.filterKeys { damager ->
+			// require they be online to get xp
+			// if they have this perm, e.g. someone in dutymode or on creative, they don't get xp
+			Bukkit.getPlayer(damager.id)?.hasPermission("starships.noxp") == false
+		}.mapValues { it.value.get() }
+
+		val sum = damagers.values.sum().toDouble()
+		val totalMoney = event.starship.initialBlockCount.toDouble() * blockCountMultipler * if (isCargoShip) 2.0 else 1.0
+
+		for ((damager, points) in damagers) {
+			val killer = Bukkit.getPlayer(damager.id) ?: continue
+			val percent = points / sum
+			val money = totalMoney / percent
+
+			val killerBounty = PlayerCache[killer].bounty
+
+			val reason = text()
+				.append(text("For killing ", NamedTextColor.RED))
+				.append(text(victim.name, NamedTextColor.DARK_RED))
+				.append(text(", ", NamedTextColor.RED))
+				.append(text(killer.name, NamedTextColor.DARK_RED))
+				.append(text("'s bounty was increased by $money! It is now ", NamedTextColor.RED))
+				.append(text((killerBounty + money).toCreditsString(), NamedTextColor.GOLD))
+				.build()
+
+			increaseBounty(killer, money, reason)
+		}
+	}
+
+	fun collectBounty(hunter: Player, target: Player) {
+		val cached = PlayerCache[target]
 		val bounty = cached.bounty
 
-		SLPlayer.updateById(killed.slPlayerId, setValue(SLPlayer::bounty, 0.0))
+		val query = and(ClaimedBounty::hunter eq hunter.slPlayerId, ClaimedBounty::target eq target.slPlayerId, ClaimedBounty::completed eq false)
+		val claimedBounty = ClaimedBounty.findOne(query) ?: return hunter.userError("Bounty not found! Please contact a mod.")
 
-		VAULT_ECO.depositPlayer(killer, bounty)
+		SLPlayer.updateById(target.slPlayerId, setValue(SLPlayer::bounty, 0.0))
+		ClaimedBounty.updateById(claimedBounty._id, setValue(ClaimedBounty::completed, true))
+
+		VAULT_ECO.depositPlayer(hunter, bounty)
 	}
 
-	fun increaseBounty(bountied: Player, amount: Int) {
+	fun increaseBounty(target: Player, amount: Double, reason: Component? = null) {
 		if (isNotSurvival()) return
 
-		val cached = PlayerCache[bountied]
-		SLPlayer.updateById(bountied.slPlayerId, inc(SLPlayer::bounty, amount))
+		val cached = PlayerCache[target]
+		SLPlayer.updateById(target.slPlayerId, inc(SLPlayer::bounty, amount))
 
-		val text = text()
-			.append(text(bountied.name, NamedTextColor.DARK_RED))
+		val defaultReason = text()
+			.append(text(target.name, NamedTextColor.DARK_RED))
 			.append(text("'s bounty was increased by $amount! It is now ", NamedTextColor.RED))
 			.append(text((cached.bounty + amount).toCreditsString(), NamedTextColor.GOLD))
 			.build()
 
-		Notify.online(text)
+		Notify.online(reason ?: defaultReason)
 	}
 
-	fun isNotSurvival(): Boolean = IonServer.configuration.serverName.equals("survival", ignoreCase = true)
+	fun isNotSurvival(): Boolean = !IonServer.configuration.serverName.equals("Survival", ignoreCase = true)
 }
