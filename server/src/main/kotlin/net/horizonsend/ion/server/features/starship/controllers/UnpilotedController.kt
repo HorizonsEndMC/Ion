c56ec529c890630f306349d4b3e7f19808c9b39f
Gutin1 <Claytonpinter@gmail.com>
2023-08-24 22:25:51 -0500

    allow modification via event, better async

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/UnpilotedController.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/controllers/UnpilotedController.kt
@@ -3,5 +3,5 @@ package net.horizonsend.ion.server.features.starship.controllers
 import net.horizonsend.ion.server.features.starship.Starship
 import org.bukkit.entity.Player
 
-class UnpilotedController(player: Player, starship: Starship) : PlayerController(player, starship, "Legacy") {
+class UnpilotedController(player: Player, starship: Starship) : PlayerController(player, starship, "Unpiloted") {
 }
