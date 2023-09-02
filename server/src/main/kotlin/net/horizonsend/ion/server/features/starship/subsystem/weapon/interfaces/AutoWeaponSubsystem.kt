c12ab4ad97b472d80a1313a9f7ae6297d21e0ecf
Gutin1 <Claytonpinter@gmail.com>
2023-09-02 15:58:03 -0500

    more robust random targeting

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/interfaces/AutoWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/interfaces/AutoWeaponSubsystem.kt
@@ -1,12 +1,13 @@
 package net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces
 
+import net.horizonsend.ion.server.features.starship.AutoTurretTargeting.AutoTurretTarget
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
 
 interface AutoWeaponSubsystem {
 	val range: Double
 
-	fun autoFire(target: Player, dir: Vector)
+	fun autoFire(target: AutoTurretTarget<*>, dir: Vector)
 
 	fun shouldTargetRandomBlock(target: Player): Boolean = true
 }
