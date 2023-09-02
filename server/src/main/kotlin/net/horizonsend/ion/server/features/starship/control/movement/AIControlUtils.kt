0ed37f01f19f62136cdb7ccbe8ec58ff9a60b692
Gutin1 <Claytonpinter@gmail.com>
2023-09-02 00:58:18 -0500

    face a direction properly, manual fire utils

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/AIControlUtils.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/AIControlUtils.kt
@@ -1,12 +1,35 @@
 package net.horizonsend.ion.server.features.starship.control.movement
 
+import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
+import net.horizonsend.ion.server.features.starship.control.weaponry.StarshipWeaponry
 import net.horizonsend.ion.server.features.starship.controllers.ai.AIController
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
+import net.horizonsend.ion.server.miscellaneous.utils.leftFace
+import net.horizonsend.ion.server.miscellaneous.utils.rightFace
+import net.horizonsend.ion.server.miscellaneous.utils.vectorToPitchYaw
+import net.horizonsend.ion.server.miscellaneous.utils.yawToBlockFace
 import org.bukkit.Location
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
+import kotlin.math.roundToInt
 
 object AIControlUtils {
+	/** Will stop moving if provided a null vector **/
+	fun moveInDirection(controller: AIController, direction: Vector?) {
+		if (direction == null) {
+			controller.isShiftFlying = false
+			return
+		}
+
+		val (pitch, yaw) = vectorToPitchYaw(direction)
+
+		controller.pitch = pitch
+		controller.yaw = yaw
+
+		controller.isShiftFlying = true
+	}
+
 	/** Will stop moving if provided a null location **/
 	fun moveToLocation(controller: AIController, location: Location?) = Tasks.async {
 		val starshipLocation = controller.starship.centerOfMass.toLocation(controller.starship.world)
@@ -16,26 +39,103 @@ object AIControlUtils {
 			return@async
 		}
 
-		controller.isShiftFlying = true
+		val dir = location.toVector().subtract(starshipLocation.toVector())
 
-		val dir = starshipLocation.toVector().subtract(location.toVector()).normalize()
-
-		starshipLocation.direction = dir
-
-		controller.pitch = location.pitch
-		controller.yaw = location.yaw
+		moveInDirection(controller, dir)
 	}
 
 	/** Will stop moving if provided a null player **/
 	fun shiftFlyTowardsPlayer(controller: AIController, player: Player?) = moveToLocation(controller, player?.location)
 
-	fun shootAtPlayer() {
-
-	}
-
+	/** Will attempt to face in the specified direction **/
 	fun faceDirection(controller: AIController, direction: BlockFace) {
+		if (controller.starship !is ActiveControlledStarship) return
+
 		if (direction == BlockFace.UP || direction == BlockFace.DOWN) return
 
 		val isFacing = controller.starship.forward
+		if (controller.starship.pendingRotations.isNotEmpty()) return
+
+		// 1.5 sec turn delay
+		if (System.currentTimeMillis() - controller.lastRotation < 1500) return
+
+		when (direction) {
+			// Facing the same direction
+			isFacing -> return
+
+			// New direction is to the right
+			isFacing.rightFace -> controller.starship.tryRotate(true)
+
+			// New direction is to the left
+			isFacing.leftFace -> controller.starship.tryRotate(false)
+
+			// New direction is backwards, just rotate either way
+			isFacing.oppositeFace -> controller.starship.tryRotate(true)
+
+			else -> return
+		}
+
+		controller.lastRotation = System.currentTimeMillis()
+	}
+
+	fun shootAtPlayer(
+		controller: AIController,
+		player: Player,
+		leftClick: Boolean,
+		controllerLoc: Vector? = null,
+		weaponSet: String? = null,
+	) {
+		shootAtTarget(
+			controller,
+			player.location.toVector(),
+			leftClick,
+			controllerLoc,
+			weaponSet
+		)
+	}
+
+	fun shootAtTarget(
+		controller: AIController,
+		target: Vector,
+		leftClick: Boolean,
+		controllerLoc: Vector? = null,
+		weaponSet: String? = null,
+	) {
+		val originLocation = controllerLoc ?: controller.starship.centerOfMass.toVector()
+
+		val direction = target.clone().subtract(originLocation)
+
+		shootInDirection(
+			controller,
+			direction,
+			leftClick,
+			controllerLoc,
+			weaponSet
+		)
+	}
+
+	fun shootInDirection(
+		controller: AIController,
+		direction: Vector,
+		leftClick: Boolean,
+		target: Vector? = null,
+		weaponSet: String? = null,
+		controllerLoc: Location? = null
+	) {
+		val originLocation = controllerLoc ?: controller.starship.centerOfMass.toLocation(controller.starship.world)
+
+		StarshipWeaponry.manualFire(
+			controller,
+			controller.starship,
+			leftClick,
+			yawToBlockFace(controller.yaw.roundToInt()),
+			direction,
+			target ?: StarshipWeaponry.getTarget(originLocation, direction, controller.starship),
+			weaponSet
+		)
+	}
+
+	fun setAutoWeapons(controller: AIController, target: Player) {
+		
 	}
 }
