3c2596f86de7543f0f52a37d0b34d29d8ce97a75
Gutin1 <Claytonpinter@gmail.com>
2023-09-06 20:22:05 -0500

    remove criteria

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/ai/AIControllers.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/ai/AIControllers.kt
@@ -3,7 +3,9 @@ package net.horizonsend.ion.server.features.starship.control.controllers.ai
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.control.controllers.Controller
+import net.horizonsend.ion.server.features.starship.control.movement.AIControlUtils
 import net.horizonsend.ion.server.miscellaneous.utils.keysSortedByValue
+import net.horizonsend.ion.server.miscellaneous.utils.vectorToBlockFace
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
@@ -13,18 +15,33 @@ object AIControllers {
 	fun dumbAI(starship: ActiveStarship): AIController = createController(
 		starship,
 		"dumbAI",
-		displayName = text("eeeevil solesey ship", NamedTextColor.DARK_RED),
-		Criterias.followAndShoot
+		displayName = text("eeeevil solesey ship", NamedTextColor.DARK_RED)
 	)
 
 	private fun createController(
 		starship: ActiveStarship,
 		name: String,
-		displayName: Component = text("name"),
-		vararg critera: Criterias.Criteria
+		displayName: Component = text("name")
 	): AIController {
-		return object : AIController(starship, name, critera) {
+		return object : AIController(starship, name) {
 			override val pilotName: Component = displayName
+			override fun tick() {
+				val location = starship.centerOfMass.toLocation(starship.world)
+				val nearestPlayer = getNearestPlayer(this, location)
+
+				val direction =
+					nearestPlayer?.location?.toVector()?.subtract(starship.centerOfMass.toVector())
+
+				direction?.let { AIControlUtils.faceDirection(this, vectorToBlockFace(direction)) }
+
+				AIControlUtils.shiftFlyTowardsPlayer(this, nearestPlayer)
+
+				nearestPlayer?.let {
+					AIControlUtils.shootAtPlayer(this, nearestPlayer, true)
+					AIControlUtils.shootAtPlayer(this, nearestPlayer, false, weaponSet = "phasers")
+				}
+				super.tick()
+			}
 		}
 	}
 
