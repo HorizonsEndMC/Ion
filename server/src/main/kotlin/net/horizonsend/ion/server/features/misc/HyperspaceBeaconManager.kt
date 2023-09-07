8cf884b05012c25050d8822b7735b141ae1b9efb
kwazedilla <kwazedilla@gmail.com>
2023-09-07 16:56:50 -0700

    detect nearby beacons upon starship exiting hyperspace

--- server/src/main/kotlin/net/horizonsend/ion/server/features/misc/HyperspaceBeaconManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/misc/HyperspaceBeaconManager.kt
@@ -2,6 +2,8 @@ package net.horizonsend.ion.server.features.misc
 
 import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.features.starship.active.ActivePlayerStarship
+import net.horizonsend.ion.server.features.starship.event.StarshipExitHyperspaceEvent
 import net.horizonsend.ion.server.features.starship.event.StarshipTranslateEvent
 import net.horizonsend.ion.server.features.starship.event.StarshipUnpilotedEvent
 import net.horizonsend.ion.server.listener.SLEventListener
@@ -24,47 +26,60 @@ object HyperspaceBeaconManager : SLEventListener() {
 		}
 	}
 
+	@Suppress("unused")
 	@EventHandler
 	fun onStarshipUnpilot(event: StarshipUnpilotedEvent) {
 		activeRequests.remove(event.player.uniqueId)
 	}
 
+	@Suppress("unused")
 	@EventHandler
 	fun onStarshipMove(event: StarshipTranslateEvent) {
 		clearExpired()
-		val pilot = event.starship.pilot ?: return
-		if (event.starship.hyperdrives.isEmpty()) return
+		detectNearbyBeacons(event.starship, event.x, event.z)
+	}
 
-		val starship = event.starship
-		val worldBeacons = beaconWorlds[event.starship.serverLevel.world] ?: return
+	@Suppress("unused")
+	@EventHandler
+	fun onStarshipExitHyperspace(event: StarshipExitHyperspaceEvent) {
+		if (event.starship is ActivePlayerStarship) {
+			detectNearbyBeacons(event.starship, 0, 0)
+		}
+	}
+
+	private fun detectNearbyBeacons(starship: ActivePlayerStarship, x: Int, z: Int) {
+		val pilot = starship.pilot ?: return
+		if (starship.hyperdrives.isEmpty()) return
+
+		val worldBeacons = beaconWorlds[starship.serverLevel.world] ?: return
 
 		if (
 			worldBeacons.any { beacon ->
 				val distance = distance(
 					beacon.spaceLocation.x,
 					beacon.spaceLocation.z,
-					(event.x + starship.centerOfMass.x),
-					(event.z + starship.centerOfMass.z)
+					(x + starship.centerOfMass.x),
+					(z + starship.centerOfMass.z)
 				)
 
 				if (distance <= beacon.radius) {
-					event.starship.beacon = beacon
+					starship.beacon = beacon
 					true
 				} else {
-					event.starship.beacon = null
+					starship.beacon = null
 					false
 				}
 			}
 		) {
 			if (activeRequests.containsKey(pilot.uniqueId)) return
-			val beacon = event.starship.beacon
+			val beacon = starship.beacon
 
 			if (beacon?.prompt != null) pilot.sendRichMessage(beacon.prompt)
 			pilot.sendRichMessage(
 				"<aqua>Detected signal from hyperspace beacon<yellow> ${beacon!!.name}<aqua>" + // not null if true
-					", destination<yellow> " +
-					"${beacon.destinationName ?: "${beacon.destination.world}: ${beacon.destination.x}, ${beacon.destination.z}"}<aqua>. " +
-					"<gold><italic><hover:show_text:'<gray>/usebeacon'><click:run_command:/usebeacon>Engage hyperdrive?</click>"
+						", destination<yellow> " +
+						"${beacon.destinationName ?: "${beacon.destination.world}: ${beacon.destination.x}, ${beacon.destination.z}"}<aqua>. " +
+						"<gold><italic><hover:show_text:'<gray>/usebeacon'><click:run_command:/usebeacon>Engage hyperdrive?</click>"
 			)
 			activeRequests[pilot.uniqueId] = System.currentTimeMillis()
 		} else {
