fbf687fa1bbd3607e7047767ae966db64e7ef5a8
Gutin1 <Claytonpinter@gmail.com>
2023-09-11 15:18:30 -0500

    starshipData abstraction

--- common/src/main/kotlin/net/horizonsend/ion/common/database/schema/starships/PlayerStarshipData.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/database/schema/starships/PlayerStarshipData.kt
@@ -1,12 +1,9 @@
 package net.horizonsend.ion.common.database.schema.starships
 
-import net.horizonsend.ion.common.database.DbObject
 import net.horizonsend.ion.common.database.Oid
-import net.horizonsend.ion.common.database.OidDbObjectCompanion
 import net.horizonsend.ion.common.database.StarshipTypeDB
 import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
 import org.litote.kmongo.contains
-import org.litote.kmongo.deleteOneById
 import org.litote.kmongo.ensureIndex
 import org.litote.kmongo.ensureUniqueIndex
 import org.litote.kmongo.eq
@@ -20,42 +17,39 @@ import org.litote.kmongo.or
  * not currently in the database.
  */
 data class PlayerStarshipData(
-    override val _id: Oid<PlayerStarshipData>,
-    /** Player UUID of the captain of the ship */
+	override val _id: Oid<PlayerStarshipData>,
+	/** Player UUID of the captain of the ship */
 	var captain: SLPlayerId,
 
-    var starshipType: StarshipTypeDB,
+	override var starshipType: StarshipTypeDB,
 
-    var serverName: String?,
-    var levelName: String,
-    var blockKey: Long,
+	override var serverName: String?,
+	override var levelName: String,
+	override var blockKey: Long,
 
-    /** UUIDs of players who have been added to the ship by the captain. Should never include the captain. */
+	/** UUIDs of players who have been added to the ship by the captain. Should never include the captain. */
 	val pilots: MutableSet<SLPlayerId> = mutableSetOf(),
-    var name: String? = null,
-    /** Chunk combined coordinates, of each chunk the detected blocks reside in */
-	var containedChunks: Set<Long>? = null,
+	var name: String? = null,
+	/** Chunk combined coordinates, of each chunk the detected blocks reside in */
+	override var containedChunks: Set<Long>? = null,
 
-    var lastUsed: Long = System.currentTimeMillis(),
-    var isLockEnabled: Boolean = false
-) : DbObject {
-	companion object : OidDbObjectCompanion<PlayerStarshipData>(PlayerStarshipData::class, setup = {
-		ensureIndex(PlayerStarshipData::captain)
-		ensureIndex(PlayerStarshipData::pilots)
-		ensureIndex(PlayerStarshipData::name)
-		ensureIndex(PlayerStarshipData::serverName)
-		ensureIndex(PlayerStarshipData::levelName)
-		ensureUniqueIndex(PlayerStarshipData::levelName, PlayerStarshipData::blockKey)
-	}) {
+	override var lastUsed: Long = System.currentTimeMillis(),
+	override var isLockEnabled: Boolean = false
+) : StarshipData {
+	companion object : StarshipDataCompanion<PlayerStarshipData>(
+		PlayerStarshipData::class,
+		PlayerStarshipData::serverName,
+		PlayerStarshipData::levelName,
+		PlayerStarshipData::blockKey,
+		setup = {
+			ensureIndex(PlayerStarshipData::captain)
+			ensureIndex(PlayerStarshipData::pilots)
+			ensureIndex(PlayerStarshipData::serverName)
+			ensureIndex(PlayerStarshipData::serverName)
+			ensureUniqueIndex(PlayerStarshipData::levelName, PlayerStarshipData::blockKey)
+		}
+	) {
 		const val LOCK_TIME_MS = 1_000 * 60 * 5
-
-		fun add(data: PlayerStarshipData) {
-			col.insertOne(data)
-		}
-
-		fun remove(dataId: Oid<PlayerStarshipData>) {
-			col.deleteOneById(dataId)
-		}
 
 		fun findByPilot(playerId: SLPlayerId) =
 			find(or(PlayerStarshipData::captain eq playerId, PlayerStarshipData::pilots contains playerId))
