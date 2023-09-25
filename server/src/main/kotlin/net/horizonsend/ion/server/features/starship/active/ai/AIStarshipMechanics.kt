30feb71aaabeaa6691a4dd71fbf519b323407906
Gutin1 <Claytonpinter@gmail.com>
2023-09-25 03:02:53 -0500

    fixup the cruise controller, fixes and improvements, despawning mechanics, name improvements

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/AIStarshipMechanics.kt
@@ -0,0 +1,31 @@
+package net.horizonsend.ion.server.features.starship.active.ai
+
+import net.horizonsend.ion.common.extensions.alert
+import net.horizonsend.ion.server.IonServerComponent
+import net.horizonsend.ion.server.features.starship.PilotedStarships
+import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
+import net.horizonsend.ion.server.features.starship.active.ActiveStarships
+import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
+import net.horizonsend.ion.server.miscellaneous.utils.blockKey
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockBreakEvent
+
+object AIStarshipMechanics : IonServerComponent() {
+	@EventHandler
+	fun onBlockBreak(event: BlockBreakEvent) {
+		if (event.block.type != Material.JUKEBOX) return
+
+		val aiShips = ActiveStarships.getInWorld(event.block.world).filter { it.controller is  AIController }
+
+		val x = event.block.x
+		val y = event.block.y
+		val z = event.block.z
+
+		val ship = aiShips.firstOrNull { it.blocks.contains(blockKey(x, y, z)) } ?: return
+
+		event.player.alert("Starship computer destroyed. AI Ship powering down...")
+
+		PilotedStarships.tryRelease(ship as ActiveControlledStarship)
+	}
+}
