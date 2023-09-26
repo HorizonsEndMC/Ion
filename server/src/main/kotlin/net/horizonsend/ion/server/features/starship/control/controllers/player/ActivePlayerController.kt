7cb683cfb935e0127d898140c949938f1bb15795
Gutin1 <Claytonpinter@gmail.com>
2023-09-25 19:58:52 -0500

    fix movement without controller

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/player/ActivePlayerController.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/player/ActivePlayerController.kt
@@ -1,10 +1,11 @@
 package net.horizonsend.ion.server.features.starship.control.controllers.player
 
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.control.movement.PlayerStarshipControl
 import org.bukkit.entity.Player
 
 class ActivePlayerController(player: Player, starship: ActiveStarship) : PlayerController(player, starship, "Player") {
-	override val isShiftFlying: Boolean get() = player.isSneaking
+	override val isShiftFlying: Boolean get() = player.isSneaking && PlayerStarshipControl.isHoldingController(player)
 	override val selectedDirectControlSpeed: Int get() = player.inventory.heldItemSlot
 
 	init {
