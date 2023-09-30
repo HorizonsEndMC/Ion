5839fdb1459a5d1e58afbe512ff91cab2f31566c
Gutin1 <Claytonpinter@gmail.com>
2023-09-30 15:49:30 -0500

    avoid world border

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/AIPathfinding.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/AIPathfinding.kt
@@ -57,6 +57,10 @@ object AIPathfinding {
 		val chunkX = chunk.pos.x
 		val chunkZ = chunk.pos.z
 
+		if (chunk.level.worldBorder.isChunkInBounds(chunkX, chunkZ)) {
+			return yRange.map { SectionNode(world, Vec3i(chunkX, it, chunkZ), false, "Out of world border") }.toSet()
+		}
+
 		for (sectionPos in yRange) {
 			val section = levelChunkSections.getOrNull(sectionPos)
 			val pos = Vec3i(chunkX, sectionPos, chunkZ)
