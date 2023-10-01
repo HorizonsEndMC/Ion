8403656b3567ea301991136ad68b21ad4651a603
Gutin1 <Claytonpinter@gmail.com>
2023-10-01 14:37:10 -0500

    fix the silent failure of nation relation cache

--- common/src/main/kotlin/net/horizonsend/ion/common/database/cache/nations/RelationCache.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/database/cache/nations/RelationCache.kt
@@ -5,12 +5,12 @@ import com.googlecode.cqengine.index.hash.HashIndex
 import com.googlecode.cqengine.query.QueryFactory.and
 import com.googlecode.cqengine.query.QueryFactory.equal
 import com.mongodb.client.model.changestream.ChangeStreamDocument
-import net.horizonsend.ion.common.database.schema.nations.Nation
+import net.horizonsend.ion.common.database.Oid
 import net.horizonsend.ion.common.database.cache.Cache
 import net.horizonsend.ion.common.database.cache.DbObjectCache
-import net.horizonsend.ion.common.database.Oid
 import net.horizonsend.ion.common.database.enumValue
 import net.horizonsend.ion.common.database.get
+import net.horizonsend.ion.common.database.schema.nations.Nation
 import net.horizonsend.ion.common.database.schema.nations.NationRelation
 
 object RelationCache : DbObjectCache<NationRelation, Oid<NationRelation>>(NationRelation.Companion), Cache {
@@ -19,17 +19,14 @@ object RelationCache : DbObjectCache<NationRelation, Oid<NationRelation>>(Nation
 	private val otherAttr = attribute(NationRelation::other)
 
 	override fun addExtraIndexes() {
-		try {
-			cache.addIndex(HashIndex.onAttribute(nationAttr))
-			cache.addIndex(HashIndex.onAttribute(otherAttr))
-			cache.addIndex(
-				CompoundIndex.onAttributes(
-					nationAttr,
-					otherAttr
-				)
+		cache.addIndex(HashIndex.onAttribute(nationAttr))
+//		cache.addIndex(HashIndex.onAttribute(otherAttr))
+		cache.addIndex(
+			CompoundIndex.onAttributes(
+				nationAttr,
+				otherAttr
 			)
-		} catch (_: Exception) {
-		} // Silently fail
+		)
 	}
 
 	override fun update(cached: NationRelation, change: ChangeStreamDocument<NationRelation>) {
