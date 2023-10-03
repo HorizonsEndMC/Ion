5d657a50bb4e87542fb92f7c768fc599ab1e5bf7
Gutin1 <Claytonpinter@gmail.com>
2023-10-02 19:55:13 -0500

    command exception handling

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/IonDiscordCommand.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/IonDiscordCommand.kt
@@ -1,3 +1,13 @@
 package net.horizonsend.ion.proxy.commands
 
-interface IonDiscordCommand
+import co.aikar.commands.InvalidCommandArgument
+import net.horizonsend.ion.common.database.Oid
+import net.horizonsend.ion.common.database.cache.nations.NationCache
+import net.horizonsend.ion.common.database.schema.nations.Nation
+
+interface IonDiscordCommand {
+	fun fail(message: () -> String): Nothing = throw InvalidCommandArgument(message())
+
+	fun resolveNation(name: String): Oid<Nation> = NationCache.getByName(name)
+		?: fail { "Nation $name not found" }
+}
--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/discord/PlayerListCommand.kt
@@ -5,13 +5,16 @@ import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Description
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.horizonsend.ion.proxy.PLUGIN
 import net.horizonsend.ion.proxy.commands.IonDiscordCommand
 import net.horizonsend.ion.proxy.messageEmbed
 import net.md_5.bungee.api.ProxyServer
 
 @CommandAlias("playerlist")
 @Description("List online players.")
-class PlayerListCommand(private val proxy: ProxyServer) : IonDiscordCommand {
+object PlayerListCommand : IonDiscordCommand {
+	val proxy: ProxyServer = PLUGIN.getProxy()
+
 	@Default
 	@Suppress("Unused")
 	fun onPlayerListCommand(event: SlashCommandInteractionEvent) {
