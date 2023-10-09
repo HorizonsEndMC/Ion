81450a84ccfc3dc6825694c2be8cca7c09c393f5
Gutin1 <Claytonpinter@gmail.com>
2023-10-09 18:54:05 -0500

    stop combat npc drowning

--- server/src/main/kotlin/net/horizonsend/ion/server/features/misc/CombatNPCs.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/misc/CombatNPCs.kt
@@ -2,23 +2,25 @@ package net.horizonsend.ion.server.features.misc
 
 import net.citizensnpcs.api.CitizensAPI
 import net.citizensnpcs.api.event.NPCDamageByEntityEvent
+import net.citizensnpcs.api.event.NPCDamageEvent
 import net.citizensnpcs.api.event.NPCDeathEvent
 import net.citizensnpcs.api.npc.MemoryNPCDataStore
 import net.citizensnpcs.api.npc.NPC
 import net.citizensnpcs.api.npc.NPCRegistry
-import net.horizonsend.ion.common.extensions.userError
-import net.horizonsend.ion.server.IonServer
-import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
 import net.horizonsend.ion.common.database.slPlayerId
-import net.horizonsend.ion.server.miscellaneous.utils.firsts
-import net.horizonsend.ion.server.miscellaneous.utils.get
+import net.horizonsend.ion.common.extensions.alert
+import net.horizonsend.ion.common.extensions.userError
+import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys
-import net.horizonsend.ion.server.miscellaneous.utils.listen
 import net.horizonsend.ion.server.miscellaneous.utils.Notify
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
+import net.horizonsend.ion.server.miscellaneous.utils.firsts
+import net.horizonsend.ion.server.miscellaneous.utils.get
+import net.horizonsend.ion.server.miscellaneous.utils.listen
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Entity
@@ -28,13 +30,15 @@ import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
 import org.bukkit.event.HandlerList
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import org.litote.kmongo.setValue
-import java.util.*
+import java.util.EnumSet
+import java.util.UUID
 import java.util.concurrent.CompletableFuture
 
 object CombatNPCs : IonServerComponent(true) {
@@ -120,6 +124,25 @@ object CombatNPCs : IonServerComponent(true) {
 			event.isCancelled = true
 		}
 
+		val acceptableCauses: EnumSet<EntityDamageEvent.DamageCause> = EnumSet.of(
+			EntityDamageEvent.DamageCause.ENTITY_ATTACK,
+			EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK,
+			EntityDamageEvent.DamageCause.ENTITY_EXPLOSION,
+			EntityDamageEvent.DamageCause.BLOCK_EXPLOSION,
+			EntityDamageEvent.DamageCause.FIRE,
+			EntityDamageEvent.DamageCause.FIRE_TICK,
+			EntityDamageEvent.DamageCause.PROJECTILE,
+			EntityDamageEvent.DamageCause.SUICIDE,
+			EntityDamageEvent.DamageCause.CUSTOM,
+			EntityDamageEvent.DamageCause.LAVA,
+		)
+
+		listen<NPCDamageEvent>(EventPriority.LOWEST) { event ->
+			if (!acceptableCauses.contains(event.cause)) return@listen
+
+			event.isCancelled = true
+		}
+
 		listen<NPCDeathEvent>(EventPriority.LOWEST) { event ->
 			val npc = event.npc
 			val playerId = npcToPlayer.entries.find { it.value.first.id == npc.id }?.key ?: return@listen
@@ -181,7 +204,7 @@ object CombatNPCs : IonServerComponent(true) {
 			if (SLPlayer[event.player].wasKilled) {
 				event.player.inventory.clear()
 				event.player.health = 0.0
-				event.player.userError("Your NPC was killed while you were offline.")
+				event.player.alert("Your NPC was killed while you were offline.")
 			}
 		}
 
