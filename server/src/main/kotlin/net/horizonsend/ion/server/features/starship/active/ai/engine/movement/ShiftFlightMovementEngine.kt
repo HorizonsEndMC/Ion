fcaabd39f9c126924d0d4e356632f3ff0a2d9fba
Gutin1 <Claytonpinter@gmail.com>
2023-10-10 00:33:46 -0500

    flesh out and integrate the engines

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/movement/ShiftFlightMovementEngine.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/movement/ShiftFlightMovementEngine.kt
@@ -1,14 +1,14 @@
 package net.horizonsend.ion.server.features.starship.active.ai.engine.movement
 
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 
 class ShiftFlightMovementEngine(
 	controller: AIController,
 	override var destination: Vec3i?
 ) : MovementEngine(controller) {
-	override var starshipLocation: Vec3i = controller.starship.centerOfMass
-		get() = controller.getCenterVec3i()
-
-
+	override fun tick() {
+		Tasks.sync { shiftFly(starshipLocation.toLocation(world), false) }
+	}
 }
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/positioning/CirclingPositionEngine.kt
@@ -0,0 +1,54 @@
+package net.horizonsend.ion.server.features.starship.active.ai.engine.positioning
+
+import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
+import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
+import net.horizonsend.ion.server.features.starship.control.movement.StarshipCruising
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.Location
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * Tries to position the ship in a circling pattern around the target
+ **/
+class CirclingPositionEngine(controller: AIController, var target: Vec3i, var holdOffDistance: Double) : PositioningEngine(controller) {
+	private val ticksPerCruise = StarshipCruising.SECONDS_PER_CRUISE * 20.0
+	var destination: Vec3i = target
+
+	override fun findPosition(): Location = destination.toLocation(world)
+
+	override fun findPositionVec3i(): Vec3i = destination
+
+	var ticks = 0
+	/** Do the calculation on tick so its not done every time its called */
+	override fun tick() {
+		ticks++
+
+		// Calculate the amount of ticks it would take to do a lap around the target
+		val moveTicks = 1 / getMovePercent()
+
+		// Convert to radians
+		val radians = 2 * PI * (ticks / moveTicks)
+
+		val x = ((cos(radians) * holdOffDistance) + target.x).toInt()
+		val y = target.y
+		val z = ((sin(radians) * holdOffDistance) + target.z).toInt()
+
+		destination = Vec3i(x, y, z)
+	}
+
+	private fun circleCircumference() = 2 * PI * holdOffDistance
+
+	/** Returns the percentage the target location should move around the target per tick */
+	private fun getMovePercent(): Double {
+		starship as ActiveControlledStarship
+
+		val circumference = circleCircumference()
+
+		val cruiseSpeed = starship.cruiseData.targetSpeed.toDouble()
+		val blocksPerTick = cruiseSpeed / ticksPerCruise
+
+		return blocksPerTick/ circumference
+	}
+}
