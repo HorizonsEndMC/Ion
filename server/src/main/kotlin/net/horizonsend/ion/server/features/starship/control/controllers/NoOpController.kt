2401247551547b193a993182f8a6c5508f010c67
Gutin1 <Claytonpinter@gmail.com>
2023-10-12 14:37:53 -0500

    damager reformat

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/NoOpController.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/controllers/NoOpController.kt
@@ -1,13 +1,14 @@
 package net.horizonsend.ion.server.features.starship.control.controllers
 
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.damager.noOpDamager
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import org.bukkit.block.Block
 import org.bukkit.block.BlockState
 
-class NoOpController(starship: ActiveStarship) : Controller(starship, "idle") {
+class NoOpController(starship: ActiveStarship) : Controller(noOpDamager, starship, "idle") {
 	// Can't move
 	override val isShiftFlying: Boolean = false
 	override val selectedDirectControlSpeed: Int = 0
@@ -19,7 +20,4 @@ class NoOpController(starship: ActiveStarship) : Controller(starship, "idle") {
 	override fun audience(): Audience = Audience.empty()
 	override fun canDestroyBlock(block: Block): Boolean = false
 	override fun canPlaceBlock(block: Block, newState: BlockState, placedAgainst: Block): Boolean = false
-	override fun getDisplayName(): Component = pilotName
-	override fun rewardXP(xp: Int) { }
-	override fun rewardMoney(credits: Double) { }
 }
