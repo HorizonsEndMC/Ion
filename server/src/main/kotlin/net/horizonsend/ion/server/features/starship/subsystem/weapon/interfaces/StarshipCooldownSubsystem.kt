fbaa6adac06a78632912bb008e36f500b21361a4
Gutin1 <Claytonpinter@gmail.com>
2023-10-20 13:47:20 -0500

    gift for fell

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/interfaces/StarshipCooldownSubsystem.kt
@@ -0,0 +1,7 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces
+
+/**
+ * Marks this subsystem as having a ship-wide cooldown.
+ * After a battery of shots is fired, the cooldown is applied to all of this subsystem.
+ **/
+interface StarshipCooldownSubsystem
