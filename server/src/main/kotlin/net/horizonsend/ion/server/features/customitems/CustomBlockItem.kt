35ef90639e8b300d7306c284eca9daf964ae2714
kwazedilla <kwazedilla@gmail.com>
2023-10-21 12:49:52 -0700

    custom blocks now drop respective custom items

--- server/src/main/kotlin/net/horizonsend/ion/server/features/customitems/CustomBlockItem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/customitems/CustomBlockItem.kt
@@ -1,5 +1,7 @@
 package net.horizonsend.ion.server.features.customitems
 
+import net.horizonsend.ion.server.features.customblocks.CustomBlock
+import net.horizonsend.ion.server.features.customblocks.CustomBlocks
 import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys.CUSTOM_ITEM
 import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
 import net.kyori.adventure.text.Component
@@ -7,13 +9,19 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType.STRING
 
-abstract class CustomBlockItem(
+open class CustomBlockItem(
     identifier: String,
 
     private val material: Material,
     private val customModelData: Int,
-    val displayName: Component
+    val displayName: Component,
+
+    val customBlockIdentifier: String
 ) : CustomItem(identifier) {
+
+    fun getCustomBlock(): CustomBlock? {
+        return CustomBlocks.getByIdentifier(customBlockIdentifier)
+    }
 
     override fun constructItemStack(): ItemStack {
         return ItemStack(material).updateMeta {
