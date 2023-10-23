80dffd02adb06b8e1e8a00f2a5e7d84ae3b1663b
CrystalzzzDeez <billcypher269@gmail.com>
2023-10-23 16:53:48 +0100

    praise be gutin for all my problems are fixed

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
@@ -29,7 +29,7 @@ class QuadTurretWeaponSubsystem(
 		target: Vector,
 	) {
 		if (starship.initialBlockCount < 16000) {
-			shooter.userError("You can't fire quad turrets on a ship smaller than 16000 blocks!")
+			starship.userError("You can't fire quad turrets on a ship smaller than 16000 blocks!")
 			return
 		}
 		super.manualFire(shooter, dir, target)
