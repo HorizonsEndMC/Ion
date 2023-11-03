1842c7c426e75fac83b0a2da48e84cbb9431ba8b
Gutin1 <Claytonpinter@gmail.com>
2023-11-03 02:07:37 -0500

    squash exceptions, pathfind if block fixes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/movement/NoOpMovementEngine.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/movement/NoOpMovementEngine.kt
@@ -1,8 +1,8 @@
 package net.horizonsend.ion.server.features.starship.active.ai.engine.movement
 
 import net.horizonsend.ion.server.features.starship.active.ai.engine.pathfinding.PathfindingEngine
-import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
+import net.horizonsend.ion.server.features.starship.control.controllers.ai.interfaces.ActiveAIController
 
-class NoOpMovementEngine(controller: AIController, pathfindingEngine: PathfindingEngine) : MovementEngine(controller, pathfindingEngine) {
+class NoOpMovementEngine(controller: ActiveAIController, pathfindingEngine: PathfindingEngine) : MovementEngine(controller, pathfindingEngine) {
 	override fun tick() {}
 }
