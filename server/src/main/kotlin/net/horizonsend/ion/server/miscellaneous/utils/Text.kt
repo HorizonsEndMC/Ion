cfb31fa0ba954da9acd9d5ba918ee8f2fea86fc7
Gutin1 <Claytonpinter@gmail.com>
2023-11-03 12:19:58 -0500

    fix weird stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Text.kt
+++
@@ -1,9 +0,0 @@
-package net.horizonsend.ion.server.miscellaneous.utils
-
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
-
-fun Component.plainText(): String = PlainTextComponentSerializer.plainText().serialize(this)
-
-fun String.miniMessage() = MiniMessage.miniMessage().deserialize(this)
