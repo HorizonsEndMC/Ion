ca23985211bbe52c01c38d4b8503cdbcdfa9a4bb
CrystalzzzDeez <billcypher269@gmail.com>
2023-11-03 02:07:22 +0000

    rebase

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/interfaces/StarshipCooldownSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/interfaces/StarshipCooldownSubsystem.kt
@@ -3,5 +3,6 @@ package net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces
 /**
  * Marks this subsystem as having a ship-wide cooldown.
  * After a battery of shots is fired, the cooldown is applied to all of this subsystem.
+ * this is needed now for light weapons or you will have no cooldown, only your power draw
  **/
 interface StarshipCooldownSubsystem
