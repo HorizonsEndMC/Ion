1c6ff23229f1a25fc0e41fb5ff086a16d99f7f53
Gutin1 <Claytonpinter@gmail.com>
2023-11-06 01:54:20 -0600

    new help command, paginated chat utils

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
@@ -22,6 +22,7 @@ import net.horizonsend.ion.server.command.misc.BountyCommand
 import net.horizonsend.ion.server.command.misc.DyeCommand
 import net.horizonsend.ion.server.command.misc.GToggleCommand
 import net.horizonsend.ion.server.command.misc.GlobalGameRuleCommand
+import net.horizonsend.ion.server.command.misc.HelpCommand
 import net.horizonsend.ion.server.command.misc.IonBroadcastCommand
 import net.horizonsend.ion.server.command.misc.ListCommand
 import net.horizonsend.ion.server.command.misc.LocatorCommands
@@ -56,7 +57,6 @@ import net.horizonsend.ion.server.command.qol.FixExtractorsCommand
 import net.horizonsend.ion.server.command.qol.SearchCommand
 import net.horizonsend.ion.server.command.qol.SetPowerCommand
 import net.horizonsend.ion.server.command.space.PlanetCommand
-import net.horizonsend.ion.server.command.space.SpaceGenCommand
 import net.horizonsend.ion.server.command.space.SpaceWorldCommand
 import net.horizonsend.ion.server.command.space.StarCommand
 import net.horizonsend.ion.server.command.starship.BlueprintCommand
@@ -175,4 +175,5 @@ val commands: List<SLCommand> = listOf(
 	FleetCommand,
 	LocatorCommands,
 	ItemDebugCommand,
+	HelpCommand,
 )
