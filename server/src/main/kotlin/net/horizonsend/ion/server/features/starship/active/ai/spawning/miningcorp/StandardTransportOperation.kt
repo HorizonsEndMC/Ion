ebe2c5ade398e275b7a56918fe7d8beb5852c4de
Gutin1 <Claytonpinter@gmail.com>
2023-11-24 02:27:33 -0600

    spawner foundational work

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/spawning/miningcorp/StandardTransportOperation.kt
@@ -0,0 +1,5 @@
+package net.horizonsend.ion.server.features.starship.active.ai.spawning.miningcorp
+
+//class StandardTransportOperation(config: AIShipConfiguration.AISpawnerConfiguration) : AISpawner(config, "MINING_CORP_STANDARD_TRANSPORT") {
+//
+//}
