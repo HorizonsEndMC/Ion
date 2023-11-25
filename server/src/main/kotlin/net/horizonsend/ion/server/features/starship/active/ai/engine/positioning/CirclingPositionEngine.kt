f7fb0141594690dfed4512bac0db22132d3d64eb
Gutin1 <Claytonpinter@gmail.com>
2023-11-25 16:04:57 -0600

    steering pathfinding, positioning changes and upgrades

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/positioning/CirclingPositionEngine.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/engine/positioning/CirclingPositionEngine.kt
@@ -1,10 +1,12 @@
 package net.horizonsend.ion.server.features.starship.active.ai.engine.positioning
 
 import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
+import net.horizonsend.ion.server.features.starship.active.ai.util.AITarget
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
 import net.horizonsend.ion.server.features.starship.control.movement.StarshipCruising
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.Location
+import java.util.function.Supplier
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
@@ -12,9 +14,13 @@ import kotlin.math.sin
 /**
  * Tries to position the ship in a circling pattern around the target
  **/
-class CirclingPositionEngine(controller: AIController, var target: Vec3i?, var holdOffDistance: Double) : PositioningEngine(controller) {
+class CirclingPositionEngine(
+	controller: AIController,
+	var targetSupplier: Supplier<AITarget?>,
+	var standoffDistance: Double
+) : PositioningEngine(controller) {
 	private val ticksPerCruise = StarshipCruising.SECONDS_PER_CRUISE * 20.0
-	private var internalDestination: Vec3i? = target
+	private var internalDestination: Vec3i? = targetSupplier.get()?.getVec3i()
 
 	override fun findPosition(): Location = internalDestination?.toLocation(world) ?: getCenter()
 
@@ -26,12 +32,14 @@ class CirclingPositionEngine(controller: AIController, var target: Vec3i?, var h
 	override fun tick() {
 		ticks++
 
-		val target = target
+		val target = targetSupplier.get()
 
 		if (target == null) {
 			internalDestination = null
 			return
 		}
+
+		val (targetX, targetY, targetZ) = target.getVec3i()
 
 		// Calculate the amount of ticks it would take to do a lap around the target
 		val moveTicks = 1 / getMovePercent()
@@ -39,14 +47,14 @@ class CirclingPositionEngine(controller: AIController, var target: Vec3i?, var h
 		// Convert to radians
 		val radians = 2 * PI * (ticks / moveTicks)
 
-		val x = ((cos(radians) * holdOffDistance) + target.x).toInt()
-		val y = target.y
-		val z = ((sin(radians) * holdOffDistance) + target.z).toInt()
+		val x = ((cos(radians) * standoffDistance) + targetX).toInt()
+		val y = targetY
+		val z = ((sin(radians) * standoffDistance) + targetZ).toInt()
 
 		internalDestination = Vec3i(x, y, z)
 	}
 
-	private fun circleCircumference() = 2 * PI * holdOffDistance
+	private fun circleCircumference() = 2 * PI * standoffDistance
 
 	/** Returns the percentage the target location should move around the target per tick */
 	private fun getMovePercent(): Double {
