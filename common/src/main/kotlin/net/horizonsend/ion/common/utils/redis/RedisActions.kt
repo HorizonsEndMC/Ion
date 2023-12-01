7bcaa29083c6aee259d450033ae9817300184896
Gutin1 <Claytonpinter@gmail.com>
2023-12-01 13:40:55 -0600

    redis backend organization

--- common/src/main/kotlin/net/horizonsend/ion/common/utils/redis/RedisActions.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/utils/redis/RedisActions.kt
@@ -1,37 +1,63 @@
 package net.horizonsend.ion.common.utils.redis
 
-import com.google.gson.*
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import net.horizonsend.ion.common.CommonConfig
 import net.horizonsend.ion.common.IonComponent
 import net.horizonsend.ion.common.database.DBManager.jedisPool
 import net.horizonsend.ion.common.database.Oid
-import org.bson.types.ObjectId
+import net.horizonsend.ion.common.utils.redis.gson.OidJsonSerializer
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.JedisPubSub
 import java.lang.reflect.Type
-import java.util.*
+import java.util.UUID
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
 import java.util.concurrent.ThreadFactory
 
 object RedisActions : IonComponent() {
-	private val gson = GsonBuilder().registerTypeAdapter(
-		Oid::class.java,
-		object : JsonSerializer<Oid<*>>, JsonDeserializer<Oid<*>>, InstanceCreator<Oid<*>> {
-			override fun serialize(src: Oid<*>, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
-				return JsonPrimitive(src.toString())
-			}
+	private val gson = GsonBuilder()
+		.registerTypeAdapter(Oid::class.java, OidJsonSerializer)
+		.create()
 
-			@Throws(JsonParseException::class)
-			override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Oid<*> {
-				return Oid<Any>(json.asString)
-			}
+	private val idActionMap = mutableMapOf<String, RedisPubSubAction<*>>()
 
-			override fun createInstance(type: Type): Oid<*> {
-				return Oid<Any>(ObjectId())
-			}
-		}
-	).create()
+	private lateinit var channel: String
+	private lateinit var pubSub: JedisPubSub
+	private lateinit var subscriber: Jedis
+
+	var enabled = false
+
+	override fun onEnable() {
+		connect()
+
+		Thread({ subscriber.subscribe(pubSub, channel) }, "Ion Plugin Messaging").start()
+
+		enabled = true
+	}
+
+	private fun connect() {
+		channel = CommonConfig.redis.channel
+
+		pubSub = IonPubSubListener
+		subscriber = jedisPool.resource
+
+		log.info("Connected to Redis with channel $channel, pubSub: $pubSub, subscriber: $subscriber")
+	}
+
+	override fun onDisable() {
+		enabled = false
+
+		pubSub.unsubscribe() // stop listening for messages
+		subscriber.close() // close the subscriber instance
+		idActionMap.clear() // clear the plugin message map
+	}
+
+	// Actions
+	fun <T : RedisPubSubAction<*>> register(message: T) {
+		check(!idActionMap.containsKey(message.id)) { "Duplicate message ${message.id}" }
+		idActionMap[message.id] = message
+	}
 
 	inline fun <reified T, B> register(id: String, runSync: Boolean, noinline function: (T) -> B): RedisPubSubAction<T> {
 		val action = object : RedisPubSubAction<T>(id, object : TypeToken<T>() {}.type, runSync) {
@@ -43,40 +69,7 @@ object RedisActions : IonComponent() {
 		return action
 	}
 
-	private val map = mutableMapOf<String, RedisPubSubAction<*>>()
-
-	private lateinit var channel: String
-
-	private lateinit var pubSub: JedisPubSub
-	private lateinit var subscriber: Jedis
-
-	var enabled = false
-	override fun onEnable() {
-		channel = CommonConfig.redis.channel
-
-		pubSub = PubSubListener
-		subscriber = jedisPool.resource
-
-		Thread({
-			subscriber.subscribe(pubSub, channel)
-		}, "Ion Plugin Messaging").start()
-
-		enabled = true
-	}
-
-	override fun onDisable() {
-		enabled = false
-		pubSub.unsubscribe() // stop listening for messages
-		subscriber.close() // close the subscriber instance
-		map.clear() // clear the plugin message map
-	}
-
-	fun <T : RedisPubSubAction<*>> register(message: T) {
-		check(!map.containsKey(message.id)) { "Duplicate message ${message.id}" }
-		map[message.id] = message
-	}
-
-	private val id = UUID.randomUUID()
+	private val serverId = UUID.randomUUID()
 
 	private val executor = Executors.newSingleThreadExecutor(object : ThreadFactory {
 		private var counter: Int = 0
@@ -86,37 +79,36 @@ object RedisActions : IonComponent() {
 		}
 	})
 
-	fun <Data> publish(messageId: String, data: Data, type: Type) {
+	fun <Data> publish(messageId: String, data: Data, type: Type) : Future<Long?> {
 		val content = gson.toJson(data, type)
-		val message = "$id:$messageId:$content"
-		executor.execute {
+		val message = "$serverId:$messageId:$content"
+
+		@Suppress("UNCHECKED_CAST")
+		val id: Future<Long?> = executor.submit {
 			jedisPool.resource.use { it.publish(channel, message) }
-		}
+		} as Future<Long?>
+
+		return id
 	}
 
-	private object PubSubListener : JedisPubSub() {
+	private object IonPubSubListener : JedisPubSub() {
+		/** Upon receiving a message */
 		override fun onMessage(channel: String, message: String) {
 			// to prevent weird things from happening, delay all update handling till initialization is complete
 			// however, we still need to listen immediately so we don't miss any updates
 			if (!enabled) return
 
-			val split = message.split(":")
+			val messageInfo = Message.breakMessage(message)
 
-			if (split[0] == id.toString()) {
-				return // ignore if it came from us
-			}
-
-			val messageId = split[1]
-			val pluginMessage = map[messageId]
+			if (messageInfo.serverId == serverId.toString()) return // ignore if it came from us
+			val pluginMessage = idActionMap[messageInfo.actionId]
 
 			if (pluginMessage == null) {
-				log.warn("Unknown message $messageId. Full contents: $message")
+				log.warn("Unknown message ${messageInfo.actionId}. Full contents: $message")
 				return
 			}
 
-			val content: String = message
-				.removePrefix(split[0]).removePrefix(":")
-				.removePrefix(split[1]).removePrefix(":")
+			val content: String = messageInfo.jsonMessage
 
 			val data = try {
 				gson.fromJson<Any>(content, pluginMessage.type)
@@ -133,4 +125,24 @@ object RedisActions : IonComponent() {
 		}
 	}
 
+	data class Message(
+		val serverId: String,
+		val actionId: String,
+		val jsonMessage: String
+	) {
+		companion object {
+			fun breakMessage(message: String): Message {
+				val split = message.split(":")
+				val serverId = split[0]
+
+				val actionId = split[1]
+
+				val content: String = message
+					.removePrefix(split[0]).removePrefix(":")
+					.removePrefix(split[1]).removePrefix(":")
+
+				return Message(serverId, actionId, content)
+			}
+		}
+	}
 }
---
+++ common/src/main/kotlin/net/horizonsend/ion/common/utils/redis/gson/OidJsonSerializer.kt
@@ -0,0 +1,28 @@
+package net.horizonsend.ion.common.utils.redis.gson
+
+import com.google.gson.InstanceCreator
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import net.horizonsend.ion.common.database.Oid
+import org.bson.types.ObjectId
+import java.lang.reflect.Type
+
+object OidJsonSerializer : JsonSerializer<Oid<*>>, JsonDeserializer<Oid<*>>, InstanceCreator<Oid<*>> {
+	override fun serialize(src: Oid<*>, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
+		return JsonPrimitive(src.toString())
+	}
+
+	@Throws(JsonParseException::class)
+	override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Oid<*> {
+		return Oid<Any>(json.asString)
+	}
+
+	override fun createInstance(type: Type): Oid<*> {
+		return Oid<Any>(ObjectId())
+	}
+}
