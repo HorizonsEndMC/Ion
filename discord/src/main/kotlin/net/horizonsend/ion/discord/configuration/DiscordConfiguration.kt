b5b09d353709619773367b9decead69bd80b2ea3
Gutin1 <Claytonpinter@gmail.com>
2023-12-03 23:10:00 -0600

    don't leave my bot token as a testing default value

--- discord/src/main/kotlin/net/horizonsend/ion/discord/configuration/DiscordConfiguration.kt
+++ discord/src/main/kotlin/net/horizonsend/ion/discord/configuration/DiscordConfiguration.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DiscordConfiguration(
-	val discordBotToken: String = "MTA2MDM0MTc0MDQyODU5MTE0NQ.GSd4kg.igUpsi0ba5NVqLJy5v34gGkTnZylyGlDY9nFoY",
-	val guildID: Long = 771506479412019221,
-	val globalChannelId: Long = 1060343929473601536
+	val discordBotToken: String = "",
+	val guildID: Long = 0,
+	val globalChannelId: Long = 0
 )
 
