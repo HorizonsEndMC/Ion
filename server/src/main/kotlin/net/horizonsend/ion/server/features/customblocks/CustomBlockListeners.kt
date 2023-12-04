af85515827f67ace76350a8b76013f7920519941
kwazedilla <kwazedilla@gmail.com>
2023-12-03 18:33:26 -0800

    picks/drills with silk touch will drop custom ore instead of item

--- server/src/main/kotlin/net/horizonsend/ion/server/features/customblocks/CustomBlockListeners.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/customblocks/CustomBlockListeners.kt
@@ -1,16 +1,19 @@
 package net.horizonsend.ion.server.features.customblocks
 
 import net.horizonsend.ion.server.features.customitems.CustomBlockItem
+import net.horizonsend.ion.server.features.customitems.CustomItems
 import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
 import net.horizonsend.ion.server.listener.SLEventListener
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.block.data.BlockData
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.inventory.ItemStack
 
 class CustomBlockListeners : SLEventListener() {
 
@@ -43,9 +46,17 @@ class CustomBlockListeners : SLEventListener() {
 
         val itemUsed = event.player.inventory.itemInMainHand
         val location = block.location.toCenterLocation()
-        Tasks.sync {
-            for (drop in customBlock.getDrops(itemUsed)) {
+        if (itemUsed.enchantments.containsKey(Enchantment.SILK_TOUCH)) {
+            Tasks.sync {
+                // Make sure future custom blocks use the same identifier as its custom block identifier
+                val drop = CustomItems.getByIdentifier(customBlock.identifier)?.constructItemStack() ?: ItemStack(Material.AIR)
                 block.world.dropItem(location, drop)
+            }
+        } else {
+            Tasks.sync {
+                for (drop in customBlock.getDrops(itemUsed)) {
+                    block.world.dropItem(location, drop)
+                }
             }
         }
     }
