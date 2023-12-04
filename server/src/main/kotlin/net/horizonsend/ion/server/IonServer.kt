a88de3917f4968275ff0561b5753725f1cb45a5e
kwazedilla <kwazedilla@gmail.com>
2023-12-03 18:13:04 -0800

    bazaar conversion utility

--- server/src/main/kotlin/net/horizonsend/ion/server/IonServer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/IonServer.kt
@@ -4,6 +4,7 @@ import co.aikar.commands.PaperCommandManager
 import net.horizonsend.ion.common.CommonConfig
 import net.horizonsend.ion.common.IonComponent
 import net.horizonsend.ion.common.database.DBManager
+import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.common.extensions.prefixProvider
 import net.horizonsend.ion.common.utils.Configuration
 import net.horizonsend.ion.common.utils.getUpdateMessage
@@ -123,6 +124,8 @@ object IonServer : JavaPlugin() {
 		}
 
 		DBManager.INITIALIZATION_COMPLETE = true // Start handling reads from the DB
+
+		BazaarItem.replaceLegacyMinerals()
 	}
 
 	override fun onDisable() {
