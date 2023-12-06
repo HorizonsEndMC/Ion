70143de36be66a20c0525c943985f344e4271e21
Gutin1 <Claytonpinter@gmail.com>
2023-12-06 02:35:59 -0600

    redo redis backend, undeprecate chat

--- common/src/main/kotlin/net/horizonsend/ion/common/utils/text/colors/ColorScheme.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/utils/text/colors/ColorScheme.kt
@@ -6,7 +6,7 @@ import net.kyori.adventure.text.format.TextColor
 class HEColorScheme(override val adventure: TextColor): PresetColor {
 	companion object {
 		val HE_LIGHT_GRAY = HEColorScheme(TextColor.fromHexString("#E1E1E1")!!)
-		val HE_LIGHT_BLUE = HEColorScheme(TextColor.fromHexString("#D8D7D7")!!)
+		val HE_LIGHT_BLUE = HEColorScheme(TextColor.fromHexString("#B8D7D7")!!)
 		val HE_LIGHT_ORANGE = HEColorScheme(TextColor.fromHexString("#F2AE67")!!)
 		val HE_MEDIUM_GRAY = HEColorScheme(TextColor.fromHexString("#768A8A")!!)
 		val HE_DARK_GRAY = HEColorScheme(TextColor.fromHexString("#414C4C")!!)
--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/features/Messaging.kt
+++
@@ -1,2 +0,0 @@
-package net.horizonsend.ion.proxy.features
-
