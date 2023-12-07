5d6eed25a9a1d1d13dd6360daf72a2d6fb7c94af
Gutin1 <Claytonpinter@gmail.com>
2023-12-07 03:20:54 -0600

    smaller AI HL variant

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/starshipweapon/heavy/AIHeavyLaserStarshipWeaponMultiblock.kt
@@ -0,0 +1,54 @@
+package net.horizonsend.ion.server.features.multiblock.starshipweapon.heavy
+
+import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.starshipweapon.SignlessStarshipWeaponMultiblock
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.secondary.AIHeavyLaserWeaponSubsystem
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.Material
+import org.bukkit.block.BlockFace
+
+object AIHeavyLaserStarshipWeaponMultiblock : SignlessStarshipWeaponMultiblock<AIHeavyLaserWeaponSubsystem>() {
+	override val requiredPermission: String = "ion.weapon.ai"
+	override fun createSubsystem(starship: ActiveStarship, pos: Vec3i, face: BlockFace): AIHeavyLaserWeaponSubsystem {
+		return AIHeavyLaserWeaponSubsystem(starship, pos, face)
+	}
+
+	override fun MultiblockShape.buildStructure() {
+		z(+9) {
+			y(+0) {
+				x(+0).pistonBase()
+			}
+		}
+		z(+1) {
+			y(+0) {
+				x(+0).type(Material.GRINDSTONE)
+			}
+		}
+		z(+2) {
+			y(+0) {
+				x(+0).furnace()
+			}
+		}
+		z(+3) {
+			y(+0) {
+				x(+0).furnace()
+			}
+		}
+		z(+4) {
+			y(+0) {
+				x(+0).thrusterBlock()
+			}
+		}
+		z(+5) {
+			y(+0) {
+				x(+0).thrusterBlock()
+			}
+		}
+		z(+6) {
+			y(+0) {
+				x(+0).thrusterBlock()
+			}
+		}
+	}
+}
