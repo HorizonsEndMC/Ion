278d5a5d0dda763138d428331e92e0f677a553df
Gutin1 <Claytonpinter@gmail.com>
2023-12-07 19:46:32 -0600

    standard color schemes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/spawning/pirate/PirateSpawner.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/spawning/pirate/PirateSpawner.kt
@@ -1,4 +1,11 @@
 package net.horizonsend.ion.server.features.starship.active.ai.spawning.pirate
 
+import net.kyori.adventure.text.format.TextColor
+
 class PirateSpawner {
+
+	companion object {
+		val PIRATE_LIGHT_RED = TextColor.fromHexString("#A06363")
+		val PIRATE_DARK_RED = TextColor.fromHexString("#C63F3F")
+	}
 }
