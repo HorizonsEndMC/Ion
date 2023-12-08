2ad30c5456cd07062dc8e09fa60f68bfcc396419
Gutin1 <Claytonpinter@gmail.com>
2023-12-08 01:32:24 -0600

    discord component serializer

--- discord/src/main/kotlin/net/horizonsend/ion/discord/features/DiscordMessages.kt
+++ discord/src/main/kotlin/net/horizonsend/ion/discord/features/DiscordMessages.kt
@@ -1,14 +1,17 @@
 package net.horizonsend.ion.discord.features
 
 import com.google.gson.reflect.TypeToken
+import dev.vankka.mcdiscordreserializer.discord.DiscordSerializer
 import net.horizonsend.ion.common.utils.redis.RedisAction
 import net.horizonsend.ion.common.utils.redis.messaging.DiscordMessages
-import net.horizonsend.ion.common.utils.text.plainText
 import net.horizonsend.ion.discord.IonDiscordBot
 import net.horizonsend.ion.discord.utils.getChannel
 import net.horizonsend.ion.discord.utils.jda
+import net.kyori.adventure.text.Component
 
 object DiscordMessages : DiscordMessages() {
+	fun asDiscord(component: Component): String = DiscordSerializer.INSTANCE.serialize(component)
+
 	override val notifyAction: RedisAction<DiscordMessage> = object : RedisAction<DiscordMessage>(
 		"notify-discord",
 		object : TypeToken<DiscordMessage>() {}.type,
@@ -18,7 +21,7 @@ object DiscordMessages : DiscordMessages() {
 			val (channel, component) = data
 
 			val textChannel = channel.getChannel(IonDiscordBot.server)
-			textChannel.sendMessage(component.plainText()).queue()
+			textChannel.sendMessage(asDiscord(component)).queue()
 		}
 	}
 
