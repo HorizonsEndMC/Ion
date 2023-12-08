7bd918b4505a6e294d71d188b43ec6e1c2c56b68
Gutin1 <Claytonpinter@gmail.com>
2023-12-07 19:46:32 -0600

    standard color schemes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/spawning/privateer/PrivateerSpawner.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/ai/spawning/privateer/PrivateerSpawner.kt
@@ -1,4 +1,10 @@
 package net.horizonsend.ion.server.features.starship.active.ai.spawning.privateer
 
+import net.kyori.adventure.text.format.TextColor
+
 class PrivateerSpawner {
+	companion object PrivateerColorScheme {
+		val PRIVATEER_LIGHT_TEAL = TextColor.fromHexString("#79B698")
+		val PRIVATEER_DARK_TEAL = TextColor.fromHexString("#63A077")
+	}
 }
