854ad341dd0f7715a215f46d90f8ede3391b62ff
Gutin1 <Claytonpinter@gmail.com>
2023-12-09 00:05:18 -0600

    bring fix for join messages to the main branch

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Components.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Components.kt
@@ -2,10 +2,11 @@ package net.horizonsend.ion.proxy
 
 import net.horizonsend.ion.common.database.DBManager
 import net.horizonsend.ion.common.utils.redisaction.RedisActions
+import net.horizonsend.ion.proxy.features.ConnectionMessages
 import net.horizonsend.ion.proxy.features.cache.Caches
-import net.horizonsend.ion.proxy.features.cache.ConnectionMessages
 import net.horizonsend.ion.proxy.features.cache.ServerMapping
 import net.horizonsend.ion.proxy.features.cache.ServerPresence
+import net.horizonsend.ion.proxy.features.messaging.PlayerTracking
 
 val components = listOf(
 	DBManager,
@@ -13,5 +14,6 @@ val components = listOf(
 	Caches,
 	ServerMapping,
 	ServerPresence,
+	PlayerTracking,
 	ConnectionMessages,
 )
