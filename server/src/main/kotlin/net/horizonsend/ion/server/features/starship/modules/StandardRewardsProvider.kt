4495632f8906ca5a80bd5035023ce9189fda7996
Gutin1 <Claytonpinter@gmail.com>
2023-12-11 00:28:01 -0600

    configurable AI rewards provider

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/modules/StandardRewardsProvider.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/modules/StandardRewardsProvider.kt
@@ -2,7 +2,6 @@ package net.horizonsend.ion.server.features.starship.modules
 
 import net.horizonsend.ion.common.database.cache.nations.RelationCache
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
-import net.horizonsend.ion.common.utils.text.plainText
 import net.horizonsend.ion.server.features.achievements.Achievement
 import net.horizonsend.ion.server.features.achievements.rewardAchievement
 import net.horizonsend.ion.server.features.progression.ShipKillXP
@@ -14,10 +13,11 @@ import net.horizonsend.ion.server.miscellaneous.utils.get
 import org.bukkit.entity.Player
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-class StandardRewardsProvider(private val starship: ActiveStarship) : RewardsProvider {
+open class StandardRewardsProvider(protected val starship: ActiveStarship) : RewardsProvider {
 	override fun onSink() {
 		val dataMap = starship.damagers
 			.filter { (damager, _) ->
@@ -43,7 +43,6 @@ class StandardRewardsProvider(private val starship: ActiveStarship) : RewardsPro
 			if (timeStamp < ShipKillXP.damagerExpiration) continue
 
 			val player = (damager as? PlayerDamager)?.player ?: continue // shouldn't happen
-			val killedSize = starship.initialBlockCount.toDouble()
 
 			val pilotNation = SLPlayer[player].nation
 
@@ -54,20 +53,27 @@ class StandardRewardsProvider(private val starship: ActiveStarship) : RewardsPro
 				if (RelationCache[pilotNation, killedNation].ordinal >= 5) continue
 			}
 
-			val percent = points.get() / sum
-			val xp = ((sqrt(killedSize.pow(2.0) / sqrt(killedSize * 0.00005))) * percent).toInt()
-
-			if (xp > 0) {
-				damager.rewardXP(xp)
-				log.info("Gave ${player.name} $xp XP for ship-killing ${starship.controller.pilotName.plainText()}")
-			}
+			processDamagerRewards(damager, points, timeStamp, sum)
 
 			if (points.get() > 0 && player.uniqueId != killedPlayer?.uniqueId) player.rewardAchievement(Achievement.KILL_SHIP)
 		}
 	}
 
+	protected open fun processDamagerRewards(damager: Damager, points: AtomicInteger, lastDamaged: Long, pointsSum: Int) {
+		val killedSize = starship.initialBlockCount.toDouble()
+
+		val percent = points.get() / pointsSum
+		val xp = ((sqrt(killedSize.pow(2.0) / sqrt(killedSize * 0.00005))) * percent).toInt()
+
+		if (xp > 0) {
+			damager.rewardXP(xp)
+			log.info("Gave $damager $xp XP for ship-killing ${starship.identifier}")
+		}
+	}
+
 	companion object {
-		private val log: Logger = LoggerFactory.getLogger(StandardRewardsProvider::class.java)
+		@JvmStatic
+		protected val log: Logger = LoggerFactory.getLogger(StandardRewardsProvider::class.java)
 	}
 }
 
