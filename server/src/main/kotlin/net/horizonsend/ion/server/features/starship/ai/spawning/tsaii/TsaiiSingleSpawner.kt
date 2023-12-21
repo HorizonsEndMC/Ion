c108af31645c1a04e208cf18befaeb3d94f76e0a
Gutin1 <Claytonpinter@gmail.com>
2023-12-21 03:16:49 -0600

    refactors, more privateer config, exception handling and improvements

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/ai/spawning/tsaii/TsaiiSingleSpawner.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/ai/spawning/tsaii/TsaiiSingleSpawner.kt
@@ -1,13 +1,14 @@
-package net.horizonsend.ion.server.features.starship.ai.spawning.privateer
+package net.horizonsend.ion.server.features.starship.ai.spawning.tsaii
 
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.configuration.AIShipConfiguration
+import net.horizonsend.ion.server.features.starship.ai.spawning.privateer.findPrivateerSpawnLocation
 import net.horizonsend.ion.server.features.starship.ai.spawning.template.BasicSpawner
 import org.bukkit.Location
 
-class PrivateerFleetSpawner : BasicSpawner(
-	"PRIVATEER_FLEET",
-	IonServer.aiShipConfiguration.spawners::privateerFleet,
+class TsaiiSingleSpawner : BasicSpawner(
+	"TSAII_ATTACK",
+	IonServer.aiShipConfiguration.spawners::tsaiiAttack,
 ) {
 	override fun findSpawnLocation(): Location? = findPrivateerSpawnLocation(configuration)
 
