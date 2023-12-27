e8c84d46a33c8f59839ca713c37b096134409ff5
Gutin1 <Claytonpinter@gmail.com>
2023-12-27 13:38:28 -0600

    fix some deprecation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/particleshield/ShieldMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/particleshield/ShieldMultiblock.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.features.multiblock.particleshield
 
 import net.horizonsend.ion.common.extensions.userError
+import net.horizonsend.ion.common.utils.text.plainText
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.multiblock.InteractableMultiblock
 import net.horizonsend.ion.server.features.multiblock.Multiblock
@@ -8,7 +9,6 @@ import net.horizonsend.ion.server.miscellaneous.utils.PerPlayerCooldown
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.block.Sign
@@ -23,7 +23,7 @@ abstract class ShieldMultiblock : Multiblock(), InteractableMultiblock {
 
 	// let people use [particleshield] if they want
 	override fun matchesUndetectedSign(sign: Sign): Boolean {
-		return super.matchesUndetectedSign(sign) || sign.getLine(0).equals("[particleshield]", ignoreCase = true)
+		return super.matchesUndetectedSign(sign) || sign.line(0).plainText().equals("[particleshield]", ignoreCase = true)
 	}
 
 	override fun onTransformSign(player: Player, sign: Sign) {
@@ -31,8 +31,8 @@ abstract class ShieldMultiblock : Multiblock(), InteractableMultiblock {
 	}
 
 	override fun setupSign(player: Player, sign: Sign) {
-		if (sign.getLine(1).isEmpty()) {
-			player.sendMessage(ChatColor.RED.toString() + "The second line must be the shield's name.")
+		if (sign.line(1).plainText().isEmpty()) {
+			player.userError("The second line must be the shield's name.")
 			return
 		}
 
@@ -41,8 +41,10 @@ abstract class ShieldMultiblock : Multiblock(), InteractableMultiblock {
 
 	abstract fun getShieldBlocks(sign: Sign): List<Vec3i>
 
-	val displayCooldown = PerPlayerCooldown.messagedCooldown(5L, TimeUnit.SECONDS) {
-		Bukkit.getPlayer(it)?.userError("You're doing that too often!")
+	companion object {
+		private val displayCooldown = PerPlayerCooldown.messagedCooldown(5L, TimeUnit.SECONDS) {
+			Bukkit.getPlayer(it)?.userError("You're doing that too often!")
+		}
 	}
 
 	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) = displayCooldown.tryExec(player.uniqueId) {
