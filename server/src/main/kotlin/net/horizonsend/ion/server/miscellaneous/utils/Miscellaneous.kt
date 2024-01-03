CrystalzzzDeez/Hehehehaw

Navigation engine

server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Miscellaneous.kt
@@ -10,12 +10,20 @@ import net.kyori.adventure.audience.ForwardingAudience
 import net.milkbowl.vault.economy.Economy
 import net.minecraft.core.BlockPos
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
+import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.network.protocol.game.ClientboundSetBorderWarningDistancePacket
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.network.ServerGamePacketListenerImpl
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.monster.Shulker
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.StructureBlock
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.StructureBlockEntity
+import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.border.WorldBorder
 import net.minecraft.world.level.chunk.ChunkStatus
 import net.minecraft.world.level.chunk.LevelChunk
@@ -31,6 +39,7 @@ import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 import java.util.EnumSet
+import kotlin.reflect.jvm.isAccessible
 
 val vaultEconomy = try {
 	Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider
@@ -94,26 +103,15 @@ fun runnable(e: BukkitRunnable.() -> Unit): BukkitRunnable = object : BukkitRunn
 fun <T : Entity> World.castSpawnEntity(location: Location, type: org.bukkit.entity.EntityType) =
 	this.spawnEntity(location, type) as T
 
-fun debugHighlightBlock(x: Number, y: Number, z: Number) {
-	IonCommand.debugEnabledPlayers.mapNotNull { Bukkit.getPlayer(it) }.forEach {
-		if (it.location.distanceSquared(
-				Location(
-					it.world,
-					x.toDouble(),
-					y.toDouble(),
-					z.toDouble()
-				)
-			) > 30 * 30
-		) return@forEach
+fun debugHighlightBlock(x: Number, y: Number, z: Number, duration: Long = 5L) = debugAudience.highlightBlock(Vec3i(x.toInt(), y.toInt(), z.toInt()), duration)
+fun debugHighlightBlocks(blocks: Collection<Vec3i>, duration: Long = 5L) = debugAudience.highlightBlocks(blocks, duration)
 
-		highlightBlock(it, BlockPos(x.toInt(), y.toInt(), z.toInt()), 5L)
-	}
+val debugAudience: ForwardingAudience = object : ForwardingAudience {
+	override fun audiences(): MutableIterable<Audience> = IonCommand.debugEnabledPlayers
 }
 
-val debugAudience: ForwardingAudience get() = Audience.audience(IonCommand.debugEnabledPlayers.map(Bukkit::getPlayer))
-
 fun areaDebugMessage(x: Number, y: Number, z: Number, msg: String) {
-	IonCommand.debugEnabledPlayers.mapNotNull { Bukkit.getPlayer(it) }.forEach {
+	IonCommand.debugEnabledPlayers.mapNotNull { it as? Player }.forEach {
 		if (it.location.distanceSquared(
 				Location(
 					it.world,
@@ -128,11 +126,10 @@ fun areaDebugMessage(x: Number, y: Number, z: Number, msg: String) {
 	}
 }
 
-fun highlightBlock(bukkitPlayer: Player, pos: BlockPos, duration: Long) {
+fun highlightBlock(bukkitPlayer: Player, pos: Vec3i, duration: Long) {
 	val player = bukkitPlayer.minecraft
 	val conn = player.connection
-	val shulker =
-		Shulker(EntityType.SHULKER, player.level).apply {
+	val shulker = Shulker(EntityType.SHULKER, player.level).apply {
 			setPos(pos.x + 0.5, pos.y.toDouble(), pos.z + 0.5)
 			setGlowingTag(true)
 			isInvisible = true
@@ -142,4 +139,63 @@ fun highlightBlock(bukkitPlayer: Player, pos: BlockPos, duration: Long) {
 	shulker.entityData.refresh(player)
 
 	Tasks.syncDelayTask(duration) { conn.send(ClientboundRemoveEntitiesPacket(shulker.id)) }
+}
+
+fun Audience.highlightBlock(pos: Vec3i, duration: Long) {
+	when (this) {
+		is Player -> highlightBlock(this, pos, duration)
+		is ForwardingAudience -> for (player in audiences().filterIsInstance<Player>()) { highlightBlock(player, pos, duration) }
+	}
+}
+
+fun Audience.highlightBlocks(positions: Collection<Vec3i>, duration: Long) {
+	for (pos in positions) this.highlightBlock(pos, duration)
+}
+
+fun buildStructureBlock(minPoint: Vec3i, maxPoint: Vec3i, message: String = ""): Pair<BlockState, StructureBlockEntity> {
+	val constructor = StructureBlock::class.constructors.first()
+	constructor.isAccessible = true
+
+	val block = Blocks.STRUCTURE_BLOCK as StructureBlock
+	val state = block.defaultBlockState()
+
+	val (x, y, z) = minPoint
+	val blockPos = BlockPos(x + 8, y + 8, z + 8)
+
+	val xDiff = maxPoint.x - minPoint.x
+	val yDiff = maxPoint.y - minPoint.y
+	val zDiff = maxPoint.z - minPoint.z
+
+	val entity: StructureBlockEntity = block.newBlockEntity(blockPos, state) as StructureBlockEntity
+	entity.showBoundingBox = true
+	entity.structureName = message
+
+	entity.setStructureSize(net.minecraft.core.Vec3i(xDiff, yDiff, zDiff))
+
+	return state to entity
+}
+
+fun Player.highlightRegion(minPoint: Vec3i, maxPoint: Vec3i, structureName: String = "") {
+	val (state, entity) = buildStructureBlock(minPoint, maxPoint, structureName)
+
+	showBlockState(state, entity)
+}
+
+fun Player.showBlockState(state: BlockState, blockEntity: BlockEntity) {
+	val position = blockEntity.blockPos
+
+	val conn: ServerGamePacketListenerImpl = this.minecraft.connection
+
+	conn.send(ClientboundBlockUpdatePacket(position, state))
+	conn.send(ClientboundBlockEntityDataPacket.create(blockEntity))
+}
+
+fun Player.showBlockState(position: BlockPos, state: BlockState, blockEntity: BlockEntity?) {
+	val conn: ServerGamePacketListenerImpl = this.minecraft.connection
+
+	conn.send(ClientboundBlockUpdatePacket(position, state))
+
+	blockEntity?.let {
+		conn.send(ClientboundBlockEntityDataPacket.create(blockEntity))
+	}
 }

HorizonsEndMC/Ion

server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Miscellaneous.kt
@@ -11,12 +11,20 @@ import net.horizonsend.ion.server.features.machine.AreaShields
 import net.milkbowl.vault.economy.Economy
 import net.minecraft.core.BlockPos
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
+import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.network.protocol.game.ClientboundSetBorderWarningDistancePacket
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.network.ServerGamePacketListenerImpl
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.monster.Slime
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.StructureBlock
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.StructureBlockEntity
+import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.border.WorldBorder
 import net.minecraft.world.level.chunk.ChunkStatus
 import net.minecraft.world.level.chunk.LevelChunk
@@ -35,6 +43,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockExplodeEvent
 import org.bukkit.scheduler.BukkitRunnable
 import java.util.EnumSet
+import kotlin.reflect.jvm.isAccessible
 
 val vaultEconomy = try {
 	Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider
@@ -98,26 +107,15 @@ fun runnable(e: BukkitRunnable.() -> Unit): BukkitRunnable = object : BukkitRunn
 fun <T : Entity> World.castSpawnEntity(location: Location, type: org.bukkit.entity.EntityType) =
 	this.spawnEntity(location, type) as T
 
-fun debugHighlightBlock(x: Number, y: Number, z: Number) {
-	IonCommand.debugEnabledPlayers.mapNotNull { Bukkit.getPlayer(it) }.forEach {
-		if (it.location.distanceSquared(
-				Location(
-					it.world,
-					x.toDouble(),
-					y.toDouble(),
-					z.toDouble()
-				)
-			) > 30 * 30
-		) return@forEach
+fun debugHighlightBlock(x: Number, y: Number, z: Number, duration: Long = 5L) = debugAudience.highlightBlock(Vec3i(x.toInt(), y.toInt(), z.toInt()), duration)
+fun debugHighlightBlocks(blocks: Collection<Vec3i>, duration: Long = 5L) = debugAudience.highlightBlocks(blocks, duration)
 
-		highlightBlock(it, BlockPos(x.toInt(), y.toInt(), z.toInt()), 5L)
-	}
+val debugAudience: ForwardingAudience = object : ForwardingAudience {
+	override fun audiences(): MutableIterable<Audience> = IonCommand.debugEnabledPlayers
 }
 
-val debugAudience: ForwardingAudience get() = Audience.audience(IonCommand.debugEnabledPlayers.map(Bukkit::getPlayer))
-
 fun areaDebugMessage(x: Number, y: Number, z: Number, msg: String) {
-	IonCommand.debugEnabledPlayers.mapNotNull { Bukkit.getPlayer(it) }.forEach {
+	IonCommand.debugEnabledPlayers.mapNotNull { it as? Player }.forEach {
 		if (it.location.distanceSquared(
 				Location(
 					it.world,
@@ -132,11 +130,10 @@ fun areaDebugMessage(x: Number, y: Number, z: Number, msg: String) {
 	}
 }
 
-fun highlightBlock(bukkitPlayer: Player, pos: BlockPos, duration: Long) {
+fun highlightBlock(bukkitPlayer: Player, pos: Vec3i, duration: Long) {
 	val player = bukkitPlayer.minecraft
 	val conn = player.connection
-	val slime =
-		Slime(EntityType.SLIME, player.level).apply {
+	val slime = Slime(EntityType.SLIME, player.level).apply {
 			setPos(pos.x + 0.5, pos.y.toDouble(), pos.z + 0.5)
 			setGlowingTag(true)
 			isInvisible = true
@@ -148,14 +145,63 @@ fun highlightBlock(bukkitPlayer: Player, pos: BlockPos, duration: Long) {
 	Tasks.syncDelayTask(duration) { conn.send(ClientboundRemoveEntitiesPacket(slime.id)) }
 }
 
-fun repeatString(string: String, count: Int): String {
-	val builder = StringBuilder()
-
-	for (x in 0 until count) {
-		builder.append(string)
+fun Audience.highlightBlock(pos: Vec3i, duration: Long) {
+	when (this) {
+		is Player -> highlightBlock(this, pos, duration)
+		is ForwardingAudience -> for (player in audiences().filterIsInstance<Player>()) { highlightBlock(player, pos, duration) }
 	}
+}
 
-	return builder.toString()
+fun Audience.highlightBlocks(positions: Collection<Vec3i>, duration: Long) {
+	for (pos in positions) this.highlightBlock(pos, duration)
+}
+
+fun buildStructureBlock(minPoint: Vec3i, maxPoint: Vec3i, message: String = ""): Pair<BlockState, StructureBlockEntity> {
+	val constructor = StructureBlock::class.constructors.first()
+	constructor.isAccessible = true
+
+	val block = Blocks.STRUCTURE_BLOCK as StructureBlock
+	val state = block.defaultBlockState()
+
+	val (x, y, z) = minPoint
+	val blockPos = BlockPos(x + 8, y + 8, z + 8)
+
+	val xDiff = maxPoint.x - minPoint.x
+	val yDiff = maxPoint.y - minPoint.y
+	val zDiff = maxPoint.z - minPoint.z
+
+	val entity: StructureBlockEntity = block.newBlockEntity(blockPos, state) as StructureBlockEntity
+	entity.showBoundingBox = true
+	entity.structureName = message
+
+	entity.setStructureSize(net.minecraft.core.Vec3i(xDiff, yDiff, zDiff))
+
+	return state to entity
+}
+
+fun Player.highlightRegion(minPoint: Vec3i, maxPoint: Vec3i, structureName: String = "") {
+	val (state, entity) = buildStructureBlock(minPoint, maxPoint, structureName)
+
+	showBlockState(state, entity)
+}
+
+fun Player.showBlockState(state: BlockState, blockEntity: BlockEntity) {
+	val position = blockEntity.blockPos
+
+	val conn: ServerGamePacketListenerImpl = this.minecraft.connection
+
+	conn.send(ClientboundBlockUpdatePacket(position, state))
+	conn.send(ClientboundBlockEntityDataPacket.create(blockEntity))
+}
+
+fun Player.showBlockState(position: BlockPos, state: BlockState, blockEntity: BlockEntity?) {
+	val conn: ServerGamePacketListenerImpl = this.minecraft.connection
+
+	conn.send(ClientboundBlockUpdatePacket(position, state))
+
+	blockEntity?.let {
+		conn.send(ClientboundBlockEntityDataPacket.create(blockEntity))
+	}
 }
 
 /**
@@ -182,16 +228,4 @@ fun regeneratingBlockChange(source: Entity?, origin: Block, changedBlocks: Mutab
 	world.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 10f, 0.5f)
 
 	return blockExplodeEvent.callEvent()
-}
-
-val alphaNumericChars : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-
-fun randomString(length: Long, inputRandom: Random? = null): String {
-	val random = inputRandom ?: Random()
-
-	return random
-		.ints(length, 0, alphaNumericChars.size)
-		.asSequence()
-		.map(alphaNumericChars::get)
-		.joinToString("")
 }