61bfce351c9cd99c218534476f43cbfb6f1f0092
Gutin1 <Claytonpinter@gmail.com>
2024-01-04 01:50:29 -0600

    move channel definitions to config

--- discord/src/main/kotlin/net/horizonsend/ion/discord/configuration/DiscordConfiguration.kt
+++ discord/src/main/kotlin/net/horizonsend/ion/discord/configuration/DiscordConfiguration.kt
@@ -1,11 +1,17 @@
 package net.horizonsend.ion.discord.configuration
 
 import kotlinx.serialization.Serializable
+import net.horizonsend.ion.common.utils.discord.Channel
 
 @Serializable
 data class DiscordConfiguration(
 	val discordBotToken: String = "",
 	val guildID: Long = 0,
-	val globalChannelId: Long = 0
+	val globalChannelId: Long = 0,
+	val channelIdMap: Map<Channel, Long> = mapOf(
+		Channel.GLOBAL to 954781227246817341,
+		Channel.EVENTS to 979124611256041473,
+		Channel.CHANGELOG to 1096907580577697833
+	)
 )
 
--- discord/src/main/kotlin/net/horizonsend/ion/discord/utils/Channel.kt
+++ discord/src/main/kotlin/net/horizonsend/ion/discord/utils/Channel.kt
@@ -3,8 +3,11 @@ package net.horizonsend.ion.discord.utils
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
 import net.horizonsend.ion.common.utils.discord.Channel
+import net.horizonsend.ion.discord.IonDiscordBot
 import net.horizonsend.ion.discord.IonDiscordBot.exit
 
+fun Channel.getID() = IonDiscordBot.configuration.channelIdMap[this]!!
+
 fun Channel.getChannel(guild: Guild): TextChannel {
-	return guild.getTextChannelById(id) ?: exit("Could not find channel $name", NullPointerException())
+	return guild.getTextChannelById(getID()) ?: exit("Could not find channel $name", NullPointerException())
 }
