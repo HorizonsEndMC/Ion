043fddb56bd022ce71eaad012e5a8fd7c0d3c340
Gutin1 <Claytonpinter@gmail.com>
2024-01-04 01:50:29 -0600

    move channel definitions to config

--- common/src/main/kotlin/net/horizonsend/ion/common/utils/discord/Channel.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/utils/discord/Channel.kt
@@ -1,7 +1,7 @@
 package net.horizonsend.ion.common.utils.discord
 
-enum class Channel(val id: Long) {
-	GLOBAL(1060343929473601536),
-	EVENTS(1070622772180566116),
-	CHANGELOG(1182158653743173632)
+enum class Channel {
+	GLOBAL,
+	EVENTS,
+	CHANGELOG,
 }
