eaf379732c18ef14f4ab49315f88b26528943541
Gutin1 <Claytonpinter@gmail.com>
2024-01-20 17:44:40 -0600

    serializable

--- server/src/main/kotlin/net/horizonsend/ion/server/configuration/FeatureFlags.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/configuration/FeatureFlags.kt
@@ -1,5 +1,8 @@
 package net.horizonsend.ion.server.configuration
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FeatureFlags(
 	val ECONOMY: Boolean = true,
 	val BOUNTIES: Boolean = true,
