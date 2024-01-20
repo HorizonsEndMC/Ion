492191a0ebc659f05b2dea59f59d4710dbfd2bcb
Gutin1 <Claytonpinter@gmail.com>
2024-01-20 13:51:41 -0600

    Implement K9's rotating messages

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/managers/ReminderManager.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/managers/ReminderManager.kt
@@ -1,36 +1,54 @@
 package net.horizonsend.ion.proxy.managers
 
 import net.horizonsend.ion.common.IonComponent
+import net.horizonsend.ion.common.utils.text.bracketed
+import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_DARK_GRAY
+import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_LIGHT_GRAY
+import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
+import net.horizonsend.ion.common.utils.text.miniMessage
+import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.proxy.PLUGIN
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.TextDecoration
 import java.util.concurrent.TimeUnit
 
 object ReminderManager : IonComponent() {
-	private val scheduledMessages = listOf<Runnable>(
-//		Runnable { voteReminder() }
+	private val messages = listOf(
+		"<$HE_MEDIUM_GRAY>Low on <dark_purple>chetherite</dark_purple>? Get your votes in, today! /vote",
+		"<$HE_MEDIUM_GRAY>Be sure to visit Prometheus Station to be able to buy or sell up to <gold>30,000C</gold> worth of crops per day.",
+		"<$HE_MEDIUM_GRAY>Bed not working? Just got blasted out of oblivion? Use /kit starter to build a cryopod.",
+		"<$HE_MEDIUM_GRAY>Commissions and cargo trading are great ways for new players to make money. Feeling confident? Try your hand at /bounty.",
+		"<$HE_MEDIUM_GRAY>Need an item? Use /bazaar browse, although be aware: the prices are quadrupled if you're buying remotely!",
+		"<$HE_MEDIUM_GRAY>24 siegeable stations exist in space. Only nations can siege stations, and owning between 1-6 stations reaps benefits for all nation members.",
+		"<$HE_MEDIUM_GRAY>Just starting out? Try visiting the planets of Luxiterna or Aret. Both have resources valuable for new players.",
+		"<$HE_MEDIUM_GRAY>Level 60 is currently the highest level required to unlock the biggest avaialable ship class: Destroyers / Heavy Friegthers (12,000 blocks).",
+		"<$HE_MEDIUM_GRAY>Want to pick a fight with NPCs? It's recommended you take a gunship, at least.",
+		"<$HE_MEDIUM_GRAY>Looking for a settlement or nation to join? Be sure to ask in global chat using /g. There are always players recruiting.",
+		"<$HE_MEDIUM_GRAY>Setting up machinery? Solar power and Gas generators are clean and reliable. Do /wiki to learn more.",
+		"<$HE_MEDIUM_GRAY>Don't forget to enable your ship lock when parking in territory that's not your own. The lock is toggleable in the ship computer (jukebox).",
+		"<$HE_MEDIUM_GRAY>Use /starship when piloting a ship to get a diagnostic overview of your vessel. This is quite useful for shipbuilding and repairs.",
+		"<$HE_MEDIUM_GRAY>Unsure who someone is? Use /pinfo [player]."
+
+		// expand list here
 	)
 
-	private const val delay: Long = 900 // Might need to be increased if more messages are added (Currently 15 minutes)
+	private val prefix = bracketed(text("Horizon's End", HE_LIGHT_GRAY, TextDecoration.BOLD))
+
+	private const val INTERVAL = 5L * 1
 
 	override fun onEnable() {
-		scheduleReminders()
+		PLUGIN.proxy.scheduler.repeat(INTERVAL, INTERVAL, TimeUnit.SECONDS, ::sendBroadcast)
 	}
 
-	private fun scheduleReminders() {
-		 for (message in scheduledMessages) {
-		 	PLUGIN.proxy.scheduler.repeat(
-				delay,
-		 		(delay / scheduledMessages.size.toLong()) * scheduledMessages.indexOf(message),
-		 		TimeUnit.SECONDS,
-				message
-		 	)
-		 }
+	private var index = 0
+
+	private fun sendBroadcast() {
+		val message = messages.getOrNull(index) ?: return
+
+		val formatted = ofChildren(prefix, text(" » ", HE_DARK_GRAY), miniMessage.deserialize(message))
+
+		PLUGIN.proxy.sendMessage(formatted)
+
+		index = (index + 1) % messages.size
 	}
-// Might bring this back eventually
-//	private fun voteReminder() = transaction {
-//		for (player in PLUGIN.proxy.players) {
-//			val playerData = PlayerData[player.uniqueId] ?: continue
-//			val shouldPrompt: Boolean = playerData.voteTimes.find { it.dateTime.isBefore(LocalDateTime.now().minusDays(1)) } != null
-//			if (shouldPrompt) player.special("Please vote for the server to help grow the community! <green><click:run_command:/vote>Run /vote to see where!")
-//		}
-//	}
 }
