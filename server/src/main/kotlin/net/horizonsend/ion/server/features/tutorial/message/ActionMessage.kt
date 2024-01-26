ec56bf735fb2658fa5f68ddad2fc9a2948d2c578
Gutin1 <Claytonpinter@gmail.com>
2024-01-26 17:29:39 -0600

    start modernization of tutorial

--- server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/message/ActionMessage.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/message/ActionMessage.kt
@@ -1,9 +1,14 @@
 package net.horizonsend.ion.server.features.tutorial.message
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
 import org.bukkit.entity.Player
 
-class ActionMessage(title: String, subtitle: String, private val action: (Player) -> Unit) :
-	PopupMessage(title, subtitle) {
+class ActionMessage(
+	title: Component = text(""),
+	subtitle: Component = text(""),
+	private val action: (Player) -> Unit
+) : PopupMessage(title, subtitle) {
 	override fun show(player: Player) {
 		super.show(player)
 		action(player)
--- server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/message/PopupMessage.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/message/PopupMessage.kt
@@ -1,13 +1,31 @@
 package net.horizonsend.ion.server.features.tutorial.message
 
-import net.horizonsend.ion.server.miscellaneous.utils.colorize
-import net.horizonsend.ion.server.miscellaneous.utils.msg
+import io.papermc.paper.util.Tick
+import net.horizonsend.ion.common.utils.text.colors.HEColorScheme
+import net.horizonsend.ion.common.utils.text.ofChildren
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.empty
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.title.Title
 import org.bukkit.entity.Player
+import java.time.Duration
 
-open class PopupMessage(private val title: String = "", private val subtitle: String = "") :
-	TutorialMessage("$title $subtitle".split(" ").count().toDouble() * 0.25 + 0.25) {
+open class PopupMessage(
+	private val title: Component = empty(),
+	private val subtitle: Component = empty()
+) : TutorialMessage("$title $subtitle".split(" ").count().toDouble() * 0.25 + 0.25) {
 	override fun show(player: Player) {
-		player.sendTitle(title.colorize(), subtitle.colorize(), 10, Int.MAX_VALUE - 20, 0)
-		player msg "&r$title &8>> &r$subtitle"
+		player.showTitle(
+			Title.title(
+				title,
+				subtitle,
+				Title.Times.times(
+					Duration.of(10, Tick.tick()),
+					Duration.of(Int.MAX_VALUE - 20L, Tick.tick()),
+					Duration.of(0,  Tick.tick()))
+			)
+		)
+
+		player.sendMessage(ofChildren(title, text(" » ", HEColorScheme.HE_DARK_GRAY), subtitle))
 	}
 }
