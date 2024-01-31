c645f8a9dbd5fe064678cd83a26b0c5c31035a4d
Gutin1 <Claytonpinter@gmail.com>
2024-01-31 15:52:18 -0600

    allow usage of tinted glass as a wildcard pipe

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Materials.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Materials.kt
@@ -14,6 +14,7 @@ fun getMatchingMaterials(filter: (Material) -> Boolean): EnumSet<Material> =
 val STAINED_GLASS_TYPES = getMatchingMaterials { it.name.endsWith("_STAINED_GLASS") }
 val Material.isGlass: Boolean get() = this == Material.GLASS || this.isStainedGlass
 val Material.isStainedGlass: Boolean get() = STAINED_GLASS_TYPES.contains(this)
+val Material.isTintedGlass: Boolean get() = equals(Material.TINTED_GLASS)
 
 val STAINED_GLASS_PANE_TYPES = getMatchingMaterials { it.name.endsWith("_STAINED_GLASS_PANE") }
 val Material.isGlassPane: Boolean get() = this == Material.GLASS_PANE || this.isStainedGlassPane
