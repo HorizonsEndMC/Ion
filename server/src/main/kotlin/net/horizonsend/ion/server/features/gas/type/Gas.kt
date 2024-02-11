59d176bf2b2ec9ab79efa6293617863adbd677b7
Gutin1 <Claytonpinter@gmail.com>
2024-02-10 23:55:34 -0600

    Move gas config to world config

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/Gas.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/Gas.kt
@@ -1,9 +1,12 @@
 package net.horizonsend.ion.server.features.gas.type
 
-import net.horizonsend.ion.server.configuration.Gasses.GasConfiguration
+import net.horizonsend.ion.server.configuration.Gasses
 import net.horizonsend.ion.server.features.gas.collectionfactors.CollectionFactor
+import net.horizonsend.ion.server.features.world.IonWorld
+import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.kyori.adventure.text.Component
 import org.bukkit.Location
+import org.bukkit.World
 import java.util.function.Supplier
 
 abstract class Gas(
@@ -11,12 +14,16 @@ abstract class Gas(
 	val displayName: Component,
 	val containerIdentifier: String,
 
-	val configurationSupplier: Supplier<GasConfiguration>
-	) {
+	val configurationSupplier: Supplier<Gasses.GasConfiguration>,
+	val collectionFactorSupplier: (IonWorld) -> List<CollectionFactor>
+) {
 	val configuration = configurationSupplier.get()
-	val factors: List<CollectionFactor> = configurationSupplier.get().formattedFactors
+
+	fun getFactors(world: World): List<CollectionFactor> = collectionFactorSupplier(world.ion)
 
     fun tryCollect(location: Location): Boolean {
+		val factors = getFactors(location.world)
+
 		if (factors.isEmpty()) return false
 
         return factors.stream().allMatch { factor: CollectionFactor ->
@@ -25,6 +32,8 @@ abstract class Gas(
     }
 
 	fun canBeFound(location: Location): Boolean {
+		val factors = getFactors(location.world)
+
 		if (factors.isEmpty()) return false
 
 		return factors.all { factor: CollectionFactor -> factor.canBeFound(location) }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/WorldSettings.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/WorldSettings.kt
@@ -1,10 +1,12 @@
 package net.horizonsend.ion.server.features.world
 
 import kotlinx.serialization.Serializable
+import net.horizonsend.ion.server.features.gas.type.WorldGasConfiguration
 import net.horizonsend.ion.server.features.world.environment.Environment
 
 @Serializable
 data class WorldSettings(
     val flags: MutableSet<WorldFlag> = mutableSetOf(),
-    val environments: MutableSet<Environment> = mutableSetOf()
+    val environments: MutableSet<Environment> = mutableSetOf(),
+	val gasses: WorldGasConfiguration = WorldGasConfiguration()
 )
