dfb0f707198a79211812a64264c2d741000fa5b2
Gutin1 <Claytonpinter@gmail.com>
2024-02-10 23:55:34 -0600

    Move gas config to world config

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/GasFuel.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/GasFuel.kt
@@ -1,6 +1,8 @@
 package net.horizonsend.ion.server.features.gas.type
 
 import net.horizonsend.ion.server.configuration.Gasses
+import net.horizonsend.ion.server.features.gas.collectionfactors.CollectionFactor
+import net.horizonsend.ion.server.features.world.IonWorld
 import net.kyori.adventure.text.Component
 import java.util.function.Supplier
 
@@ -8,9 +10,10 @@ abstract class GasFuel(
 	identifier: String,
 	displayName: Component,
 	containerIdentifier: String,
-	factorSupplier: Supplier<Gasses.GasConfiguration>,
+	configurationSupplier: Supplier<Gasses.GasConfiguration>,
+	collectionFactorSupplier: (IonWorld) -> List<CollectionFactor>,
 
 	val powerPerUnit: Int,
 	val cooldown: Int
-) : Gas(identifier, displayName, containerIdentifier, factorSupplier)
+) : Gas(identifier, displayName, containerIdentifier, configurationSupplier, collectionFactorSupplier)
 
