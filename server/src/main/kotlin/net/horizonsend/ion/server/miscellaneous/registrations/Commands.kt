7cd9b9e06aab10d249bd4d9fa5923ff9c378bfd9
Gutin1 <Claytonpinter@gmail.com>
2024-02-14 20:53:24 -0600

    tutorial npc work

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.miscellaneous.registrations
 
 import net.horizonsend.ion.server.command.SLCommand
+import net.horizonsend.ion.server.command.TutorialCommand
 import net.horizonsend.ion.server.command.admin.AdminCommands
 import net.horizonsend.ion.server.command.admin.BatteryCommand
 import net.horizonsend.ion.server.command.admin.GracePeriod
@@ -163,5 +164,6 @@ val commands: List<SLCommand> = listOf(
 	IonBroadcastCommand,
 	BlockCommand,
 
-	HelpCommand
+	HelpCommand,
+	TutorialCommand
 )
