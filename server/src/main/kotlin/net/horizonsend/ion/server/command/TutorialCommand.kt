20b10a35d860da0a1fd6093263d66aa8c1f53f58
Gutin1 <Claytonpinter@gmail.com>
2024-02-18 03:19:39 -0600

    npc work

--- server/src/main/kotlin/net/horizonsend/ion/server/command/TutorialCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/TutorialCommand.kt
@@ -3,7 +3,6 @@ package net.horizonsend.ion.server.command
 import co.aikar.commands.PaperCommandManager
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.Subcommand
-import net.horizonsend.ion.server.configuration.ServerConfiguration
 import net.horizonsend.ion.server.features.npcs.TutorialNPCs
 import net.horizonsend.ion.server.features.tutorial.npcs.TutorialNPCType
 import org.bukkit.entity.Player
@@ -13,33 +12,26 @@ import java.util.UUID
 object TutorialCommand : SLCommand() {
 	override fun onEnable(manager: PaperCommandManager) {
 		manager.commandCompletions.registerAsyncCompletion("tutorialNPCIds") {
-			TutorialNPCs.store.storage.npcs.map { it.uuid.toString() }
+			TutorialNPCs.manager.allNPCs().map { it.uniqueId.toString() }
 		}
 	}
 
-	@Subcommand("admin create npc")
+	@Subcommand("create npc")
 	fun onCreateNPC(sender: Player, type: TutorialNPCType) {
-		TutorialNPCs.store.storage.npcs.add(TutorialNPCs.TutorialDroid(
-			ServerConfiguration.Pos(
-				sender.location.world.name,
-				sender.location.x.toInt(),
-				sender.location.y.toInt(),
-				sender.location.z.toInt(),),
-			UUID.randomUUID(),
+		TutorialNPCs.createNPC(
+			sender.location,
 			type
-		))
-
-		TutorialNPCs.store.saveStorage()
+		)
 	}
-
-	@Subcommand("admin remove npc")
+//
+	@Subcommand("remove npc")
 	fun onRemoveNPC(sender: Player, uuid: String) {
-		TutorialNPCs.store.storage.npcs.removeAll { it.uuid == UUID.fromString(uuid) }
-
-		TutorialNPCs.store.saveStorage()
+		TutorialNPCs.editStorage {
+			types.remove(UUID.fromString(uuid))
+		}
 	}
 
-	@Subcommand("admin reload npc")
+	@Subcommand("reload npc")
 	fun onReloadNPC(sender: Player) {
 		TutorialNPCs.onDisable()
 		TutorialNPCs.onEnable()
--- server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/CitizensUtils.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/CitizensUtils.kt
@@ -1,10 +1,23 @@
 package net.horizonsend.ion.server.features.npcs
 
+import net.citizensnpcs.api.CitizensAPI
+import net.citizensnpcs.api.npc.MemoryNPCDataStore
 import net.citizensnpcs.api.npc.NPCRegistry
 import net.horizonsend.ion.server.IonServer
 import org.bukkit.entity.Player
+import org.slf4j.Logger
 
 val isCitizensLoaded get() = IonServer.server.pluginManager.isPluginEnabled("Citizens")
 val registries: MutableList<NPCRegistry> = mutableListOf()
 
 fun Player.isNPC() = registries.any { it.isNPC(this) }
+
+fun createNamedMemoryRegistry(logger: Logger, npcRegistryName: String): NPCRegistry {
+	logger.info("Creating Citizens memory data store $npcRegistryName")
+
+	val dataStore = MemoryNPCDataStore()
+	val registry = CitizensAPI.createNamedNPCRegistry(npcRegistryName, dataStore)
+	registries.add(registry)
+
+	return registry
+}
