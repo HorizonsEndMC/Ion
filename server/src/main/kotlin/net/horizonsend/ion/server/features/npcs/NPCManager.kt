815bec8fb3c1ad2a91e363553a5bc2dc8b10b4f1
Gutin1 <Claytonpinter@gmail.com>
2024-02-18 03:19:39 -0600

    npc work

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/NPCManager.kt
@@ -0,0 +1,93 @@
+package net.horizonsend.ion.server.features.npcs
+
+import net.citizensnpcs.api.CitizensAPI
+import net.citizensnpcs.api.npc.NPC
+import net.citizensnpcs.api.npc.NPCRegistry
+import net.horizonsend.ion.server.IonServerComponent
+import net.horizonsend.ion.server.miscellaneous.utils.loadChunkAsync
+import org.bukkit.Location
+import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
+import java.util.UUID
+
+class NPCManager(val name: String) : IonServerComponent(true) {
+	private lateinit var npcRegistry: NPCRegistry
+	var enabled = false
+
+	fun enableRegistry() {
+		if (!isCitizensLoaded) {
+			log.warn("Citizens not loaded! $name may not function properly!")
+			return
+		}
+
+		npcRegistry = createNamedMemoryRegistry(log, name)
+		enabled = true
+	}
+
+	fun disableRegistry() {
+		if (!enabled) return
+
+		enabled = false
+		clearNPCs()
+	}
+
+	fun clearNPCs() {
+		if (::npcRegistry.isInitialized) {
+			npcRegistry.toList().forEach(NPC::destroy)
+			npcRegistry.deregisterAll()
+			CitizensAPI.removeNamedNPCRegistry(name)
+		}
+	}
+
+	fun spawnNPCAsync(npc: NPC, location: Location, preCheck: (NPC) -> Boolean = { true }, callback: (NPC) -> Unit = {}) {
+		loadChunkAsync(location.world, location) {
+			if (!preCheck(npc)) return@loadChunkAsync
+
+			npc.spawn(location)
+			callback(npc)
+		}
+	}
+
+	fun allNPCs(): List<NPC> {
+		if (!enabled) return listOf()
+		return npcRegistry.toList()
+	}
+
+	fun isNpc(entity: Entity): Boolean {
+		if (!enabled) return false
+
+		return npcRegistry.any { it.uniqueId == entity.uniqueId }
+	}
+
+	fun contains(npc: NPC): Boolean {
+		if (!enabled) return false
+
+		return npcRegistry.contains(npc)
+	}
+
+	/**
+	 * Creates the NPC and spawns it into the world
+	 **/
+	fun createNPC(name: String, uuid: UUID, id: Int, location: Location, preCheck: (NPC) -> Boolean = { true }, callback: (NPC) -> Unit = {}): NPC {
+		if (!enabled) throw IllegalStateException()
+
+		val npc = npcRegistry.createNPC(EntityType.PLAYER, uuid, id, name)
+		spawnNPCAsync(npc, location, callback = callback)
+
+		return npc
+	}
+
+	/**
+	 * Returns whether the npc was successfully removed
+	 **/
+	fun removeNPC(uuid: UUID): Boolean {
+		if (!enabled) return false
+
+		val npc = npcRegistry.getByUniqueId(uuid) ?: return false
+
+		npc.destroy()
+		npcRegistry.deregister(npc)
+
+		return true
+	}
+}
