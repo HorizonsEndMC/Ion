a94395596a5299339df1c952eac90729df475ab6
CrystalzzzDeez <billcypher269@gmail.com>
2024-02-18 22:47:02 +0000

    actually add the stuff i mentioned last pr

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/centrifuge/CentrifugeMultiblock.kt
@@ -0,0 +1,139 @@
+package net.horizonsend.ion.server.features.multiblock.centrifuge
+
+import net.horizonsend.ion.server.features.customitems.CustomItems.ENRICHED_URANIUM
+import net.horizonsend.ion.server.features.customitems.CustomItems.URANIUM
+import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
+import net.horizonsend.ion.server.features.machine.PowerMachines
+import net.horizonsend.ion.server.features.multiblock.FurnaceMultiblock
+import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.PowerStoringMultiblock
+import org.bukkit.Material
+import org.bukkit.block.Furnace
+import org.bukkit.block.Sign
+import org.bukkit.event.inventory.FurnaceBurnEvent
+import org.bukkit.inventory.ItemStack
+
+
+abstract class CentrifugeMultiblock	: Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+	override fun MultiblockShape.buildStructure() {
+		z(+0) {
+			y(-1) {
+				x(-2).steelBlock()
+				x(-1).anyGlassPane()
+				x(+0).wireInputComputer()
+				x(+1).anyGlassPane()
+				x(+2).steelBlock()
+			}
+			y(+0) {
+				x(-2).steelBlock()
+				x(-1).anyGlassPane()
+				x(+0).machineFurnace()
+				x(+1).anyGlassPane()
+				x(+2).steelBlock()
+			}
+		}
+		z(+1) {
+			y(-1) {
+				x(-2).anyGlassPane()
+				x(-1).copperBlock()
+				x(+0).endRod()
+				x(+1).copperBlock()
+				x(+2).anyGlassPane()
+			}
+			y(+0) {
+				x(-2).anyGlassPane()
+				x(-1).anyStairs()
+				x(+0).anyStairs()
+				x(+1).anyStairs()
+				x(+2).anyGlassPane()
+			}
+		}
+		z(+2) {
+			y(-1) {
+				x(-2).ironBlock()
+				x(-1).copperBlock()
+				x(+0).sponge()
+				x(+1).copperBlock()
+				x(+2).ironBlock()
+			}
+			y(+0) {
+				x(-2).ironBlock()
+				x(-1).anyStairs()
+				x(+0).sculkCatalyst()
+				x(+1).anyStairs()
+				x(+2).ironBlock()
+			}
+		z(+3) {
+			y(-1) {
+				x(-2).anyGlassPane()
+				x(-1).copperBlock()
+				x(+0).endRod()
+				x(+1).copperBlock()
+				x(+2).anyGlassPane()
+			}
+			y(+0) {
+				x(-2).anyGlassPane()
+				x(-1).anyStairs()
+				x(+0).anyStairs()
+				x(+1).anyStairs()
+				x(+2).anyGlassPane()
+			}
+		}
+		z(+4) {
+			y(-1) {
+				x(-2).steelBlock()
+				x(-1).anyGlassPane()
+				x(+0).sponge()
+				x(+1).anyGlassPane()
+				x(+2).steelBlock()
+			}
+			y(+0) {
+				x(-2).steelBlock()
+				x(-1).anyGlassPane()
+				x(+0).ironBlock()
+				x(+1).anyGlassPane()
+				x(+2).steelBlock()
+			}
+		}
+		}
+		}
+
+	override val name = "centrifuge"
+
+	override val signText = createSignText(
+			line1 = "&6Centrifuge",
+			line2 = null,
+			line3 = null,
+			line4 = null
+	)
+
+	override fun onFurnaceTick(
+			event: FurnaceBurnEvent,
+			furnace: Furnace,
+			sign: Sign
+	) {
+		event.isBurning = false
+		event.burnTime = 360000
+		furnace.cookTime = (-1000).toShort()
+		event.isCancelled = false
+
+		val smelting = furnace.inventory.smelting
+		val fuel = furnace.inventory.fuel
+		val result = furnace.inventory.result
+
+		if (PowerMachines.getPower(sign) == 0 ||
+				smelting == null ||
+				smelting.type != Material.PRISMARINE_CRYSTALS ||
+				fuel == null
+		) {
+			return
+		}
+		if (fuel.customItem != URANIUM) return
+		event.isCancelled = false
+		fuel.subtract(1)
+		if (result == null)  furnace.inventory.result = ENRICHED_URANIUM.constructItemStack()
+		else result.add(1)
+		PowerMachines.removePower(sign, 300)
+	}
+}
