4be7b3630a3a8b5a8be3eac05365249ae9c25a54
CrystalzzzDeez <billcypher269@gmail.com>
2024-02-18 04:50:41 +0000

    oxygen furnace and steel ingot

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Crafting.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Crafting.kt
@@ -11,7 +11,9 @@ import net.horizonsend.ion.server.features.customitems.CustomItems.CHETHERITE_BL
 import net.horizonsend.ion.server.features.customitems.CustomItems.CIRCUITRY
 import net.horizonsend.ion.server.features.customitems.CustomItems.DETONATOR
 import net.horizonsend.ion.server.features.customitems.CustomItems.GAS_CANISTER_EMPTY
+import net.horizonsend.ion.server.features.customitems.CustomItems.GAS_CANISTER_OXYGEN
 import net.horizonsend.ion.server.features.customitems.CustomItems.GUN_BARREL
+import net.horizonsend.ion.server.features.customitems.CustomItems.OXYGEN_TANK
 import net.horizonsend.ion.server.features.customitems.CustomItems.PISTOL
 import net.horizonsend.ion.server.features.customitems.CustomItems.PISTOL_RECEIVER
 import net.horizonsend.ion.server.features.customitems.CustomItems.RAW_ALUMINUM
@@ -104,6 +106,11 @@ import org.bukkit.inventory.ShapelessRecipe
 
 object Crafting : IonServerComponent() {
 	override fun onEnable() {
+		//Oxygen Tank
+		itemStackShapelessRecipe("oxygenTank", OXYGEN_TANK.constructItemStack()) {
+			addIngredient(GAS_CANISTER_OXYGEN.constructItemStack().asQuantity(9))
+		}
+
 		// Prismarine Bricks
 		Bukkit.addRecipe(
 			FurnaceRecipe(
