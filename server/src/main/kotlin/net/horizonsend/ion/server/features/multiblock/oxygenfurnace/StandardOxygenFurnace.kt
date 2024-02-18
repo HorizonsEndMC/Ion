35c6587d60f2e4e213c71d9968e6248010dbf155
CrystalzzzDeez <billcypher269@gmail.com>
2024-02-18 22:47:02 +0000

    actually add the stuff i mentioned last pr

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/oxygenfurnace/StandardOxygenFurnace.kt
@@ -0,0 +1,6 @@
+package net.horizonsend.ion.server.features.multiblock.oxygenfurnace
+
+object StandardOxygenFurnaceMultiblock : OxygenFurnaceMultiblock() {
+	override val maxPower = 250_000
+	override val speed: Double = 1.0
+}
