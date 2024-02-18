6fa8493f0ae410b44b74742cfbd3b01463e8c691
Gutin1 <Claytonpinter@gmail.com>
2024-02-18 14:18:26 -0600

    finally get tutorial NPCs working

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
@@ -1,7 +1,7 @@
 package net.horizonsend.ion.server.miscellaneous.registrations
 
 import net.horizonsend.ion.server.command.SLCommand
-import net.horizonsend.ion.server.command.TutorialCommand
+import net.horizonsend.ion.server.command.TutorialAdminCommand
 import net.horizonsend.ion.server.command.admin.AdminCommands
 import net.horizonsend.ion.server.command.admin.BatteryCommand
 import net.horizonsend.ion.server.command.admin.ConvertCommand
@@ -180,5 +180,5 @@ val commands: List<SLCommand> = listOf(
 	LocatorCommands,
 	ItemDebugCommand,
 	HelpCommand,
-	TutorialCommand
+	TutorialAdminCommand
 )
