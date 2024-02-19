e669a98fea3855cfeea88531c9be950725ce95b7
CrystalzzzDeez <billcypher269@gmail.com>
2024-02-19 00:01:38 +0000

    reactive line finished plate press added circuitfab added respective items and recipes added

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/circuitfab/CircuitfabMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/circuitfab/CircuitfabMultiblock.kt
@@ -1,7 +1,7 @@
-package net.horizonsend.ion.server.features.multiblock.compressor
+package net.horizonsend.ion.server.features.multiblock.circuitfab
 
-import net.horizonsend.ion.server.features.customitems.CustomItems.URANIUM_ROD
-import net.horizonsend.ion.server.features.customitems.CustomItems.URANIUM_CORE
+import net.horizonsend.ion.server.features.customitems.CustomItems.CIRCUITRY
+import net.horizonsend.ion.server.features.customitems.CustomItems.CIRCUIT_BOARD
 import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
 import net.horizonsend.ion.server.features.machine.PowerMachines
 import net.horizonsend.ion.server.features.multiblock.FurnaceMultiblock
@@ -16,99 +16,63 @@ import org.bukkit.event.inventory.FurnaceBurnEvent
 import org.bukkit.inventory.ItemStack
 
 
-abstract class CompressorMultiblock	: Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+abstract class CircuitfabMultiblock	: Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
 	override fun MultiblockShape.buildStructure() {
 		z(+0) {
 			y(-1) {
+				x(-2).anyStairs()
 				x(-1).ironBlock()
 				x(+0).wireInputComputer()
-				x(+1).ironBlock()
+				x(+1).anyGlass()
+				x(+2).anyStairs()
 			}
 			y(+0) {
-				x(-1).anyStairs()
+				x(-2).anyStairs()
+				x(-1).craftingTable()
 				x(+0).machineFurnace()
-				x(+1).anyStairs()
+				x(+1).anyGlass()
+				x(+2).anyStairs()
 			}
 		}
 		z(+1) {
 			y(-1) {
-				x(-1).netheriteBlock()
-				x(+0).endRod()
-				x(+1).netheriteBlock()
+				x(-2).ironBlock()
+				x(-1).redstoneBlock()
+				x(+0).redstoneBlock()
+				x(+1).redstoneBlock()
+				x(+2).ironBlock()
 			}
 			y(+0) {
-				x(-1).ironBlock()
-				x(+0).craftingTable()
-				x(+1).ironBlock()
+				x(-2).ironBlock()
+				x(-1).endRod()
+				x(+0).anvil()
+				x(+1).endRod()
+				x(+2).ironBlock()
 			}
 		}
 		z(+2) {
 			y(-1) {
-				x(-1).netheriteBlock()
-				x(+0).endRod()
-				x(+1).netheriteBlock()
-			}
-			y(+0) {
-				x(-1).anyGlassPane()
-				x(+0).pistonBase()
-				x(+1).anyGlassPane()
-			}
-		}
-		z(+3) {
-			y(-1) {
-				x(-1).anyGlass()
-				x(+0).sponge()
-				x(+1).anyGlass()
-			}
-			y(+0) {
-				x(-1).anyGlass()
-				x(+0).lodestone()
-				x(+1).anyGlass()
-			}
-		}
-		z(+4) {
-			y(-1) {
-				x(-1).netheriteBlock()
-				x(+0).endRod()
-				x(+1).netheriteBlock()
-			}
-			y(+0) {
-				x(-1).anyGlassPane()
-				x(+0).pistonBase()
-				x(+1).anyGlassPane()
-			}
-		}
-		z(+5) {
-			y(-1) {
-				x(-1).netheriteBlock()
-				x(+0).endRod()
-				x(+1).netheriteBlock()
-			}
-			y(+0) {
+				x(-2).anyStairs()
 				x(-1).ironBlock()
-				x(+0).ironBlock()
+				x(+0).titaniumBlock()
 				x(+1).ironBlock()
-			}
-		}
-		z(+6) {
-			y(-1) {
-				x(-1).ironBlock()
-				x(+0).sponge()
-				x(+1).ironBlock()
+				x(+2).anyStairs()
 			}
 			y(+0) {
-				x(-1).anyStairs()
-				x(+0).ironBlock()
-				x(+1).anyStairs()
+				x(-2).anyStairs()
+				x(-1).ironBlock()
+				x(+0).titaniumBlock()
+				x(+1).ironBlock()
+				x(+2).anyStairs()
 			}
 		}
 	}
 
 
-	override val name = "compressor"
+	override val name = "circuitfab"
 
 	override val signText = createSignText(
-			line1 = "&6Compressor",
+			line1 = "&6Circuitfab",
 			line2 = null,
 			line3 = null,
 			line4 = null
@@ -135,10 +99,10 @@ abstract class CompressorMultiblock	: Multiblock(), PowerStoringMultiblock, Furn
 		) {
 			return
 		}
-		if (fuel.customItem != URANIUM_CORE) return
+		if (fuel.customItem != CIRCUITRY) return
 		event.isCancelled = false
 		fuel.subtract(1)
-		if (result == null)  furnace.inventory.result = URANIUM_ROD.constructItemStack()
+		if (result == null)  furnace.inventory.result = CIRCUIT_BOARD.constructItemStack()
 		else result.add(1)
 		PowerMachines.removePower(sign, 300)
 	}
