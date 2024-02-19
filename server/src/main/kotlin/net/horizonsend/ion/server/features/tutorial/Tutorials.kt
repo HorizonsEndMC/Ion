c9de972399a9206578912f0d25e17b7bb0417f6f
Gutin1 <Claytonpinter@gmail.com>
2024-02-19 14:20:17 -0600

    redo tutorial class structure

--- server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/Tutorials.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/Tutorials.kt
@@ -2,85 +2,31 @@ package net.horizonsend.ion.server.features.tutorial
 
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.IonServerComponent
-import net.horizonsend.ion.server.features.starship.DeactivatedPlayerStarships
+import net.horizonsend.ion.server.features.tutorial.tutorials.FlightTutorial
+import net.horizonsend.ion.server.features.tutorial.tutorials.Tutorial
 import net.horizonsend.ion.server.miscellaneous.utils.listen
 import net.horizonsend.ion.server.miscellaneous.utils.minecraft
 import net.minecraft.world.level.chunk.LevelChunkSection
 import org.bukkit.Chunk
-import org.bukkit.World
-import org.bukkit.entity.Player
-import org.bukkit.event.block.BlockBreakEvent
-import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
-import org.bukkit.event.world.ChunkUnloadEvent
 import java.lang.ref.WeakReference
-import java.util.UUID
 
 object Tutorials : IonServerComponent() {
-	var playersInTutorials = mutableMapOf<Player, TutorialPhase>()
-	private var readTimes = mutableMapOf<UUID, Long>()
-
-	private val tutorials: Set<TutorialCompanion> = setOf(
-		FlightTutorialPhase
+	private val tutorials: Set<Tutorial> = setOf(
+		FlightTutorial
 	)
-
-	fun isTutorialWorld(world: World) = tutorials.any { it.isTutorialWorld(world) }
-
-	operator fun get(world: World): TutorialCompanion? = tutorials.find { it.isTutorialWorld(world) }
 
 	override fun onEnable() {
 		if (!IonServer.featureFlags.tutorials) return
 
-		listen<PlayerJoinEvent> { event ->
-			val player = event.player
-
-			player.resetTitle()
-			playersInTutorials.remove(player) // who knows...
-
-			get(player.world)?.start(player)
-		}
-
 		listen<PlayerQuitEvent> { event ->
-			tutorials.forEach { it.stop(event.player) }
-		}
-
-		listen<BlockBreakEvent> { event ->
-			if (isTutorialWorld(event.block.world)) event.isCancelled = true
-		}
-
-		// Disable all damage in the world
-		listen<EntityDamageEvent> { event ->
-			if (isTutorialWorld(event.entity.world)) {
-				event.isCancelled = true
-			}
-		}
-
-		// erase chunks in the world
-		listen<ChunkUnloadEvent> { event ->
-			if (!isTutorialWorld(event.world)) {
-				return@listen
-			}
-
-			val chunk = event.chunk
-			val chunkReference = WeakReference(chunk)
-
-			val worldShipData = DeactivatedPlayerStarships.getInChunk(chunk)
-			if (worldShipData.any()) {
-				log.warn("Deleting " + worldShipData.size + " starship computers in tutorial world")
-				DeactivatedPlayerStarships.destroyManyAsync(worldShipData) {
-					clearChunk(chunkReference)
-				}
-				return@listen
-			}
-
-			clearChunk(chunkReference)
+			tutorials.forEach { it.endTutorial(event.player) }
 		}
 
 		tutorials.forEach { tutorial ->
-			tutorial.onEnable()
+			tutorial.setup()
 
-			tutorial.entries.forEach {
+			tutorial.phases.forEach {
 				it.setupHandlers()
 			}
 		}
--- server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/npcs/TutorialNPCType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/tutorial/npcs/TutorialNPCType.kt
@@ -2,22 +2,21 @@ package net.horizonsend.ion.server.features.tutorial.npcs
 
 import net.citizensnpcs.api.event.NPCRightClickEvent
 import net.horizonsend.ion.common.utils.text.miniMessage
+import net.horizonsend.ion.server.features.starship.ai.spawning.explorer.EXPLORER_MEDIUM_CYAN
+import net.horizonsend.ion.server.features.starship.ai.spawning.miningcorp.MINING_CORP_LIGHT_ORANGE
 import net.horizonsend.ion.server.features.starship.ai.spawning.pirate.PIRATE_DARK_RED
-import net.horizonsend.ion.server.features.tutorial.FlightTutorialPhase
+import net.horizonsend.ion.server.features.tutorial.tutorials.FlightTutorial
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 
-enum class TutorialNPCType(
-	val npcName: Component,
-	val underName: Component? = null
-) {
-	FLIGHT_TUTORIAL(text("TUTORIAL NPC", PIRATE_DARK_RED)) {
+enum class TutorialNPCType(val displayName: Component, val billboardText: List<Component> = listOf()) {
+	FLIGHT_TUTORIAL(text("TUTORIAL NPC", PIRATE_DARK_RED), listOf(text("1", MINING_CORP_LIGHT_ORANGE), text("2", EXPLORER_MEDIUM_CYAN), text("3", PIRATE_DARK_RED))) {
 		override fun onRightClick(event: NPCRightClickEvent) {
-			FlightTutorialPhase.start(event.clicker)
+			FlightTutorial.startTutorial(event.clicker)
 		}
 	},
 
-	MINIMESSAGE_TEST("<rainbow>||||||||||||".miniMessage()) {
+	MINIMESSAGE_TEST("<rainbow>|||||||||||||||||||||||||||||".miniMessage()) {
 		override fun onRightClick(event: NPCRightClickEvent) {
 			println("received right click")
 		}
