f0e82b3f25897c8692979c6d42bc64d650dbb1fe
CrystalzzzDeez <billcypher269@gmail.com>
2024-02-20 18:23:02 +0000

    Most of the starship work done

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/misc/FuelTankMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/misc/FuelTankMultiblock.kt
@@ -54,7 +54,7 @@ object FuelTankMultiblock : Multiblock() {
 				x(+1).ironBlock()
 				x(+2).anyStairs()
 			}
-			y(+0) {
+			y(+1) {
 				x(-2).anyStairs()
 				x(-1).ironBlock()
 				x(+0).anyGlass()
