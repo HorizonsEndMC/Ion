8f935b390956e076241af1befc91fb16d13d271f
Gutin1 <Claytonpinter@gmail.com>
2024-03-04 22:56:51 -0600

    get async bukkit blockstate access working

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/BlockRequirement.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/BlockRequirement.kt
@@ -5,11 +5,17 @@ import org.bukkit.block.BlockFace
 import org.bukkit.block.data.BlockData
 
 class BlockRequirement(
-	val example: BlockData,
+	var example: BlockData,
 	private val check: (Block, BlockFace, Boolean) -> Boolean,
 	private val asyncCheck: suspend (Block, BlockFace, Boolean) -> Boolean
 ) {
 	operator fun invoke(block: Block, inward: BlockFace, loadChunks: Boolean) = check.invoke(block, inward, loadChunks)
 
 	suspend fun checkAsync(block: Block, inward: BlockFace, loadChunks: Boolean) = asyncCheck.invoke(block, inward, loadChunks)
+
+	fun setExample(blockData: BlockData): BlockRequirement {
+		this.example = blockData
+
+		return this
+	}
 }
