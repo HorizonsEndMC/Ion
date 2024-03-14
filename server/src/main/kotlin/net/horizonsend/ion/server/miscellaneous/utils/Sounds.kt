46195b46daa8ad21654d5cc0e4ce6912aeb10df3
Gutin1 <Claytonpinter@gmail.com>
2024-03-14 03:30:16 -0500

    use common utils, refactoring

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/Sounds.kt
+++
@@ -1,6 +0,0 @@
-package net.horizonsend.ion.server.miscellaneous.utils
-
-import org.bukkit.Sound
-import org.bukkit.craftbukkit.CraftSound
-
-val Sound.mcName: String get() = CraftSound.getSound(this)
