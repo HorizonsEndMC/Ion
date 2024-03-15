c68cc580864505e9b2f4b48bf8d892bad97ca2e2
Gutin1 <Claytonpinter@gmail.com>
2024-03-15 01:22:03 -0500

    start on new extractors and power

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportNetwork.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportNetwork.kt
@@ -1,46 +1,58 @@
 package net.horizonsend.ion.server.features.transport
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import net.horizonsend.ion.server.features.multiblock.entity.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.transport.grid.PowerGrid
 import net.horizonsend.ion.server.features.world.IonChunk
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import org.bukkit.Chunk
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
 
-class ChunkPowerNetwork(
+class ChunkTransportNetwork(
 	val chunk: IonChunk,
 ) {
+	// Each chunk gets a scope for parallelism
+	private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+
 	val extractorData = getExtractorData(chunk.inner)
+
+	val poweredMultiblockEntities = ConcurrentHashMap<Long, PoweredMultiblockEntity>()
+	val powerGrid = PowerGrid(extractorData)
 
 	init {
 	    setup()
 	}
 
 	private fun setup() {
-
+		collectPowerMultiblockEntities()
 	}
 
 	fun tick() {
+		for ((location, extractor) in extractorData.extractorLocations) {
 
+		}
 	}
 
 	fun save() {
 
 	}
 
-	private fun getPowerMultiblockEntities(): ConcurrentHashMap<Long, PoweredMultiblockEntity> {
-		val poweredMultiblockEntities = ConcurrentHashMap<Long, PoweredMultiblockEntity>()
+	private fun tickExtractors() {
 
+	}
+
+	private fun collectPowerMultiblockEntities() {
 		chunk.multiblockManager.getAllMultiblockEntities().forEach { (key, entity) ->
 			if (entity !is PoweredMultiblockEntity) return@forEach
 
 			poweredMultiblockEntities[key] = entity
 		}
-
-		return poweredMultiblockEntities
 	}
 
-	fun getExtractorData(chunk: Chunk): ExtractorData {
+	private fun getExtractorData(chunk: Chunk): ExtractorData {
 		val extractors = chunk.persistentDataContainer.get(NamespacedKeys.EXTRACTOR_DATA, ExtractorData)
 
 		return extractors ?: ExtractorData(ConcurrentLinkedQueue())
