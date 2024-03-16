c862ffe69e6aa1339e6df98b1fb9503fb70ccdc0
Gutin1 <Claytonpinter@gmail.com>
2024-03-16 14:38:31 -0500

    graphs

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportNetwork.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportNetwork.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import net.horizonsend.ion.server.features.multiblock.entity.type.PoweredMultiblockEntity
-import net.horizonsend.ion.server.features.transport.grid.PowerGrid
+import net.horizonsend.ion.server.features.transport.grid.power.PowerGrid
 import net.horizonsend.ion.server.features.world.IonChunk
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import org.bukkit.Chunk
