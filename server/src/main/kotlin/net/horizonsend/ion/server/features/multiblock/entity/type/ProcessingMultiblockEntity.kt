0107a1c552287b10e81b844193c52d42c128f50c
Gutin1 <Claytonpinter@gmail.com>
2024-03-16 01:34:16 -0500

    build out some multiblock entity types

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ProcessingMultiblockEntity.kt
@@ -0,0 +1,48 @@
+package net.horizonsend.ion.server.features.multiblock.entity.type
+
+import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import org.bukkit.World
+import org.bukkit.block.BlockFace
+import org.bukkit.persistence.PersistentDataType
+
+/**
+ * A multiblock that handles the processing of something
+ *
+ * @param requiredProgress The number of ticks to finish this process
+ * @param currentProgress The current progress of the process
+ **/
+abstract class ProcessingMultiblockEntity(
+	type: Multiblock,
+	x: Int,
+	y: Int,
+	z: Int,
+	world: World,
+	signOffset: BlockFace,
+	val requiredProgress: Int,
+	var currentProgress: Int = 0
+) : MultiblockEntity(type, x, y, z, world, signOffset), TickingMultiblockEntity {
+	//TODO
+	// -recipe system
+
+	abstract fun canProcess(): Boolean
+	abstract fun finishProcessing()
+	abstract fun startProcessing()
+	abstract fun process()
+
+	override suspend fun tick() {
+		if (currentProgress >= requiredProgress) {
+			currentProgress = 0
+			finishProcessing()
+			return
+		}
+
+		currentProgress++
+	}
+
+	override fun storeAdditionalData(store: PersistentMultiblockData) {
+		store.addAdditionalData(NamespacedKeys.PROCESSING_PROGRESS, PersistentDataType.INTEGER, currentProgress)
+	}
+}
