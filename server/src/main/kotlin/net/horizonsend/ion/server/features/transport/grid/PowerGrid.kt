b41915b8e95fe57bbd75e176f083f42b889b0583
Gutin1 <Claytonpinter@gmail.com>
2024-03-16 14:38:31 -0500

    graphs

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/PowerGrid.kt
+++
@@ -1,7 +0,0 @@
-package net.horizonsend.ion.server.features.transport.grid
-
-import net.horizonsend.ion.server.features.transport.ExtractorData
-
-class PowerGrid(private val extractorData: ExtractorData) {
-
-}
