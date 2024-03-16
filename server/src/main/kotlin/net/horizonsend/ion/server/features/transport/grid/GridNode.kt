a505904c5ebae4cd284da6af87046a40bc7e1f66
Gutin1 <Claytonpinter@gmail.com>
2024-03-16 14:38:31 -0500

    graphs

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/GridNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/GridNode.kt
@@ -1,5 +1,15 @@
 package net.horizonsend.ion.server.features.transport.grid
 
-class GridNode {
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import org.bukkit.block.BlockFace
+import java.util.concurrent.ConcurrentHashMap
 
+interface GridNode {
+	val x: Int
+	val y: Int
+	val z: Int
+
+	val key get() = toBlockKey(x, y, z)
+
+	val neighbors: ConcurrentHashMap<BlockFace, GridNode>
 }
