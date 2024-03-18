7114316541c7e61ec2ddd4a4319af267073b8b25
Gutin1 <Claytonpinter@gmail.com>
2024-03-18 14:28:16 -0500

    node collection and straight wire consolidation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/util/BlockSnapshot.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/util/BlockSnapshot.kt
@@ -1,11 +1,23 @@
 package net.horizonsend.ion.server.features.multiblock.util
 
+import kotlinx.coroutines.runBlocking
+import org.bukkit.Bukkit
 import org.bukkit.ChunkSnapshot
 import org.bukkit.Material
+import org.bukkit.World
 import org.bukkit.block.Block
+import org.bukkit.block.BlockState
 import org.bukkit.block.data.BlockData
 
-class BlockSnapshot(val x: Int, val y: Int, val z: Int, val type: Material, val data: BlockData) {
+class BlockSnapshot(val world: World, val x: Int, val y: Int, val z: Int, val type: Material, val data: BlockData) {
+	val state: BlockState? get() {
+		return runBlocking { getBukkitBlockState(block, false) }
+	}
+
+	val block: Block get() {
+		return world.getBlockAt(x, y, z)
+	}
+
 	companion object {
 		fun ChunkSnapshot.getBlockSnapshot(x: Int, y: Int, z: Int): BlockSnapshot? {
 			if (x shr 4 != getX()) return null
@@ -17,9 +29,9 @@ class BlockSnapshot(val x: Int, val y: Int, val z: Int, val type: Material, val 
 			val localX = x - chunkOriginX
 			val localZ = z - chunkOriginZ
 
-			return BlockSnapshot(x, y, z, getBlockType(localX, y, localZ), getBlockData(localX, y, localZ))
+			return BlockSnapshot(Bukkit.getWorld(worldName)!!, x, y, z, getBlockType(localX, y, localZ), getBlockData(localX, y, localZ))
 		}
 
-		fun Block.snapshot(): BlockSnapshot = BlockSnapshot(x, y, z, type, blockData)
+		fun Block.snapshot(): BlockSnapshot = BlockSnapshot(world, x, y, z, type, blockData)
 	}
 }
