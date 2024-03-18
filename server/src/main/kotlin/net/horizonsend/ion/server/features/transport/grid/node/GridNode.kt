e2e9ddedd55e15bf102a4760f1e1234c4c445739
Gutin1 <Claytonpinter@gmail.com>
2024-03-18 14:28:16 -0500

    node collection and straight wire consolidation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/node/GridNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/node/GridNode.kt
@@ -1,10 +1,12 @@
 package net.horizonsend.ion.server.features.transport.grid.node
 
+import net.horizonsend.ion.server.features.transport.grid.AbstractGrid
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import org.bukkit.block.BlockFace
 import java.util.concurrent.ConcurrentHashMap
 
 interface GridNode {
+	val parent: AbstractGrid
 	val x: Int
 	val y: Int
 	val z: Int
@@ -12,4 +14,24 @@ interface GridNode {
 	val key get() = toBlockKey(x, y, z)
 
 	val neighbors: ConcurrentHashMap<BlockFace, GridNode>
+
+	fun getNeighbor(face: BlockFace): GridNode? {
+		return neighbors[face]
+	}
+
+	/**
+	 * Consolidates this node if possible
+	 **/
+	fun consolidate()
+
+	/**
+	 * Replace this node with another and update its neighbors
+	 **/
+	fun replace(new: GridNode) {
+		for ((offset, neighbor) in neighbors) {
+			val neighborRelation = offset.oppositeFace
+
+			neighbor.neighbors[neighborRelation] = neighbor
+		}
+	}
 }
