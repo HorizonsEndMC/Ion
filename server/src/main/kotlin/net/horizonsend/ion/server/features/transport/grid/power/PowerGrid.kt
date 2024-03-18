d55d377dd8acd12932b12bb0ea941f3ac7d2fffb
Gutin1 <Claytonpinter@gmail.com>
2024-03-18 01:54:29 -0500

    start of nodes and grids

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/power/PowerGrid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/power/PowerGrid.kt
@@ -1,30 +1,7 @@
 package net.horizonsend.ion.server.features.transport.grid.power
 
-import com.google.common.graph.ElementOrder
 import net.horizonsend.ion.server.features.transport.ExtractorData
-import net.horizonsend.ion.server.features.transport.grid.AbstractGrid
 
-class PowerGrid(private val extractorData: ExtractorData) : AbstractGrid<PowerNode>() {
-	override fun isDirected(): Boolean = false
-	override fun allowsSelfLoops(): Boolean = false
+class PowerGrid(private val extractorData: ExtractorData) {
 
-	override fun successors(node: PowerNode): MutableSet<PowerNode> {
-		TODO("Not yet implemented")
-	}
-
-	override fun predecessors(node: PowerNode): MutableSet<PowerNode> {
-		TODO("Not yet implemented")
-	}
-
-	override fun nodes(): MutableSet<PowerNode> {
-		TODO("Not yet implemented")
-	}
-
-	override fun nodeOrder(): ElementOrder<PowerNode> {
-		TODO("Not yet implemented")
-	}
-
-	override fun adjacentNodes(node: PowerNode): MutableSet<PowerNode> {
-		return node.neighbors.values.mapTo(mutableSetOf()) { it as PowerNode }
-	}
 }
