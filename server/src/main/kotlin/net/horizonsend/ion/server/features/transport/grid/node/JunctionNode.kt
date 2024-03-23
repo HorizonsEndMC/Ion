ad0cd3d53c7fe11f11e4faa7fc74c719c092c3aa
Gutin1 <Claytonpinter@gmail.com>
2024-03-23 02:23:59 -0500

    more work

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/node/JunctionNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/node/JunctionNode.kt
@@ -1,17 +1,16 @@
 package net.horizonsend.ion.server.features.transport.grid.node
 
-import net.horizonsend.ion.server.features.transport.grid.AbstractGrid
+import net.horizonsend.ion.server.features.transport.grid.Grid
 import org.bukkit.block.BlockFace
 import java.util.concurrent.ConcurrentHashMap
 
+/**
+ * An omnidirectional node
+ **/
 class JunctionNode(
-	override val parent: AbstractGrid,
+	override val parentGrid: Grid,
 	override val x: Int,
 	override val y: Int,
 	override val z: Int,
 	override val neighbors: ConcurrentHashMap<BlockFace, GridNode> = ConcurrentHashMap()
-) : GridNode {
-	override fun consolidate() {
-		return
-	}
-}
+) : GridNode
