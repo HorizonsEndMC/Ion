609bc5fc6c4599baeef54b5e6372e9a416fd4c1d
Gutin1 <Claytonpinter@gmail.com>
2024-03-30 01:50:36 -0500

    compilable

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerInputNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerInputNode.kt
@@ -1,24 +1,44 @@
 package net.horizonsend.ion.server.features.transport.node.power
 
 import net.horizonsend.ion.server.features.multiblock.entity.type.PoweredMultiblockEntity
-import net.horizonsend.ion.server.features.transport.grid.Grid
+import net.horizonsend.ion.server.features.transport.grid.PowerGrid
 import net.horizonsend.ion.server.features.transport.node.GridNode
+import net.horizonsend.ion.server.features.transport.step.Step
+import net.horizonsend.ion.server.features.transport.step.TransferStatus
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import org.bukkit.block.BlockFace
 import java.util.concurrent.ConcurrentHashMap
 
 class PowerInputNode(
-	override val parentGrid: Grid,
+	override val parentGrid: PowerGrid,
 	override val x: Int,
 	override val y: Int,
 	override val z: Int,
-	override val transferableNeighbors: ConcurrentHashMap<BlockFace, GridNode> = ConcurrentHashMap(),
-	override val multiblocks: Collection<PoweredMultiblockEntity>
 ) : GridNode, PowerNode {
+	override val transferableNeighbors: ConcurrentHashMap<BlockFace, GridNode> = ConcurrentHashMap()
+	override val multiblocks: MutableList<PoweredMultiblockEntity> = getNeighboringMultiblocks()
+
 	/**
 	 * Gets the multiblocks to which this can input power
 	 **/
-	suspend fun getMultiblocks(): Collection<PoweredMultiblockEntity> = TODO()
+	fun getNeighboringMultiblocks(): MutableList<PoweredMultiblockEntity> {
+		val multiblocks = mutableListOf<PoweredMultiblockEntity>()
+
+		for ((offsetX, offsetY, offsetZ) in offsets) {
+			val x = this.x + offsetX
+			val y = this.y + offsetY
+			val z = this.z + offsetZ
+
+			val key = toBlockKey(x, y, z)
+
+			val multi = parentGrid.poweredMultiblockEntities[key] ?: continue
+
+			multiblocks.add(multi)
+		}
+
+		return multiblocks
+	}
 
 	override fun isTransferableTo(offset: BlockFace, node: GridNode): Boolean {
 		// May only accept power
@@ -39,5 +59,11 @@ class PowerInputNode(
 			// upside down mining lasers have signs below
 			Vec3i(1, -1, 0), Vec3i(-1, -1, 0), Vec3i(0, -1, -1), Vec3i(0, -1, 1),
 		)
+	}
+
+	override fun processStep(step: Step) {
+		step.currentNode = this
+		step.status = TransferStatus.COMPLETE
+//		TODO("Not yet implemented")
 	}
 }
