8aa079a72c806fb36142897c4e34508e9a362ed2
Gutin1 <Claytonpinter@gmail.com>
2024-04-04 14:27:26 -0500

    rework recipes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/recipe/MultiblockRecipe.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/recipe/MultiblockRecipe.kt
@@ -1,23 +1,15 @@
 package net.horizonsend.ion.server.features.multiblock.recipe
 
 import net.horizonsend.ion.server.features.multiblock.Multiblock
-import net.horizonsend.ion.server.features.multiblock.recipe.ingredient.MultiblockRecipeIngredient
 import org.bukkit.block.Sign
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
-interface MultiblockRecipe<T: Multiblock> {
+interface MultiblockRecipe<out T: Multiblock> {
 	val multiblock: T
 	val result: ItemStack
 
-	fun execute(multiblock: T, sign: Sign, inventory: Inventory) // TODO redesign this on multiblock rewrite branch
+	fun matches(sign: Sign, inventory: Inventory): Boolean
 
-	fun matches(multiblock: T, sign: Sign, inventory: Inventory): Boolean
-
-	fun checkAndConsume(ingredient: MultiblockRecipeIngredient, multiblock: T, sign: Sign, inventory: Inventory): Boolean {
-		if (!ingredient.checkRequirement(multiblock, sign, inventory)) return false
-		ingredient.consume(multiblock, sign, inventory)
-
-		return true
-	}
+	fun execute(sign: Sign, inventory: Inventory) // TODO redesign this on multiblock rewrite branch
 }
