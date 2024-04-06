58312ad8d9879c18376831932a83107b6ccae913
Gutin1 <Claytonpinter@gmail.com>
2024-04-05 22:26:42 -0500

    do the rest through multi recipes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/ProgressHolderItemIngredient.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/ProgressHolderItemIngredient.kt
@@ -7,13 +7,13 @@ import net.horizonsend.ion.server.features.multiblock.Multiblock
 import org.bukkit.block.Sign
 import org.bukkit.inventory.ItemStack
 
-class ProgressHolderItemIngredient(val from: ItemIngredient, val ingredient: CustomItem) : MultiblockRecipeIngredient {
+class ProgressHolderItemIngredient(val from: ItemIngredient, val to: CustomItem) : MultiblockRecipeIngredient {
 	override fun checkRequirement(multiblock: Multiblock, sign: Sign, itemStack: ItemStack?): Boolean {
 		if (from.checkRequirement(multiblock, sign, itemStack)) return true
 
 		if (itemStack == null) return false
 		if (itemStack.customItem !is ProgressHolder) return false
 
-		return ProgressHolder.getResult(itemStack) == ingredient
+		return ProgressHolder.getResult(itemStack) == to
 	}
 }
