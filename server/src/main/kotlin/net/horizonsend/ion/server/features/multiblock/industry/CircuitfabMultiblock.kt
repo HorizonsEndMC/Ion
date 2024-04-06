b9a7bb0c4624ea689ec4e56a74f9e29d7233ea1d
Gutin1 <Claytonpinter@gmail.com>
2024-04-05 22:26:42 -0500

    do the rest through multi recipes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/industry/CircuitfabMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/industry/CircuitfabMultiblock.kt
@@ -1,20 +1,24 @@
 package net.horizonsend.ion.server.features.multiblock.industry
 
-import net.horizonsend.ion.server.features.customitems.CustomItems.CIRCUITRY
-import net.horizonsend.ion.server.features.customitems.CustomItems.ENHANCED_CIRCUITRY
-import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
-import net.horizonsend.ion.server.features.machine.PowerMachines
 import net.horizonsend.ion.server.features.multiblock.FurnaceMultiblock
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.PowerStoringMultiblock
-import org.bukkit.Material
 import org.bukkit.block.Furnace
 import org.bukkit.block.Sign
 import org.bukkit.event.inventory.FurnaceBurnEvent
 
 
 object CircuitfabMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+	override val name = "circuitfab"
+
+	override val signText = createSignText(
+		line1 = "&6Circuitfab",
+		line2 = null,
+		line3 = null,
+		line4 = null
+	)
+
 	override val maxPower = 300_000
 
 	override fun MultiblockShape.buildStructure() {
@@ -68,53 +72,11 @@ object CircuitfabMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMulti
 		}
 	}
 
-
-	override val name = "circuitfab"
-
-	override val signText = createSignText(
-		line1 = "&6Circuitfab",
-		line2 = null,
-		line3 = null,
-		line4 = null
-	)
-
 	override fun onFurnaceTick(
 		event: FurnaceBurnEvent,
 		furnace: Furnace,
 		sign: Sign,
 	) {
-		event.isBurning = false
-		event.burnTime = 200
-		event.isCancelled = false
-		furnace.cookSpeedMultiplier = 0.00277777777 // TODO: improve implementation after multiblock rewrite
-
-		val smelting = furnace.inventory.smelting
-		val fuel = furnace.inventory.fuel
-		val result = furnace.inventory.result
-
-		if (PowerMachines.getPower(sign) <= 100000 ||
-			smelting == null ||
-			smelting.type != Material.PRISMARINE_CRYSTALS ||
-			fuel == null
-		) {
-			furnace.cookTime = 0
-			event.isCancelled = true
-			return
-		}
-
-		if (fuel.customItem != CIRCUITRY) {
-			furnace.cookTime = 0
-			event.isCancelled = true
-			return
-		}
-
-		// Produce new item if it is not the first burn event
-		if (furnace.cookTime >= 200) {
-			fuel.subtract(1)
-			if (result == null) furnace.inventory.result = ENHANCED_CIRCUITRY.constructItemStack()
-			else result.add(1)
-			PowerMachines.removePower(sign, 100000)
-		}
-		furnace.cookTime = 0
+		handleRecipe(this, event, furnace, sign)
 	}
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/industry/FabricatorMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/industry/FabricatorMultiblock.kt
@@ -1,24 +1,24 @@
 package net.horizonsend.ion.server.features.multiblock.industry
 
-import net.horizonsend.ion.server.features.customitems.CustomItems.FABRICATED_ASSEMBLY
-import net.horizonsend.ion.server.features.customitems.CustomItems.FUEL_CELL
-import net.horizonsend.ion.server.features.customitems.CustomItems.FUEL_ROD_CORE
-import net.horizonsend.ion.server.features.customitems.CustomItems.REACTIVE_ASSEMBLY
-import net.horizonsend.ion.server.features.customitems.CustomItems.REINFORCED_FRAME
-import net.horizonsend.ion.server.features.customitems.CustomItems.STEEL_ASSEMBLY
-import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
-import net.horizonsend.ion.server.features.machine.PowerMachines
 import net.horizonsend.ion.server.features.multiblock.FurnaceMultiblock
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.PowerStoringMultiblock
-import org.bukkit.Material
 import org.bukkit.block.Furnace
 import org.bukkit.block.Sign
 import org.bukkit.event.inventory.FurnaceBurnEvent
 
 
 object FabricatorMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+	override val name = "fabricator"
+
+	override val signText = createSignText(
+		line1 = "&8Fabricator",
+		line2 = null,
+		line3 = null,
+		line4 = null
+	)
+
 	override val maxPower = 300_000
 
 	override fun MultiblockShape.buildStructure() {
@@ -98,87 +98,11 @@ object FabricatorMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMulti
 		}
 	}
 
-	override val name = "fabricator"
-
-	override val signText = createSignText(
-		line1 = "&8Fabricator",
-		line2 = null,
-		line3 = null,
-		line4 = null
-	)
-
 	override fun onFurnaceTick(
 		event: FurnaceBurnEvent,
 		furnace: Furnace,
 		sign: Sign,
 	) {
-		event.isBurning = false
-		event.burnTime = 200
-		event.isCancelled = false
-		furnace.cookSpeedMultiplier = 0.00138888888 // TODO: improve implementation after multiblock rewrite
-
-		val smelting = furnace.inventory.smelting
-		val fuel = furnace.inventory.fuel
-		val result = furnace.inventory.result
-
-		if (PowerMachines.getPower(sign) <= 100000 ||
-			smelting == null ||
-			smelting.type != Material.PRISMARINE_CRYSTALS ||
-			fuel == null
-		) {
-			furnace.cookTime = 0
-			event.isCancelled = true
-			return
-		}
-
-		if (furnace.cookTime >= 200) {
-			when (fuel.customItem) {
-				FUEL_ROD_CORE -> {
-					event.isCancelled = false
-					if (result == null) furnace.inventory.result = FUEL_CELL.constructItemStack()
-					else if (result.customItem == FUEL_CELL) result.add(1)
-					else {
-						furnace.cookTime = 0
-						event.isCancelled = true
-						return
-					}
-					fuel.subtract(1)
-					PowerMachines.removePower(sign, 100000)
-				}
-
-				REACTIVE_ASSEMBLY -> {
-					event.isCancelled = false
-					if (result == null) furnace.inventory.result = FABRICATED_ASSEMBLY.constructItemStack()
-					else if (result.customItem == FABRICATED_ASSEMBLY) result.add(1)
-					else {
-						furnace.cookTime = 0
-						event.isCancelled = true
-						return
-					}
-					fuel.subtract(1)
-					PowerMachines.removePower(sign, 300)
-				}
-
-				STEEL_ASSEMBLY -> {
-					event.isCancelled = false
-					if (result == null) furnace.inventory.result = REINFORCED_FRAME.constructItemStack()
-					else if (result.customItem == REINFORCED_FRAME) result.add(1)
-					else {
-						furnace.cookTime = 0
-						event.isCancelled = true
-						return
-					}
-					fuel.subtract(1)
-					PowerMachines.removePower(sign, 300)
-				}
-
-				else -> {
-					furnace.cookTime = 0
-					event.isCancelled = true
-					return
-				}
-			}
-		}
-		furnace.cookTime = 0
+		handleRecipe(this, event, furnace, sign)
 	}
 }
