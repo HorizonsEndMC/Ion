3608f2d3c96729104cda97a48a55c5a68367e885
CrystalzzzDeez <billcypher269@gmail.com>
2024-04-12 23:41:11 +0100

    sigmaligmasugma

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/IonTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/IonTurretWeaponSubsystem.kt
@@ -1,12 +1,15 @@
 package net.horizonsend.ion.server.features.starship.subsystem.weapon.primary
 
 import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.customitems.CustomItems
 import net.horizonsend.ion.server.features.multiblock.starshipweapon.turret.IonTurretMultiblock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.damager.Damager
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.TurretWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.AmmoConsumingWeaponSubsystem
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.block.BlockFace
+import org.bukkit.inventory.ItemStack
 import org.bukkit.util.Vector
 import java.util.concurrent.TimeUnit
 
@@ -16,7 +19,7 @@ class IonTurretWeaponSubsystem(
 		pos: Vec3i,
 		face: BlockFace,
 		override val multiblock: IonTurretMultiblock
-) : TurretWeaponSubsystem(ship, pos, face) {
+) : TurretWeaponSubsystem(ship, pos, face), AmmoConsumingWeaponSubsystem {
 	override val balancing: StarshipWeapons.StarshipWeapon = starship.balancing.weapons.ionTurret
 
 	override val inaccuracyRadians: Double get() = Math.toRadians(balancing.inaccuracyRadians)
@@ -29,5 +32,8 @@ class IonTurretWeaponSubsystem(
 			target: Vector
 	) {
 		multiblock.shoot(starship.world, pos, face, dir, starship, shooter, false)
+	}
+	override fun getRequiredAmmo(): ItemStack {
+		return CustomItems.LOADED_TURRET_SHELL.constructItemStack()
 	}
 }
