83b255ce61b22f248db0282fc0bc0142b4c30a52
kwazedilla <kwazedilla@gmail.com>
2024-04-12 21:42:00 -0700

    re-add random block auto turret targeting

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/AutoTurretTargeting.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/AutoTurretTargeting.kt
@@ -10,7 +10,20 @@ import org.bukkit.entity.Player
 
 object AutoTurretTargeting : IonServerComponent() {
 	sealed class TargetType<T>(val get: (String) -> Location?) {
-		data object PlayerTarget: TargetType<Player>({ name -> Bukkit.getPlayer(name)?.location })
+		data object PlayerTarget: TargetType<Player>({ name ->
+
+			val player = Bukkit.getPlayer(name)
+			if (player != null) {
+				val starship = ActiveStarships.findByPilot(player)
+				if (starship != null) {
+					starship.blocks.random()?.let { Vec3i(it).toLocation(starship.world) }
+				} else {
+					Bukkit.getPlayer(name)?.location
+				}
+			} else {
+				Bukkit.getPlayer(name)?.location
+			}
+		})
 		data object StarshipTarget: TargetType<ActiveStarship>({ identifier ->
 			val starship = ActiveStarships[identifier]
 
