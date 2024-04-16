30c0d690d282cc795d09ba094ca615ad9827c189
Gutin1 <Claytonpinter@gmail.com>
2024-04-16 10:53:32 -0500

    delete unused / useless modules

--- server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/misc/DespawnModule.kt
+++
@@ -1,6 +0,0 @@
-package net.horizonsend.ion.server.features.ai.module.misc
-
-import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
-
-class DespawnModule(controller: AIController) : net.horizonsend.ion.server.features.ai.module.AIModule(controller) {
-}
