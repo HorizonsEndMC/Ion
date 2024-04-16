89f28be1a781c2fd0d211f79b62babceb338aaac
Gutin1 <Claytonpinter@gmail.com>
2024-04-16 12:04:13 -0500

    revert cruising change & ion turret fix

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/thruster/ThrusterSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/thruster/ThrusterSubsystem.kt
@@ -9,6 +9,8 @@ import org.bukkit.block.BlockFace
 /** thruster subsystem representing a single thruster, the facing direction being forward */
 class ThrusterSubsystem(starship: ActiveStarship, pos: Vec3i, override var face: BlockFace, val type: ThrusterType) :
 	StarshipSubsystem(starship, pos), DirectionalSubsystem {
+	var lastIonTurretLimited: Long = 0L
+
 	override fun isIntact(): Boolean = type.matchesStructure(starship, pos.x, pos.y, pos.z, face)
 
 	// TODO : remove automatically when the block gets destroyed
