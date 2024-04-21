ec51aa9bcdcb8a0a18317340acf7d36cd488e954
Sciath <67932943+SciathGH@users.noreply.github.com>
2024-04-21 23:39:41 +0100

    Bottom arsenal missile, /targetPosition, and missile trajectory

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/starshipweapon/heavy/ArsenalRocketStarshipWeaponMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/starshipweapon/heavy/ArsenalRocketStarshipWeaponMultiblock.kt
@@ -7,91 +7,115 @@ import net.horizonsend.ion.server.features.starship.subsystem.weapon.secondary.A
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.block.BlockFace
 
-object ArsenalRocketStarshipWeaponMultiblock : SignlessStarshipWeaponMultiblock<ArsenalRocketStarshipWeaponSubsystem>() {
+sealed class ArsenalRocketStarshipWeaponMultiblock :
+	SignlessStarshipWeaponMultiblock<ArsenalRocketStarshipWeaponSubsystem>() {
+	override fun createSubsystem(
+		starship: ActiveStarship,
+		pos: Vec3i,
+		face: BlockFace
+	): ArsenalRocketStarshipWeaponSubsystem {
+		return ArsenalRocketStarshipWeaponSubsystem(starship, pos, getAdjustedDirection(), this)
+	}
+
+	protected abstract fun getAdjustedDirection(): BlockFace
+}
+
+sealed class VerticalArsenalStarshipWeaponMultiblock : ArsenalRocketStarshipWeaponMultiblock() {
+	protected abstract fun getYFactor(): Int
+
 	override fun MultiblockShape.buildStructure() {
-		z(-1){
-			y(+0){
+		val yFactor = getYFactor()
+
+		z(-1) {
+			y(+0 * yFactor) {
 				x(+1).anyStairs()
 				x(0).noteBlock()
 				x(-1).anyStairs()
 			}
-			y(1){
+			y(1 * yFactor) {
 				x(0).sponge()
 			}
-			y(2){
+			y(2 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(3){
+			y(3 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(4){
+			y(4 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(5){
+			y(5 * yFactor) {
 				x(-1).anyStairs()
 				x(0).ironBlock()
 				x(1).anyStairs()
 			}
 		}
-		z(0){
-			y(0){
+		z(0) {
+			y(0 * yFactor) {
 				x(1).ironBlock()
 				x(0).sponge()
 				x(-1).ironBlock()
 			}
-			y(1){
+			y(1 * yFactor) {
 				x(0).sponge()
 				x(1).sponge()
 				x(-1).sponge()
 			}
-			y(2){
+			y(2 * yFactor) {
 				x(0).sponge()
 				x(-1).titaniumBlock()
 				x(1).titaniumBlock()
 			}
-			y(3){
+			y(3 * yFactor) {
 				x(0).sponge()
 				x(-1).titaniumBlock()
 				x(1).titaniumBlock()
 			}
-			y(4){
+			y(4 * yFactor) {
 				x(0).dispenser()
 				x(-1).titaniumBlock()
 				x(1).titaniumBlock()
 			}
-			y(5){
+			y(5 * yFactor) {
 				x(-1).ironBlock()
 				x(1).ironBlock()
 			}
 		}
-		z(1){
-			y(0){
+		z(1) {
+			y(0 * yFactor) {
 				x(1).anyStairs()
 				x(0).ironBlock()
 				x(-1).anyStairs()
 			}
-			y(1){
+			y(1 * yFactor) {
 				x(0).sponge()
 			}
-			y(2){
+			y(2 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(3){
+			y(3 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(4){
+			y(4 * yFactor) {
 				x(0).titaniumBlock()
 			}
-			y(5){
+			y(5 * yFactor) {
 				x(1).anyStairs()
 				x(0).ironBlock()
 				x(-1).anyStairs()
 			}
 		}
-
 	}
+}
 
-	override fun createSubsystem(starship: ActiveStarship, pos: Vec3i, face: BlockFace): ArsenalRocketStarshipWeaponSubsystem {
-	return ArsenalRocketStarshipWeaponSubsystem(starship,pos,face, this)
-	}
+object TopArsenalStarshipWeaponMultiblock : VerticalArsenalStarshipWeaponMultiblock() {
+	override fun getYFactor() = 1
+
+	override fun getAdjustedDirection(): BlockFace = BlockFace.UP
+}
+
+object BottomArsenalStarshipWeaponMultiblock : VerticalArsenalStarshipWeaponMultiblock() {
+	override fun getYFactor() = -1
+
+	override fun getAdjustedDirection(): BlockFace = BlockFace.DOWN
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/DisplayEntityProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/DisplayEntityProjectile.kt
@@ -1,121 +1,78 @@
 package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
 
 import com.mojang.math.Transformation
-import net.horizonsend.ion.common.utils.miscellaneous.d
+import io.netty.buffer.Unpooled
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities
-import net.horizonsend.ion.server.features.nations.gui.item
-import net.horizonsend.ion.server.miscellaneous.utils.minecraft
-import net.horizonsend.ion.server.miscellaneous.utils.toBlockPos
-import net.horizonsend.ion.server.miscellaneous.utils.vectorToPitchYaw
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
+import net.minecraft.network.FriendlyByteBuf
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
-import net.minecraft.network.syncher.EntityDataAccessor
-import net.minecraft.network.syncher.EntityDataSerializers
-import net.minecraft.network.syncher.SynchedEntityData.DataValue
+import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket
 import net.minecraft.world.entity.Display
-import net.minecraft.world.entity.EntityType
 import org.bukkit.Bukkit
 import org.bukkit.Location
-import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer
-import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack
+import org.bukkit.entity.Player
 import org.bukkit.util.Vector
-import org.joml.Matrix4f
 import org.joml.Quaternionf
 import org.joml.Vector3f
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
 
 
 interface DisplayEntityProjectile {
-	var displayEntity: Display.ItemDisplay?
+	var displayEntities: MutableMap<Player, Display.ItemDisplay?>
 
 	val originLocation: Location //The position where the displayEntity was spawned, this is used in the translation maths
 
 	var oldLocation: Location
 
+	var scale: Vector3f
+
 	fun updateDisplayEntity(newLocation: Location, velocity: Vector) {
-		velocity.multiply(3)
-		//Ripped from https://github.com/PimvanderLoos/AnimatedArchitecture/blob/master/animatedarchitecture-spigot/spigot-core/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/core/animation/BlockDisplayHelper.java#L66
-		//The code I stole this from used its own vector thing called a rotation, but this should work fine
-		val (pitch, yaw) = vectorToPitchYaw(velocity.clone())
-		var differencePitch: Double =
-			(toMinecraftAngle(getPitchBetweenLocations(originLocation, newLocation).toDouble() - pitch))//Idek
-		var differenceYaw: Double = (newLocation.yaw - yaw).toDouble()
+		for (player in Bukkit.getServer().onlinePlayers.filter { it.world == newLocation.world }) {
+			val displayEntity = displayEntities[player as CraftPlayer] ?: continue
+			//if (player.location.distance(Location(newLocation.world ,displayEntity.x, displayEntity.y, displayEntity.y)) > 100.0){
+			//	val buf = FriendlyByteBuf(Unpooled.buffer())
+			//	buf.writeVarInt(displayEntity.id)
+			//	buf.writeDouble(newLocation.x)
+			//	buf.writeDouble(newLocation.y)
+			//	buf.writeDouble(newLocation.z)
+			//	buf.writeBoolean(false)
+			//	val teleportEntityPacket = ClientboundTeleportEntityPacket(buf)
+			//	player.handle.connection.send(teleportEntityPacket)
+			//	displayEntity.entityData.refresh(player.handle)
+			//}
 
-		//We get the difference in angle, no idea why, but we dont use roll, the code I stole this` from did use roll, should work tho
-		//the values we get are between 0-360, we need them to be between -180 to 180
-		//Quaternions dont use a point in space that they try rotate too, they use pitch and yaw to figure out how much to rotate by
-		//thats why its important to supply the difference in pitch and yaw, as thats how much we want to rotate it by
-		differencePitch = -toMinecraftAngle(differencePitch) //minus because for some ungodly reason it was inverted
-		differenceYaw = toMinecraftAngle(differenceYaw)
+			//We multiply the velocity to pretend we're interpolating for 3 ticks, to make it smooth
+			velocity.multiply(3)
+			//Get the translation needed from the originalLocation.?.clone()
+			val translation =
+				newLocation.toVector().subtract(Vector(displayEntity.x, displayEntity.y, displayEntity.z))
+					.toVector3f()
+			val invertedVector = velocity.clone().multiply(-1).toVector3f().normalize()
+			val transformation = Transformation(
+				translation,
+				ClientDisplayEntities.rotateToFaceVector(invertedVector),
+				scale,
+				Quaternionf()
+			) //Set the new transformation
 
-		//We can only use radians
-		val differencePitchRadians = Math.toRadians(differencePitch).toFloat()
-		val differenceYawRadians = Math.toRadians(differenceYaw).toFloat()
+			displayEntity.setTransformation(transformation)
+			displayEntity.transformationInterpolationDuration = 3
+			val packet = ClientboundSetEntityDataPacket(
+				displayEntity.id,
+				displayEntity.entityData.packDirty()!!
+			)
 
-		//Couldnt tell you wtf is happening here, some archaic magic anyways
-		val transformation = Matrix4f().rotate(fromPitchYaw(differencePitchRadians, differenceYawRadians))
-		//Get the translation needed from the originalLocation
-		val translation = newLocation.toVector().subtract(originLocation.toVector()).toVector3f()
-			.sub(transformation.getTranslation(Vector3f())) //Get how much we need to offset the displayEntity, and do some archaic magic with the transformation
-		val invertedVector = velocity.clone().multiply(-1).toVector3f().normalize()
-		val trans = Transformation(
-			translation,
-			ClientDisplayEntities.rotateToFaceVector(invertedVector),
-			Vector3f(1.0f, 1.0f, 2.0f),
-			Quaternionf()
-		) //Set the new transformation
-
-		displayEntity?.setTransformation(trans)
-		displayEntity?.transformationInterpolationDuration = 3
-		val packet = ClientboundSetEntityDataPacket(displayEntity?.id ?: return,
-			displayEntity?.entityData?.packDirty()!!
-		)
-
-		Bukkit.getServer().onlinePlayers.forEach {
-			(it as CraftPlayer).handle.connection.send(packet)
-			displayEntity?.entityData?.refresh(it.handle)
+			player.handle.connection.send(packet)
+			displayEntity.entityData.refresh(player.handle)
 		}
-	}
-
-	fun fromPitchYaw(pitch: Float, yaw: Float): Quaternionf = Quaternionf().rotateY(yaw).rotateX(pitch)
-	fun toMinecraftAngle(angle: Double): Double {
-		var angleToUse = angle
-		angleToUse %= 360.0
-		if (angleToUse > 180) angleToUse -= 360.0
-		if (angleToUse < -180) angleToUse += 360.0
-		return angleToUse
-	}
-
-	fun getPitchBetweenLocations(from: Location, to: Location): Float {
-		val direction = to.toVector().subtract(from.toVector())
-		val horizontalDistance =
-			sqrt(direction.x * direction.x + direction.z * direction.z)
-		return Math.toDegrees(atan2(direction.y, horizontalDistance)).toFloat()
 	}
 
 	fun destroyDisplayEntity() {
-		val packet = ClientboundRemoveEntitiesPacket(displayEntity?.id ?: return)
-		Bukkit.getServer().onlinePlayers.forEach { (it as CraftPlayer).handle.connection.send(packet) }
-	}
-
-	fun makeDisplayEntity() {
-		displayEntity?.lerpHeadTo(0f, 0)
-		for (playerBukkit in Bukkit.getServer().onlinePlayers) {
-			val player = (playerBukkit as CraftPlayer)
-			val connection = player.handle.connection
-			val itemDisplay = Display.ItemDisplay(EntityType.ITEM_DISPLAY, player.minecraft.level()).apply {
-				this.itemStack = CraftItemStack.asNMSCopy(item(Material.DRAGON_HEAD))
-				setPos(originLocation.toBlockPos().center)
-			}
-			this.displayEntity = itemDisplay
-
-			connection.send(ClientboundAddEntityPacket(itemDisplay))
-			itemDisplay.entityData.refresh(player.handle)
+		for ((player, displayEntity) in displayEntities) {
+			val packet = ClientboundRemoveEntitiesPacket(displayEntity?.id ?: return)
+			(player as CraftPlayer).handle.connection.send(packet)
 		}
 	}
+
+	fun makeDisplayEntities() {}
 }
