757315049458adf3aaee2468df09e275a5029608
s1ice <alexwlefebvre@gmail.com>
2024-04-24 23:45:50 -0700

    allow multiple items to be searched at once

--- server/src/main/kotlin/net/horizonsend/ion/server/features/client/whereisit/SearchCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/client/whereisit/SearchCommand.kt
@@ -4,75 +4,44 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
-import co.aikar.commands.annotation.Optional
-import co.aikar.commands.annotation.Subcommand
 import net.horizonsend.ion.common.extensions.userError
+import net.horizonsend.ion.server.command.GlobalCompletions
+import net.horizonsend.ion.server.command.SLCommand
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.highlightBlock
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.sendEntityPacket
 import net.horizonsend.ion.server.features.client.whereisit.mod.Searcher
-import net.horizonsend.ion.server.features.customitems.CustomItems
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.minecraft
 import net.horizonsend.ion.server.miscellaneous.utils.toVec3i
-import org.bukkit.Material
+import net.minecraft.core.BlockPos
 import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemStack
 
 @CommandAlias("itemsearch")
 @CommandPermission("ion.search")
-object SearchCommand : net.horizonsend.ion.server.command.SLCommand() {
+object SearchCommand : SLCommand() {
 	@Default
-	fun default(
-		player: Player,
-		@Optional material: Material?
-	) = searchAndSend(player, if (material != null) ItemStack(material) else player.inventory.itemInMainHand)
-
-	@Subcommand("custom")
-	@CommandCompletion("@customItem")
-	fun custom(
-		player: Player,
-		customItem: String
-	) {
-		val itemStack = CustomItems.getByIdentifier(customItem)?.constructItemStack() ?: run {
-			player.userError("Can't find item $customItem!")
-			return
+	@CommandCompletion("@anyItem")
+	fun default( player: Player, vararg itemStrList: String) = Tasks.async {
+		val strList = itemStrList.toList()
+		val locationSet = mutableSetOf<BlockPos>()
+		for(str in strList){
+			val item = GlobalCompletions.stringToItem(str) ?: player.inventory.itemInMainHand
+			val res = Searcher.searchItemStack(player, item)
+			for (pos in res.keys) {
+				sendEntityPacket(player, highlightBlock(player.world.minecraft, pos.toVec3i()), 10 * 20)
+				res.mapTo(locationSet) { it.key }
+			}
 		}
-
-		searchAndSend(player, itemStack)
-	}
-
-	@Subcommand("legacy")
-	@CommandCompletion("@customitems")
-	fun legacy(
-		player: Player,
-		customItem: String
-	) {
-		val itemStack = net.horizonsend.ion.server.miscellaneous.registrations.legacy.CustomItems[customItem]?.itemStack(1) ?: run {
-			player.userError("Can't find item $customItem!")
-			return
-		}
-
-		searchAndSend(player, itemStack)
-	}
-
-	private fun searchAndSend(player: Player, stack: ItemStack) = Tasks.async {
-		val res = Searcher.searchItemStack(
-			player,
-			stack
-		)
-
-		if (res.isEmpty()) {
+		if(itemStrList.isEmpty()){
+			player.userError("Specify which item to search.")
+		}else if (locationSet.size == 0) {
 			player.userError("Couldn't find any item in a 20 block range.")
 			return@async
+		} else {
+			player.sendRichMessage(
+				"<hover:show_text:\"${locationSet.joinToString("\n") { "<gray>X: ${it.x} Y: ${it.y} Z: ${it.z}" }}\">" +
+				"<#7f7fff>Found ${locationSet.size} containers. [Hover]"
+			)
 		}
-
-		for (pos in res.keys) {
-			sendEntityPacket(player, highlightBlock(player.world.minecraft, pos.toVec3i()), 10 * 20)
-		}
-
-		player.sendRichMessage(
-			"<hover:show_text:\"${res.keys.joinToString("\n") { "<gray>X: ${it.x} Y: ${it.y} Z: ${it.z}" }}\">" +
-				"<#7f7fff>Found ${res.size} containers. [Hover]"
-		)
 	}
 }
