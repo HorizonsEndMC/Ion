9a010fe03c432190e4762eb5b621ec8c53166877
kwazedilla <kwazedilla@gmail.com>
2024-04-27 17:04:38 -0700

    SoundResult, onComplete action for ProgressItemResult, more sound examples

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/result/ProgressItemResult.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/result/ProgressItemResult.kt
@@ -12,7 +12,7 @@ import org.bukkit.inventory.ItemStack
 /**
  * This item will convert into the result custom item after the amount of ticks provided (Assuming no lag and a 200 tick interval on the furnace)
  **/
-class ProgressItemResult<R: CustomItem>(val result: R, time: Long) : ActionResult {
+class ProgressItemResult<R: CustomItem>(val result: R, time: Long, private val onComplete: ActionResult? = null) : ActionResult {
 	// 200 is the standard furnace tick interval
 	private val increment = 200.0 / time.toDouble()
 
@@ -22,19 +22,20 @@ class ProgressItemResult<R: CustomItem>(val result: R, time: Long) : ActionResul
 		val smelting = craftingInventory.smelting ?: return
 
 		if (smelting.customItem is ProgressHolder) {
-			incrementProgress(smelting, craftingInventory)
+			incrementProgress(smelting, craftingInventory, recipe, craftingInventory, sign)
 			return
 		}
 
 		craftingInventory.smelting = ProgressHolder.create(result)
 	}
 
-	private fun incrementProgress(progressItem: ItemStack, inventory: FurnaceInventory) {
+	private fun incrementProgress(progressItem: ItemStack, inventory: FurnaceInventory, recipe: MultiblockRecipe<*>, craftingInventory: Inventory, sign: Sign) {
 		val current = ProgressHolder.getProgress(progressItem)
 
 		if (ProgressHolder.setProgress(progressItem, current + increment)) {
 			inventory.result = inventory.smelting
 			inventory.smelting = null
+			onComplete?.execute(recipe, craftingInventory, sign) ?: return
 		}
 	}
 }
