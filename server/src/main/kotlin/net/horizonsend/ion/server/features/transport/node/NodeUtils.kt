827ebe8906b6eacfd9c5cfbe9c00d221ec9a52e5
Gutin1 <Claytonpinter@gmail.com>
2024-04-28 22:36:07 -0500

    continued

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeUtils.kt
@@ -0,0 +1,26 @@
+package net.horizonsend.ion.server.features.transport.node
+
+import net.horizonsend.ion.server.features.transport.node.nodes.TransportNode
+import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
+import net.horizonsend.ion.server.miscellaneous.utils.associateWithNotNull
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import org.bukkit.block.BlockFace
+
+fun getNeighborNodes(position: Long, nodes: Map<Long, TransportNode>, checkFaces: Set<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.associateWithNotNull {
+	val x = getX(position)
+	val y = getY(position)
+	val z = getZ(position)
+
+	nodes[toBlockKey(x + it.modX, y + it.modY, z + it.modZ)]
+}
+
+fun getNeighborNodes(position: Long, nodes: Collection<Long>, checkFaces: Set<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.mapNotNull {
+	val x = getX(position)
+	val y = getY(position)
+	val z = getZ(position)
+
+	toBlockKey(x + it.modX, y + it.modY, z + it.modZ).takeIf { key -> nodes.contains(key) }
+}
