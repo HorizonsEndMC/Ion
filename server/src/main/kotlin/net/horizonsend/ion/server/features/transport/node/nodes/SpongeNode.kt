41c6bb6594355a3a8541496528f0e46ff5bd868f
Gutin1 <Claytonpinter@gmail.com>
2024-04-29 12:18:59 -0500

    node serialization / deserialization

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/nodes/SpongeNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/nodes/SpongeNode.kt
@@ -1,25 +1,39 @@
-package net.horizonsend.ion.server.features.transport.node.power
+package net.horizonsend.ion.server.features.transport.node.nodes
 
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 import net.horizonsend.ion.server.features.transport.node.getNeighborNodes
-import net.horizonsend.ion.server.features.transport.node.nodes.MultiNode
-import net.horizonsend.ion.server.features.transport.node.nodes.TransportNode
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_COVERED_POSITIONS
 import net.horizonsend.ion.server.miscellaneous.utils.ALL_DIRECTIONS
 import org.bukkit.block.BlockFace
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
 
 /**
  * Represents a sponge [omnidirectional pipe]
  *
  * Since there is no use in keeping the individual steps, all touching sponges are consolidated into a single node with multiple inputs / outputs, weighted evenly
  **/
-class SpongeNode(origin: Long) : MultiNode {
+class SpongeNode() : MultiNode {
+	constructor(origin: Long) : this() {
+		positions.add(origin)
+	}
+
 	override val positions: MutableSet<Long> = LongOpenHashSet()
 
 	override val transferableNeighbors: MutableSet<TransportNode> = ObjectOpenHashSet()
 
 	override fun isTransferable(position: Long, node: TransportNode): Boolean {
 		return true
+	}
+
+	override fun loadData(persistentDataContainer: PersistentDataContainer) {
+		val coveredPositions = persistentDataContainer.get(NODE_COVERED_POSITIONS, PersistentDataType.LONG_ARRAY)
+		coveredPositions?.let { positions.addAll(it.asIterable()) }
+	}
+
+	override fun storeData(persistentDataContainer: PersistentDataContainer) {
+		persistentDataContainer.set(NODE_COVERED_POSITIONS, PersistentDataType.LONG_ARRAY, positions.toLongArray())
 	}
 
 	override fun shouldSplit(position: Long, nodes: MutableMap<Long, TransportNode>): Boolean {
@@ -35,10 +49,7 @@ class SpongeNode(origin: Long) : MultiNode {
 		// All 26 adjacent directions
 		val adjacentNeighborPositions = getNeighborNodes(position, positions, ALL_DIRECTIONS)
 
-		if (adjacentNeighborPositions)
-	}
-
-	init {
-		positions.add(origin)
+//		if (adjacentNeighborPositions)
+		return false
 	}
 }
