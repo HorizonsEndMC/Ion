4762b36f70af1d47c35474a7513fe62fb38b29fd
Gutin1 <Claytonpinter@gmail.com>
2024-04-29 12:18:59 -0500

    node serialization / deserialization

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
@@ -1,7 +1,26 @@
 package net.horizonsend.ion.server.features.transport.node
 
-enum class NodeType {
-	ORIGIN,
-	INTERMEDIATE,
-	DESTINATION
+import com.manya.pdc.base.EnumDataType
+import net.horizonsend.ion.server.features.transport.node.nodes.SpongeNode
+import net.horizonsend.ion.server.features.transport.node.nodes.TransportNode
+
+enum class NodeType(val clazz: Class<out TransportNode>) {
+	//POWER
+	SPONGE_NODE(SpongeNode::class.java),
+
+	//GAS
+
+	//ITEM
+	;
+
+	fun newInstance(): TransportNode {
+		return clazz.getDeclaredConstructor().newInstance()
+	}
+
+	companion object {
+		val type = EnumDataType(NodeType::class.java)
+		private val byNode: Map<Class<out TransportNode>, NodeType> = entries.associateBy { it.clazz }
+
+		operator fun get(node: TransportNode): NodeType = byNode[node.javaClass] ?: throw NoSuchElementException("Unregistered node type ${node.javaClass.simpleName}!")
+	}
 }
