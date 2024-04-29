77569b604231a2eb38fded9ab2f9c5cdfecd2e41
Gutin1 <Claytonpinter@gmail.com>
2024-04-29 14:38:20 -0500

    storing/ loading of node data, rename grids to networks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
@@ -3,8 +3,8 @@ package net.horizonsend.ion.server.features.transport.node.power
 import kotlinx.coroutines.runBlocking
 import net.horizonsend.ion.server.features.multiblock.util.getBlockSnapshotAsync
 import net.horizonsend.ion.server.features.transport.container.ResourceContainer
-import net.horizonsend.ion.server.features.transport.grid.Grid
-import net.horizonsend.ion.server.features.transport.grid.PowerGrid
+import net.horizonsend.ion.server.features.transport.grid.ChunkPowerNetwork
+import net.horizonsend.ion.server.features.transport.grid.TransportNetwork
 import net.horizonsend.ion.server.features.transport.node.ExtractorNode
 import net.horizonsend.ion.server.features.transport.node.GridNode
 import net.horizonsend.ion.server.features.transport.step.PowerStep
@@ -15,12 +15,12 @@ import org.bukkit.World
 import org.bukkit.block.BlockFace
 
 class PowerExtractorNode(
-	override val parentGrid: PowerGrid,
+	override val parentTransportNetwork: ChunkPowerNetwork,
 	x: Int,
 	y: Int,
 	z: Int,
-	val solarPanel: Boolean = isSolarPanel(parentGrid, x, y, z)
-) : ExtractorNode<NamespacedKey>(parentGrid, x, y, z) {
+	val solarPanel: Boolean = isSolarPanel(parentTransportNetwork, x, y, z)
+) : ExtractorNode<NamespacedKey>(parentTransportNetwork, x, y, z) {
 	var lastTicked: Long = System.currentTimeMillis()
 
 	override fun getExtractableInventories(): Collection<ResourceContainer<NamespacedKey>> {
@@ -47,7 +47,7 @@ class PowerExtractorNode(
 	}
 
 	private fun getPower(delta: Double): Int {
-		val environment = parentGrid.world.environment
+		val environment = parentTransportNetwork.world.environment
 
 		val power = 5.0 / if (environment == World.Environment.NORMAL) 1.0 else 2.0
 
@@ -62,7 +62,7 @@ class PowerExtractorNode(
 		val direction = transferableNeighbors.keys().toList().random()
 
 		return PowerStep(
-			grid = parentGrid,
+			transportNetwork = parentTransportNetwork,
 			origin = null,
 			direction = direction,
 			this
@@ -70,9 +70,9 @@ class PowerExtractorNode(
 	}
 
 	companion object {
-		fun isSolarPanel(parentGrid: Grid, x: Int, y: Int, z: Int): Boolean = runBlocking {
-			val aboveOne = getBlockSnapshotAsync(parentGrid.world, x, y + 1, z, false)
-			val aboveTwo = getBlockSnapshotAsync(parentGrid.world, x, y + 2, z, false)
+		fun isSolarPanel(parentTransportNetwork: TransportNetwork, x: Int, y: Int, z: Int): Boolean = runBlocking {
+			val aboveOne = getBlockSnapshotAsync(parentTransportNetwork.world, x, y + 1, z, false)
+			val aboveTwo = getBlockSnapshotAsync(parentTransportNetwork.world, x, y + 2, z, false)
 
 			return@runBlocking aboveOne?.type == Material.DIAMOND_BLOCK && aboveTwo?.type == Material.DAYLIGHT_DETECTOR
 		}
