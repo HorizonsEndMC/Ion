be0bd19f9e6c10a648af5c9e02439bf8bfb4f20d
Gutin1 <Claytonpinter@gmail.com>
2024-04-30 14:23:03 -0500

    Reduce data duplication, simplification

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/ExtractorNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/ExtractorNode.kt
@@ -1,12 +1,12 @@
 package net.horizonsend.ion.server.features.transport.node
 
 import net.horizonsend.ion.server.features.transport.container.ResourceContainer
-import net.horizonsend.ion.server.features.transport.grid.TransportNetwork
+import net.horizonsend.ion.server.features.transport.grid.ChunkTransportNetwork
 import org.bukkit.block.BlockFace
 import java.util.concurrent.ConcurrentHashMap
 
 abstract class ExtractorNode<T>(
-	override val parentTransportNetwork: TransportNetwork,
+	override val parentTransportNetwork: ChunkTransportNetwork,
 	override val x: Int,
 	override val y: Int,
 	override val z: Int,
