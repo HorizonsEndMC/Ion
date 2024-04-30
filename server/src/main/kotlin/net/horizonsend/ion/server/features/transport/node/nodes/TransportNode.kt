56c62a75d50d34a6e344ac96812a04771fba8fa0
Gutin1 <Claytonpinter@gmail.com>
2024-04-30 13:29:36 -0500

    Revert "better(ish) deserialization"
    This reverts commit 1e9e69d350683fb0b9e882c62653ff802aa793e6.

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/nodes/TransportNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/nodes/TransportNode.kt
@@ -2,19 +2,17 @@ package net.horizonsend.ion.server.features.transport.node.nodes
 
 import kotlinx.serialization.SerializationException
 import net.horizonsend.ion.server.features.transport.node.NodeType
-import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_DATA
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_TYPE
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.PDCSerializable
 import org.bukkit.persistence.PersistentDataAdapterContext
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
-import kotlin.reflect.KClass
 
 /**
- * Represents a single node, or step, in transport transportNetwork
+ * Represents a single node, or step, in a transport transportNetwork
  **/
 interface TransportNode : PDCSerializable<TransportNode, TransportNode.Companion> {
-	override val serializationType: Companion get() = Companion
+	override val type: Companion get() = Companion
 
 	/**
 	 * The neighboring nodes that this node may transport to
@@ -31,6 +29,11 @@ interface TransportNode : PDCSerializable<TransportNode, TransportNode.Companion
 	 **/
 	fun storeData(persistentDataContainer: PersistentDataContainer)
 
+	/**
+	 * Load required data from the serialized container
+	 **/
+	fun loadData(persistentDataContainer: PersistentDataContainer)
+
 	companion object : PersistentDataType<PersistentDataContainer, TransportNode> {
 		override fun getPrimitiveType() = PersistentDataContainer::class.java
 		override fun getComplexType() = TransportNode::class.java
@@ -39,24 +42,21 @@ interface TransportNode : PDCSerializable<TransportNode, TransportNode.Companion
 			val pdc = context.newPersistentDataContainer()
 			pdc.set(NODE_TYPE, NodeType.type, NodeType[complex])
 
-			val requiredData = context.newPersistentDataContainer()
-
-			complex.storeData(requiredData)
-			pdc.set(NODE_DATA, PersistentDataType.TAG_CONTAINER, requiredData)
+			complex.storeData(pdc)
 
 			return pdc
 		}
 
 		override fun fromPrimitive(primitive: PersistentDataContainer, context: PersistentDataAdapterContext): TransportNode = try {
 			val type = primitive.get(NODE_TYPE, NodeType.type)!!
+			val instance = type.newInstance()
 
-			val requiredData = primitive.getOrDefault(NODE_DATA, PersistentDataType.TAG_CONTAINER, context.newPersistentDataContainer())
-			type.load(requiredData)
+			instance.loadData(primitive)
+
+			instance
 		} catch (e: Throwable) {
 			e.printStackTrace()
 			throw SerializationException("Error deserializing multiblock data!")
 		}
 	}
-
-	data class NodeData<T: Any>(val field: KClass<T>, val data: T)
 }
