56ac3db6afaae2b8edbe6a9c2ab37f6fe399add7
Gutin1 <Claytonpinter@gmail.com>
2024-04-30 02:07:32 -0500

    better(ish) deserialization

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/persistence/PDCSerializable.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/persistence/PDCSerializable.kt
@@ -6,13 +6,13 @@ import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 
 interface PDCSerializable <V : Any, T : PersistentDataType<PersistentDataContainer, V>> {
-	val type: T
+	val serializationType: T
 
 	fun serialize(adapterContext: PersistentDataAdapterContext, data: V): PersistentDataContainer {
-		return type.toPrimitive(data, adapterContext)
+		return serializationType.toPrimitive(data, adapterContext)
 	}
 
 	fun store(destination: PersistentDataContainer, key: NamespacedKey, data: V) {
-		destination.set(key, type, data)
+		destination.set(key, serializationType, data)
 	}
 }
