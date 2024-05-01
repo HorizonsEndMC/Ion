71e2e7a5e76286b7d7f443f273a5626fa0121343
Gutin1 <Claytonpinter@gmail.com>
2024-05-01 02:45:11 -0500

    delete old stuff, end rod node

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/SpongeNode.kt
@@ -0,0 +1,36 @@
+package net.horizonsend.ion.server.features.transport.node.power
+
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
+import net.horizonsend.ion.server.features.transport.node.type.MultiNode
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_COVERED_POSITIONS
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
+
+/**
+ * Represents a sponge [omnidirectional pipe]
+ *
+ * Since there is no use in keeping the individual steps, all touching sponges are consolidated into a single node with multiple inputs / outputs, weighted evenly
+ **/
+class SpongeNode() : MultiNode {
+	constructor(origin: Long) : this() {
+		positions.add(origin)
+	}
+
+	override val positions: MutableSet<Long> = LongOpenHashSet()
+
+	override val transferableNeighbors: MutableSet<TransportNode> = ObjectOpenHashSet()
+
+	override fun isTransferable(position: Long, node: TransportNode): Boolean {
+		return true
+	}
+
+	override fun loadData(persistentDataContainer: PersistentDataContainer) {
+		val coveredPositions = persistentDataContainer.get(NODE_COVERED_POSITIONS, PersistentDataType.LONG_ARRAY)
+		coveredPositions?.let { positions.addAll(it.asIterable()) }
+	}
+
+	override fun storeData(persistentDataContainer: PersistentDataContainer) {
+		persistentDataContainer.set(NODE_COVERED_POSITIONS, PersistentDataType.LONG_ARRAY, positions.toLongArray())
+	}
+}
