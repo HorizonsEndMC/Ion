0a88f7781ba1a3f9210c77dbe5614b688f72fa37
Gutin1 <Claytonpinter@gmail.com>
2024-05-01 02:45:11 -0500

    delete old stuff, end rod node

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeUtils.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeUtils.kt
@@ -1,6 +1,6 @@
 package net.horizonsend.ion.server.features.transport.node
 
-import net.horizonsend.ion.server.features.transport.node.nodes.TransportNode
+import net.horizonsend.ion.server.features.transport.node.power.TransportNode
 import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.associateWithNotNull
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
@@ -9,7 +9,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import org.bukkit.block.BlockFace
 
-fun getNeighborNodes(position: Long, nodes: Map<Long, TransportNode>, checkFaces: Set<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.associateWithNotNull {
+fun getNeighborNodes(position: Long, nodes: Map<Long, TransportNode>, checkFaces: Collection<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.associateWithNotNull {
 	val x = getX(position)
 	val y = getY(position)
 	val z = getZ(position)
@@ -17,7 +17,7 @@ fun getNeighborNodes(position: Long, nodes: Map<Long, TransportNode>, checkFaces
 	nodes[toBlockKey(x + it.modX, y + it.modY, z + it.modZ)]
 }
 
-fun getNeighborNodes(position: Long, nodes: Collection<Long>, checkFaces: Set<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.mapNotNull {
+fun getNeighborNodes(position: Long, nodes: Collection<Long>, checkFaces: Collection<BlockFace> = ADJACENT_BLOCK_FACES) = checkFaces.mapNotNull {
 	val x = getX(position)
 	val y = getY(position)
 	val z = getZ(position)
