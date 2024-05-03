f8787e1b0aa77c88eea9d1754bab461f4baf4d68
Gutin1 <Claytonpinter@gmail.com>
2024-05-03 15:18:46 -0500

    data fixers

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/data/ChunkDataFixer.kt
@@ -0,0 +1,15 @@
+package net.horizonsend.ion.server.features.world.data
+
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntityFixer
+import net.horizonsend.ion.server.features.world.IonChunk
+
+object ChunkDataFixer {
+	const val DATA_VERSION = 1
+
+	fun upgrade(chunk: IonChunk) {
+		if (chunk.dataVersion == DATA_VERSION) return
+		if (chunk.dataVersion > DATA_VERSION) throw IllegalStateException("Invalid chunk data version!")
+
+		MultiblockEntityFixer.upgrade(chunk)
+	}
+}
