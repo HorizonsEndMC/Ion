4522440c7124ddeed64aac26abd7639bd629bd4e
Consuelo Ugarte <consuelougartem@gmail.com>
2024-05-04 17:49:16 -0400

    draft 2

--- server/src/main/kotlin/net/horizonsend/ion/server/features/customitems/blasters/objects/CratePlacer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/customitems/blasters/objects/CratePlacer.kt
@@ -1,27 +1,35 @@
 package net.horizonsend.ion.server.features.customitems.blasters.objects
 
 
+import fr.skytasul.guardianbeam.Laser.GuardianLaser
 import net.horizonsend.ion.common.extensions.alert
-import net.horizonsend.ion.server.configuration.PVPBalancingConfiguration.EnergyWeapons.Balancing
+import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.customitems.CustomItem
 import net.horizonsend.ion.server.features.customitems.CustomItems.customItem
-import net.horizonsend.ion.server.features.space.data.BlockData
+import net.horizonsend.ion.server.features.economy.cargotrade.ShipmentManager.getShipmentItemId
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
-import net.horizonsend.ion.server.miscellaneous.utils.getBlockIfLoaded
+import net.horizonsend.ion.server.miscellaneous.utils.minecraft
 import net.kyori.adventure.key.Key.key
 import net.kyori.adventure.sound.Sound.Source.PLAYER
 import net.kyori.adventure.sound.Sound.sound
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import net.minecraft.core.BlockPos
+import net.minecraft.nbt.StringTag
+import net.minecraft.world.level.block.entity.BlockEntity
+import org.bukkit.FluidCollisionMode
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.SoundCategory.PLAYERS
 import org.bukkit.block.ShulkerBox
+import org.bukkit.craftbukkit.v1_20_R3.block.CraftShulkerBox
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BlockStateMeta
 
 abstract class CratePlacer(
 	identifier: String,
@@ -37,10 +45,20 @@ abstract class CratePlacer(
 		val magazineSize = 100
 		val ammoPerRefill = 1
 		val cooldown = 20
+		val range = 16;
 	}
+	var laser : GuardianLaser? = null
 
 	override fun handleSecondaryInteract(livingEntity: LivingEntity, itemStack: ItemStack) {
+		println("boop")
+		if (livingEntity !is Player) return
+		if (livingEntity.hasCooldown(itemStack.type)) return // Cooldown
+		if (getAmmunition(itemStack) == 0) {
+			handleTertiaryInteract(livingEntity, itemStack) // Force a reload
+			return // No Ammo
+		}
 		placeCrate(livingEntity, itemStack)
+		fireLaser(livingEntity)
 	}
 
 	override fun handleTertiaryInteract(livingEntity: LivingEntity, itemStack: ItemStack) {
@@ -112,31 +130,23 @@ abstract class CratePlacer(
 	override fun getAmmoPerRefill(): Int = ammoPerRefill
 	override fun getConsumesAmmo(): Boolean = true
 
-	fun placeCrate(livingEntity: LivingEntity, itemStack: ItemStack) {
-		println("boop")
-		if (livingEntity !is Player) return
-		if (livingEntity.hasCooldown(itemStack.type)) return // Cooldown
-		if (getAmmunition(itemStack) == 0) {
-			handleTertiaryInteract(livingEntity, itemStack) // Force a reload
-			return // No Ammo
-		}
-		println("boop1")
+	fun placeCrate(livingEntity: Player, itemStack: ItemStack) {
 		//todo fix check currently returing false
 		//if (!livingEntity.inventory.containsAtLeast(ItemStack(Material.SHULKER_BOX), 1)) return
 		//println("boop2")
-		val target = livingEntity.getTargetBlockExact(4) ?: return
+		val target = livingEntity.getTargetBlockExact(range) ?: return
 		println("target")
 		println(target)
 		println()
 		if (target.type != Material.STICKY_PISTON) return
 
-		val face = livingEntity.getTargetBlockFace(4) ?: return
+		val face = livingEntity.getTargetBlockFace(range) ?: return
 		//cordinates of the block above where to place crate
 		val x = target.x + face.modX
 		val y = target.y + face.modY
 		val z = target.z + face.modZ
 
-		var toReplace = getBlockIfLoaded(livingEntity.world, x, y, z)!!
+		val toReplace = livingEntity.world.getBlockAt(x, y, z)
 		println("to replace")
 		println(toReplace)
 		println()
@@ -156,21 +166,38 @@ abstract class CratePlacer(
 			println("item")
 			println(item)
 			println()
+			val itemState = (item.itemMeta as BlockStateMeta).blockState as ShulkerBox
 			//attempt to place the crate
+			//I copied gutins code and prayed that it worked
 			//fake block place event
 			toReplace.setBlockData(item.type.createBlockData(), true)
-			//todo add nbtdata
+
+			val boxEntity = toReplace.state as ShulkerBox
+			boxEntity.customName = item.itemMeta.displayName
+			boxEntity.inventory.addItem(*itemState.inventory.filterNotNull().toList().toTypedArray())
+
+			// Add the raw nms tag for shipment id
+			val id = getShipmentItemId(item)
+			val entity = (toReplace.state as CraftShulkerBox).tileEntity
+			val chunk = entity.location.chunk.minecraft
+			// Save the full compound tag
+			val base = entity.saveWithFullMetadata()
+			println(base)
+			println(id)
+			//incomplete crates dont have shipment ids
+			if (id != null) base.put("shipment_oid", StringTag.valueOf(id))
+
+			val blockPos = BlockPos(x, y, z)
+			// Remove old
+			chunk.removeBlockEntity(blockPos)
+
+			val blockEntity = BlockEntity.loadStatic(blockPos, entity.blockState, base)!!
+			chunk.addAndRegisterBlockEntity(blockEntity)
 			println("to replace")
 			println(toReplace)
 			println()
-			println("tempData")
-			println(tempData)
-			println()
-			println("state")
-			println(state)
-			println()
 			//event check
-			var event = BlockPlaceEvent(toReplace,
+			val event = BlockPlaceEvent(toReplace,
 										state,
 										target,
 										item,
@@ -190,6 +217,24 @@ abstract class CratePlacer(
 			}
 		}
 
+	}
+
+	fun fireLaser(livingEntity: LivingEntity) {
+		val start = livingEntity.eyeLocation.clone()
+		start.y -= 0.125
+
+		val raytrace = livingEntity.world.rayTrace(
+			livingEntity.eyeLocation,
+			start.direction.clone(),
+			range.toDouble(),
+			FluidCollisionMode.NEVER,
+			true,
+			0.1,
+			{false}
+		)
+		val end : Location = raytrace?.hitPosition?.toLocation(livingEntity.world)
+			?: livingEntity.eyeLocation.clone().add(start.direction.clone().multiply(range))
+		GuardianLaser(end,start,5, -1).durationInTicks().start(IonServer)
 	}
 
 }
