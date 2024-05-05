0931acb84f628f7879f2467010e609f151c312dc
Gutin1 <Claytonpinter@gmail.com>
2024-05-05 01:36:09 -0500

    solar panels

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
@@ -1,12 +1,16 @@
 package net.horizonsend.ion.server.features.transport.node
 
 import com.manya.pdc.base.EnumDataType
+import net.horizonsend.ion.server.features.transport.node.power.EndRodNode
+import net.horizonsend.ion.server.features.transport.node.power.SolarPanelNode
 import net.horizonsend.ion.server.features.transport.node.power.SpongeNode
 import net.horizonsend.ion.server.features.transport.node.power.TransportNode
 
 enum class NodeType(val clazz: Class<out TransportNode>) {
 	//POWER
 	SPONGE_NODE(SpongeNode::class.java),
+	END_ROD_NODE(EndRodNode::class.java),
+	SOLAR_PANEL_NODE(SolarPanelNode::class.java),
 
 	//GAS
 
