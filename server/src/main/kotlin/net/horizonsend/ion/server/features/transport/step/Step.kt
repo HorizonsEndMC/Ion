3ceb3115acfcff89652fd569cc992b7223a7ae95
Gutin1 <Claytonpinter@gmail.com>
2024-05-05 02:10:10 -0500

    cleanup, extractor and input

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
@@ -1,18 +1,13 @@
 package net.horizonsend.ion.server.features.transport.step
 
-import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.highlightBlock
 import net.horizonsend.ion.server.features.transport.container.ResourceContainer
 import net.horizonsend.ion.server.features.transport.grid.ChunkTransportNetwork
-import net.horizonsend.ion.server.features.transport.node.GridNode
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
-import net.horizonsend.ion.server.miscellaneous.utils.debugAudience
 import org.bukkit.block.BlockFace
 
 abstract class Step(
 	open val transportNetwork: ChunkTransportNetwork,
 	open val origin: ResourceContainer<*>?,
 	var direction: BlockFace,
-	var currentNode: GridNode
 ) {
 	var status: TransferStatus = TransferStatus.STEPPING
 
@@ -24,8 +19,6 @@ abstract class Step(
 		depth++
 
 		if (depth >= MAX_STEP_DEPTH) return
-
-		debugAudience.highlightBlock(Vec3i(currentNode.x, currentNode.y, currentNode.z), 50L)
 
 		when (status) {
 			TransferStatus.COMPLETE -> return
