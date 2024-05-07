bc80c9af43ae508754f976be12ff67f5558e7700
Gutin1 <Claytonpinter@gmail.com>
2024-05-07 00:31:34 -0500

    test power multis

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/powerbank/new/NewPowerBankMultiblock.kt
@@ -0,0 +1,84 @@
+package net.horizonsend.ion.server.features.multiblock.type.powerbank.new
+
+import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.EntityMultiblock
+import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.block.BlockFace
+
+abstract class NewPowerBankMultiblock<T: NewPowerBankMultiblock.PowerBankEntity>(tierText: String) : Multiblock(), EntityMultiblock<T> {
+	abstract val tierMaterial: Material
+	override val name = "newpowerbank"
+
+	override val signText = createSignText(
+		line1 = "&2Power &8Bank",
+		line2 = "&4------",
+		line3 = null,
+		line4 = tierText
+	)
+
+	override fun MultiblockShape.buildStructure() {
+		z(+0) {
+			y(-1) {
+				x(-1).extractor()
+				x(+0).wireInputComputer()
+				x(+1).extractor()
+			}
+
+			y(+0) {
+				x(-1).anyGlassPane()
+				x(+0).anyGlass()
+				x(+1).anyGlassPane()
+			}
+
+			y(+1) {
+				x(-1).anyGlassPane()
+				x(+0).anyGlass()
+				x(+1).anyGlassPane()
+			}
+		}
+
+		z(+1) {
+			for (i in -1..1) {
+				y(i) {
+					x(-1).anyGlass()
+					x(+0).redstoneBlock()
+					x(+1).anyGlass()
+				}
+			}
+		}
+
+		z(+2) {
+			y(-1) {
+				x(-1).type(tierMaterial)
+				x(+0).anyGlass()
+				x(+1).type(tierMaterial)
+			}
+
+			y(+0) {
+				x(-1).anyGlassPane()
+				x(+0).anyGlass()
+				x(+1).anyGlassPane()
+			}
+
+			y(+1) {
+				x(-1).anyGlassPane()
+				x(+0).anyGlass()
+				x(+1).anyGlassPane()
+			}
+		}
+	}
+
+	abstract class PowerBankEntity(
+		multiblock: NewPowerBankMultiblock<*>,
+		x: Int,
+		y: Int,
+		z: Int,
+		world: World,
+		signDirection: BlockFace,
+		override val maxPower: Int
+	) : MultiblockEntity(multiblock, x, y, z, world, signDirection), PoweredMultiblockEntity
+}
