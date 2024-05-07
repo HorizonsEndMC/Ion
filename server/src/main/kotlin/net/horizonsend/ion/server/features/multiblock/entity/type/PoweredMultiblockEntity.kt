a48a2a53664a5280b74083c0e849308d6d6fb922
Gutin1 <Claytonpinter@gmail.com>
2024-05-07 12:48:23 -0500

    update signs (temp)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/PoweredMultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/PoweredMultiblockEntity.kt
@@ -1,10 +1,20 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.front
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.NamedTextColor.GREEN
 import org.bukkit.persistence.PersistentDataType
 
 interface PoweredMultiblockEntity {
@@ -16,6 +26,8 @@ interface PoweredMultiblockEntity {
 		val correctedPower = amount.coerceIn(0, maxPower)
 
 		powerUnsafe = correctedPower
+
+		powerUpdateScope.launch { updatePowerVisually() }
 	}
 
 	fun getPower(): Int {
@@ -59,11 +71,23 @@ interface PoweredMultiblockEntity {
 	}
 
 	companion object {
-		private val prefixComponent = Component.text("E: ", NamedTextColor.YELLOW)
+		private val prefixComponent = text("E: ", NamedTextColor.YELLOW)
+		private val powerUpdateScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 	}
 
 	/** Store power data */
 	fun storePower(store: PersistentMultiblockData) {
 		store.addAdditionalData(NamespacedKeys.POWER, PersistentDataType.INTEGER, powerUnsafe)
 	}
+
+	suspend fun updatePowerVisually() {
+		//TODO replace this
+		require(this is MultiblockEntity)
+
+		val sign = getSign() ?: return
+		sign.front().line(2, formatPower())
+		Tasks.sync { sign.update() }
+	}
+
+	fun formatPower(): Component = ofChildren(prefixComponent, text(powerUnsafe, GREEN))
 }
