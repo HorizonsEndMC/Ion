5875ca6f81384dbe04d6b7a435988e5e8090a22d
Gutin1 <Claytonpinter@gmail.com>
2024-05-09 01:02:20 -0500

    working power transfer

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/ChunkTransportManager.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot
 import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot.Companion.snapshot
-import net.horizonsend.ion.server.features.transport.grid.ChunkPowerNetwork
+import net.horizonsend.ion.server.features.transport.network.ChunkPowerNetwork
 import net.horizonsend.ion.server.features.world.IonChunk
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.utils.IntervalExecutor
