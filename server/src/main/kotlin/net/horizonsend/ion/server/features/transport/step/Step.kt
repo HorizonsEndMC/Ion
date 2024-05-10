f751aa5d3a35e3b75565f0c882a5c5f0c6219596
Gutin1 <Claytonpinter@gmail.com>
2024-05-10 13:41:18 -0500

    power transport work, remove some debug

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
@@ -7,11 +7,21 @@ interface Step {
 	val steps: AtomicInteger
 	val currentNode: TransportNode
 	val share: Float
+	val traversedNodes: MutableList<TransportNode>
 
 	suspend operator fun invoke() {
 		if (steps.incrementAndGet() > MAX_DEPTH) return
 
+		traversedNodes.add(currentNode)
 		currentNode.handleStep(this)
+
+//		println("""
+//			Step has been invoked
+//			$steps steps taken
+//			currently on $currentNode
+//			has $share share
+//			traversed through $traversedNodes
+//		""".trimIndent())
 	}
 
 	companion object {
@@ -24,7 +34,9 @@ data class PowerOriginStep(
 	override val currentNode: TransportNode,
 	var power: Int,
 	override val share: Float = 1f
-) : Step
+) : Step {
+	override val traversedNodes: MutableList<TransportNode> = mutableListOf()
+}
 
 data class TransportStep(
 	val origin: PowerOriginStep,
@@ -32,4 +44,6 @@ data class TransportStep(
 	override val currentNode: TransportNode,
 	val previous: Step,
 	override val share: Float = 1f
-) : Step
+) : Step {
+	override val traversedNodes: MutableList<TransportNode> = mutableListOf()
+}
