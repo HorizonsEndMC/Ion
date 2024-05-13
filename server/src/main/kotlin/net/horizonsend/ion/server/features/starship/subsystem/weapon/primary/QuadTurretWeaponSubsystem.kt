680a64e9daa76d4c02bb4104a9eaa10f6a316489
Crystalzzz <billcypher269@gmail.com>
2024-05-13 16:44:12 +0100

    make quad turrets consume ammo (VANDRAYK's fault it was removed)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
@@ -5,16 +5,19 @@ import net.horizonsend.ion.server.features.multiblock.starshipweapon.turret.Quad
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.TurretWeaponSubsystem
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.PermissionWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.AmmoConsumingWeaponSubsystem
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.block.BlockFace
+import org.bukkit.inventory.ItemStack
 import java.util.concurrent.TimeUnit
+import net.horizonsend.ion.server.features.customitems.CustomItems
 
 class QuadTurretWeaponSubsystem(
     ship: ActiveStarship,
     pos: Vec3i,
     face: BlockFace,
     override val multiblock: QuadTurretMultiblock
-) : TurretWeaponSubsystem(ship, pos, face), PermissionWeaponSubsystem {
+) : TurretWeaponSubsystem(ship, pos, face), PermissionWeaponSubsystem, AmmoConsumingWeaponSubsystem {
 	override val balancing: StarshipWeapons.StarshipWeapon = starship.balancing.weapons.quadTurret
 	override val inaccuracyRadians: Double get() = Math.toRadians(balancing.inaccuracyRadians)
 	override val powerUsage: Int get() = balancing.powerUsage
@@ -22,4 +25,8 @@ class QuadTurretWeaponSubsystem(
 	override fun getMaxPerShot(): Int = balancing.maxPerShot
 
 	override val permission: String = "ion.multiblock.quadturret"
+
+	override fun getRequiredAmmo(): ItemStack {
+		return CustomItems.LOADED_TURRET_SHELL.constructItemStack()
+	}
 }
