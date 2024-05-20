3ff46cc8471d42331d0cc2d5f0ac406f2ce4f531
Gutin1 <Claytonpinter@gmail.com>
2024-05-20 11:03:28 -0500

    Drill nerf

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/drills/DrillMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/drills/DrillMultiblock.kt
@@ -275,7 +275,7 @@ abstract class DrillMultiblock(tierText: String, val tierMaterial: Material) :
 			}
 		)
 
-		val powerUsage = broken * 10
+		val powerUsage = broken * 60
 		PowerMachines.setPower(sign, power - powerUsage, true)
 	}
 
