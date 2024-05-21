f0afc83e9d1806afd4d71c487a424bb5b913918c
Gutin1 <Claytonpinter@gmail.com>
2024-05-21 01:04:46 -0500

    Revert "just don't bother checking for levers or trapdoors until I can figure this out"
    This reverts commit 980b43d5f4bec6980597c63f30daa5f3f627a937.

--- server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/ProtectionListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/ProtectionListener.kt
@@ -18,6 +18,9 @@ import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.block.Block
 import org.bukkit.block.data.BlockData
+import org.bukkit.block.data.type.Door
+import org.bukkit.block.data.type.Switch
+import org.bukkit.block.data.type.TrapDoor
 import org.bukkit.entity.Animals
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.EntityType
@@ -46,9 +49,9 @@ object ProtectionListener : SLEventListener() {
 		// Chests, button doors
 		if (event.action != Action.RIGHT_CLICK_BLOCK) return
 
-//		if (clickedBlockData is Switch) return onUseSwitch(event, clickedBlock)
+		if (clickedBlockData is Switch) return onUseSwitch(event, clickedBlock)
 		if (clickedBlockData is InventoryHolder) return onEditInventory(event, clickedBlock)
-//		if (clickedBlockData is TrapDoor || clickedBlockData is Door) return onUseDoor(event, clickedBlock)
+		if (clickedBlockData is TrapDoor || clickedBlockData is Door) return onUseDoor(event, clickedBlock)
 	}
 
 	private fun onUseSwitch(event: PlayerInteractEvent, clickedBlock: Block) {
