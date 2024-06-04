6014075ea5e3e212f2bccb53ebc8864fb47805c9
Gutin1 <Claytonpinter@gmail.com>
2024-06-03 22:41:33 -0500

    move new multiblock code to separate file for now

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/ItemConsumable.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/ItemConsumable.kt
@@ -1,4 +1,4 @@
-package net.horizonsend.ion.server.features.multiblock.type.crafting.ingredient
+package net.horizonsend.ion.server.features.multiblock.crafting.ingredient
 
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import org.bukkit.block.Sign
