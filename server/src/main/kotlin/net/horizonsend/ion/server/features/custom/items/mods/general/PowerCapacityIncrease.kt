d3f117fea5b88af833a4ec354b0a570766e4d335
Gutin1 <Claytonpinter@gmail.com>
2024-06-06 02:13:05 -0500

    power capacity upgrades

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/mods/general/PowerCapacityIncrease.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/mods/general/PowerCapacityIncrease.kt
@@ -1,5 +1,14 @@
 package net.horizonsend.ion.server.features.custom.items.mods.general
 
-interface PowerCapacityIncrease {
+import net.horizonsend.ion.server.features.custom.items.CustomItem
+import net.horizonsend.ion.server.features.custom.items.mods.ItemModification
+import net.horizonsend.ion.server.features.custom.items.powered.PowerDrill
+import kotlin.reflect.KClass
+
+class PowerCapacityIncrease(
 	val increaseAmount: Int
+) : ItemModification {
+	override val identifier: String = "POWER_CAPACITY_$increaseAmount"
+	override val applicableTo: Array<KClass<out CustomItem>> = arrayOf(PowerDrill::class)
+	override val incompatibleWithMods: Array<KClass<out ItemModification>> = arrayOf()
 }
