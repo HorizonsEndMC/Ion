e4f6822ac4e772246cd2f363ea92b7b0170537c5
Gutin1 <Claytonpinter@gmail.com>
2024-06-08 20:23:03 -0500

    organize a listener

--- server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/BlockListener.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listener/misc/BlockListener.kt
@@ -1,10 +1,7 @@
 package net.horizonsend.ion.server.listener.misc
 
-import net.horizonsend.ion.server.features.custom.items.CustomItems.customItem
-import net.horizonsend.ion.server.features.custom.items.powered.PowerDrill
 import net.horizonsend.ion.server.features.multiblock.misc.MobDefender
 import net.horizonsend.ion.server.listener.SLEventListener
-import net.horizonsend.ion.server.miscellaneous.registrations.legacy.CustomItems
 import net.horizonsend.ion.server.miscellaneous.utils.stripColor
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
@@ -20,26 +17,6 @@ object BlockListener : SLEventListener() {
 	fun onBlockPhysicsEvent(event: BlockPhysicsEvent) {
 		if (event.block.type != Material.END_PORTAL) return
 		event.isCancelled = true
-	}
-
-	// Don't allow breaking blocks with custom items
-
-	@EventHandler
-	fun onBlockBreakEventA(event: BlockBreakEvent) {
-		val item = event.player.inventory.itemInMainHand
-		val customItem = CustomItems[item]
-		if (customItem != null && !customItem.id.startsWith("power_tool_")) {
-			event.isCancelled = true
-		}
-	}
-
-	@EventHandler
-	fun onBlockBreakEventB(event: BlockBreakEvent) {
-		val item = event.player.inventory.itemInMainHand
-		val customItem = item.customItem
-		if (customItem != null && customItem !is PowerDrill) {
-			event.isCancelled = true
-		}
 	}
 
 	// Prevent huge mushroom trees from growing as large mushroom blocks are used as custom ores
