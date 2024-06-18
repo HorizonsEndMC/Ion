6aa2aa6052d87caa4d08832a19e1a1ab55c2c1e1
Gutin1 <Claytonpinter@gmail.com>
2024-06-18 14:35:07 -0500

    start rebuild of multiblock access

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newer/MultiblockDetectedEvent.kt
@@ -0,0 +1,7 @@
+package net.horizonsend.ion.server.features.multiblock.newer
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.player.PlayerEvent
+
+class MultiblockDetectedEvent(): PlayerEvent, Cancellable {
+}
