bb81af2b31033a28463f2a2dfb20e2fc1982dc9b
Gutin1 <Claytonpinter@gmail.com>
2024-06-21 14:50:17 -0500

    organization

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/starshipweapon/heavy/HeavyLaserStarshipWeaponMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/starshipweapon/heavy/HeavyLaserStarshipWeaponMultiblock.kt
@@ -1,6 +1,6 @@
 package net.horizonsend.ion.server.features.multiblock.type.starshipweapon.heavy
 
-import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.SignlessStarshipWeaponMultiblock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.secondary.HeavyLaserWeaponSubsystem
