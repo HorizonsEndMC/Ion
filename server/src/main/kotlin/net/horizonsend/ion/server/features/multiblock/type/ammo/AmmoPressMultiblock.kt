5a0bfe03829c8e5f109b4ebded0f56b62b9dcdea
Gutin1 <Claytonpinter@gmail.com>
2024-06-21 14:50:17 -0500

    organization

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/ammo/AmmoPressMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/ammo/AmmoPressMultiblock.kt
@@ -4,7 +4,7 @@ import net.horizonsend.ion.server.features.custom.items.CustomItems.customItem
 import net.horizonsend.ion.server.features.custom.items.objects.AmmunitionHoldingItem
 import net.horizonsend.ion.server.features.machine.PowerMachines
 import net.horizonsend.ion.server.features.multiblock.Multiblock
-import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.type.FurnaceMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.PowerStoringMultiblock
 import net.horizonsend.ion.server.miscellaneous.utils.getFacing
--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/mininglasers/MiningLaserMultiblockTier1.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/mininglasers/MiningLaserMultiblockTier1.kt
@@ -1,6 +1,6 @@
 package net.horizonsend.ion.server.features.multiblock.type.mininglasers
 
-import net.horizonsend.ion.server.features.multiblock.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
