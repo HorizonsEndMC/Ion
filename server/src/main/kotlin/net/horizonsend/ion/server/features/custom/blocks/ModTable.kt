a4b30120e7742ab39d21f2c8dc46e71597e517c2
Gutin1 <Claytonpinter@gmail.com>
2024-06-21 13:30:09 -0500

    start on implementation using paper api

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/ModTable.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/ModTable.kt
@@ -1,5 +1,6 @@
 package net.horizonsend.ion.server.features.custom.blocks
 
+import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.custom.items.CustomItems
 import net.horizonsend.ion.server.features.custom.items.CustomItems.customItem
 import net.horizonsend.ion.server.features.custom.items.mods.items.ModificationItem
@@ -8,6 +9,7 @@ import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.features.gui.custom.CustomGUI
 import net.horizonsend.ion.server.features.gui.custom.slot.GUISlot
 import net.horizonsend.ion.server.features.gui.custom.slot.NormalSlot
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.empty
 import org.bukkit.Location
 import org.bukkit.block.Block
@@ -16,6 +18,8 @@ import org.bukkit.block.BlockFace.EAST
 import org.bukkit.block.BlockFace.NORTH
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
 import xyz.xenondevs.invui.window.Window
@@ -123,6 +127,27 @@ object ModTable : InteractableCustomBlock(
 
 				return customItem.getMods(itemStack).mapNotNull { it.modItem.get()?.constructItemStack() }
 			}
+		}
+	}
+
+	class ModTableGUI : InventoryHolder {
+		private val inventory: Inventory = IonServer.server.createInventory(this, 36, getTitle())
+		private val internalInventory = IonServer.server.createInventory(this, 6, getTitle())
+
+		override fun getInventory(): Inventory = inventory
+
+		private fun getTitle(): Component {
+			val subtitle = empty()
+			val titleText = GuiText("Modification Table")
+
+			titleText.add(subtitle, 0)
+			titleText.setSlotOverlay(
+				"# # # # # # # # #",
+				"# . # . . . . . #",
+				"# # # # # # # # #"
+			)
+
+			return titleText.build()
 		}
 	}
 }
