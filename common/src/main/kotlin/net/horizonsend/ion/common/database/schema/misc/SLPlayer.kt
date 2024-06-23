1f4f98be0daabb1f975b9a43bece51fc98515232
kwazedilla <kwazedilla@gmail.com>
2024-06-22 19:25:51 -0700

    add sidebar color changing method

--- common/src/main/kotlin/net/horizonsend/ion/common/database/schema/misc/SLPlayer.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/database/schema/misc/SLPlayer.kt
@@ -67,6 +67,7 @@ data class SLPlayer(
 	var contactsDistance: Int = 6000,
 	var contactsMaxNameLength: Int = 64,
 	var contactsSort: Int = 0,
+	var contactsColoring: Int = 0,
 	var contactsEnabled: Boolean = true,
 	var contactsStarships: Boolean = true,
 	var lastStarshipEnabled: Boolean = true,
