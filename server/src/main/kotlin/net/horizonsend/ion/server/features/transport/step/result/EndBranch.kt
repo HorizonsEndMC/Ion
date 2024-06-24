47ec315a8f56d66f27f43e781985760440537e6d
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/result/EndBranch.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/result/EndBranch.kt
@@ -2,10 +2,10 @@ package net.horizonsend.ion.server.features.transport.step.result
 
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
 import net.horizonsend.ion.server.features.transport.step.head.HeadHolder
-import net.horizonsend.ion.server.features.transport.step.head.StepHead
 
-class ChangeHead<T: ChunkTransportNetwork>(val new: StepHead<T>) : StepResult<T> {
-	override fun apply(head: HeadHolder<T>) {
-		head.head = new
+/** A result which marks the branch as dead */
+class EndBranch<T: ChunkTransportNetwork> : StepResult<T> {
+	override suspend fun apply(headHolder: HeadHolder<T>) {
+		headHolder.head.setDead()
 	}
 }
