f8351303ed9cb8abeb35ecb38fc2739bbca068d1
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/SingleBranchHead.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/SingleBranchHead.kt
@@ -3,6 +3,6 @@ package net.horizonsend.ion.server.features.transport.step.head
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
 import net.horizonsend.ion.server.features.transport.node.TransportNode
 
-interface SingleHead<T: ChunkTransportNetwork> : StepHead<T> {
-	var head: TransportNode
+interface SingleBranchHead<T: ChunkTransportNetwork> : BranchHead<T> {
+	var currentNode: TransportNode
 }
