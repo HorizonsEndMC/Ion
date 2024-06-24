cd85b697c73e4eb6d153377a924e1f03980bd4e7
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/result/MoveForward.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/result/MoveForward.kt
@@ -1,10 +1,39 @@
 package net.horizonsend.ion.server.features.transport.step.result
 
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
+import net.horizonsend.ion.server.features.transport.node.type.DestinationNode
+import net.horizonsend.ion.server.features.transport.node.type.IntermediateNode
+import net.horizonsend.ion.server.features.transport.node.type.SourceNode
 import net.horizonsend.ion.server.features.transport.step.head.HeadHolder
+import net.horizonsend.ion.server.features.transport.step.head.SingleBranchHead
 
+/** A result which moves the head of the branch forward, using the current node's pathfinding */
 class MoveForward<T: ChunkTransportNetwork> : StepResult<T> {
-	override fun apply(head: HeadHolder<T>) {
-		val currentNode = head.head.currentNode
+	override suspend fun apply(headHolder: HeadHolder<T>) {
+		val branchHead = headHolder.head
+
+		tryCast<IntermediateNode<T>>(branchHead) {
+			val next = getNextNode(headHolder.head) ?: return EndBranch<T>().apply(headHolder)
+
+			if (branchHead is SingleBranchHead<*>) {
+				branchHead.currentNode = next
+			}
+		}
+
+		tryCast<DestinationNode<T>>(branchHead) {
+			finishChain(headHolder.head)
+		}
+
+		if (branchHead is SourceNode<*>) throw NotImplementedError()
+	}
+
+	// Get around runtime type erasure
+	private inline fun <reified T> tryCast(instance: Any?, block: T.() -> Unit): Boolean {
+		if (instance is T) {
+			block(instance)
+			return true
+		}
+
+		return false
 	}
 }
