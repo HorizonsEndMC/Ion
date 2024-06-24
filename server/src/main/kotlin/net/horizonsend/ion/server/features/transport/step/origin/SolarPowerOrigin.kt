fe06e52762c9f03d4e14532a8d1b779973c92ad4
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/SolarPowerOrigin.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/SolarPowerOrigin.kt
@@ -1,6 +1,15 @@
 package net.horizonsend.ion.server.features.transport.step.origin
 
+import net.horizonsend.ion.server.features.multiblock.entity.type.PoweredMultiblockEntity
 import net.horizonsend.ion.server.features.transport.network.ChunkPowerNetwork
 import net.horizonsend.ion.server.features.transport.node.power.SolarPanelNode
 
-class SolarPowerOrigin(val origin: SolarPanelNode) : StepOrigin<ChunkPowerNetwork>
+class SolarPowerOrigin(val origin: SolarPanelNode) : StepOrigin<ChunkPowerNetwork>, PowerOrigin {
+	override fun getTransferPower(destination: PoweredMultiblockEntity): Int {
+		val room = destination.maxPower - destination.getPower()
+		val power = origin.tickAndGetPower()
+
+		// No max transfer limit for solar fields, as large ones would quickly overwhelm any limit
+		return minOf(power, room)
+	}
+}
