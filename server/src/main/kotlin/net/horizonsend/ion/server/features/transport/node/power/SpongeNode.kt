4659b567935bdbcc14a73207e1995f9abc50eb68
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/SpongeNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/SpongeNode.kt
@@ -5,10 +5,12 @@ import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 import net.horizonsend.ion.server.features.transport.network.ChunkPowerNetwork
 import net.horizonsend.ion.server.features.transport.node.NodeRelationship
 import net.horizonsend.ion.server.features.transport.node.TransportNode
+import net.horizonsend.ion.server.features.transport.node.type.IntermediateNode
 import net.horizonsend.ion.server.features.transport.node.type.MultiNode
 import net.horizonsend.ion.server.features.transport.node.type.SourceNode
-import net.horizonsend.ion.server.features.transport.step.PowerTransportStep
-import net.horizonsend.ion.server.features.transport.step.Step
+import net.horizonsend.ion.server.features.transport.step.head.BranchHead
+import net.horizonsend.ion.server.features.transport.step.result.MoveForward
+import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_COVERED_POSITIONS
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import org.bukkit.persistence.PersistentDataContainer
@@ -19,7 +21,7 @@ import org.bukkit.persistence.PersistentDataType
  *
  * Since there is no use in keeping the individual steps, all touching sponges are consolidated into a single node with multiple inputs / outputs, weighted evenly
  **/
-class SpongeNode(override val network: ChunkPowerNetwork) : MultiNode<SpongeNode, SpongeNode> {
+class SpongeNode(override val network: ChunkPowerNetwork) : MultiNode<SpongeNode, SpongeNode>, IntermediateNode<ChunkPowerNetwork> {
 	constructor(network: ChunkPowerNetwork, origin: BlockKey) : this(network) {
 		positions.add(origin)
 	}
@@ -29,7 +31,7 @@ class SpongeNode(override val network: ChunkPowerNetwork) : MultiNode<SpongeNode
 	override val relationships: MutableSet<NodeRelationship> = ObjectOpenHashSet()
 
 	override fun isTransferableTo(node: TransportNode): Boolean {
-		return node !is SourceNode
+		return node !is SourceNode<*>
 	}
 
 	override fun loadData(persistentDataContainer: PersistentDataContainer) {
@@ -54,24 +56,14 @@ class SpongeNode(override val network: ChunkPowerNetwork) : MultiNode<SpongeNode
 		}
 	}
 
-	override suspend fun handleStep(step: Step) {
-		// This is not an origin node, so we can assume that it is not an origin step
-		step as PowerTransportStep
-
-		val next = getTransferableNodes()
-			.filterNot { step.traversedNodes.contains(it) }
-			.filterNot { step.previous.currentNode == it }
-			.randomOrNull() ?: return
-
+	override suspend fun handleHeadStep(head: BranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
 		// Simply move on to the next node
-		PowerTransportStep(
-			step.origin,
-			step.steps,
-			next,
-			step,
-			step.traversedNodes
-		).invoke()
+		return MoveForward()
 	}
+
+	override suspend fun getNextNode(head: BranchHead<ChunkPowerNetwork>): TransportNode? = getTransferableNodes()
+		.filterNot { head.previousNodes.contains(it) }
+		.firstOrNull()
 
 	override fun toString(): String = "(SPONGE NODE: ${positions.size} positions, Transferable to: ${getTransferableNodes().joinToString { it.javaClass.simpleName }} nodes)"
 }
