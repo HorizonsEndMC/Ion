25d2b96c9d0dda4b6257105b6e45be27678efdf1
Gutin1 <Claytonpinter@gmail.com>
2024-06-24 01:27:35 -0500

    finish new step system (broken)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/IntermediateNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/IntermediateNode.kt
@@ -1,7 +1,8 @@
 package net.horizonsend.ion.server.features.transport.node.type
 
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
-import net.horizonsend.ion.server.features.transport.step.head.StepHead
+import net.horizonsend.ion.server.features.transport.node.TransportNode
+import net.horizonsend.ion.server.features.transport.step.head.BranchHead
 import net.horizonsend.ion.server.features.transport.step.result.StepResult
 
 interface IntermediateNode<T: ChunkTransportNetwork> {
@@ -11,5 +12,7 @@ interface IntermediateNode<T: ChunkTransportNetwork> {
 	 * This may create a new step for a single node, spawn off multiple steps, or more
 	 * Each node defines how it is stepped.
 	 **/
-	suspend fun handleHeadStep(head: StepHead<T>): StepResult<T>
+	suspend fun handleHeadStep(head: BranchHead<T>): StepResult<T>
+
+	suspend fun getNextNode(head: BranchHead<T>): TransportNode?
 }
