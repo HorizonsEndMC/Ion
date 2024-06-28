4deadc6996a07161b0e50bc1fd5ff48a3d36a106
Gutin1 <Claytonpinter@gmail.com>
2024-06-28 00:52:54 -0500

    fixes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
@@ -7,10 +7,13 @@ import net.horizonsend.ion.server.features.transport.node.NodeRelationship
 import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.type.SingleNode
 import net.horizonsend.ion.server.features.transport.node.type.SourceNode
+import net.horizonsend.ion.server.features.transport.step.head.BranchHead
 import net.horizonsend.ion.server.features.transport.step.head.power.SinglePowerBranchHead
 import net.horizonsend.ion.server.features.transport.step.new.NewStep
 import net.horizonsend.ion.server.features.transport.step.origin.ExtractorPowerOrigin
 import net.horizonsend.ion.server.features.transport.step.origin.StepOrigin
+import net.horizonsend.ion.server.features.transport.step.result.MoveForward
+import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_COVERED_POSITIONS
 import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
@@ -84,7 +87,7 @@ class PowerExtractorNode(override val network: ChunkPowerNetwork) : SingleNode, 
 
 		return NewStep(
 			network = this.network,
-			origin = getOriginData()
+			origin = getOriginData() ?: return null
 		) {
 			SinglePowerBranchHead(
 				holder = this,
@@ -94,7 +97,18 @@ class PowerExtractorNode(override val network: ChunkPowerNetwork) : SingleNode, 
 		}
 	}
 
-	override suspend fun getOriginData(): StepOrigin<ChunkPowerNetwork> = ExtractorPowerOrigin(this)
+	override suspend fun getNextNode(head: BranchHead<ChunkPowerNetwork>): TransportNode? {
+		return getTransferableNodes().randomOrNull()
+	}
+
+	override suspend fun handleHeadStep(head: BranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
+		return MoveForward()
+	}
+
+	override suspend fun getOriginData(): StepOrigin<ChunkPowerNetwork>? {
+		if (getTransferableNodes().isEmpty()) return null
+		return ExtractorPowerOrigin(this)
+	}
 
 	fun getTransferPower(): Int {
 		val interval = IonServer.transportSettings.extractorTickIntervalMS.toDouble()
