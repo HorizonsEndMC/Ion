435eb86c3f7aee9857edb260cd7fe3c45af68484
Gutin1 <Claytonpinter@gmail.com>
2024-06-29 16:03:11 -0500

    cleanup, fixes, remove debug println, documentation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/Step.kt
@@ -3,10 +3,9 @@ package net.horizonsend.ion.server.features.transport.step
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
 import net.horizonsend.ion.server.features.transport.step.head.BranchHead
 import net.horizonsend.ion.server.features.transport.step.head.HeadHolder
-import net.horizonsend.ion.server.features.transport.step.head.power.SinglePowerBranchHead
 import net.horizonsend.ion.server.features.transport.step.origin.StepOrigin
 
-class Step<T: ChunkTransportNetwork>(
+class Step<T: ChunkTransportNetwork> private constructor(
 	val network: T,
 	val origin: StepOrigin<T>,
 ) : HeadHolder<T> {
@@ -22,9 +21,6 @@ class Step<T: ChunkTransportNetwork>(
 
 	suspend operator fun invoke() {
 		while (!head.isDead()) {
-			println("Stepping forward. Head: $head")
-			println("Current head position: ${(head as? SinglePowerBranchHead)?.currentNode}")
-			println("Previous nodes: ${head.previousNodes}")
 			head.stepForward()
 		}
 	}
