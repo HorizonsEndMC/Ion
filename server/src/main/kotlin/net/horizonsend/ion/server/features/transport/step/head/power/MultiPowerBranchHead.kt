158ffed41bf2e86c43d52e5be3dcf8011a969d30
Gutin1 <Claytonpinter@gmail.com>
2024-06-29 01:47:34 -0500

    cleanup, reliability, working power extraction

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/power/MultiPowerBranchHead.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/power/MultiPowerBranchHead.kt
@@ -2,15 +2,15 @@ package net.horizonsend.ion.server.features.transport.step.head.power
 
 import net.horizonsend.ion.server.features.transport.network.ChunkPowerNetwork
 import net.horizonsend.ion.server.features.transport.node.TransportNode
-import net.horizonsend.ion.server.features.transport.step.head.HeadHolder
 import net.horizonsend.ion.server.features.transport.step.head.MultiBranchHead
+import net.horizonsend.ion.server.features.transport.step.new.NewStep
 
 /**
  *
  *
  **/
 class MultiPowerBranchHead(
-	override val holder: HeadHolder<ChunkPowerNetwork>,
+	override val holder: NewStep<ChunkPowerNetwork>,
 	override val share: Double,
 	override val previousNodes: MutableSet<TransportNode> = mutableSetOf(),
 	override val heads: MutableSet<MultiBranchHead.MultiHeadHolder<ChunkPowerNetwork>> = mutableSetOf(),
