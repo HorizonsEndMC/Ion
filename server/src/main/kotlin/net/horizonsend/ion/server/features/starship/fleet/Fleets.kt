0e0a9eca1894bd5236364344b9eafe51feed6664
kwazedilla <kwazedilla@gmail.com>
2024-06-29 20:56:26 -0700

    fleets foundation; creation and deletion

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/fleet/Fleets.kt
@@ -0,0 +1,30 @@
+package net.horizonsend.ion.server.features.starship.fleet
+
+import net.horizonsend.ion.server.IonServerComponent
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerQuitEvent
+
+object Fleets : IonServerComponent() {
+
+    private val fleetList = mutableListOf<Fleet>()
+
+    fun Player.fleet() = fleetList.find { it.get(this) }
+
+    fun create(player: Player) = fleetList.add(Fleet(player.uniqueId))
+
+    fun delete(fleet: Fleet) {
+        if (fleetList.contains(fleet)) {
+            fleet.delete()
+            fleetList.remove(fleet)
+        }
+    }
+
+    @Suppress("unused")
+    @EventHandler
+    fun onPlayerLeave(event: PlayerQuitEvent) {
+        val player = event.player
+
+        player.fleet()?.remove(player) ?: return
+    }
+}
