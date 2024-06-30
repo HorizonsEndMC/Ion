97a4cf4c0e23399f452c4417d64e63ce77cbaada
Gutin1 <Claytonpinter@gmail.com>
2024-06-29 20:10:39 -0500

    display entites

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerFlowMeter.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerFlowMeter.kt
@@ -1,6 +1,9 @@
 package net.horizonsend.ion.server.features.transport.node.power
 
+import com.manya.pdc.base.EnumDataType
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
+import net.horizonsend.ion.common.utils.miscellaneous.roundToHundredth
+import net.horizonsend.ion.server.features.client.display.container.TextDisplayHandler
 import net.horizonsend.ion.server.features.transport.network.ChunkPowerNetwork
 import net.horizonsend.ion.server.features.transport.node.NodeRelationship
 import net.horizonsend.ion.server.features.transport.node.TransportNode
@@ -12,20 +15,28 @@ import net.horizonsend.ion.server.features.transport.step.result.MoveForward
 import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
-import net.kyori.adventure.text.Component
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.GREEN
+import org.bukkit.block.BlockFace
+import org.bukkit.block.BlockFace.NORTH
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 import kotlin.properties.Delegates
 
 class PowerFlowMeter(override val network: ChunkPowerNetwork) : SingleNode, StepHandler<ChunkPowerNetwork> {
-	constructor(network: ChunkPowerNetwork, position: BlockKey) : this(network) {
+	constructor(network: ChunkPowerNetwork, position: BlockKey, direction: BlockFace) : this(network) {
 		this.position = position
+		this.direction = direction
 	}
 
 	override var position by Delegates.notNull<Long>()
 	override var isDead: Boolean = false
 	override val relationships: MutableSet<NodeRelationship> = ObjectOpenHashSet()
+	var direction: BlockFace by Delegates.notNull()
 
 	/*
 	 * Should transfer power like any normal node.
@@ -50,7 +61,9 @@ class PowerFlowMeter(override val network: ChunkPowerNetwork) : SingleNode, Step
 
 	override suspend fun onCompleteChain(final: BranchHead<*>, destination: PowerInputNode, transferred: Int) {
 		addAverage(TransferredPower(transferred, System.currentTimeMillis()))
-		network.world.sendMessage(Component.text("${toVec3i(position)} ${network.world.name} Running average transferred is ${calculateAverage()}"))
+
+		val avg = runCatching { calculateAverage().roundToHundredth() }.getOrDefault(0.0)
+		displayHandler.setText(text(avg, GREEN))
 	}
 
 	private fun addAverage(average: TransferredPower) {
@@ -77,12 +90,51 @@ class PowerFlowMeter(override val network: ChunkPowerNetwork) : SingleNode, Step
 		return sum / timeDiff
 	}
 
+	private lateinit var displayHandler: TextDisplayHandler
+
+	private fun setupDisplayEntity() {
+		// 95% of the way to the edge of the block once added to the center of the block, to avoid z fighting
+		val offset = direction.direction.multiply(0.45)
+
+		val facingBlock = getRelative(position, direction)
+
+		val x = getX(facingBlock).toDouble() + 0.5 - offset.x
+		val y = getY(facingBlock).toDouble() + 0.5
+		val z = getZ(facingBlock).toDouble() + 0.5 - offset.z
+
+		displayHandler = TextDisplayHandler(
+			network.world,
+			x,
+			y,
+			z,
+			direction
+		)
+	}
+
+	override fun loadIntoNetwork() {
+		setupDisplayEntity()
+
+		super.loadIntoNetwork()
+	}
+
+	override suspend fun onPlace(position: BlockKey) {
+		setupDisplayEntity()
+
+		super.onPlace(position)
+	}
+
+	override suspend fun handleRemoval(position: BlockKey) {
+		if (::displayHandler.isInitialized) displayHandler.remove()
+	}
+
 	override fun storeData(persistentDataContainer: PersistentDataContainer) {
 		persistentDataContainer.set(NamespacedKeys.NODE_COVERED_POSITIONS, PersistentDataType.LONG, position)
+		persistentDataContainer.set(NamespacedKeys.AXIS, EnumDataType(BlockFace::class.java), direction)
 	}
 
 	override fun loadData(persistentDataContainer: PersistentDataContainer) {
 		position = persistentDataContainer.get(NamespacedKeys.NODE_COVERED_POSITIONS, PersistentDataType.LONG)!!
+		direction = persistentDataContainer.getOrDefault(NamespacedKeys.AXIS, EnumDataType(BlockFace::class.java), NORTH)
 	}
 
 	private data class TransferredPower(val transferred: Int, val time: Long)
