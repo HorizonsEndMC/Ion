bcf7f839f17f3fd17d290769aacdb99abe70b150
s1ice <alexwlefebvre@gmail.com>
2024-07-05 22:41:20 -0500

    fix setpower only using fast mode

--- server/src/main/kotlin/net/horizonsend/ion/server/command/qol/SetPowerCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/qol/SetPowerCommand.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.command.qol
 
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Optional
@@ -18,12 +19,11 @@ import org.bukkit.entity.Player
 object SetPowerCommand : SLCommand() {
 	@Default
 	@Suppress("unused")
-
-	fun onSetPower(sender: Player, amount: Int, @Optional restricted: Boolean?) {
+	@CommandCompletion("0|1000|500000|2147483647 true|false")
+	fun onSetPower(sender: Player, amount: Int, @Optional ignoreLimit: Boolean?) {
 		val maxSelectionVolume = 200000
 		val selection = sender.getSelection() ?: return
-		val limited = restricted ?: true
-
+		
 		if(selection.volume > maxSelectionVolume) {
 			sender.userError("Selection too large! The maximum volume is $maxSelectionVolume.")
 			return
@@ -42,7 +42,7 @@ object SetPowerCommand : SLCommand() {
 			val sign = block.state as? org.bukkit.block.Sign ?: continue
 			sender.debug("sign found at $x $y $z")
 
-			setPower(sign, amount, true)
+			setPower(sign, amount, fast = ignoreLimit ?: false)
 			sender.debug("power sent")
 		}
 		sender.success("Set multiblock power to $amount.")
