4fe7b1c3d54a6fa7ccc64421957ec4d9d38d054a
Gutin1 <Claytonpinter@gmail.com>
2024-07-13 14:43:18 -0500

    record direction when stepping

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerFlowMeter.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerFlowMeter.kt
@@ -11,6 +11,7 @@ import net.horizonsend.ion.server.features.transport.node.type.SingleNode
 import net.horizonsend.ion.server.features.transport.node.type.SourceNode
 import net.horizonsend.ion.server.features.transport.node.type.StepHandler
 import net.horizonsend.ion.server.features.transport.step.head.BranchHead
+import net.horizonsend.ion.server.features.transport.step.head.SingleBranchHead
 import net.horizonsend.ion.server.features.transport.step.result.MoveForward
 import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
@@ -47,13 +48,13 @@ class PowerFlowMeter(override val network: ChunkPowerNetwork) : SingleNode, Step
 		return node !is SourceNode<*>
 	}
 
-	override suspend fun handleHeadStep(head: BranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
+	override suspend fun handleHeadStep(head: SingleBranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
 		// Simply move on to the next node
 		return MoveForward()
 	}
 
-	override suspend fun getNextNode(head: BranchHead<ChunkPowerNetwork>): TransportNode? = getTransferableNodes()
-		.filterNot { head.previousNodes.contains(it) }
+	override suspend fun getNextNode(head: SingleBranchHead<ChunkPowerNetwork>, entranceDirection: BlockFace): Pair<TransportNode, BlockFace>? = getTransferableNodes()
+		.filterNot { head.previousNodes.contains(it.first) }
 		.randomOrNull()
 
 	private val STORED_AVERAGES = 20
