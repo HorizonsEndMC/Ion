633112b95b05a535a11b7228cbfdb4bc6b303c27
Gutin1 <Claytonpinter@gmail.com>
2024-07-13 14:43:18 -0500

    record direction when stepping

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/DirectionalNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/DirectionalNode.kt
@@ -8,12 +8,13 @@ import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.type.SingleNode
 import net.horizonsend.ion.server.features.transport.node.type.SourceNode
 import net.horizonsend.ion.server.features.transport.node.type.StepHandler
-import net.horizonsend.ion.server.features.transport.step.head.BranchHead
+import net.horizonsend.ion.server.features.transport.step.head.SingleBranchHead
 import net.horizonsend.ion.server.features.transport.step.result.MoveForward
 import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import org.bukkit.Material
+import org.bukkit.block.BlockFace
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 import kotlin.properties.Delegates
@@ -33,11 +34,12 @@ class DirectionalNode(override val network: ChunkPowerNetwork) : SingleNode, Ste
 		return node !is SourceNode<*>
 	}
 
-	override suspend fun getNextNode(head: BranchHead<ChunkPowerNetwork>): TransportNode? = getTransferableNodes()
-		.filterNot { head.previousNodes.contains(it) }
+	override suspend fun getNextNode(head: SingleBranchHead<ChunkPowerNetwork>, entranceDirection: BlockFace): Pair<TransportNode, BlockFace>? = getTransferableNodes()
+		.filterNot { head.previousNodes.contains(it.first) }
 		.randomOrNull()
 
-	override suspend fun handleHeadStep(head: BranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
+
+	override suspend fun handleHeadStep(head: SingleBranchHead<ChunkPowerNetwork>): StepResult<ChunkPowerNetwork> {
 		// Simply move on to the next node
 		return MoveForward()
 	}
