92c3b459f3702b7dd0b96b1ce62d85d920a5023e
kwazedilla <kwazedilla@gmail.com>
2024-07-14 19:23:15 -0700

    frigate controller improvements; cycle turret now slows DC on impact for 3 seconds

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/CycleTurretProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/CycleTurretProjectile.kt
@@ -1,9 +1,15 @@
 package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
 
+import net.horizonsend.ion.common.extensions.informationAction
+import net.horizonsend.ion.common.extensions.userErrorAction
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.multiblock.starshipweapon.turret.CycleTurretMultiblock
+import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.active.ActiveStarships
 import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.util.Vector
@@ -24,6 +30,7 @@ class CycleTurretProjectile(
     override val balancing: StarshipWeapons.ProjectileBalancing = ship?.balancing?.weapons?.cycleTurret ?: IonServer.starshipBalancing.nonStarshipFired.cycleTurret,
     shooter: Damager,
     private val shotIndex: Int,
+    private val multiblock: CycleTurretMultiblock
 ) : LaserProjectile(ship, loc, dir, shooter) {
 
     override val volume: Int = (range / 16).toInt()
@@ -40,5 +47,22 @@ class CycleTurretProjectile(
         super.fire()
 
         this.speed = 1.0
+    }
+
+    override fun onImpactStarship(starship: ActiveStarship, impactLocation: Location) {
+        if (starship is ActiveControlledStarship) {
+            multiblock.slowedStarships[starship] = System.currentTimeMillis() + 3000
+            starship.userErrorAction("Direct Control speed slowed by 80%!")
+            starship.directControlCooldown = starship.initialDirectControlCooldown * 5
+
+            Tasks.syncDelay(3 * 20L) {
+                val unslowTime = multiblock.slowedStarships[starship]?.minus(100) ?: 0 // 100 buffer
+                if (ActiveStarships.isActive(starship) && unslowTime < System.currentTimeMillis()) {
+                    multiblock.slowedStarships[starship]?.let { multiblock.slowedStarships.remove(starship) }
+                    starship.directControlCooldown = starship.initialDirectControlCooldown
+                    starship.informationAction("Direct Control speed restored")
+                }
+            }
+        }
     }
 }
