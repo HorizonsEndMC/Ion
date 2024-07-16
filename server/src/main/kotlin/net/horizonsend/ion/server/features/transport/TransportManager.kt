10fc58376a7593cf2472eeece33629d1cddb7e9b
Gutin1 <Claytonpinter@gmail.com>
2024-07-16 12:18:52 -0500

    half finished worldedit listener

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/TransportManager.kt
@@ -0,0 +1,65 @@
+package net.horizonsend.ion.server.features.transport
+
+import net.horizonsend.ion.server.IonServerComponent
+import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot
+import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot.Companion.snapshot
+import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import org.bukkit.World
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+
+object TransportManager : IonServerComponent() {
+	@EventHandler
+	fun onBlockBreak(event: BlockBreakEvent) {
+		val world = event.block.world
+		val key = toBlockKey(event.block.x, event.block.y, event.block.z)
+
+		handleBlockRemoval(world, key)
+	}
+
+	@EventHandler
+	fun onBlockPlace(event: BlockPlaceEvent) {
+		val world = event.block.world
+
+		handleBlockAddition(world, event.block.snapshot())
+	}
+
+	fun handleBlockAddition(world: World, new: BlockSnapshot) {
+		val chunkX = new.x.shr(4)
+		val chunkZ = new.z.shr(4)
+
+		val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
+		chunk.transportNetwork.processBlockAddition(new)
+	}
+
+	fun handleBlockAdditions(world: World, newBlocks: Iterable<BlockSnapshot>) {
+		for (new in newBlocks) {
+			handleBlockAddition(world, new)
+		}
+	}
+
+	fun handleBlockRemoval(world: World, key: BlockKey) {
+		val chunkX = getX(key).shr(4)
+		val chunkZ = getZ(key).shr(4)
+
+		val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
+
+		chunk.transportNetwork.processBlockRemoval(key)
+	}
+
+	fun handleBlockRemovals(world: World, keys: Iterable<BlockKey>) {
+		for (key in keys) {
+			val chunkX = getX(key).shr(4)
+			val chunkZ = getZ(key).shr(4)
+
+			val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
+
+			chunk.transportNetwork.processBlockRemoval(key)
+		}
+	}
+}
