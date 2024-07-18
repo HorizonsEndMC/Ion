9942be9020a7d5d569645a656ca2f202af50f331
Gutin1 <Claytonpinter@gmail.com>
2024-07-18 13:13:21 -0500

    equal splitter node

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/HeadHolder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/HeadHolder.kt
@@ -1,7 +1,10 @@
 package net.horizonsend.ion.server.features.transport.step.head
 
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
+import net.horizonsend.ion.server.features.transport.step.origin.StepOrigin
 
 interface HeadHolder<T: ChunkTransportNetwork> {
 	var head: BranchHead<T>
+
+	fun getOrigin(): StepOrigin<T>
 }
