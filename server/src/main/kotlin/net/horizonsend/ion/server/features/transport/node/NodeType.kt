ec93e27c2feeadfcd4d3dbec9e6c9d2334e2a485
Gutin1 <Claytonpinter@gmail.com>
2024-07-18 13:13:21 -0500

    equal splitter node

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
@@ -4,6 +4,7 @@ import com.manya.pdc.base.EnumDataType
 import net.horizonsend.ion.server.features.transport.network.ChunkTransportNetwork
 import net.horizonsend.ion.server.features.transport.node.power.DirectionalNode
 import net.horizonsend.ion.server.features.transport.node.power.EndRodNode
+import net.horizonsend.ion.server.features.transport.node.power.PowerEqualSplitterNode
 import net.horizonsend.ion.server.features.transport.node.power.PowerExtractorNode
 import net.horizonsend.ion.server.features.transport.node.power.PowerFlowMeter
 import net.horizonsend.ion.server.features.transport.node.power.PowerInputNode
@@ -18,7 +19,9 @@ enum class NodeType(val clazz: Class<out TransportNode>) {
 	POWER_EXTRACTOR_NODE(PowerExtractorNode::class.java),
 	POWER_INPUT_NODE(PowerInputNode::class.java),
 	POWER_FLOW_METER(PowerFlowMeter::class.java),
-	MERGE_NODE(DirectionalNode::class.java),
+	POWER_DIRECTIONAL_NODE(DirectionalNode::class.java),
+//	POWER_INVERSE_DIRECTIONAL_NODE(InvertedDirectionalNode::class.java),
+	POWER_EQUAL_SPLITTER(PowerEqualSplitterNode::class.java),
 
 	//GAS
 
