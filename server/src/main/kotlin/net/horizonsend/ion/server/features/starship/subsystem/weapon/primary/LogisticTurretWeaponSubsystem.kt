63c982ae79481ed0f9faf222d3c675934f114766
kwazedilla <kwazedilla@gmail.com>
2024-07-24 23:02:24 -0700

    logistic turret start

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/LogisticTurretWeaponSubsystem.kt
@@ -0,0 +1,22 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.primary
+
+import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.multiblock.starshipweapon.turret.LogisticTurretMultiblock
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.TurretWeaponSubsystem
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.block.BlockFace
+import java.util.concurrent.TimeUnit
+
+class LogisticTurretWeaponSubsystem(
+    ship: ActiveStarship,
+    pos: Vec3i,
+    face: BlockFace,
+    override val multiblock: LogisticTurretMultiblock
+) : TurretWeaponSubsystem(ship, pos, face) {
+    override val balancing: StarshipWeapons.StarshipWeapon = starship.balancing.weapons.logisticTurret
+    override val inaccuracyRadians: Double get() = Math.toRadians(balancing.inaccuracyRadians)
+    override val powerUsage: Int get() = balancing.powerUsage
+    override var fireCooldownNanos: Long = TimeUnit.MILLISECONDS.toNanos(balancing.fireCooldownMillis)
+    override fun getMaxPerShot(): Int = balancing.maxPerShot
+}
