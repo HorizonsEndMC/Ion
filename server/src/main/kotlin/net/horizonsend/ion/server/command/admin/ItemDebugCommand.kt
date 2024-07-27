050a882c77a19e05288f4870fda402795e2938f5
Gutin1 <Claytonpinter@gmail.com>
2024-07-27 00:36:57 -0500

    permission lock itemdebug

--- server/src/main/kotlin/net/horizonsend/ion/server/command/admin/ItemDebugCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/admin/ItemDebugCommand.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.server.command.admin
 import co.aikar.commands.InvalidCommandArgument
 import co.aikar.commands.PaperCommandManager
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
 import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.server.command.SLCommand
@@ -10,9 +11,11 @@ import net.horizonsend.ion.server.features.custom.items.CustomItems.customItem
 import net.horizonsend.ion.server.features.custom.items.mods.ItemModRegistry
 import net.horizonsend.ion.server.features.custom.items.mods.ItemModification
 import net.horizonsend.ion.server.features.custom.items.objects.ModdedCustomItem
+import net.horizonsend.ion.server.features.custom.items.powered.PoweredItem
 import org.bukkit.entity.Player
 
 @CommandAlias("itemdebug")
+@CommandPermission("ion.debug.command.item")
 object ItemDebugCommand : SLCommand() {
 	override fun onEnable(manager: PaperCommandManager) {
 		manager.commandContexts.registerContext(ItemModification::class.java) {
@@ -53,5 +56,15 @@ object ItemDebugCommand : SLCommand() {
 		custom.removeMod(item, mod)
 
 		sender.information("Removed ${mod.identifier}")
+	}
+
+	@Subcommand("set power")
+	fun onSetPower(sender: Player, amount: Int) {
+		val item = sender.inventory.itemInMainHand
+		val custom = item.customItem as? PoweredItem ?: fail { "${item.customItem?.identifier} is not powered" }
+
+		custom.setPower(item, amount)
+
+		sender.information("Removed power to $amount")
 	}
 }
