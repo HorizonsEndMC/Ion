38dc7c717366b1b2122e12e38f4548204608db15
Gutin1 <Claytonpinter@gmail.com>
2024-08-01 11:15:52 -0500

    destroyed ship smoke effect

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/movement/StarshipMovement.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/movement/StarshipMovement.kt
@@ -133,6 +133,7 @@ abstract class StarshipMovement(val starship: ActiveStarship, val newWorld: Worl
 			starship.calculateMinMax()
 			updateCenter()
 			updateSubsystems(world2)
+			updateMisc(world2)
 
 			onComplete()
 		}
@@ -295,6 +296,13 @@ abstract class StarshipMovement(val starship: ActiveStarship, val newWorld: Worl
 				}
 			}
 		}
+	}
+
+	private fun updateMisc(world2: World) {
+		starship.previousDestroyedBlocks = starship.destroyedBlockKeys
+
+		val new = starship.destroyedBlockKeys.mapTo(mutableListOf()) { displacedKey(it) }
+		starship.destroyedBlockKeys = new.toLongArray()
 	}
 
 	private fun exitPlanet(world: World, starship: ActiveControlledStarship): Boolean {
