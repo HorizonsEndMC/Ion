9c852852a09c52b4c68aaa330cee40b30c075a6a
kwazedilla <kwazedilla@gmail.com>
2024-08-04 20:16:19 -0700

    one-time teleport item; menu

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/item/PersonalTransporterGui.kt
@@ -0,0 +1,104 @@
+package net.horizonsend.ion.server.features.gui.custom.item
+
+import net.horizonsend.ion.server.features.gui.AbstractBackgroundPagedGui
+import net.horizonsend.ion.server.features.gui.GuiItems
+import net.horizonsend.ion.server.features.gui.GuiText
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import xyz.xenondevs.invui.gui.PagedGui
+import xyz.xenondevs.invui.gui.structure.Markers
+import xyz.xenondevs.invui.item.Item
+import xyz.xenondevs.invui.window.Window
+import kotlin.math.ceil
+import kotlin.math.min
+
+class PersonalTransporterGui(val player: Player) : AbstractBackgroundPagedGui {
+
+    companion object {
+        private const val SETTINGS_PER_PAGE = 5
+        private const val PAGE_NUMBER_VERTICAL_SHIFT = 4
+    }
+
+    override var currentWindow: Window? = null
+    // cache current player list
+    private val playerList = mutableListOf<Player>()
+
+    override fun createGui(): PagedGui<Item> {
+        val gui = PagedGui.items()
+
+        gui.setStructure(
+            "x x x x x x x x x",
+            "x x x x x x x x x",
+            "x x x x x x x x x",
+            "x x x x x x x x x",
+            "x x x x x x x x x",
+            "< . . . . . . . >"
+        )
+
+        gui.addIngredient('x', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
+            .addIngredient('<', GuiItems.LeftItem())
+            .addIngredient('>', GuiItems.RightItem())
+
+        // populate player list cache
+        playerList.addAll(Bukkit.getOnlinePlayers())
+
+        for (player in playerList) {
+            val button = GuiItems.PlayerHeadItem(player.uniqueId, player.name)
+            gui.addContent(button)
+
+            for (i in 1..8) {
+                gui.addContent(GuiItems.BlankItem(button))
+            }
+        }
+
+        return gui.build()
+    }
+
+    override fun createText(player: Player, currentPage: Int): Component {
+        // create a new GuiText builder
+        val header = "Personal Transporter"
+        val guiText = GuiText(header)
+        guiText.addBackground()
+
+        // get the index of the first setting to display for this page
+        val startIndex = currentPage * SETTINGS_PER_PAGE
+
+        for (buttonIndex in startIndex until min(startIndex + SETTINGS_PER_PAGE, playerList.size)) {
+
+            val title = text("Request TP to " + playerList[buttonIndex].name)
+            val line = (buttonIndex - startIndex) * 2
+
+            // player username
+            guiText.add(
+                component = title,
+                line = line,
+                horizontalShift = 21
+            )
+
+            // location
+            guiText.add(
+                component = text("in: " + playerList[buttonIndex].world.name),
+                line = line + 1,
+                horizontalShift = 21
+            )
+        }
+
+        // page number
+        val pageNumberString =
+            "${currentPage + 1} / ${ceil((playerList.size.toDouble() / SETTINGS_PER_PAGE)).toInt()}"
+        guiText.add(
+            text(pageNumberString),
+            line = 10,
+            GuiText.TextAlignment.CENTER,
+            verticalShift = PAGE_NUMBER_VERTICAL_SHIFT
+        )
+
+        return guiText.build()
+    }
+
+    fun openMainWindow() {
+        currentWindow = open(player).apply { open() }
+    }
+}
