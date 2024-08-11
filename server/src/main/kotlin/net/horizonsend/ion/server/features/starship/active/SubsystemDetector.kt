68578b489ac6afa4fc20e267ba87566a300f7eec
Gutin1 <Claytonpinter@gmail.com>
2024-08-11 13:57:16 -0500

    start on new gas collection system

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/SubsystemDetector.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/active/SubsystemDetector.kt
@@ -3,7 +3,6 @@ package net.horizonsend.ion.server.features.starship.active
 import net.horizonsend.ion.common.database.schema.Cryopod
 import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.features.multiblock.Multiblocks
-import net.horizonsend.ion.server.features.multiblock.misc.FuelTankMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.areashield.AreaShield
 import net.horizonsend.ion.server.features.multiblock.type.checklist.BargeReactorMultiBlock
 import net.horizonsend.ion.server.features.multiblock.type.checklist.BattleCruiserReactorMultiblock
@@ -12,6 +11,7 @@ import net.horizonsend.ion.server.features.multiblock.type.drills.DrillMultibloc
 import net.horizonsend.ion.server.features.multiblock.type.gravitywell.GravityWellMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.hyperdrive.HyperdriveMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.misc.AbstractMagazineMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.misc.FuelTankMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.misc.CryoPodMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.misc.LandingGearMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.navigationcomputer.NavigationComputerMultiblock
