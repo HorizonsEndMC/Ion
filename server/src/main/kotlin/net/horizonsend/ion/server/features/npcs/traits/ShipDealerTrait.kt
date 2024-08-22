3076ff35c957dafb126aeaf485550c7e7dcf0feb
Gutin1 <Claytonpinter@gmail.com>
2024-08-22 13:51:20 -0500

    allow ship dealers to have any name

--- server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/traits/ShipDealerTrait.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/traits/ShipDealerTrait.kt
@@ -1,14 +1,9 @@
 package net.horizonsend.ion.server.features.npcs.traits
 
-import net.citizensnpcs.api.event.NPCRightClickEvent
 import net.citizensnpcs.api.trait.Trait
 import net.citizensnpcs.api.trait.TraitName
-import org.bukkit.event.EventHandler
 
 @TraitName("ionshipdealer")
 class ShipDealerTrait : Trait("ionshipdealer") {
-	@EventHandler
-	fun onNPCInteract(event: NPCRightClickEvent) {
-		println("b")
-	}
+	//TODO varied ships
 }
