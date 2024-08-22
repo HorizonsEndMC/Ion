a063aee2741ce036b7d3a3b104f038976278b682
Gutin1 <Claytonpinter@gmail.com>
2024-08-22 13:51:33 -0500

    disable unfinished help command

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Commands.kt
@@ -22,7 +22,6 @@ import net.horizonsend.ion.server.command.misc.BountyCommand
 import net.horizonsend.ion.server.command.misc.DyeCommand
 import net.horizonsend.ion.server.command.misc.GToggleCommand
 import net.horizonsend.ion.server.command.misc.GlobalGameRuleCommand
-import net.horizonsend.ion.server.command.misc.HelpCommand
 import net.horizonsend.ion.server.command.misc.IonBroadcastCommand
 import net.horizonsend.ion.server.command.misc.ListCommand
 import net.horizonsend.ion.server.command.misc.LocatorCommands
@@ -171,7 +170,7 @@ val commands: List<SLCommand> = listOf(
 	FleetCommand,
 	LocatorCommands,
 	ItemDebugCommand,
-	HelpCommand,
+//	HelpCommand,
 	TutorialAdminCommand,
 	TutorialCommand,
 )
