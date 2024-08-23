1f9055e499b91f1fe96d49563fa3f323cf42d6fb
Gutin1 <Claytonpinter@gmail.com>
2024-08-23 13:52:52 -0500

    AI Phaser

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/secondary/AIPhaserWeaponSystem.kt
@@ -0,0 +1,69 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.secondary
+
+import net.horizonsend.ion.common.extensions.userError
+import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.CannonWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.HeavyWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.PermissionWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.PhaserProjectile
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.Location
+import org.bukkit.block.BlockFace
+import org.bukkit.block.data.FaceAttachable
+import org.bukkit.block.data.type.Grindstone
+import org.bukkit.block.data.type.Hopper
+import org.bukkit.util.Vector
+import java.util.concurrent.TimeUnit
+
+class AIPhaserWeaponSystem(
+	starship: ActiveStarship,
+	pos: Vec3i,
+	face: BlockFace
+) : CannonWeaponSubsystem(starship, pos, face),
+	HeavyWeaponSubsystem,
+	PermissionWeaponSubsystem {
+	override val permission: String = "ion.weapon.ai"
+	override val balancing: StarshipWeapons.StarshipWeapon = starship.balancing.weapons.phaser
+	override val length: Int = balancing.length
+	override val convergeDist: Double = balancing.convergeDistance
+	override val extraDistance: Int = balancing.extraDistance
+	override val angleRadiansHorizontal: Double = Math.toRadians(balancing.angleRadiansHorizontal)
+	override val angleRadiansVertical: Double = Math.toRadians(balancing.angleRadiansVertical) // unrestricted
+	override val powerUsage: Int = balancing.powerUsage
+	override val boostChargeNanos: Long = TimeUnit.SECONDS.toNanos(balancing.boostChargeSeconds)
+
+	override fun isAcceptableDirection(face: BlockFace) = true
+
+	override fun fire(loc: Location, dir: Vector, shooter: Damager, target: Vector?) {
+		if (starship.initialBlockCount > 12000) {
+			starship.userError("You can't fire phasers on a ship larger than 12000 blocks!")
+			return
+		}
+
+		fixDirections(loc)
+		PhaserProjectile(starship, loc, dir, shooter).fire()
+	}
+
+	private fun fixDirections(loc: Location) {
+		fixGrindstoneData(loc)
+		fixHopperData(loc)
+	}
+
+	private fun fixGrindstoneData(loc: Location) {
+		val grindstone = pos.toLocation(loc.world).add(face.direction.multiply(7)).block
+		val grindstoneData = grindstone.blockData as Grindstone
+		grindstoneData.attachedFace = FaceAttachable.AttachedFace.WALL
+		grindstoneData.facing = face
+		grindstone.setBlockData(grindstoneData, false)
+	}
+
+	private fun fixHopperData(loc: Location) {
+		val hopper = pos.toLocation(loc.world).add(face.direction.multiply(6)).block
+		val hopperData = hopper.blockData as Hopper
+		hopperData.isEnabled = false
+		hopperData.facing = face
+		hopper.setBlockData(hopperData, false)
+	}
+}
