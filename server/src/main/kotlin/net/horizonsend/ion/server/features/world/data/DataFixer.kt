37e7035d5aea588fb124a5d083d2b2829846bbdf
Gutin1 <Claytonpinter@gmail.com>
2024-08-28 13:47:23 -0500

    World multiblock manager, data upgraders, mob defender upgraded

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/data/DataFixer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/data/DataFixer.kt
@@ -1,5 +1,9 @@
 package net.horizonsend.ion.server.features.world.data
 
 interface DataFixer {
-	fun registerFixers()
+	/**
+	 * The data version this will apply to.
+	 * If the target data's version is lower than this value, this will be triggered
+	 **/
+	val dataVersion: Int
 }
