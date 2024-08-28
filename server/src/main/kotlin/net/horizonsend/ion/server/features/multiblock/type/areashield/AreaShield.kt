511778fcbffec70c04b29b9d7737679e2cf16ad5
Gutin1 <Claytonpinter@gmail.com>
2024-08-28 16:00:35 -0500

    bug fixes, utility command

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/areashield/AreaShield.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/areashield/AreaShield.kt
@@ -1,25 +1,36 @@
 package net.horizonsend.ion.server.features.multiblock.type.areashield
 
+import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.server.IonServer
-import net.horizonsend.ion.server.features.machine.AreaShields
+import net.horizonsend.ion.server.features.client.display.container.TextDisplayHandler
 import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.SimpleTextDisplayPoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.SimpleTextDisplayPoweredMultiblockEntity.Companion.createTextDisplayHandler
 import net.horizonsend.ion.server.features.multiblock.type.InteractableMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.PowerStoringMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.EntityMultiblock
+import net.horizonsend.ion.server.features.multiblock.world.ChunkMultiblockManager
+import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.POWER
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getSphereBlocks
-import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.Particle
+import org.bukkit.World
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType.INTEGER
 import java.util.concurrent.TimeUnit
 
-abstract class AreaShield(val radius: Int) : Multiblock(), PowerStoringMultiblock, InteractableMultiblock {
+abstract class AreaShield(val radius: Int) : Multiblock(), PowerStoringMultiblock, InteractableMultiblock, EntityMultiblock<AreaShield.AreaShieldEntity> {
 	override fun onTransformSign(player: Player, sign: Sign) {
-		AreaShields.register(sign.location, radius)
-		player.sendMessage(ChatColor.GREEN.toString() + "Area Shield created.")
+		player.success("Area Shield created.")
 	}
 
 	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) {
@@ -64,4 +75,53 @@ abstract class AreaShield(val radius: Int) : Multiblock(), PowerStoringMultibloc
 		null,
 		"&8Radius: &a$radius"
 	)
+
+	override fun createEntity(manager: ChunkMultiblockManager, data: PersistentMultiblockData, world: World, x: Int, y: Int, z: Int, signOffset: BlockFace): AreaShieldEntity {
+		return AreaShieldEntity(
+			manager,
+			this,
+			x,
+			y,
+			z,
+			world,
+			signOffset,
+			data.getAdditionalDataOrDefault(POWER, INTEGER, 0),
+			maxPower
+		)
+	}
+
+	class AreaShieldEntity(
+		manager: ChunkMultiblockManager,
+		override val multiblock: AreaShield,
+		x: Int,
+		y: Int,
+		z: Int,
+		world: World,
+		signDirection: BlockFace,
+		override var powerUnsafe: Int,
+		override val maxPower: Int
+	) : MultiblockEntity(manager, multiblock, x, y, z, world, signDirection), PoweredMultiblockEntity, SimpleTextDisplayPoweredMultiblockEntity {
+		override val powerDisplay: TextDisplayHandler = createTextDisplayHandler(this)
+
+		override fun onLoad() {
+			world.ion.multiblockManager.register(this)
+		}
+
+		override fun handleRemoval() {
+			world.ion.multiblockManager.deregister(this)
+			powerDisplay.remove()
+		}
+
+		override fun onUnload() {
+			world.ion.multiblockManager.deregister(this)
+		}
+
+		override fun updatePowerVisually() {
+			powerDisplay.update()
+		}
+
+		override fun storeAdditionalData(store: PersistentMultiblockData) {
+			store.addAdditionalData(POWER, INTEGER, getPower())
+		}
+	}
 }
