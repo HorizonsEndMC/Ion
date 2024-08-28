b37af95839012a7b48a6cf4284a1a1b416c10bf4
Gutin1 <Claytonpinter@gmail.com>
2024-08-28 13:47:23 -0500

    World multiblock manager, data upgraders, mob defender upgraded

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/data/ChunkDataFixer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/data/ChunkDataFixer.kt
@@ -1,15 +1,7 @@
 package net.horizonsend.ion.server.features.world.data
 
-import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntityFixer
 import net.horizonsend.ion.server.features.world.chunk.IonChunk
 
-object ChunkDataFixer {
-	const val DATA_VERSION = 1
-
-	fun upgrade(chunk: IonChunk) {
-		if (chunk.dataVersion == DATA_VERSION) return
-		if (chunk.dataVersion > DATA_VERSION) throw IllegalStateException("Invalid chunk data version!")
-
-		MultiblockEntityFixer.upgrade(chunk)
-	}
+interface ChunkDataFixer : DataFixer {
+	fun fix(chunk: IonChunk)
 }
