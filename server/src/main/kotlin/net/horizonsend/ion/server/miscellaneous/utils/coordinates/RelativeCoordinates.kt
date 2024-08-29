adbd936230c5419a7ecd0cc5873d13b8e890aa47
Gutin1 <Claytonpinter@gmail.com>
2024-08-28 23:19:59 -0500

    Naming, function standardization

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/coordinates/RelativeCoordinates.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/coordinates/RelativeCoordinates.kt
@@ -3,18 +3,18 @@ package net.horizonsend.ion.server.miscellaneous.utils.coordinates
 import net.horizonsend.ion.server.miscellaneous.utils.rightFace
 import org.bukkit.block.BlockFace
 
-fun toAbsolute(inwardFace: BlockFace, backFourth: Int, leftRight: Int, upDown: Int): Vec3i {
-	val rightFace = inwardFace.rightFace
+fun toAbsolute(forwardFace: BlockFace, backFourth: Int, leftRight: Int, upDown: Int): Vec3i {
+	val rightFace = forwardFace.rightFace
 
 	return Vec3i(
-		x = rightFace.modX * leftRight + inwardFace.modX * backFourth,
+		x = rightFace.modX * leftRight + forwardFace.modX * backFourth,
 		y = upDown,
-		z = rightFace.modZ * leftRight + inwardFace.modZ * backFourth
+		z = rightFace.modZ * leftRight + forwardFace.modZ * backFourth
 	)
 }
 
-fun getRelative(origin: Vec3i, face: BlockFace, backFourth: Int, leftRight: Int, upDown: Int): Vec3i {
-	val relative = toAbsolute(face, backFourth, leftRight, upDown)
+fun getRelative(origin: Vec3i, forwardFace: BlockFace, forward: Int, right: Int, up: Int): Vec3i {
+	val relative = toAbsolute(forwardFace, forward, right, up)
 
 	return origin + relative
 }
