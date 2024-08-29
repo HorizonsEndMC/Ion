feb37faf0129a1d8b68270c95ccd069842d7314b
Gutin1 <Claytonpinter@gmail.com>
2024-08-28 23:47:14 -0500

    more cleaning, add built in alternate sign names

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Listeners.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Listeners.kt
@@ -9,9 +9,8 @@ import net.horizonsend.ion.server.features.custom.items.blasters.BlasterListener
 import net.horizonsend.ion.server.features.custom.items.misc.PersonalTransporterManager
 import net.horizonsend.ion.server.features.custom.items.mods.ToolModMenu
 import net.horizonsend.ion.server.features.machine.CryoPods
-import net.horizonsend.ion.server.features.progression.achievements.AchievementListeners
-import net.horizonsend.ion.server.features.multiblock.type.InteractableMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.misc.AbstractTractorBeam
+import net.horizonsend.ion.server.features.progression.achievements.AchievementListeners
 import net.horizonsend.ion.server.features.space.encounters.EncounterManager
 import net.horizonsend.ion.server.features.starship.hyperspace.HyperspaceBeaconManager
 import net.horizonsend.ion.server.features.transport.TransportManager
@@ -55,7 +54,6 @@ val listeners: List<Listener> = listOf(
 	EntityListener,
 	FurnaceListener,
 	InteractListener,
-	InteractableMultiblock,
 	InventoryListener,
 	BowHitListener,
 
