7dd977e460cfd77613c3349214f05c7a8c3dacbc
Gutin1 <Claytonpinter@gmail.com>
2024-08-29 00:49:47 -0500

    basic piped resource registration, add gasses

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/GasOxidizer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gas/type/GasOxidizer.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.features.gas.type
 
 import net.horizonsend.ion.server.configuration.Gasses
+import net.horizonsend.ion.server.features.transport.fluids.types.GasFluid
 import net.kyori.adventure.text.Component
 import java.util.function.Supplier
 
@@ -9,6 +10,7 @@ abstract class GasOxidizer(
 	displayName: Component,
 	containerIdentifier: String,
 	configurationSupplier: Supplier<Gasses.GasConfiguration>,
+	transportedFluidSupplier: Supplier<GasFluid>,
 
 	val powerMultiplier: Double
-) : Gas(identifier, displayName, containerIdentifier, configurationSupplier)
+) : Gas(identifier, displayName, containerIdentifier, configurationSupplier, transportedFluidSupplier)
