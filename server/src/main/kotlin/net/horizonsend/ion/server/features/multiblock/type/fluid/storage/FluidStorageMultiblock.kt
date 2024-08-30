9c0e9a53db8e287596a57f73e019f11a2019a621
Gutin1 <Claytonpinter@gmail.com>
2024-08-30 17:01:31 -0500

    cleanup, make class to handle simple fluid storage cases

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
@@ -2,14 +2,12 @@ package net.horizonsend.ion.server.features.multiblock.type.fluid.storage
 
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.multiblock.Multiblock
-import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.BasicFluidStoringEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.StorageContainer
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.UnlimitedInternalStorage
 import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.EntityMultiblock
 import net.horizonsend.ion.server.features.multiblock.world.ChunkMultiblockManager
-import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
@@ -40,14 +38,5 @@ abstract class FluidStorageMultiblock(val capacity: Int) : Multiblock(), EntityM
 		z: Int,
 		world: World,
 		structureDirection: BlockFace,
-	) : MultiblockEntity(manager, multiblock, x, y, z, world, structureDirection), FluidStoringEntity {
-		override val capacities: Array<StorageContainer> = arrayOf(
-			loadStoredResource(data, "main", text("Main Storage"), NamespacedKeys.MAIN_STORAGE, UnlimitedInternalStorage(multiblock.capacity))
-		)
-
-		override fun storeAdditionalData(store: PersistentMultiblockData) {
-			val rawStorage = store.getAdditionalDataRaw()
-			storeStorageData(rawStorage, rawStorage.adapterContext)
-		}
-	}
+	) : BasicFluidStoringEntity(manager, multiblock, data, x, y, z, world, structureDirection, UnlimitedInternalStorage(multiblock.capacity)), FluidStoringEntity
 }
