5e76854dea805a6e2077228bb4cf4e1ed338ef55
Gutin1 <Claytonpinter@gmail.com>
2024-08-30 15:08:24 -0500

    Add fluid storage multiblocks

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidTankSmall.kt
@@ -0,0 +1,44 @@
+package net.horizonsend.ion.server.features.multiblock.type.fluid.storage
+
+import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
+
+object FluidTankSmall : FluidStorageMultiblock(25_000) {
+	override fun MultiblockShape.buildStructure() {
+		z(0) {
+			y(-1) {
+				x(-1).anyStairs()
+				x(+0).fluidInput()
+				x(+1).anyStairs()
+			}
+			y(+0) {
+				x(-1).anyStairs()
+				x(+0).extractor()
+				x(+1).anyStairs()
+			}
+		}
+		z(1) {
+			y(-1) {
+				x(-1).titaniumBlock()
+				x(+0).copperGrate()
+				x(+1).titaniumBlock()
+			}
+			y(+0) {
+				x(-1).titaniumBlock()
+				x(+0).copperGrate()
+				x(+1).titaniumBlock()
+			}
+		}
+		z(2) {
+			y(-1) {
+				x(-1).anyStairs()
+				x(+0).anyCopperVariant()
+				x(+1).anyStairs()
+			}
+			y(+0) {
+				x(-1).anyStairs()
+				x(+0).anyCopperVariant()
+				x(+1).anyStairs()
+			}
+		}
+	}
+}
