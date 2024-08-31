fe72da72ce635d32ac7d4e52f943ee217e055c2b
Gutin1 <Claytonpinter@gmail.com>
2024-08-31 13:57:18 -0500

    configuration for another

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidTankMedium.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidTankMedium.kt
@@ -1,16 +1,22 @@
 package net.horizonsend.ion.server.features.multiblock.type.fluid.storage
 
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.util.PrepackagedPreset.stairs
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace.OPPOSITE
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace.SELF
+import org.bukkit.block.data.Bisected.Half.BOTTOM
+import org.bukkit.block.data.Bisected.Half.TOP
+import org.bukkit.block.data.type.Stairs.Shape.STRAIGHT
 
 object FluidTankMedium : FluidStorageMultiblock(75_000) {
 	override fun MultiblockShape.buildStructure() {
 		z(0) {
 			y(-1) {
-				x(-2).anyStairs()
+				x(-2).anyStairs(stairs(SELF, TOP, shape = STRAIGHT))
 				x(-1).extractor()
 				x(+0).fluidInput()
 				x(+1).extractor()
-				x(+2).anyStairs()
+				x(+2).anyStairs(stairs(SELF, TOP, shape = STRAIGHT))
 			}
 			y(+0) {
 				x(-2).anyCopperVariant()
@@ -20,11 +26,11 @@ object FluidTankMedium : FluidStorageMultiblock(75_000) {
 				x(+2).anyCopperVariant()
 			}
 			y(+1) {
-				x(-2).anyStairs()
+				x(-2).anyStairs(stairs(SELF, BOTTOM, shape = STRAIGHT))
 				x(-1).titaniumBlock()
 				x(+0).copperGrate()
 				x(+1).titaniumBlock()
-				x(+2).anyStairs()
+				x(+2).anyStairs(stairs(SELF, BOTTOM, shape = STRAIGHT))
 			}
 		}
 		z(1) {
@@ -52,11 +58,11 @@ object FluidTankMedium : FluidStorageMultiblock(75_000) {
 		}
 		z(2) {
 			y(-1) {
-				x(-2).anyStairs()
+				x(-2).anyStairs(stairs(OPPOSITE, TOP, shape = STRAIGHT))
 				x(-1).titaniumBlock()
 				x(+0).copperGrate()
 				x(+1).titaniumBlock()
-				x(+2).anyStairs()
+				x(+2).anyStairs(stairs(OPPOSITE, TOP, shape = STRAIGHT))
 			}
 			y(+0) {
 				x(-2).anyCopperVariant()
@@ -66,11 +72,11 @@ object FluidTankMedium : FluidStorageMultiblock(75_000) {
 				x(+2).anyCopperVariant()
 			}
 			y(+1) {
-				x(-2).anyStairs()
+				x(-2).anyStairs(stairs(OPPOSITE, BOTTOM, shape = STRAIGHT))
 				x(-1).titaniumBlock()
 				x(+0).anyCopperBlock()
 				x(+1).titaniumBlock()
-				x(+2).anyStairs()
+				x(+2).anyStairs(stairs(OPPOSITE, BOTTOM, shape = STRAIGHT))
 			}
 		}
 	}
