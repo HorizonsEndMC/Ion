01295da4d10590c9298d5d333c05b2f021fc891a
Gutin1 <Claytonpinter@gmail.com>
2024-09-01 00:17:42 -0500

    rename gas network to fluid, build out some code

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/gas/GasJunctionNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/gas/GasJunctionNode.kt
@@ -1,11 +1,11 @@
 package net.horizonsend.ion.server.features.transport.node.gas
 
-import net.horizonsend.ion.server.features.transport.network.GasNetwork
+import net.horizonsend.ion.server.features.transport.network.FluidNetwork
 import net.horizonsend.ion.server.features.transport.node.general.JunctionNode
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 
-class GasJunctionNode(network: GasNetwork) : JunctionNode<GasNetwork, GasJunctionNode, GasJunctionNode>(network) {
-	constructor(network: GasNetwork, origin: BlockKey) : this(network) {
+class GasJunctionNode(network: FluidNetwork) : JunctionNode<FluidNetwork, GasJunctionNode, GasJunctionNode>(network) {
+	constructor(network: FluidNetwork, origin: BlockKey) : this(network) {
 		positions.add(origin)
 	}
 
--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/gas/ExtractedGasOrigin.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/gas/ExtractedGasOrigin.kt
@@ -1,9 +1,9 @@
 package net.horizonsend.ion.server.features.transport.step.origin.gas
 
-import net.horizonsend.ion.server.features.transport.network.GasNetwork
-import net.horizonsend.ion.server.features.transport.node.gas.GasExtractorNode
+import net.horizonsend.ion.server.features.transport.network.FluidNetwork
+import net.horizonsend.ion.server.features.transport.node.gas.FluidExtractorNode
 import net.horizonsend.ion.server.features.transport.step.origin.StepOrigin
 
 class ExtractedGasOrigin(
-	val extractorNode: GasExtractorNode
-) : StepOrigin<GasNetwork>
+	val extractorNode: FluidExtractorNode
+) : StepOrigin<FluidNetwork>
