8a4c9f0adf2af1f1bac1237ef4440269180f50c4
Gutin1 <Claytonpinter@gmail.com>
2024-08-31 23:04:27 -0500

    register water, electrolysis multi structure

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/fluids/storage/SingleFluidStorage.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/fluids/storage/SingleFluidStorage.kt
@@ -1,17 +1,14 @@
-package net.horizonsend.ion.server.features.multiblock.entity.type.fluids
+package net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage
 
 import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
 
-/**
- * Internal storage with no limits on what fluid can be stored
- **/
-class UnlimitedInternalStorage(private val storageCapacity: Int) : InternalStorage() {
+class SingleFluidStorage(private val storageCapacity: Int, private val restrictedFluid: PipedFluid) : InternalStorage() {
 	override fun getCapacity(): Int = storageCapacity
 
 	override fun canStore(resource: PipedFluid, liters: Int): Boolean {
 		if (liters + getAmount() > getCapacity()) return false
 
 		// Check that the fluid attempting to be stored is the same as the one currently stored
-		return !(getStoredFluid() != null && resource != getStoredFluid())
+		return (getStoredFluid() == null || resource == restrictedFluid)
 	}
 }
