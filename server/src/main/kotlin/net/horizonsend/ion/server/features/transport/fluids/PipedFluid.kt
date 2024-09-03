42e00f3067cb6e914346e21ddfdf4361b297778f
Gutin1 <Claytonpinter@gmail.com>
2024-09-03 14:25:01 -0500

    display improvements, add to ElectrolysisMultiblock

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/PipedFluid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/PipedFluid.kt
@@ -1,8 +1,10 @@
 package net.horizonsend.ion.server.features.transport.fluids
 
 import net.horizonsend.ion.server.features.transport.fluids.properties.FluidCategory
+import net.kyori.adventure.text.Component
 
 abstract class PipedFluid(val identifier: String) {
+	abstract val displayName: Component
 	abstract val categories: Array<FluidCategory>
 
 	override fun toString(): String {
