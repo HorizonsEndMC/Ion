8cfb909188b4b2091ce272f741e1154d57fb4068
Gutin1 <Claytonpinter@gmail.com>
2024-09-03 00:55:58 -0500

    make new multiblock code the default, cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/misc/DecomposerMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/misc/DecomposerMultiblock.kt
@@ -1,14 +1,18 @@
 package net.horizonsend.ion.server.features.multiblock.type.misc
 
+import net.horizonsend.ion.server.features.machine.decomposer.Decomposers
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
+import net.horizonsend.ion.server.features.multiblock.type.InteractableMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.PowerStoringMultiblock
 import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 
-object DecomposerMultiblock : Multiblock(), PowerStoringMultiblock {
+object DecomposerMultiblock : Multiblock(), PowerStoringMultiblock, InteractableMultiblock {
 	override val maxPower: Int = 75_000
 	override val name: String = "decomposer"
 	override val signText = createSignText(
@@ -25,5 +29,9 @@ object DecomposerMultiblock : Multiblock(), PowerStoringMultiblock {
 
 	fun getStorage(sign: Sign): Inventory {
 		return (sign.block.getRelative(BlockFace.DOWN).state as InventoryHolder).inventory
+	}
+
+	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) {
+		Decomposers.onClick(event)
 	}
 }
