7b9c5deee881510549c740a792f5f5957e3d02a9
Gutin1 <Claytonpinter@gmail.com>
2024-09-03 01:07:25 -0500

    organize multiblocks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/checklist/BargeReactorSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/checklist/BargeReactorSubsystem.kt
@@ -1,8 +1,9 @@
 package net.horizonsend.ion.server.features.starship.subsystem.checklist
 
+import net.horizonsend.ion.server.features.multiblock.type.starship.checklist.BargeReactorMultiBlock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.subsystem.AbstractMultiblockSubsystem
 import org.bukkit.block.Sign
 
-class BargeReactorSubsystem (starship: ActiveStarship, sign: Sign, multiblock: net.horizonsend.ion.server.features.multiblock.type.checklist.BargeReactorMultiBlock) :
-        AbstractMultiblockSubsystem<net.horizonsend.ion.server.features.multiblock.type.checklist.BargeReactorMultiBlock>(starship, sign, multiblock)
+class BargeReactorSubsystem (starship: ActiveStarship, sign: Sign, multiblock: BargeReactorMultiBlock) :
+        AbstractMultiblockSubsystem<BargeReactorMultiBlock>(starship, sign, multiblock)
