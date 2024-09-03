262992d85c74941d34630ba37cfde4f885bb705e
Gutin1 <Claytonpinter@gmail.com>
2024-09-03 01:11:16 -0500

    forgot to select some files

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/QuadTurretWeaponSubsystem.kt
@@ -2,7 +2,7 @@ package net.horizonsend.ion.server.features.starship.subsystem.weapon.primary
 
 import net.horizonsend.ion.server.configuration.StarshipWeapons
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry
-import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.turret.QuadTurretMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.starship.weapon.turret.QuadTurretMultiblock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.TurretWeaponSubsystem
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.AmmoConsumingWeaponSubsystem
