56f134bb45b9830e2b5e2e81738bb76226df4e8f
Gutin1 <Claytonpinter@gmail.com>
2024-09-03 14:25:01 -0500

    display improvements, add to ElectrolysisMultiblock

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/types/GasPipedFluid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/types/GasPipedFluid.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.server.features.transport.fluids.types
 import net.horizonsend.ion.server.features.gas.type.Gas
 import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
 import net.horizonsend.ion.server.features.transport.fluids.properties.FluidCategory
+import net.kyori.adventure.text.Component
 import java.util.function.Supplier
 
 class GasPipedFluid(
@@ -11,5 +12,6 @@ class GasPipedFluid(
 ) : PipedFluid(identifier) {
 	override val categories: Array<FluidCategory> = arrayOf(FluidCategory.GAS)
 
+	override val displayName: Component get() = gas.displayName
 	val gas get() = gasSupplier.get()
 }
