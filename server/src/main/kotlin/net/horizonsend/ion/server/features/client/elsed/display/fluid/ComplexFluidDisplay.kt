1c5c1b0e0d30836a827b7e1ff2586a1bca435333
Gutin1 <Claytonpinter@gmail.com>
2024-09-04 00:19:25 -0500

    remove background

--- server/src/main/kotlin/net/horizonsend/ion/server/features/client/elsed/display/fluid/ComplexFluidDisplay.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/client/elsed/display/fluid/ComplexFluidDisplay.kt
@@ -1,13 +1,10 @@
 package net.horizonsend.ion.server.features.client.elsed.display.fluid
 
 import net.horizonsend.ion.common.utils.text.ofChildren
-import net.horizonsend.ion.server.features.client.elsed.TextDisplayHandler
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage.StorageContainer
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.newline
-import org.bukkit.Color
 import org.bukkit.block.BlockFace
-import org.bukkit.craftbukkit.v1_20_R3.entity.CraftTextDisplay
 
 class ComplexFluidDisplay(
 	storage: StorageContainer,
@@ -18,11 +15,6 @@ class ComplexFluidDisplay(
 	face: BlockFace,
 	scale: Float
 ) : FluidDisplay(storage, offsetLeft, offsetUp, offsetBack, face, scale) {
-	override fun createEntity(parent: TextDisplayHandler): CraftTextDisplay {
-		return super.createEntity(parent).apply {
-			backgroundColor = Color.fromARGB(0x00BBBBBB)
-		}
-	}
 
 	override fun getText(): Component {
 		return ofChildren(title, newline(), formatFluid())
