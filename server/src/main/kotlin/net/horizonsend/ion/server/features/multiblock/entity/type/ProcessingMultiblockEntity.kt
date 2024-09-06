dd5a0635ba7484ec733703b1646f0ff63fea4208
Gutin1 <Claytonpinter@gmail.com>
2024-09-05 22:19:02 -0500

    abstract out chunk multiblock manager to allow ship multiblock manager

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ProcessingMultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ProcessingMultiblockEntity.kt
@@ -4,7 +4,7 @@ import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.world.ChunkMultiblockManager
+import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import org.bukkit.World
 import org.bukkit.block.BlockFace
@@ -17,15 +17,15 @@ import org.bukkit.persistence.PersistentDataType
  * @param currentProgress The current progress of the process
  **/
 abstract class ProcessingMultiblockEntity(
-    manager: ChunkMultiblockManager,
-    type: Multiblock,
-    x: Int,
-    y: Int,
-    z: Int,
-    world: World,
-    signOffset: BlockFace,
-    val requiredProgress: Int,
-    var currentProgress: Int = 0
+	manager: MultiblockManager,
+	type: Multiblock,
+	x: Int,
+	y: Int,
+	z: Int,
+	world: World,
+	signOffset: BlockFace,
+	val requiredProgress: Int,
+	var currentProgress: Int = 0
 ) : MultiblockEntity(manager, type, x, y, z, world, signOffset), SyncTickingMultiblockEntity {
 	//TODO
 	// -recipe system
