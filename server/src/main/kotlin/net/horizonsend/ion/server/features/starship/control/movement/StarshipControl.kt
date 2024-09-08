972b691eb569144d1219495f808c563dae2e762a
Gutin1 <Claytonpinter@gmail.com>
2024-09-08 17:08:08 -0500

    migrate direct control

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/StarshipControl.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/StarshipControl.kt
@@ -1,217 +1,21 @@
 package net.horizonsend.ion.server.features.starship.control.movement
 
-import io.papermc.paper.entity.TeleportFlag
-import net.horizonsend.ion.common.extensions.userErrorAction
 import net.horizonsend.ion.common.utils.miscellaneous.d
 import net.horizonsend.ion.server.IonServerComponent
-import net.horizonsend.ion.server.features.cache.PlayerCache
-import net.horizonsend.ion.server.features.nations.utils.getPing
 import net.horizonsend.ion.server.features.space.Space
 import net.horizonsend.ion.server.features.starship.StarshipType.BATTLECRUISER
-import net.horizonsend.ion.server.features.starship.StarshipType.PLATFORM
 import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
-import net.horizonsend.ion.server.features.starship.active.ActiveStarships
-import net.horizonsend.ion.server.features.starship.hyperspace.Hyperspace
 import net.horizonsend.ion.server.features.starship.movement.StarshipTeleportation
-import net.horizonsend.ion.server.features.starship.movement.TranslateMovement
-import net.horizonsend.ion.server.miscellaneous.utils.Tasks
-import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Location
 import org.bukkit.Material
-import org.bukkit.event.player.PlayerTeleportEvent
-import org.bukkit.util.Vector
-import java.util.Collections
-import kotlin.math.abs
 import kotlin.math.max
-import kotlin.math.min
 import kotlin.math.pow
-import kotlin.math.round
-import kotlin.math.sign
 
 object StarshipControl : IonServerComponent() {
 	val CONTROLLER_TYPE = Material.CLOCK
-	const val DIRECT_CONTROL_DIVISOR = 1.75
-
-	override fun onEnable() {
-		Tasks.syncRepeat(1, 1) { ActiveStarships.allControlledStarships().forEach(::processManualFlight) }
-	}
-
-	private fun processManualFlight(starship: ActiveControlledStarship) {
-		if (starship.type == PLATFORM) {
-			starship.controller.userErrorAction("This ship type is not capable of moving.")
-			return
-		}
-
-		if (starship.isTeleporting) return
-
-		if (starship.isDirectControlEnabled) {
-			processDirectControl(starship)
-			return
-		}
-	}
-
-	private fun processDirectControl(starship: ActiveControlledStarship) {
-		val controller = starship.controller
-
-		if (starship.isTeleporting) return
-
-		if (starship.type == PLATFORM) return controller.userErrorAction("This ship type is not capable of moving.")
-
-		if (Hyperspace.isWarmingUp(starship) || Hyperspace.isMoving(starship)) {
-			starship.setDirectControlEnabled(false)
-			return
-		}
-
-		val playerPilot = starship.playerPilot
-
-		// Ping compensation
-		val ping = playerPilot?.let { getPing(playerPilot) }
-		val movementCooldown = starship.directControlCooldown
-		val playerDcModifier = playerPilot?.let { PlayerCache[playerPilot.uniqueId].dcSpeedModifier } ?: 1
-		val speedFac = if (ping != null && ping > movementCooldown) max(2, playerDcModifier) else playerDcModifier
-
-		val selectedSpeed = (controller.selectedDirectControlSpeed * starship.directControlSpeedModifier).toInt().coerceAtLeast(0)
-
-		val cooldown = calculateCooldown(movementCooldown, selectedSpeed) * speedFac
-		val currentTime = System.currentTimeMillis()
-		val lastManualMove = starship.lastManualMove
-
-		val elapsedSinceLastMove = currentTime - lastManualMove
-		if (elapsedSinceLastMove < cooldown) return
-
-		starship.lastManualMove = currentTime
-
-		// Start calculating offset
-		var dx = 0
-		var dy = 0
-		var dz = 0
-
-		val direction = starship.getTargetForward()
-		val targetSpeed = calculateSpeed(selectedSpeed)
-
-		dx += (targetSpeed * direction.modX)
-		dz += (targetSpeed * direction.modZ)
-
-		// Boost if shift flying
-		if (controller.isSneakFlying()) {
-			dx *= 2
-			dz *= 2
-		}
-
-		// Use the player's location if available, otherwise the computer loc
-		val pilotLocation = playerPilot?.location ?: Vec3i(starship.data.blockKey).toLocation(starship.world)
-
-		var center = starship.directControlCenter
-		if (center == null) {
-			center = pilotLocation.toBlockLocation().add(0.5, 0.0, 0.5)
-			starship.directControlCenter = center
-		}
-
-		// Calculate the movement vector
-		var vector = pilotLocation.toVector().subtract(center.toVector())
-		vector.setY(0)
-		vector.normalize()
-
-		// Clone the vector to do some additional math
-		val directionWrapper = center.clone()
-		directionWrapper.direction = Vector(direction.modX, direction.modY, direction.modZ)
-
-		val playerDirectionWrapper = center.clone()
-		playerPilot?.let { playerDirectionWrapper.direction = it.location.direction }
-
-		val vectorWrapper = center.clone()
-		vectorWrapper.direction = vector
-
-		vectorWrapper.yaw = vectorWrapper.yaw - (playerDirectionWrapper.yaw - directionWrapper.yaw)
-		vector = vectorWrapper.direction
-
-		vector.x = round(vector.x)
-		vector.setY(0)
-		vector.z = round(vector.z)
-
-		val vectors = starship.directControlPreviousVectors
-		if (vectors.size > 3) {
-			vectors.poll()
-		}
-		vectors.add(vector)
-
-		playerPilot?.let {
-			if (vector.x != 0.0 || vector.z != 0.0) {
-				val newLoc = center.clone()
-
-				newLoc.pitch = playerPilot.location.pitch
-				newLoc.yaw = playerPilot.location.yaw
-
-				playerPilot.teleport(
-					newLoc,
-					PlayerTeleportEvent.TeleportCause.PLUGIN,
-					*TeleportFlag.Relative.values(),
-					TeleportFlag.EntityState.RETAIN_OPEN_INVENTORY
-				)
-			}
-		}
-
-		var highestFrequency = Collections.frequency(vectors, vector)
-		for (previousVector in vectors) {
-			val frequency = Collections.frequency(vectors, previousVector)
-			if (previousVector != vector && frequency > highestFrequency) {
-				highestFrequency = frequency
-				vector = previousVector
-			}
-		}
-
-		val forwardZ = direction.modZ != 0
-		val strafeAxis = if (forwardZ) vector.x else vector.z
-		val strafe = sign(strafeAxis).toInt() * abs(targetSpeed)
-		val ascensionAxis = if (forwardZ) vector.z * -direction.modZ else vector.x * -direction.modX
-		val ascension = sign(ascensionAxis).toInt() * abs(targetSpeed)
-		if (forwardZ) dx += strafe else dz += strafe
-		dy += ascension
-
-		val deltaTime = elapsedSinceLastMove / 1000.0
-		val maxChange = 15 * starship.reactor.powerDistributor.thrusterPortion * deltaTime
-
-		val directControlVec = starship.directControlVector
-		val offset = Vector(dx, dy, dz).subtract(directControlVec)
-		if (offset.length() > maxChange) {
-			offset.normalize().multiply(maxChange)
-		}
-		directControlVec.add(offset)
-		dx = directControlVec.blockX
-		dy = directControlVec.blockY
-		dz = directControlVec.blockZ
-
-		dx *= speedFac
-		dy *= speedFac
-		dz *= speedFac
-
-		when {
-			dy < 0 && starship.min.y + dy < 0 -> {
-				dy = -starship.min.y
-			}
-
-			dy > 0 && starship.max.y + dy > starship.world.maxHeight -> {
-				dy = starship.world.maxHeight - starship.max.y
-			}
-		}
-
-		if (locationCheck(starship, dx, dy, dz)) {
-			return
-		}
-
-		if (dx == 0 && dy == 0 && dz == 0) {
-			return
-		}
-
-		playerPilot?.walkSpeed = 0.009f
-		TranslateMovement.loadChunksAndMove(starship, dx, dy, dz)
-	}
-
-	fun calculateSpeed(selectedSlot: Int) = if (selectedSlot == 0) -1 else (selectedSlot / DIRECT_CONTROL_DIVISOR).toInt()
-	fun calculateCooldown(movementCooldown: Long, heldItemSlot: Int) = movementCooldown - heldItemSlot * 8
 
 	fun locationCheck(starship: ActiveControlledStarship, dx: Int, dy: Int, dz: Int): Boolean {
 		val world = starship.world
@@ -255,14 +59,5 @@ object StarshipControl : IonServerComponent() {
 
 		StarshipTeleportation.teleportStarship(starship, target)
 		return true
-	}
-
-	private fun accel(old: Double, new: Double, maxChange: Double): Double {
-		val diff = new - old
-		if (diff < maxChange) {
-			return new
-		}
-
-		return old + min(maxChange, abs(diff)) * sign(diff)
 	}
 }
