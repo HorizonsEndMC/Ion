c0af67be0185ed3dff0bcf617939b9e5247f1617
Gutin1 <Claytonpinter@gmail.com>
2024-09-08 17:45:50 -0500

    create ship class menu dynamically

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/TypeCategory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/TypeCategory.kt
@@ -1,8 +1,8 @@
 package net.horizonsend.ion.server.features.starship
 
-enum class TypeCategory {
-	WAR_SHIP,
-	TRADE_SHIP,
-	SPECIALTY,
-	MISC,
+enum class TypeCategory(val index: Int) {
+	WAR_SHIP(2),
+	TRADE_SHIP(1),
+	SPECIALTY(3),
+	MISC(4),
 }
