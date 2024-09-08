1b0c89216ce976bef6507936740edb47c94dadb9
Gutin1 <Claytonpinter@gmail.com>
2024-09-08 00:06:17 -0500

    most of the change class menu

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/starship/pilots/ManagePilotsMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/starship/pilots/ManagePilotsMenu.kt
@@ -1,4 +1,4 @@
-package net.horizonsend.ion.server.features.gui.custom.starship
+package net.horizonsend.ion.server.features.gui.custom.starship.pilots
 
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
 import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
@@ -8,6 +8,7 @@ import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.common.utils.text.ITALIC
 import net.horizonsend.ion.server.features.gui.GuiItems
+import net.horizonsend.ion.server.features.gui.custom.starship.StarshipComputerMenu
 import net.horizonsend.ion.server.features.nations.gui.skullItem
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.setDisplayNameAndGet
