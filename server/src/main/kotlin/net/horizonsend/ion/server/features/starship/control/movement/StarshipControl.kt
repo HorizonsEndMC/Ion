721ac3f82c0a288acc1fc44458810f7382e8722c
Gutin1 <Claytonpinter@gmail.com>
2024-09-08 19:19:35 -0500

    add back simple AI Movement processing

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/StarshipControl.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/movement/StarshipControl.kt
@@ -5,17 +5,66 @@ import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.features.space.Space
 import net.horizonsend.ion.server.features.starship.StarshipType.BATTLECRUISER
 import net.horizonsend.ion.server.features.starship.active.ActiveControlledStarship
+import net.horizonsend.ion.server.features.starship.active.ActiveStarships
+import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
+import net.horizonsend.ion.server.features.starship.hyperspace.Hyperspace
 import net.horizonsend.ion.server.features.starship.movement.StarshipTeleportation
+import net.horizonsend.ion.server.features.starship.movement.TranslateMovement
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Location
 import org.bukkit.Material
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sin
 
 object StarshipControl : IonServerComponent() {
 	val CONTROLLER_TYPE = Material.CLOCK
+
+	override fun onEnable() {
+		Tasks.syncRepeat(1, 1, ::moveAIShips)
+	}
+
+	private fun moveAIShips() {
+		val aIShips = ActiveStarships.all().filter { it.controller is AIController }
+
+		for (starship in aIShips) {
+			val controller = starship.controller as? AIController ?: continue
+			val sneakMovements = controller.sneakMovements
+
+			if (Hyperspace.isWarmingUp(starship)) return
+			if (!controller.isSneakFlying()) return
+
+			val now = System.currentTimeMillis()
+			if (now - starship.lastManualMove < starship.manualMoveCooldownMillis) return
+			starship.lastManualMove = now
+
+			val maxAccel = starship.balancing.maxSneakFlyAccel
+			val accelDistance = starship.balancing.sneakFlyAccelDistance
+
+			val yawRadians = Math.toRadians(controller.yaw.toDouble())
+			val pitchRadians = Math.toRadians(controller.pitch.toDouble())
+
+			val distance = max(min(maxAccel, sneakMovements / min(1, accelDistance)), 1)
+
+			val vertical = abs(pitchRadians) >= PI * 5 / 12 // 75 degrees
+
+			val dx = if (vertical) 0 else sin(-yawRadians).roundToInt() * distance
+			val dy = sin(-pitchRadians).roundToInt() * distance
+			val dz = if (vertical) 0 else cos(yawRadians).roundToInt() * distance
+
+			if (locationCheck(starship, dx, dy, dz)) return
+
+			TranslateMovement.loadChunksAndMove(starship, dx, dy, dz)
+		}
+	}
 
 	fun locationCheck(starship: ActiveControlledStarship, dx: Int, dy: Int, dz: Int): Boolean {
 		val world = starship.world
