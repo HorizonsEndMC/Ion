4194997437582e873472f008314d6639777d7e0c
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 01:26:41 -0500

    Remove the first attempt at transfer code

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/StepOrigin.kt
+++
@@ -1,6 +0,0 @@
-package net.horizonsend.ion.server.features.transport.step.origin
-
-import net.horizonsend.ion.server.features.transport.network.TransportNetwork
-
-interface StepOrigin<T: TransportNetwork>
-
