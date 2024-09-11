07bbcefc68d7a3a4c2c34f2eb3eb57941f0838a9
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 01:26:41 -0500

    Remove the first attempt at transfer code

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/InvertedDirectionalNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/InvertedDirectionalNode.kt
@@ -5,19 +5,13 @@ import net.horizonsend.ion.server.features.transport.network.PowerNetwork
 import net.horizonsend.ion.server.features.transport.node.NodeRelationship
 import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.type.SingleNode
-import net.horizonsend.ion.server.features.transport.node.type.SourceNode
-import net.horizonsend.ion.server.features.transport.node.type.StepHandler
-import net.horizonsend.ion.server.features.transport.step.head.SingleBranchHead
-import net.horizonsend.ion.server.features.transport.step.result.MoveForward
-import net.horizonsend.ion.server.features.transport.step.result.StepResult
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
-import org.bukkit.block.BlockFace
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 import kotlin.properties.Delegates
 
-class InvertedDirectionalNode(override val network: PowerNetwork) : SingleNode, StepHandler<PowerNetwork> {
+class InvertedDirectionalNode(override val network: PowerNetwork) : SingleNode {
 	override var isDead: Boolean = false
 	override var position: BlockKey by Delegates.notNull()
 	override val relationships: MutableSet<NodeRelationship> = ObjectOpenHashSet()
@@ -28,17 +22,7 @@ class InvertedDirectionalNode(override val network: PowerNetwork) : SingleNode, 
 
 	override fun isTransferableTo(node: TransportNode): Boolean {
 		if (node is EndRodNode) return false
-		return node !is SourceNode<*>
-	}
-
-	override suspend fun getNextNode(head: SingleBranchHead<PowerNetwork>, entranceDirection: BlockFace): Pair<TransportNode, BlockFace>? = getTransferableNodes()
-		.filterNot { head.previousNodes.contains(it.first) }
-		.randomOrNull()
-
-
-	override suspend fun handleHeadStep(head: SingleBranchHead<PowerNetwork>): StepResult<PowerNetwork> {
-		// Simply move on to the next node
-		return MoveForward()
+		return node !is PowerExtractorNode && node !is SolarPanelNode
 	}
 
 	override fun storeData(persistentDataContainer: PersistentDataContainer) {
