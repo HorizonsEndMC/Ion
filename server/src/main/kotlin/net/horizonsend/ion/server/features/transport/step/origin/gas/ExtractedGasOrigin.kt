d05bf646c859459f67cabaff63cb67a3e5a2f768
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 01:26:41 -0500

    Remove the first attempt at transfer code

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/gas/ExtractedGasOrigin.kt
+++
@@ -1,9 +0,0 @@
-package net.horizonsend.ion.server.features.transport.step.origin.gas
-
-import net.horizonsend.ion.server.features.transport.network.FluidNetwork
-import net.horizonsend.ion.server.features.transport.node.gas.FluidExtractorNode
-import net.horizonsend.ion.server.features.transport.step.origin.StepOrigin
-
-class ExtractedGasOrigin(
-	val extractorNode: FluidExtractorNode
-) : StepOrigin<FluidNetwork>
