1c96782b362ff81866cf10191cc8bb031ddb150c
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 01:26:41 -0500

    Remove the first attempt at transfer code

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/head/gas/GasBranchHead.kt
+++
@@ -1,5 +0,0 @@
-package net.horizonsend.ion.server.features.transport.step.head.gas
-
-interface GasBranchHead {
-	val share: Double
-}
--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/step/origin/power/PowerOrigin.kt
+++
@@ -1,9 +0,0 @@
-package net.horizonsend.ion.server.features.transport.step.origin.power
-
-import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
-
-interface PowerOrigin {
-	fun getTransferPower(destination: PoweredMultiblockEntity): Int
-
-	val transferLimit: Int
-}
