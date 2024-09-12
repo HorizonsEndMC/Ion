e3f891058ba5821dd59fb4a29826f91eaf46b396
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 23:00:03 -0500

    refactored power storing entities

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/power/PoweredMultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/power/PoweredMultiblockEntity.kt
@@ -1,89 +1,27 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type.power
 
-import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.type.NewPoweredMultiblock
 import net.horizonsend.ion.server.features.transport.node.power.PowerInputNode
 import net.horizonsend.ion.server.features.world.chunk.IonChunk
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.NamedTextColor.GREEN
 import org.bukkit.persistence.PersistentDataType
 
 interface PoweredMultiblockEntity {
-	val position: BlockKey
-	var powerUnsafe: Int
-	val maxPower: Int
+	val multiblock: NewPoweredMultiblock<*>
+	val storage: PowerStorage
 
-	fun setPower(amount: Int) {
-		val correctedPower = amount.coerceIn(0, maxPower)
-
-		powerUnsafe = correctedPower
-
-		updatePowerVisually()
+	fun loadStoredPower(data: PersistentMultiblockData): PowerStorage {
+		return PowerStorage(data.getAdditionalDataOrDefault(NamespacedKeys.POWER, PersistentDataType.INTEGER, 0), multiblock.maxPower)
 	}
 
-	fun isEmpty() = getPower() <= 0
-	fun isFull() = getPower() >= maxPower
-
-	fun getPower(): Int {
-		return powerUnsafe
+	fun savePowerData(store: PersistentMultiblockData) {
+		store.addAdditionalData(NamespacedKeys.POWER, PersistentDataType.INTEGER, storage.getPower())
 	}
-
-	/**
-	 * Returns the amount of power that could not be added
-	 **/
-	fun addPower(amount: Int): Int {
-		val newAmount = getPower() + amount
-
-		setPower(newAmount)
-
-		return if (newAmount > maxPower) maxPower - newAmount else 0
-	}
-
-	/**
-	 * Returns the amount of power that could not be removed
-	 **/
-	fun removePower(amount: Int): Int {
-		val newAmount = getPower() - amount
-
-		setPower(newAmount)
-
-		return if (newAmount < 0) newAmount else 0
-	}
-
-	/**
-	 * Returns whether this multiblock has the capacity to fit the specified amount of power
-	 **/
-	fun canFitPower(amount: Int): Boolean {
-		return getPower() + amount < maxPower
-	}
-
-	/**
-	 * Returns true if this amount of power can be removed without reaching zero.
-	 **/
-	fun canRemovePower(amount: Int): Boolean {
-		return getPower() - amount > 0
-	}
-
-	companion object {
-		private val prefixComponent = text("E: ", NamedTextColor.YELLOW)
-	}
-
-	/** Store power data */
-	fun storePower(store: PersistentMultiblockData) {
-		store.addAdditionalData(NamespacedKeys.POWER, PersistentDataType.INTEGER, powerUnsafe)
-	}
-
-	fun updatePowerVisually()
-
-	fun formatPower(): Component = ofChildren(prefixComponent, text(powerUnsafe, GREEN))
 
 	val powerInputOffset: Vec3i
 
