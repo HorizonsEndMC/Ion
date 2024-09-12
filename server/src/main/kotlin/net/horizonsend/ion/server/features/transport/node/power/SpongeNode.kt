877ffd0ad461c98e8e8d37e74ca0e60c737563aa
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 22:26:20 -0500

    2 steps back

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/SpongeNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/SpongeNode.kt
@@ -1,8 +1,7 @@
 package net.horizonsend.ion.server.features.transport.node.power
 
-import net.horizonsend.ion.server.features.transport.grid.GridType
-import net.horizonsend.ion.server.features.transport.node.manager.PowerNodeManager
 import net.horizonsend.ion.server.features.transport.node.general.JunctionNode
+import net.horizonsend.ion.server.features.transport.node.manager.PowerNodeManager
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
 
@@ -11,7 +10,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
  *
  * Since there is no use in keeping the individual steps, all touching sponges are consolidated into a single node with multiple inputs / outputs, weighted evenly
  **/
-class SpongeNode(network: PowerNodeManager) : JunctionNode<PowerNodeManager, SpongeNode, SpongeNode>(network, GridType.Power) {
+class SpongeNode(network: PowerNodeManager) : JunctionNode<PowerNodeManager, SpongeNode, SpongeNode>(network) {
 	constructor(network: PowerNodeManager, origin: BlockKey) : this(network) {
 		positions.add(origin)
 	}
