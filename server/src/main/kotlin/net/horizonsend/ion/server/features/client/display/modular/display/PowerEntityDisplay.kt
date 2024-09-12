472a054a4154e133f8f2df4fbc9eb5c327f7e732
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 23:00:03 -0500

    refactored power storing entities

--- server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/PowerEntityDisplay.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/PowerEntityDisplay.kt
@@ -2,35 +2,36 @@ package net.horizonsend.ion.server.features.client.display.modular.display
 
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.machine.PowerMachines.prefixComponent
-import net.horizonsend.ion.server.features.multiblock.entity.type.power.UpdatedPowerDisplayEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerStorage
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.newline
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 
 class PowerEntityDisplay(
-	private val multiblockEntity: UpdatedPowerDisplayEntity,
+	private val multiblockEntity: PoweredMultiblockEntity,
 	offsetLeft: Double,
 	offsetUp: Double,
 	offsetBack: Double,
 	scale: Float,
 	val title: Component? = null
 ): Display(offsetLeft, offsetUp, offsetBack, scale) {
-	private val updateHandler: (UpdatedPowerDisplayEntity) -> Unit = {
+	private val updateHandler: (PowerStorage) -> Unit = {
 		display()
 	}
 
 	override fun register() {
-		multiblockEntity.displayUpdates.add(updateHandler)
+		multiblockEntity.storage.registerUpdateHandler(updateHandler)
 	}
 
 	override fun deRegister() {
-		multiblockEntity.displayUpdates.remove(updateHandler)
+		multiblockEntity.storage.removeUpdateHandler(updateHandler)
 	}
 
 	override fun getText(): Component {
 		return title?.let { ofChildren(it, newline(), formatPower()) } ?: formatPower()
 	}
 
-	private fun formatPower(): Component = ofChildren(prefixComponent, text(multiblockEntity.getPower(), NamedTextColor.GREEN))
+	private fun formatPower(): Component = ofChildren(prefixComponent, text(multiblockEntity.storage.getPower(), NamedTextColor.GREEN))
 }
