60ab8b1a1b4afed151e28937b7d35ee9c73b920d
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 20:41:49 -0500

    different sources and sinks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/FluidGrid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/FluidGrid.kt
@@ -1,18 +1,18 @@
 package net.horizonsend.ion.server.features.transport.grid
 
-import net.horizonsend.ion.server.features.transport.grid.util.Sink
-import net.horizonsend.ion.server.features.transport.grid.util.Source
+import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
+import net.horizonsend.ion.server.features.transport.grid.sink.FluidSource
 
-class FluidGrid(manager: WorldGridManager) : Grid(GridType.Fluid, manager) {
-	override fun transferResources(from: Source, to: Sink, resistanceContribution: Int, totalResistance: Int) {
+class FluidGrid(manager: WorldGridManager) : Grid<FluidSource, FluidStoringEntity>(GridType.Power, manager, FluidSource::class, FluidStoringEntity::class) {
+	override fun transferResources(from: FluidSource, to: FluidStoringEntity, resistanceContribution: Int, totalResistance: Int) {
 //		TODO("Not yet implemented")
 	}
 
-	override fun postMerge(other: Grid) {
+	override fun postMerge(other: Grid<FluidSource, FluidStoringEntity>) {
 //		TODO("Not yet implemented")
 	}
 
-	override fun postSplit(new: List<Grid>) {
+	override fun postSplit(new: List<Grid<FluidSource, FluidStoringEntity>>) {
 //		TODO("Not yet implemented")
 	}
 }
