b3d64c337def845f19a60f71770d00c625b97803
Gutin1 <Claytonpinter@gmail.com>
2024-09-11 20:41:49 -0500

    different sources and sinks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/PowerGrid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/grid/PowerGrid.kt
@@ -1,18 +1,31 @@
 package net.horizonsend.ion.server.features.transport.grid
 
-import net.horizonsend.ion.server.features.transport.grid.util.Sink
-import net.horizonsend.ion.server.features.transport.grid.util.Source
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.transport.grid.sink.PowerSource
+import net.horizonsend.ion.server.features.transport.node.power.PowerExtractorNode
+import net.horizonsend.ion.server.features.transport.node.power.SolarPanelNode
 
-class PowerGrid(manager: WorldGridManager) : Grid(GridType.Power, manager) {
-	override fun transferResources(from: Source, to: Sink, resistanceContribution: Int, totalResistance: Int) {
+class PowerGrid(manager: WorldGridManager) : Grid<PowerSource, PoweredMultiblockEntity>(GridType.Power, manager, PowerSource::class, PoweredMultiblockEntity::class) {
+	override fun transferResources(from: PowerSource, to: PoweredMultiblockEntity, resistanceContribution: Int, totalResistance: Int) {
+		val share = resistanceContribution.toDouble() / totalResistance.toDouble()
+
+		when (from) {
+			is PowerExtractorNode -> {
+				val transferPower = from.getTransferPower() * share
+
+			}
+
+			is SolarPanelNode -> {
+
+			}
+		}
+	}
+
+	override fun postMerge(other: Grid<PowerSource, PoweredMultiblockEntity>) {
 //		TODO("Not yet implemented")
 	}
 
-	override fun postMerge(other: Grid) {
-//		TODO("Not yet implemented")
-	}
-
-	override fun postSplit(new: List<Grid>) {
+	override fun postSplit(new: List<Grid<PowerSource, PoweredMultiblockEntity>>) {
 //		TODO("Not yet implemented")
 	}
 }
