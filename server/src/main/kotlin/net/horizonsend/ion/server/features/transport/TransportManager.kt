6afc57e79ba8bd8b9362888eea2d004966dab865
Gutin1 <Claytonpinter@gmail.com>
2024-09-14 13:47:00 -0500

    get world edit node mostly collection working

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/TransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/TransportManager.kt
@@ -1,15 +1,16 @@
 package net.horizonsend.ion.server.features.transport
 
-import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot
-import net.horizonsend.ion.server.features.multiblock.util.BlockSnapshot.Companion.snapshot
 import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
+import net.horizonsend.ion.server.features.world.chunk.IonChunk
 import net.horizonsend.ion.server.listener.SLEventListener
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import org.bukkit.Chunk
 import org.bukkit.World
 import org.bukkit.block.Block
+import org.bukkit.block.data.BlockData
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
@@ -27,20 +28,50 @@ object TransportManager : SLEventListener() {
 	fun onBlockPlace(event: BlockPlaceEvent) {
 		val world = event.block.world
 
-		handleBlockAddition(world, event.block)
+		handleBlockChange(world, event.block)
 	}
 
-	fun handleBlockAddition(world: World, new: Block) {
+	fun handleBlockChange(world: World, new: Block) {
 		val chunkX = new.x.shr(4)
 		val chunkZ = new.z.shr(4)
 
 		val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
-		chunk.transportNetwork.processBlockAddition(new)
+		chunk.transportNetwork.processBlockChange(new)
+	}
+
+	fun handleBlockChange(world: World, position: BlockKey, data: BlockData) {
+		val chunkX = getX(position).shr(4)
+		val chunkZ = getZ(position).shr(4)
+
+		val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
+		chunk.transportNetwork.processBlockChange(position, data)
+	}
+
+	fun handleBlockChanges(world: World, changeMap: Map<BlockKey, BlockData>) {
+		val byChunk = changeMap.entries.groupBy {
+			Chunk.getChunkKey(getX(it.key), getZ(it.key))
+		}
+
+		for ((chunkKey, changes) in byChunk) {
+			val chunk = IonChunk[world, chunkKey] ?: continue
+
+			for ((position, data) in changes) {
+				chunk.transportNetwork.processBlockChange(position, data)
+			}
+		}
+	}
+
+	fun refreshBlock(world: World, position: BlockKey) {
+		val chunkX = getX(position).shr(4)
+		val chunkZ = getZ(position).shr(4)
+
+		val chunk = world.ion.getChunk(chunkX, chunkZ) ?: return
+		chunk.transportNetwork.refreshBlock(position)
 	}
 
 	fun handleBlockAdditions(world: World, newBlocks: Iterable<Block>) {
 		for (new in newBlocks) {
-			handleBlockAddition(world, new)
+			handleBlockChange(world, new)
 		}
 	}
 
