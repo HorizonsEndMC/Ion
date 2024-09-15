956aeb0da4b5f8fddaf65c5123c8b20baa1d4df2
Gutin1 <Claytonpinter@gmail.com>
2024-09-15 15:46:25 -0500

    make node relations based on position

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/MultiNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/MultiNode.kt
@@ -1,9 +1,13 @@
 package net.horizonsend.ion.server.features.transport.node.type
 
 import net.horizonsend.ion.server.features.transport.node.TransportNode
+import net.horizonsend.ion.server.features.transport.node.manager.holders.ChunkNetworkHolder
+import net.horizonsend.ion.server.features.transport.node.separateNode
 import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import java.util.concurrent.ConcurrentHashMap
 
 /**
@@ -30,8 +34,6 @@ abstract class MultiNode<Self: MultiNode<Self, Z>, Z: MultiNode<Z, Self>> : Tran
 			manager.nodes[it]?.buildRelations(it)
 		}
 	}
-
-	abstract fun addBack(position: BlockKey)
 
 	/**
 	 * Adds new a position to this node
@@ -76,7 +78,7 @@ abstract class MultiNode<Self: MultiNode<Self, Z>, Z: MultiNode<Z, Self>> : Tran
 
 			if (this == neighborNode) continue
 
-			addRelationship(neighborNode, offset)
+			addRelationship(position, neighborNode, offset)
 		}
 	}
 
@@ -88,29 +90,67 @@ abstract class MultiNode<Self: MultiNode<Self, Z>, Z: MultiNode<Z, Self>> : Tran
 		}
 	}
 
-	override fun handleRemoval(position: BlockKey) {
-		isDead = true
-
+	/**
+	 * Handle the removal of a position. Slits the node if necessary
+	 *
+	 **/
+	override fun handlePositionRemoval(position: BlockKey) {
 		// Remove the position from the network
 		manager.nodes.remove(position)
+
 		// Remove the position from this node
 		positions.remove(position)
+		removeRelationship(position)
 
-		// Remove all positions
-		positions.forEach {
-			manager.nodes.remove(it)
+		if (separateNode(this)) {
+			positions.clear()
+			clearRelations()
 		}
-
-		// Rebuild relations after cleared
-		clearRelations()
-
-		// Rebuild the node without the lost position
-		rebuildNode(position)
 	}
 
 	override fun loadIntoNetwork() {
 		for (key in positions) {
 			manager.nodes[key] = this
 		}
+	}
+
+	/**
+	 * Returns the directly adjacent positions to this position that this node contains
+	 **/
+	fun adjacentPositions(key: BlockKey): Set<BlockKey> {
+		return ADJACENT_BLOCK_FACES.mapNotNullTo(mutableSetOf()) { face ->
+			getRelative(key, face).takeIf { positions.contains(it) }
+		}
+	}
+
+	/**
+	 * Places the set of positions into the network as this type of node.
+	 **/
+	fun ofPositions(positions: Set<BlockKey>) {
+		@Suppress("UNCHECKED_CAST")
+		val newNode = type.newInstance(manager) as Self
+
+		positions.forEach {
+			newNode.addPosition(it)
+			newNode.buildRelations(it)
+		}
+	}
+
+	abstract fun addBack(position: BlockKey)
+
+	open val maxPositions: Int = 32
+
+	open fun canAdd(position: BlockKey): Boolean {
+		val holder = manager.holder as? ChunkNetworkHolder<*>
+		if (holder != null &&
+			(getX(position).shr(4) != holder.manager.chunk.x ||
+			getZ(position).shr(4) != holder.manager.chunk.z)
+		) return false
+
+		return positions.size < maxPositions
+	}
+
+	fun removeFromNetwork() {
+		positions.forEach { manager.nodes.remove(it) }
 	}
 }
