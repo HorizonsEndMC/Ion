bd2ea287fb80fde24bc4114a667f643326751281
Gutin1 <Claytonpinter@gmail.com>
2024-09-19 14:05:35 -0500

    better implementation of transfer, pathfinding resistance, code cleanup and generalization

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/power/PowerExtractorNode.kt
@@ -6,9 +6,7 @@ import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.manager.node.PowerNodeManager
 import net.horizonsend.ion.server.features.transport.node.type.SingleNode
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.NODE_COVERED_POSITIONS
-import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 import kotlin.math.roundToInt
@@ -21,9 +19,6 @@ class PowerExtractorNode(override val manager: PowerNodeManager) : SingleNode() 
 		network.extractors[position] = this
 	}
 
-	val extractableNodes: MutableSet<PowerInputNode> get() = relationships.mapNotNullTo(mutableSetOf()) { it.value.other as? PowerInputNode }
-
-	// Region transfer
 	/*
 	 * The extractor node should be allowed to transfer into any regular node.
 	 *
@@ -47,9 +42,7 @@ class PowerExtractorNode(override val manager: PowerNodeManager) : SingleNode() 
 
 		return (IonServer.transportSettings.maxPowerRemovedPerExtractorTick * ((System.currentTimeMillis() - lastTicked) / interval)).roundToInt()
 	}
-	// End region
 
-	// Start region loading
 	override fun storeData(persistentDataContainer: PersistentDataContainer) {
 		persistentDataContainer.set(NODE_COVERED_POSITIONS, PersistentDataType.LONG, position)
 	}
@@ -63,36 +56,48 @@ class PowerExtractorNode(override val manager: PowerNodeManager) : SingleNode() 
 		manager.extractors[position] = this
 	}
 
+	override fun onPlace(position: BlockKey) {
+		super.onPlace(position)
+		manager.extractors[position] = this
+	}
+
 	override fun handlePositionRemoval(position: BlockKey) {
 		manager.extractors.remove(position)
 		super.handlePositionRemoval(position)
 	}
 
-	override fun buildRelations(position: BlockKey) {
-		for (offset in ADJACENT_BLOCK_FACES) {
-			val offsetKey = getRelative(position, offset, 1)
-			val neighborNode = manager.getNode(offsetKey) ?: continue
+	/**
+	 * Attempts to draw power from connected inputs.
+	 * Returns the amount that couldn't be removed.
+	 **/
+	fun drawPower(amount: Int): Int {
+		val entities = relationships.mapNotNull { (it.value.other as? PowerInputNode)?.boundMultiblockEntity }
 
-			if (this == neighborNode) return
+		var remaining = amount
 
-			if (neighborNode is PowerInputNode) {
-				extractableNodes.add(neighborNode)
+		while (remaining > 0) {
+			val available = entities.filterNot { it.storage.isEmpty() }
+			if (available.isEmpty()) break
+
+			val minPower = entities.minOf { it.storage.getPower() }
+			val idealShare = remaining / available.size
+			val toRemove = minOf(idealShare, minPower)
+
+			available.forEach {
+				val r = it.storage.removePower(toRemove)
+				remaining -= (toRemove - r)
 			}
-
-			// Add a relationship, if one should be added
-			addRelationship(position, neighborNode, offset)
 		}
+
+		return remaining
+	}
+
+	override fun getPathfindingResistance(previousNode: TransportNode?, nextNode: TransportNode?): Int {
+		return 0
 	}
 
 	override fun toString(): String {
-//		val destinations: Set<PowerInputNode>
-//		val time = measureNanoTime {
-//			destinations = manager.tickExtractor(this)
-//		}
-//
-//		debugAudience.highlightBlocks(destinations.map { toVec3i(it.position) }, 50L)
-
-		return "Extractor. found"// ${destinations.size} in $time ns"
+		return "Extractor. found, can trasnsfer: ${getTransferPower()}"
 	}
 }
 
