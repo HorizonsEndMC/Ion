e17df78de4dc03edc4ccd45439fa316a2358170d
Gutin1 <Claytonpinter@gmail.com>
2024-09-20 01:22:45 -0500

    implement fluid tank mechanics

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
@@ -1,6 +1,8 @@
 package net.horizonsend.ion.server.features.multiblock.type.fluid.storage
 
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
+import net.horizonsend.ion.server.features.client.display.modular.display.fluid.SimpleFluidDisplay
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.BasicFluidStoringEntity
@@ -8,6 +10,7 @@ import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidSt
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage.UnlimitedInternalStorage
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
+import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
@@ -15,8 +18,7 @@ import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.World
 import org.bukkit.block.BlockFace
 
-abstract class FluidStorageMultiblock(val capacity: Int) : Multiblock(),
-	EntityMultiblock<FluidStorageMultiblock.FluidStorageMultiblockEntity> {
+abstract class FluidStorageMultiblock(val capacity: Int) : Multiblock(), EntityMultiblock<FluidStorageMultiblock.FluidStorageMultiblockEntity> {
 	override val name: String = "tank"
 	override val alternativeDetectionNames: Array<String> = arrayOf("gastank", "fluidtank")
 
@@ -42,5 +44,30 @@ abstract class FluidStorageMultiblock(val capacity: Int) : Multiblock(),
 		structureDirection: BlockFace,
 	) : BasicFluidStoringEntity(manager, multiblock, data, x, y, z, world, structureDirection, UnlimitedInternalStorage(multiblock.capacity)), FluidStoringEntity {
 		override val fluidInputOffset: Vec3i = Vec3i(0, -1, 0)
+		private val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+			this,
+			SimpleFluidDisplay(mainStorage, +0.0, -0.0, +0.0, 0.45f),
+		).register()
+
+		override fun onLoad() {
+			displayHandler.update()
+		}
+
+		override fun onUnload() {
+			displayHandler.remove()
+		}
+
+		override fun handleRemoval() {
+			displayHandler.remove()
+		}
+
+		override fun displaceAdditional(movement: StarshipMovement) {
+			displayHandler.displace(movement)
+		}
+
+		override fun storeAdditionalData(store: PersistentMultiblockData) {
+			val rawStorage = store.getAdditionalDataRaw()
+			storeFluidData(rawStorage, rawStorage.adapterContext)
+		}
 	}
 }
