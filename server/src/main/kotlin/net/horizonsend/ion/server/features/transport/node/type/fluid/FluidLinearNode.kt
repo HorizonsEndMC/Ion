207891353b4ef6b11cb39ca5a5720b9114f741ba
Gutin1 <Claytonpinter@gmail.com>
2024-09-21 14:26:21 -0500

    node factory simplification

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/fluid/FluidLinearNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/fluid/FluidLinearNode.kt
@@ -1,8 +1,10 @@
 package net.horizonsend.ion.server.features.transport.node.type.fluid
 
 import net.horizonsend.ion.server.features.transport.node.NodeType
+import net.horizonsend.ion.server.features.transport.node.NodeType.LIGHTNING_ROD
 import net.horizonsend.ion.server.features.transport.node.manager.FluidNodeManager
 import net.horizonsend.ion.server.features.transport.node.type.general.LinearNode
+import net.horizonsend.ion.server.miscellaneous.utils.axis
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
@@ -11,8 +13,9 @@ import net.horizonsend.ion.server.miscellaneous.utils.getBlockDataSafe
 import org.bukkit.Axis
 import org.bukkit.block.data.Directional
 
-class LightningRodNode(network: FluidNodeManager) : LinearNode<FluidNodeManager, LightningRodNode, LightningRodNode>(network) {
-	override val type: NodeType = NodeType.LIGHTNING_ROD
+// Aka lightning rod
+class FluidLinearNode(network: FluidNodeManager) : LinearNode<FluidNodeManager, FluidLinearNode, FluidLinearNode>(network) {
+	override val type: NodeType = LIGHTNING_ROD
 
 	constructor(network: FluidNodeManager, origin: Long, axis: Axis) : this(network) {
 		positions.add(origin)
@@ -22,10 +25,6 @@ class LightningRodNode(network: FluidNodeManager) : LinearNode<FluidNodeManager,
 	override fun addBack(position: BlockKey) {
 		val data = getBlockDataSafe(manager.world, getX(position), getY(position), getZ(position)) as? Directional ?: return
 
-		manager.nodeFactory.addLightningRod(
-			data,
-			position,
-			handleRelationships = false
-		)
+		manager.nodeFactory.addLinearNode<FluidLinearNode>(position, data.facing.axis, type, handleRelationships = false)
 	}
 }
