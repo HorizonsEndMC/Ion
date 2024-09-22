e4ab4953b48a47d4e5147df08974dec6aaf6f06a
Gutin1 <Claytonpinter@gmail.com>
2024-09-22 16:19:22 -0500

    finished power transfer

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/PowerNodeManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/PowerNodeManager.kt
@@ -1,7 +1,6 @@
 package net.horizonsend.ion.server.features.transport.node.manager
 
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
-import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
 import net.horizonsend.ion.server.features.transport.NewTransport
 import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.manager.holders.NetworkHolder
@@ -54,86 +53,117 @@ class PowerNodeManager(holder: NetworkHolder<PowerNodeManager>) : NodeManager(ho
 	}
 
 	private fun tickSolarPanel(solarPanelNode: SolarPanelNode) = NewTransport.executor.submit {
-		val powerCheck = solarPanelNode.getPower()
+		val powerCheck = solarPanelNode.tickAndGetPower()
 		if (powerCheck == 0) return@submit
 
 		val destinations: ObjectOpenHashSet<PowerInputNode> = getPowerInputs(solarPanelNode)
-		runPowerTransfer(solarPanelNode, destinations.toList(), solarPanelNode.tickAndGetPower())
+		runPowerTransfer(solarPanelNode, destinations.toList(), powerCheck)
+	}
+}
+
+// These methods are outside the class for speed
+
+private fun getPowerInputs(origin: TransportNode) = getNetworkDestinations<PowerInputNode>(origin) { it.isCalling() }
+
+/**
+ * Runs the power transfer from the source to the destinations. pending rewrite
+ **/
+private fun runPowerTransfer(source: TransportNode, destinations: List<PowerInputNode>, availableTransferPower: Int): Int {
+	if (destinations.isEmpty()) return availableTransferPower
+	val numDestinations = destinations.size
+
+	var maximumResistance: Double = -1.0
+
+	val paths: Array<Array<TransportNode>?> = Array(numDestinations) { runCatching {
+		getIdealPath(source, destinations[it])
+	}.getOrNull() }
+
+	// Perform the calc & max find in the same loop
+	val pathResistance: Array<Double?> = Array(numDestinations) {
+		val res = calculatePathResistance(paths[it])
+		if (res != null && maximumResistance < res) maximumResistance = res
+
+		res
 	}
 
-	private fun getPowerInputs(origin: TransportNode) = getNetworkDestinations<PowerInputNode>(origin) { it.isCalling() }
+	// All null, no paths found
+	if (maximumResistance == -1.0) return availableTransferPower
 
-	/**
-	 * Runs the power transfer from the source to the destinations. pending rewrite
-	 **/
-	private fun runPowerTransfer(source: TransportNode, destinations: List<PowerInputNode>, availableTransferPower: Int): Int {
-		if (destinations.isEmpty()) return availableTransferPower
-		val numDestinations = destinations.size
+	var shareFactorSum = 0.0
 
-		var maximumResistance: Double = -1.0
+	// Get a parallel array containing the ascending order of resistances
+	val sortedIndexes = getSorted(pathResistance)
 
-		val paths: Array<Array<TransportNode>?> = Array(numDestinations) { runCatching {
-			getIdealPath(source, destinations[it])
-		}.getOrNull() }
+	val shareFactors: Array<Double?> = Array(numDestinations) { index ->
+		val resistance = pathResistance[index] ?: return@Array null
+		val fac = (numDestinations - sortedIndexes[index]).toDouble() / (resistance / maximumResistance)
+		shareFactorSum += fac
 
-		// Perform the calc & max find in the same loop
-		val pathResistance: Array<Double?> = Array(numDestinations) {
-			val res = calculatePathResistance(paths[it])
-			if (res != null && maximumResistance < res) maximumResistance = res
-
-			res
-		}
-
-		// All null, no paths found
-		if (maximumResistance == -1.0) return availableTransferPower
-
-		var shareFactorSum = 0.0
-
-		val shareFactors: Array<Double?> = Array(numDestinations) { index ->
-			val resistance = pathResistance[index] ?: return@Array null
-			val fac = (numDestinations - index).toDouble() / (resistance / maximumResistance)
-			shareFactorSum += fac
-
-			fac
-		}
-
-		var remainingPower = availableTransferPower
-
-		// Just cast once
-		val powerSource = source as? PowerExtractorNode
-
-		for ((index, destination) in destinations.withIndex()) {
-			val shareFactor = shareFactors[index] ?: continue
-			val share = shareFactor / shareFactorSum
-
-			val recipient = destination.getPoweredEntities().randomOrNull() ?: continue
-
-			val idealSend = (availableTransferPower * share).roundToInt()
-			val toSend = minOf(idealSend, recipient.storage.getRemainingCapacity())
-			val couldNotRemove = powerSource?.drawPower(toSend) ?: 0 // If null, source is a solar panel, and can't be removed from.
-
-			val realAdd = toSend - couldNotRemove
-
-			val remainder = recipient.storage.addPower(realAdd)
-			val realTaken = realAdd - remainder
-
-			remainingPower -= realTaken
-			paths[index]?.filterIsInstance(PowerFlowMeter::class.java)?.forEach { it.onCompleteChain(realTaken) }
-
-			// All sent
-			if (remainder == 0) continue
-
-			// Get the proportion of the amount of power that sent compared to the ideal calculations.
-			val usedShare = realTaken.toDouble() / idealSend.toDouble()
-			// Use that to get a proportion of the share factor, and remove that from the sum.
-			val toRemove = shareFactor * usedShare
-			shareFactorSum -= toRemove
-		}
-
-		return remainingPower
+		fac
 	}
 
-	fun addPower(recipients: Collection<PoweredMultiblockEntity>): Int {
-		TODO("")
+	var remainingPower = availableTransferPower
+
+	// Just cast once
+	val powerSource = (source as? PowerExtractorNode)
+
+	for ((index, destination) in destinations.withIndex()) {
+		val shareFactor = shareFactors[index] ?: continue
+		val share = shareFactor / shareFactorSum
+
+		val idealSend = (availableTransferPower * share).roundToInt()
+		val toSend = minOf(idealSend, getRemainingCapacity(destination))
+		val couldNotRemove = powerSource?.drawPower(toSend) ?: 0 // If null, source is a solar panel, and can't be removed from.
+
+		// Following the power distribution
+		val realAdd = toSend - couldNotRemove
+
+		// Amount of power that didn't fit
+		val remainder = destination.distributePower(realAdd)
+		val realTaken = realAdd - remainder
+
+		remainingPower -= realTaken
+		completeChain(paths[index], realTaken)
+
+		if (remainder == 0) continue
+
+		// Get the proportion of the amount of power that sent compared to the ideal calculations.
+		val usedShare = realTaken.toDouble() / idealSend.toDouble()
+		// Use that to get a proportion of the share factor, and remove that from the sum.
+		val toRemove = shareFactor * usedShare
+		shareFactorSum -= toRemove
 	}
+
+	return remainingPower
+}
+
+private fun getRemainingCapacity(destination: PowerInputNode): Int {
+	return destination.getPoweredEntities().sumOf { it.storage.getRemainingCapacity() }
+}
+
+private fun completeChain(path: Array<TransportNode>?, transferred: Int) {
+	path?.filterIsInstance(PowerFlowMeter::class.java)?.forEach { it.onCompleteChain(transferred) }
+}
+
+// I hate this function but it works
+fun getSorted(pathResistance: Array<Double?>): IntArray {
+	// Store the shuffled indicies
+	val ranks = IntArray(pathResistance.size) { it }
+	val tempSorted = pathResistance.clone()
+
+	for (index in ranks.indices) {
+		for (j in 0..< ranks.lastIndex) {
+			if ((tempSorted[j] ?: Double.MAX_VALUE) > (tempSorted[j + 1] ?: Double.MAX_VALUE)) {
+				val temp = tempSorted[j]
+				tempSorted[j] = tempSorted[j + 1]
+				tempSorted[j + 1] = temp
+
+				val prev = ranks[j]
+				ranks[j] = prev + 1
+				ranks[j + 1] = prev
+			}
+		}
+	}
+
+	return ranks
 }
