86076bea69da8ccb40840e0dd561372b0a4970db
Gutin1 <Claytonpinter@gmail.com>
2024-09-22 23:30:33 -0500

    implement directional nodes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/power/EndRodNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/power/EndRodNode.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.features.transport.node.type.power
 
 import net.horizonsend.ion.server.features.transport.node.NodeType
+import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.manager.PowerNodeManager
 import net.horizonsend.ion.server.features.transport.node.type.general.LinearNode
 import net.horizonsend.ion.server.miscellaneous.utils.axis
@@ -11,7 +12,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import net.horizonsend.ion.server.miscellaneous.utils.getBlockDataSafe
 import org.bukkit.block.data.Directional
 
-class EndRodNode(network: PowerNodeManager) : LinearNode<PowerNodeManager, EndRodNode, EndRodNode>(network) {
+class EndRodNode(network: PowerNodeManager) : LinearNode<PowerNodeManager, EndRodNode, EndRodNode>(network), PowerPathfindingNode {
 	override val type: NodeType = NodeType.END_ROD_NODE
 
 	override fun addBack(position: BlockKey) {
@@ -19,4 +20,6 @@ class EndRodNode(network: PowerNodeManager) : LinearNode<PowerNodeManager, EndRo
 
 		manager.nodeFactory.addLinearNode<EndRodNode>(position, data.facing.axis, type, handleRelationships = false)
 	}
+
+	override fun getNextNodes(previous: TransportNode): ArrayDeque<TransportNode> = cachedTransferable
 }
