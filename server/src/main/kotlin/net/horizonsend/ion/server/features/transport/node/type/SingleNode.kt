d583736e21f4d292b59dd622599a47c780c316ad
Gutin1 <Claytonpinter@gmail.com>
2024-09-26 00:52:27 -0500

    fix times power isn't transferring, more robust relation system

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/SingleNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/SingleNode.kt
@@ -1,10 +1,8 @@
 package net.horizonsend.ion.server.features.transport.node.type
 
 import net.horizonsend.ion.server.features.transport.node.TransportNode
-import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
 import kotlin.properties.Delegates
 
@@ -16,18 +14,6 @@ abstract class SingleNode : TransportNode() {
 
 	override fun loadIntoNetwork() {
 		manager.nodes[position] = this
-	}
-
-	override fun buildRelations(position: BlockKey) {
-		for (offset in ADJACENT_BLOCK_FACES) {
-			val offsetKey = getRelative(position, offset, 1)
-			val neighborNode = manager.getNode(offsetKey) ?: continue
-
-			if (this == neighborNode) return
-
-			// Add a relationship, if one should be added
-			addRelationship(position, neighborNode, offset)
-		}
 	}
 
 	override fun onPlace(position: BlockKey) {
