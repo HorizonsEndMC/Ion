9789879b1efc740ddacc648bbd6b242cf0e3a956
Gutin1 <Claytonpinter@gmail.com>
2024-09-27 14:07:54 -0500

    more fluid nodes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/fluid/FluidInvertedDirectionalNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/fluid/FluidInvertedDirectionalNode.kt
@@ -1,4 +1,34 @@
 package net.horizonsend.ion.server.features.transport.node.type.fluid
 
-class FluidInvertedDirectionalNode {
+import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
+import net.horizonsend.ion.server.features.transport.node.NodeType
+import net.horizonsend.ion.server.features.transport.node.TransportNode
+import net.horizonsend.ion.server.features.transport.node.manager.FluidNodeManager
+import net.horizonsend.ion.server.features.transport.node.type.general.DirectionalNode
+
+class FluidInvertedDirectionalNode(override val manager: FluidNodeManager) : DirectionalNode(), FluidPathfindingNode {
+	override val type: NodeType = NodeType.FLUID_INVERTED_DIRECTIONAL_NODE
+
+	override fun isTransferableTo(node: TransportNode): Boolean {
+		if (node is FluidLinearNode) return false
+		return node !is FluidExtractorNode
+	}
+
+	override fun getPathfindingResistance(previousNode: TransportNode?, nextNode: TransportNode?): Int {
+		return 1
+	}
+
+	override fun getNextNodes(previous: TransportNode, destination: TransportNode?, resource: PipedFluid): ArrayDeque<TransportNode> {
+		if (destination != null && relationHolder.hasRelationAtWith(position, destination)) return ArrayDeque(listOf(destination))
+
+		// Since this is a single node, and the previous node must be transferable to this, it can't be a sponge.
+		// So there will likely only be a single relation to this
+		val direction = previous.getRelationshipWith(this).values
+		if (direction.isEmpty()) return cachedTransferable // just in case
+		val face = direction.first().offset
+
+		getForwardTransferable(face)?.let { return ArrayDeque(listOf(it)) }
+
+		return cachedTransferable
+	}
 }
