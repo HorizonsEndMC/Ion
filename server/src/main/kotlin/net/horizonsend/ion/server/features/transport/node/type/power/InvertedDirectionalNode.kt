3e4b30eaeb8ab826f0cf6b857e6a38f8bc956093
Gutin1 <Claytonpinter@gmail.com>
2024-09-27 12:49:39 -0500

    simplify placement of flow meters

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/power/InvertedDirectionalNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/type/power/InvertedDirectionalNode.kt
@@ -4,10 +4,7 @@ import net.horizonsend.ion.server.features.transport.node.NodeType
 import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.manager.PowerNodeManager
 import net.horizonsend.ion.server.features.transport.node.type.SingleNode
-import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import org.bukkit.block.BlockFace
-import org.bukkit.persistence.PersistentDataContainer
-import org.bukkit.persistence.PersistentDataType
 
 class InvertedDirectionalNode(override val manager: PowerNodeManager) : SingleNode(), PowerPathfindingNode {
 	override val type: NodeType = NodeType.POWER_INVERSE_DIRECTIONAL_NODE
@@ -15,14 +12,6 @@ class InvertedDirectionalNode(override val manager: PowerNodeManager) : SingleNo
 	override fun isTransferableTo(node: TransportNode): Boolean {
 		if (node is EndRodNode) return false
 		return node !is PowerExtractorNode && node !is SolarPanelNode
-	}
-
-	override fun storeData(persistentDataContainer: PersistentDataContainer) {
-		persistentDataContainer.set(NamespacedKeys.NODE_COVERED_POSITIONS, PersistentDataType.LONG, position)
-	}
-
-	override fun loadData(persistentDataContainer: PersistentDataContainer) {
-		position = persistentDataContainer.get(NamespacedKeys.NODE_COVERED_POSITIONS, PersistentDataType.LONG)!!
 	}
 
 	override fun getPathfindingResistance(previousNode: TransportNode?, nextNode: TransportNode?): Int {
