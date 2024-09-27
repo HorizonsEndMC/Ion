e39cb942dcb4c3d846327872dfcae08e681e06f3
Gutin1 <Claytonpinter@gmail.com>
2024-09-27 14:07:54 -0500

    more fluid nodes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/NodeType.kt
@@ -2,13 +2,19 @@ package net.horizonsend.ion.server.features.transport.node
 
 import com.manya.pdc.base.EnumDataType
 import net.horizonsend.ion.server.features.transport.node.manager.NodeManager
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidDirectionalNode
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidExtractorNode
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidFlowMeter
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidInputNode
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidInvertedDirectionalNode
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidJunctionNode
 import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidLinearNode
 import net.horizonsend.ion.server.features.transport.node.type.power.EndRodNode
-import net.horizonsend.ion.server.features.transport.node.type.power.InvertedDirectionalNode
 import net.horizonsend.ion.server.features.transport.node.type.power.PowerDirectionalNode
 import net.horizonsend.ion.server.features.transport.node.type.power.PowerExtractorNode
 import net.horizonsend.ion.server.features.transport.node.type.power.PowerFlowMeter
 import net.horizonsend.ion.server.features.transport.node.type.power.PowerInputNode
+import net.horizonsend.ion.server.features.transport.node.type.power.PowerInvertedDirectionalNode
 import net.horizonsend.ion.server.features.transport.node.type.power.SolarPanelNode
 import net.horizonsend.ion.server.features.transport.node.type.power.SpongeNode
 
@@ -21,12 +27,16 @@ enum class NodeType(val clazz: Class<out TransportNode>) {
 	POWER_INPUT_NODE(PowerInputNode::class.java),
 	POWER_FLOW_METER(PowerFlowMeter::class.java),
 	POWER_DIRECTIONAL_NODE(PowerDirectionalNode::class.java),
-	POWER_INVERSE_DIRECTIONAL_NODE(InvertedDirectionalNode::class.java),
+	POWER_INVERSE_DIRECTIONAL_NODE(PowerInvertedDirectionalNode::class.java),
 
 	//FLUID
-	FLUID_INPUT(net.horizonsend.ion.server.features.transport.node.type.fluid.FluidInputNode::class.java),
-	FLUID_JUNCTION(net.horizonsend.ion.server.features.transport.node.type.fluid.FluidJunctionNode::class.java),
+	FLUID_INPUT(FluidInputNode::class.java),
+	FLUID_EXTRACTOR_NODE(FluidExtractorNode::class.java),
+	FLUID_JUNCTION(FluidJunctionNode::class.java),
 	LIGHTNING_ROD(FluidLinearNode::class.java),
+	FLUID_FLOW_METER(FluidFlowMeter::class.java),
+	FLUID_INVERTED_DIRECTIONAL_NODE(FluidInvertedDirectionalNode::class.java),
+	FLUID_DIRECTIONAL_NODE(FluidDirectionalNode::class.java),
 
 	//ITEM
 	;
