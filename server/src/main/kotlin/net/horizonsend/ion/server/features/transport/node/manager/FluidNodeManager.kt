dff9d139ccf03785e09b63c50280766607b35623
Gutin1 <Claytonpinter@gmail.com>
2024-09-28 14:21:31 -0500

    everything but the transfer part

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/FluidNodeManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/FluidNodeManager.kt
@@ -1,10 +1,14 @@
 package net.horizonsend.ion.server.features.transport.node.manager
 
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 import net.horizonsend.ion.server.features.transport.NewTransport
+import net.horizonsend.ion.server.features.transport.node.TransportNode
 import net.horizonsend.ion.server.features.transport.node.manager.holders.NetworkHolder
 import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidExtractorNode
+import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidInputNode
 import net.horizonsend.ion.server.features.transport.node.type.fluid.FluidNodeFactory
 import net.horizonsend.ion.server.features.transport.node.util.NetworkType
+import net.horizonsend.ion.server.features.transport.node.util.getNetworkDestinations
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.FLUID_TRANSPORT
 import org.bukkit.NamespacedKey
 import java.util.concurrent.Future
@@ -26,19 +30,19 @@ class FluidNodeManager(holder: NetworkHolder<FluidNodeManager>) : NodeManager<Fl
 	}
 
 	private fun tickExtractor(extractorNode: FluidExtractorNode): Future<*> = NewTransport.executor.submit {
-		val check = extractorNode.getTransferAmount()
-		if (check == 0) return@submit
+		val transferCheck = extractorNode.getTransferAmount()
+		if (transferCheck == 0) return@submit
 
 		extractorNode.markTicked()
 
-//		val source = extractorNode.getSourcePool().filterNot { it.storage.isEmpty() }.randomOrNull() ?: return@submit
-//
-//		val destinations: ObjectOpenHashSet<PowerInputNode> = getPowerInputs(extractorNode)
-//		destinations.removeAll(extractorNode.getTransferableNodes().filterIsInstanceTo(ObjectOpenHashSet()))
-//
-//		if (destinations.isEmpty()) return@submit
-//
-//		val transferred = minOf(source.storage.getPower(), check)
+		val source = extractorNode.getSourcePool().filterNot { it.isFull() }.randomOrNull() ?: return@submit
+
+		val destinations: ObjectOpenHashSet<FluidInputNode> = getFluidInputs(extractorNode)
+		destinations.removeAll(extractorNode.getTransferableNodes().filterIsInstanceTo(ObjectOpenHashSet()))
+
+		if (destinations.isEmpty()) return@submit
+
+//		val transferred = minOf(source.getPower(), check)
 //		val notRemoved = source.storage.removePower(transferred)
 //		val remainder = runPowerTransfer(extractorNode, destinations.toMutableList(), (transferred - notRemoved))
 //
@@ -51,3 +55,5 @@ class FluidNodeManager(holder: NetworkHolder<FluidNodeManager>) : NodeManager<Fl
 //		}
 	}
 }
+
+fun getFluidInputs(origin: TransportNode) = getNetworkDestinations<FluidInputNode>(origin) { it.isCalling() }
