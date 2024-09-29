3462a6eadde56a189f1aab1faff5f1236a2e46f8
Gutin1 <Claytonpinter@gmail.com>
2024-09-29 16:05:52 -0500

    halloween gun 1

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/event/FireWaveWeaponSubsystem.kt
@@ -0,0 +1,50 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.event
+
+import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.features.starship.subsystem.DirectionalSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.WeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.HeavyWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.ManualWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.PermissionWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.TestAOEProjectile
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.block.BlockFace
+import org.bukkit.util.Vector
+
+class FireWaveWeaponSubsystem(
+	starship: ActiveStarship,
+	pos: Vec3i,
+	override var face: BlockFace
+) : WeaponSubsystem(starship, pos),
+	ManualWeaponSubsystem,
+	HeavyWeaponSubsystem,
+	PermissionWeaponSubsystem,
+	DirectionalSubsystem {
+
+	override val powerUsage: Int = 1
+
+	override fun getAdjustedDir(dir: Vector, target: Vector): Vector {
+		return target
+	}
+
+	override fun canFire(dir: Vector, target: Vector): Boolean {
+		return true
+	}
+
+	override fun isIntact(): Boolean {
+		return true
+	}
+
+	override val balancing: StarshipWeapons.StarshipWeapon = StarshipWeapons().protonTorpedo
+
+	override val boostChargeNanos: Long = 2L
+
+	override fun manualFire(shooter: Damager, dir: Vector, target: Vector) {
+		val firePos = pos.toLocation(starship.world).add(face.direction.multiply(10))
+		TestAOEProjectile(starship, shooter, firePos).fire()
+	}
+
+	override val permission: String = "ioncore.eventweapon"
+}
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/AOEWave.kt
@@ -0,0 +1,61 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
+
+import net.horizonsend.ion.server.features.starship.Starship
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import org.bukkit.Location
+import org.bukkit.util.Vector
+import kotlin.math.PI
+
+abstract class AOEWave(starship: Starship, shooter: Damager, val firePos: Location) : Projectile(starship, shooter) {
+	protected abstract val speed: Double
+	protected abstract val separation: Double
+	protected abstract val range: Double
+
+	protected var lastTick: Long = -1
+	protected var delta: Double = 0.0
+	protected var distance: Double = 0.0
+	protected var firedAtNanos: Long = -1
+
+	private val circumference get() = 2.0 * PI * distance
+
+	override fun fire() {
+		firedAtNanos = System.nanoTime()
+		lastTick = firedAtNanos
+
+		super.fire()
+	}
+
+	override fun tick() {
+		delta = (System.nanoTime() - lastTick) / 1_000_000_000.0 // Convert to seconds
+
+		iterateCircumference()
+
+		distance += (speed * delta)
+
+		println("Distance: $distance")
+
+		if (distance >= range) {
+			return
+		}
+
+		lastTick = System.nanoTime()
+		reschedule()
+	}
+
+	private fun iterateCircumference() {
+		val vec = Vector(0.0, 0.0, distance)
+
+		// Get the degree separation by getting the number of distance separations in the circumference, and converting to degrees
+		val numberPoints = circumference / separation
+		val separationDegree = 360.0 / numberPoints
+
+		for (n in 0..numberPoints.toInt()) {
+			val degrees = n * separationDegree
+			val adjusted = vec.clone().rotateAroundY(Math.toRadians(degrees))
+
+			handleCircumferencePosition(firePos.clone().add(adjusted))
+		}
+	}
+
+	abstract fun handleCircumferencePosition(position: Location)
+}
