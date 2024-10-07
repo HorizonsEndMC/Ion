2c60efa75a705cb0c7040881401eb3dab3b212db
Gutin1 <Claytonpinter@gmail.com>
2024-10-06 21:43:22 -0500

    stop spamming of them

--- server/src/main/kotlin/net/horizonsend/ion/server/command/misc/AIOpponentCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/misc/AIOpponentCommand.kt
@@ -6,6 +6,7 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
 import net.horizonsend.ion.common.extensions.hint
 import net.horizonsend.ion.common.extensions.success
+import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.command.SLCommand
 import net.horizonsend.ion.server.features.ai.AIControllerFactories
 import net.horizonsend.ion.server.features.ai.configuration.AITemplate
@@ -13,6 +14,7 @@ import net.horizonsend.ion.server.features.ai.module.AIModule
 import net.horizonsend.ion.server.features.ai.spawning.createAIShipFromTemplate
 import net.horizonsend.ion.server.features.ai.spawning.spawner.AISpawners
 import net.horizonsend.ion.server.features.ai.starship.AITemplateRegistry
+import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.active.ActiveStarships
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
 import net.horizonsend.ion.server.features.starship.destruction.StarshipDestruction
@@ -62,18 +64,24 @@ object AIOpponentCommand : SLCommand() {
 	private fun summonShip(summoner: Player, template: AITemplate, vec: Vec3i?) {
 		val location = vec?.toLocation(summoner.world) ?: summoner.location.add(summoner.location.direction.multiply(500.0))
 
-		createAIShipFromTemplate(
-			log,
-			template,
-			location,
-			{ starship ->
-				val factory = AIControllerFactories[template.behaviorInformation.controllerFactory]
-				val controller = factory.invoke(starship, template.starshipInfo.componentName())
-				processController(summoner, controller)
-				controller
+		Tasks.async {
+			if (getExisting(summoner).isNotEmpty()) return@async summoner.userError("You may only have one AI opponent active at once.")
+
+			Tasks.sync {
+				createAIShipFromTemplate(
+					log,
+					template,
+					location,
+					{ starship ->
+						val factory = AIControllerFactories[template.behaviorInformation.controllerFactory]
+						val controller = factory.invoke(starship, template.starshipInfo.componentName())
+						processController(summoner, controller)
+						controller
+					}
+				) {
+					summoner.success("Summoned ${template.starshipInfo.miniMessageName}")
+				}
 			}
-		) {
-			summoner.success("Summoned ${template.starshipInfo.miniMessageName}")
 		}
 	}
 
@@ -81,20 +89,23 @@ object AIOpponentCommand : SLCommand() {
 		controller.modules["opponent"] = OpponentTrackerModule(controller, summoner.uniqueId)
 	}
 
+	fun getExisting(player: Player): Collection<Starship> {
+		return ActiveStarships.all().filter {
+			val controller = it.controller
+			if (controller !is AIController) return@filter false
+
+			val trackers = controller.modules.values.filterIsInstance<OpponentTrackerModule>()
+			if (trackers.isEmpty()) return@filter false
+
+			trackers.any { tracker -> tracker.opponent == player.uniqueId }
+		}
+	}
+
 	@Subcommand("despawn")
 	fun deSpawn(sender: Player) {
-		val senderId = sender.uniqueId
-
+		// Don't want to clog async command thread
 		Tasks.async {
-			val toRemove = ActiveStarships.all().filter {
-				val controller = it.controller
-				if (controller !is AIController) return@filter false
-
-				val trackers = controller.modules.values.filterIsInstance<OpponentTrackerModule>()
-				if (trackers.isEmpty()) return@filter false
-
-				trackers.any { tracker -> tracker.opponent == senderId }
-			}
+			val toRemove = getExisting(sender)
 
 			Tasks.sync {
 				toRemove.forEach {
