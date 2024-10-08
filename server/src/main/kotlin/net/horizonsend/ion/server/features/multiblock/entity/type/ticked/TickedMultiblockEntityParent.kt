9c7b2dbebcb5d3b415d8f72b8c7658ee8d3fb97f
Gutin1 <Claytonpinter@gmail.com>
2024-10-08 13:12:53 -0500

    improve multiblock ticking

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ticked/TickedMultiblockEntityParent.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ticked/TickedMultiblockEntityParent.kt
@@ -1,32 +1,44 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type.ticked
 
+import com.nijiko.permissions.ModularControl.tickInterval
+
 interface TickedMultiblockEntityParent {
-	val tickInterval: Int
-	var currentTick: Int
-
-	var sleepTicks: Int
-
-	/**
-	 * Returns true if the current tick should be allowed to proceed
-	 **/
-	fun checkTickInterval(): Boolean {
-		if (sleepTicks >= 1) {
-			sleepTicks--
-
-			return false
-		}
-
-		currentTick++
-		if (currentTick >= tickInterval) {
-			currentTick = 0
-			return false
-		}
-
-		return true
-	}
-
+	val tickingManager: TickingManager
+	
 	/**
 	 *
 	 **/
 	fun shouldCheckIntegrity(): Boolean = true
+
+	class TickingManager(val interval: Int) {
+		private var currentTick: Int = 0
+		private var sleepTicks: Int = 0
+
+		/**
+		 * Returns true if the current tick should be allowed to proceed
+		 **/
+		fun checkTickInterval(): Boolean {
+			if (sleepTicks >= 1) {
+				sleepTicks--
+
+				return false
+			}
+
+			currentTick++
+			if (currentTick >= tickInterval) {
+				currentTick = 0
+				return false
+			}
+
+			return true
+		}
+
+		fun sleep(ticks: Int) {
+			sleepTicks += ticks
+		}
+
+		fun clearSleep() {
+			sleepTicks = 0
+		}
+	}
 }
