acdf7128529da380ab8f80e37864e19205795794
Gutin1 <Claytonpinter@gmail.com>
2024-10-09 18:13:49 -0500

    Add sign status displays

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/harvester/HarvesterMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/harvester/HarvesterMultiblock.kt
@@ -3,14 +3,16 @@ package net.horizonsend.ion.server.features.multiblock.type.farming.harvester
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
 import net.horizonsend.ion.server.features.client.display.modular.display.PowerEntityDisplay
+import net.horizonsend.ion.server.features.client.display.modular.display.StatusDisplay
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.StatusMultiblock
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerStorage
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent.TickingManager
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.type.NewPoweredMultiblock
@@ -20,9 +22,12 @@ import net.horizonsend.ion.server.miscellaneous.utils.LegacyItemUtils
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor.AQUA
+import net.kyori.adventure.text.format.NamedTextColor.BLUE
 import net.kyori.adventure.text.format.NamedTextColor.DARK_AQUA
 import net.kyori.adventure.text.format.NamedTextColor.GRAY
 import net.kyori.adventure.text.format.NamedTextColor.GREEN
+import net.kyori.adventure.text.format.NamedTextColor.RED
+import net.kyori.adventure.text.format.TextDecoration.ITALIC
 import org.bukkit.Material
 import org.bukkit.World
 import org.bukkit.block.BlockFace
@@ -30,7 +35,7 @@ import org.bukkit.block.Sign
 import org.bukkit.block.data.Ageable
 import org.bukkit.persistence.PersistentDataAdapterContext
 
-abstract class HarvesterMultiblock(val tierMaterial: Material, tierNumber: Int) : Multiblock(), NewPoweredMultiblock<HarvesterMultiblock.HarvesterEntity> {
+abstract class HarvesterMultiblock(val tierMaterial: Material, val tierNumber: Int) : Multiblock(), NewPoweredMultiblock<HarvesterMultiblock.HarvesterEntity> {
 	override val name: String = "harvester"
 	override val signText: Array<Component?> = arrayOf(
 		ofChildren(text("Auto ", GRAY), text("Harvester", GREEN)),
@@ -114,16 +119,29 @@ abstract class HarvesterMultiblock(val tierMaterial: Material, tierNumber: Int) 
 		z: Int,
 		world: World,
 		structureDirection: BlockFace,
-	) : MultiblockEntity(manager, multiblock, x, y, z, world, structureDirection), PoweredMultiblockEntity, SyncTickingMultiblockEntity, LegacyMultiblockEntity {
+	) : MultiblockEntity(manager, multiblock, x, y, z, world, structureDirection), PoweredMultiblockEntity, SyncTickingMultiblockEntity, LegacyMultiblockEntity, StatusMultiblock {
+		override val statusManager: StatusMultiblock.StatusManager = StatusMultiblock.StatusManager()
 		override val storage: PowerStorage = loadStoredPower(data)
-		override val tickingManager: TickedMultiblockEntityParent.TickingManager = TickedMultiblockEntityParent.TickingManager(interval = 20)
+		override val tickingManager: TickingManager = TickingManager(interval = 20)
+
+		private val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+			this,
+			PowerEntityDisplay(this, +0.0, +0.0, +0.0, 0.45f),
+			StatusDisplay(statusManager, +0.0, -0.10, +0.0, 0.45f)
+		).register()
+
+		private fun cancelWithStatus(status: Component, sleepTicks: Int) {
+			setStatus(status)
+			tickingManager.sleep(sleepTicks)
+		}
 
 		override fun tick() {
 			val inventory = getInventory(leftRight = 0, upDown = 0, backFourth = 2) ?: return tickingManager.sleep(1000)
 			var broken = 0
-			val initialPower = storage.getPower()
 
-			if (initialPower == 0) return tickingManager.sleep(500)
+			val initialPower = storage.getPower()
+			if (initialPower == 0) return cancelWithStatus(text("No Power", RED), 500)
+
 			val region = getRegionWithDimensions(-1 ,-1 ,4, 3, 1, multiblock.regionDepth)
 
 			for (block in region) {
@@ -153,24 +171,22 @@ abstract class HarvesterMultiblock(val tierMaterial: Material, tierNumber: Int) 
 				val didNotFit = inventory.addItem(*drops)
 
 				if (didNotFit.isNotEmpty()) {
-					tickingManager.sleep(800)
+					cancelWithStatus(text("No Space", RED), 500)
 					break
 				}
+
+				if (broken >= multiblock.tierNumber) break
 			}
 
-			if (broken == 0) return tickingManager.sleep(100)
+			if (broken == 0) return cancelWithStatus(text("Sleeping", BLUE, ITALIC), 100)
 
 			storage.removePower(broken * multiblock.powerPerCrop)
+			setStatus(text("Working", GREEN))
 		}
 
 		override fun storeAdditionalData(store: PersistentMultiblockData, adapterContext: PersistentDataAdapterContext) {
 			savePowerData(store)
 		}
-
-		private val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
-			this,
-			PowerEntityDisplay(this, +0.0, +0.0, +0.0, 0.5f)
-		).register()
 
 		override fun onLoad() {
 			displayHandler.update()
