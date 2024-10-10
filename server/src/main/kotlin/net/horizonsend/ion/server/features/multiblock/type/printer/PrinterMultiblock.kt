62fc7ec8ab98a738af9635dad6fd94c966a69db2
Gutin1 <Claytonpinter@gmail.com>
2024-10-09 21:32:52 -0500

    migrate printers

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/printer/PrinterMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/printer/PrinterMultiblock.kt
@@ -1,20 +1,34 @@
 package net.horizonsend.ion.server.features.multiblock.type.printer
 
-import net.horizonsend.ion.server.features.machine.PowerMachines
+import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
+import net.horizonsend.ion.server.features.client.display.modular.display.PowerEntityDisplay
+import net.horizonsend.ion.server.features.client.display.modular.display.StatusDisplay
 import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.StatusMultiblock
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerStorage
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent.TickingManager
+import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
-import net.horizonsend.ion.server.features.multiblock.type.FurnaceMultiblock
-import net.horizonsend.ion.server.features.multiblock.type.PowerStoringMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.NewPoweredMultiblock
+import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
 import net.horizonsend.ion.server.miscellaneous.utils.LegacyItemUtils
-import net.horizonsend.ion.server.miscellaneous.utils.getFacing
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.RED
 import org.bukkit.Material
-import org.bukkit.block.Furnace
+import org.bukkit.World
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
-import org.bukkit.event.inventory.FurnaceBurnEvent
-import org.bukkit.inventory.InventoryHolder
+import org.bukkit.inventory.FurnaceInventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataAdapterContext
 
-abstract class PrinterMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+abstract class PrinterMultiblock : Multiblock(), NewPoweredMultiblock<PrinterMultiblock.PrinterEntity> {
 	override val name: String = "printer"
 	override val maxPower: Int = 50_000
 	abstract fun getOutput(product: Material): ItemStack
@@ -115,41 +129,85 @@ abstract class PrinterMultiblock : Multiblock(), PowerStoringMultiblock, Furnace
 		}
 	}
 
-	override fun onFurnaceTick(
-		event: FurnaceBurnEvent,
-		furnace: Furnace,
-		sign: Sign
-	) {
-		event.isCancelled = true
-		val smelting = furnace.inventory.smelting
-		val fuel = furnace.inventory.fuel
+	override fun createEntity(manager: MultiblockManager, data: PersistentMultiblockData, world: World, x: Int, y: Int, z: Int, structureDirection: BlockFace): PrinterEntity {
+		return PrinterEntity(data, manager, this, x, y, z, world, structureDirection)
+	}
 
-		if (PowerMachines.getPower(sign) < 250
-			|| smelting == null
-			|| smelting.type != Material.PRISMARINE_CRYSTALS
-			|| fuel == null
-			|| fuel.type != Material.COBBLESTONE
-		) {
-			return
+	class PrinterEntity(
+		data: PersistentMultiblockData,
+		manager: MultiblockManager,
+		override val multiblock: PrinterMultiblock,
+		x: Int,
+		y: Int,
+		z: Int,
+		world: World,
+		structureDirection: BlockFace,
+	) : MultiblockEntity(manager, multiblock, x, y, z, world, structureDirection), PoweredMultiblockEntity, SyncTickingMultiblockEntity, LegacyMultiblockEntity, StatusMultiblock {
+		override val powerStorage: PowerStorage = loadStoredPower(data)
+		override val tickingManager: TickingManager = TickingManager(interval = 20)
+		override val statusManager: StatusMultiblock.StatusManager = StatusMultiblock.StatusManager()
+
+		private val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+			this,
+			PowerEntityDisplay(this, +0.0, +0.0, +0.0, 0.45f),
+			StatusDisplay(statusManager, +0.0, -0.10, +0.0, 0.45f)
+		).register()
+
+		private fun cancelWithStatus(status: Component, sleepTicks: Int) {
+			setStatus(status)
+			tickingManager.sleep(sleepTicks)
 		}
 
-		event.isBurning = false
-		event.burnTime = 100
-		furnace.cookTime = (-1000).toShort()
-		event.isCancelled = false
+		override fun tick() {
+			val furnaceInventory = getInventory(0, 0, 0) as? FurnaceInventory ?: return cancelWithStatus(text("No Furnace"), 250)
+			val outputInventory = getInventory(0, 0, 4) ?: return cancelWithStatus(text("No Output Inventory", RED), 250)
 
-		val direction = sign.getFacing().oppositeFace
+			val fuel = furnaceInventory.fuel
 
-		val state = sign.block.getRelative(direction, 5).getState(false) as? InventoryHolder ?: return
+			if (powerStorage.getPower() < 250) return cancelWithStatus(text("No Power", RED), 100)
+			if (fuel?.type != Material.COBBLESTONE) return cancelWithStatus(text("Out of Cobblestone", RED), 100)
 
-		val product = sign.block.getRelative(sign.getFacing().oppositeFace, 3).type
-		val output = getOutput(product)
+			val product = getBlockRelative(0, 0, 2).type
+			val output = multiblock.getOutput(product)
 
-		val inventory = state.inventory
-		if (!LegacyItemUtils.canFit(inventory, output)) return
+			if (!LegacyItemUtils.canFit(outputInventory, output)) return cancelWithStatus(text("No Space", RED), 100)
+			LegacyItemUtils.addToInventory(outputInventory, output)
 
-		LegacyItemUtils.addToInventory(inventory, output)
-		fuel.amount = fuel.amount - 1
-		PowerMachines.removePower(sign, 250)
+			fuel.amount--
+
+			powerStorage.removePower(250)
+			tickingManager.sleep(100)
+
+			val furnace = furnaceInventory.holder ?: return
+
+			furnace.burnTime = Short.MAX_VALUE
+			furnace.cookTime = 100
+
+			furnace.update()
+		}
+
+		override fun storeAdditionalData(store: PersistentMultiblockData, adapterContext: PersistentDataAdapterContext) {
+			savePowerData(store)
+		}
+
+		override fun onLoad() {
+			displayHandler.update()
+		}
+
+		override fun onUnload() {
+			displayHandler.remove()
+		}
+
+		override fun handleRemoval() {
+			displayHandler.remove()
+		}
+
+		override fun displaceAdditional(movement: StarshipMovement) {
+			displayHandler.displace(movement)
+		}
+
+		override fun loadFromSign(sign: Sign) {
+			migrateLegacyPower(sign)
+		}
 	}
 }
