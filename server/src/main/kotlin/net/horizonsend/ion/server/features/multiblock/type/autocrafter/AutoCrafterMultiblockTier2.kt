8a530ef27338c6bf23daaa4455ed1c69f1c381b9
Gutin1 <Claytonpinter@gmail.com>
2024-10-10 00:02:57 -0500

    migrate auto crafters

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblockTier2.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblockTier2.kt
@@ -1,11 +1,13 @@
 package net.horizonsend.ion.server.features.multiblock.type.autocrafter
 
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.YELLOW
 import org.bukkit.Material
 
-object AutoCrafterMultiblockTier2 : AutoCrafterMultiblock("&eTier 2", Material.GOLD_BLOCK, iterations = 4) {
+object AutoCrafterMultiblockTier2 : AutoCrafterMultiblock(text("Tier 2", YELLOW), Material.GOLD_BLOCK, iterations = 4) {
 	override val maxPower: Int = 400_000
 }
 
-object AutoCrafterMultiblockTier2Mirrored : AutoCrafterMultiblockMirrored("&eTier 2", Material.GOLD_BLOCK, iterations = 4) {
+object AutoCrafterMultiblockTier2Mirrored : AutoCrafterMultiblockMirrored(text("Tier 2", YELLOW), Material.GOLD_BLOCK, iterations = 4) {
 	override val maxPower: Int = 400_000
 }
