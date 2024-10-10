df469a731b4169943fe19f8ef5fe350e06b76c7b
Gutin1 <Claytonpinter@gmail.com>
2024-10-10 00:02:57 -0500

    migrate auto crafters

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblockTier3.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblockTier3.kt
@@ -1,8 +1,10 @@
 package net.horizonsend.ion.server.features.multiblock.type.autocrafter
 
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.AQUA
 import org.bukkit.Material
 
-object AutoCrafterMultiblockTier3 : AutoCrafterMultiblock("&bTier 3", Material.DIAMOND_BLOCK, iterations = 6) {
+object AutoCrafterMultiblockTier3 : AutoCrafterMultiblock(text("Tier 3", AQUA), Material.DIAMOND_BLOCK, iterations = 6) {
 	override val maxPower: Int = 600_000
 }
 
