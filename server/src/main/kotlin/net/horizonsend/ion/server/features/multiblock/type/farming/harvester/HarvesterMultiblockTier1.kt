ad80ebc87df0603bea2ac65c7c865579672b21b7
Gutin1 <Claytonpinter@gmail.com>
2024-10-09 23:15:52 -0500

    update colors of planter / harvester tiers on signs

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/harvester/HarvesterMultiblockTier1.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/harvester/HarvesterMultiblockTier1.kt
@@ -1,8 +1,9 @@
 package net.horizonsend.ion.server.features.multiblock.type.farming.harvester
 
+import net.kyori.adventure.text.format.NamedTextColor.DARK_GRAY
 import org.bukkit.Material
 
-object HarvesterMultiblockTier1 : HarvesterMultiblock(Material.IRON_BLOCK, 1) {
+object HarvesterMultiblockTier1 : HarvesterMultiblock(Material.IRON_BLOCK, 1, DARK_GRAY) {
 	override val regionDepth: Int = 9
 	override val maxPower: Int = 50_000
 }
