a9bbbd7f93f553db737e7d733cc35c833b9eeb8f
Gutin1 <Claytonpinter@gmail.com>
2024-10-09 23:15:52 -0500

    update colors of planter / harvester tiers on signs

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/planter/PlanterMultiblockTier2.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/planter/PlanterMultiblockTier2.kt
@@ -1,8 +1,9 @@
 package net.horizonsend.ion.server.features.multiblock.type.farming.planter
 
+import net.kyori.adventure.text.format.NamedTextColor.GOLD
 import org.bukkit.Material
 
-object PlanterMultiblockTier2 : PlanterMultiblock(Material.GOLD_BLOCK, 2) {
+object PlanterMultiblockTier2 : PlanterMultiblock(Material.GOLD_BLOCK, 2, GOLD) {
 	override val regionDepth: Int = 15
 	override val maxPower: Int = 100_000
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/planter/PlanterMultiblockTier3.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/farming/planter/PlanterMultiblockTier3.kt
@@ -1,8 +1,9 @@
 package net.horizonsend.ion.server.features.multiblock.type.farming.planter
 
+import net.kyori.adventure.text.format.NamedTextColor.GREEN
 import org.bukkit.Material
 
-object PlanterMultiblockTier3 : PlanterMultiblock(Material.EMERALD_BLOCK, 3) {
+object PlanterMultiblockTier3 : PlanterMultiblock(Material.EMERALD_BLOCK, 3, GREEN) {
 	override val regionDepth: Int = 18
 	override val maxPower: Int = 200_000
 }
