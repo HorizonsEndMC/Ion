8cf3d18eb049d018f90037cd3640227d04777f1f
Gutin1 <Claytonpinter@gmail.com>
2024-10-13 14:41:53 -0500

    fix

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/misc/Halloweeeeeen.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/misc/Halloweeeeeen.kt
@@ -3,6 +3,8 @@ package net.horizonsend.ion.server.features.multiblock.type.misc
 import net.horizonsend.ion.common.utils.miscellaneous.testRandom
 import net.horizonsend.ion.common.utils.text.colors.ABYSSAL_DESATURATED_RED
 import net.horizonsend.ion.server.IonServerComponent
+import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
+import net.horizonsend.ion.server.features.world.WorldFlag
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
@@ -11,7 +13,7 @@ import org.bukkit.entity.Player
 import java.util.UUID
 
 object Halloweeeeeen : IonServerComponent() {
-	val spooked = mutableSetOf<UUID>()
+	private val spooked = mutableSetOf<UUID>()
 
 	override fun onEnable() {
 		Tasks.asyncRepeat(1200L, 1200L, ::checkPlayers)
@@ -23,11 +25,12 @@ object Halloweeeeeen : IonServerComponent() {
 
 	private fun checkPlayer(player: Player) {
 		if (spooked.contains(player.uniqueId)) return
+		if (!player.world.ion.hasFlag(WorldFlag.SPACE_WORLD)) return
 		if (testRandom(0.9)) return
 
 		Tasks.sync {
-			val rayCast = player.world.rayTrace(player.eyeLocation, player.location.direction, 200.0, FluidCollisionMode.ALWAYS, false, 0.1) { false } ?: return@sync
-			if (rayCast.hitBlock == null) return@sync
+			val rayCast = player.world.rayTrace(player.eyeLocation, player.location.direction, 200.0, FluidCollisionMode.ALWAYS, false, 0.1) { false }
+			if (rayCast != null) return@sync
 
 			player.sendMessage(Component.text("The abyss stares back...", ABYSSAL_DESATURATED_RED))
 			spooked.add(player.uniqueId)
