3eb75ab49ad7478619eff8da3ef986295acc98cb
Gutin1 <Claytonpinter@gmail.com>
2024-10-13 19:58:56 -0500

    improve event shield

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/particleshield/EventShieldMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/particleshield/EventShieldMultiblock.kt
@@ -3,27 +3,30 @@ package net.horizonsend.ion.server.features.multiblock.type.particleshield
 import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.features.multiblock.MultiblockShape
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.PermissionWeaponSubsystem
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.Material
 import org.bukkit.block.Sign
 import org.bukkit.entity.Player
 
-object EventShieldMultiblock : SphereShieldMultiblock(), PermissionWeaponSubsystem {
+object EventShieldMultiblock : ShieldMultiblock(), PermissionWeaponSubsystem {
+	override val isReinforced: Boolean = true
 	override val permission: String = "ioncore.eventweapon"
 
 	override val signText = createSignText(
-		line1 = "&3Particle Shield",
+		line1 = "&EVENT Shield",
 		line2 = "&7Generator",
 		line3 = null,
-		line4 = "&8Class &d0.8i"
+		line4 = "DONOTUSE"
 	)
-
-	override val maxRange = 220
-	override val isReinforced: Boolean = true
 
 	override fun setupSign(player: Player, sign: Sign) {
 		if (!player.hasPermission("ioncore.eventweapon")) return player.userError("Nuh uh")
 
 		super.setupSign(player, sign)
+	}
+
+	override fun getShieldBlocks(sign: Sign): List<Vec3i> {
+		return listOf()
 	}
 
 	override fun MultiblockShape.buildStructure() {
