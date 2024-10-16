038bfee1f3bac61a4dd4bf967e8fc6a519c2ae0a
Gutin1 <Claytonpinter@gmail.com>
2024-10-16 14:09:34 -0500

    persist progress over server restarts

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ProgressMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/ProgressMultiblock.kt
@@ -4,17 +4,27 @@ import net.horizonsend.ion.common.utils.miscellaneous.roundToHundredth
 import net.horizonsend.ion.common.utils.text.colors.HEColorScheme
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.LAST_PROGRESS_TICK
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.PROGRESS
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.persistence.PersistentDataType.DOUBLE
+import org.bukkit.persistence.PersistentDataType.LONG
 import java.time.Duration
 
 interface ProgressMultiblock {
 	val progressManager: ProgressManager
+	fun tickProgress(totalDuration: Duration) {
+		val current = System.currentTimeMillis()
+		val delta = Duration.ofMillis(current - progressManager.lastProgressTick)
+
+		progressManager.lastProgressTick = current
+		progressManager.addProgress(totalDuration, delta)
+	}
 
 	class ProgressManager(data: PersistentMultiblockData) {
+		var lastProgressTick: Long = data.getAdditionalDataOrDefault(LAST_PROGRESS_TICK, LONG, System.currentTimeMillis())
 		private var currentProgress: Double = data.getAdditionalDataOrDefault(PROGRESS, DOUBLE, 0.0)
 
 		/**
@@ -39,6 +49,7 @@ interface ProgressMultiblock {
 
 		fun saveProgressData(store: PersistentMultiblockData) {
 			store.addAdditionalData(PROGRESS, DOUBLE, currentProgress)
+			store.addAdditionalData(LAST_PROGRESS_TICK, LONG, lastProgressTick)
 		}
 
 		fun formatProgress(color: TextColor): Component {
