666e095382da31b9bbd72f5b8c3687f65467744b
Gutin1 <Claytonpinter@gmail.com>
2024-10-17 00:14:58 -0500

    new recipe system, needs testing

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/recipe/ProcessingMultiblockRecipe.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/recipe/ProcessingMultiblockRecipe.kt
@@ -1,63 +1,42 @@
 package net.horizonsend.ion.server.features.multiblock.crafting.recipe
 
-import net.horizonsend.ion.server.features.multiblock.Multiblock
-import net.horizonsend.ion.server.features.multiblock.crafting.ingredient.ItemConsumable
+import net.horizonsend.ion.server.features.multiblock.crafting.ingredient.ItemIngredient
 import net.horizonsend.ion.server.features.multiblock.crafting.ingredient.MultiblockRecipeIngredient
-import net.horizonsend.ion.server.features.multiblock.crafting.ingredient.ResourceIngredient
+import net.horizonsend.ion.server.features.multiblock.crafting.ingredient.PowerIngredient
 import net.horizonsend.ion.server.features.multiblock.crafting.result.MultiblockRecipeResult
-import net.horizonsend.ion.server.features.multiblock.type.FurnaceMultiblock
-import org.bukkit.block.Furnace
-import org.bukkit.block.Sign
-import org.bukkit.event.inventory.FurnaceBurnEvent
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
 import org.bukkit.inventory.FurnaceInventory
 import org.bukkit.inventory.Inventory
+import kotlin.reflect.KClass
 
 /**
  * A multiblock recipe that takes one ingredient, and produces one result
  * Usually furnace multiblocks with prismarine crystals occupying the bottom slot
  *
- * @param time, in ticks
  * @param smelting, the input resource
  * @param result, the result of the recipe
  **/
-class ProcessingMultiblockRecipe<T: Multiblock>(
-	override val multiblock: T,
-	val smelting: MultiblockRecipeIngredient,
-	override val result: MultiblockRecipeResult,
-	private val resources: List<ResourceIngredient> = listOf(),
-) : MultiblockRecipe<T>, FurnaceEventHandler {
-	init {
-	    require(multiblock is FurnaceMultiblock)
+class ProcessingMultiblockRecipe<T: MultiblockEntity>(
+	multiblock: EntityMultiblock<T>,
+	multiblockEntityClass: KClass<T>,
+	val smelting: MultiblockRecipeIngredient<T>,
+	override val result: MultiblockRecipeResult<T>,
+	private val resources: List<PowerIngredient<T>> = listOf(),
+) : MultiblockRecipe<T>(multiblock, multiblockEntityClass) {
+	override fun finalize(context: RecipeExecutionContext<T>) {
+		result.execute(context)
 	}
 
-	override fun matches(sign: Sign, inventory: Inventory): Boolean {
-		inventory as FurnaceInventory
-		return smelting.checkRequirement(multiblock, sign, inventory.smelting)
+	override fun getLabeledInventories(context: RecipeExecutionContext<T>): Map<String, Inventory> {
+		return mapOf(ItemIngredient.MAIN_FURNACE_STRING to context.entity.getInventory(0, 0, 0) as FurnaceInventory)
 	}
 
-	override fun execute(sign: Sign, inventory: Inventory) {
-		inventory as FurnaceInventory
-
-		val holder = inventory.holder!!
-		if (!(holder.blockData as org.bukkit.block.data.type.Furnace).isLit) return
-
-		if (!result.canFit(this, inventory, sign)) return
-
-		// Return if enough ingredients are not present
-		if (!smelting.checkRequirement(multiblock, sign, inventory.smelting)) return
-
-		if (resources.any { !it.checkRequirement(multiblock, sign, null) }) return
-
-		result.execute(this, inventory, sign)
-
-		if (smelting is ItemConsumable) smelting.consume(multiblock, sign, inventory.smelting!!)
-
-		resources.forEach { it.consume(multiblock, sign) }
+	override fun checkResourcesAvailable(context: RecipeExecutionContext<T>): Boolean {
+		return smelting.checkRequirement(context) && resources.all { it.checkRequirement(context) }
 	}
 
-	override fun handleFurnaceEvent(event: FurnaceBurnEvent, furnace: Furnace, sign: Sign) {
-		event.isBurning = false
-		event.burnTime = 200
-		event.isCancelled = false
+	override fun consumeResources(context: RecipeExecutionContext<T>): Boolean {
+		return smelting.consumeIngredient(context) && resources.all { it.consumeIngredient(context) }
 	}
 }
