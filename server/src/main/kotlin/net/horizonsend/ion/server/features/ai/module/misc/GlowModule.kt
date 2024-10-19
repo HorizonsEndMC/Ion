5c42cb8bc65ab1f9648ffddf3612ccdbf28e2bf9
Gutin1 <Claytonpinter@gmail.com>
2024-10-18 22:40:32 -0500

    improve AI glow

--- server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/misc/GlowModule.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/misc/GlowModule.kt
@@ -1,34 +1,27 @@
 package net.horizonsend.ion.server.features.ai.module.misc
 
-import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
 import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
-import net.horizonsend.ion.server.miscellaneous.utils.minecraft
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.ItemDisplayContainer
 import org.bukkit.Material
-import java.util.UUID
+import org.bukkit.block.BlockFace
+import org.bukkit.inventory.ItemStack
 
 class GlowModule(controller: AIController) : net.horizonsend.ion.server.features.ai.module.AIModule(controller) {
-	private val entity = ClientDisplayEntities.displayBlock(world.minecraft, JUKEBOX, getCenter().toVector(), 1.5f, true)
-	private val seen = LinkedHashMap<UUID, Long>()
+	val container = ItemDisplayContainer(
+		world,
+		1.5f,
+		starship.centerOfMass.toCenterVector(),
+		BlockFace.UP.direction,
+		ItemStack(Material.JUKEBOX)
+	).apply {
+		getEntity().setGlowingTag(true)
+	}
 
 	override fun onMove(movement: StarshipMovement) {
-		val newLoc = movement.displaceLocation(getCenter().toLocation(world))
+		container.position = starship.centerOfMass.toCenterVector()
 
-		entity.setPos(newLoc.x, newLoc.y, newLoc.z)
-
-		for (player in newLoc.chunk.minecraft.playerChunk?.getPlayers(false).orEmpty()) {
-			ClientDisplayEntities.moveDisplayEntityPacket(
-				player,
-				entity,
-				newLoc.x,
-				newLoc.y,
-				newLoc.z
-			)
-		}
-
-		if (movement.newWorld == null) return
-
-		entity.setLevel(movement.newWorld.minecraft)
+		container.update()
 	}
 
 	private var ticks = 0
@@ -38,28 +31,6 @@ class GlowModule(controller: AIController) : net.horizonsend.ion.server.features
 
 		if (ticks % 20 != 0) return
 
-		updatePlayers()
-	}
-
-	private fun updatePlayers() {
-		val chunk = getCenter().toLocation(world).chunk.minecraft
-
-		val playerChunk = chunk.playerChunk ?: return
-
-		for (player in playerChunk.getPlayers(false)) {
-			if (!seen.contains(player.uuid)) {
-				ClientDisplayEntities.sendEntityPacket(player, entity, 10 * 20L)
-				seen[player.uuid] = System.currentTimeMillis()
-			} else if (System.currentTimeMillis() - seen.getOrDefault(player.uuid, 0) >= 5000) {
-				seen.remove(player.uuid)
-				ClientDisplayEntities.deleteDisplayEntityPacket(player.bukkitEntity, entity)
-			}
-
-			entity.entityData.refresh(player)
-		}
-	}
-
-	companion object {
-		private val JUKEBOX = Material.JUKEBOX.createBlockData()
+		container.update()
 	}
 }
