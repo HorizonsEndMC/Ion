9ca9ad381f5e7d73316063503451dca05fedf0f4
Gutin1 <Claytonpinter@gmail.com>
2024-10-23 23:56:03 -0500

    update combat tag stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/features/player/CombatTimer.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/player/CombatTimer.kt
@@ -14,6 +14,7 @@ import net.horizonsend.ion.server.features.cache.PlayerCache
 import net.horizonsend.ion.server.features.nations.utils.toPlayersInRadius
 import net.horizonsend.ion.server.features.player.NewPlayerProtection.hasProtection
 import net.horizonsend.ion.server.features.starship.PilotedStarships
+import net.horizonsend.ion.server.features.starship.TypeCategory
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
 import net.horizonsend.ion.server.features.starship.control.controllers.player.PlayerController
@@ -80,7 +81,7 @@ object CombatTimer : IonServerComponent() {
 
 				// Only actively controlled warships can cause proximity triggered combat tags
 				if (pilotedStarship != null && pilotedStarship.controller !is UnpilotedController &&
-					pilotedStarship.type.isWarship) {
+					pilotedStarship.type.typeCategory == TypeCategory.WAR_SHIP) {
 					val starshipCom  = pilotedStarship.centerOfMass.toLocation(player.world)
 
 					if (pilotedStarship.isInterdicting) {
