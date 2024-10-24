54be1342e1a6dd982e5a2d86247c4f3afe589906
Gutin1 <Claytonpinter@gmail.com>
2024-10-24 01:49:46 -0500

    use new itemName util

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/text/Formatting.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/text/Formatting.kt
@@ -1,9 +1,11 @@
 package net.horizonsend.ion.server.miscellaneous.utils.text
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor.WHITE
 import net.kyori.adventure.text.format.TextDecoration
 
 val Component.itemName get() = Component.text()
 	.decoration(TextDecoration.ITALIC, false)
+	.color(WHITE)
 	.append(this)
 	.build()
