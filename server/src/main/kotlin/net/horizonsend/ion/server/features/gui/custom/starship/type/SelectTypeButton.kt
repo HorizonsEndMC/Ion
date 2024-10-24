a1d879287e46baa2bd34256c2b9e67aa0411d1e9
Gutin1 <Claytonpinter@gmail.com>
2024-10-24 02:35:48 -0500

    Ability to test Tank movement, implement menu subclasses

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/starship/type/SelectTypeButton.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/starship/type/SelectTypeButton.kt
@@ -3,20 +3,28 @@ package net.horizonsend.ion.server.features.gui.custom.starship.type
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.utils.text.ITALIC
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.server.features.gui.GuiItems
 import net.horizonsend.ion.server.features.starship.DeactivatedPlayerStarships
 import net.horizonsend.ion.server.features.starship.StarshipType
 import net.horizonsend.ion.server.miscellaneous.utils.setDisplayNameAndGet
 import net.horizonsend.ion.server.miscellaneous.utils.setLoreAndGet
+import net.horizonsend.ion.server.miscellaneous.utils.text.itemName
 import net.kyori.adventure.text.Component.empty
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor.AQUA
 import net.kyori.adventure.text.format.NamedTextColor.GRAY
+import net.kyori.adventure.text.format.NamedTextColor.WHITE
+import org.bukkit.Material.BARRIER
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
+import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
+import xyz.xenondevs.invui.gui.PagedGui
+import xyz.xenondevs.invui.gui.structure.Markers
 import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.item.impl.AbstractItem
+import xyz.xenondevs.invui.window.Window
 
 class SelectTypeButton(val parent: ChangeTypeButton, val type: StarshipType) : AbstractItem() {
 	private val subClasses = type.menuSubclasses.get().filter { it.canUse(parent.main.player) }
@@ -29,7 +37,7 @@ class SelectTypeButton(val parent: ChangeTypeButton, val type: StarshipType) : A
 				ofChildren(text("Maximum Block Count: ", GRAY), text(type.maxSize, AQUA)).decoration(ITALIC, false),
 				empty(),
 				text("Left click to select", AQUA).decoration(ITALIC, false),
-				if (subClasses.isNotEmpty()) text("Right click to view subclasses", AQUA).decoration(ITALIC, false) else empty(),
+				if (subClasses.isNotEmpty()) text("Right click to view [${subClasses.size}] subclass(es)", AQUA).decoration(ITALIC, false) else empty(),
 			))
 	}
 
@@ -37,7 +45,7 @@ class SelectTypeButton(val parent: ChangeTypeButton, val type: StarshipType) : A
 
 	override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
 		if (subClasses.isNotEmpty() && clickType == ClickType.RIGHT) {
-			player.success("boop")
+			openSubclassMenu(player)
 			return
 		}
 
@@ -46,5 +54,36 @@ class SelectTypeButton(val parent: ChangeTypeButton, val type: StarshipType) : A
 		parent.main.open()
 
 		player.success("Changed type to $type")
+	}
+
+	fun openSubclassMenu(player: Player) {
+		val returnToPilotMenu = GuiItems.createButton(
+			ItemStack(BARRIER).setDisplayNameAndGet(text("Go back to class selection menu", WHITE).itemName)
+		) { _, _, _ ->
+			player.closeInventory()
+			parent.openClassMenu(player)
+		}
+
+		val subclassButtons = type.menuSubclasses.get().map { SelectTypeButton(parent, it) }
+
+		val gui = PagedGui.items()
+			.setStructure(
+				"b . . . . . . l r",
+				"x x x x x x x x x",
+				"x x x x x x x x x",
+			)
+			.addIngredient('b', returnToPilotMenu)
+			.addIngredient('l', GuiItems.PageLeftItem())
+			.addIngredient('r', GuiItems.PageRightItem())
+			.addIngredient('x', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
+			.setContent(subclassButtons)
+			.build()
+
+		Window.single()
+			.setViewer(player)
+			.setTitle(AdventureComponentWrapper(ofChildren(type.displayNameComponent, text("'s subclasses")).itemName))
+			.setGui(gui)
+			.build()
+			.open()
 	}
 }
