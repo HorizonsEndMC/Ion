de483a3b88a3b48a4e2f1e6eebf9969bd68edd1b
kwazedilla <kwazedilla@gmail.com>
2024-10-27 23:55:11 -0700

    shifting menus for planets and beacons (plus a TON of refactoring)

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/navigation/NavigationSystemMapGui.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/navigation/NavigationSystemMapGui.kt
@@ -6,6 +6,7 @@ import com.google.common.cache.LoadingCache
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.custom.items.CustomItems
 import net.horizonsend.ion.server.features.gui.GuiItem
+import net.horizonsend.ion.server.features.gui.GuiItems
 import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.features.space.Space
 import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
@@ -21,35 +22,99 @@ import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.item.impl.SimpleItem
 
 class NavigationSystemMapGui(val world: World) {
+
+	private var planetListShift = 0
+	private var beaconListShift = 0
+	private val gui = Gui.empty(9, 5)
+
+	private var planetsInWorld = 0
+	private var beaconsInWorld = 0
+
+	private val planetItems = mutableListOf<ItemStack>()
+	private val beaconItems = mutableListOf<ItemStack>()
+
+	private val leftPlanetButton = GuiItems.CustomControlItem("Previous Planet In This System", GuiItem.LEFT) {
+		planetListShift--
+		updateGui()
+		println("DEBUG: Current value of planetListShift: $planetListShift")
+	}
+
+	private val rightPlanetButton = GuiItems.CustomControlItem("Next Planet In This System", GuiItem.RIGHT) {
+		planetListShift++
+		updateGui()
+		println("DEBUG: Current value of planetListShift: $planetListShift")
+	}
+
+	private val leftBeaconButton = GuiItems.CustomControlItem("Previous Beacon In This System", GuiItem.LEFT) {
+		beaconListShift--
+		updateGui()
+		println("DEBUG: Current value of beaconListShift: $beaconListShift")
+	}
+
+	private val rightBeaconButton = GuiItems.CustomControlItem("Next Beacon In This System", GuiItem.RIGHT) {
+		beaconListShift++
+		updateGui()
+		println("DEBUG: Current value of beaconListShift: $beaconListShift")
+	}
+
+	companion object {
+		val ionWorldCache: LoadingCache<WorldFlag, Collection<World>> = CacheBuilder.newBuilder().build(
+			CacheLoader.from { worldFlag: WorldFlag ->
+				return@from Bukkit.getWorlds().filter { world -> world.ion.hasFlag(worldFlag) }
+			}
+		)
+
+		private const val MIDDLE_COLUMN = 4
+		private const val MAX_ELEMENTS_PER_ROW = 7
+		private const val PLANET_ROW = 1
+		private const val BEACON_ROW = 2
+	}
+
 	fun createGui(): Gui {
-		val gui = Gui.empty(9, 5)
 
 		val star = Space.getStars().firstOrNull { star -> star.spaceWorld == world }
 		val starItem = if (star != null) getItemStack(star.name) else ItemStack(Material.AIR)
-		val planetList = Space.getPlanets()
+
+		planetItems.addAll(Space.getPlanets()
 			.filter { planet -> planet.spaceWorld == world }
 			.map { planet -> getItemStack(planet.name) }
-		val beaconList = IonServer.configuration.beacons
+		)
+		planetsInWorld = planetItems.size
+
+		beaconItems.addAll(IonServer.configuration.beacons
 			.filter { beacon -> beacon.spaceLocation.bukkitWorld() == world }
 			.map { beacon -> ItemStack(Material.WARPED_FUNGUS_ON_A_STICK).updateMeta { item ->
 				item.setCustomModelData(GuiItem.BEACON.customModelData)
 				item.displayName(Component.text(beacon.name).decoration(TextDecoration.ITALIC, false))
 			}}
+		)
+		beaconsInWorld = beaconItems.size
 
-		gui.setItem(0, 1, SimpleItem(starItem))
-		for (widthIndex in 0 until planetList.size.coerceAtMost(7)) {
-			gui.setItem(widthIndex + 2, 1, SimpleItem(planetList[widthIndex]))
-		}
-		for (widthIndex in 0 until beaconList.size.coerceAtMost(7)) {
-			gui.setItem(widthIndex + 2, 2, SimpleItem(beaconList[widthIndex]))
-		}
+		gui.setItem(MIDDLE_COLUMN, 0, SimpleItem(starItem))
 
 		gui.setItem(0, 4, SimpleItem(ItemStack(Material.WARPED_FUNGUS_ON_A_STICK).updateMeta {
 			it.displayName(Component.text("Return to Galactic Map").decoration(TextDecoration.ITALIC, false))
 			it.setCustomModelData(GuiItem.DOWN.customModelData)
 		}))
 
+		updateGui()
+
 		return gui
+	}
+
+	private fun updateGui() {
+		gui.setItem(0, PLANET_ROW, if (planetListShift > 0) leftPlanetButton else GuiItems.EmptyItem())
+		gui.setItem(8, PLANET_ROW, if (planetListShift + MAX_ELEMENTS_PER_ROW < planetsInWorld) rightPlanetButton else GuiItems.EmptyItem())
+		gui.setItem(0, BEACON_ROW, if (beaconListShift > 0) leftBeaconButton else GuiItems.EmptyItem())
+		gui.setItem(8, BEACON_ROW, if (beaconListShift + MAX_ELEMENTS_PER_ROW < beaconsInWorld) rightBeaconButton else GuiItems.EmptyItem())
+
+		for (widthIndex in 0 until planetItems.size.coerceAtMost(MAX_ELEMENTS_PER_ROW)) {
+			gui.setItem(widthIndex + 1, PLANET_ROW, SimpleItem(planetItems[widthIndex + planetListShift]))
+		}
+
+		for (widthIndex in 0 until beaconItems.size.coerceAtMost(MAX_ELEMENTS_PER_ROW)) {
+			gui.setItem(widthIndex + 1, BEACON_ROW, SimpleItem(beaconItems[widthIndex + beaconListShift]))
+		}
 	}
 
 	fun createText(): Component {
@@ -95,13 +160,5 @@ class NavigationSystemMapGui(val world: World) {
 
 			else -> CustomItems.AERACH
 		}.constructItemStack()
-	}
-
-	companion object {
-		val ionWorldCache: LoadingCache<WorldFlag, Collection<World>> = CacheBuilder.newBuilder().build(
-			CacheLoader.from { worldFlag: WorldFlag ->
-				return@from Bukkit.getWorlds().filter { world -> world.ion.hasFlag(worldFlag) }
-			}
-		)
 	}
 }
