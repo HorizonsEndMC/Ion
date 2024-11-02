593bdfcf7abaf862c32d12443d5d6922b32e66e5
Gutin1 <Claytonpinter@gmail.com>
2024-11-02 14:30:27 -0500

    Add common interface for multiblocks with displays

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/DisplayMultiblockEntity.kt
@@ -0,0 +1,7 @@
+package net.horizonsend.ion.server.features.multiblock.entity.type
+
+import net.horizonsend.ion.server.features.client.display.modular.TextDisplayHandler
+
+interface DisplayMultiblockEntity {
+	val displayHandler: TextDisplayHandler
+}
