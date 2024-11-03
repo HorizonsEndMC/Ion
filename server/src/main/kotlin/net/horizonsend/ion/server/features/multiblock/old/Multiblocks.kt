e7570e71bfda51556b63a8d64aa1f840d1cab873
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 13:31:20 -0600

    migrate power furnaces

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
@@ -7,17 +7,9 @@ import net.horizonsend.ion.server.features.multiblock.type.fluid.GasPowerPlantMu
 import net.horizonsend.ion.server.features.multiblock.type.fluid.VentMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.fluid.collector.GasCollectorMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.misc.ItemSplitterMultiblock
-import net.horizonsend.ion.server.features.multiblock.type.power.powerfurnace.PowerFurnaceMultiblockTier1
-import net.horizonsend.ion.server.features.multiblock.type.power.powerfurnace.PowerFurnaceMultiblockTier2
-import net.horizonsend.ion.server.features.multiblock.type.power.powerfurnace.PowerFurnaceMultiblockTier3
 
 object Multiblocks : IonServerComponent() {
 	private fun initMultiblocks() {
-		// Furnace start
-		registerMultiblock(PowerFurnaceMultiblockTier1)
-		registerMultiblock(PowerFurnaceMultiblockTier2)
-		registerMultiblock(PowerFurnaceMultiblockTier3)
-
 		registerMultiblock(StandardAmmoPressMultiblock)
 
 		registerMultiblock(GasCollectorMultiblock)
