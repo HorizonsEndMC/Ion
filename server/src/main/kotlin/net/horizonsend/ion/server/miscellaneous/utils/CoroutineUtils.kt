0b56d7969edd55fcf787e4becf202e06f9d2d75e
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 16:11:59 -0600

    Remove jank async block utils

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/CoroutineUtils.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/utils/CoroutineUtils.kt
@@ -1,11 +1,5 @@
 package net.horizonsend.ion.server.miscellaneous.utils
 
-import kotlinx.coroutines.CoroutineScope
-import org.bukkit.World
-import org.bukkit.block.Block
+import kotlinx.coroutines.Deferred
 
-//TODO async block requests, updates, etc
-
-fun CoroutineScope.getBlock(world: World, x: Int, y: Int, z: Int): Block? {
-	return Tasks.getSyncBlocking { world.getBlockAt(x, y, z) } // TODO
-}
+suspend fun <K, V> Map<K, Deferred<V>>.awaitAllValues(): Map<K, V> = if (isEmpty()) mapOf() else mapValues { (_, v) -> v.await() }
