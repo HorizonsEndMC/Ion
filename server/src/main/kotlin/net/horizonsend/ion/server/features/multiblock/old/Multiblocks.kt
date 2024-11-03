53034c972389b82c065087ef0f6b067cf9dab122
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 14:37:36 -0600

    migrate item splitter

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
@@ -6,7 +6,6 @@ import net.horizonsend.ion.server.features.multiblock.type.ammo.StandardAmmoPres
 import net.horizonsend.ion.server.features.multiblock.type.fluid.GasPowerPlantMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.fluid.VentMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.fluid.collector.GasCollectorMultiblock
-import net.horizonsend.ion.server.features.multiblock.type.misc.ItemSplitterMultiblock
 
 object Multiblocks : IonServerComponent() {
 	private fun initMultiblocks() {
@@ -15,9 +14,6 @@ object Multiblocks : IonServerComponent() {
 		registerMultiblock(GasCollectorMultiblock)
 		registerMultiblock(VentMultiblock)
 		registerMultiblock(GasPowerPlantMultiblock)
-		// Furnace end
-
-		registerMultiblock(ItemSplitterMultiblock)
 	}
 
 	private fun registerMultiblock(multiblock: Multiblock) {}
