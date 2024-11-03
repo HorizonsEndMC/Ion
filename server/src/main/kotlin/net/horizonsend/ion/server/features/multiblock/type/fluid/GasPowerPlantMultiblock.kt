607ae88fb2248d9118f1b178396783761f733046
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 15:52:31 -0600

    migrate gas power plant, all old multis done

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/GasPowerPlantMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/GasPowerPlantMultiblock.kt
@@ -1,33 +1,44 @@
 package net.horizonsend.ion.server.features.multiblock.type.fluid
 
 import net.horizonsend.ion.server.configuration.ConfigurationFiles
+import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
+import net.horizonsend.ion.server.features.client.display.modular.display.PowerEntityDisplay
+import net.horizonsend.ion.server.features.client.display.modular.display.StatusDisplay
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.customItem
 import net.horizonsend.ion.server.features.custom.items.type.GasCanister
 import net.horizonsend.ion.server.features.gas.Gasses.EMPTY_CANISTER
 import net.horizonsend.ion.server.features.gas.type.GasFuel
 import net.horizonsend.ion.server.features.gas.type.GasOxidizer
-import net.horizonsend.ion.server.features.machine.PowerMachines
 import net.horizonsend.ion.server.features.multiblock.Multiblock
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.DisplayMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.StatusMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerStorage
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.StatusTickedMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent
+import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
-import net.horizonsend.ion.server.features.multiblock.type.FurnaceMultiblock
-import net.horizonsend.ion.server.features.multiblock.type.PowerStoringMultiblock
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
-import net.horizonsend.ion.server.miscellaneous.utils.getFacing
-import net.horizonsend.ion.server.miscellaneous.utils.getStateIfLoaded
-import net.horizonsend.ion.server.miscellaneous.utils.rightFace
+import net.horizonsend.ion.server.features.multiblock.type.NewPoweredMultiblock
+import net.horizonsend.ion.server.miscellaneous.utils.LegacyItemUtils
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Effect
-import org.bukkit.block.Container
-import org.bukkit.block.Furnace
+import org.bukkit.World
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
-import org.bukkit.event.inventory.FurnaceBurnEvent
+import org.bukkit.inventory.FurnaceInventory
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataAdapterContext
 import kotlin.math.roundToInt
 
-object GasPowerPlantMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMultiblock {
+object GasPowerPlantMultiblock : Multiblock(), NewPoweredMultiblock<GasPowerPlantMultiblock.GasPowerPlantMultiblockEntity> {
+
 	override val maxPower: Int = 500000
 
 	override val name: String = "gaspowerplant"
@@ -157,108 +168,110 @@ object GasPowerPlantMultiblock : Multiblock(), PowerStoringMultiblock, FurnaceMu
 		}
 	}
 
-	override fun onFurnaceTick(event: FurnaceBurnEvent, furnace: Furnace, sign: Sign) {
-		event.isBurning = false
-		event.burnTime = 0
-		event.isCancelled = true
-
-		val inventory = furnace.inventory
-
-		val fuelItem = inventory.smelting ?: return
-		val oxidizerItem = inventory.fuel ?: return
-
-		val fuel = (fuelItem.customItem as? GasCanister) ?: return
-		val oxidizer = (oxidizerItem.customItem as? GasCanister) ?: return
-
-		val fuelType = fuel.gas
-		val oxidizerType = oxidizer.gas
-
-		if (fuelType !is GasFuel || oxidizerType !is GasOxidizer) return
-
-		val consumed = checkCanisters(sign, furnace, fuelItem, fuel, oxidizerItem, oxidizer) ?: return
-
-		if (PowerMachines.getPower(sign) <= maxPower) {
-			event.isBurning = true
-			furnace.burnTime = fuelType.cooldown.toShort()
-			furnace.cookTime = (-1000).toShort()
-
-			val power = (fuelType.powerPerUnit * oxidizerType.powerMultiplier) * consumed
-			PowerMachines.addPower(sign, power.roundToInt())
-		} else {
-			furnace.world.playEffect(furnace.location.add(0.5, 0.5, 0.5), Effect.SMOKE, 4)
-		}
+	override fun createEntity(manager: MultiblockManager, data: PersistentMultiblockData, world: World, x: Int, y: Int, z: Int, structureDirection: BlockFace): GasPowerPlantMultiblockEntity {
+		return GasPowerPlantMultiblockEntity(data, manager, x, y, z, world, structureDirection)
 	}
 
-	private fun checkCanisters(
-		sign: Sign,
-		furnace: Furnace,
-		fuelItem: ItemStack,
-		fuelType: GasCanister,
-		oxidizerItem: ItemStack,
-		oxidizerType: GasCanister
-	): Int? {
-		val fuelFill = fuelType.getFill(fuelItem)
-		val oxidizerFill = oxidizerType.getFill(oxidizerItem)
+	class GasPowerPlantMultiblockEntity(
+		data: PersistentMultiblockData,
+		manager: MultiblockManager,
+		x: Int,
+		y: Int,
+		z: Int,
+		world: World,
+		structureDirection: BlockFace,
+	) : MultiblockEntity(manager, GasPowerPlantMultiblock, x, y, z, world, structureDirection), SyncTickingMultiblockEntity, PoweredMultiblockEntity, StatusTickedMultiblockEntity, LegacyMultiblockEntity, DisplayMultiblockEntity {
+		override val multiblock: GasPowerPlantMultiblock = GasPowerPlantMultiblock
+		override val powerStorage: PowerStorage = loadStoredPower(data)
+		override val tickingManager: TickedMultiblockEntityParent.TickingManager = TickedMultiblockEntityParent.TickingManager(interval = 20)
+		override val statusManager: StatusMultiblockEntity.StatusManager = StatusMultiblockEntity.StatusManager()
 
-		// Burn fuel and oxidizer at 1:1
-		// Cap consumption at 30 units
-		val consumed = minOf(ConfigurationFiles.globalGassesConfiguration().powerPlantConsumption, fuelFill, oxidizerFill)
+		override val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+			this,
+			PowerEntityDisplay(this, +0.0, +0.0, +0.0, 0.45f),
+			StatusDisplay(statusManager, +0.0, -0.10, +0.0, 0.45f)
+		).register()
 
-		// God forbid it goes negative
-		if (fuelFill <= 0) {
-			clearEmpty(sign, furnace.inventory, fuelItem)
-			return null
+		override fun tick() {
+			val inventory = getInventory(0, 0, 0) as? FurnaceInventory ?: return
+
+			val fuelItem = inventory.smelting ?: return
+			val oxidizerItem = inventory.fuel ?: return
+
+			val fuel = (fuelItem.customItem as? GasCanister) ?: return
+			val oxidizer = (oxidizerItem.customItem as? GasCanister) ?: return
+
+			val fuelType = fuel.gas
+			val oxidizerType = oxidizer.gas
+
+			if (fuelType !is GasFuel || oxidizerType !is GasOxidizer) return
+
+			val consumed = checkCanisters(inventory, fuelItem, fuel, oxidizerItem, oxidizer) ?: return
+
+			if (powerStorage.getPower() < maxPower) {
+				tickingManager.sleep(fuelType.cooldown)
+
+				inventory.holder?.burnTime = fuelType.cooldown.toShort()
+				inventory.holder?.update()
+
+				val power = (fuelType.powerPerUnit * oxidizerType.powerMultiplier) * consumed
+				powerStorage.addPower(power.roundToInt())
+			} else {
+				world.playEffect(location.toCenterLocation(), Effect.SMOKE, 4)
+			}
 		}
 
-		if (oxidizerFill <= 0) {
-			clearEmpty(sign, furnace.inventory, oxidizerItem)
-			return null
+		override fun storeAdditionalData(store: PersistentMultiblockData, adapterContext: PersistentDataAdapterContext) {
+			savePowerData(store)
 		}
 
-		if (fuelFill - consumed <= 0) {
-			// Replaces with empty, no need to set fill to zero
-			if (clearEmpty(sign, furnace.inventory, fuelItem)) return null
-		} else {
-			fuelType.setFill(fuelItem, fuelFill - consumed)
+		override fun loadFromSign(sign: Sign) {
+			migrateLegacyPower(sign)
 		}
 
-		if (oxidizerFill - consumed <= 0) {
-			// Replaces with empty, no need to set fill to zero
-			if (clearEmpty(sign, furnace.inventory, oxidizerItem)) return null
-		} else {
-			oxidizerType.setFill(oxidizerItem, oxidizerFill - consumed)
+		private fun checkCanisters(furnace: FurnaceInventory, fuelItem: ItemStack, fuelType: GasCanister, oxidizerItem: ItemStack, oxidizerType: GasCanister): Int? {
+			val fuelFill = fuelType.getFill(fuelItem)
+			val oxidizerFill = oxidizerType.getFill(oxidizerItem)
+
+			// Burn fuel and oxidizer at 1:1
+			// Cap consumption at 30 units
+			val consumed = minOf(ConfigurationFiles.globalGassesConfiguration().powerPlantConsumption, fuelFill, oxidizerFill)
+
+			// God forbid it goes negative
+			if (fuelFill <= 0) {
+				clearEmpty(furnace, fuelItem)
+				return null
+			}
+
+			if (oxidizerFill <= 0) {
+				clearEmpty(furnace, oxidizerItem)
+				return null
+			}
+
+			if (fuelFill - consumed <= 0) {
+				// Replaces with empty, no need to set fill to zero
+				if (clearEmpty(furnace, fuelItem)) return null
+			} else {
+				fuelType.setFill(fuelItem, fuelFill - consumed)
+			}
+
+			if (oxidizerFill - consumed <= 0) {
+				// Replaces with empty, no need to set fill to zero
+				if (clearEmpty(furnace, oxidizerItem)) return null
+			} else {
+				oxidizerType.setFill(oxidizerItem, oxidizerFill - consumed)
+			}
+
+			return consumed
 		}
 
-		return consumed
+		/** Returns whether the process should be aborted due to a problem **/
+		private fun clearEmpty(furnaceInventory: Inventory, itemStack: ItemStack): Boolean {
+			val discardChest = getInventory(0, 0, 6) ?: return true
+			if (!LegacyItemUtils.canFit(discardChest, EMPTY_CANISTER)) return true
+
+			furnaceInventory.remove(itemStack)
+			return false
+		}
 	}
-
-	/** Returns whether the process should be aborted due to a problem **/
-	private fun clearEmpty(sign: Sign, furnaceInventory: Inventory, itemStack: ItemStack): Boolean {
-		val discardChest = getStorage(sign, outputInventory) ?: return true
-		val noFit = discardChest.inventory.addItem(EMPTY_CANISTER).values.isNotEmpty()
-
-		if (noFit) return true
-
-		furnaceInventory.remove(itemStack)
-		return false
-	}
-
-	private fun getStorage(sign: Sign, offset: Vec3i): Container? {
-		val (x, y, z) = offset
-		val facing = sign.getFacing()
-		val right = facing.rightFace
-
-		val absoluteOffset = Vec3i(
-			x = (right.modX * x) + (facing.modX * z),
-			y = y,
-			z = (right.modZ * x) + (facing.modZ * z)
-		)
-
-		val absolute = absoluteOffset + Vec3i(sign.location)
-		val (absoluteX, absoluteY, absoluteZ) = absolute
-
-		return getStateIfLoaded(sign.world, absoluteX, absoluteY, absoluteZ) as? Container
-	}
-
-	private val outputInventory: Vec3i = Vec3i(0, 0, -7)
 }
