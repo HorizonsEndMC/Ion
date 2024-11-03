742fca6e791e5317cd0727a27f10d3d6996bc2ca
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 17:19:22 -0600

    Update & reregister new multiblocks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/event/AbyssalGazeSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/event/AbyssalGazeSubsystem.kt
@@ -6,7 +6,7 @@ import net.horizonsend.ion.server.features.starship.damager.Damager
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.CannonWeaponSubsystem
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.event.projectile.AbyssalGazeProjectile
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.interfaces.PermissionWeaponSubsystem
-import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.kyori.adventure.text.Component
 import org.bukkit.Location
 import org.bukkit.block.BlockFace
