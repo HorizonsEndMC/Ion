64724cb0f2881fd6bedd5d16ba8f3991ea47eeef
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 15:37:59 -0600

    migrate gas canister vent

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/old/Multiblocks.kt
@@ -3,11 +3,9 @@ package net.horizonsend.ion.server.features.multiblock.old
 import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.type.fluid.GasPowerPlantMultiblock
-import net.horizonsend.ion.server.features.multiblock.type.fluid.VentMultiblock
 
 object Multiblocks : IonServerComponent() {
 	private fun initMultiblocks() {
-		registerMultiblock(VentMultiblock)
 		registerMultiblock(GasPowerPlantMultiblock)
 	}
 
