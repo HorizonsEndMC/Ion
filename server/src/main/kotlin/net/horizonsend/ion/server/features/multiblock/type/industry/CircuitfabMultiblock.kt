303909949577b62c640f7fb71d4a58ea5427acb9
Gutin1 <Claytonpinter@gmail.com>
2024-11-03 20:36:29 -0600

    Add a try catch, fix initialization order, remove old powered multiblock interface

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CircuitfabMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CircuitfabMultiblock.kt
@@ -10,13 +10,13 @@ import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerSto
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
-import net.horizonsend.ion.server.features.multiblock.type.NewPoweredMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.PoweredMultiblock
 import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
 import org.bukkit.World
 import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
 
-object CircuitfabMultiblock : Multiblock(), NewPoweredMultiblock<CircuitfabMultiblock.CircuitfabMultiblockEntity> {
+object CircuitfabMultiblock : Multiblock(), PoweredMultiblock<CircuitfabMultiblock.CircuitfabMultiblockEntity> {
 	override val name = "circuitfab"
 
 	override val signText = createSignText(
