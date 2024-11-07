6428e27f67193ca16a80677dd1f195ad7eb908c2
Gutin1 <Claytonpinter@gmail.com>
2024-11-07 11:42:32 -0600

    half implemented replacement recipe system

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/recipe/take2/result/FluidResult.kt
@@ -0,0 +1,5 @@
+package net.horizonsend.ion.server.features.multiblock.crafting.recipe.take2.result
+
+import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
+
+class FluidResult(val fluid: PipedFluid, val amount: Int) : MultiblockRecipeResult()
