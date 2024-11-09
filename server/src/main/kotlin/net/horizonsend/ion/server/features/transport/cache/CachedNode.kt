3f2f02e27c93cdf4c539858349cf2dc156532dc5
Gutin1 <Claytonpinter@gmail.com>
2024-11-09 11:39:12 -0600

    Add back more nodes to new system

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/cache/CachedNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/cache/CachedNode.kt
@@ -9,6 +9,18 @@ interface CachedNode {
 
 	/**
 	 * Gets the directions that should be checked for the next nodes
+	 *
+	 * Returns a map of block face to transfer priority
 	 **/
-	fun getNextNodes(inputDirection: BlockFace): Set<BlockFace>
+	fun getNextNodes(inputDirection: BlockFace): Collection<Pair<BlockFace, Int>>
+
+	/**
+	 * Adds any restrictions on transferring to another node
+	 **/
+	fun canTransferTo(other: CachedNode, offset: BlockFace): Boolean
+
+	/**
+	 * Adds any restrictions on transferring from another node
+	 **/
+	fun canTransferFrom(other: CachedNode, offset: BlockFace): Boolean
 }
