bb830b1245ea9ba646547fe8bd89c11bf450fc63
Gutin1 <Claytonpinter@gmail.com>
2024-11-09 23:12:37 -0600

    Halfway through implementation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/cache/TransportCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/cache/TransportCache.kt
@@ -3,17 +3,22 @@ package net.horizonsend.ion.server.features.transport.cache
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap
 import net.horizonsend.ion.server.features.transport.cache.state.CacheState
 import net.horizonsend.ion.server.features.transport.node.manager.holders.NetworkHolder
+import net.horizonsend.ion.server.features.transport.node.util.NetworkType
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
-import net.horizonsend.ion.server.miscellaneous.utils.getBlockTypeSafe
-import org.bukkit.Material
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.getBlockIfLoaded
+import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 
 abstract class TransportCache(val holder: NetworkHolder<*> /* TODO temp network holder, works for now */) {
 	private val cache: Long2ObjectOpenHashMap<CacheState> = Long2ObjectOpenHashMap()
+
+	abstract val type: NetworkType
+	abstract val nodeFactory: NodeCacheFactory
 
 	fun isCached(at: BlockKey): Boolean = cache.containsKey(at)
 
@@ -25,27 +30,37 @@ abstract class TransportCache(val holder: NetworkHolder<*> /* TODO temp network 
 		}
 	}
 
-	fun cache(location: BlockKey, material: Material): CachedNode? {
-		val type = getNodeType(material)
+	fun getOrCache(location: BlockKey): CachedNode? {
+		if (isCached(location)) return getCached(location)
+			else return cache(location, getBlockIfLoaded(holder.getWorld(), getX(location), getY(location), getZ(location)) ?: return null)
+	}
+
+	fun cache(location: BlockKey) {
+		val world = holder.getWorld()
+		val block = getBlockIfLoaded(world, getX(location), getY(location), getZ(location)) ?: return
+		cache(location, block)
+	}
+
+	fun cache(location: BlockKey, block: Block): CachedNode? {
+		val type = nodeFactory.cache(block)
 		val state = if (type == null) CacheState.Empty else CacheState.Present(type)
 
 		cache[location] = state
 		return type
 	}
 
-	abstract fun getNodeType(material: Material): CachedNode?
-
 	fun getNextNodeLocations(location: BlockKey, from: BlockFace): List<Triple<BlockFace, BlockKey, Int>> {
-		val cachedAt = if (!isCached(location)) {
-			// If the requested node is not cached, cache the node at the location
-			val material = getBlockTypeSafe(holder.getWorld(), getX(location), getY(location), getZ(location)) ?: return listOf()
-			cache(location, material)
-		} else getCached(location) // If it is already cached, this function will handle the empty node case
-
-		if (cachedAt == null) return listOf()
-
+		val cachedAt = getOrCache(location) ?: return listOf()
 		return cachedAt.getNextNodes(from).map { (face, priority) -> Triple(face, getRelative(location, face), priority) }
 	}
 
+	fun invalidate(x: Int, y: Int, z: Int) {
+		return invalidate(toBlockKey(x, y, z))
+	}
 
+	fun invalidate(key: BlockKey) {
+
+	}
+
+	fun getRawCache() = cache
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/ShipTransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/node/manager/ShipTransportManager.kt
@@ -3,11 +3,13 @@ package net.horizonsend.ion.server.features.transport.node.manager
 import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
 import net.horizonsend.ion.server.features.transport.NewTransport
+import net.horizonsend.ion.server.features.transport.cache.FluidTransportCache
+import net.horizonsend.ion.server.features.transport.cache.PowerTransportCache
 import net.horizonsend.ion.server.features.transport.node.manager.holders.ShipNetworkHolder
 
 class ShipTransportManager(val starship: Starship) : TransportManager() {
-	override val powerNodeManager = ShipNetworkHolder(this) { PowerNodeManager(it) }
-	override val fluidNodeManager = ShipNetworkHolder(this) { FluidNodeManager(it) }
+	override val powerNodeManager = ShipNetworkHolder(this) { PowerTransportCache(it) }
+	override val fluidNodeManager = ShipNetworkHolder(this) { FluidTransportCache(it) }
 
 	init {
 		load()
