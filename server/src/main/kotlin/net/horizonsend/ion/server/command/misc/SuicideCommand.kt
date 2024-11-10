9c2c0f3a52b9b82ae1daa120bcc4868c2d09c5aa
kwazedilla <35514618+kwazedilla@users.noreply.github.com>
2024-11-10 09:10:02 -0800

    remove "unalive" command alias

--- server/src/main/kotlin/net/horizonsend/ion/server/command/misc/SuicideCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/misc/SuicideCommand.kt
@@ -10,7 +10,7 @@ import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
 
-@CommandAlias("suicide|unalive")
+@CommandAlias("suicide")
 object SuicideCommand : SLCommand() {
     @Default
     @Suppress("unused")
