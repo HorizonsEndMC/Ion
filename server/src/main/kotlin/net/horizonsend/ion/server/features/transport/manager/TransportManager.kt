51d16e37f92139299a82a11bf17c255d90d28c43
Gutin1 <Claytonpinter@gmail.com>
2024-11-11 12:46:54 -0600

    Add input manager

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/TransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/TransportManager.kt
@@ -1,14 +1,17 @@
 package net.horizonsend.ion.server.features.transport.manager
 
-import net.horizonsend.ion.server.features.transport.nodes.cache.FluidTransportCache
-import net.horizonsend.ion.server.features.transport.nodes.cache.PowerTransportCache
 import net.horizonsend.ion.server.features.transport.manager.extractors.ExtractorManager
 import net.horizonsend.ion.server.features.transport.manager.holders.NetworkHolder
+import net.horizonsend.ion.server.features.transport.nodes.cache.FluidTransportCache
+import net.horizonsend.ion.server.features.transport.nodes.cache.PowerTransportCache
+import net.horizonsend.ion.server.features.transport.nodes.inputs.InputManager
 
 abstract class TransportManager {
 	abstract val extractorManager: ExtractorManager
 	abstract val powerNodeManager: NetworkHolder<PowerTransportCache>
 	abstract val fluidNodeManager: NetworkHolder<FluidTransportCache>
+
+	abstract fun getInputProvider(): InputManager
 
 	fun tick() {
 
