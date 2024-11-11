4739162f1ae1875216fe608e001c2def70d23baf
Gutin1 <Claytonpinter@gmail.com>
2024-11-10 23:51:12 -0600

    Register extractors in the new system

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ExtractorManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ExtractorManager.kt
@@ -4,12 +4,19 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import org.bukkit.Material
 
 abstract class ExtractorManager {
 	abstract fun getExtractors(): List<BlockKey>
+
 	fun registerExtractor(key: BlockKey, ensureExtractor: Boolean): Boolean = registerExtractor(getX(key), getY(key), getZ(key), ensureExtractor)
 	abstract fun registerExtractor(x: Int, y: Int, z: Int, ensureExtractor: Boolean): Boolean
+
+	abstract fun removeExtractor(key: BlockKey): Boolean
+	abstract fun removeExtractor(x: Int, y: Int, z: Int): Boolean
+
+	fun isExtractor(x: Int, y: Int, z: Int): Boolean = isExtractor(toBlockKey(x, y, z))
 	abstract fun isExtractor(key: BlockKey): Boolean
 
 	companion object {
