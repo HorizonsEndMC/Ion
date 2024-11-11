0be6409670e3bdaa2d8d0a4918a218b95e08a9a1
Gutin1 <Claytonpinter@gmail.com>
2024-11-11 13:32:21 -0600

    Add input locations to multiblock classes, register / release them

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/power/PoweredMultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/power/PoweredMultiblockEntity.kt
@@ -1,12 +1,21 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type.power
 
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import org.bukkit.persistence.PersistentDataType
 
 interface PoweredMultiblockEntity {
 	val powerStorage: PowerStorage
 	val maxPower: Int
+
+	val powerInputOffsets: Array<Vec3i>
+
+	fun getPowerInputLocations(): Set<Vec3i> {
+		this as MultiblockEntity
+		return powerInputOffsets.mapTo(mutableSetOf()) { (right, up, forward) -> getPosRelative(right, up, forward) }
+	}
 
 	fun loadStoredPower(data: PersistentMultiblockData): PowerStorage {
 		return PowerStorage(this, data.getAdditionalDataOrDefault(NamespacedKeys.POWER, PersistentDataType.INTEGER, 0), maxPower)
