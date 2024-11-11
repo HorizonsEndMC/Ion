9dd014ec60300ad3fb61f78d1dbdd99ca73f8447
Gutin1 <Claytonpinter@gmail.com>
2024-11-11 12:46:54 -0600

    Add input manager

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/ShipTransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/ShipTransportManager.kt
@@ -3,13 +3,16 @@ package net.horizonsend.ion.server.features.transport.manager
 import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
 import net.horizonsend.ion.server.features.transport.NewTransport
-import net.horizonsend.ion.server.features.transport.nodes.cache.FluidTransportCache
-import net.horizonsend.ion.server.features.transport.nodes.cache.PowerTransportCache
 import net.horizonsend.ion.server.features.transport.manager.extractors.ShipExtractorManager
 import net.horizonsend.ion.server.features.transport.manager.holders.ShipNetworkHolder
+import net.horizonsend.ion.server.features.transport.nodes.cache.FluidTransportCache
+import net.horizonsend.ion.server.features.transport.nodes.cache.PowerTransportCache
+import net.horizonsend.ion.server.features.transport.nodes.inputs.InputManager
+import net.horizonsend.ion.server.features.transport.nodes.inputs.ShipInputManager
 
 class ShipTransportManager(val starship: Starship) : TransportManager() {
 	override val extractorManager: ShipExtractorManager = ShipExtractorManager(starship)
+	private val inputManager = ShipInputManager(this)
 
 	override val powerNodeManager = ShipNetworkHolder(this) { PowerTransportCache(it) }
 	override val fluidNodeManager = ShipNetworkHolder(this) { FluidTransportCache(it) }
@@ -30,5 +33,9 @@ class ShipTransportManager(val starship: Starship) : TransportManager() {
 
 	fun displace(movement: StarshipMovement) {
 
+	}
+
+	override fun getInputProvider(): InputManager {
+		return inputManager
 	}
 }
