3263210331f3c0e0c5c26dbed51e2d7017f682fe
Gutin1 <Claytonpinter@gmail.com>
2024-11-11 13:32:21 -0600

    Add input locations to multiblock classes, register / release them

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/storage/FluidStorageMultiblock.kt
@@ -5,12 +5,13 @@ import net.horizonsend.ion.server.features.client.display.modular.DisplayHandler
 import net.horizonsend.ion.server.features.client.display.modular.display.fluid.SplitFluidDisplay
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.DisplayMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.BasicFluidStoringEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage.UnlimitedInternalStorage
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
-import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
+import net.horizonsend.ion.server.features.transport.util.NetworkType
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
@@ -43,27 +44,25 @@ abstract class FluidStorageMultiblock(val capacity: Int) : Multiblock(), EntityM
 		z: Int,
 		world: World,
 		structureDirection: BlockFace,
-	) : BasicFluidStoringEntity(manager, multiblock, data, x, y, z, world, structureDirection, UnlimitedInternalStorage(multiblock.capacity)), FluidStoringEntity {
+	) : BasicFluidStoringEntity(manager, multiblock, data, x, y, z, world, structureDirection, UnlimitedInternalStorage(multiblock.capacity)), FluidStoringEntity, DisplayMultiblockEntity {
 		override val fluidInputOffset: Vec3i = Vec3i(0, -1, 0)
-		private val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+		override val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
 			this,
 			SplitFluidDisplay(mainStorage, +0.0, -0.0, +0.0, 0.45f),
 		).register()
 
+		override val fluidInputOffsets: Array<Vec3i> = arrayOf(Vec3i(0, -1, 0))
+
 		override fun onLoad() {
-			displayHandler.update()
+			registerInputs(NetworkType.FLUID, getFluidInputLocations())
 		}
 
 		override fun onUnload() {
-			displayHandler.remove()
+			releaseInputs(NetworkType.FLUID, getFluidInputLocations())
 		}
 
 		override fun handleRemoval() {
-			displayHandler.remove()
-		}
-
-		override fun displaceAdditional(movement: StarshipMovement) {
-			displayHandler.displace(movement)
+			releaseInputs(NetworkType.FLUID, getFluidInputLocations())
 		}
 
 		override fun storeAdditionalData(store: PersistentMultiblockData, adapterContext: PersistentDataAdapterContext) {
