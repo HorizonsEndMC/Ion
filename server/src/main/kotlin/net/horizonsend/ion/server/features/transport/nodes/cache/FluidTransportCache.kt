d8481153d51068fed9521617f02527121fe9f9df
Gutin1 <Claytonpinter@gmail.com>
2024-11-12 14:13:19 -0600

    tick extractors

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/FluidTransportCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/FluidTransportCache.kt
@@ -2,9 +2,14 @@ package net.horizonsend.ion.server.features.transport.nodes.cache
 
 import net.horizonsend.ion.server.features.transport.manager.holders.NetworkHolder
 import net.horizonsend.ion.server.features.transport.util.NetworkType
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 
 class FluidTransportCache(holder: NetworkHolder<FluidTransportCache>): TransportCache(holder) {
 	override val type: NetworkType = NetworkType.FLUID
 	override val nodeFactory: NodeCacheFactory = NodeCacheFactory.builder()
 		.build()
+
+	override fun tickExtractor(location: BlockKey) {
+
+	}
 }
