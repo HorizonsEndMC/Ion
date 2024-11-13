acf54954bb31a01d67a5d05b1c2684a910984804
Gutin1 <Claytonpinter@gmail.com>
2024-11-13 12:23:24 -0600

    move files around, rename stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/CacheState.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/CacheState.kt
@@ -1,6 +1,8 @@
 package net.horizonsend.ion.server.features.transport.nodes.cache
 
+import net.horizonsend.ion.server.features.transport.nodes.types.Node
+
 sealed interface CacheState {
 	data object Empty: CacheState
-	data class Present(val node: CachedNode): CacheState
+	data class Present(val node: Node): CacheState
 }
