1d63fd9cd8c8eeb4bac5abea9018c12019076b48
Gutin1 <Claytonpinter@gmail.com>
2024-11-13 12:07:30 -0600

    Give extractors metadata, reimplement transfer delta

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/TransportConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/TransportConfiguration.kt
@@ -4,6 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TransportConfiguration(
-	val extractorTickIntervalMS: Long = 2000,
-	val maxPowerRemovedPerExtractorTick: Int = 1000
-)
+	val extractorConfiguration: ExtractorConfiguration = ExtractorConfiguration()
+) {
+	@Serializable
+	data class ExtractorConfiguration(
+		val extractorTickIntervalMS: Long = 2000,
+		val maxPowerRemovedPerExtractorTick: Int = 1000
+	)
+}
