d9e8f57c58ca77657fb818f14389cde955949185
Gutin1 <Claytonpinter@gmail.com>
2024-11-13 12:23:24 -0600

    move files around, rename stuff

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/TransportManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/TransportManager.kt
@@ -1,23 +1,23 @@
 package net.horizonsend.ion.server.features.transport.manager
 
 import net.horizonsend.ion.server.features.transport.manager.extractors.ExtractorManager
-import net.horizonsend.ion.server.features.transport.manager.holders.NetworkHolder
+import net.horizonsend.ion.server.features.transport.manager.holders.CacheHolder
 import net.horizonsend.ion.server.features.transport.nodes.cache.FluidTransportCache
 import net.horizonsend.ion.server.features.transport.nodes.cache.PowerTransportCache
 import net.horizonsend.ion.server.features.transport.nodes.inputs.InputManager
 
 abstract class TransportManager {
 	abstract val extractorManager: ExtractorManager
-	abstract val powerNodeManager: NetworkHolder<PowerTransportCache>
-	abstract val fluidNodeManager: NetworkHolder<FluidTransportCache>
+	abstract val powerNodeManager: CacheHolder<PowerTransportCache>
+	abstract val fluidNodeManager: CacheHolder<FluidTransportCache>
 
 	abstract fun getInputProvider(): InputManager
 
 	fun tick() {
 		for (extractor in extractorManager.getExtractors()) {
 			val delta = extractor.markTicked()
-			powerNodeManager.network.tickExtractor(extractor.pos, delta)
-			fluidNodeManager.network.tickExtractor(extractor.pos, delta)
+			powerNodeManager.cache.tickExtractor(extractor.pos, delta)
+			fluidNodeManager.cache.tickExtractor(extractor.pos, delta)
 		}
 	}
 }
