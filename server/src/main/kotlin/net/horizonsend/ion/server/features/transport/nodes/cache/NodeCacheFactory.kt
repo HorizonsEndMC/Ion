e3a5c84412bfbec3a48849594548e38f94108867
Gutin1 <Claytonpinter@gmail.com>
2024-11-13 11:04:46 -0600

    Provide block key to cache

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/NodeCacheFactory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/NodeCacheFactory.kt
@@ -1,5 +1,7 @@
 package net.horizonsend.ion.server.features.transport.nodes.cache
 
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.getBlockDataSafe
 import net.horizonsend.ion.server.miscellaneous.utils.getTypeSafe
 import org.bukkit.Material
@@ -14,19 +16,19 @@ class NodeCacheFactory private constructor(private val materialHandlers: Map<Mat
 		val blockData = getBlockDataSafe(block.world, block.x, block.y, block.z) ?: return null
 
 		if (!materialFactory.blockDataClass.isInstance(blockData)) return null
-		return materialFactory.construct(blockData)
+		return materialFactory.construct(blockData, toBlockKey(block.x, block.y, block.z))
 	}
 
 	class Builder	{
 		val materialHandlers = mutableMapOf<Material, MaterialHandler<*>>()
 
-		inline fun <reified T: BlockData> addDataHandler(material: Material, noinline constructor: (T) -> CachedNode): Builder {
+		inline fun <reified T: BlockData> addDataHandler(material: Material, noinline constructor: (T, BlockKey) -> CachedNode): Builder {
 			this.materialHandlers[material] = MaterialHandler(T::class, constructor)
 			return this
 		}
 
 		fun addSimpleNode(material: Material, node: CachedNode): Builder {
-			this.materialHandlers[material] = MaterialHandler(BlockData::class) { node }
+			this.materialHandlers[material] = MaterialHandler(BlockData::class) { _, _ -> node }
 			return this
 		}
 
@@ -35,10 +37,10 @@ class NodeCacheFactory private constructor(private val materialHandlers: Map<Mat
 		}
 	}
 
-	class MaterialHandler<T: BlockData>(val blockDataClass: KClass<T>, val constructor: (T) -> CachedNode) {
-		fun construct(blockData: BlockData): CachedNode {
+	class MaterialHandler<T: BlockData>(val blockDataClass: KClass<T>, val constructor: (T, BlockKey) -> CachedNode) {
+		fun construct(blockData: BlockData, key: BlockKey): CachedNode {
 			@Suppress("UNCHECKED_CAST")
-			return constructor.invoke(blockData as T)
+			return constructor.invoke(blockData as T, key)
 		}
 	}
 
