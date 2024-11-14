6d24156480cfa7d374cdf4110d92b9565e9ed769
Gutin1 <Claytonpinter@gmail.com>
2024-11-13 18:38:58 -0600

    fix + util

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/FluidTransportCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/FluidTransportCache.kt
@@ -1,17 +1,21 @@
 package net.horizonsend.ion.server.features.transport.nodes.cache
 
+import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
 import net.horizonsend.ion.server.features.transport.manager.holders.CacheHolder
 import net.horizonsend.ion.server.features.transport.nodes.types.FluidNode
 import net.horizonsend.ion.server.features.transport.util.CacheType
+import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.UNWAXED_CHISELED_COPPER_TYPES
 import net.horizonsend.ion.server.miscellaneous.utils.axis
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import org.bukkit.Material
 import org.bukkit.Material.CRAFTING_TABLE
 import org.bukkit.Material.WAXED_CHISELED_COPPER
 import org.bukkit.Material.WAXED_EXPOSED_CHISELED_COPPER
 import org.bukkit.Material.WAXED_OXIDIZED_COPPER
 import org.bukkit.Material.WAXED_WEATHERED_CHISELED_COPPER
+import org.bukkit.World
 import org.bukkit.craftbukkit.v1_20_R3.block.impl.CraftLightningRod
 
 class FluidTransportCache(holder: CacheHolder<FluidTransportCache>): TransportCache(holder) {
@@ -31,6 +35,23 @@ class FluidTransportCache(holder: CacheHolder<FluidTransportCache>): TransportCa
 		.build()
 
 	override fun tickExtractor(location: BlockKey, delta: Double) {
+		println("Delta: $delta")
+	}
 
+	private fun getPowerExtractorSourcePool(extractorLocation: BlockKey, world: World): List<FluidStoringEntity> {
+		val sources = mutableListOf<FluidStoringEntity>()
+
+		for (face in ADJACENT_BLOCK_FACES) {
+			val inputLocation = getRelative(extractorLocation, face)
+			if (holder.getOrCacheGlobalNode(inputLocation) !is FluidNode.FluidInputNode) continue
+			val entities = getInputEntities(world, inputLocation)
+
+			for (entity in entities) {
+				if (entity !is FluidStoringEntity) continue
+				sources.add(entity)
+			}
+		}
+
+		return sources
 	}
 }
