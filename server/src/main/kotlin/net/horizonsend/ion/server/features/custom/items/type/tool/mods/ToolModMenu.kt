d051e520e0ee8a1a21b226638f70b42c99988dfe
Gutin1 <Claytonpinter@gmail.com>
2024-11-15 11:04:55 -0600

    Abstract out tool mod menu

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/tool/mods/ToolModMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/tool/mods/ToolModMenu.kt
@@ -6,46 +6,32 @@ import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.customItem
 import net.horizonsend.ion.server.features.custom.items.component.ModManager
+import net.horizonsend.ion.server.features.gui.interactable.InteractableGUI
 import net.horizonsend.ion.server.features.nations.gui.playerClicker
 import net.horizonsend.ion.server.listener.SLEventListener
-import org.bukkit.Material.AIR
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
-import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
-import org.bukkit.event.inventory.InventoryDragEvent
 import org.bukkit.event.inventory.InventoryInteractEvent
-import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.inventory.Inventory
-import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.InventoryView
 import org.bukkit.inventory.ItemStack
-import java.util.UUID
 import kotlin.math.ceil
 
 class ToolModMenu(
-	private val viewer: Player,
+	viewer: Player,
 	private val itemStack: ItemStack,
 	private val customItem: CustomItem,
 	private val modManager: ModManager
-) : InventoryHolder {
-	private val inventorySize = (ceil((modManager.getMods(itemStack).size + 1).toDouble() / 9.0) * 9).toInt()
-	private val internalInventory = IonServer.server.createInventory(this, inventorySize)
+) : InteractableGUI(viewer) {
+	override val inventorySize = (ceil((modManager.getMods(itemStack).size + 1).toDouble() / 9.0) * 9).toInt()
+	override val internalInventory: Inventory = IonServer.server.createInventory(this, inventorySize)
 
 	override fun getInventory(): Inventory = internalInventory
 
-	fun open() {
-		// Will return CRAFTING if none is open
-		if (viewer.openInventory.type != InventoryType.CRAFTING && viewer.openInventory.type != InventoryType.CREATIVE) return
-
-		val view = viewer.openInventory(internalInventory) ?: return
-
-		setup(view)
-	}
-
-	private fun setup(view: InventoryView) {
+	override fun setup(view: InventoryView) {
 		view.title = "Tool Modifications"
 
 		populateMods()
@@ -76,7 +62,7 @@ class ToolModMenu(
 
 		val mods = contents
 			.mapNotNull { it?.customItem }
-			.filterIsInstance<net.horizonsend.ion.server.features.custom.items.type.tool.mods.ModificationItem>()
+			.filterIsInstance<ModificationItem>()
 			.mapTo(mutableSetOf()) { it.modification }
 			.plus(nonItemMods)
 			.toTypedArray()
@@ -99,73 +85,11 @@ class ToolModMenu(
 	/**
 	 * Fired when the inventory is closed
 	 **/
-	fun handleClose(event: InventoryCloseEvent) {
+	override fun handleClose(event: InventoryCloseEvent) {
 		rebuildFromContents()
 	}
 
-	/**
-	 * Events from the player's clicked inventory while the mod menu is on top
-	 **/
-	fun handlePlayerClick(event: InventoryClickEvent) {
-		val currentItem = event.currentItem
-		val cursorItem = event.cursor
-		val index = event.slot
-
-		if (event.click != ClickType.SHIFT_LEFT && event.click != ClickType.SHIFT_RIGHT) return
-
-		when {
-			currentItem != null && cursorItem.type == AIR -> handleAddItem(index, currentItem, event)
-			currentItem == null && cursorItem.type != AIR -> handleRemoveItem(index, event)
-			else -> event.isCancelled = true
-		}
-	}
-
-	/**
-	 * Events from the player's clicked mod menu
-	 **/
-	fun handleClick(event: InventoryClickEvent) {
-		val currentItem = event.currentItem
-		val cursorItem = event.cursor
-		val index = event.slot
-
-		if (event.isCancelled) return
-
-		when {
-			event.click == ClickType.NUMBER_KEY -> handleNumberKey(index, currentItem, event)
-			currentItem == null && cursorItem.type != AIR -> handleAddItem(index, cursorItem, event)
-			currentItem != null && cursorItem.type == AIR -> handleRemoveItem(index, event)
-			currentItem != null && cursorItem.type != AIR -> handleSwapItem(index, currentItem, cursorItem, event)
-			else -> event.isCancelled = true
-		}
-	}
-
-	fun handleDrag(event: InventoryDragEvent) {
-		// If dragging something outside the inventory
-		if (event.rawSlots.none { it < inventorySize }) return
-
-		when {
-			// Unstackable item can't be split across multiple slots
-			event.cursor == null && event.oldCursor.maxStackSize == 1 -> {
-				// Will only end up in a single slot
-				val slot = event.rawSlots.first()
-				handleAddItem(slot, event.newItems[slot]!!, event)
-			}
-			else -> event.isCancelled = true
-		}
-	}
-
-	private fun handleNumberKey(slotNumber: Int, currentItem: ItemStack?, event: InventoryClickEvent) {
-		val playerItem = viewer.inventory.getItem(event.hotbarButton) ?: ItemStack(AIR)
-
-		when {
-			currentItem == null && playerItem.type != AIR -> handleAddItem(slotNumber, playerItem, event)
-			currentItem != null && playerItem.type == AIR -> handleRemoveItem(slotNumber, event)
-			currentItem != null && playerItem.type != AIR -> handleSwapItem(slotNumber, currentItem, playerItem, event)
-			else -> event.isCancelled = true
-		}
-	}
-
-	private fun handleAddItem(slot: Int, item: ItemStack, event: InventoryInteractEvent) {
+	override fun handleAddItem(slot: Int, item: ItemStack, event: InventoryInteractEvent) {
 		if (!canAdd(item, event.whoClicked as Player)) {
 			event.isCancelled = true
 			return
@@ -174,12 +98,12 @@ class ToolModMenu(
 		rebuildFromContents(listOf(*internalInventory.contents, item))
 	}
 
-	private fun handleRemoveItem(slot: Int, event: InventoryClickEvent) {
+	override fun handleRemoveItem(slot: Int, event: InventoryClickEvent) {
 		// Rebuild with the item removed
 		rebuildFromContents(internalInventory.contents.subtract(setOf(internalInventory.contents[slot])))
 	}
 
-	private fun handleSwapItem(slot: Int, currentItem: ItemStack, new: ItemStack, event: InventoryClickEvent) {
+	override fun handleSwapItem(slot: Int, currentItem: ItemStack, new: ItemStack, event: InventoryClickEvent) {
 		if (!canAdd(new, event.playerClicker)) {
 			event.isCancelled = true
 			return
@@ -227,55 +151,12 @@ class ToolModMenu(
 	}
 
 	companion object : SLEventListener() {
-		private val inventories = mutableMapOf<UUID, net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu>()
-
-		fun create(viewer: Player, itemStack: ItemStack, customItem: CustomItem, manager: ModManager): net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu {
-			val holder = net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu(viewer, itemStack, customItem, manager)
-			net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu.Companion.inventories[viewer.uniqueId] = holder
+		fun create(viewer: Player, itemStack: ItemStack, customItem: CustomItem, manager: ModManager): ToolModMenu {
+			val holder = ToolModMenu(viewer, itemStack, customItem, manager)
+			setInventory(viewer.uniqueId, holder)
 
 			return holder
-		}
 
-		@EventHandler
-		fun onInventoryClick(event: InventoryClickEvent) {
-			val inventory = event.clickedInventory ?: return
-
-			val holder = inventory.holder
-
-			if (holder is net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu) {
-				holder.handleClick(event)
-			}
-
-			if (holder is Player) {
-				val topHolder = holder.openInventory.topInventory.holder
-
-				if (topHolder is net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu) {
-					topHolder.handlePlayerClick(event)
-				}
-
-				return
-			}
-		}
-
-		@EventHandler
-		fun onInventoryDrag(event: InventoryDragEvent) {
-			val inventory = event.inventory
-
-			val holder = inventory.holder
-
-			if (holder is net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu) {
-				holder.handleDrag(event)
-			}
-		}
-
-		@EventHandler
-		fun onInventoryClose(event: InventoryCloseEvent) {
-			val inventory = event.inventory
-			val holder = inventory.holder
-
-			if (holder is net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu) {
-				holder.handleClose(event)
-			}
 		}
 
 		@EventHandler
@@ -283,7 +164,7 @@ class ToolModMenu(
 			val player = event.player
 			val holder = player.openInventory.topInventory.holder
 
-			if (holder is net.horizonsend.ion.server.features.custom.items.type.tool.mods.ToolModMenu) {
+			if (ToolModMenu) {
 				// Assume they dropped the item
 
 				if (event.itemDrop.itemStack.itemMeta == holder.itemStack.itemMeta) {
