8a97b21f9959af9fb1756e441b64fda5bb7ba363
Gutin1 <Claytonpinter@gmail.com>
2024-11-15 10:48:57 -0600

    Add multiblock workbench

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/CustomBlocks.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/CustomBlocks.kt
@@ -197,6 +197,8 @@ object CustomBlocks {
 		)
 	))
 
+	val MULTIBLOCK_WORKBENCH = register(MultiblockWorkbench)
+
     private fun customItemDrop(identifier: String, amount: Int = 1): Supplier<Collection<ItemStack>> {
         val customItem = CustomItemRegistry.getByIdentifier(identifier)?.constructItemStack() ?: return Supplier { listOf() }
         customItem.amount = amount
