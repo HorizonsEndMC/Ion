6616c5bdd79731c87b7071abf28770d11ee83868
Gutin1 <Claytonpinter@gmail.com>
2024-11-16 12:50:11 -0600

    improve item consumption, packaging

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/MultiblockWorkbench.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/MultiblockWorkbench.kt
@@ -1,32 +1,36 @@
 package net.horizonsend.ion.server.features.custom.blocks
 
-import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks.customItemDrop
 import net.horizonsend.ion.server.features.custom.items.CustomItems
+import net.horizonsend.ion.server.features.custom.items.misc.PackagedMultiblock
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.features.gui.interactable.InteractableGUI
 import net.horizonsend.ion.server.features.multiblock.MultiblockRegistration
 import net.horizonsend.ion.server.features.multiblock.shape.BlockRequirement
 import net.horizonsend.ion.server.features.multiblock.type.DisplayNameMultilblock.Companion.getDisplayName
+import net.horizonsend.ion.server.miscellaneous.utils.LegacyItemUtils
 import net.horizonsend.ion.server.miscellaneous.utils.PerPlayerCooldown
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.text.itemName
 import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.Component.empty
 import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.GREEN
 import net.kyori.adventure.text.format.NamedTextColor.RED
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
+import org.bukkit.block.Chest
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryView
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BlockStateMeta
 import java.util.concurrent.TimeUnit
 
 object MultiblockWorkbench : InteractableCustomBlock(
@@ -80,10 +84,15 @@ object MultiblockWorkbench : InteractableCustomBlock(
 				it.setCustomModelData(GuiItem.RIGHT.customModelData)
 				it.displayName(text("Missing Materials!", RED).itemName)
 			}) {
-				updateMultiblock(it.view)
+				tryPack()
+				updateConfirmationButton()
 			}
 
-			view.setTitle(getGuiText(empty()))
+			lockedSlots.addAll(setOf(0..11, 27..29).flatten())
+			lockedSlots.add(18)
+			lockedSlots.add(20)
+
+			view.setTitle(getGuiText(currentMultiblock.getDisplayName()))
 		}
 
 		private fun getGuiText(secondLine: Component): Component = GuiText("Multiblock Workbench")
@@ -107,68 +116,102 @@ object MultiblockWorkbench : InteractableCustomBlock(
 
 		private val editSlots = setOf(12..17, 21..26, 30..35).flatten()
 
-		private fun checkRequirements(): List<BlockRequirement> {
-			val items = internalInventory.contents.withIndex().filter { editSlots.contains(it.index) }.mapNotNull { it.value }
-			return consumeItems(items.mapTo(mutableListOf()) { it.clone() })
+		private fun getConsumableItems(): List<ItemStack> {
+			return internalInventory.contents.withIndex()
+				.filter { editSlots.contains(it.index) }
+				.mapNotNull { it.value }
 		}
 
-		private fun consumeItems(items: MutableList<ItemStack>): List<BlockRequirement> {
+		private fun checkRequirements(): List<BlockRequirement> {
+			val items = getConsumableItems().mapTo(mutableListOf()) { it.clone() }
+
 			val itemRequirements = currentMultiblock.shape.getRequirementMap(BlockFace.NORTH).map { it.value }
 			val missing = mutableListOf<BlockRequirement>()
 
 			for (blockRequirement in itemRequirements) {
 				val requirement = blockRequirement.itemRequirement
-				if (items.any { requirement.itemCheck(it) && requirement.consumeItem(it) }) continue
+				if (items.any { requirement.itemCheck(it) && requirement.consume(it) }) continue
 				missing.add(blockRequirement)
 			}
 
 			return missing
 		}
 
-		private fun updateConfirmationButton() {
-			val item = inventory.contents[28] ?: return
+		fun packageTo(destination: Inventory) {
+			val items = getConsumableItems()
+			val itemRequirements = currentMultiblock.shape.getRequirementMap(BlockFace.NORTH).map { it.value }
+			val missing = mutableListOf<BlockRequirement>()
+
+			for (blockRequirement in itemRequirements) {
+				val requirement = blockRequirement.itemRequirement
+				val success = items.firstOrNull { requirement.itemCheck(it) && requirement.consume(it) }
+
+				if (success == null) {
+					missing.add(blockRequirement)
+					continue
+				}
+
+				val amount = requirement.amountConsumed(success)
+				LegacyItemUtils.addToInventory(destination, success.asQuantity(amount))
+			}
+		}
+
+		private fun createPackagedItem(): ItemStack {
+			val base = PackagedMultiblock.createFor(currentMultiblock)
+			return base.updateMeta {
+				it as BlockStateMeta
+
+				val newState = Material.CHEST.createBlockData().createBlockState() as Chest
+				packageTo(newState.inventory)
+
+				it.blockState = newState
+			}
+		}
+
+		private var ready: Boolean = false
+
+		private fun updateConfirmationButton() = Tasks.sync {
+			val item = inventory.contents[28] ?: return@sync
 			val missing = checkRequirements()
 
 			if (missing.isNotEmpty()) {
 				item.type = Material.BARRIER
-
-				val missingNames = missing.groupBy { it.alias }.map { text("${it.key.replaceFirstChar { char -> char.uppercase() }}: ${it.value.size}").itemName }
-
 				item.updateMeta {
 					it.displayName(text("Missing Materials!", RED).itemName)
-					it.lore(missingNames)
+					it.lore(missing.groupBy { it.alias }.map { text("${it.key.replaceFirstChar { char -> char.uppercase() }}: ${it.value.size}").itemName })
 				}
-				return
+
+				ready = false
+				return@sync
 			}
 
 			item.type = Material.WARPED_FUNGUS_ON_A_STICK
 			item.updateMeta {
-				it.displayName(ofChildren(text("Package "), currentMultiblock.getDisplayName()).itemName)
-				it.setCustomModelData(GuiItem.UP.customModelData)
+				it.lore(listOf())
+				it.displayName(text("Packaged multiblock ready!", GREEN).itemName)
+				it.setCustomModelData(114)
 			}
+
+			ready = true
 		}
 
-		private fun isLockedSlot(slot: Int): Boolean {
-			if (slot in 0..11) return true
-			if (slot in 18..20) return true
-			return slot in 27..29
-		}
+		private fun tryPack() {
+			if (!ready) return
+			val packagedItem = createPackagedItem()
 
-		override fun getInventory(): Inventory {
-			return internalInventory
+			if (internalInventory.getItem(19)?.isSimilar(packagedItem) == true) { internalInventory.getItem(19)?.let { it.amount++ } } else {
+				internalInventory.setItem(19, packagedItem)
+			}
+
+			updateConfirmationButton()
 		}
 
 		override fun itemChanged(changedSlot: Int, changedItem: ItemStack) {
 			updateConfirmationButton()
 		}
 
-		override fun canRemove(slot: Int, player: Player): Boolean {
-			return !isLockedSlot(slot)
-		}
-
-		override fun canAdd(itemStack: ItemStack, slot: Int, player: Player): Boolean {
-			return !isLockedSlot(slot)
-		}
+		override fun canRemove(slot: Int, player: Player): Boolean { return true }
+		override fun canAdd(itemStack: ItemStack, slot: Int, player: Player): Boolean { return true }
 
 		override fun handleClose(event: InventoryCloseEvent) {
 			for ((slot, content) in inventory.contents.withIndex()) {
