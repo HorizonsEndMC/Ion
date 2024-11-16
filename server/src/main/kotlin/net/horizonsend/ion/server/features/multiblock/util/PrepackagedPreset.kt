0be73963189b8cde83c6f54bbc4e459841ef224b
Gutin1 <Claytonpinter@gmail.com>
2024-11-16 14:33:28 -0600

    Move placement modifications to their own function

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/util/PrepackagedPreset.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/util/PrepackagedPreset.kt
@@ -3,28 +3,28 @@ package net.horizonsend.ion.server.features.multiblock.util
 import net.horizonsend.ion.server.features.multiblock.shape.BlockRequirement
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
 import org.bukkit.Material
+import org.bukkit.block.BlockFace
 import org.bukkit.block.data.Bisected
+import org.bukkit.block.data.BlockData
 import org.bukkit.block.data.type.Fence
 import org.bukkit.block.data.type.Stairs
 
 object PrepackagedPreset {
 	fun stairs(face: RelativeFace, half: Bisected.Half, shape: Stairs.Shape): BlockRequirement.() -> Unit = {
-		example = { multiblockDirection ->
-			Material.STONE_BRICK_STAIRS.createBlockData { stairs ->
-				stairs as Stairs
-				stairs.half = half
-				stairs.shape = shape
-				stairs.facing = face[multiblockDirection]
-			}
+		example = Material.STONE_BRICK_STAIRS.createBlockData()
+		addPlacementModification { multiblockDirection: BlockFace, stairs: BlockData ->
+			stairs as Stairs
+			stairs.half = half
+			stairs.shape = shape
+			stairs.facing = face[multiblockDirection]
 		}
 	}
 
 	fun pane(vararg face: RelativeFace): BlockRequirement.() -> Unit = {
-		example = { multiblockDirection ->
-			Material.GLASS_PANE.createBlockData { pane ->
-				pane as Fence
-				face.forEach { pane.setFace(it[multiblockDirection], true) }
-			}
+		example = Material.GLASS_PANE.createBlockData()
+		addPlacementModification { multiblockDirection: BlockFace, pane: BlockData ->
+			pane as Fence
+			face.forEach { pane.setFace(it[multiblockDirection], true) }
 		}
 	}
 }
