09df20c79b63105d736cb24049d5f1b87d093e3d
Gutin1 <Claytonpinter@gmail.com>
2024-11-19 01:09:31 -0600

    Fluid work

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/fluids/storage/CategoryRestrictedInternalStorage.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/fluids/storage/CategoryRestrictedInternalStorage.kt
@@ -1,25 +1,29 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage
 
-import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
+import net.horizonsend.ion.server.features.transport.fluids.Fluid
+import net.horizonsend.ion.server.features.transport.fluids.FluidRegistry.EMPTY
+import net.horizonsend.ion.server.features.transport.fluids.FluidStack
 import net.horizonsend.ion.server.features.transport.fluids.properties.FluidCategory
 
 class CategoryRestrictedInternalStorage(
 	private val storageCapacity: Int,
 	override val inputAllowed: Boolean,
+	override val extractionAllowed: Boolean,
 	private vararg val allowedCategories: FluidCategory,
 ) : InternalStorage() {
 	override fun getCapacity(): Int = storageCapacity
 
-	override fun canStore(resource: PipedFluid, liters: Int): Boolean {
-		if (liters + getAmount() > getCapacity()) {
-			return false
-		}
+	override fun canStore(fluid: FluidStack): Boolean {
+		if (!canStore(fluid.type)) return false
+		return fluid.amount + getAmount() <= getCapacity()
+	}
 
-		// Check that the fluid attempting to be stored is the same as the one currently stored
-		if (getStoredFluid() != null && resource != getStoredFluid()) {
-			return false
-		}
+	override fun canStore(type: Fluid): Boolean {
+		if (type == EMPTY) return false // Cannot add an empty fluid stack
+		if (type == getFluidType()) return true // If it is the same as the current fluid
 
-		return allowedCategories.intersect(resource.categories.toSet()).isNotEmpty()
+		return if (getFluidType() == EMPTY) { // If this is currently empty
+			allowedCategories.intersect(type.categories.toSet()).isNotEmpty() // Check the category restriction
+ 		} else false // Already know its not the stored fluid, so can't store
 	}
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/types/FluidNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/types/FluidNode.kt
@@ -1,22 +1,33 @@
 package net.horizonsend.ion.server.features.transport.nodes.types
 
+import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
 import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
+import net.horizonsend.ion.server.features.transport.fluids.Fluid
 import net.horizonsend.ion.server.features.transport.util.CacheType
+import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.axis
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.faces
 import org.bukkit.Axis
 import org.bukkit.Material
+import org.bukkit.World
 import org.bukkit.block.BlockFace
 
 sealed interface FluidNode : Node {
 	override val cacheType: CacheType get() = CacheType.FLUID
+
+	/**
+	 * Used for filters
+	 **/
+	fun canTransport(fluid: Fluid): Boolean
 
 	data class FluidJunctionNode(val channel: Material) : FluidNode {
 		override val pathfindingResistance: Double = 1.0
 		override fun canTransferFrom(other: Node, offset: BlockFace): Boolean = if (other is FluidJunctionNode) other.channel == channel else true
 		override fun canTransferTo(other: Node, offset: BlockFace): Boolean = if (other is FluidJunctionNode) other.channel == channel else true
 		override fun getTransferableDirections(backwards: BlockFace): Set<BlockFace> = ADJACENT_BLOCK_FACES.minus(backwards)
+		override fun canTransport(fluid: Fluid): Boolean = true
 	}
 
 	data class LightningRodNode(var axis: Axis) : FluidNode, ComplexNode {
@@ -27,6 +38,7 @@ sealed interface FluidNode : Node {
 		override fun onTranslate(movement: StarshipMovement) {
 			this.axis = movement.displaceFace(this.axis.faces.first).axis
 		}
+		override fun canTransport(fluid: Fluid): Boolean = true
 	}
 
 	data object FluidExtractorNode : FluidNode {
@@ -34,6 +46,7 @@ sealed interface FluidNode : Node {
 		override fun canTransferFrom(other: Node, offset: BlockFace): Boolean = false
 		override fun canTransferTo(other: Node, offset: BlockFace): Boolean = other !is FluidInputNode
 		override fun getTransferableDirections(backwards: BlockFace): Set<BlockFace> = ADJACENT_BLOCK_FACES.minus(backwards)
+		override fun canTransport(fluid: Fluid): Boolean = true
 	}
 
 	data object FluidInputNode : FluidNode {
@@ -41,6 +54,8 @@ sealed interface FluidNode : Node {
 		override fun canTransferFrom(other: Node, offset: BlockFace): Boolean = true
 		override fun canTransferTo(other: Node, offset: BlockFace): Boolean = false
 		override fun getTransferableDirections(backwards: BlockFace): Set<BlockFace> = ADJACENT_BLOCK_FACES.minus(backwards)
+		override fun canTransport(fluid: Fluid): Boolean = true
+		fun getFluidEntities(world: World, location: BlockKey) = world.ion.inputManager.getHolders(CacheType.FLUID, location).filterIsInstance<FluidStoringEntity>()
 	}
 
 	data object FluidMergeNode : FluidNode {
@@ -48,6 +63,7 @@ sealed interface FluidNode : Node {
 		override fun canTransferFrom(other: Node, offset: BlockFace): Boolean = other is FluidJunctionNode
 		override fun canTransferTo(other: Node, offset: BlockFace): Boolean = other !is FluidJunctionNode
 		override fun getTransferableDirections(backwards: BlockFace): Set<BlockFace> = ADJACENT_BLOCK_FACES.minus(backwards)
+		override fun canTransport(fluid: Fluid): Boolean = true
 	}
 
 	data object FluidInvertedMergeNode : FluidNode {
@@ -55,5 +71,6 @@ sealed interface FluidNode : Node {
 		override fun canTransferFrom(other: Node, offset: BlockFace): Boolean = other is LightningRodNode
 		override fun canTransferTo(other: Node, offset: BlockFace): Boolean = other !is LightningRodNode
 		override fun getTransferableDirections(backwards: BlockFace): Set<BlockFace> = ADJACENT_BLOCK_FACES.minus(backwards)
+		override fun canTransport(fluid: Fluid): Boolean = true
 	}
 }
