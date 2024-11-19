c62554a8afbcccc55cb59287821eeeac8707b07b
Gutin1 <Claytonpinter@gmail.com>
2024-11-19 01:09:31 -0600

    Fluid work

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/FluidIngredient.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/ingredient/FluidIngredient.kt
@@ -3,15 +3,15 @@ package net.horizonsend.ion.server.features.multiblock.crafting.ingredient
 import net.horizonsend.ion.server.features.multiblock.crafting.recipe.RecipeExecutionContext
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.FluidStoringEntity
-import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
+import net.horizonsend.ion.server.features.transport.fluids.Fluid
 
-class FluidIngredient<T: MultiblockEntity>(val fluid: PipedFluid, val amount: Int) : MultiblockRecipeIngredient<T> {
+class FluidIngredient<T: MultiblockEntity>(val fluid: Fluid, val amount: Int) : MultiblockRecipeIngredient<T> {
 	override fun checkRequirement(context: RecipeExecutionContext<T>): Boolean {
 		val entity = context.entity
 
 		if (entity !is FluidStoringEntity) return false
 
-		return entity.capacities.any { it.internalStorage.getStoredFluid() == fluid && it.internalStorage.getAmount() >= amount }
+		return entity.fluidStores.any { it.internalStorage.getFluidType() == fluid && it.internalStorage.getAmount() >= amount }
 	}
 
 	override fun consumeIngredient(context: RecipeExecutionContext<T>): Boolean {
@@ -19,8 +19,8 @@ class FluidIngredient<T: MultiblockEntity>(val fluid: PipedFluid, val amount: In
 
 		if (entity !is FluidStoringEntity) return false
 
-		return (entity.capacities.firstOrNull {
-			it.internalStorage.getStoredFluid() == fluid && it.internalStorage.getAmount() >= amount
-		}?.internalStorage?.remove(amount) ?: amount) == 0
+		return (entity.fluidStores.firstOrNull {
+			it.internalStorage.getFluidType() == fluid && it.internalStorage.getAmount() >= amount
+		}?.internalStorage?.removeAmount(amount) ?: amount) == 0
 	}
 }
