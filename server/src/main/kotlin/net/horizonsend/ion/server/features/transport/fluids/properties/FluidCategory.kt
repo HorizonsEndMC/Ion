6739cbee5587683f146344e917ee9ad4839c91a1
Gutin1 <Claytonpinter@gmail.com>
2024-11-19 01:09:31 -0600

    Fluid work

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/properties/FluidCategory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/properties/FluidCategory.kt
@@ -2,16 +2,16 @@ package net.horizonsend.ion.server.features.transport.fluids.properties
 
 import com.manya.pdc.DataTypes
 import com.manya.pdc.base.EnumDataType
-import net.horizonsend.ion.server.features.transport.fluids.PipedFluid
+import net.horizonsend.ion.server.features.transport.fluids.Fluid
 
 enum class FluidCategory {
 	GAS;
 
-	private val members = mutableListOf<PipedFluid>()
+	private val members = mutableListOf<Fluid>()
 
-	fun getMembers(): List<PipedFluid> = members
+	fun getMembers(): List<Fluid> = members
 
-	fun addMember(fluid: PipedFluid) = members.add(fluid)
+	fun addMember(fluid: Fluid) = members.add(fluid)
 
 	companion object {
 		val persistentDataType = EnumDataType(FluidCategory::class.java)
