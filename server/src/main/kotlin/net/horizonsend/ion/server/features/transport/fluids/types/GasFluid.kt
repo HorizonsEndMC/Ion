25fe5eec892b1f285a581c069753c8bd88e2281c
Gutin1 <Claytonpinter@gmail.com>
2024-11-20 12:16:19 -0600

    rename gas fluids to gas to allow future liquid variants

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/types/GasFluid.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/types/GasFluid.kt
@@ -1,9 +1,12 @@
 package net.horizonsend.ion.server.features.transport.fluids.types
 
+import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.gas.type.Gas
 import net.horizonsend.ion.server.features.transport.fluids.Fluid
 import net.horizonsend.ion.server.features.transport.fluids.properties.FluidCategory
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.GRAY
 import java.util.function.Supplier
 
 class GasFluid(
@@ -12,6 +15,7 @@ class GasFluid(
 ) : Fluid(identifier) {
 	override val categories: Array<FluidCategory> = arrayOf(FluidCategory.GAS)
 
-	override val displayName: Component get() = gas.displayName
+	override val displayName: Component get() = ofChildren(gas.displayName, text(" Gas", GRAY))
+
 	val gas get() = gasSupplier.get()
 }
