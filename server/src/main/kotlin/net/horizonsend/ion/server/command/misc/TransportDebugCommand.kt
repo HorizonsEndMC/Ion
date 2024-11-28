157cf6ad2a1220d11fd1d35a8a55d6419193b690
Gutin1 <Claytonpinter@gmail.com>
2024-11-27 23:25:22 -0600

    copy paper thread dump code into a command

--- server/src/main/kotlin/net/horizonsend/ion/server/command/misc/TransportDebugCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/misc/TransportDebugCommand.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.server.command.misc
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
+import io.papermc.paper.util.StacktraceDeobfuscator
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.server.command.SLCommand
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.highlightBlocks
@@ -15,6 +16,9 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.slf4j.Logger
+import java.lang.management.ManagementFactory
+import java.lang.management.ThreadInfo
 import java.util.concurrent.Executors
 
 @CommandPermission("starlegacy.transportdebug")
@@ -51,5 +55,38 @@ object TransportDebugCommand : SLCommand() {
 			.filter { it.distance(loc) < 100.0 }
 
 		sender.highlightBlocks(inputs, 50L)
+	}
+
+	@Subcommand("force dump")
+	fun forceDump(sender: Player) {
+		log.error("Entire Thread Dump:")
+		val threads = ManagementFactory.getThreadMXBean().dumpAllThreads(true, true)
+		for (thread in threads) {
+			dumpThread(thread, log)
+		}
+	}
+
+	private fun dumpThread(thread: ThreadInfo, log: Logger) {
+		log.error("------------------------------")
+		//
+		log.error("Current Thread: " + thread.threadName)
+		log.error(
+			("\tPID: " + thread.threadId
+			+ " | Suspended: " + thread.isSuspended
+			+ " | Native: " + thread.isInNative
+			+ " | State: " + thread.threadState)
+		)
+		if (thread.lockedMonitors.size != 0) {
+			log.error("\tThread is waiting on monitor(s):")
+			for (monitor in thread.lockedMonitors) {
+				log.error("\t\tLocked on:" + monitor.lockedStackFrame)
+			}
+		}
+		log.error("\tStack:")
+		//
+		for (stack in StacktraceDeobfuscator.INSTANCE.deobfuscateStacktrace(thread.stackTrace))  // Paper
+		{
+			log.error("\t\t" + stack)
+		}
 	}
 }
