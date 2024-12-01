8f72e90d229c92c3cd2a807466c890d93fba0308
Gutin1 <Claytonpinter@gmail.com>
2024-12-01 13:39:01 -0600

    Check multiblock integrity when smacking a sign with a wrench

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/misc/Wrench.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/misc/Wrench.kt
@@ -1,8 +1,12 @@
 package net.horizonsend.ion.server.features.custom.items.misc
 
+import net.horizonsend.ion.common.extensions.information
+import net.horizonsend.ion.server.command.misc.MultiblockCommand
 import net.horizonsend.ion.server.features.custom.items.CustomItem
+import net.horizonsend.ion.server.features.multiblock.MultiblockAccess
 import net.horizonsend.ion.server.features.multiblock.PrePackaged
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.CUSTOM_ITEM
+import net.horizonsend.ion.server.miscellaneous.utils.isWallSign
 import net.horizonsend.ion.server.miscellaneous.utils.text.itemName
 import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
 import net.kyori.adventure.text.Component.text
@@ -24,7 +28,20 @@ object Wrench : CustomItem("WRENCH") {
 	}
 
 	override fun handlePrimaryInteract(livingEntity: LivingEntity, itemStack: ItemStack, event: PlayerInteractEvent) {
+		if (livingEntity !is Player) return
 
+		val hitBlock = event.clickedBlock
+		if (hitBlock?.type?.isWallSign != true) return
+
+		val sign = hitBlock.state as? Sign ?: return
+		val multiblock = MultiblockAccess.getFast(sign) ?: return
+
+		if (multiblock.signMatchesStructure(sign, loadChunks = false, particles =  false)) {
+			livingEntity.information("Multiblock structure is correct.")
+			return
+		}
+
+		MultiblockCommand.onCheck(livingEntity, multiblock, sign.x, sign.y, sign.z)
 	}
 
 	override fun handleSecondaryInteract(livingEntity: LivingEntity, itemStack: ItemStack, event: PlayerInteractEvent?) {
