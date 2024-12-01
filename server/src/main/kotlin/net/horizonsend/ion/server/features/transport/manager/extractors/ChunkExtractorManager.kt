d8325b399aa0aca89c4a33bc9f48bf2520f12bc2
Gutin1 <Claytonpinter@gmail.com>
2024-12-01 11:49:10 -0600

    load extractors if no data is present

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ChunkExtractorManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ChunkExtractorManager.kt
@@ -1,8 +1,10 @@
 package net.horizonsend.ion.server.features.transport.manager.extractors
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap
+import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.transport.manager.ChunkTransportManager
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.getBlockTypeSafe
@@ -19,11 +21,18 @@ class ChunkExtractorManager(val manager: ChunkTransportManager) : ExtractorManag
 		return extractors.values
 	}
 
-	override fun registerExtractor(x: Int, y: Int, z: Int, ensureExtractor: Boolean): Boolean = synchronized(mutex) {
+	override fun registerExtractor(x: Int, y: Int, z: Int, ensureExtractor: Boolean): Boolean {
 		if (ensureExtractor && getBlockTypeSafe(manager.chunk.world, x, y, z) != EXTRACTOR_TYPE) return false
-		if (!manager.chunk.isInBounds(x, y, z)) return false
+		if (!manager.chunk.isInBounds(x, y, z)) {
+			IonServer.slF4JLogger.warn("Extractor manager of ${manager.chunk} tried to register an extractor outside its bounds!")
+			return false
+		}
 		val key = toBlockKey(x, y, z)
-		extractors[key] = ExtractorData(key)
+
+		synchronized(mutex) {
+			extractors[key] = ExtractorData(key)
+		}
+
 		needsSave = true
 		return true
 	}
@@ -42,12 +51,33 @@ class ChunkExtractorManager(val manager: ChunkTransportManager) : ExtractorManag
 		return extractors.contains(key)
 	}
 
-	override fun onLoad(): Unit = synchronized(mutex) {
-		val existing = manager.chunk.inner.persistentDataContainer.get(NamespacedKeys.EXTRACTORS, PersistentDataType.LONG_ARRAY) ?: return
-		existing.associateWithTo(extractors) { ExtractorData(it) }
+	override fun onLoad() {
+		val existing = manager.chunk.inner.persistentDataContainer.get(NamespacedKeys.EXTRACTORS, PersistentDataType.LONG_ARRAY)
+
+		if (existing == null) {
+			loadFromChunk()
+			return
+		}
+
+		synchronized(mutex) { existing.associateWithTo(extractors) { ExtractorData(it) } }
 	}
 
 	override fun save() {
 		manager.chunk.inner.persistentDataContainer.set(NamespacedKeys.EXTRACTORS, PersistentDataType.LONG_ARRAY, extractors.keys.toLongArray())
+	}
+
+	private fun loadFromChunk() = Tasks.async {
+		val snapshot = manager.chunk.inner.chunkSnapshot
+		val minBlockX = snapshot.x.shl(4)
+		val minBlockZ = snapshot.z.shl(4)
+
+		for (x in 0..15) for (z in 0..15) for (y in manager.chunk.world.minHeight..snapshot.getHighestBlockYAt(x, z)) {
+			if (snapshot.getBlockType(x, y, z) != EXTRACTOR_TYPE) continue
+			val key = toBlockKey(x + minBlockX, y, z + minBlockZ)
+			extractors[key] = ExtractorData(key)
+		}
+
+
+		save()
 	}
 }
