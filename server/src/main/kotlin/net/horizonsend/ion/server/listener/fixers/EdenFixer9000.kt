ec871b97c99a96b3a22499c856a13bf953dfd684
Gutin1 <Claytonpinter@gmail.com>
2024-12-01 17:55:38 -0600

    fix nullability problem

--- server/src/main/kotlin/net/horizonsend/ion/server/listener/fixers/EdenFixer9000.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/listener/fixers/EdenFixer9000.kt
@@ -19,7 +19,7 @@ class EdenFixer9000 : SLEventListener() {
 	fun onChunkLoad(event: ChunkLoadEvent) {
 		if (!event.chunk.world.name.lowercase().contains("eden")) return
 
-		val chunkOreVersion = event.chunk.persistentDataContainer.get(NamespacedKeys.EDEN_FIX, PersistentDataType.BYTE)
+		val chunkOreVersion: Byte? = event.chunk.persistentDataContainer.get(NamespacedKeys.EDEN_FIX, PersistentDataType.BYTE)
 		if (chunkOreVersion == 2.toByte()) return
 
 		val evilBlocks = setOf(Material.SCULK_SENSOR, Material.SCULK_CATALYST, Material.SCULK_SHRIEKER)
