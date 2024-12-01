9f285019e3bcde54bee7e971ea2b27c42c81475f
Gutin1 <Claytonpinter@gmail.com>
2024-12-01 12:50:38 -0600

    Add serializable filter data

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/filters/FilterData.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/filters/FilterData.kt
@@ -1,26 +1,35 @@
 package net.horizonsend.ion.server.features.transport.filters
 
-import com.manya.pdc.base.UuidDataType
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import org.bukkit.persistence.PersistentDataAdapterContext
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
-import java.util.UUID
 
-class FilterData(
-	val uuid: UUID,
+class FilterData<T : Any>(
+	val isWhitelist: Boolean,
+	val entries: MutableList<FilterKey<out T>>
 ) {
-	companion object : PersistentDataType<PersistentDataContainer, FilterData> {
+	companion object : PersistentDataType<PersistentDataContainer, FilterData<*>> {
 		override fun getPrimitiveType(): Class<PersistentDataContainer> = PersistentDataContainer::class.java
-		override fun getComplexType(): Class<FilterData> = FilterData::class.java
+		override fun getComplexType(): Class<FilterData<*>> = FilterData::class.java
 
-		override fun fromPrimitive(primitive: PersistentDataContainer, context: PersistentDataAdapterContext): FilterData {
-			return FilterData(primitive.get(NamespacedKeys.USER, UuidDataType())!!)
+		override fun fromPrimitive(primitive: PersistentDataContainer, context: PersistentDataAdapterContext): FilterData<*> {
+			val whitelist = primitive.getOrDefault(NamespacedKeys.WHITELIST, PersistentDataType.BOOLEAN, true)
+			val entries = primitive.getOrDefault(NamespacedKeys.FILTER_ENTRY, PersistentDataType.TAG_CONTAINER_ARRAY, arrayOf())
+			val formatted = entries.mapTo(mutableListOf()) { entry ->
+				FilterKey.fromPrimitive(entry, context)
+			}
+
+			return FilterData(whitelist, formatted)
 		}
 
-		override fun toPrimitive(complex: FilterData, context: PersistentDataAdapterContext): PersistentDataContainer {
+		override fun toPrimitive(complex: FilterData<*>, context: PersistentDataAdapterContext): PersistentDataContainer {
 			val data = context.newPersistentDataContainer()
-			data.set(NamespacedKeys.USER,UuidDataType(), complex.uuid)
+
+			data.set(NamespacedKeys.WHITELIST, PersistentDataType.BOOLEAN, complex.isWhitelist)
+			val array = Array(complex.entries.size) { FilterKey.toPrimitive(complex.entries[it], context) }
+			data.set(NamespacedKeys.FILTER_ENTRY, PersistentDataType.TAG_CONTAINER_ARRAY, array)
+
 			return data
 		}
 	}
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/filters/FilterType.kt
@@ -0,0 +1,32 @@
+package net.horizonsend.ion.server.features.transport.filters
+
+import com.manya.pdc.minecraft.ItemStackDataType
+import net.horizonsend.ion.server.features.transport.fluids.Fluid
+import net.horizonsend.ion.server.features.transport.fluids.FluidPersistentDataType
+import net.horizonsend.ion.server.features.transport.util.CacheType
+import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
+
+abstract class FilterType<T : Any>(val cacheType: CacheType, val typeClass: Class<T>, val persistentDataType: PersistentDataType<*, T>) {
+	data object FluidType : FilterType<Fluid>(CacheType.FLUID, Fluid::class.java, FluidPersistentDataType)
+	data object ItemType : FilterType<ItemStack>(CacheType.SOLAR_PANELS /*TODO*/, ItemStack::class.java, ItemStackDataType())
+
+	fun store(pdc: PersistentDataContainer, data: Any) {
+		if (!typeClass.isInstance(data)) return
+		@Suppress("UNCHECKED_CAST")
+		pdc.set(NamespacedKeys.FILTER_ENTRY, persistentDataType, data as T)
+	}
+
+	fun retrieve(pdc: PersistentDataContainer): T? {
+		return pdc.get(NamespacedKeys.FILTER_ENTRY, persistentDataType)
+	}
+
+	companion object {
+		val filterTypes = mutableMapOf<String, FilterType<*>>(
+			FluidType.javaClass.simpleName to FluidType,
+			ItemType.javaClass.simpleName to ItemType,
+		)
+	}
+}
