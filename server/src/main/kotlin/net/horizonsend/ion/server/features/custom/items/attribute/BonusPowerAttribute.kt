985ec4ccf6bc614e29fd4891c1fe8519c687fe7b
Gutin1 <Claytonpinter@gmail.com>
2024-12-04 00:55:09 -0600

    Outline and basic implementation of custom items v3

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/attribute/BonusPowerAttribute.kt
@@ -0,0 +1,3 @@
+package net.horizonsend.ion.server.features.custom.items.attribute
+
+data class BonusPowerAttribute(val amount: Int) : CustomItemAttribute
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/attribute/CustomItemAttribute.kt
@@ -0,0 +1,3 @@
+package net.horizonsend.ion.server.features.custom.items.attribute
+
+interface CustomItemAttribute
