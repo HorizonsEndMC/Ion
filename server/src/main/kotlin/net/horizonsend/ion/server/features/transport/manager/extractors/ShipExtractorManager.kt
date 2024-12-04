7188e0c2d60f26aa716fb2f4a3329697234d0e2f
Gutin1 <Claytonpinter@gmail.com>
2024-12-04 14:09:12 -0600

    capture nodes & extractors when ship pilots, delete old extractors

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ShipExtractorManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ShipExtractorManager.kt
@@ -2,12 +2,18 @@ package net.horizonsend.ion.server.features.transport.manager.extractors
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap
 import net.horizonsend.ion.server.features.starship.Starship
+import net.horizonsend.ion.server.features.starship.movement.StarshipMovement
+import net.horizonsend.ion.server.features.transport.NewTransport
+import net.horizonsend.ion.server.features.world.chunk.IonChunk
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.chunkKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.getBlockTypeSafe
 
 class ShipExtractorManager(val starship: Starship) : ExtractorManager() {
-	val extractors = Long2ObjectOpenHashMap<ExtractorData>()
+	var extractors = Long2ObjectOpenHashMap<ExtractorData>()
 
 	override fun getExtractors(): Collection<ExtractorData> {
 		return extractors.values
@@ -17,6 +23,7 @@ class ShipExtractorManager(val starship: Starship) : ExtractorManager() {
 		return extractors.contains(key)
 	}
 
+	/** Returns true if an extractor was registered */
 	override fun registerExtractor(x: Int, y: Int, z: Int, ensureExtractor: Boolean): Boolean {
 		if (ensureExtractor && getBlockTypeSafe(starship.world, x, y, z) != EXTRACTOR_TYPE) return false
 		val key = toBlockKey(x, y, z)
@@ -33,10 +40,31 @@ class ShipExtractorManager(val starship: Starship) : ExtractorManager() {
 	}
 
 	fun loadExtractors() {
-		starship.iterateBlocks { x, y, z -> registerExtractor(x, y, z, true) }
+		starship.iterateBlocks { x, y, z ->
+			if (registerExtractor(x, y, z, true)) NewTransport.removeExtractor(starship.world, x, y, z)
+		}
 	}
 
 	fun releaseExtractors() {
-		//TODO
+		// We can disregard the extractor data
+		// Its more so a convenience thing
+		val byChunk = extractors.keys.groupBy { key ->
+			chunkKey(getX(key).shr(4), getZ(key).shr(4))
+		}
+
+		for ((chunkKey, entries) in byChunk) {
+			val ionChunk = IonChunk[starship.world, chunkKey] ?: continue
+
+			for (entry in entries) {
+				ionChunk.transportNetwork.extractorManager.registerExtractor(entry, ensureExtractor = true)
+			}
+		}
+	}
+
+	fun displace(movement: StarshipMovement) {
+		val new = Long2ObjectOpenHashMap<ExtractorData>()
+		extractors.forEach { new[movement.displaceKey(it.key)] = it.value }
+
+		extractors = new
 	}
 }
--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
@@ -94,7 +94,6 @@ import net.horizonsend.ion.server.features.transport.NewTransport
 import net.horizonsend.ion.server.features.transport.filters.FilterAccess
 import net.horizonsend.ion.server.features.transport.filters.FilterBlocks
 import net.horizonsend.ion.server.features.transport.fluids.FluidRegistry
-import net.horizonsend.ion.server.features.transport.old.Extractors
 import net.horizonsend.ion.server.features.transport.old.TransportConfig
 import net.horizonsend.ion.server.features.transport.old.Wires
 import net.horizonsend.ion.server.features.transport.old.pipe.Pipes
@@ -152,7 +151,6 @@ val components: List<IonComponent> = listOf(
 	FilterBlocks,
 	FilterAccess,
 	TransportConfig.Companion,
-	Extractors,
 	Pipes,
 	Filters,
 	Wires,
