7eed5420bf7721388e22865143f064023252d447
Gutin1 <Claytonpinter@gmail.com>
2024-12-04 00:55:09 -0600

    Outline and basic implementation of custom items v3

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/components/LoreManager.kt
@@ -0,0 +1,18 @@
+package net.horizonsend.ion.server.features.custom.items.components
+
+import net.kyori.adventure.text.Component
+import org.bukkit.inventory.ItemStack
+
+interface LoreManager {
+	val priority: Int
+
+	/**
+	 * Returns the constructed lore lines of this block
+	 **/
+	fun getLines(itemStack: ItemStack): List<Component>
+
+	/**
+	 * Returns whether there should be an empty separator line between this lore block and the following, should there be one.
+	 **/
+	fun shouldIncludeSeparator(): Boolean
+}
