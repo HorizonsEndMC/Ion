4a00d09d3a3499527c4a6de8689d6b3aef8702ae
Gutin1 <Claytonpinter@gmail.com>
2024-12-05 11:32:44 -0600

    Add model to itembuilder, implement builder functions

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/util/ItemFactory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/util/ItemFactory.kt
@@ -1,11 +1,12 @@
 package net.horizonsend.ion.server.features.custom.items.util
 
 import io.papermc.paper.datacomponent.DataComponentTypes
-import io.papermc.paper.datacomponent.item.ItemLore
+import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys.HORIZONSEND_NAMESPACE
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
+import java.util.function.Consumer
 import java.util.function.Supplier
 
 class ItemFactory private constructor(
@@ -17,20 +18,44 @@ class ItemFactory private constructor(
 	fun construct(): ItemStack {
 		val base = ItemStack(material)
 
-		if (customModel != null) base.setData(DataComponentTypes.ITEM_MODEL, Key.key("horizonsend", customModel))
+		if (customModel != null) base.setData(DataComponentTypes.ITEM_MODEL, Key.key(HORIZONSEND_NAMESPACE, customModel))
 		if (maxStackSize != null) base.setData(DataComponentTypes.MAX_STACK_SIZE, maxStackSize)
 		if (nameSupplier != null) base.setData(DataComponentTypes.CUSTOM_NAME, nameSupplier.get())
-		if (loreSupplier != null) base.setData(DataComponentTypes.LORE, ItemLore.lore(loreSupplier.get()))
 
 		return base
 	}
 
-	class Builder() {
-		var material = Material.WARPED_FUNGUS_ON_A_STICK
-		var customModel: String? = null
-		var maxStackSize: Int? = null
-		var nameSupplier: Supplier<Component>? = null
-		var loreSupplier: Supplier<List<Component>>? = null
+	fun construct(modifier: Consumer<ItemStack>) {
+		val base = construct()
+		modifier.accept(base)
+	}
+
+	class Builder {
+		private var material = Material.WARPED_FUNGUS_ON_A_STICK
+		private var customModel: String? = null
+		private var maxStackSize: Int? = null
+		private var nameSupplier: Supplier<Component>? = null
+
+		fun setMaterial(material: Material): Builder {
+			this.material = material
+			return this
+		}
+
+		fun setCustomModel(customModel: String): Builder {
+			this.customModel = customModel
+			return this
+		}
+
+		fun setMaxStackSize(maxStackSize: Int): Builder {
+			check(maxStackSize < 100)
+			this.maxStackSize = maxStackSize
+			return this
+		}
+
+		fun setNameSupplier(nameSupplier: Supplier<Component>): Builder {
+			this.nameSupplier = nameSupplier
+			return this
+		}
 
 		fun build(): ItemFactory {
 			return ItemFactory(
@@ -38,7 +63,6 @@ class ItemFactory private constructor(
 				customModel = this.customModel,
 				maxStackSize = this.maxStackSize,
 				nameSupplier = this.nameSupplier,
-				loreSupplier = this.loreSupplier
 			)
 		}
 	}
