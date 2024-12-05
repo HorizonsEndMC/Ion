b65996fbf135efe4f4d51f4f814ddc484e3d29c1
Gutin1 <Claytonpinter@gmail.com>
2024-12-05 12:17:06 -0600

    Add data migrators skeleton

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/data/migrator/types/CustomItemStackMigrator.kt
@@ -0,0 +1,9 @@
+package net.horizonsend.ion.server.features.data.migrator.types
+
+import net.horizonsend.ion.server.features.custom.NewCustomItem
+import net.horizonsend.ion.server.features.custom.items.CustomItems
+import org.bukkit.inventory.ItemStack
+
+abstract class CustomItemStackMigrator(val customItemIdentifier: String, dataVersion: Int) : DataMigrator<ItemStack, NewCustomItem>(dataVersion) {
+	val customItem get() = CustomItems.getByIdentifier(customItemIdentifier)!!
+}
