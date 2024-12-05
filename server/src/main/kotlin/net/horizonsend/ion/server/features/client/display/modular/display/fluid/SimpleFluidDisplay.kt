327540e1fb77d6b965ce98009f9f8fe0d4017b05
Gutin1 <Claytonpinter@gmail.com>
2024-12-04 23:31:23 -0600

    fix display rotation

--- server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/fluid/SimpleFluidDisplay.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/fluid/SimpleFluidDisplay.kt
@@ -1,12 +1,14 @@
 package net.horizonsend.ion.server.features.client.display.modular.display.fluid
 
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.server.features.client.display.modular.TextDisplayHandler
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage.StorageContainer
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.space
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftTextDisplay
 
 class SimpleFluidDisplay(
 	storage: StorageContainer,
@@ -21,5 +23,12 @@ class SimpleFluidDisplay(
 
 	companion object {
 		val empty = text("Empty", NamedTextColor.GRAY, TextDecoration.ITALIC)
+	}
+
+	override fun createEntity(parent: TextDisplayHandler): CraftTextDisplay {
+		Throwable().printStackTrace()
+
+		println("Create entity $this")
+		return super.createEntity(parent)
 	}
 }
