0c8fdbad67b0579bf453b4039d637aa8935e37e2
Gutin1 <Claytonpinter@gmail.com>
2024-12-07 12:06:20 -0600

    Add ammo component and attribute

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/components/PoweredItemComponent.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/components/PoweredItemComponent.kt
@@ -5,23 +5,19 @@ import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.custom.NewCustomItem
 import net.horizonsend.ion.server.features.custom.items.attribute.BonusPowerAttribute
 import net.horizonsend.ion.server.features.custom.items.attribute.CustomItemAttribute
+import net.horizonsend.ion.server.features.custom.items.objects.StoredValues
 import net.horizonsend.ion.server.features.custom.items.powered.CratePlacer.displayDurability
 import net.horizonsend.ion.server.features.custom.items.powered.CratePlacer.updateDurability
 import net.horizonsend.ion.server.features.custom.items.powered.PoweredItem.PowerLoreManager.powerPrefix
-import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys.POWER
-import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.entity.Damageable
 import org.bukkit.inventory.ItemStack
-import org.bukkit.persistence.PersistentDataType.INTEGER
 
 class PoweredItemComponent(private val baseMaxPower: Int) : CustomItemComponent, LoreManager {
 	override fun decorateBase(baseItem: ItemStack) {
-		baseItem.updateMeta {
-			it.persistentDataContainer.set(POWER, INTEGER, baseMaxPower)
-		}
+		StoredValues.POWER.setAmount(baseItem, baseMaxPower)
 	}
 
 	fun getMaxPower(customItem: NewCustomItem, itemStack: ItemStack): Int {
@@ -32,24 +28,22 @@ class PoweredItemComponent(private val baseMaxPower: Int) : CustomItemComponent,
 		val capacity = getMaxPower(customItem, itemStack)
 		val corrected = amount.coerceAtMost(capacity)
 
-		itemStack.updateMeta {
-			it.persistentDataContainer.set(POWER, INTEGER, amount)
-		}
+		StoredValues.POWER.setAmount(itemStack, corrected)
 
 		if (displayDurability && itemStack.itemMeta is Damageable) {
-			updateDurability(itemStack, amount, capacity)
+			updateDurability(itemStack, corrected, capacity)
 		}
 	}
 
-	fun getPower(customItem: NewCustomItem, itemStack: ItemStack): Int {
-		return itemStack.itemMeta.persistentDataContainer.getOrDefault(POWER, INTEGER, 0)
+	fun getPower(itemStack: ItemStack): Int {
+		return StoredValues.POWER.getAmount(itemStack)
 	}
 
 	override val priority: Int = 100
 	override fun shouldIncludeSeparator(): Boolean = true
 
 	override fun getLines(customItem: NewCustomItem, itemStack: ItemStack): List<Component> {
-		val power = getPower(customItem, itemStack)
+		val power = getPower(itemStack)
 
 		return listOf(ofChildren(
 			powerPrefix,
