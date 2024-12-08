6e8b2b0e6cf2d91f22cc4267d4d8326546430fb4
kwazedilla <kwazedilla@gmail.com>
2024-12-07 17:21:22 -0800

    allow ships to pass through: - fluids - non-solid plant blocks/vines - leaves - non-solid coral blocks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/movement/OptimizedMovement.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/movement/OptimizedMovement.kt
@@ -15,10 +15,16 @@ import net.horizonsend.ion.server.miscellaneous.utils.minecraft
 import net.horizonsend.ion.server.miscellaneous.utils.nms
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.level.block.BaseCoralPlantTypeBlock
 import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.BushBlock
+import net.minecraft.world.level.block.GrowingPlantBlock
+import net.minecraft.world.level.block.LeavesBlock
+import net.minecraft.world.level.block.LiquidBlock
 import net.minecraft.world.level.block.NetherPortalBlock
 import net.minecraft.world.level.block.StainedGlassBlock
+import net.minecraft.world.level.block.VineBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.chunk.LevelChunk
@@ -130,7 +136,15 @@ object OptimizedMovement {
 		}
 	}
 
-	private fun isHangar(newBlockData: BlockState) = newBlockData.block is StainedGlassBlock || newBlockData.block is NetherPortalBlock
+	private fun isHangar(newBlockData: BlockState) =
+		newBlockData.block is StainedGlassBlock ||
+				newBlockData.block is NetherPortalBlock ||
+				newBlockData.block is LiquidBlock ||
+				newBlockData.block is BushBlock || // most types of crop/grass blocks
+				newBlockData.block is VineBlock || // normal vines
+				newBlockData.block is GrowingPlantBlock || // twisted vines on Luxiterna, kelp, etc.
+				newBlockData.block is LeavesBlock ||
+				newBlockData.block is BaseCoralPlantTypeBlock
 
 	private fun dissipateHangarBlocks(world2: World, hangars: LinkedList<Long>) {
 		for (blockKey in hangars.iterator()) {
