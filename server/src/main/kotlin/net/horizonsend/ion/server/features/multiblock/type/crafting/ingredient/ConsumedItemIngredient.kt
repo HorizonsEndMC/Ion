ed3b52a692b2b71379a6fad3833a34090d8d3d12
Gutin1 <Claytonpinter@gmail.com>
2024-12-09 14:16:54 -0600

    Remove old blasters

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/crafting/ingredient/ConsumedItemIngredient.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/crafting/ingredient/ConsumedItemIngredient.kt
@@ -1,5 +1,6 @@
 package net.horizonsend.ion.server.features.multiblock.type.crafting.ingredient
 
+import net.horizonsend.ion.server.features.custom.NewCustomItem
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import org.bukkit.block.Sign
@@ -7,6 +8,7 @@ import org.bukkit.inventory.ItemStack
 
 class ConsumedItemIngredient(ingredient: ItemStack, amount: Int) : ItemIngredient(ingredient, amount), ItemConsumable {
 	constructor(ingredient: CustomItem, amount: Int) : this(ingredient.constructItemStack(), amount)
+	constructor(ingredient: NewCustomItem, amount: Int) : this(ingredient.constructItemStack(), amount)
 
 	override fun consume(multiblock: Multiblock, sign: Sign, itemStack: ItemStack) {
 		itemStack.amount -= amount
