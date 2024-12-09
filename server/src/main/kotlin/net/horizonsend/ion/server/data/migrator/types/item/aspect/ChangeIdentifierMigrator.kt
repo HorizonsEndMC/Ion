0bc46ec1a2c7476f84fb04f6b805c9eb8df58219
Gutin1 <Claytonpinter@gmail.com>
2024-12-09 00:43:05 -0600

    Add custom item identifier update aspect, simplify aspect migrator

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/aspect/ChangeIdentifierMigrator.kt
@@ -0,0 +1,14 @@
+package net.horizonsend.ion.server.data.migrator.types.item.aspect
+
+import net.horizonsend.ion.server.data.migrator.types.item.MigratorResult
+import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+class ChangeIdentifierMigrator(val old: String, val new: String) : ItemAspectMigrator {
+	override fun migrate(subject: ItemStack): MigratorResult<ItemStack> {
+		subject.updateMeta { it.persistentDataContainer.set(NamespacedKeys.CUSTOM_ITEM, PersistentDataType.STRING, new) }
+		return MigratorResult.Mutation()
+	}
+}
