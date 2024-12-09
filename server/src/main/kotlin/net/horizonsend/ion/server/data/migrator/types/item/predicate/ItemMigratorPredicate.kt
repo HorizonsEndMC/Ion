61e0c8be6cd17291497e80c6c025ae5fb9e27f6e
Gutin1 <Claytonpinter@gmail.com>
2024-12-09 01:24:12 -0600

    rebuild blaster migrators, ability to set predicate

--- server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/predicate/ItemMigratorPredicate.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/predicate/ItemMigratorPredicate.kt
@@ -4,4 +4,10 @@ import org.bukkit.inventory.ItemStack
 
 fun interface ItemMigratorPredicate {
 	fun shouldMigrate(item: ItemStack): Boolean
+
+	data object True : ItemMigratorPredicate {
+		override fun shouldMigrate(item: ItemStack): Boolean {
+			return true
+		}
+	}
 }
