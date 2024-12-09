e53e8efadffba0627b50571cd3ae1de16338c40f
Gutin1 <Claytonpinter@gmail.com>
2024-12-08 21:09:39 -0600

    more permanent data migrator system?

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/aspect/ChangeTypeMigrator.kt
@@ -0,0 +1,11 @@
+package net.horizonsend.ion.server.data.migrator.types.item.aspect
+
+import net.horizonsend.ion.server.data.migrator.types.item.MigratorResult
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class ChangeTypeMigrator(private val newType: Material) : ItemAspectMigrator {
+	override fun migrate(subject: ItemStack) : MigratorResult<ItemStack> {
+		return MigratorResult.Replacement(subject.withType(newType))
+	}
+}
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/aspect/PullLoreMigrator.kt
@@ -0,0 +1,15 @@
+package net.horizonsend.ion.server.data.migrator.types.item.aspect
+
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.ItemLore
+import net.horizonsend.ion.server.data.migrator.types.item.MigratorResult
+import net.horizonsend.ion.server.features.custom.NewCustomItem
+import org.bukkit.inventory.ItemStack
+
+/** Pulls lore from the custom item registry and sets the item's lore */
+class PullLoreMigrator(val customItem: NewCustomItem) : ItemAspectMigrator {
+	override fun migrate(subject: ItemStack) : MigratorResult<ItemStack> {
+		subject.setData(DataComponentTypes.LORE, ItemLore.lore(customItem.assembleLore(subject)))
+		return MigratorResult.Mutation()
+	}
+}
