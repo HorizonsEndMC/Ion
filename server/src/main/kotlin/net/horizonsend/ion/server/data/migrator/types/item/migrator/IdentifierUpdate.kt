6667bf2f945ebfd29b703746d48be5a3ce2d14a0
Gutin1 <Claytonpinter@gmail.com>
2024-12-08 23:59:05 -0600

    fix migrators

--- server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/migrator/IdentifierUpdate.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/data/migrator/types/item/migrator/IdentifierUpdate.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.server.data.migrator.types.item.migrator
 import net.horizonsend.ion.server.data.migrator.types.item.MigratorResult
 import net.horizonsend.ion.server.data.migrator.types.item.predicate.CustomItemPredicate
 import net.horizonsend.ion.server.miscellaneous.registrations.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.updateMeta
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 
@@ -12,7 +13,7 @@ class IdentifierUpdate(val old: String, val new: String) : CustomItemStackMigrat
 	}
 
 	override fun performMigration(subject: ItemStack): MigratorResult<ItemStack> {
-		subject.itemMeta.persistentDataContainer.set(NamespacedKeys.CUSTOM_ITEM, PersistentDataType.STRING, new)
+		subject.updateMeta { it.persistentDataContainer.set(NamespacedKeys.CUSTOM_ITEM, PersistentDataType.STRING, new) }
 		return MigratorResult.Mutation()
 	}
 }
