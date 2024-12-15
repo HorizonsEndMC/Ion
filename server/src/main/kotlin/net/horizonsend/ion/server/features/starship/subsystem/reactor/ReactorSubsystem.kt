60e86bb9a310d0e2aa2b338614cb89572823cf7c
kwazedilla <kwazedilla@gmail.com>
2024-12-15 12:38:40 -0800

    experimental testing: - pulse cannon extra explosion damage `12.0 -> 8.0` - changed sub 1k shield regen factor equation to match better

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/reactor/ReactorSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/reactor/ReactorSubsystem.kt
@@ -4,6 +4,7 @@ import net.horizonsend.ion.server.features.starship.active.ActiveControlledStars
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.subsystem.StarshipSubsystem
 import net.horizonsend.ion.server.features.starship.subsystem.shield.StarshipShields
+import kotlin.math.cbrt
 import kotlin.math.min
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
@@ -37,7 +38,10 @@ class ReactorSubsystem(
 		val shieldPortion = this.powerDistributor.shieldPortion
 		val shieldEfficiency = starship.shieldEfficiency
 		val shieldPower = reactorOutput * shieldPortion * shieldEfficiency * delta *
-				if (starship.initialBlockCount < 1000) 0.0301606 * sqrt(starship.initialBlockCount.toDouble()) else 1.0
+				if (starship.initialBlockCount < 1000)
+					// approx. 0.25 shield regen at 150 blocks, 0.62 regen at 500, and 1.0 at 1k+
+					-0.0953256 * cbrt(starship.initialBlockCount.toDouble()) + 0.0617674 * sqrt(starship.initialBlockCount.toDouble())
+				else 1.0
 		val totalMissing = starship.shields.sumOf { shield -> shield.maxPower - shield.power }
 
 		if (totalMissing == 0) {
