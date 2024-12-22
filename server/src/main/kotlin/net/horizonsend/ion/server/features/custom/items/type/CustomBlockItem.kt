97112cacd300bf89cca7b5cfe465918c1559bc59
Gutin1 <Claytonpinter@gmail.com>
2024-12-22 12:37:04 -0600

    everything wfoas

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/CustomBlockItem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/CustomBlockItem.kt
@@ -4,13 +4,11 @@ import net.horizonsend.ion.server.features.custom.blocks.CustomBlock
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.custom.items.util.ItemFactory
 import net.kyori.adventure.text.Component
-import org.bukkit.Material
 import java.util.function.Supplier
 
 open class CustomBlockItem(
 	identifier: String,
 
-	material: Material,
 	customModel: String,
 	displayName: Component,
 
@@ -21,7 +19,6 @@ open class CustomBlockItem(
 	ItemFactory
 		.builder(ItemFactory.stackableCustomItem)
 		.setCustomModel(customModel)
-		.setMaterial(material)
 		.build()
 ) {
     fun getCustomBlock(): CustomBlock {
