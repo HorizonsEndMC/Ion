e5794dfccf896c764a4178a09e9f4e359dee32e8
Gutin1 <claytonpinter@gmail.com>
2025-01-06 18:01:57 -0800

    Work on cross-chunk feature storage, abuse NMS

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/GeneratedFeature.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/GeneratedFeature.kt
@@ -5,32 +5,45 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import net.horizonsend.ion.server.features.space.data.CompletedSection
 import net.horizonsend.ion.server.features.world.generation.WorldGenerationManager
+import net.horizonsend.ion.server.features.world.generation.feature.meta.FeatureMetaData
+import net.horizonsend.ion.server.features.world.generation.feature.meta.FeatureMetadataFactory
+import net.horizonsend.ion.server.features.world.generation.feature.start.FeatureStart
+import net.horizonsend.ion.server.features.world.generation.generators.IonWorldGenerator
 import net.horizonsend.ion.server.features.world.generation.generators.configuration.FeaturePlacementConfiguration
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import net.minecraft.world.level.ChunkPos
 import org.bukkit.NamespacedKey
+import kotlin.collections.map
+import kotlin.random.Random
 
-abstract class GeneratedFeature(val key: NamespacedKey, val placementConfiguration: FeaturePlacementConfiguration) {
-	abstract suspend fun generateSection(context: FeaturePlacementContext, sectionMin: Int, sectionMax: Int): CompletedSection
+abstract class GeneratedFeature<T: FeatureMetaData>(val key: NamespacedKey, val placementConfiguration: FeaturePlacementConfiguration) {
+	abstract val metaFactory: FeatureMetadataFactory<T>
+	abstract suspend fun generateSection(generator: IonWorldGenerator<*>, chunkPos: ChunkPos, start: FeatureStart, metaData: T, sectionY: Int, sectionMin: Int, sectionMax: Int): CompletedSection
 
-	suspend fun generateChunk(context: FeaturePlacementContext): List<CompletedSection> {
-		val (minPoint, maxPoint) = getExtents(context)
-		val minY = minPoint.y
-		val maxY = maxPoint.y
+	@Suppress("UNCHECKED_CAST")
+	suspend fun castAndGenerateChunk(generator: IonWorldGenerator<*>, chunkPos: ChunkPos, start: FeatureStart): List<CompletedSection> = generateChunk(generator, chunkPos, start, start.metaData as T)
+
+	suspend fun generateChunk(generator: IonWorldGenerator<*>, chunkPos: ChunkPos, start: FeatureStart, metaData: T): List<CompletedSection> {
+		val (minPoint, maxPoint) = getExtents(metaData)
+		val minY = minPoint.y + start.y
+		val maxY = maxPoint.y + start.y
 
 		val sections = IntRange(
-			context.heightAccessor.getSectionIndex(minY),
-			context.heightAccessor.getSectionIndex(maxY),
+			generator.heightAccessor.getSectionIndex(minY),
+			generator.heightAccessor.getSectionIndex(maxY),
 		)
 
 		val deferredSections = mutableListOf<CompletableDeferred<CompletedSection>>()
 
 		for (section: Int in sections) {
 			val deferred = CompletableDeferred<CompletedSection>()
-			val sectionMin = (section.shl(4) + context.heightAccessor.minY)
-			val sectionMax = (section.shl(4) + context.heightAccessor.minY) + 15
+			deferredSections.add(deferred)
+
+			val sectionMin = (section.shl(4) + generator.heightAccessor.minY)
+			val sectionMax = (section.shl(4) + generator.heightAccessor.minY) + 15
 
 			WorldGenerationManager.coroutineScope.launch {
-				deferred.complete(generateSection(context, sectionMin, sectionMax))
+				deferred.complete(generateSection(generator, chunkPos, start, metaData, section, sectionMin, sectionMax))
 			}
 		}
 
@@ -38,9 +51,30 @@ abstract class GeneratedFeature(val key: NamespacedKey, val placementConfigurati
 	}
 
 	/**
-	 * Returns min point to max point
+	 * Returns min point to max point, centered on the placement origin.
 	 **/
-	abstract fun getExtents(context: FeaturePlacementContext): Pair<Vec3i, Vec3i>
+	abstract fun getExtents(metaData: T): Pair<Vec3i, Vec3i>
+
+	/**
+	 * Gets a transformed minimum and maximum chunk
+	 **/
+	fun getChunkExtents(start: FeatureStart): Pair<ChunkPos, ChunkPos> {
+		val (minPoint, maxPoint) = @Suppress("UNCHECKED_CAST") getExtents(start.metaData as T)
+		val origin = Vec3i(start.x, start.y, start.z)
+
+		val minAdjusted = minPoint.plus(origin)
+		val maxAdjusted = maxPoint.plus(origin)
+
+		return ChunkPos(minAdjusted.x.shr(4), minAdjusted.z.shr(4)) to ChunkPos(maxAdjusted.x.shr(4), maxAdjusted.z.shr(4))
+	}
+
+	fun buildStartsData(chunkPos: ChunkPos, random: Random): List<FeatureStart> {
+		return placementConfiguration.generatePlacements(chunkPos, random).map { context ->
+			FeatureStart(this, context.x, context.y, context.z, generateMetaData(random))
+		}
+	}
+
+	abstract fun generateMetaData(chunkRandom: Random): T
 
 	fun canPlace(): Boolean = true
 }
