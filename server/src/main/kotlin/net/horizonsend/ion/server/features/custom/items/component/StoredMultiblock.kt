4c4391c9f182d666e1d7454046c7e5c40efdd494
Gutin1 <Claytonpinter@gmail.com>
2025-01-12 20:52:11 -0600

    Add more descriptions / display names

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/component/StoredMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/component/StoredMultiblock.kt
@@ -1,9 +1,11 @@
 package net.horizonsend.ion.server.features.custom.items.component
 
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.common.utils.text.wrap
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.custom.items.attribute.CustomItemAttribute
 import net.horizonsend.ion.server.features.multiblock.PrePackaged
+import net.horizonsend.ion.server.features.multiblock.type.DisplayNameMultilblock.Companion.getDescription
 import net.horizonsend.ion.server.miscellaneous.utils.text.itemLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
@@ -20,7 +22,10 @@ object StoredMultiblock : CustomItemComponent, LoreManager {
 
 	override fun getLines(customItem: CustomItem, itemStack: ItemStack): List<Component> {
 		val multiblock = PrePackaged.getTokenData(itemStack) ?: return listOf()
-		return listOf(
+		val description = multiblock.getDescription().wrap(150).map { it.itemLore }
+
+		return description + listOf(
+			Component.empty(),
 			ofChildren(text("Multiblock: ", GRAY), text(multiblock.name.replaceFirstChar { char -> char.uppercase(Locale.getDefault()) })).itemLore,
 			ofChildren(text("Variant: ", GRAY), text(multiblock.javaClass.simpleName)).itemLore,
 			text("Left click to preview").itemLore,
