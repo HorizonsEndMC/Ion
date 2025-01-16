79f8bad1aa1e2eb9210934ed92da057bce7ea05d
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 14:09:31 -0600

    abstract for the future

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/MultiblockRecipeRegistry.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/MultiblockRecipeRegistry.kt
@@ -1,9 +1,15 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting
 
 import net.horizonsend.ion.server.IonServerComponent
+import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry
 import net.horizonsend.ion.server.features.multiblock.entity.type.RecipeProcessingMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.FurnaceMultiblockRecipe
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.NewMultiblockRecipe
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.ItemRequirement
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.PowerRequirement
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.result.ItemResult
+import net.horizonsend.ion.server.features.multiblock.type.industry.CentrifugeMultiblock
 import net.horizonsend.ion.server.miscellaneous.utils.multimapOf
 import kotlin.reflect.KClass
 
@@ -11,7 +17,14 @@ object MultiblockRecipeRegistry : IonServerComponent() {
 	val recipes = mutableListOf<NewMultiblockRecipe<*>>()
 	val byMultiblock = multimapOf<KClass<out RecipeProcessingMultiblockEntity<*>>, NewMultiblockRecipe<*>>()
 
-
+	val URANIUM_ENRICHMENT = register(FurnaceMultiblockRecipe(
+		identifier = "URANIUM_ENRICHMENT",
+		clazz = CentrifugeMultiblock.CentrifugeMultiblockEntity::class,
+		smeltingItem = ItemRequirement.CustomItemRequirement(CustomItemRegistry.URANIUM),
+		fuelItem = null,
+		power = PowerRequirement(100),
+		result = ItemResult.simpleResult(CustomItemRegistry.ENRICHED_URANIUM)
+	))
 
 	fun <I: RecipeEnviornment, R: NewMultiblockRecipe<I>> register(recipe: R): R {
 		recipes.add(recipe)
