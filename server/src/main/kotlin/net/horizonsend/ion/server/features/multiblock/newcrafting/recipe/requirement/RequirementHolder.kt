5d43324798868ca2b812fc96549f71a5e79df726
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 14:09:31 -0600

    abstract for the future

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
@@ -1,17 +1,43 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement
 
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.util.SlotModificationWrapper
 
-class RequirementHolder<T: RecipeEnviornment, V: Any?>(val dataTypeClass: Class<V>, val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
+open class RequirementHolder<T: RecipeEnviornment, V: Any?, out R: RecipeRequirement<V>>(val dataTypeClass: Class<V>, val getter: (T) -> V, val requirement: R) {
 	fun checkRequirement(enviornment: T): Boolean {
 		val resourceValue = getter.invoke(enviornment)
 		return requirement.ensureAvailable(resourceValue)
 	}
 
+	open fun consume(enviornment: T) {
+		if (requirement is Consumable<*, *>) {
+			@Suppress("UNCHECKED_CAST")
+			(requirement as Consumable<V, T>).consume(enviornment)
+		}
+	}
+
+	class BundledRequirementHolder<T: RecipeEnviornment, V: Any?, R: RecipeRequirement<V>>(
+		dataTypeClass: Class<V>,
+		getter: (T) -> V,
+		requirement: R,
+		val slotModificationWrapper: (T) -> SlotModificationWrapper
+	) : RequirementHolder<T, V, R>(dataTypeClass, getter, requirement) {
+		override fun consume(enviornment: T) {
+			val wrapper = slotModificationWrapper.invoke(enviornment)
+			wrapper.removeFromSlot(1)
+		}
+	}
+
 	companion object {
-		inline fun <T: RecipeEnviornment, reified V: Any?> of(
+		inline fun <T: RecipeEnviornment, reified V: Any?, R: Consumable<V, T>> simpleConsumable(
 			noinline getter: (T) -> V,
-			requirement: RecipeRequirement<V>
-		): RequirementHolder<T, V> = RequirementHolder(V::class.java, getter, requirement)
+			requirement: R,
+		): RequirementHolder<T, V, R> = RequirementHolder(V::class.java, getter, requirement)
+
+		inline fun <T: RecipeEnviornment, reified V: Any?, R: RecipeRequirement<V>> itemConsumable(
+			noinline getter: (T) -> V,
+			requirement: R,
+			noinline slotModificationWrapper: (T) -> SlotModificationWrapper,
+		): RequirementHolder<T, V, R> = BundledRequirementHolder(V::class.java, getter, requirement, slotModificationWrapper)
 	}
 }
