252a0f1ee50402308dfe416a15e3159f086e499b
Gutin1 <Claytonpinter@gmail.com>
2025-01-15 23:59:46 -0600

    start of (attempt 4) permanent multiblock recipe system

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/crafting/recipe/take2/result/ItemResult.kt
+++
@@ -1,7 +0,0 @@
-package net.horizonsend.ion.server.features.multiblock.crafting.recipe.take2.result
-
-import org.bukkit.inventory.ItemStack
-
-class ItemResult(private val item: ItemStack) : MultiblockRecipeResult() {
-	fun assemble(): ItemStack = item.clone()
-}
