ddb649bad4684a26cfe46ce6648d94afa9dc4fea
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 01:34:21 -0600

    probably everything I need to migrate recipes over

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/FurnaceMultiblockRecipe.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/FurnaceMultiblockRecipe.kt
@@ -1,7 +1,7 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe
 
 import net.horizonsend.ion.server.features.multiblock.entity.type.RecipeProcessingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.IndustryMultiblockEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.FurnaceEnviornment
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.ItemRequirement
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.PowerRequirement
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.RequirementHolder
@@ -14,21 +14,22 @@ import kotlin.reflect.KClass
  * @param smeltingItem Requirement for the item in the top slot. If it is null, there will be a requirement for this slot to be empty.
  * @param fuelitem Requirement for the item in the bottom slot. If it is null, there will be a requirement for this slot to be empty.
  **/
-class IndustryMultiblockRecipe(
-	clazz: KClass<RecipeProcessingMultiblockEntity<IndustryMultiblockEnviornment>>,
+class FurnaceMultiblockRecipe(
+	identifier: String,
+	clazz: KClass<RecipeProcessingMultiblockEntity<FurnaceEnviornment>>,
 	smeltingItem: ItemRequirement?,
 	fuelitem: ItemRequirement?,
 	power: PowerRequirement,
-	val result: RecipeResult<IndustryMultiblockEnviornment>
-) : NewMultiblockRecipe<IndustryMultiblockEnviornment>(clazz) {
+	val result: RecipeResult<FurnaceEnviornment>
+) : NewMultiblockRecipe<FurnaceEnviornment>(identifier, clazz) {
 
-	override val requirements: Collection<RequirementHolder<IndustryMultiblockEnviornment, *>> = listOf(
+	override val requirements: Collection<RequirementHolder<FurnaceEnviornment, *>> = listOf(
 		RequirementHolder.of({ it.getItem(0) }, smeltingItem ?: ItemRequirement.empty()),
 		RequirementHolder.of({ it.getItem(1) }, fuelitem ?: ItemRequirement.empty()),
 		RequirementHolder.of({ it.powerStorage.getPower() }, power)
 	)
 
-	override fun assemble(enviornment: IndustryMultiblockEnviornment) {
+	override fun assemble(enviornment: FurnaceEnviornment) {
 		if (!verifyAllRequirements(enviornment)) result
 		if (result.verifySpace(enviornment)) return
 
--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
@@ -1,15 +1,15 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement
 
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
 
-class RequirementHolder<T: MultiblockRecipeEnviornment, V: Any?>(val dataTypeClass: Class<V>, val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
+class RequirementHolder<T: RecipeEnviornment, V: Any?>(val dataTypeClass: Class<V>, val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
 	fun checkRequirement(enviornment: T): Boolean {
 		val resourceValue = getter.invoke(enviornment)
 		return requirement.ensureAvailable(resourceValue)
 	}
 
 	companion object {
-		inline fun <T: MultiblockRecipeEnviornment, reified V: Any?> of(
+		inline fun <T: RecipeEnviornment, reified V: Any?> of(
 			noinline getter: (T) -> V,
 			requirement: RecipeRequirement<V>
 		): RequirementHolder<T, V> = RequirementHolder(V::class.java, getter, requirement)
