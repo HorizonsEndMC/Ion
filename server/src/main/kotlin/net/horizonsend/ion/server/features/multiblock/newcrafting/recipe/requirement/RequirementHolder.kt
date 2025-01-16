ae065ac3226b972af578637a02a80264daf12346
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 00:29:06 -0600

    work on the base recipe class

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
@@ -2,9 +2,16 @@ package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requir
 
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
 
-class RequirementHolder<T: MultiblockRecipeEnviornment, V: Any?>(val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
+class RequirementHolder<T: MultiblockRecipeEnviornment, V: Any?>(val dataTypeClass: Class<V>, val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
 	fun checkRequirement(enviornment: T): Boolean {
 		val resourceValue = getter.invoke(enviornment)
 		return requirement.ensureAvailable(resourceValue)
+	}
+
+	companion object {
+		inline fun <T: MultiblockRecipeEnviornment, reified V: Any?> of(
+			noinline getter: (T) -> V,
+			requirement: RecipeRequirement<V>
+		): RequirementHolder<T, V> = RequirementHolder(V::class.java, getter, requirement)
 	}
 }
