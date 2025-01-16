53b59d968f465e6a0158c687be35357042ff818e
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 01:34:21 -0600

    probably everything I need to migrate recipes over

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/NewMultiblockRecipe.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/NewMultiblockRecipe.kt
@@ -1,12 +1,12 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe
 
 import net.horizonsend.ion.server.features.multiblock.entity.type.RecipeProcessingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.RequirementHolder
 import org.bukkit.inventory.ItemStack
 import kotlin.reflect.KClass
 
-abstract class NewMultiblockRecipe<E: MultiblockRecipeEnviornment>(val entityType: KClass<RecipeProcessingMultiblockEntity<E>>) {
+abstract class NewMultiblockRecipe<E: RecipeEnviornment>(val identifier: String, val entityType: KClass<RecipeProcessingMultiblockEntity<E>>) {
 	protected abstract val requirements: Collection<RequirementHolder<E, *>>
 
 	fun getItemRequirements(): Collection<RequirementHolder<E, ItemStack?>> = requirements
@@ -16,8 +16,6 @@ abstract class NewMultiblockRecipe<E: MultiblockRecipeEnviornment>(val entityTyp
 	fun verifyAllRequirements(enviornment: E): Boolean = getAllRequirements().all { holder -> holder.checkRequirement(enviornment) }
 
 	fun getAllRequirements(): Collection<RequirementHolder<E, *>> = requirements
-
-	fun ensureRequirementsAvailable(input: E): Boolean
 
 	abstract fun assemble(input: E)
 }
