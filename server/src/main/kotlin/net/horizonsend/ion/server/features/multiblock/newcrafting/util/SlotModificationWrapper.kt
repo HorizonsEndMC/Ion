1c63f0489c8b4aeb8a9993d02aba55a2c1b3a270
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 14:09:31 -0600

    abstract for the future

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/util/SlotModificationWrapper.kt
@@ -0,0 +1,41 @@
+package net.horizonsend.ion.server.features.multiblock.newcrafting.util
+
+import com.google.common.base.Supplier
+import org.bukkit.inventory.FurnaceInventory
+import org.bukkit.inventory.ItemStack
+import java.util.function.Consumer
+
+/**
+ * Handles the addition of items to a slot in an inventory
+ **/
+class SlotModificationWrapper(private val getter: Supplier<ItemStack?>, private val setter: Consumer<ItemStack>) {
+	fun removeFromSlot(amount: Int) {
+		getter.get()?.amount -= amount
+	}
+
+	fun addToSlot(newItem: ItemStack) {
+		val currentItem = getter.get()
+
+		if (currentItem == null || currentItem.isEmpty) {
+			setter.accept(newItem)
+			return
+		}
+
+		currentItem.amount = (currentItem.amount + newItem.amount).coerceIn(1, newItem.maxStackSize)
+	}
+
+	fun verifySpace(newItem: ItemStack): Boolean {
+		val resultOccupant = getter.get() ?: return true
+		if (resultOccupant.isEmpty) return true
+		if (!resultOccupant.isSimilar(newItem)) return false
+
+		val maxStackSize = resultOccupant.maxStackSize
+		return resultOccupant.amount + newItem.amount <= maxStackSize
+	}
+
+	companion object {
+		fun furnaceSmelting(inventory: FurnaceInventory): SlotModificationWrapper = SlotModificationWrapper(inventory::getSmelting, inventory::setSmelting)
+		fun furnaceFuel(inventory: FurnaceInventory): SlotModificationWrapper = SlotModificationWrapper(inventory::getFuel, inventory::setFuel)
+		fun furnaceResult(inventory: FurnaceInventory): SlotModificationWrapper = SlotModificationWrapper(inventory::getResult, inventory::setResult)
+	}
+}
