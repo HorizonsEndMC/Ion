a5c2dc545098eb624691459a8ab052fd502b01cc
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 01:34:21 -0600

    probably everything I need to migrate recipes over

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/RecipeProcessingMultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/type/RecipeProcessingMultiblockEntity.kt
@@ -1,11 +1,27 @@
 package net.horizonsend.ion.server.features.multiblock.entity.type
 
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
+import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.features.multiblock.newcrafting.MultiblockRecipeRegistry
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.NewMultiblockRecipe
 
-interface RecipeProcessingMultiblockEntity<Input: MultiblockRecipeEnviornment> {
-	fun buildInput(): Input
+interface RecipeProcessingMultiblockEntity<E: RecipeEnviornment> {
+	fun buildRecipeEnviornment(): E
 
-	fun getRecipesFor() {
+	fun getRecipesFor(): NewMultiblockRecipe<E>? {
+		val enviornment = buildRecipeEnviornment()
 
+		val recipes = MultiblockRecipeRegistry.getRecipesFor(this)
+		val match = recipes.filter { recipe -> recipe.verifyAllRequirements(enviornment) }
+		if (match.size > 1) IonServer.slF4JLogger.warn("Multiple recipes match input! This should not happen!!! Infringing recipes: ${match.joinToString { it.identifier }}")
+
+		return match.firstOrNull()
+	}
+
+	fun tryProcessRecipe(): Boolean {
+		val enviornment = buildRecipeEnviornment()
+		val recipe = getRecipesFor()
+
+		return recipe?.assemble(enviornment) != null
 	}
 }
