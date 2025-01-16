aa3e053ca41338eb0a52e7be71d8cb63da2908ad
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-16 17:52:38 -0600

    some optimizations, work on progress recipes, fix tick interval

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/input/ItemResultEnviornment.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/input/ItemResultEnviornment.kt
@@ -1,9 +1,11 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.input
 
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.newcrafting.util.SlotModificationWrapper
 import org.bukkit.inventory.ItemStack
 
 interface ItemResultEnviornment : RecipeEnviornment {
+	val multiblock: MultiblockEntity
 	fun getResultItem(): ItemStack?
 	fun getResultItemSlotModifier(): SlotModificationWrapper
 }
