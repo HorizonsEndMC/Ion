98a93975dcb16f98e28b912fddc4c224f6faa95b
Gutin1 <Claytonpinter@gmail.com>
2025-01-15 23:59:46 -0600

    start of (attempt 4) permanent multiblock recipe system

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/requirement/RequirementHolder.kt
@@ -0,0 +1,10 @@
+package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement
+
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
+
+class RequirementHolder<T: MultiblockRecipeEnviornment, V: Any?>(val getter: (T) -> V, val requirement: RecipeRequirement<V>) {
+	fun checkRequirement(enviornment: T): Boolean {
+		val resourceValue = getter.invoke(enviornment)
+		return requirement.ensureAvailable(resourceValue)
+	}
+}
