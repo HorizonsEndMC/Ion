1fd924ffbfa63177d111ea3770c2dbdeee0ee998
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 00:29:06 -0600

    work on the base recipe class

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/RecipeResult.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/RecipeResult.kt
@@ -2,7 +2,7 @@ package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.result
 
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
 
-interface RecipeResult<Input: MultiblockRecipeEnviornment> {
-	fun verifySpace(input: Input)
-	fun execute(input: Input)
+interface RecipeResult<E: MultiblockRecipeEnviornment> {
+	fun verifySpace(input: E): Boolean
+	fun execute(input: E)
 }
