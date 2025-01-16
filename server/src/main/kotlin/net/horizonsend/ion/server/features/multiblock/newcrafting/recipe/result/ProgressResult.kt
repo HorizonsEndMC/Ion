996d676fcdf012d3e040a4cac88a2d0fc1c5ef6c
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-16 17:52:38 -0600

    some optimizations, work on progress recipes, fix tick interval

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/ProgressResult.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/ProgressResult.kt
@@ -1,7 +1,9 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.result
 
+import net.horizonsend.ion.server.features.multiblock.entity.type.StatusMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.FurnaceEnviornment
 import net.horizonsend.ion.server.features.multiblock.newcrafting.util.SlotModificationWrapper
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.inventory.ItemStack
 import java.time.Duration
 
@@ -15,10 +17,21 @@ class ProgressResult(val duration: Duration, val resultItem: ItemStack) : ItemRe
 		return resultItem.amount + resultItem.amount <= maxStackSize
 	}
 
+	override fun shouldConsumeIngredients(enviornment: FurnaceEnviornment): Boolean {
+		return enviornment.progressManager.wouldComplete(duration)
+	}
+
 	override fun execute(enviornment: FurnaceEnviornment, slotModificationWrapper: SlotModificationWrapper) {
 		val complete = enviornment.progressManager.addProgress(duration)
+
+		val multiblock = enviornment.multiblock
+		if (multiblock is StatusMultiblockEntity) multiblock.setStatus(enviornment.progressManager.formatProgress(NamedTextColor.RED))
+
 		if (!complete) return
 
+		if (multiblock is StatusMultiblockEntity) multiblock.setStatus(enviornment.progressManager.formatProgress(NamedTextColor.GREEN))
+
+		enviornment.progressManager.reset()
 		slotModificationWrapper.addToSlot(resultItem)
 	}
 
