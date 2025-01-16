ba057cee92d70743b99c015ecb5531fe1457fd5a
Gutin1 <Claytonpinter@gmail.com>
2025-01-16 01:34:21 -0600

    probably everything I need to migrate recipes over

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/MultiblockRecipeRegistry.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/MultiblockRecipeRegistry.kt
@@ -2,7 +2,7 @@ package net.horizonsend.ion.server.features.multiblock.newcrafting
 
 import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.features.multiblock.entity.type.RecipeProcessingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.MultiblockRecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
 import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.NewMultiblockRecipe
 import net.horizonsend.ion.server.miscellaneous.utils.multimapOf
 import kotlin.reflect.KClass
@@ -13,10 +13,15 @@ object MultiblockRecipeRegistry : IonServerComponent() {
 
 
 
-	fun <I: MultiblockRecipeEnviornment, R: NewMultiblockRecipe<I>> register(recipe: R): R {
+	fun <I: RecipeEnviornment, R: NewMultiblockRecipe<I>> register(recipe: R): R {
 		recipes.add(recipe)
 		byMultiblock[recipe.entityType].add(recipe)
 
 		return recipe
+	}
+
+	fun <E: RecipeEnviornment> getRecipesFor(entity: RecipeProcessingMultiblockEntity<E>): Collection<NewMultiblockRecipe<E>> {
+		@Suppress("UNCHECKED_CAST")
+		return byMultiblock[entity::class] as Collection<NewMultiblockRecipe<E>>
 	}
 }
