a7b47ffce65b7bf77d68d3c8549fe2e2340bc218
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-16 18:32:57 -0600

    fix progress result power consumption during normal ticking

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/ResultHolder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/recipe/result/ResultHolder.kt
@@ -2,10 +2,11 @@ package net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.result
 
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.ItemResultEnviornment
 import net.horizonsend.ion.server.features.multiblock.newcrafting.input.RecipeEnviornment
+import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.requirement.RequirementHolder
 
 abstract class ResultHolder<E: RecipeEnviornment, R: RecipeResult<E>>(val result: R) {
 	fun verifySpace(enviornment: E) = result.verifySpace(enviornment)
-	fun shouldConsumeIngredients(enviornment: E): Boolean = result.shouldConsumeIngredients(enviornment)
+	fun filterConsumedIngredients(enviornment: E, requirements: Collection<RequirementHolder<E, *, *>>) = result.filterConsumedIngredients(enviornment, requirements)
 	abstract fun execute(input: E)
 
 	class ItemResultHolder<E: ItemResultEnviornment, R: ItemResult<E>>(result: R) : ResultHolder<E, R>(result) {
