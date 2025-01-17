a89854ba6bfaf7ea880c67ac90784875d02e4604
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-17 00:40:01 -0600

    Make progress result more general use, rename to WarmupResult, build out more utils

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/input/RecipeEnviornment.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/newcrafting/input/RecipeEnviornment.kt
@@ -1,9 +1,12 @@
 package net.horizonsend.ion.server.features.multiblock.newcrafting.input
 
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.kyori.adventure.sound.Sound
 import org.bukkit.inventory.ItemStack
 
 interface RecipeEnviornment {
+	val multiblock: MultiblockEntity
+
 	fun getItemSize(): Int
 	fun getItem(index: Int): ItemStack?
 
