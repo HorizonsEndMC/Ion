cb00fa72dc77d527967c4d660e9ac25d8c0afffc
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-16 18:17:05 -0600

    Add industry entity, migrate industry entities to it

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CentrifugeMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CentrifugeMultiblock.kt
@@ -2,20 +2,12 @@ package net.horizonsend.ion.server.features.multiblock.type.industry
 
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
-import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.ProgressMultiblock
-import net.horizonsend.ion.server.features.multiblock.entity.type.RecipeProcessingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.power.SimplePoweredEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
-import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent
+import net.horizonsend.ion.server.features.multiblock.entity.type.power.IndustryEntity
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
-import net.horizonsend.ion.server.features.multiblock.newcrafting.input.FurnaceEnviornment
-import net.horizonsend.ion.server.features.multiblock.newcrafting.recipe.NewMultiblockRecipe
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
 import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
 import org.bukkit.World
 import org.bukkit.block.BlockFace
-import org.bukkit.block.Sign
 
 
 object CentrifugeMultiblock : Multiblock(), EntityMultiblock<CentrifugeMultiblock.CentrifugeMultiblockEntity> {
@@ -123,27 +115,5 @@ object CentrifugeMultiblock : Multiblock(), EntityMultiblock<CentrifugeMultibloc
 		z: Int,
 		world: World,
 		structureDirection: BlockFace
-	) : SimplePoweredEntity(data, CentrifugeMultiblock, manager, x, y, z, world, structureDirection, 300_000),
-		LegacyMultiblockEntity,
-		SyncTickingMultiblockEntity,
-		RecipeProcessingMultiblockEntity<FurnaceEnviornment>,
-		ProgressMultiblock {
-		override var lastRecipe: NewMultiblockRecipe<FurnaceEnviornment>? = null
-		override val progressManager: ProgressMultiblock.ProgressManager = ProgressMultiblock.ProgressManager(data)
-		override val tickingManager: TickedMultiblockEntityParent.TickingManager = TickedMultiblockEntityParent.TickingManager(20)
-
-		override val displayHandler = standardPowerDisplay(this)
-
-		override fun loadFromSign(sign: Sign) {
-			migrateLegacyPower(sign)
-		}
-
-		override fun buildRecipeEnviornment(): FurnaceEnviornment {
-			return FurnaceEnviornment(this)
-		}
-
-		override fun tick() {
-			tryProcessRecipe()
-		}
-	}
+	) : IndustryEntity(data, CentrifugeMultiblock, manager, x, y, z, world, structureDirection, 300_000)
 }
