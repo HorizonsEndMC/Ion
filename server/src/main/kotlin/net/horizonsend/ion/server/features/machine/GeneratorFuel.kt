d286cab8967d826947868aa8a2c2d9db5be53bc2
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-19 01:25:46 -0600

    Add regular dried kelp as a generator fuel

--- server/src/main/kotlin/net/horizonsend/ion/server/features/machine/GeneratorFuel.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/machine/GeneratorFuel.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.server.features.machine
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.customItem
+import net.horizonsend.ion.server.features.machine.GeneratorFuel.values
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
@@ -16,7 +17,8 @@ enum class GeneratorFuel(private val item: ItemStack, val cooldown: Int, val pow
 	COAL_BLOCK(itemStack(Material.COAL_BLOCK), cooldown = 300, power = 4000),
 	REDSTONE(itemStack(Material.REDSTONE), cooldown = 75, power = 750),
 	REDSTONE_BLOCK(itemStack(Material.REDSTONE_BLOCK), cooldown = 350, power = 6500),
-	DRIED_KELP_BLOCK(itemStack(Material.DRIED_KELP_BLOCK), cooldown = 60, power = 800);
+	DRIED_KELP_BLOCK(itemStack(Material.DRIED_KELP_BLOCK), cooldown = 60, power = 800),
+	DRIED_KELP(itemStack(Material.DRIED_KELP_BLOCK), cooldown = 7, power = 80);
 
 	companion object {
 		private val itemMap: Map<String, GeneratorFuel> = values().associateBy { createKey(it.item) }
