989be0a6bb0bb6df9d986f02428d86181f7134ae
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-20 00:13:51 -0600

    Store ship nodes & multiblocks in relative positions

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/linkage/MultiblockLinkageHolder.kt
@@ -0,0 +1,75 @@
+package net.horizonsend.ion.server.features.multiblock.linkage
+
+import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.linkages.MultiblockLinkage
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
+import java.util.function.Supplier
+import kotlin.reflect.KClass
+
+class MultiblockLinkageHolder(
+	val entity: MultiblockEntity,
+	val offsetRight: Int,
+	val offsetUp: Int,
+	val offsetForward: Int,
+	val allowedEntities: Array<KClass<out MultiblockEntity>>,
+	val linkageDirection: RelativeFace
+) : Supplier<MultiblockEntity?> {
+	var location: BlockKey = toBlockKey(entity.getPosRelative(offsetRight, offsetUp, offsetForward))
+
+	fun register() {
+		location = toBlockKey(entity.getPosRelative(offsetRight, offsetUp, offsetForward))
+
+		val manager = entity.manager
+
+		val new = MultiblockLinkage(
+			entity,
+			allowedEntities,
+			location,
+			linkageDirection[entity.structureDirection]
+		)
+
+		manager.getLinkageManager().registerLinkage(location, new)
+	}
+
+	fun deRegister() {
+		val manager = entity.manager
+
+		manager.getLinkageManager().deRegisterLinkage(location)
+	}
+
+	override fun get(): MultiblockEntity? {
+		val manager = entity.manager
+		val linkage = manager
+			.getLinkageManager()
+			.getLinkages(location)
+			.firstOrNull { it.owner == this }
+
+		return linkage?.getOtherEnd(manager.getLinkageManager())
+	}
+
+	companion object {
+		fun MultiblockEntity.createLinkage(
+			offsetRight: Int,
+			offsetUp: Int,
+			offsetForward: Int,
+			linkageDirection: RelativeFace,
+			allowedEntities: Array<KClass<out MultiblockEntity>>
+		): MultiblockLinkageHolder {
+			val holder = MultiblockLinkageHolder(
+				this,
+				offsetRight,
+				offsetUp,
+				offsetForward,
+				allowedEntities,
+				linkageDirection
+			)
+
+			linkages.add(holder)
+
+			holder.register()
+			return holder
+		}
+	}
+}
