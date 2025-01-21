83e65f3eacf2b643110240a8ce24c926f8b90233
s1ice <alexwlefebvre@gmail.com>
2025-01-21 00:48:15 -0600

    code improvements & fixes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/chat/messages/NationsChatMessage.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/chat/messages/NationsChatMessage.kt
@@ -4,10 +4,12 @@ import net.horizonsend.ion.common.database.DbObject
 import net.horizonsend.ion.common.database.Oid
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
+import org.bukkit.entity.Player
 
 data class NationsChatMessage<A : DbObject>(
 	val id: Oid<A>,
 	override val ionPrefix: Component,
+	val sender: Player,
 	override val luckPermsPrefix: Component?,
 	override val playerDisplayName: Component,
 	override val luckPermsSuffix: Component?,
