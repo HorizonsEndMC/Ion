727f05394a7b37d5e5874339b0775fbf8193b92d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-21 23:59:55 -0600

    Serialization util

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/persistence/MetaDataContainer.kt
@@ -0,0 +1,39 @@
+package net.horizonsend.ion.server.miscellaneous.registrations.persistence
+
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
+
+data class MetaDataContainer<V : Any, C : PDCSerializers.RegisteredSerializer<V>>(
+	val persistentDataType: C,
+	val data: V
+) {
+	companion object : PersistentDataType<PersistentDataContainer, MetaDataContainer<*, *>> {
+		override fun getPrimitiveType(): Class<PersistentDataContainer> = PersistentDataContainer::class.java
+		override fun getComplexType(): Class<MetaDataContainer<*, *>> = MetaDataContainer::class.java
+
+		override fun toPrimitive(
+			complex: MetaDataContainer<*, *>,
+			context: PersistentDataAdapterContext,
+		): PersistentDataContainer {
+			val pdc = context.newPersistentDataContainer()
+			pdc.set(NamespacedKeys.SERIALIZATION_TYPE, PersistentDataType.STRING, complex.persistentDataType.identifier)
+			pdc.set(NamespacedKeys.META_DATA, PersistentDataType.TAG_CONTAINER, complex.persistentDataType.serialize(complex.data, context))
+
+			return pdc
+		}
+
+		override fun fromPrimitive(
+			primitive: PersistentDataContainer,
+			context: PersistentDataAdapterContext,
+		): MetaDataContainer<*, *> {
+			val typeId = primitive.get(NamespacedKeys.SERIALIZATION_TYPE, PersistentDataType.STRING)!!
+			val type = PDCSerializers[typeId]
+
+			return type.loadMetaDataContainer(
+				primitive.get(NamespacedKeys.META_DATA, PersistentDataType.TAG_CONTAINER)!!,
+				context
+			)
+		}
+	}
+}
