c1594704025266d007aa8c22c9e2b41cfe17b2dc
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-22 12:24:03 -0600

    start work on new types of extractors

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ExtractorManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/extractors/ExtractorManager.kt
@@ -1,12 +1,14 @@
 package net.horizonsend.ion.server.features.transport.manager.extractors
 
-import net.horizonsend.ion.server.configuration.ConfigurationFiles.transportSettings
+import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks
+import net.horizonsend.ion.server.features.transport.manager.extractors.data.ExtractorData
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getZ
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import org.bukkit.Material
+import org.bukkit.block.data.BlockData
 
 abstract class ExtractorManager {
 	abstract fun getExtractors(): Collection<ExtractorData>
@@ -17,29 +19,30 @@ abstract class ExtractorManager {
 	abstract fun removeExtractor(key: BlockKey): ExtractorData?
 	abstract fun removeExtractor(x: Int, y: Int, z: Int): ExtractorData?
 
-	fun isExtractor(x: Int, y: Int, z: Int): Boolean = isExtractor(toBlockKey(x, y, z))
-	abstract fun isExtractor(key: BlockKey): Boolean
+	/**
+	 * Returns whether an extractor is present at this location
+	 **/
+	fun isExtractorPresent(x: Int, y: Int, z: Int): Boolean = isExtractorPresent(toBlockKey(x, y, z))
+
+	/**
+	 * Returns whether an extractor is present at this location
+	 **/
+	abstract fun isExtractorPresent(key: BlockKey): Boolean
 
 	companion object {
-		val EXTRACTOR_TYPE = Material.CRAFTING_TABLE
+		val STANDARD_EXTRACTOR_TYPE = Material.CRAFTING_TABLE
+
+		fun getExtractorData(data: BlockData, pos: BlockKey): ExtractorData? {
+			if (data.material == STANDARD_EXTRACTOR_TYPE) return ExtractorData(pos)
+
+			val customBlock = CustomBlocks.getByBlockData(data)
+
+
+
+			return null
+		}
 	}
 
 	open fun onLoad() {}
 	open fun save() {}
-
-	class ExtractorData(val pos: BlockKey) {
-		private var lastTicked = System.currentTimeMillis()
-
-		private fun getDelta(time: Long): Double {
-			val diff = time - lastTicked
-			return (diff.toDouble() / transportSettings().extractorConfiguration.extractorTickIntervalMS.toDouble())
-		}
-
-		fun markTicked(): Double {
-			val now = System.currentTimeMillis()
-			val delta = getDelta(now)
-			lastTicked = now
-			return delta
-		}
-	}
 }
