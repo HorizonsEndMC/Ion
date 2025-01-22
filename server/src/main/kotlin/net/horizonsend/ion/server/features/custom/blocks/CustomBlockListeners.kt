724176d573e337a8ad6a1e4130283402aa9cf7f5
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-22 17:05:52 -0600

    Add pick block for custom items

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/CustomBlockListeners.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/CustomBlockListeners.kt
@@ -1,12 +1,15 @@
 package net.horizonsend.ion.server.features.custom.blocks
 
+import io.papermc.paper.event.player.PlayerPickItemEvent
 import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks.customBlock
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.customItem
 import net.horizonsend.ion.server.features.custom.items.type.CustomBlockItem
 import net.horizonsend.ion.server.listener.SLEventListener
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
+import net.minecraft.server.network.ServerGamePacketListenerImpl
 import org.bukkit.GameMode
 import org.bukkit.Material
+import org.bukkit.attribute.Attribute
 import org.bukkit.block.data.BlockData
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
@@ -16,6 +19,7 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.math.roundToInt
 
 object CustomBlockListeners : SLEventListener() {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -75,6 +79,53 @@ object CustomBlockListeners : SLEventListener() {
 			Action.LEFT_CLICK_BLOCK -> customBlock.onLeftClick(event, clickedBlock)
 			Action.RIGHT_CLICK_BLOCK -> customBlock.onRightClick(event, clickedBlock)
 			else -> return
+		}
+	}
+
+	// Avoid magic string
+	private val entityPickMethodName = ServerGamePacketListenerImpl::handlePickItemFromEntity.name
+
+	@EventHandler
+	fun onPlayerPickBlock(event: PlayerPickItemEvent) {
+		if (Thread.currentThread().stackTrace.any { element -> element.methodName == entityPickMethodName }) return
+		val player = event.player
+
+		val targetedBlock = player.getTargetBlockExact(player.getAttribute(Attribute.BLOCK_INTERACTION_RANGE)?.value?.roundToInt() ?: 5) ?: return
+		val customBlock = CustomBlocks.getByBlockData(targetedBlock.blockData) ?: return
+		val customBlockItem = customBlock.customItem.constructItemStack()
+
+		// Source slot is for survival, when taking the item from the slot in the inventory, -1 if it is not present, e.g. creative mode
+		// Target slot is the slot in the hotbar that the picked block will go to
+
+		if (player.gameMode == GameMode.CREATIVE) {
+			if (event.player.inventory.containsAtLeast(customBlockItem, 1)) {
+				val index = event.player.inventory.indexOfFirst { stack -> stack?.isSimilar(customBlockItem) == true }
+
+				// If in hotbar, jump to slot, else grab from inventory
+				if (index in 0..8) {
+					event.targetSlot = index
+					event.sourceSlot = index
+				} else {
+					event.sourceSlot = index
+				}
+			} else {
+				event.isCancelled = true
+				event.player.inventory.setItem(event.targetSlot, customBlockItem)
+			}
+		}
+
+		if (player.gameMode == GameMode.SURVIVAL) {
+			if (event.player.inventory.containsAtLeast(customBlockItem, 1)) {
+				val index = event.player.inventory.indexOfFirst { stack -> stack?.isSimilar(customBlockItem) == true }
+
+				// If in hotbar, jump to slot, else grab from inventory
+				if (index in 0..8) {
+					event.targetSlot = index
+					event.sourceSlot = index
+				} else {
+					event.sourceSlot = index
+				}
+			}
 		}
 	}
 }
