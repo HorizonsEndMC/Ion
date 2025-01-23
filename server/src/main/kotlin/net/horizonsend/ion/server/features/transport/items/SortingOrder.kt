343500bd37a6ebd31c821804e1c925d6c2e4a811
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-23 01:05:56 -0600

    Implement destination methods

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/items/SortingOrder.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/items/SortingOrder.kt
@@ -1,12 +1,39 @@
 package net.horizonsend.ion.server.features.transport.items
 
 import com.manya.pdc.base.EnumDataType
+import net.horizonsend.ion.server.features.transport.manager.extractors.data.ItemExtractorData.ItemExtractorMetaData
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
 
 enum class SortingOrder {
-	NEAREST_FIRST,
-	ROUND_ROBIN,
-	FARTHEST_FIRST,
-	RANDOM;
+	NEAREST_FIRST {
+		override fun getDestination(extractorData: ItemExtractorMetaData, destinations: List<BlockKey>): BlockKey {
+			val extractorPosition = toVec3i(extractorData.key)
+			return destinations.minBy { key -> extractorPosition.distance(toVec3i(key)) }
+		}
+	},
+	ROUND_ROBIN {
+		override fun getDestination(extractorData: ItemExtractorMetaData, destinations: List<BlockKey>): BlockKey {
+			val currentIndex = extractorData.roundRobinIndex
+
+			val nextIndex = (currentIndex + 1) % destinations.size
+			extractorData.roundRobinIndex = nextIndex
+			return destinations[nextIndex]
+		}
+	},
+	FARTHEST_FIRST {
+		override fun getDestination(extractorData: ItemExtractorMetaData, destinations: List<BlockKey>): BlockKey {
+			val extractorPosition = toVec3i(extractorData.key)
+			return destinations.maxBy { key -> extractorPosition.distance(toVec3i(key)) }
+		}
+	},
+	RANDOM {
+		override fun getDestination(extractorData: ItemExtractorMetaData, destinations: List<BlockKey>): BlockKey {
+			return destinations.random()
+		}
+	};
+
+	abstract fun getDestination(extractorData: ItemExtractorMetaData, destinations: List<BlockKey>): BlockKey
 
 	companion object {
 		val serializationType = EnumDataType(SortingOrder::class.java)
