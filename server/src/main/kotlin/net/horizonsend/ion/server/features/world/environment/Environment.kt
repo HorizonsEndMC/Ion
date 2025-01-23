18861916aa29414f3d2fe469007f80dc3defdb5a
kwazedilla <kwazedilla@gmail.com>
2025-01-22 23:35:04 -0800

    Revert "possible fix to item velocity bug? put velocity setting in task  so that it potentially runs after whatever is spawning the item before the block breaks"
    This reverts commit a95c78fe7d963a676fa830b7588263a0c56857c8.

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/environment/Environment.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/environment/Environment.kt
@@ -5,7 +5,6 @@ import net.horizonsend.ion.server.features.custom.items.component.CustomComponen
 import net.horizonsend.ion.server.features.custom.items.type.tool.mods.ItemModRegistry
 import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.horizonsend.ion.server.miscellaneous.utils.PerPlayerCooldown
-import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.isInside
 import net.horizonsend.ion.server.miscellaneous.utils.listen
 import org.bukkit.GameMode
@@ -91,7 +90,7 @@ enum class Environment {
 				if (!entity.world.hasEnvironment()) return@listen
 
 				entity.setGravity(false)
-				Tasks.syncDelay(0L) { entity.velocity = entity.velocity.multiply(0.05) }
+				entity.velocity = entity.velocity.multiply(0.05)
 			}
 
 			listen<PlayerMoveEvent> { event ->
