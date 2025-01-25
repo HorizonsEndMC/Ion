3e243db70a80168279df379ca238e48e5dec8cf6
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-25 14:01:14 -0600

    implement filter type specific meta data

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/filter/ItemFilterGui.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/filter/ItemFilterGui.kt
@@ -1,9 +1,5 @@
 package net.horizonsend.ion.server.features.gui.custom.filter
 
-import io.papermc.paper.datacomponent.DataComponentTypes
-import io.papermc.paper.datacomponent.item.ItemAttributeModifiers
-import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.POWER_DRILL_BASIC
-import net.horizonsend.ion.server.features.custom.items.component.CustomComponentTypes
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiItems.createButton
 import net.horizonsend.ion.server.features.gui.GuiText
@@ -11,13 +7,10 @@ import net.horizonsend.ion.server.features.gui.GuiWrapper
 import net.horizonsend.ion.server.features.gui.interactable.InteractableGUI.Companion.setTitle
 import net.horizonsend.ion.server.features.transport.filters.FilterData
 import net.horizonsend.ion.server.features.transport.filters.FilterEntry
+import net.horizonsend.ion.server.features.transport.filters.FilterMeta
+import net.horizonsend.ion.server.features.transport.filters.FilterMethod
 import net.horizonsend.ion.server.features.transport.filters.FilterType
 import net.horizonsend.ion.server.features.transport.filters.manager.FilterManager
-import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
-import net.horizonsend.ion.server.miscellaneous.utils.updateData
-import net.horizonsend.ion.server.miscellaneous.utils.updateDisplayName
-import net.horizonsend.ion.server.miscellaneous.utils.updateLore
-import net.horizonsend.ion.server.miscellaneous.utils.updatePersistentDataContainer
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import org.bukkit.block.CommandBlock
@@ -26,7 +19,6 @@ import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.InventoryView
 import org.bukkit.inventory.ItemStack
-import org.bukkit.persistence.PersistentDataType
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
 import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.item.ItemProvider
@@ -34,9 +26,7 @@ import xyz.xenondevs.invui.item.impl.AbstractItem
 import xyz.xenondevs.invui.window.Window
 import java.util.function.Supplier
 
-var strictess = mutableMapOf<Int, Boolean>()
-
-class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack>, private val commandBlock: Supplier<CommandBlock>) : GuiWrapper {
+class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack, FilterMeta.ItemFilterMeta>, private val commandBlock: Supplier<CommandBlock>) : GuiWrapper {
 	private var currentWindow: Window? = null
 
 	val whitelistText get() = if (data.isWhitelist) text("Whitelist") else text("Blacklist")
@@ -55,19 +45,19 @@ class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack>,
 			.addIngredient('z', toggleWhitelistButton)
 
 		@Suppress("UNCHECKED_CAST")
-		val filterEntries = data.entries.take(6).toMutableList() as MutableList<FilterEntry<ItemStack>>
+		val filterEntries = data.entries.take(6).toMutableList() as MutableList<FilterEntry<ItemStack, FilterMeta.ItemFilterMeta>>
 
 		// Pad list
 		if (filterEntries.size < 6) repeat(6 - filterEntries.size) {
-			filterEntries.add(FilterEntry(null, FilterType.ItemType))
+			filterEntries.add(FilterType.ItemType.buildEmptyEntry())
 		}
 
 		data.entries = filterEntries
 
 		for (slot in data.entries.indices) {
-			val entry = data.entries[slot] as FilterEntry<ItemStack>
-			gui.addIngredient('1' + slot, filterSlotProvider(entry, slot))
-			gui.addIngredient('a' + slot, strictnessSlotProvider(entry, slot))
+			val entry = data.entries[slot] as FilterEntry<ItemStack, FilterMeta.ItemFilterMeta>
+			gui.addIngredient('1' + slot, filterSlotProvider(entry))
+			gui.addIngredient('a' + slot, strictnessSlotProvider(entry))
 		}
 
 		val window = Window
@@ -96,7 +86,7 @@ class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack>,
 		view.setTitle(getSlotOverlay())
 	}
 
-	fun filterSlotProvider(entry: FilterEntry<ItemStack>, slot: Int) = object : AbstractItem() {
+	fun filterSlotProvider(entry: FilterEntry<ItemStack, FilterMeta.ItemFilterMeta>) = object : AbstractItem() {
 		override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
 			val cursor = event.cursor
 
@@ -109,8 +99,6 @@ class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack>,
 			notifyWindows()
 			updateSlotOverlay(event.view)
 
-			@Suppress("UNCHECKED_CAST")
-			(data.entries as MutableList<FilterEntry<ItemStack>>)[slot] = entry
 			FilterManager.save(commandBlock.get(), data)
 		}
 
@@ -120,45 +108,21 @@ class ItemFilterGui(val viewer: Player, private val data: FilterData<ItemStack>,
 		}
 	}
 
-	fun strictnessSlotProvider(entry: FilterEntry<ItemStack>, slot: Int) = object : AbstractItem() {
+	fun strictnessSlotProvider(entry: FilterEntry<ItemStack, FilterMeta.ItemFilterMeta>) = object : AbstractItem() {
 		override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-			strictess[slot] = !(strictess.getOrDefault(slot, true))
+			val currentIndex = entry.metaData.filterMethod.ordinal
+			val size = FilterMethod.entries.size
+			val nextIndex = (currentIndex + 1) % size
+
+			entry.metaData.filterMethod = FilterMethod.entries[nextIndex]
 
 			notifyWindows()
 			updateSlotOverlay(event.view)
 
-			@Suppress("UNCHECKED_CAST")
-			(data.entries as MutableList<FilterEntry<ItemStack>>)[slot] = entry
 			FilterManager.save(commandBlock.get(), data)
 		}
 
 		override fun getItemProvider(viewer: Player): ItemProvider = provider
-		val provider = ItemProvider { getStrictnessItem(strictess.getOrDefault(slot, true)) }
-	}
-
-	fun getStrictnessItem(strict: Boolean): ItemStack {
-		val base = POWER_DRILL_BASIC.constructItemStack().updatePersistentDataContainer {
-			set(NamespacedKeys.CUSTOM_ITEM, PersistentDataType.STRING, "USELESS")
-		}
-
-		base.updateData(DataComponentTypes.HIDE_ADDITIONAL_TOOLTIP, true)
-		base.updateData(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes().build())
-
-		if (strict) {
-			POWER_DRILL_BASIC.getComponent(CustomComponentTypes.POWER_STORAGE).setPower(POWER_DRILL_BASIC, base, 25000)
-			base.updateDisplayName(text("Strict item checks"))
-			base.updateLore(mutableListOf(
-				text("All item data will be matched.")
-			))
-
-			return base
-		}
-
-		base.updateDisplayName(text("Loose item checks"))
-		base.updateLore(mutableListOf(
-			text("Only item IDs will be matched.")
-		))
-
-		return base
+		val provider = ItemProvider { entry.metaData.filterMethod.icon }
 	}
 }
