df2910be3bda1a69995218e15af96a162eb3b171
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-26 21:47:47 -0600

    Use filters when pathfinding

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/ItemTransportCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/ItemTransportCache.kt
@@ -1,6 +1,7 @@
 package net.horizonsend.ion.server.features.transport.nodes.cache
 
 import net.horizonsend.ion.common.extensions.information
+import net.horizonsend.ion.common.extensions.serverError
 import net.horizonsend.ion.server.command.misc.TransportDebugCommand
 import net.horizonsend.ion.server.command.misc.TransportDebugCommand.measureOrFallback
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.highlightBlock
@@ -12,6 +13,7 @@ import net.horizonsend.ion.server.features.transport.manager.extractors.data.Ite
 import net.horizonsend.ion.server.features.transport.manager.holders.CacheHolder
 import net.horizonsend.ion.server.features.transport.nodes.types.ItemNode
 import net.horizonsend.ion.server.features.transport.nodes.types.ItemNode.SolidGlassNode
+import net.horizonsend.ion.server.features.transport.nodes.types.Node
 import net.horizonsend.ion.server.features.transport.util.CacheType
 import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
 import net.horizonsend.ion.server.miscellaneous.utils.STAINED_GLASS_PANE_TYPES
@@ -25,9 +27,13 @@ import net.minecraft.core.BlockPos
 import net.minecraft.world.Container
 import org.bukkit.Material
 import org.bukkit.Material.CRAFTING_TABLE
+import org.bukkit.block.BlockFace
+import org.bukkit.block.data.type.CommandBlock
+import org.bukkit.block.data.type.Hopper
 import org.bukkit.craftbukkit.block.impl.CraftGrindstone
 import org.bukkit.craftbukkit.inventory.CraftInventory
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 class ItemTransportCache(holder: CacheHolder<ItemTransportCache>): TransportCache(holder) {
 	override val type: CacheType = CacheType.ITEMS
@@ -40,6 +46,8 @@ class ItemTransportCache(holder: CacheHolder<ItemTransportCache>): TransportCach
 		.addSimpleNode(Material.GLASS_PANE, ItemNode.PaneGlassNode(ItemNode.PipeChannel.CLEAR))
 		.addSimpleNode(Material.TINTED_GLASS, ItemNode.WildcardSolidGlassNode)
 		.addDataHandler<CraftGrindstone>(Material.GRINDSTONE) { data, key -> ItemNode.ItemMergeNode }
+		.addDataHandler<CommandBlock>(CustomBlocks.ITEM_FILTER) { data, key -> ItemNode.AdvancedFilterNode(key, this) }
+		.addDataHandler<Hopper>(Material.HOPPER) { data, key -> ItemNode.HopperFilterNode(key, data.facing, this) }
 		.addSimpleNode(
 			Material.CHEST,
 			Material.TRAPPED_CHEST,
@@ -56,8 +64,6 @@ class ItemTransportCache(holder: CacheHolder<ItemTransportCache>): TransportCach
 		delta: Double,
 		metaData: ExtractorMetaData?,
 	) {
-//		return
-
 		NewTransport.executor.submit {
 			measureOrFallback(TransportDebugCommand.extractorTickTimes) {
 				handleExtractorTick(location, delta, metaData as? ItemExtractorMetaData)
@@ -73,18 +79,68 @@ class ItemTransportCache(holder: CacheHolder<ItemTransportCache>): TransportCach
 			return
 		}
 
-		val destinations: List<BlockKey> = getNetworkDestinations<ItemNode.InventoryNode>(location) { node ->
-			val inventory = getInventory(node.position)
-			inventory != null && inventory.isEmpty //TODO full
-		}.toList()
+		val items = sources.flatMap { inv -> inv }.filterNotNull()
+		if (items.isEmpty()) return
 
-		val destination = if (meta != null) distributionOrder.getDestination(meta, destinations) else {
-			val extractorPosition = toVec3i(location)
-			destinations.minBy { key -> extractorPosition.distance(toVec3i(key)) }
+		val byCount = mutableMapOf<ItemStack, Int>()
+
+		for (item in items) {
+			val asOne = item.asOne()
+			if (byCount.containsKey(asOne)) continue
+			val count = items.sumOf { stack -> if (stack.isSimilar(item)) stack.amount else 0 }
+			byCount[asOne] = count
 		}
 
-		debugAudience.highlightBlock(toVec3i(destination), 40L)
-		debugAudience.information("Destinations: ${destinations.size}")
+		debugAudience.information("counts: [${byCount.entries.joinToString { "${it.key.type}, ${it.value}]" }}, ${toVec3i(location)}")
+
+		for ((item, count) in byCount) {
+			debugAudience.information("Checking ${item.type} [$count]")
+			val destinations: List<BlockKey> = getNetworkDestinations<ItemNode.InventoryNode>(location) { node ->
+				val inventory = getInventory(node.position)
+				inventory != null && inventory.isEmpty //TODO full
+			}.toList()
+
+			if (destinations.isEmpty()) {
+				debugAudience.information("No destinations found")
+				continue
+			}
+
+			val numDestinations = destinations.size
+
+			val paths: Array<PathfindingReport?> = measureOrFallback(TransportDebugCommand.pathfindTimes) { Array(numDestinations) {
+				getOrCachePath(
+					Node.NodePositionData(
+						ItemNode.ItemExtractorNode,
+						holder.getWorld(),
+						location,
+						BlockFace.SELF
+					),
+					destinations[it]
+				) { node, blockFace ->
+					if (node !is ItemNode.FilterNode) return@getOrCachePath true
+					debugAudience.serverError("checking filter")
+					node.matches(item)
+				}
+			} }
+
+			val validDestinations = destinations.filterIndexed { index, _ ->
+				paths[index] != null
+			}
+
+			if (validDestinations.isEmpty()) {
+				debugAudience.serverError("Could not find valid destination.")
+				println(paths.toList())
+			}
+
+			val destination = if (meta != null) {
+				distributionOrder.getDestination(meta, validDestinations)
+			} else {
+				val extractorPosition = toVec3i(location)
+				destinations.minBy { key -> extractorPosition.distance(toVec3i(key)) }
+			}
+
+			debugAudience.highlightBlock(toVec3i(destination), 40L)
+		}
 	}
 
 	fun getInventory(key: BlockKey): Inventory? {
