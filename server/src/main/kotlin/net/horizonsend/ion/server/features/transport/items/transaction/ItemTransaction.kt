f6258b2f61105db589cdd94e82091de35cf7489c
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-01-30 14:20:50 -0600

    Back item transfers with removals from origin

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/items/transaction/ItemTransaction.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/items/transaction/ItemTransaction.kt
@@ -1,63 +1,22 @@
 package net.horizonsend.ion.server.features.transport.items.transaction
 
-import net.horizonsend.ion.server.features.transport.items.transaction.Change.ItemAddition
-import net.horizonsend.ion.server.features.transport.items.transaction.Change.ItemRemoval
-import net.horizonsend.ion.server.features.transport.manager.holders.CacheHolder
-import net.horizonsend.ion.server.features.transport.nodes.cache.ItemTransportCache
-import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
-import net.horizonsend.ion.server.miscellaneous.utils.multimapOf
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
-class ItemTransaction(val originCacheHolder: CacheHolder<ItemTransportCache>) {
-	val removals = multimapOf<BlockKey, ItemRemoval>()
-	val additions = multimapOf<BlockKey, ItemAddition>()
+class ItemTransaction() {
+	val transactions = mutableListOf<BackedItemTransaction>()
 
-	fun addRemoval(sourcePos: BlockKey, diff: ItemRemoval) {
-		removals[sourcePos].add(diff)
-	}
-
-	fun addAddition(sourcePos: BlockKey, diff: ItemAddition) {
-		additions[sourcePos].add(diff)
-	}
-
-	data class CommitContext(val key: BlockKey, val inventory: Inventory, val change: Change) {
-		fun check() = change.check(inventory)
-		fun commit() = change.execute(inventory)
+	fun addTransfer(sourceReference: ItemReference, destinationInventory: Inventory, transferredItem: ItemStack, transferredAmount: Int) {
+		transactions += BackedItemTransaction(sourceReference, transferredItem, transferredAmount, destinationInventory)
 	}
 
 	fun commit() {
-		val inventories = mutableMapOf<BlockKey, Inventory?>()
-
-		val removalContexts = mutableListOf<CommitContext>()
-		val additionContexts = mutableListOf<CommitContext>()
-
-		val getInventory = fun(key: BlockKey): Inventory? {
-			return inventories.getOrPut(key) { originCacheHolder.cache.getInventory(key) }
-		}
-
-		removals.entries().mapNotNullTo(removalContexts) { entry ->
-			val inventory = getInventory(entry.key) ?: return@mapNotNullTo null
-			CommitContext(entry.key, inventory, entry.value)
-		}
-
-		additions.entries().mapNotNullTo(additionContexts) { entry ->
-			val inventory = getInventory(entry.key) ?: return@mapNotNullTo null
-			CommitContext(entry.key, inventory, entry.value)
-		}
-
-		if (!checkAll(removalContexts)) return
-		if (!checkAll(additionContexts)) return
-
-		for (removal in removalContexts) {
-			removal.commit()
-		}
-
-		for (addition in additionContexts) {
-			addition.commit()
-		}
+		transactions
+			.filter { transaction -> transaction.check() }
+			.forEach { t -> t.execute() }
 	}
 
-	fun checkAll(contexts: Collection<CommitContext>): Boolean {
-		return contexts.all { context -> context.check() }
+	fun checkAll(): Boolean {
+		return transactions.all { transaction -> transaction.check() }
 	}
 }
