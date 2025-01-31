a6d048ba47f6d3d373195f3b1db48e50261156cb
kwazedilla <kwazedilla@gmail.com>
2025-01-30 16:50:50 -0800

    WIP: First pass at multiblock descriptions

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/defense/passive/areashield/AreaShield.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/defense/passive/areashield/AreaShield.kt
@@ -8,12 +8,15 @@ import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultibloc
 import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.SimplePoweredEntity
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
+import net.horizonsend.ion.server.features.multiblock.type.DisplayNameMultilblock
 import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
 import net.horizonsend.ion.server.features.multiblock.type.InteractableMultiblock
 import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getSphereBlocks
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
 import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.World
@@ -23,10 +26,15 @@ import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
 import java.util.concurrent.TimeUnit
 
-abstract class AreaShield(val radius: Int) : Multiblock(), EntityMultiblock<AreaShield.AreaShieldEntity>, InteractableMultiblock {
+abstract class AreaShield(val radius: Int) : Multiblock(), EntityMultiblock<AreaShield.AreaShieldEntity>, InteractableMultiblock, DisplayNameMultilblock {
 	override fun onTransformSign(player: Player, sign: Sign) {
 		player.success("Area Shield created.")
 	}
+
+	override val displayName: Component
+		get() = text("Area Shield (Radius $radius)")
+	override val description: Component
+		get() = text("Prevents explosions within a $radius radius.")
 
 	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) {
 		val blocks: List<Vec3i> = getSphereBlocks(radius)
