bbc4509ae1b3dd58b95826437427eed44757fa41
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-01 11:53:09 -0600

    Improve world edit integration

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/MultiblockEntities.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/MultiblockEntities.kt
@@ -1,16 +1,17 @@
 package net.horizonsend.ion.server.features.multiblock
 
 import com.destroystokyo.paper.event.server.ServerTickEndEvent
+import net.horizonsend.ion.server.IonServerComponent
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.features.multiblock.entity.type.LegacyMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.type.EntityMultiblock
-import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.horizonsend.ion.server.features.world.chunk.IonChunk
-import net.horizonsend.ion.server.listener.SLEventListener
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.MULTIBLOCK_ENTITY_DATA
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getX
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getY
@@ -19,18 +20,22 @@ import net.horizonsend.ion.server.miscellaneous.utils.getFacing
 import org.bukkit.World
 import org.bukkit.block.Block
 import org.bukkit.block.Sign as SignState
-import org.bukkit.block.data.type.WallSign as SignData
+import org.bukkit.block.data.type.WallSign as WallSignData
 import org.bukkit.event.EventHandler
 
 /**
  * Provides utility functions for multiblock entities, and handles sign backups
  **/
-object MultiblockEntities : SLEventListener() {
+object MultiblockEntities : IonServerComponent() {
+	override fun onEnable() {
+		Tasks.asyncRepeat(20L, 20L, ::performCleaup)
+	}
+
 	/**
 	 *
 	 **/
 	fun getMultiblockEntity(world: World, x: Int, y: Int, z: Int): MultiblockEntity? {
-		val ionChunk = getIonChunk(world, x, z) ?: return null
+		val ionChunk = IonChunk.getFromWorldCoordinates(world, x, z) ?: return null
 
 		return ionChunk.multiblockManager[x, y, z]
 	}
@@ -42,7 +47,7 @@ object MultiblockEntities : SLEventListener() {
 		val x = getX(key)
 		val y = getY(key)
 		val z = getZ(key)
-		val ionChunk = getIonChunk(world, x, z) ?: return null
+		val ionChunk = IonChunk.getFromWorldCoordinates(world, x, z) ?: return null
 
 		return ionChunk.multiblockManager[x, y, z]
 	}
@@ -56,12 +61,12 @@ object MultiblockEntities : SLEventListener() {
 		return getMultiblockEntity(sign.world, origin.x, origin.y, origin.z)
 	}
 
-	fun getMultiblockEntity(signLocation: BlockKey, world: World, sign: SignData): MultiblockEntity? {
+	fun getMultiblockEntity(signLocation: BlockKey, world: World, sign: WallSignData): MultiblockEntity? {
 		val origin = getRelative(signLocation, sign.facing.oppositeFace)
 		return getMultiblockEntity(world, getX(origin), getY(origin), getZ(origin))
 	}
 
-	fun getMultiblockEntity(signX: Int, signY: Int, signZ: Int, world: World, sign: SignData): MultiblockEntity? {
+	fun getMultiblockEntity(signX: Int, signY: Int, signZ: Int, world: World, sign: WallSignData): MultiblockEntity? {
 		val structureDirection = sign.facing.oppositeFace
 		return getMultiblockEntity(world, signX + structureDirection.modX, signY + structureDirection.modY, signZ + structureDirection.modZ)
 	}
@@ -70,7 +75,7 @@ object MultiblockEntities : SLEventListener() {
 	 * Add a new multiblock entity to the chunk
 	 **/
 	fun setMultiblockEntity(world: World, x: Int, y: Int, z: Int, createEntity: (MultiblockManager) -> MultiblockEntity): MultiblockEntity? {
-		val ionChunk = getIonChunk(world, x, z) ?: return null
+		val ionChunk = IonChunk.getFromWorldCoordinates(world, x, z) ?: return null
 
 		val manager = ionChunk.multiblockManager
 
@@ -84,16 +89,19 @@ object MultiblockEntities : SLEventListener() {
 	 *
 	 **/
 	fun removeMultiblockEntity(world: World, x: Int, y: Int, z: Int): MultiblockEntity? {
-		val ionChunk = getIonChunk(world, x, z) ?: return null
+		val ionChunk = IonChunk.getFromWorldCoordinates(world, x, z) ?: return null
 
 		return ionChunk.multiblockManager.removeMultiblockEntity(x, y, z)
 	}
 
-	private fun getIonChunk(world: World, x: Int, z: Int): IonChunk? {
-		val chunkX = x.shr(4)
-		val chunkZ = z.shr(4)
+	/**
+	 *
+	 **/
+	fun removeMultiblockEntity(world: World, signX: Int, signY: Int, signZ: Int, wallSignData: WallSignData): MultiblockEntity? {
+		val (x, y, z) = Vec3i(signX, signY, signZ).getRelative(wallSignData.facing.oppositeFace)
+		val ionChunk = IonChunk.getFromWorldCoordinates(world, x, z) ?: return null
 
-		return world.ion.getChunk(chunkX, chunkZ)
+		return ionChunk.multiblockManager.removeMultiblockEntity(x, y, z)
 	}
 
 	fun <T: MultiblockEntity> loadFromData(multiblock: EntityMultiblock<T>, manager: MultiblockManager, stored: PersistentMultiblockData): T {
@@ -124,7 +132,7 @@ object MultiblockEntities : SLEventListener() {
 	fun migrateFromSign(sign: SignState, type: EntityMultiblock<*>) {
 		val origin = MultiblockEntity.getOriginFromSign(sign)
 
-		val ionChunk = getIonChunk(sign.world, origin.x, origin.z) ?: return
+		val ionChunk = IonChunk.getFromWorldCoordinates(sign.world, origin.x, origin.z) ?: return
 		val new = ionChunk.multiblockManager.handleNewMultiblockEntity(type, origin.x, origin.y, origin.z, sign.getFacing().oppositeFace)
 
 		if (new is LegacyMultiblockEntity) new.loadFromSign(sign)
@@ -145,6 +153,11 @@ object MultiblockEntities : SLEventListener() {
 
 			manager.markSignSaved()
 		}
+	}
 
+	fun performCleaup() {
+		MultiblockTicking.iterateManagers {
+
+		}
 	}
 }
