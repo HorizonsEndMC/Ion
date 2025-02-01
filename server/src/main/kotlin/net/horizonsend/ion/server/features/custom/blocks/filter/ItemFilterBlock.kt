5cd6ea92139470be35764e3f4989187dbd88317e
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-01 12:19:38 -0600

    Replace command blocks with creaking hearts

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/filter/ItemFilterBlock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/filter/ItemFilterBlock.kt
@@ -9,27 +9,27 @@ import net.horizonsend.ion.server.features.transport.filters.FilterData
 import net.horizonsend.ion.server.features.transport.filters.FilterMeta.ItemFilterMeta
 import net.horizonsend.ion.server.features.transport.filters.FilterType
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import org.bukkit.Axis
 import org.bukkit.Material
 import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
-import org.bukkit.block.CommandBlock
+import org.bukkit.block.data.type.CreakingHeart
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.function.Supplier
 
 object ItemFilterBlock : CustomFilterBlock<ItemStack, ItemFilterMeta>(
 	identifier = "ITEM_FILTER",
-	blockData = Material.COMMAND_BLOCK.createBlockData { t ->
-		t as org.bukkit.block.data.type.CommandBlock
-		t.facing = BlockFace.UP
-		t.isConditional = true
+	blockData = Material.CREAKING_HEART.createBlockData { t ->
+		t as CreakingHeart
+		t.axis = Axis.Z
+		t.isNatural = false
+		t.isActive = false
 	},
 	drops = BlockLoot(
 		requiredTool = { BlockLoot.Tool.PICKAXE },
 		drops = customItemDrop(CustomItemRegistry::ITEM_FILTER)
 	),
 	customBlockItem = CustomItemRegistry::ITEM_FILTER,
-
 ) {
 	override fun createData(pos: BlockKey): FilterData<ItemStack, ItemFilterMeta> {
 		return FilterData<ItemStack, ItemFilterMeta>(pos, FilterType.ItemType)
@@ -39,8 +39,8 @@ object ItemFilterBlock : CustomFilterBlock<ItemStack, ItemFilterMeta>(
 		player: Player,
 		block: Block,
 		filterData: FilterData<ItemStack, ItemFilterMeta>,
-		commandBlock: Supplier<CommandBlock>
+		tileState: Supplier<org.bukkit.block.TileState>
 	): GuiWrapper {
-		return ItemFilterGui(player, filterData, commandBlock)
+		return ItemFilterGui(player, filterData, tileState)
 	}
 }
