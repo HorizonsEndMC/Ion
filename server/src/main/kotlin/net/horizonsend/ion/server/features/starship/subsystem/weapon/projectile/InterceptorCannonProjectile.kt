1a54f1879c57b735dbbc873d08bebef1c8a592ff
kwazedilla <kwazedilla@gmail.com>
2025-02-01 18:38:45 -0800

    Revert "forgot to remove another file"
    This reverts commit 35dc869d77dd32f9f40be1be4c20b2997faa4964.

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/InterceptorCannonProjectile.kt
@@ -0,0 +1,36 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
+
+import net.horizonsend.ion.server.configuration.ConfigurationFiles
+import net.horizonsend.ion.server.configuration.StarshipWeapons
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.kyori.adventure.text.Component
+import org.bukkit.Color
+import org.bukkit.Location
+import org.bukkit.util.Vector
+
+class InterceptorCannonProjectile(
+	starship: ActiveStarship?,
+	name: Component,
+	loc: Location,
+	dir: Vector,
+	shooter: Damager
+) : LaserProjectile(starship, name, loc, dir, shooter) {
+	override val balancing: StarshipWeapons.ProjectileBalancing = starship?.balancing?.weapons?.interceptorCannon ?: ConfigurationFiles.starshipBalancing().nonStarshipFired.interceptorCannon
+	override val range: Double = balancing.range
+	override val speed: Double = balancing.speed
+	override val starshipShieldDamageMultiplier = balancing.starshipShieldDamageMultiplier
+	override val areaShieldDamageMultiplier: Double = balancing.areaShieldDamageMultiplier
+	override val color: Color = Color.ORANGE
+	override val particleThickness: Double = balancing.particleThickness
+	override val explosionPower: Float = balancing.explosionPower
+	override val volume: Int = balancing.volume
+	override val pitch: Float = balancing.pitch
+	override val soundName: String = balancing.soundName
+
+	override fun onImpactStarship(starship: ActiveStarship, impactLocation: Location) {
+		if (starship.initialBlockCount < 1000) {
+			impactLocation.createExplosion(12.0f)
+		}
+	}
+}
