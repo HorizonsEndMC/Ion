129675f849dddb04558a0f4ee8cbd84a424396f9
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-05 12:03:22 -0600

    add directional custom blocks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/misc/InteractableCustomBlock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/misc/InteractableCustomBlock.kt
@@ -1,19 +1,9 @@
 package net.horizonsend.ion.server.features.custom.blocks.misc
 
-import net.horizonsend.ion.server.features.custom.blocks.BlockLoot
-import net.horizonsend.ion.server.features.custom.blocks.CustomBlock
-import net.horizonsend.ion.server.features.custom.items.type.CustomBlockItem
 import org.bukkit.block.Block
-import org.bukkit.block.data.BlockData
 import org.bukkit.event.player.PlayerInteractEvent
-import java.util.function.Supplier
 
-abstract class InteractableCustomBlock(
-    identifier: String,
-    blockData: BlockData,
-    drops: BlockLoot,
-    customBlockItem: Supplier<CustomBlockItem>
-) : CustomBlock(identifier, blockData, drops, customBlockItem) {
-	open fun onRightClick(event: PlayerInteractEvent, block: Block) {}
-	open fun onLeftClick(event: PlayerInteractEvent, block: Block) {}
+interface InteractableCustomBlock {
+	fun onRightClick(event: PlayerInteractEvent, block: Block) {}
+	fun onLeftClick(event: PlayerInteractEvent, block: Block) {}
 }
