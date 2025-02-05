0d2129a99e7e8bd26b23a6348208ee08383f710b
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-05 15:21:19 -0600

    convert new custom blocks to vaults, disable vault mechanics

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/filter/ItemFilterBlock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/filter/ItemFilterBlock.kt
@@ -1,8 +1,8 @@
 package net.horizonsend.ion.server.features.custom.blocks.filter
 
 import net.horizonsend.ion.server.features.custom.blocks.BlockLoot
-import net.horizonsend.ion.server.features.custom.blocks.CustomBlock
 import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks.customItemDrop
+import net.horizonsend.ion.server.features.custom.blocks.misc.DirectionalCustomBlock
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry
 import net.horizonsend.ion.server.features.gui.GuiWrapper
 import net.horizonsend.ion.server.features.gui.custom.filter.ItemFilterGui
@@ -11,23 +11,57 @@ import net.horizonsend.ion.server.features.transport.filters.FilterMeta.ItemFilt
 import net.horizonsend.ion.server.features.transport.filters.FilterType
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
-import org.bukkit.Axis
 import org.bukkit.Material
 import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
 import org.bukkit.block.TileState
-import org.bukkit.block.data.type.CreakingHeart
+import org.bukkit.block.Vault as VaultState
+import org.bukkit.block.data.type.Vault as VaultData
+import org.bukkit.craftbukkit.block.CraftVault
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.function.Supplier
 
-object ItemFilterBlock : CustomBlock(
+object ItemFilterBlock : DirectionalCustomBlock(
 	identifier = "ITEM_FILTER",
-	blockData = Material.CREAKING_HEART.createBlockData { t ->
-		t as CreakingHeart
-		t.axis = Axis.Z
-		t.isNatural = false
-		t.isActive = false
-	},
+	faceData = mapOf(
+		BlockFace.NORTH to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.NORTH
+			t.isOminous = true
+			t.vaultState = VaultData.State.INACTIVE
+		},
+		BlockFace.SOUTH to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.SOUTH
+			t.isOminous = true
+			t.vaultState = VaultData.State.INACTIVE
+		},
+		BlockFace.EAST to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.EAST
+			t.isOminous = true
+			t.vaultState = VaultData.State.INACTIVE
+		},
+		BlockFace.WEST to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.WEST
+			t.isOminous = true
+			t.vaultState = VaultData.State.INACTIVE
+		},
+		BlockFace.UP to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.NORTH
+			t.isOminous = true
+			t.vaultState = VaultData.State.EJECTING
+		},
+		BlockFace.DOWN to Material.VAULT.createBlockData { t ->
+			t as VaultData
+			t.facing = BlockFace.NORTH
+			t.isOminous = true
+			t.vaultState = VaultData.State.UNLOCKING
+		}
+	),
 	drops = BlockLoot(
 		requiredTool = { BlockLoot.Tool.PICKAXE },
 		drops = customItemDrop(CustomItemRegistry::ITEM_FILTER)
@@ -35,7 +69,7 @@ object ItemFilterBlock : CustomBlock(
 	customBlockItem = CustomItemRegistry::ITEM_FILTER,
 ), CustomFilterBlock<ItemStack, ItemFilterMeta> {
 	override fun createData(pos: BlockKey): FilterData<ItemStack, ItemFilterMeta> {
-		return FilterData<ItemStack, ItemFilterMeta>(pos, FilterType.ItemType)
+		return FilterData(pos, FilterType.ItemType)
 	}
 
 	override fun getGui(
@@ -51,9 +85,12 @@ object ItemFilterBlock : CustomBlock(
 		val storedFilterData = placedItem.persistentDataContainer.get(NamespacedKeys.FILTER_DATA, FilterData) ?: return
 
 		val state = block.state
-		if (state !is TileState) return
+		if (state !is VaultState) return
+		state as CraftVault
 
 		state.persistentDataContainer.set(NamespacedKeys.FILTER_DATA, FilterData, storedFilterData)
+		state.tileEntity.sharedData
+
 		state.update()
 	}
 }
