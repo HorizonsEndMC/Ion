3ae9d5e84745d014a9776e2b45135e6d7afbe57d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-10 23:49:29 -0600

    Add rotation buttons

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/ShipFactoryGui.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/ShipFactoryGui.kt
@@ -11,6 +11,7 @@ import net.horizonsend.ion.server.features.gui.custom.blueprint.BlueprintMenu
 import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.TextInputMenu.Companion.anvilInputText
 import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.validator.InputValidator
 import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.validator.ValidatorResult
+import net.horizonsend.ion.server.features.gui.item.EnumScrollButton
 import net.horizonsend.ion.server.features.gui.item.FeedbackItem
 import net.horizonsend.ion.server.features.gui.item.FeedbackItem.FeedbackItemResult
 import net.horizonsend.ion.server.features.gui.item.ValueScrollButton
@@ -18,6 +19,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.ShadowColor
+import net.minecraft.world.level.block.Rotation
 import org.bukkit.entity.Player
 import org.litote.kmongo.eq
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
@@ -28,14 +30,12 @@ class ShipFactoryGui(private val viewer: Player, val entity: ShipFactoryEntity) 
 	private fun isValid(): Boolean = entity.isAlive
 
 	override fun open() {
-		if (!isValid()) return
-
 		val gui = Gui.normal()
 			.setStructure(
 				"s s s s s i . . .",
-				"x y z . ^ ^ . d .",
+				"x y z . R L . d .",
 				". . . . . . . . .",
-				"X Y Z . v v . . .",
+				"X Y Z . r l . . .",
 				"B A M R . P . e .",
 				". 1 3 6 . I . . ."
 			)
@@ -47,8 +47,10 @@ class ShipFactoryGui(private val viewer: Player, val entity: ShipFactoryEntity) 
 			.addIngredient('X', ValueScrollButton(GuiItem.UP.makeItem(Component.text("Decrease X offset")), false, { entity.settings.offsetX },-1, -100..100) { entity.settings.offsetX = it })
 			.addIngredient('Y', ValueScrollButton(GuiItem.UP.makeItem(Component.text("Decrease Y offset")), false, { entity.settings.offsetY },-1, -100..100) { entity.settings.offsetY = it })
 			.addIngredient('Z', ValueScrollButton(GuiItem.UP.makeItem(Component.text("Decrease Z offset")), false, { entity.settings.offsetZ },-1, -100..100) { entity.settings.offsetZ = it })
-			.addIngredient('^', GuiItems.CustomControlItem(Component.text("up"), GuiItem.UP))
-			.addIngredient('v', GuiItems.CustomControlItem(Component.text("down"), GuiItem.DOWN))
+			.addIngredient('R', EnumScrollButton(GuiItem.UP.makeItem(Component.text("Rotate 90 degrees right")), 1, { entity.settings.offsetZ }, Rotation::class.java, { it.name.toComponent() }) { entity.settings.offsetZ = it })
+			.addIngredient('r', EnumScrollButton(GuiItem.UP.makeItem(Component.text("Rotate 90 degrees right")), 1, { entity.settings.offsetY }, Rotation::class.java, { it.name.toComponent() }) { entity.settings.offsetY = it })
+			.addIngredient('L', EnumScrollButton(GuiItem.UP.makeItem(Component.text("Rotate 90 degrees left")), 1, { entity.settings.offsetZ }, Rotation::class.java, { it.name.toComponent() }) { entity.settings.offsetZ = it })
+			.addIngredient('l', EnumScrollButton(GuiItem.UP.makeItem(Component.text("Rotate 90 degrees left")), 1, { entity.settings.offsetY }, Rotation::class.java, { it.name.toComponent() }) { entity.settings.offsetY = it })
 			.addIngredient('B', GuiItems.CustomControlItem(Component.text("outline"), GuiItem.OUTLINE))
 			.addIngredient('A', GuiItems.CustomControlItem(Component.text("align"), GuiItem.ALIGN))
 			.addIngredient('M', GuiItems.CustomControlItem(Component.text("materials"), GuiItem.MATERIALS))
@@ -60,6 +62,8 @@ class ShipFactoryGui(private val viewer: Player, val entity: ShipFactoryEntity) 
 			.addIngredient('I', GuiItems.CustomControlItem(Component.text("item settings"), GuiItem.GEAR))
 			.addIngredient('d', disableButton)
 			.addIngredient('e', enableButton)
+		if (!isValid()) return
+
 
 		Window.single()
 			.setGui(gui)
