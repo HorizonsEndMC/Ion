daf063d8691bd79e69a5c9ca158c16c5dbce7f70
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-13 20:15:02 -0600

    Rework to use NMS

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
@@ -87,6 +87,7 @@ import net.horizonsend.ion.server.features.transport.NewTransport
 import net.horizonsend.ion.server.features.transport.filters.FilterType
 import net.horizonsend.ion.server.features.tutorial.Tutorials
 import net.horizonsend.ion.server.features.waypoint.WaypointManager
+import net.horizonsend.ion.server.features.world.generation.feature.nms.IonStructureTypes
 import net.horizonsend.ion.server.features.world.IonWorld
 import net.horizonsend.ion.server.features.world.data.DataFixers
 import net.horizonsend.ion.server.listener.misc.WorldEditListener
@@ -96,6 +97,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.Notify
 
 val components: List<IonComponent> = listOf(
 	GameplayTweaks,
+	IonStructureTypes,
 	DBManager,
 	RedisActions,
 	AutoRestart,
