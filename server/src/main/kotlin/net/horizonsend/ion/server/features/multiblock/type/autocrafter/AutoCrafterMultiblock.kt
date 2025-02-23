a785585e9252bf0fc56fd5c909c36f7bc67be3da
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-23 12:59:56 -0600

    auto crafter cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/autocrafter/AutoCrafterMultiblock.kt
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.common.utils.text.orEmpty
+import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
 import net.horizonsend.ion.server.features.client.display.modular.display.PowerEntityDisplayModule
 import net.horizonsend.ion.server.features.client.display.modular.display.StatusDisplayModule
@@ -38,6 +39,7 @@ import org.bukkit.craftbukkit.inventory.CraftItemStack as CBItemStack
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import java.util.Optional
+import java.util.concurrent.atomic.AtomicInteger
 
 private const val POWER_USAGE_PER_INGREDIENT = 15
 
@@ -118,8 +120,8 @@ abstract class AutoCrafterMultiblock(
 		null
 	)
 
-	companion object {															  /* Items */
-		val recipeCache: LoadingCache<List<ItemStack?>, Optional<ItemStack>> = CacheBuilder.newBuilder().build(
+	companion object {
+		private val recipeCache: LoadingCache<List<ItemStack?>, Optional<ItemStack>> = CacheBuilder.newBuilder().build(
 			CacheLoader.from { items ->
 				requireNotNull(items)
 				val level = Bukkit.getWorlds().first().minecraft
@@ -166,104 +168,153 @@ abstract class AutoCrafterMultiblock(
 			val output: Inventory = getOutput() ?: return sleepWithStatus(text("Not Intact", RED), 500)
 
 			// material data of each item in the recipe holder, used as the crafting transportNetwork
-			val grid: List<ItemStack?> = recipeHolder.toList()
+			val grid: List<ItemStack?> = recipeHolder.contents.toList()
 
-			val basePower = powerStorage.getPower()
-			if (basePower < POWER_USAGE_PER_INGREDIENT) return sleepWithStatus(text("Low Power", RED), 250)
-
-			var power = basePower
+			val startPower = powerStorage.getPower()
+			if (startPower < POWER_USAGE_PER_INGREDIENT) return sleepWithStatus(text("Low Power", RED), 250)
 
 			// result item of this recipe
-			val result: ItemStack = recipeCache[grid].orElse(null)?.clone() ?: return sleepWithStatus(text("Invalid Recipe", RED), 200)
+			val result = recipeCache[grid].orElse(null)?.clone()
+
+			if (result == null) {
+				sleepWithStatus(text("Invalid Recipe", RED), 200)
+				return
+			}
 
 			val powerUsage = grid.filterNotNull().distinct().count() * POWER_USAGE_PER_INGREDIENT
+			var power = startPower
 
-			try { for (iteration in (1..multiblock.iterations)) {
-				if (power < powerUsage) return sleepWithStatus(text("Low Power", RED), 250)
-
-				val removeSlots = mutableListOf<Int>() // can be multiple times per slot, so list, not set
-				var requiredIngredients = 0
-				var matchedIngredients = 0
-
-				// for each slot in the crafting transportNetwork,
-				// if it's not null,
-				// increment required ingredients to keep track of how many are needed,
-				// and loop through the input inventory,
-				// if an item's data matches the ingredient,
-				// flag that slot for removal,
-				// increment matched ingredients,
-				// and move on to the next ingredient
-				ingredientLoop@
-				for (ingredient: ItemStack? in grid) {
-					if (ingredient != null) {
-						requiredIngredients++
-						for ((index: Int, item: ItemStack?) in inputInventory.withIndex()) {
-							// if it matches AND we haven't already taken too much from it, use it
-							if (item?.isSimilar(ingredient) == true && item.amount >= removeSlots.count { it == index } + 1) {
-								removeSlots += index
-								matchedIngredients++
-								continue@ingredientLoop
-							}
-						}
-					}
-				}
-
-				// stop iterating if not all of the ingredients were found
-				if (matchedIngredients != requiredIngredients) return
-
-				val remaining: HashMap<Int, ItemStack> = output.addItem(result)
-
-				if (remaining.isNotEmpty()) {
-					val added = result.amount - remaining.values.sumOf { it.amount }
-					check(added >= 0)
-
-					if (added > 0) output.removeItem(result.clone().apply { amount = added })
-
-					return sleepWithStatus(text("Output Full", RED), 100)
-				}
-
-				power -= powerUsage
-
-				// below code leaves incomplete stacks, but might be faster
-				/*// attempt to add the item to the output inventory (we already checked that we can remove it from the input)
-				var fit = false
-				for ((index: Int, item: ItemStack?) in output.inventory.contents.withIndex()) {
-					if (item == null) {
-						output.inventory.setItem(index, result)
-						fit = true
-						break
-					} else if(item.isSimilar(result) && item.amount + result.amount <= item.maxStackSize) {
-						item.amount += result.amount
-						fit = true
+			try {
+				for (iteration in (1..multiblock.iterations)) {
+					if (power < powerUsage) {
+						sleepWithStatus(text("Low Power", RED), 150)
 						break
 					}
-				}
 
-				if (!fit) {
-					return
-				}*/
+					val consumed = runCraftingIteration(powerUsage, grid, inputInventory, output, result)
+					if (consumed == -1) break
 
-				// remove the items
-				for (index in removeSlots) {
-					// since AFAIK recipes only call for one item per ingredient, just decrement the amount
-					// it will automatically remove the item if the amount hits 0
-					inputInventory.getItem(index)!!.amount--
+					power -= consumed
 				}
-			} } finally {
-				if (basePower != power) {
+			}
+			catch (e: Throwable) {
+				IonServer.slF4JLogger.warn("Auto crafter execution threw exception!")
+				e.printStackTrace()
+			}
+			finally {
+				if (startPower != power) {
 					powerStorage.setPower(power)
-				} else {
 					val newHash = result.hashCode()
+
 					if (resultHash != newHash) {
 						// Skip re-computing the display name, small but adds up with big factories
 						resultHash = newHash
 						// Nothing crafted, could be temporary resource shortage, pause for shorter time period
-						sleepWithStatus(result.displayName(), 200)
-					}  else {
-						tickingManager.sleepForTicks(200)
+						statusManager.setStatus(result.displayName())
 					}
+				} else {
+					tickingManager.sleepForTicks(200)
 				}
 			}
+		}
+
+		/**
+		 * Runs an iteration of crafting, the number of iterations depends on the tier of them multiblock
+		 * Returns power used
+		 **/
+		private fun runCraftingIteration(
+			powerUsage: Int,
+			grid: List<ItemStack?>,
+			inputInventory: Inventory,
+			outputInventory: Inventory,
+			result: ItemStack
+		): Int {
+			val removeSlots = mutableMapOf<Int, AtomicInteger>() // can be multiple times per slot, so list, not set
+			var requiredIngredients = 0
+			var matchedIngredients = 0
+
+			// for each slot in the crafting grid,
+			ingredientLoop@
+			for (ingredient: ItemStack? in grid) {
+				// if it's not null,
+				if (ingredient == null) continue
+
+				// increment required ingredients to keep track of how many are needed,
+				requiredIngredients++
+
+				// Loop through the input inventory
+				for ((index: Int, item: ItemStack?) in inputInventory.withIndex()) {
+					// if it matches AND we haven't already taken too much from it, use it
+					if (item == null) continue
+
+					// if an item's data matches the ingredient,
+					if (!item.isSimilar(ingredient)) continue
+
+					// Boxed int to keep track
+					val atomic = removeSlots.getOrPut(index) { AtomicInteger() }
+
+					// And has enough
+					if (item.amount < atomic.get() + 1) continue
+
+					// flag that slot for removal,
+					atomic.incrementAndGet()
+
+					// Increment matched ingredient.
+					// This will only be called once per ingredient, since at this point it will continue onto the next one.
+					matchedIngredients++
+
+					// and move on to the next ingredient
+					continue@ingredientLoop
+				}
+			}
+
+			// stop iterating if not all the ingredients were found
+			if (matchedIngredients != requiredIngredients) {
+				return 0
+			}
+
+			val remaining: HashMap<Int, ItemStack> = outputInventory.addItem(result.clone())
+
+			if (remaining.isNotEmpty()) {
+				// Only 1 ingredient is added, it will be the 0 index of the param.
+				val notAdded = remaining[0]!!.amount
+				check(notAdded >= 0)
+
+				// Remove the amount that was actually added, since the ingredients will not be consumed
+				val toRemove = result.amount - notAdded
+				outputInventory.removeItem(result.asQuantity(toRemove))
+
+				sleepWithStatus(text("Output Full", RED), 100)
+				return -1
+			}
+
+			// below code leaves incomplete stacks, but might be faster
+			/*// attempt to add the item to the output inventory (we already checked that we can remove it from the input)
+			var fit = false
+			for ((index: Int, item: ItemStack?) in output.inventory.contents.withIndex()) {
+				if (item == null) {
+					output.inventory.setItem(index, result)
+					fit = true
+					break
+				} else if(item.isSimilar(result) && item.amount + result.amount <= item.maxStackSize) {
+					item.amount += result.amount
+					fit = true
+					break
+				}
+			}
+
+			if (!fit) {
+				return
+			}*/
+
+			// remove the items
+			for ((index, count) in removeSlots) {
+				// it will automatically remove the item if the amount hits 0
+				inputInventory.getItem(index)!!.amount -= count.get()
+				println("Consuming ${count.get()}")
+			}
+
+			return powerUsage
 		}
 
 		override fun loadFromSign(sign: Sign) {
