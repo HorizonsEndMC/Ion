639390eddbd6f621e00872a653add701e07b4305
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-27 13:54:28 -0600

    update advanced ship factory multi

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/AdvancedShipFactoryMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/AdvancedShipFactoryMultiblock.kt
@@ -48,38 +48,6 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 	override val description: Component get() = text("Print starships and other structures with materials and credits. Provides more configuration settings.")
 
 	override fun MultiblockShape.buildStructure() {
-		z(2) {
-			y(-1) {
-				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).anyGlass()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
-			}
-			y(0) {
-				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).anyGlass()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
-			}
-		}
-		z(1) {
-			y(-1) {
-				x(2).anyGlass()
-				x(1).anyGlass()
-				x(0).sponge()
-				x(-1).anyGlass()
-				x(-2).anyGlass()
-			}
-			y(0) {
-				x(2).anyPipedInventory()
-				x(1).sponge()
-				x(0).type(Material.LODESTONE)
-				x(-1).sponge()
-				x(-2).anyPipedInventory()
-			}
-		}
 		z(0) {
 			y(-1) {
 				x(2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = STRAIGHT))
@@ -94,6 +62,38 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 				x(0).anyGlass()
 				x(-1).ironBlock()
 				x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+			}
+		}
+		z(1) {
+			y(-1) {
+				x(2).type(Material.LODESTONE)
+				x(1).sponge()
+				x(0).sponge()
+				x(-1).sponge()
+				x(-2).type(Material.LODESTONE)
+			}
+			y(0) {
+				x(2).anyPipedInventory()
+				x(1).sponge()
+				x(0).anyGlass()
+				x(-1).sponge()
+				x(-2).anyPipedInventory()
+			}
+		}
+		z(2) {
+			y(-1) {
+				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
+				x(1).ironBlock()
+				x(0).anyGlass()
+				x(-1).ironBlock()
+				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
+			}
+			y(0) {
+				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+				x(1).ironBlock()
+				x(0).anyGlass()
+				x(-1).ironBlock()
+				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
 			}
 		}
 	}
@@ -145,9 +145,9 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 
 		private val pipeInputOffsets = arrayOf(
 			Vec3i(0, 0, 0),
+			Vec3i(0, 0, 1),
 			Vec3i(0, 0, 2),
-			Vec3i(-2, -1, 1),
-			Vec3i(+2, -1, 1),
+			Vec3i(0, -1, 2)
 		)
 
 		private val inventoryOffsets = arrayOf(
