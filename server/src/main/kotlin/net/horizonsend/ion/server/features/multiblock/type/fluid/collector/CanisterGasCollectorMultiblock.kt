6e3b6c278393af3cb3feb0f91710c5c3ad1f416e
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-02-28 01:16:34 -0600

    Modernize canister gas collectors

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/collector/CanisterGasCollectorMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/collector/CanisterGasCollectorMultiblock.kt
@@ -1,8 +1,9 @@
 package net.horizonsend.ion.server.features.multiblock.type.fluid.collector
 
 import net.horizonsend.ion.common.extensions.information
-import net.horizonsend.ion.server.configuration.ConfigurationFiles
 import net.horizonsend.ion.server.configuration.ConfigurationFiles.globalGassesConfiguration
+import net.horizonsend.ion.server.features.client.display.modular.DisplayHandlers
+import net.horizonsend.ion.server.features.client.display.modular.display.StatusDisplayModule
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry
 import net.horizonsend.ion.server.features.custom.items.CustomItemRegistry.customItem
 import net.horizonsend.ion.server.features.custom.items.type.GasCanister
@@ -12,6 +13,10 @@ import net.horizonsend.ion.server.features.gas.type.Gas
 import net.horizonsend.ion.server.features.multiblock.Multiblock
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
+import net.horizonsend.ion.server.features.multiblock.entity.type.DisplayMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.FurnaceBasedMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.StatusMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.StatusTickedMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.SyncTickingMultiblockEntity
 import net.horizonsend.ion.server.features.multiblock.entity.type.ticked.TickedMultiblockEntityParent.TickingManager
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
@@ -28,6 +33,8 @@ import net.horizonsend.ion.server.miscellaneous.utils.rightFace
 import net.horizonsend.ion.server.miscellaneous.utils.weightedRandomOrNull
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.BLUE
+import net.kyori.adventure.text.format.NamedTextColor.RED
 import org.bukkit.Material
 import org.bukkit.World
 import org.bukkit.block.BlockFace
@@ -77,56 +84,92 @@ object CanisterGasCollectorMultiblock : Multiblock(), EntityMultiblock<CanisterG
 		z: Int,
 		world: World,
 		structureDirection: BlockFace,
-	) : MultiblockEntity(manager, CanisterGasCollectorMultiblock, world, x, y, z, structureDirection), SyncTickingMultiblockEntity {
-		val configuration get() = ConfigurationFiles.globalGassesConfiguration()
+	) : MultiblockEntity(manager, CanisterGasCollectorMultiblock, world, x, y, z, structureDirection), SyncTickingMultiblockEntity, FurnaceBasedMultiblockEntity, DisplayMultiblockEntity, StatusTickedMultiblockEntity {
+		val configuration get() = globalGassesConfiguration()
 		override val tickingManager: TickingManager = TickingManager(20)
+		override val statusManager: StatusMultiblockEntity.StatusManager = StatusMultiblockEntity.StatusManager()
 
 		override val inputsData: InputsData = none()
 
+		override val displayHandler = DisplayHandlers.newMultiblockSignOverlay(
+			this,
+			{ StatusDisplayModule(it, statusManager) }
+		).register()
+
 		override fun tick() {
-			val furnaceInventory = getInventory(0, 0, 0) as? FurnaceInventory ?: return
-			if (!Gasses.isCanister(furnaceInventory.fuel)) return
+			val furnaceInventory = getFurnaceInventory()
+			if (furnaceInventory == null) {
+				sleepWithStatus(text("Not intact!", RED), configuration.collectorTickInterval)
+				return
+			}
+
+			if (!Gasses.isCanister(furnaceInventory.fuel)) {
+				sleepWithStatus(text("No canister.", RED), configuration.collectorTickInterval)
+				return
+			}
 
 			tickingManager.sleepForTicks(configuration.collectorTickInterval)
-
-			furnaceInventory.holder?.burnTime = configuration.collectorTickInterval.toShort()
-			furnaceInventory.holder?.update()
+			setBurningForTicks(configuration.collectorTickInterval)
 
 			tickCollector(furnaceInventory)
 		}
 
 		private fun tickCollector(furnaceInventory: FurnaceInventory) {
-			val hopperInventory = getInventory(0, 0, 1) ?: return
+			val hopperInventory = getInventory(0, 0, 1)
+			if (hopperInventory == null) {
+				sleepWithStatus(text("Not intact!", RED), configuration.collectorTickInterval)
+				return
+			}
 
 			// Weight gas output based on the number of lightning rods
-			val weight = arrayOf(structureDirection.rightFace, structureDirection.leftFace, BlockFace.UP, BlockFace.DOWN).count { face ->
-				getOrigin().getRelativeIfLoaded(face)?.type == Material.LIGHTNING_ROD
-			}.toDouble() / 4.0
+			val weight = arrayOf(structureDirection.rightFace, structureDirection.leftFace, BlockFace.UP, BlockFace.DOWN)
+				.count { face ->
+					getOrigin().getRelativeIfLoaded(face)?.type == Material.LIGHTNING_ROD
+				}.toDouble().div(4.0)
 
 			val worldConfiguration = world.ion.configuration.gasConfiguration.gasses.shuffled(ThreadLocalRandom.current())
 			val availableGasses = worldConfiguration.map { it.tryCollect(location) }.filter { it.amount > 0 }
 
-			val random = availableGasses.weightedRandomOrNull { result: CollectedGas.CollectionResult -> result.amount.toDouble() } ?: return
+			val random = availableGasses.weightedRandomOrNull { result: CollectedGas.CollectionResult -> result.amount.toDouble() }
+			if (random == null) {
+				sleepWithStatus(text("No gasses available.", RED), configuration.collectorTickInterval)
+				return
+			}
 
 			val delta = globalGassesConfiguration().collectorTickInterval / 20L
 			val amount = (random.amount * weight) * (delta)
 
-			Tasks.sync { tryHarvestGas(furnaceInventory, hopperInventory, random.gas, amount.roundToInt()) }
+			Tasks.sync {
+				tryHarvestGas(furnaceInventory, hopperInventory, random.gas, amount.roundToInt())
+			}
 		}
 
-		private fun tryHarvestGas(furnaceInventory: FurnaceInventory, hopperInventory: Inventory, gas: Gas, amount: Int): Boolean {
-			val canisterItem = furnaceInventory.fuel ?: return false
-			val customItem = canisterItem.customItem ?: return false
+		private fun tryHarvestGas(furnaceInventory: FurnaceInventory, hopperInventory: Inventory, gas: Gas, amount: Int) {
+			val canisterItem = furnaceInventory.fuel
+			if (canisterItem == null) {
+				sleepWithStatus(text("No canister.", RED), configuration.collectorTickInterval)
+				return
+			}
 
-			return when (customItem) {
+			val customItem = canisterItem.customItem
+			if (customItem == null) {
+				sleepWithStatus(text("Invalid canister.", RED), configuration.collectorTickInterval)
+				return
+			}
+
+			when (customItem) {
 				CustomItemRegistry.GAS_CANISTER_EMPTY -> fillEmptyCanister(furnaceInventory, gas, amount)
 				is GasCanister -> fillGasCanister(canisterItem, furnaceInventory, hopperInventory, amount) // Don't even bother with the gas
-				else -> false
 			}
 		}
 
 		private fun fillEmptyCanister(furnaceInventory: FurnaceInventory, gas: Gas, amount: Int): Boolean {
-			val newType = CustomItemRegistry.getByIdentifier(gas.containerIdentifier) as? GasCanister ?: return false
+			val newType = CustomItemRegistry.getByIdentifier(gas.containerIdentifier) as? GasCanister
+			if (newType == null) {
+				sleepWithStatus(text("Invalid canister.", RED), configuration.collectorTickInterval)
+				return false
+			}
+
 			val newCanister = newType.createWithFill(amount)
 
 			furnaceInventory.fuel = newCanister
@@ -134,15 +177,15 @@ object CanisterGasCollectorMultiblock : Multiblock(), EntityMultiblock<CanisterG
 			return true
 		}
 
-		private fun fillGasCanister(canisterItem: ItemStack, furnaceInventory: FurnaceInventory, hopperInventory: Inventory, amount: Int): Boolean {
-			val type = canisterItem.customItem ?: return false
-			if (type !is GasCanister) return  false
+		private fun fillGasCanister(canisterItem: ItemStack, furnaceInventory: FurnaceInventory, hopperInventory: Inventory, amount: Int) {
+			val type = canisterItem.customItem ?: return
+			if (type !is GasCanister) return
 
 			val currentFill = type.getFill(canisterItem)
 			val newFill = currentFill + amount
 
 			// If the canister would be filled
-			return if (newFill >= type.maximumFill) {
+			if (newFill >= type.maximumFill) {
 				// Try to add a full canister to the hopper
 				val canAdd = hopperInventory.addItem(type.constructItemStack())
 
@@ -153,17 +196,13 @@ object CanisterGasCollectorMultiblock : Multiblock(), EntityMultiblock<CanisterG
 				} else {
 					// Put a full one in its spot
 					furnaceInventory.fuel = type.constructItemStack()
-
-					return false
 				}
-
-				true
 			} else {
 				// If it's completely not filled, just fill it to the new level
 				type.setFill(canisterItem, newFill)
-
-				true
 			}
+
+			setStatus(text("Running", BLUE))
 		}
 	}
 }
