e65c8b05e9e65aff15fe0c2e919c2f1afa6920df
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-06 15:28:54 -0600

    rename standard asteroid feature to configurable

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/FeatureRegistry.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/FeatureRegistry.kt
@@ -5,7 +5,7 @@ import org.bukkit.NamespacedKey
 object FeatureRegistry {
 	val features = mutableMapOf<NamespacedKey, GeneratedFeature<*>>()
 
-	val ASTEROID = register(StandardAsteroidFeature)
+	val ASTEROID = register(ConfigurableAsteroidFeature)
 
 	fun <T: GeneratedFeature<*>> register(feature: T): T {
 		features[feature.key] = feature
