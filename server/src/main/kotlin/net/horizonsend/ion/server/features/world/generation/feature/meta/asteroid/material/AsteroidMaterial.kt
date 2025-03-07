971856439fa904960c7534ab298a00ece10185a6
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-07 14:10:15 -0600

    organization, add in random and weighted random materials

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/material/AsteroidMaterial.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/material/AsteroidMaterial.kt
@@ -1,7 +1,10 @@
 package net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.material
 
 import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.ConfigurableAsteroidMeta
+import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise.IterativeValueProvider
 import net.horizonsend.ion.server.miscellaneous.utils.nms
+import net.horizonsend.ion.server.miscellaneous.utils.weightedEntry
+import net.horizonsend.ion.server.miscellaneous.utils.weightedRandom
 import net.minecraft.world.level.block.state.BlockState
 import org.bukkit.Material
 
@@ -23,5 +26,49 @@ interface AsteroidMaterial {
 	class SimpleMaterial(val state: BlockState) : AsteroidMaterial {
 		constructor(material: Material) : this(material.createBlockData().nms)
 		override fun getValue(x: Double, y: Double, z: Double, distanceSquared: Double, meta: ConfigurableAsteroidMeta): BlockState = state
+	}
+}
+
+class NoiseAsteroidMaterial(
+	val meta: ConfigurableAsteroidMeta,
+	val noiseProvider: IterativeValueProvider,
+	val blocks: List<AsteroidMaterial.WeightedMaterial>
+) : AsteroidMaterial {
+	private fun getBlock(
+		noiseValue: Double,
+		x: Double,
+		y: Double,
+		z: Double,
+		distanceSquared: Double,
+		meta: ConfigurableAsteroidMeta
+	): BlockState {
+		return blocks.weightedEntry(noiseValue) { it.weight }.material.getValue(x, y, z, distanceSquared, meta)
+	}
+
+	override fun getValue(
+		x: Double,
+		y: Double,
+		z: Double,
+		distanceSquared: Double,
+		meta: ConfigurableAsteroidMeta,
+	): BlockState {
+		val noiseValue = noiseProvider.getValue(x, y, z, meta)
+		return getBlock(noiseValue, x, y, z, distanceSquared, meta)
+	}
+}
+
+class RamdomWeightedAsteroidMaterial(
+	val blocks: List<AsteroidMaterial.WeightedMaterial>
+) : AsteroidMaterial {
+	override fun getValue(x: Double, y: Double, z: Double, distanceSquared: Double, meta: ConfigurableAsteroidMeta): BlockState {
+		return blocks.weightedRandom(meta.random) { it.weight }.getValue(x, y, z, distanceSquared, meta)
+	}
+}
+
+class RamdomAsteroidMaterial(
+	val blocks: List<AsteroidMaterial>
+) : AsteroidMaterial {
+	override fun getValue(x: Double, y: Double, z: Double, distanceSquared: Double, meta: ConfigurableAsteroidMeta): BlockState {
+		return blocks.random(meta.random).getValue(x, y, z, distanceSquared, meta)
 	}
 }
