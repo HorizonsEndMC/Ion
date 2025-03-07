ed291d3f013667a8d5b0e27110b2de2338ea40ae
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-07 13:48:23 -0600

    separate config versions of the evaluations, more material work

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidConfiguration.kt
@@ -5,21 +5,6 @@ import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroi
 
 @Serializable
 data class AsteroidConfiguration(
-	val startValue: StartValue,
 	val noiseLayers: List<NoiseConfiguration>
 ) {
-	enum class StartValue {
-		ASTEROID_SIZE {
-			override fun getValue(meta: ConfigurableAsteroidMeta): Double {
-				return meta.size
-			}
-		},
-		NONE {
-			override fun getValue(meta: ConfigurableAsteroidMeta): Double {
-				return 0.0
-			}
-		};
-
-		abstract fun getValue(meta: ConfigurableAsteroidMeta): Double
-	}
 }
