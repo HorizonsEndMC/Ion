bfdf2c878eff9a3b5bc3bb2f4b7b3d90639d667d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-08 12:38:21 -0600

    rename

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidSpecification.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidSpecification.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise.NoiseConfiguration
 
 @Serializable
-data class AsteroidConfiguration(
+data class AsteroidSpecification(
 	val noiseLayers: List<NoiseConfiguration>
 ) {
 }
