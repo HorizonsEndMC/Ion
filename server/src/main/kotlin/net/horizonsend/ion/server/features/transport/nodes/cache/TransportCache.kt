9ac5c75b42460a2b7bfa132fbc8e7b11e00dbf84
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-09 21:39:57 -0500

    move pathfinding report

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/TransportCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/cache/TransportCache.kt
@@ -249,23 +249,4 @@ abstract class TransportCache(open val holder: CacheHolder<*>) {
 		return destinations
 	}
 
-	data class PathfindingReport(val traversedNodes: Array<NodePositionData>, val resistance: Double) {
-		override fun equals(other: Any?): Boolean {
-			if (this === other) return true
-			if (javaClass != other?.javaClass) return false
-
-			other as PathfindingReport
-
-			if (resistance != other.resistance) return false
-			if (!traversedNodes.contentEquals(other.traversedNodes)) return false
-
-			return true
-		}
-
-		override fun hashCode(): Int {
-			var result = resistance.hashCode()
-			result = 31 * result + traversedNodes.contentHashCode()
-			return result
-		}
-	}
 }
