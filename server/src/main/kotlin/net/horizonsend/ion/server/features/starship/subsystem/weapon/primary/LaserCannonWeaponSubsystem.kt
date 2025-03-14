6ab40eb88e74dea8d026ee6bb566934c8bfdce68
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-14 12:32:21 -0500

    fix ship weapon damage types

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/LaserCannonWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/LaserCannonWeaponSubsystem.kt
@@ -2,7 +2,7 @@ package net.horizonsend.ion.server.features.starship.subsystem.weapon.primary
 
 import net.horizonsend.ion.server.command.admin.debug
 import net.horizonsend.ion.server.configuration.StarshipWeapons
-import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.cannon.LaserCannonStarshipWeaponMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.starship.weapon.cannon.LaserCannonStarshipWeaponMultiblock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.damager.Damager
 import net.horizonsend.ion.server.features.starship.subsystem.weapon.CannonWeaponSubsystem
