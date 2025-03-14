78a5ebb509dadcda3395db6de17ecb9678f5741d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-13 21:41:27 -0500

    surface based noise capability

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidSpecification.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/AsteroidSpecification.kt
@@ -1,10 +1,10 @@
 package net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid
 
 import kotlinx.serialization.Serializable
-import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise.NoiseConfiguration
+import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise.NoiseConfiguration3d
 
 @Serializable
 data class AsteroidSpecification(
-	val noiseLayers: List<NoiseConfiguration>
+	val noiseLayers: List<NoiseConfiguration3d>
 ) {
 }
