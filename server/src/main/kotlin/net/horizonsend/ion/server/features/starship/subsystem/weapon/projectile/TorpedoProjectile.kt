4d32a50e6707a0e8638d43a52cbf0bba837eb467
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-14 12:32:21 -0500

    fix ship weapon damage types

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/TorpedoProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/TorpedoProjectile.kt
@@ -2,7 +2,7 @@ package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
 
 import net.horizonsend.ion.server.configuration.ConfigurationFiles
 import net.horizonsend.ion.server.configuration.StarshipWeapons
-import net.horizonsend.ion.server.features.multiblock.type.starshipweapon.heavy.TorpedoStarshipWeaponMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.starship.weapon.heavy.TorpedoStarshipWeaponMultiblock
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
 import net.horizonsend.ion.server.features.starship.damager.Damager
 import net.kyori.adventure.text.Component
