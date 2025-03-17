635c8191e170798936762543d41ce0250b6136c4
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-17 13:12:03 -0500

    Add accel to directer control

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/input/DirecterControlHandler.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/control/input/DirecterControlHandler.kt
@@ -4,6 +4,7 @@ import com.destroystokyo.paper.event.player.PlayerJumpEvent
 import net.horizonsend.ion.server.features.starship.StarshipType
 import net.horizonsend.ion.server.features.starship.control.controllers.player.PlayerController
 import net.horizonsend.ion.server.features.starship.control.movement.StarshipControl
+import net.horizonsend.ion.server.features.starship.movement.StarshipMovementException
 import net.horizonsend.ion.server.features.starship.movement.TranslateMovement
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.vectorToBlockFace
@@ -12,8 +13,26 @@ import org.bukkit.event.player.PlayerToggleSneakEvent
 import kotlin.math.roundToInt
 
 class DirecterControlHandler(controller: PlayerController) : PlayerMovementInputHandler(controller, "Direct-er") {
+	private var accel = 0
+	private var lastMove = System.currentTimeMillis()
+
+	private val MAX_ACCEL = 5
+
+	private fun tickAndGetAccel(): Int {
+		if (accel >= MAX_ACCEL) return MAX_ACCEL
+		accel++
+		lastMove = System.currentTimeMillis()
+		return accel
+	}
+
+	override fun onBlocked(reason: StarshipMovementException) {
+		accel = 0
+	}
+
 	override fun handleMove(event: PlayerMoveEvent) {
-		val delta = event.to.toVector().subtract(event.from.toVector()).normalize().multiply(2)
+		val accel = tickAndGetAccel()
+
+		val delta = event.to.toVector().subtract(event.from.toVector()).normalize().multiply(accel)
 
 		if (delta.length() > 0.0) {
 			val face = vectorToBlockFace(delta)
@@ -31,13 +50,20 @@ class DirecterControlHandler(controller: PlayerController) : PlayerMovementInput
 	override fun handleSneak(event: PlayerToggleSneakEvent) {}
 
 	override fun handleJump(event: PlayerJumpEvent) {
+		val accel = tickAndGetAccel()
+
 		event.isCancelled = true
-		move(Vec3i(0, 1, 0))
+		move(Vec3i(0, accel, 0))
 	}
 
 	override fun tick() {
 		val now = System.currentTimeMillis()
+
 		if (now - starship.lastManualMove < starship.manualMoveCooldownMillis) return
+
+		if (now - lastMove > 500 && accel >= 1) {
+			accel -= 1
+		}
 
 		if (controller.player.isSneaking) move(Vec3i(0, -1, 0))
 	}
