a0e25cd61d22e2cbcb7808465a9b3ce94e209ab1
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-19 01:05:27 -0500

    Make noise configurations able to be used beyond asteroids

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/noise/Evaluations.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/noise/Evaluations.kt
@@ -1,91 +1,86 @@
 package net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise
 
-import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.ConfigurableAsteroidMeta
-import net.horizonsend.ion.server.features.world.generation.feature.start.FeatureStart
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 
 data class Sum(val values: List<IterativeValueProvider>) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return values.sumOf { it.getFallbackValue(meta) }
+	override fun getFallbackValue(): Double {
+		return values.sumOf { it.getFallbackValue() }
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return values.sumOf { it.getValue(x, y, z, meta, start) }
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return values.sumOf { it.getValue(x, y, z, origin) }
 	}
 
 }
 
 data class Add(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return a.getFallbackValue(meta) + b.getFallbackValue(meta)
+	override fun getFallbackValue(): Double {
+		return a.getFallbackValue() + b.getFallbackValue()
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return a.getValue(x, y, z, meta, start) + b.getValue(x, y, z, meta, start)
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return a.getValue(x, y, z, origin) + b.getValue(x, y, z, origin)
 	}
 
 }
 
 data class Subtract(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return a.getFallbackValue(meta) - b.getFallbackValue(meta)
+	override fun getFallbackValue(): Double {
+		return a.getFallbackValue() - b.getFallbackValue()
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return a.getValue(x, y, z, meta, start) - b.getValue(x, y, z, meta, start)
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return a.getValue(x, y, z, origin) - b.getValue(x, y, z, origin)
 	}
 
 }
 
 data class Multiply(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return a.getFallbackValue(meta) * b.getFallbackValue(meta)
+	override fun getFallbackValue(): Double {
+		return a.getFallbackValue() * b.getFallbackValue()
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return a.getValue(x, y, z, meta, start) * b.getValue(x, y, z, meta, start)
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return a.getValue(x, y, z, origin) * b.getValue(x, y, z, origin)
 	}
 
 }
 
 data class Divide(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return a.getFallbackValue(meta) / b.getFallbackValue(meta)
+	override fun getFallbackValue(): Double {
+		return a.getFallbackValue() / b.getFallbackValue()
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return a.getValue(x, y, z, meta, start) / b.getValue(x, y, z, meta, start)
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return a.getValue(x, y, z, origin) / b.getValue(x, y, z, origin)
 	}
 
 }
 
 data class Min(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return minOf(a.getFallbackValue(meta,), b.getFallbackValue(meta))
+	override fun getFallbackValue(): Double {
+		return minOf(a.getFallbackValue(), b.getFallbackValue())
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return minOf(a.getValue(x, y, z, meta, start), b.getValue(x, y, z, meta, start))
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return minOf(a.getValue(x, y, z, origin), b.getValue(x, y, z, origin))
 	}
 
 }
 
 data class Max(val a: IterativeValueProvider, val b: IterativeValueProvider) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double {
-		return maxOf(a.getFallbackValue(meta), b.getFallbackValue(meta))
+	override fun getFallbackValue(): Double {
+		return maxOf(a.getFallbackValue(), b.getFallbackValue())
 	}
 
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double {
-		return maxOf(a.getValue(x, y, z, meta, start), b.getValue(x, y, z, meta, start))
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double {
+		return maxOf(a.getValue(x, y, z, origin), b.getValue(x, y, z, origin))
 	}
 
 }
 
 data class Static(val value: Double) : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double = value
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double = value
+	override fun getFallbackValue(): Double = value
+	override fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double = value
 }
 
-data object Size : IterativeValueProvider {
-	override fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double = meta.size
-	override fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double  = meta.size
-}
--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/noise/IterativeValueProvider.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/noise/IterativeValueProvider.kt
@@ -1,10 +1,9 @@
 package net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise
 
-import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.ConfigurableAsteroidMeta
-import net.horizonsend.ion.server.features.world.generation.feature.start.FeatureStart
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 
 interface IterativeValueProvider {
 	/** Gets a maximum value ignoring any state introduced by the coordinates. */
-	fun getFallbackValue(meta: ConfigurableAsteroidMeta): Double
-	fun getValue(x: Double, y: Double, z: Double, meta: ConfigurableAsteroidMeta, start: FeatureStart): Double
+	fun getFallbackValue(): Double
+	fun getValue(x: Double, y: Double, z: Double, origin: Vec3i): Double
 }
