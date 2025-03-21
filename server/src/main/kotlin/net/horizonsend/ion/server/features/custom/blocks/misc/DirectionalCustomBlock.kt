cbfc69035f2fc6d0c133561825666e7aa6b0d356
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-21 01:44:42 -0500

    typed registry keys

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/misc/DirectionalCustomBlock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/blocks/misc/DirectionalCustomBlock.kt
@@ -11,10 +11,10 @@ import org.bukkit.block.BlockFace
 import org.bukkit.block.data.BlockData
 
 abstract class DirectionalCustomBlock(
-	identifier: IonRegistryKey<CustomBlock>,
+	identifier: IonRegistryKey<CustomBlock, out CustomBlock>,
 	val faceData: Map<BlockFace, BlockData>,
 	drops: BlockLoot,
-	customBlockItem: IonRegistryKey<CustomItem>
+	customBlockItem: IonRegistryKey<CustomItem, out CustomItem>
 ) : CustomBlock(identifier, faceData[BlockFace.NORTH]!!, drops, customBlockItem) {
 	val bukkitFaceLookup = faceData.entries.associateTo(Object2ObjectOpenHashMap()) { entry -> entry.value to entry.key }
 	val nmsFaceLookup = faceData.entries.associateTo(Object2ObjectOpenHashMap()) { entry -> entry.value.nms to entry.key }
--- server/src/main/kotlin/net/horizonsend/ion/server/features/machine/GeneratorFuel.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/machine/GeneratorFuel.kt
@@ -8,7 +8,7 @@ import net.horizonsend.ion.server.features.machine.GeneratorFuel.values
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
-private fun customItem(customItem: IonRegistryKey<CustomItem>): ItemStack = customItem.getValue().constructItemStack()
+private fun customItem(customItem: IonRegistryKey<CustomItem, out CustomItem>): ItemStack = customItem.getValue().constructItemStack()
 private fun itemStack(material: Material): ItemStack = ItemStack(material, 1)
 
 enum class GeneratorFuel(private val item: ItemStack, val cooldown: Int, val power: Int) {
