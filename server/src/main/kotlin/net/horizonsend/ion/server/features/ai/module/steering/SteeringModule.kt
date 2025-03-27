4908f10b6d842a4b289535dec3a30f31f78ef7b8
Consuelo Ugarte <consuelougartem@gmail.com>
2025-03-27 12:59:34 -0700

    steering cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/steering/SteeringModule.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/steering/SteeringModule.kt
@@ -8,9 +8,6 @@ import net.horizonsend.ion.server.features.starship.movement.StarshipMovementExc
 import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
 import org.bukkit.util.Vector
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.math.exp
-import kotlin.math.ln
-import kotlin.math.pow
 
 /**
 Context steering based control for AI
@@ -69,7 +66,7 @@ abstract class SteeringModule(controller: AIController, val difficulty: Difficul
 
 	fun decision(thrustContext : ContextMap, headingContext : ContextMap) {
 		val heading = headingContext.maxDir().setY(0)
-		if (heading.lengthSquared() > 1e-4) heading.normalize()
+		heading.normalize()
 		val thrustMag = thrustContext.lincontext!!.interpolotedMax()
 		val thrust = thrustContext.maxDir().normalize()
 
@@ -83,14 +80,5 @@ abstract class SteeringModule(controller: AIController, val difficulty: Difficul
 		location?.let {
 			obstructions[location] = time
 		}
-	}
-
-	fun biasGain(x: Double, gain: Double, bias: Double): Double {
-		// Compute the bias term based on the target midpoint p
-		val b = ln(ln(0.5) / ln(bias))
-		// Compute the adjusted input z
-		val z = x.pow(exp(b))
-		// Apply the S-curve function
-		return z.pow(gain) / (z.pow(gain) + (1 - z).pow(gain))
 	}
 }
