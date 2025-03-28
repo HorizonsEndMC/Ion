a2ac9d391559d7a399228235513234abbf3b676b
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-28 14:32:10 -0500

    progress on migrating ship types

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/type/restriction/DetectionParameters.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/type/restriction/DetectionParameters.kt
@@ -1,15 +1,33 @@
 package net.horizonsend.ion.server.features.starship.type.restriction
 
-import net.horizonsend.ion.server.features.starship.FLYABLE_BLOCKS
-import org.bukkit.Material
-import java.util.EnumSet
+import kotlinx.serialization.Serializable
+import net.horizonsend.ion.server.configuration.StarshipBalancing
+import net.horizonsend.ion.server.core.registries.IonRegistryKey
+import net.horizonsend.ion.server.features.starship.type.StarshipType
 
+sealed interface DetectionParmeterHolder {
+	fun getDetectionParameters(): DetectionParameters
+}
+
+class BalancingProvided(val typeKey: IonRegistryKey<StarshipType<*>, StarshipType<StarshipBalancing>>): DetectionParmeterHolder {
+	override fun getDetectionParameters(): DetectionParameters {
+		return (typeKey.getValue().balancing as DetectionBalancing).parameters
+	}
+}
+
+interface DetectionBalancing {
+	val parameters: DetectionParameters
+}
+
+@Serializable
 class DetectionParameters(
 	val minSize: Int = 25,
 	val maxSize: Int = 100,
 	val containerPercent: Double,
 	val concretePercent: Double = 0.3,
-	val crateLimitMultiplier: Double,
-	val pilotableBlockList: EnumSet<Material> = FLYABLE_BLOCKS
-) {
+	val crateLimitMultiplier: Double
+) : DetectionParmeterHolder {
+	override fun getDetectionParameters(): DetectionParameters {
+		return this
+	}
 }
