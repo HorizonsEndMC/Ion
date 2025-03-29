2bd4118a45c03d26713a989af4fda12e7ec9f261
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-03-29 05:11:52 -0500

    migrate fluid system to registries

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/FluidRegistry.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/fluids/FluidRegistry.kt
@@ -1,6 +1,8 @@
 package net.horizonsend.ion.server.features.transport.fluids
 
-import net.horizonsend.ion.server.core.IonServerComponent
+import net.horizonsend.ion.server.core.registries.Registry
+import net.horizonsend.ion.server.core.registries.keys.FluidTypeKeys
+import net.horizonsend.ion.server.core.registries.keys.KeyRegistry
 import net.horizonsend.ion.server.features.gas.Gasses
 import net.horizonsend.ion.server.features.transport.fluids.properties.FluidCategory
 import net.horizonsend.ion.server.features.transport.fluids.types.GasFluid
@@ -9,43 +11,25 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor.BLUE
 import net.kyori.adventure.text.format.NamedTextColor.WHITE
-import java.util.concurrent.ConcurrentHashMap
 
-object FluidRegistry : IonServerComponent() {
-	private val fluids = mutableListOf<Fluid>()
-	private val byIdentifier = ConcurrentHashMap<String, Fluid>()
+class FluidTypeRegistry() : Registry<FluidType>("FLUID_TYPE") {
+	override val keySet: KeyRegistry<FluidType> = FluidTypeKeys
 
-	val EMPTY = register(object : Fluid("EMPTY") {
-		override val displayName: Component = text("Empty", WHITE)
-		override val categories: Array<FluidCategory> = arrayOf()
-	})
+	override fun boostrap() {
+		register(FluidTypeKeys.EMPTY, object : FluidType(FluidTypeKeys.EMPTY) {
+			override val displayName: Component = text("Empty", WHITE)
+			override val categories: Array<FluidCategory> = arrayOf()
+		})
 
-	val LOW_PRESSURE_HYDROGEN_GAS = register(GasFluid("LOW_PRESSURE_HYDROGEN_GAS") { Gasses.HYDROGEN })
-	val LOW_PRESSURE_NITROGEN_GAS = register(GasFluid("LOW_PRESSURE_NITROGEN_GAS") { Gasses.NITROGEN })
-	val LOW_PRESSURE_METHANE_GAS = register(GasFluid("LOW_PRESSURE_METHANE_GAS") { Gasses.METHANE })
-	val LOW_PRESSURE_OXYGEN_GAS = register(GasFluid("LOW_PRESSURE_OXYGEN_GAS") { Gasses.OXYGEN })
-	val LOW_PRESSURE_CHLORINE_GAS = register(GasFluid("LOW_PRESSURE_CHLORINE_GAS") { Gasses.CHLORINE })
-	val LOW_PRESSURE_FLUORINE_GAS = register(GasFluid("LOW_PRESSURE_FLUORINE_GAS") { Gasses.FLUORINE })
-	val LOW_PRESSURE_HELIUM_GAS = register(GasFluid("LOW_PRESSURE_HELIUM_GAS") { Gasses.HELIUM })
-	val LOW_PRESSURE_CARBON_DIOXIDE_GAS = register(GasFluid("LOW_PRESSURE_CARBON_DIOXIDE_GAS") { Gasses.CARBON_DIOXIDE })
+		register(FluidTypeKeys.HYDROGEN, GasFluid(FluidTypeKeys.HYDROGEN) { Gasses.HYDROGEN })
+		register(FluidTypeKeys.NITROGEN, GasFluid(FluidTypeKeys.NITROGEN) { Gasses.NITROGEN })
+		register(FluidTypeKeys.METHANE, GasFluid(FluidTypeKeys.METHANE) { Gasses.METHANE })
+		register(FluidTypeKeys.OXYGEN, GasFluid(FluidTypeKeys.OXYGEN) { Gasses.OXYGEN })
+		register(FluidTypeKeys.CHLORINE, GasFluid(FluidTypeKeys.CHLORINE) { Gasses.CHLORINE })
+		register(FluidTypeKeys.FLUORINE, GasFluid(FluidTypeKeys.FLUORINE) { Gasses.FLUORINE })
+		register(FluidTypeKeys.HELIUM, GasFluid(FluidTypeKeys.HELIUM) { Gasses.HELIUM })
+		register(FluidTypeKeys.CARBON_DIOXIDE, GasFluid(FluidTypeKeys.CARBON_DIOXIDE) { Gasses.CARBON_DIOXIDE })
 
-	val WATER = register(SimpleFluid("WATER", text("Water", BLUE), arrayOf()))
-
-	init {
-	    fluids.associateByTo(byIdentifier) { it.identifier }
+		register(FluidTypeKeys.WATER, SimpleFluid(FluidTypeKeys.WATER, text("Water", BLUE), arrayOf()))
 	}
-
-	fun <T: Fluid> register(fluid: T): T {
-		fluids.add(fluid)
-
-		fluid.categories.forEach { it.addMember(fluid) }
-
-		return fluid
-	}
-
-	operator fun get(identifier: String): Fluid? {
-		return byIdentifier[identifier]
-	}
-
-	fun getAll() = fluids
 }
