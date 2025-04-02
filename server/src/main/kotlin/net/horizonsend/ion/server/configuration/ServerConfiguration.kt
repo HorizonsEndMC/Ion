444a9e965b0cceb6dea4782d1c59c53b29eb3ad1
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-02 17:41:33 -0500

    Hold off on starship type for now

--- server/src/main/kotlin/net/horizonsend/ion/server/configuration/ServerConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/configuration/ServerConfiguration.kt
@@ -5,11 +5,12 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import net.horizonsend.ion.common.database.StarshipTypeDB
 import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.configuration.ServerConfiguration.AsteroidConfig.Palette
 import net.horizonsend.ion.server.configuration.util.Pos
-import net.horizonsend.ion.server.features.starship.type.StarshipType
+import net.horizonsend.ion.server.features.starship.StarshipType
 import net.horizonsend.ion.server.features.world.WorldSettings
 import net.horizonsend.ion.server.miscellaneous.utils.WeightedRandomList
-import net.horizonsend.ion.server.miscellaneous.utils.getValue
+import net.horizonsend.ion.server.miscellaneous.utils.actualType
 import net.horizonsend.ion.server.miscellaneous.utils.nms
 import net.horizonsend.ion.server.miscellaneous.utils.readSchematic
 import net.minecraft.world.level.block.state.BlockState
@@ -196,7 +197,7 @@ data class ServerConfiguration(
 		private val shipClass: StarshipTypeDB,
 		val lore: List<String>
 	) {
-		val shipType: StarshipType<*> get() = shipClass.getValue()
+		val shipType: StarshipType get() = shipClass.actualType
 
 		@Transient
 		val schematicFile = IonServer.dataFolder.resolve("sold_ships").resolve("$schematicName.schem")
--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/type/restriction/SubsystemRestrictions.kt
+++
@@ -1,9 +0,0 @@
-package net.horizonsend.ion.server.features.starship.type.restriction
-
-import net.horizonsend.ion.server.features.starship.Starship
-
-class SubsystemRestrictions(vararg val subsystemRestrictions: SubsystemRestriction) {
-	fun check(starship: Starship): Boolean {
-		return subsystemRestrictions.all { restriction -> restriction.check(starship) }
-	}
-}
