99fdb163f45e4f491b71837a6cafc0d46cc456c0
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-03 00:59:36 -0500

    pass in world data

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/FeaturePlacementConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/FeaturePlacementConfiguration.kt
@@ -4,11 +4,12 @@ import kotlinx.serialization.Serializable
 import net.horizonsend.ion.server.features.world.generation.feature.meta.FeatureMetaData
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.minecraft.world.level.ChunkPos
+import org.bukkit.World
 import kotlin.random.Random
 
 @Serializable
 sealed interface FeaturePlacementConfiguration<T: FeatureMetaData> {
 	val placementPriority: Int
 
-	fun generatePlacements(chunk: ChunkPos, random: Random): List<Pair<Vec3i, T>>
+	fun generatePlacements(world: World, chunk: ChunkPos, random: Random): List<Pair<Vec3i, T>>
 }
