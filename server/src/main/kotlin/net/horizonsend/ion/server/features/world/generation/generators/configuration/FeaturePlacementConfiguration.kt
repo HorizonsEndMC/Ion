d4711198f09df724763ca6821e20290505ecc2c4
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-03 14:33:16 -0500

    organize configuration

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/FeaturePlacementConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/FeaturePlacementConfiguration.kt
@@ -1,7 +1,9 @@
 package net.horizonsend.ion.server.features.world.generation.generators.configuration
 
 import kotlinx.serialization.Serializable
+import net.horizonsend.ion.server.features.world.generation.feature.GeneratedFeature
 import net.horizonsend.ion.server.features.world.generation.feature.meta.FeatureMetaData
+import net.horizonsend.ion.server.features.world.generation.feature.start.FeatureStart
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.minecraft.world.level.ChunkPos
 import org.bukkit.World
@@ -12,4 +14,8 @@ sealed interface FeaturePlacementConfiguration<T: FeatureMetaData> {
 	val placementPriority: Int
 
 	fun generatePlacements(world: World, chunk: ChunkPos, random: Random): List<Pair<Vec3i, T>>
+
+	fun getFeature(): GeneratedFeature<T>
+
+	fun buildStartsData(world: World, chunkPos: ChunkPos, random: Random): List<FeatureStart> = getFeature().buildStartsData(world, chunkPos, random, this)
 }
