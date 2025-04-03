5a9a903fe1f0b36ff68d4b60c736713a0f1a5ffd
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-03 00:48:07 -0500

    Allow placement configuration to handle the creation of metadata

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/AsteroidPlacementConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/AsteroidPlacementConfiguration.kt
@@ -1,16 +1,18 @@
 package net.horizonsend.ion.server.features.world.generation.generators.configuration
 
 import kotlinx.serialization.Serializable
+import net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.ConfigurableAsteroidMeta
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.minecraft.world.level.ChunkPos
+import org.bukkit.Material
 import kotlin.random.Random
 import kotlin.random.asJavaRandom
 
 @Serializable
-class AsteroidPlacementConfiguration() : FeaturePlacementConfiguration {
+class AsteroidPlacementConfiguration : FeaturePlacementConfiguration<ConfigurableAsteroidMeta> {
 	override val placementPriority: Int = 0
 
-	override fun generatePlacements(chunk: ChunkPos, random: Random): List<Vec3i> {
+	override fun generatePlacements(chunk: ChunkPos, random: Random): List<Pair<Vec3i, ConfigurableAsteroidMeta>> {
 		val density = 0.0612
 		val stdev = density * 4.0
 
@@ -20,19 +22,30 @@ class AsteroidPlacementConfiguration() : FeaturePlacementConfiguration {
 			.coerceAtLeast(0.0)
 			.toInt()
 
-		val list = mutableListOf<Vec3i>()
+		val list = mutableListOf<Pair<Vec3i, ConfigurableAsteroidMeta>>()
 
 		repeat(count) {
 			val chunkStartX = chunk.x.shl(4)
 			val chunkStartZ = chunk.z.shl(4)
 
-			list.add(Vec3i(
-				chunkStartX + random.nextInt(0, 15),
-				random.asJavaRandom().nextGaussian(84.0, 196 / 2.0).toInt().coerceIn(1, 383),
-				chunkStartZ + random.nextInt(0, 15)
-			))
+			val x = chunkStartX + random.nextInt(0, 15)
+			val z = chunkStartZ + random.nextInt(0, 15)
+
+			val meta = generateMetaData(random, x, z)
+
+			val y = random.asJavaRandom()
+				.nextGaussian(84.0, 196 / 2.0).toInt()
+				.coerceIn(1 + meta.totalDisplacement.toInt(), 383 - meta.totalDisplacement.toInt())
+
+			list.add(Vec3i(x, y, z) to meta)
 		}
 
 		return list
+	}
+
+	fun generateMetaData(chunkRandom: Random, x: Int, z: Int): ConfigurableAsteroidMeta {
+//		val material = Material.entries.filter { material -> material.isBlock }.random(chunkRandom)
+		// chunkRandom.nextDouble(5.0, 40.0)
+		return ConfigurableAsteroidMeta(chunkRandom.nextLong(), 150.0, Material.STONE)
 	}
 }
