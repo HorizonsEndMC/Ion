cdee42060de5e2b6a80f5f7c44bc9f201d2225e9
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-10 15:45:43 -0500

    replace old sink providers & other cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/destruction/RemoveBlockSink.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/destruction/RemoveBlockSink.kt
@@ -1,16 +1,16 @@
 package net.horizonsend.ion.server.features.starship.destruction
 
-import net.horizonsend.ion.server.features.nations.utils.playSoundInRadius
+import net.horizonsend.ion.common.utils.miscellaneous.testRandom
+import net.horizonsend.ion.server.features.custom.blocks.CustomBlock
+import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks
+import net.horizonsend.ion.server.features.custom.blocks.CustomBlocks.customBlock
+import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.active.ActiveStarship
-import net.horizonsend.ion.server.features.starship.subsystem.checklist.BattlecruiserReactorSubsystem
 import org.bukkit.Material
-import org.bukkit.Particle
 import org.bukkit.block.Block
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
+import org.bukkit.block.data.BlockData
 
-class RemoveBlockSink(starship: ActiveStarship, val checkRemove: (Block) -> Boolean) : StandardSinkProvider(starship) {
+class RemoveBlockSink(starship: ActiveStarship, val checkRemove: (Block) -> Boolean) : AdvancedSinkProvider(starship) {
 	override fun setup() {
 		starship.iterateBlocks { x, y, z ->
 			if (checkRemove(starship.world.getBlockAt(x, y, z))) {
@@ -18,42 +18,43 @@ class RemoveBlockSink(starship: ActiveStarship, val checkRemove: (Block) -> Bool
 			}
 		}
 
-		starship.balancing.sounds.explode?.let {
-			playSoundInRadius(starship.centerOfMass.toLocation(starship.world), 7_500.0, it.sound)
-		}
-
 		super.setup()
-	}
-
-	private var particleRadius = 0.0
-
-	private val reactor = starship.subsystems.filterIsInstance<BattlecruiserReactorSubsystem>().firstOrNull()
-	private val center = (reactor?.pos ?: starship.centerOfMass) .toLocation(starship.world)
-	private val world = starship.world
-
-	override fun tick() {
-		super.tick()
-
-		if (particleRadius >= MAX_PARTICLE_RADIUS) return
-
-		particleRadius += 3.5
-
-		val number = (2.0 * PI * particleRadius).toInt()
-
-		for (count in 0..number) {
-			// Get the fraction around the circle
-			val degrees = 360.0 - (360.0 * (count.toDouble() / number.toDouble()))
-			val radians = degrees * (PI / 180.0)
-			val xOffset = cos(radians) * particleRadius
-			val zOffset = sin(radians) * particleRadius
-
-			val newLoc = center.clone().add(xOffset, 0.0, zOffset)
-
-			world.spawnParticle(Particle.SONIC_BOOM, newLoc.x, newLoc.y, newLoc.z, 1, 0.0, 0.0, 0.0, 0.0, null,true)
-		}
 	}
 
 	companion object {
 		private const val MAX_PARTICLE_RADIUS = 100.0
+
+		fun withChance(chances: Map<BlockWrapper, Double>): (Starship) -> SinkProvider {
+			return { RemoveBlockSink(it, { block ->
+				val wrapped = BlockWrapper.getWrapper(block.blockData)
+				val chance = chances[wrapped] ?: return@RemoveBlockSink false
+				return@RemoveBlockSink testRandom(chance)
+			}) }
+		}
+
+		sealed interface BlockWrapper {
+			fun matches(block: Block): Boolean
+
+			class CustomBlockWrapper(val customBlock: CustomBlock) : BlockWrapper {
+				override fun matches(block: Block): Boolean {
+					return block.customBlock?.identifier == customBlock.identifier
+				}
+			}
+
+			class MaterialWrapper(val material: Material) : BlockWrapper {
+				override fun matches(block: Block): Boolean {
+					return block.type == material
+				}
+			}
+
+			companion object {
+				fun getWrapper(blockData: BlockData): BlockWrapper {
+					val customBlock = CustomBlocks.getByBlockData(blockData)
+					if (customBlock != null) return CustomBlockWrapper(customBlock)
+
+					return MaterialWrapper(blockData.material)
+				}
+			}
+		}
 	}
 }
