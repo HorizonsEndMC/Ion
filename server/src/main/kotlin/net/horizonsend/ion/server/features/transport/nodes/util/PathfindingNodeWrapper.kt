6746b17571068bf216231fb419b8ba159fb2eff4
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-20 01:26:09 -0500

    save some time on tracked path building

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/util/PathfindingNodeWrapper.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/util/PathfindingNodeWrapper.kt
@@ -9,6 +9,7 @@ import net.horizonsend.ion.server.features.transport.nodes.types.TrackedNode
  **/
 class PathfindingNodeWrapper(val node: Node.NodePositionData, parent: PathfindingNodeWrapper?) : Comparable<PathfindingNodeWrapper> {
 	private var depth: Int = (parent?.depth ?: 0) + 1
+	var interestingPath: Boolean = node.type is TrackedNode || (parent != null && parent.interestingPath)
 
 	var parent: PathfindingNodeWrapper? = parent
 		set(value) {
@@ -16,10 +17,19 @@ class PathfindingNodeWrapper(val node: Node.NodePositionData, parent: Pathfindin
 			depth = (value?.depth ?: 0) + 1
 		}
 
+	/**
+	 * @param retainfull If true, build a full path of every node pathed over, if false the list will only contain tracked nodes.
+	 **/
 	fun buildPath(retainfull: Boolean): Path {
+		// No tracked nodes on path, no need to compute the whole thing if the full path isn't requested
+		if (!interestingPath && !retainfull) {
+			return Path(depth, arrayOf())
+		}
+
 		val list = arrayListOf(this.node.position to this.node.type)
 		var current: PathfindingNodeWrapper? = this
 
+		// Iterate through each node's parents
 		while (current?.parent != null) {
 			current = current.parent!!
 
