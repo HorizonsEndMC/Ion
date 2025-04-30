ec82bf80d199b949ec3e3ccbcfaf22113560d2e5
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-30 18:35:02 -0500

    Main menu polishing

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGuiUtils.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGuiUtils.kt
@@ -1,15 +1,20 @@
 package net.horizonsend.ion.server.gui.invui.bazaar
 
+import com.mongodb.client.FindIterable
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.ItemAttributeModifiers
 import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.server.command.GlobalCompletions.fromItemString
 import net.horizonsend.ion.server.features.economy.city.CityNPCs
 import net.horizonsend.ion.server.features.economy.city.TradeCities
 import net.horizonsend.ion.server.features.economy.city.TradeCityData
+import net.horizonsend.ion.server.features.gui.item.AsyncItem
+import net.horizonsend.ion.server.features.nations.gui.playerClicker
 import net.horizonsend.ion.server.features.nations.region.Regions
 import net.horizonsend.ion.server.features.nations.region.types.RegionTerritory
 import net.horizonsend.ion.server.features.space.Space
 import net.horizonsend.ion.server.features.space.body.planet.CachedPlanet.Companion.DEFAULT_ITEM_FACTORY
-import net.horizonsend.ion.server.gui.invui.utils.makeGuiButton
+import net.horizonsend.ion.server.miscellaneous.utils.updateData
 import net.horizonsend.ion.server.miscellaneous.utils.updateDisplayName
 import net.horizonsend.ion.server.miscellaneous.utils.updateLore
 import net.kyori.adventure.text.Component
@@ -35,10 +40,11 @@ fun getCityButtons(
 		val territory: RegionTerritory = Regions[territoryId]
 
 		// attempt to get the planet icon, just use a detonator if unavailable
-		(Space.getPlanet(territory.world)?.planetIconFactory?.construct() ?: DEFAULT_ITEM_FACTORY.construct())
-			.updateDisplayName(nameBuilder(city))
-			.updateLore(loreBuilder(city))
-			.makeGuiButton { clickType, player -> clickHandler.invoke(city, clickType, player) }
+		AsyncItem({
+			(Space.getPlanet(territory.world)?.planetIconFactory?.construct() ?: DEFAULT_ITEM_FACTORY.construct())
+				.updateDisplayName(nameBuilder(city))
+				.updateLore(loreBuilder(city))
+		}) { event -> clickHandler.invoke(city, event.click, event.playerClicker) }
 	}
 }
 
@@ -46,17 +52,27 @@ fun getItemButtons(
 	filter: Bson,
 	sort: BazaarSort,
 	ascending: Boolean,
-	nameBuilder: (BazaarItem) -> Component = { item -> Component.text(item.itemString) },
-	loreBuilder: (BazaarItem) -> List<Component> = { listOf() },
-	clickHandler: (BazaarItem, ClickType, Player) -> Unit,
+	nameBuilder: ((String, FindIterable<BazaarItem>) -> Component)? = null,
+	loreBuilder: (String, FindIterable<BazaarItem>) -> List<Component> = { _, _ ->listOf() },
+	clickHandler: (String, ClickType, Player) -> Unit,
 ): List<AbstractItem> {
 	val items = BazaarItem.find(filter)
 	sort.sort(items, ascending)
 
-	return items.mapTo(mutableListOf()) { bazaarItem ->
-		fromItemString(bazaarItem.itemString)
-			.updateDisplayName(nameBuilder.invoke(bazaarItem))
-			.updateLore(loreBuilder.invoke(bazaarItem))
-			.makeGuiButton { clickType, player -> clickHandler.invoke(bazaarItem, clickType, player)  }
-	}
+	return items
+		.filter { TradeCities.isCity(Regions[it.cityTerritory]) }
+		.map { it.itemString }
+		.distinct()
+		.mapTo(mutableListOf()) { itemString ->
+			AsyncItem({
+				with(fromItemString(itemString)) {
+					nameBuilder?.let { updateDisplayName(it.invoke(itemString, items)) }
+					updateLore(loreBuilder.invoke(itemString, items))
+					// Clear attributes
+					updateData(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes().build())
+				}
+			}) { event ->
+				clickHandler.invoke(itemString, event.click, event.playerClicker)
+			}
+		}
 }
