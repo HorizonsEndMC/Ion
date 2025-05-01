f629c425ebf910b5e80552e4cc266375e75c34d7
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 14:31:22 -0500

    Re-organize class hierarchy

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGui.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGui.kt
@@ -1,6 +1,7 @@
-package net.horizonsend.ion.server.gui.invui.bazaar.purchase
+package net.horizonsend.ion.server.gui.invui.bazaar
 
 import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarPurchaseMenuParent
 
 interface BazaarGui {
 	val parent: BazaarPurchaseMenuParent
