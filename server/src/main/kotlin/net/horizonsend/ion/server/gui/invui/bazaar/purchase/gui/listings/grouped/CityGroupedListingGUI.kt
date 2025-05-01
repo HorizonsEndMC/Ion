768a190ca4d76206561f6daf43953d4f961e86ab
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 18:11:44 -0500

    rename before starting work on individual listings

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/gui/listings/grouped/CityGroupedListingGUI.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/gui/listings/grouped/CityGroupedListingGUI.kt
@@ -1,16 +1,19 @@
-package net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui.browse
+package net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui.listings.grouped
 
 import net.horizonsend.ion.common.database.schema.economy.BazaarItem
+import net.horizonsend.ion.server.features.economy.city.TradeCityData
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
 import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarPurchaseMenuParent
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.kyori.adventure.text.Component.text
 import org.bson.conversions.Bson
+import org.litote.kmongo.and
+import org.litote.kmongo.eq
 import org.litote.kmongo.gt
 
-class GlobalListingGUI(parent: BazaarPurchaseMenuParent, pageNumber: Int = 0) : ListingGUIParent(parent, pageNumber) {
-	override val searchBson: Bson = BazaarItem::stock gt 0
+class CityGroupedListingGUI(parent: BazaarPurchaseMenuParent, val city: TradeCityData, pageNumber: Int = 0) : GroupedListingGUIParent(parent, pageNumber) {
+	override val searchBson: Bson = and(BazaarItem::stock gt 0, BazaarItem::cityTerritory eq city.territoryId)
 
 	override val searchButton = GuiItem.MAGNIFYING_GLASS
 		.makeItem(text("Search for Items"))
@@ -19,6 +22,6 @@ class GlobalListingGUI(parent: BazaarPurchaseMenuParent, pageNumber: Int = 0) : 
 		}
 
 	override fun reOpen() {
-		BazaarGUIs.openGlobalBrowse(parent.viewer, parent.remote, pageNumber)
+		BazaarGUIs.openCityBrowse(parent.viewer, parent.remote, city, pageNumber)
 	}
 }
