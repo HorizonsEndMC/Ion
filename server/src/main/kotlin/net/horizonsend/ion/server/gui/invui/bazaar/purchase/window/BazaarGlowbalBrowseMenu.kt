a064bfae024a7a7cdc7fbf1faed8e1293429460b
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 15:42:52 -0500

    Improve sorting, fix page number display

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/BazaarGlowbalBrowseMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/BazaarGlowbalBrowseMenu.kt
@@ -2,17 +2,17 @@ package net.horizonsend.ion.server.gui.invui.bazaar.purchase.window
 
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiItems.closeMenuItem
-import net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui.GlobalBrowseGUI
+import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui.browse.GlobalListingGUI
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.horizonsend.ion.server.miscellaneous.utils.updateLore
 import net.kyori.adventure.text.Component.text
 import org.bukkit.entity.Player
-import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.item.impl.AbstractItem
 
 class BazaarGlowbalBrowseMenu(viewer: Player, remote: Boolean, pageNumber: Int = 0) : BazaarPurchaseMenuParent(viewer, remote) {
 	override val menuTitle: String = "Browsing All Items"
-	override val contained: Gui = GlobalBrowseGUI(this, pageNumber).getGui()
+	override val contained: GlobalListingGUI = GlobalListingGUI(this, pageNumber)
 
 	override val citySelectionButton: AbstractItem = getCitySelectionButton(false)
 	override val globalBrowseButton: AbstractItem = getGlobalBrowseButton(true)
@@ -27,4 +27,9 @@ class BazaarGlowbalBrowseMenu(viewer: Player, remote: Boolean, pageNumber: Int =
 				text("Lore Line 3"),
 			))
 			.makeGuiButton { _, _ -> }
+
+	override fun GuiText.populateGuiText(): GuiText {
+		contained.modifyGuiText(this)
+		return this
+	}
 }
