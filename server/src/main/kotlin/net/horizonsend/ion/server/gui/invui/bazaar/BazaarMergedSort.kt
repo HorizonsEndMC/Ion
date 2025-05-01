467495319f82e53ca728f74405062446ca8a6bd3
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 18:04:02 -0500

    change default sort to min price

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarMergedSort.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarMergedSort.kt
@@ -4,18 +4,18 @@ import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.server.features.gui.item.AsyncItem
 
 enum class BazaarMergedSort {
-	MAX_PRICE {
-		private val property = BazaarItem::price
-
-		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
-			collection.sortByDescending { it.first.value.maxOfOrNull(property) }
-		}
-	},
 	MIN_PRICE {
 		private val property = BazaarItem::price
 
 		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
 			collection.sortBy { it.first.value.maxOfOrNull(property) }
+		}
+	},
+	MAX_PRICE {
+		private val property = BazaarItem::price
+
+		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
+			collection.sortByDescending { it.first.value.maxOfOrNull(property) }
 		}
 	},
 	HIGHEST_STOCK {
