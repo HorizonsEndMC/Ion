ec19b4bdea21533e3c9377980f3956502454d3f6
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 01:09:48 -0500

    Implement sorting button

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/BazaarMainPurchaseMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/BazaarMainPurchaseMenu.kt
@@ -5,7 +5,9 @@ import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.gui.invui.InvUIGuiWrapper
 import org.bukkit.entity.Player
 import xyz.xenondevs.invui.gui.Gui
+import xyz.xenondevs.invui.gui.PagedGui
 import xyz.xenondevs.invui.gui.TabGui
+import xyz.xenondevs.invui.item.Item
 import xyz.xenondevs.invui.item.impl.AbstractItem
 
 class BazaarMainPurchaseMenu(viewer: Player, remote: Boolean) : BazaarPurchaseMenuParent(viewer, remote) {
@@ -14,22 +16,42 @@ class BazaarMainPurchaseMenu(viewer: Player, remote: Boolean) : BazaarPurchaseMe
 		GlobalBrowseGUI(this)
 	)
 
-	private var currentTab = 0
+	var currentTab = 0; private set
+
+	private lateinit var guiInstance: TabGui
 
 	override fun getMenuGUI(): Gui {
+		val new = buildGui()
+		guiInstance = new
+		return new
+	}
+
+	private fun buildGui(): TabGui {
 		return TabGui.normal()
 			.applyPurchaseMenuStructure()
 			.setTabs(tabs.map(InvUIGuiWrapper<*>::getGui))
 			.addTabChangeHandler { _, tab ->
 				currentTab = tab
+				(tabs[currentTab] as? BazaarGui)?.refresh()
 				refreshGuiText()
 			}
 			.build()
+			.apply { setTab(currentTab) }
 	}
 
 	override fun GuiText.populateGuiText(): GuiText {
 		(tabs[currentTab] as? BazaarGui)?.modifyGuiText(this)
 		return this
+	}
+
+	fun setTab(tabNumber: Int) {
+		guiInstance.setTab(tabNumber)
+	}
+
+	fun getTabGUI(): PagedGui<Item> {
+		@Suppress("UNCHECKED_CAST")
+		// It will always be a paged GUI
+		return guiInstance.tabs[currentTab] as PagedGui<Item>
 	}
 
 	override val backButton: AbstractItem = closeMenuItem(viewer)
