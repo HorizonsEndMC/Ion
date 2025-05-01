01cd81c44fc0c9a853e652dbe7879222df979f8c
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-04-30 20:54:09 -0500

    Move tabs out to their own class, add a parent for all purchase windows that contains the top bar

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/CitySelectionGUI.kt
@@ -0,0 +1,51 @@
+package net.horizonsend.ion.server.gui.invui.bazaar.purchase
+
+import net.horizonsend.ion.common.database.schema.economy.BazaarItem
+import net.horizonsend.ion.common.extensions.information
+import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.server.features.nations.region.Regions
+import net.horizonsend.ion.server.features.nations.region.types.RegionTerritory
+import net.horizonsend.ion.server.gui.invui.InvUIGuiWrapper
+import net.horizonsend.ion.server.gui.invui.bazaar.getCityButtons
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.AQUA
+import net.kyori.adventure.text.format.NamedTextColor.GRAY
+import org.litote.kmongo.eq
+import xyz.xenondevs.invui.gui.Gui
+import xyz.xenondevs.invui.gui.PagedGui
+import xyz.xenondevs.invui.gui.structure.Markers
+
+class CitySelectionGUI(val parent: BazaarPurchaseMenuParent) : InvUIGuiWrapper<Gui> {
+	override fun getGui(): Gui {
+		val cityButtons = getCityButtons(
+			nameBuilder = { city -> text(city.displayName) },
+			loreBuilder = { city ->
+				val listingCount = BazaarItem.count(BazaarItem::cityTerritory eq city.territoryId)
+				val territoryRegion = Regions.get<RegionTerritory>(city.territoryId)
+
+				listOf(
+					ofChildren(
+						text("Located at ", GRAY), text(territoryRegion.name, AQUA),
+						text(" on ", GRAY), text(territoryRegion.world, AQUA), text(".", GRAY)
+					),
+					text("$listingCount item listing${if (listingCount != 1L) "s" else ""}.")
+				)
+			},
+			clickHandler = { city, _, player ->
+				player.information(city.displayName)
+			}
+		)
+
+		return PagedGui.items()
+			.setStructure(
+				". . . . . . . . .",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"< . . . . . . . >"
+			)
+			.addIngredient('#', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
+			.setContent(cityButtons)
+			.build()
+	}
+}
