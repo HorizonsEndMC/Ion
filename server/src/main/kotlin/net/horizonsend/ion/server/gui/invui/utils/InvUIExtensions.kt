31be695a335fadd5a1788e05e9aa52396bf65bb0
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 13:48:53 -0500

    reduce duplication

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/utils/InvUIExtensions.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/utils/InvUIExtensions.kt
@@ -1,7 +1,9 @@
 package net.horizonsend.ion.server.gui.invui.utils
 
 import net.kyori.adventure.text.Component
+import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
+import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.window.Window
 
 fun <W : Window, S: Window.Builder<W, S>> S.setTitle(title: Component): S {
@@ -13,3 +15,5 @@ fun <W : Window> W.changeTitle(title: Component): W {
 	changeTitle(AdventureComponentWrapper(title))
 	return this
 }
+
+fun ItemStack.asItemProvider(): ItemProvider = ItemProvider { this }
