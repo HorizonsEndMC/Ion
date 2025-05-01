a5eaff0cb1b81c4b809403eed1803e39f6fd62ef
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 18:41:18 -0500

    Add sort icon & prettify sorting display

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarMergedSort.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarMergedSort.kt
@@ -2,30 +2,32 @@ package net.horizonsend.ion.server.gui.invui.bazaar
 
 import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.server.features.gui.item.AsyncItem
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
 
-enum class BazaarMergedSort {
-	MIN_PRICE {
+enum class BazaarMergedSort(val displayName: Component) {
+	MIN_PRICE(text("Min Price")) {
 		private val property = BazaarItem::price
 
 		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
 			collection.sortBy { it.first.value.maxOfOrNull(property) }
 		}
 	},
-	MAX_PRICE {
+	MAX_PRICE(text("Max Price")) {
 		private val property = BazaarItem::price
 
 		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
 			collection.sortByDescending { it.first.value.maxOfOrNull(property) }
 		}
 	},
-	HIGHEST_STOCK {
+	HIGHEST_STOCK(text("Highest Stock")) {
 		private val property = BazaarItem::stock
 
 		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
 			collection.sortByDescending { it.first.value.maxOfOrNull(property) }
 		}
 	},
-	LOWEST_STOCK {
+	LOWEST_STOCK(text("Lowest Stock")) {
 		private val property = BazaarItem::stock
 
 		override fun sort(collection: MutableList<Pair<Map. Entry<String, List<BazaarItem>>, AsyncItem>>) {
