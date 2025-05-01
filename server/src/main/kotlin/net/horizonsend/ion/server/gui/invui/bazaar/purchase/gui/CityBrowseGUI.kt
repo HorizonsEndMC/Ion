0b3de45884000d89295fd0845d74041a5f28738c
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 14:31:22 -0500

    Re-organize class hierarchy

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/gui/CityBrowseGUI.kt
@@ -0,0 +1,27 @@
+package net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui
+
+import net.horizonsend.ion.common.database.schema.economy.BazaarItem
+import net.horizonsend.ion.server.features.economy.city.TradeCityData
+import net.horizonsend.ion.server.features.gui.GuiItem
+import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarPurchaseMenuParent
+import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
+import net.kyori.adventure.text.Component.text
+import org.bson.conversions.Bson
+import org.litote.kmongo.and
+import org.litote.kmongo.eq
+import org.litote.kmongo.gt
+
+class CityBrowseGUI(parent: BazaarPurchaseMenuParent, val city: TradeCityData, pageNumber: Int = 0) : BrowseGUIParent(parent, pageNumber) {
+	override val searchBson: Bson = and(BazaarItem::stock gt 0, BazaarItem::cityTerritory eq city.territoryId)
+
+	override val searchButton = GuiItem.MAGNIFYING_GLASS
+		.makeItem(text("Search for Items"))
+		.makeGuiButton { _, _ ->
+			println("Search")
+		}
+
+	override fun reOpen() {
+		BazaarGUIs.openCityBrowse(parent.viewer, parent.remote, city, pageNumber)
+	}
+}
