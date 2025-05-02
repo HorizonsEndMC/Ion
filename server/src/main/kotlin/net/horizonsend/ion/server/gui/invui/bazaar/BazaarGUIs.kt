896b6d6d0da9fffcb96f5dc3464f6fd1e8d7f0fb
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-01 22:43:13 -0500

    Add the individual listings menu

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGUIs.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/BazaarGUIs.kt
@@ -1,9 +1,11 @@
 package net.horizonsend.ion.server.gui.invui.bazaar
 
 import net.horizonsend.ion.server.features.economy.city.TradeCityData
-import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarCityBrowseMenu
 import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarCitySelectionMenu
-import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarGlowbalBrowseMenu
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.browse.BazaarCityBrowseMenu
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.browse.BazaarGlowbalBrowseMenu
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.listings.CityItemListingsMenu
+import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.listings.GlobalItemListingsMenu
 import org.bukkit.entity.Player
 
 object BazaarGUIs {
@@ -17,5 +19,13 @@ object BazaarGUIs {
 
 	fun openGlobalBrowse(player: Player, remote: Boolean, pageNumber: Int = 0) {
 		BazaarGlowbalBrowseMenu(player, remote, pageNumber).openGui()
+	}
+
+	fun openCityItemListings(player: Player, remote: Boolean, city: TradeCityData, itemString: String, pageNumber: Int = 0) {
+		CityItemListingsMenu(player, remote, city, itemString, pageNumber).openGui()
+	}
+
+	fun openGlobalItemListings(player: Player, remote: Boolean, itemString: String, pageNumber: Int = 0) {
+		GlobalItemListingsMenu(player, remote, itemString, pageNumber).openGui()
 	}
 }
