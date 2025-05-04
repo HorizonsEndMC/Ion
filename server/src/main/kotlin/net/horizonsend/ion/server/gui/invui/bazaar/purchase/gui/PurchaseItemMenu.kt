feb653ea4202303875f18e8e5ab064fa6cc93e66
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-04 13:23:43 -0500

    improve formatting

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/gui/PurchaseItemMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/gui/PurchaseItemMenu.kt
@@ -2,11 +2,10 @@ package net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui
 
 import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
-import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
 import net.horizonsend.ion.common.utils.text.template
 import net.horizonsend.ion.server.command.GlobalCompletions.fromItemString
 import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.TextInputMenu
-import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.validator.IntegerValidator
+import net.horizonsend.ion.server.features.gui.custom.misc.anvilinput.validator.RangeIntegerValidator
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 
@@ -22,12 +21,14 @@ class PurchaseItemMenu(
 
 		TextInputMenu(
 			player = viewer,
-			title = template(Component.text("Buying {0}'s {1}", HE_MEDIUM_GRAY), useQuotesAroundObjects = false, listerName,itemStack.displayName()),
-			description = template(Component.text("Enter a value between {0} and {1}", HE_MEDIUM_GRAY), 1, item.stock),
+			title = template(message = Component.text("Buying {0}'s {1}"), paramColor = null, useQuotesAroundObjects = false, listerName, itemStack.displayName()),
+			description = template(Component.text("Enter a value between {0} and {1}"), paramColor = null, 1, item.stock),
 			backButtonHandler = { backButtonHandler.invoke() },
-			inputValidator = IntegerValidator,
+			inputValidator = RangeIntegerValidator(1..item.stock),
 			componentTransformer = { Component.text(it) },
-			successfulInputHandler = { _, (_, result) -> println("result: $result") }
+			successfulInputHandler = { _, (_, result) ->
+				println("click result: $result")
+			}
 		).open()
 	}
 }
