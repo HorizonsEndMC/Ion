0c076749d98aaba9c3d9f9c80726babd29bf6e32
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-04 15:47:44 -0500

    remote detection improvement

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/listings/GlobalItemListingsMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/listings/GlobalItemListingsMenu.kt
@@ -4,6 +4,7 @@ import net.horizonsend.ion.common.database.schema.economy.BazaarItem
 import net.horizonsend.ion.common.database.schema.misc.SLPlayer
 import net.horizonsend.ion.common.utils.text.BACKGROUND_EXTENDER
 import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
+import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.common.utils.text.template
 import net.horizonsend.ion.server.command.GlobalCompletions.fromItemString
 import net.horizonsend.ion.server.features.economy.city.TradeCities
@@ -15,11 +16,13 @@ import net.horizonsend.ion.server.features.waypoint.WaypointManager
 import net.horizonsend.ion.server.features.world.IonWorld.Companion.ion
 import net.horizonsend.ion.server.features.world.WorldFlag
 import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
+import net.horizonsend.ion.server.gui.invui.bazaar.REMOTE_WARINING
 import net.horizonsend.ion.server.gui.invui.bazaar.purchase.gui.IndividualListingGUI
 import net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.BazaarPurchaseMenuParent
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.horizonsend.ion.server.miscellaneous.utils.updateLore
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.empty
 import net.kyori.adventure.text.Component.text
 import org.bukkit.Location
 import org.bukkit.entity.Player
@@ -42,7 +45,7 @@ class GlobalItemListingsMenu(
 			verticalShift = -11
 		))
 		.add(fromItemString(itemString).displayName(), line = -2, verticalShift = -4)
-		.add(text("Global Browse"), line = -1, verticalShift = -2)
+		.add(ofChildren(text("Global Browse")), line = -1, verticalShift = -2)
 		.build()
 
 	override val contained: IndividualListingGUI = IndividualListingGUI(
@@ -55,10 +58,15 @@ class GlobalItemListingsMenu(
 			val region = Regions.get<RegionTerritory>(bazaarItem.cityTerritory)
 			val regionWorld = region.bukkitWorld
 
+			var remoteLoc: Boolean = remote
+
 			val distance = regionWorld?.let { world ->
 				if (region.contains(viewer.location)) {
+					remoteLoc = false
 					return@let "0 Blocks"
 				}
+
+				remoteLoc = true
 
 				val regionCenter = Location(world, region.centerX.toDouble(), 128.toDouble(), region.centerZ.toDouble())
 
@@ -83,7 +91,7 @@ class GlobalItemListingsMenu(
 				template(text("Seller: {0}", HE_MEDIUM_GRAY), useQuotesAroundObjects = false, SLPlayer.getName(bazaarItem.seller)),
 				template(text("Stock: {0}", HE_MEDIUM_GRAY), useQuotesAroundObjects = false, bazaarItem.stock),
 				template(text("Sold at {0} on {1}", HE_MEDIUM_GRAY), useQuotesAroundObjects = false, TradeCities.getIfCity(region)?.displayName, region.world),
-				template(text("Distance: {0}", HE_MEDIUM_GRAY), useQuotesAroundObjects = false, distance),
+				template(ofChildren(text("Distance: {0} ", HE_MEDIUM_GRAY), if (remoteLoc) REMOTE_WARINING else empty()), useQuotesAroundObjects = false, distance),
 			)
 		},
 		purchaseBackButton = {
