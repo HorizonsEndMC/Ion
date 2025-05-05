08fffd95c3781599875549e72e204d6f1bde580e
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-04 20:50:58 -0500

    Implement search button

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/misc/ItemMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/misc/ItemMenu.kt
@@ -2,52 +2,57 @@ package net.horizonsend.ion.server.features.gui.custom.misc
 
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiItems
+import net.horizonsend.ion.server.features.gui.item.AsyncItem
 import net.horizonsend.ion.server.gui.invui.InvUIWindowWrapper
+import net.horizonsend.ion.server.gui.invui.utils.setTitle
+import net.horizonsend.ion.server.miscellaneous.utils.text.itemName
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
-import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper
 import xyz.xenondevs.invui.gui.Gui
+import xyz.xenondevs.invui.gui.PagedGui
+import xyz.xenondevs.invui.gui.structure.Markers
 import xyz.xenondevs.invui.item.Item
-import xyz.xenondevs.invui.item.ItemProvider
-import xyz.xenondevs.invui.item.impl.AbstractItem
 import xyz.xenondevs.invui.window.Window
-import kotlin.math.min
 
 class ItemMenu(
 	val title: Component,
 	viewer: Player,
-	private val guiItems: Collection<Item>,
+	private val guiItems: List<Item>,
 	private val backButtonHandler: (Player) -> Unit
 ) : InvUIWindowWrapper(viewer) {
-    private lateinit var gui: Gui
-
     private fun createGui(): Gui {
-        val rows = ((guiItems.size + 8) / 9).coerceIn(1, 5) + 1
-        // create a GUI that can store the number of elements in guiItems, plus an additional row for control items
-        gui = Gui.empty(9, rows)
-
-        // add all of guiItems or the first 45 items, whichever is smaller
-        gui.addItems(*(guiItems.toList().subList(0, min(guiItems.size, 9 * 5))).toTypedArray())
-
-        gui.setItem(0, rows - 1, backButton)
+		val gui = PagedGui.items()
+			.setStructure(
+				"x . . . . . . . .",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"l . . . . . . . r",
+			)
+			.addIngredient('x', backButton)
+			.addIngredient('l', GuiItems.PageLeftItem())
+			.addIngredient('r', GuiItems.PageRightItem())
+			.addIngredient('#', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
+			.setContent(guiItems)
+			.build()
 
         return gui
     }
 
 	override fun buildWindow(): Window {
-		createGui()
 		return Window.single()
 			.setViewer(viewer)
-			.setGui(gui)
-			.setTitle(AdventureComponentWrapper(title))
+			.setGui(createGui())
+			.setTitle(title)
 			.build()
 	}
 
     private val backButton = GuiItems.CustomControlItem(
-        Component.text("Back"), GuiItem.DOWN, callback = { _: ClickType, player: Player, _: InventoryClickEvent ->
+        Component.text("Back").itemName, GuiItem.LEFT, callback = { _: ClickType, player: Player, _: InventoryClickEvent ->
             player.closeInventory()
             backButtonHandler.invoke(player)
         }
@@ -63,15 +68,10 @@ class ItemMenu(
 			backButtonHandler: (Player) -> Unit
 		) {
 			val mapped = entries.map { entry ->
-				object : AbstractItem() {
-					val item = itemTransformer.invoke(entry)
-					override fun getItemProvider(): ItemProvider = ItemProvider { item }
-
-					override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-						resultConsumer.invoke(clickType, entry)
-						backButtonHandler.invoke(player)
-					}
-				}
+				AsyncItem(
+					resultProvider = { itemTransformer.invoke(entry) },
+					handleClick = { event -> resultConsumer.invoke(event.click, entry) }
+				)
 			}
 
 			ItemMenu(title, player, mapped, backButtonHandler).openGui()
