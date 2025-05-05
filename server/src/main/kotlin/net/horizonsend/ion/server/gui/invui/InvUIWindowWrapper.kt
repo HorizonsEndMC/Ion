1b45d81632cf4be544ce58011222462af94524a6
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-05 01:34:43 -0500

    prettify listings menu

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/InvUIWindowWrapper.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/InvUIWindowWrapper.kt
@@ -1,17 +1,27 @@
 package net.horizonsend.ion.server.gui.invui
 
 import net.horizonsend.ion.server.gui.CommonGuiWrapper
+import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import org.bukkit.entity.Player
 import xyz.xenondevs.invui.window.Window
 
-abstract class InvUIWindowWrapper(val viewer: Player) : CommonGuiWrapper {
+abstract class InvUIWindowWrapper(val viewer: Player, val async: Boolean = false) : CommonGuiWrapper {
 	protected var currentWindow: Window? = null
 
 	abstract fun buildWindow(): Window
 
 	override fun openGui() {
-		currentWindow = buildWindow()
-		currentWindow?.open()
+		if (async) {
+			Tasks.async {
+				currentWindow = buildWindow()
+				Tasks.sync { currentWindow?.open() }
+			}
+		} else {
+			Tasks.sync {
+				currentWindow = buildWindow()
+				currentWindow?.open()
+			}
+		}
 	}
 
 	fun getOpenWindow() = currentWindow
