fffb680f6c365a51a6b2bc0a2eba8b639e380874
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-06 14:40:32 -0500

    migrate settings gui

--- server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/settings/button/DBCachedSettingsButton.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/settings/button/DBCachedSettingsButton.kt
@@ -1,16 +1,13 @@
 package net.horizonsend.ion.server.features.gui.custom.settings.button
 
-import net.horizonsend.ion.common.database.cache.nations.AbstractPlayerCache
-import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.database.schema.misc.PlayerSettings
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.utils.text.plainText
-import net.horizonsend.ion.server.features.cache.PlayerCache
+import net.horizonsend.ion.server.features.cache.PlayerSettingsCache
 import net.horizonsend.ion.server.features.gui.GuiItem
-import net.horizonsend.ion.server.miscellaneous.utils.Tasks
 import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
-import org.litote.kmongo.setValue
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty1
 
@@ -20,24 +17,16 @@ abstract class DBCachedSettingsButton<T : Any>(
 	icon: GuiItem,
 	defautValue: T,
 	val paramType: KClass<T>,
-	val db: KMutableProperty1<SLPlayer, T>,
-	val cache: KMutableProperty1<AbstractPlayerCache.PlayerData, T>
+	val db: KMutableProperty1<PlayerSettings, T>
 ) : SettingsMenuButton<T>(name, butonDescription, icon, defautValue) {
 	override fun getState(player: Player): T {
-		val playerData = PlayerCache.getIfOnline(player)
-		return if (playerData != null) { cache.get(playerData) } else SLPlayer.findPropById(player.slPlayerId, paramType, db) ?: defautValue
+		return PlayerSettingsCache.getSettingOrThrow(player.slPlayerId, db)
 	}
 
 	override fun setState(player: Player, state: T) {
-		val cached = PlayerCache.getIfOnline(player)
+		if (!player.isOnline) return
 
-		if (cached != null) {
-			cache.set(cached, state)
-		}
-
-		Tasks.async {
-			SLPlayer.updateById(player.slPlayerId, setValue(db, state))
-		}
+		PlayerSettingsCache.updateSetting(player.slPlayerId, db, state)
 
 		sendUpdateMessage(player, state)
 	}
