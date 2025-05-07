8265ac96fd5a6995b6897dca1fa99f7f7c1d3c9a
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-07 12:27:44 -0500

    Finish migrating the last of the settings

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sidebar/command/SidebarCombatTimerCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sidebar/command/SidebarCombatTimerCommand.kt
@@ -2,14 +2,12 @@ package net.horizonsend.ion.server.features.sidebar.command
 
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.Subcommand
-import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.database.schema.misc.PlayerSettings
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.extensions.userError
 import net.horizonsend.ion.server.command.SLCommand
-import net.horizonsend.ion.server.features.cache.PlayerCache
-import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
+import net.horizonsend.ion.server.features.cache.PlayerSettingsCache.setSetting
 import org.bukkit.entity.Player
-import org.litote.kmongo.setValue
 
 @CommandAlias("sidebar")
 object SidebarCombatTimerCommand : SLCommand() {
@@ -24,17 +22,17 @@ object SidebarCombatTimerCommand : SLCommand() {
     fun onEnableCombatTimer(
         sender: Player
     ) {
-        SLPlayer.updateById(sender.slPlayerId, setValue(SLPlayer::combatTimerEnabled, true))
-        PlayerCache[sender].combatTimerEnabled = true
-        sender.success("Enabled combat timer info on sidebar")
+		sender.setSetting(PlayerSettings::combatTimerEnabled, true)
+
+		sender.success("Enabled combat timer info on sidebar")
     }
 
     @Subcommand("combattimer disable")
     fun onDisableCombatTimer(
         sender: Player
     ) {
-        SLPlayer.updateById(sender.slPlayerId, setValue(SLPlayer::combatTimerEnabled, false))
-        PlayerCache[sender].combatTimerEnabled = false
-        sender.success("Disabled combat timer info on sidebar")
+		sender.setSetting(PlayerSettings::combatTimerEnabled, false)
+
+		sender.success("Disabled combat timer info on sidebar")
     }
 }
