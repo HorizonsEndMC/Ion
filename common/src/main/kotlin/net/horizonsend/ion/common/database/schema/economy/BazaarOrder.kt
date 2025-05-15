f43059f988455cc5c4e7e4d9ce20dfdd56c767e0
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-15 17:52:06 -0500

    cleanup and prep work for orders

--- common/src/main/kotlin/net/horizonsend/ion/common/database/schema/economy/BazaarOrder.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/database/schema/economy/BazaarOrder.kt
@@ -10,7 +10,11 @@ import net.horizonsend.ion.common.database.trx
 import org.bson.conversions.Bson
 import org.litote.kmongo.combine
 import org.litote.kmongo.ensureIndex
+import org.litote.kmongo.gte
+import org.litote.kmongo.inc
+import org.litote.kmongo.push
 import org.litote.kmongo.setValue
+import org.litote.kmongo.updateOneById
 import java.time.Instant
 import java.util.Date
 
@@ -20,14 +24,16 @@ class BazaarOrder(
 	val cityTerritory: Oid<Territory>,
 
 	val itemString: String,
+
 	val requestedQuantity: Int,
 	val pricePerItem: Double,
+	var balance: Double,
 
 	var lastUpdated: Date,
 
-	var fulfilledQuantity: Int,
-	var stock: Int,
-	var balance: Double,
+	var fulfilledQuantity: Int = 0,
+	var stock: Int = 0,
+	var fulfillments: List<Pair<SLPlayerId, Int>> = listOf()
 ) : DbObject {
 	companion object : OidDbObjectCompanion<BazaarOrder>(BazaarOrder::class, setup = {
 		ensureIndex(BazaarOrder::cityTerritory)
@@ -51,8 +57,6 @@ class BazaarOrder(
 				requestedQuantity = quantity,
 				pricePerItem = pricePerItem,
 				lastUpdated = Date.from(Instant.now()),
-				fulfilledQuantity = 0,
-				stock = 0,
 				balance = pricePerItem * quantity
 			)
 
@@ -64,6 +68,39 @@ class BazaarOrder(
 		private fun alsoUpdateTime(update: Bson): Bson {
 			val date = Date.from(Instant.now())
 			return combine(update, setValue(BazaarOrder::lastUpdated, date))
+		}
+
+		/** Returns whether this order has been fully fulfilled */
+		fun isFulfilled(order: Oid<BazaarOrder>): Boolean {
+			val requested = findPropById(order, BazaarOrder::requestedQuantity)
+			return matches(order, BazaarOrder::fulfilledQuantity gte requested)
+		}
+
+		/**
+		 * Fulfills the amount of stock to the order
+		 * Returns the profit
+		 **/
+		fun fulfillStock(orderId: Oid<BazaarOrder>, fulfiller: SLPlayerId, amount: Int): Double = trx { sess ->
+			val order = BazaarOrder.findById(sess, orderId) ?: return@trx 0.0
+
+			val profit = amount * order.pricePerItem
+
+			BazaarOrder.updateById(orderId, alsoUpdateTime(combine(
+				inc(BazaarOrder::fulfilledQuantity, amount),
+				inc(BazaarOrder::stock, amount),
+				inc(BazaarOrder::balance, -profit),
+				push(BazaarOrder::fulfillments, fulfiller to amount)
+			)))
+
+			return@trx profit
+		}
+
+		/**
+		 * Fulfills the amount of stock to the order
+		 **/
+		fun collectItemBalance(orderId: Oid<BazaarOrder>, amount: Int) = trx { sess ->
+			require(amount >= 0)
+			BazaarOrder.col.updateOneById(sess, orderId, alsoUpdateTime(inc(BazaarOrder::stock, -amount)))
 		}
 	}
 }
