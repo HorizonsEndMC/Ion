c0ca93a805401cec0cd263183d6bd345386815e5
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-20 14:11:36 -0500

    start of the manage orders menu

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/orders/window/BuyOrderMainMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/orders/window/BuyOrderMainMenu.kt
@@ -6,6 +6,7 @@ import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.gui.invui.InvUIWindowWrapper
+import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
@@ -48,6 +49,6 @@ class BuyOrderMainMenu(viewer: Player) : InvUIWindowWrapper(viewer) {
 	private val settingsButton = GuiItem.GEAR.makeItem(Component.text("Settings")).makeGuiButton { _, _ -> println("info") }
 	private val infoButton = GuiItem.INFO.makeItem(Component.text("Manage Your Orders")).makeGuiButton { _, _ -> println("info") }
 
-	private val manageButton = GuiItem.EMPTY.makeItem(Component.text("Manage Your Orders")).makeGuiButton { _, _ -> println("manage") }
+	private val manageButton = GuiItem.EMPTY.makeItem(Component.text("Manage Your Orders")).makeGuiButton { _, _ -> BazaarGUIs.openBuyOrderManageMenu(viewer, this) }
 	private val browseButton = GuiItem.EMPTY.makeItem(Component.text("Browse Sell Orders")).makeGuiButton { _, _ -> println("browse") }
 }
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/orders/window/ManageOrdersMenu.kt
@@ -0,0 +1,66 @@
+package net.horizonsend.ion.server.gui.invui.bazaar.orders.window
+
+import com.mongodb.client.FindIterable
+import net.horizonsend.ion.common.database.schema.economy.BazaarOrder
+import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
+import net.horizonsend.ion.common.utils.text.template
+import net.horizonsend.ion.server.command.GlobalCompletions.fromItemString
+import net.horizonsend.ion.server.features.economy.city.TradeCities
+import net.horizonsend.ion.server.features.gui.GuiItems
+import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.features.nations.region.Regions
+import net.horizonsend.ion.server.gui.invui.InvUIWindowWrapper
+import net.horizonsend.ion.server.gui.invui.bazaar.stripAttributes
+import net.horizonsend.ion.server.gui.invui.utils.asItemProvider
+import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
+import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
+import net.horizonsend.ion.server.miscellaneous.utils.updateLore
+import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
+import org.litote.kmongo.eq
+import xyz.xenondevs.invui.gui.PagedGui
+import xyz.xenondevs.invui.gui.structure.Markers
+import xyz.xenondevs.invui.window.Window
+
+class ManageOrdersMenu(viewer: Player) : InvUIWindowWrapper(viewer, true) {
+	private lateinit var orders: FindIterable<BazaarOrder>
+
+	override fun buildWindow(): Window {
+		orders = BazaarOrder.find(BazaarOrder::player eq viewer.slPlayerId)
+
+		val items = orders.toList().mapNotNull {
+			val cityName = TradeCities.getIfCity(Regions[it.cityTerritory])?.displayName ?: "Unknown"
+
+			fromItemString(it.itemString)
+				.stripAttributes()
+				.updateLore(listOf(
+					template(Component.text("City: {0}", HE_MEDIUM_GRAY), cityName),
+					template(Component.text("Order Quantity: {0}", HE_MEDIUM_GRAY), it.requestedQuantity),
+					template(Component.text("Fulfilled Quantity: {0}", HE_MEDIUM_GRAY), it.fulfilledQuantity)
+				))
+				.asItemProvider()
+				.makeGuiButton { _, _ ->  }
+		}
+
+		val gui = PagedGui.items()
+			.setStructure(
+				". . . . . . . . .",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"# # # # # # # # #",
+				"l . . . . . . . r",
+			)
+			.addIngredient('#', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
+			.addIngredient('l', GuiItems.PageLeftItem())
+			.addIngredient('r', GuiItems.PageRightItem())
+			.setContent(items)
+			.build()
+
+		return normalWindow(gui)
+	}
+
+	override fun buildTitle(): Component {
+		return GuiText("")
+			.build()
+	}
+}
