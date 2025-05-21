a3e7fd361fb1c3573f79666bda2a93906daf2142
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-21 01:45:28 -0500

    Remember which layout was picked last, implement most of the list view for order management

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/manage/GridListingManagementMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/purchase/window/manage/GridListingManagementMenu.kt
@@ -1,9 +1,12 @@
 package net.horizonsend.ion.server.gui.invui.bazaar.purchase.window.manage
 
+import net.horizonsend.ion.common.database.schema.misc.PlayerSettings
 import net.horizonsend.ion.common.utils.text.DEFAULT_GUI_WIDTH
+import net.horizonsend.ion.server.features.cache.PlayerSettingsCache.setSetting
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiItems
 import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
@@ -12,7 +15,7 @@ import xyz.xenondevs.invui.gui.PagedGui
 import xyz.xenondevs.invui.gui.structure.Markers
 import xyz.xenondevs.invui.window.Window
 
-class GridListingMenu(viewer: Player, backButtonHandler: () -> Unit = {}) : AbstractListingMenu(viewer, backButtonHandler) {
+class GridListingManagementMenu(viewer: Player) : AbstractListingManagementMenu(viewer) {
 	override val listingsPerPage: Int = 36
 
     override fun buildWindow(): Window {
@@ -25,7 +28,7 @@ class GridListingMenu(viewer: Player, backButtonHandler: () -> Unit = {}) : Abst
                 "# # # # # # # # #",
                 "< . . . . . f s >",
             )
-            .addIngredient('x', backButton)
+            .addIngredient('x', parentOrBackButton())
             .addIngredient('f', filterButton)
 			.addIngredient('s', sortButton)
             .addIngredient('S', searchButton)
@@ -57,5 +60,8 @@ class GridListingMenu(viewer: Player, backButtonHandler: () -> Unit = {}) : Abst
         return withPageNumber(guiText)
     }
 
-    private val listViewButton = GuiItem.LIST_VIEW.makeItem(text("List view")).makeGuiButton { _, _ -> ListListingMenu(viewer, backButtonHandler).openGui() }
+    private val listViewButton = GuiItem.LIST_VIEW.makeItem(text("Switch to List view")).makeGuiButton { _, _ ->
+		viewer.setSetting(PlayerSettings::listingManageDefaultListView, true)
+		BazaarGUIs.openListingManageListMenu(viewer, parentWindow)
+	}
 }
