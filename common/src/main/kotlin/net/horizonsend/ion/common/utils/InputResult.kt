a243d6fdd944871f20e941f87608b2cb10ad759f
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-21 14:10:43 -0500

    Move most of the bazaar command functions to bazaars

--- common/src/main/kotlin/net/horizonsend/ion/common/utils/InputResult.kt
+++ common/src/main/kotlin/net/horizonsend/ion/common/utils/InputResult.kt
@@ -1,11 +1,22 @@
 package net.horizonsend.ion.common.utils
 
+import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 
 interface InputResult {
 	fun isSuccess(): Boolean
 
 	fun getReason(): List<Component>?
+
+	fun sendReason(audience: Audience) { getReason()?.forEach(audience::sendMessage) }
+
+	fun sendReasonIfFailure(audience: Audience) {
+		if (!isSuccess()) sendReason(audience)
+	}
+
+	fun sendReasonIfSuccess(audience: Audience) {
+		if (isSuccess()) sendReason(audience)
+	}
 
 	interface Success : InputResult {
 		override fun isSuccess(): Boolean = true
