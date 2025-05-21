c9469e434509c6fb410ce9d8f8243924d77b1fb6
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-21 01:45:28 -0500

    Remember which layout was picked last, implement most of the list view for order management

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/orders/window/manage/GridOrderManagementWindow.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/orders/window/manage/GridOrderManagementWindow.kt
@@ -1,20 +1,26 @@
 package net.horizonsend.ion.server.gui.invui.bazaar.orders.window.manage
 
+import net.horizonsend.ion.common.database.schema.misc.PlayerSettings
+import net.horizonsend.ion.server.features.cache.PlayerSettingsCache.setSetting
+import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiItems
 import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.gui.invui.bazaar.BazaarGUIs
+import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import xyz.xenondevs.invui.gui.PagedGui
 import xyz.xenondevs.invui.gui.structure.Markers
+import xyz.xenondevs.invui.item.Item
 import xyz.xenondevs.invui.window.Window
 
-class ListManageMenu(viewer: Player) : ManageOrdersMenu(viewer) {
-	override val listingsPerPage: Int = 4
+class GridOrderManagementWindow(viewer: Player) : AbstractOrderManagementMenu(viewer) {
+	override val listingsPerPage: Int = 36
 
 	override fun buildWindow(): Window {
 		val gui = PagedGui.items()
 			.setStructure(
-				"b . . . p . . . i",
+				"b . . . p . . g i",
 				"# # # # # # # # #",
 				"# # # # # # # # #",
 				"# # # # # # # # #",
@@ -24,6 +30,7 @@ class ListManageMenu(viewer: Player) : ManageOrdersMenu(viewer) {
 			.addIngredient('f', filterButton)
 			.addIngredient('s', sortButton)
 			.addIngredient('b', parentOrBackButton())
+			.addIngredient('g', switchLayoutButton)
 			.addIngredient('i', infoButton)
 
 			.addIngredient('#', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
@@ -48,5 +55,10 @@ class ListManageMenu(viewer: Player) : ManageOrdersMenu(viewer) {
 			)
 
 		return withPageNumber(text)
+	}
+
+	override val switchLayoutButton: Item = GuiItem.LIST_VIEW.makeItem(Component.text("Switch to List Layout")).makeGuiButton { _, _ ->
+		viewer.setSetting(PlayerSettings::orderManageDefaultListView, true)
+		BazaarGUIs.openBuyOrderManageListMenu(viewer, parentWindow)
 	}
 }
