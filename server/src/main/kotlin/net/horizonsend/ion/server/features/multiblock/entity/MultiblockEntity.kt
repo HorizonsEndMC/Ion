2251c24f6762d59ca44014c133dcf42d6fe23c5d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-29 12:34:05 -0500

    move create linkage function from extension to parent class

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/MultiblockEntity.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/MultiblockEntity.kt
@@ -10,6 +10,7 @@ import net.horizonsend.ion.server.features.transport.nodes.inputs.InputsData
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys.MULTIBLOCK_ENTITY_DATA
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.PDCSerializable
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.getRelative
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
@@ -26,6 +27,7 @@ import org.bukkit.block.data.type.WallSign
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 import org.bukkit.persistence.PersistentDataAdapterContext
+import kotlin.reflect.KClass
 
 /**
  * @param manager The multiblock manager that this is registered to
@@ -347,6 +349,28 @@ abstract class MultiblockEntity(
 
 	fun removeLinkages() {
 		linkages.forEach { linkage -> linkage.deRegister() }
+	}
+
+	fun createLinkage(
+		offsetRight: Int,
+		offsetUp: Int,
+		offsetForward: Int,
+		linkageDirection: RelativeFace,
+		vararg allowedEntities: KClass<out MultiblockEntity>
+	): MultiblockLinkageHolder {
+		val holder = MultiblockLinkageHolder(
+			this,
+			offsetRight,
+			offsetUp,
+			offsetForward,
+			allowedEntities,
+			linkageDirection
+		)
+
+		linkages.add(holder)
+
+		holder.register()
+		return holder
 	}
 
 	override fun handlerGetWorld(): World {
