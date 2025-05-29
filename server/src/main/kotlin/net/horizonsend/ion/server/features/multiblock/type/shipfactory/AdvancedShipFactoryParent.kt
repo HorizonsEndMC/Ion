45a3667d77f9fd63b62a264fb0e62e4239f3c205
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-29 01:06:27 -0500

    Add bazaar terminal multiblock and mergeable ship factory

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/AdvancedShipFactoryParent.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/shipfactory/AdvancedShipFactoryParent.kt
@@ -8,15 +8,18 @@ import net.horizonsend.ion.server.features.client.display.modular.display.Status
 import net.horizonsend.ion.server.features.multiblock.entity.PersistentMultiblockData
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.PowerStorage
 import net.horizonsend.ion.server.features.multiblock.entity.type.power.PoweredMultiblockEntity
+import net.horizonsend.ion.server.features.multiblock.linkage.MultiblockLinkageHolder.Companion.createLinkage
 import net.horizonsend.ion.server.features.multiblock.manager.MultiblockManager
 import net.horizonsend.ion.server.features.multiblock.shape.MultiblockShape
-import net.horizonsend.ion.server.features.multiblock.type.shipfactory.AdvancedShipFactoryMultiblock.AdvancedShipFactoryEntity
+import net.horizonsend.ion.server.features.multiblock.type.economy.BazaarTerminalMultiblock
+import net.horizonsend.ion.server.features.multiblock.type.shipfactory.AdvancedShipFactoryParent.AdvancedShipFactoryEntity
 import net.horizonsend.ion.server.features.multiblock.util.PrepackagedPreset
 import net.horizonsend.ion.server.features.transport.TransportTask
 import net.horizonsend.ion.server.features.transport.nodes.PathfindResult
 import net.horizonsend.ion.server.features.transport.nodes.inputs.InputsData
 import net.horizonsend.ion.server.features.transport.nodes.types.ItemNode
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace.BACKWARD
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace.FORWARD
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
@@ -29,12 +32,13 @@ import org.bukkit.World
 import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
 import org.bukkit.block.data.Bisected
+import org.bukkit.block.data.type.Stairs
 import org.bukkit.block.data.type.Stairs.Shape.STRAIGHT
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.persistence.PersistentDataAdapterContext
 
-object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShipFactoryEntity>() {
+sealed class AdvancedShipFactoryParent : AbstractShipFactoryMultiblock<AdvancedShipFactoryEntity>() {
 	override val signText = createSignText(
 		line1 = text("Starship Factory", NamedTextColor.BLUE),
 		line2 = text("Advanced", NamedTextColor.YELLOW),
@@ -47,53 +51,108 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 	override val displayName: Component get() = text("Advanced Ship Factory")
 	override val description: Component get() = text("Print starships and other structures with materials and credits. Provides more configuration settings.")
 
-	override fun MultiblockShape.buildStructure() {
-		z(0) {
-			y(-1) {
-				x(2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).powerInput()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = STRAIGHT))
+	data object AdvancedShipFactoryMultiblock : AdvancedShipFactoryParent() {
+		override fun MultiblockShape.buildStructure() {
+			z(0) {
+				y(-1) {
+					x(2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = STRAIGHT))
+					x(1).ironBlock()
+					x(0).powerInput()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = STRAIGHT))
+				}
+				y(0) {
+					x(2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+				}
 			}
-			y(0) {
-				x(2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).anyGlass()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+			z(1) {
+				y(-1) {
+					x(2).type(Material.LODESTONE)
+					x(1).sponge()
+					x(0).sponge()
+					x(-1).sponge()
+					x(-2).type(Material.LODESTONE)
+				}
+				y(0) {
+					x(2).anyPipedInventory()
+					x(1).sponge()
+					x(0).anyGlass()
+					x(-1).sponge()
+					x(-2).anyPipedInventory()
+				}
+			}
+			z(2) {
+				y(-1) {
+					x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
+				}
+				y(0) {
+					x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+				}
 			}
 		}
-		z(1) {
-			y(-1) {
-				x(2).type(Material.LODESTONE)
-				x(1).sponge()
-				x(0).sponge()
-				x(-1).sponge()
-				x(-2).type(Material.LODESTONE)
+	}
+
+	data object AdvancedShipFactoryMergeable : AdvancedShipFactoryParent() {
+		override fun MultiblockShape.buildStructure() {
+			z(2) {
+				y(-1) {
+					x(2).netheriteCasing()
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = Stairs.Shape.STRAIGHT))
+				}
+				y(0) {
+					x(2).netheriteCasing()
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = Stairs.Shape.STRAIGHT))
+				}
 			}
-			y(0) {
-				x(2).anyPipedInventory()
-				x(1).sponge()
-				x(0).anyGlass()
-				x(-1).sponge()
-				x(-2).anyPipedInventory()
+			z(1) {
+				y(-1) {
+					x(2).type(Material.LODESTONE)
+					x(1).sponge()
+					x(0).sponge()
+					x(-1).sponge()
+					x(-2).type(Material.LODESTONE)
+				}
+				y(0) {
+					x(2).type(Material.LODESTONE)
+					x(1).sponge()
+					x(0).anyGlass()
+					x(-1).sponge()
+					x(-2).anyPipedInventory()
+				}
 			}
-		}
-		z(2) {
-			y(-1) {
-				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).anyGlass()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.TOP, shape = STRAIGHT))
-			}
-			y(0) {
-				x(2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
-				x(1).ironBlock()
-				x(0).anyGlass()
-				x(-1).ironBlock()
-				x(-2).anyStairs(PrepackagedPreset.stairs(BACKWARD, Bisected.Half.BOTTOM, shape = STRAIGHT))
+			z(0) {
+				y(-1) {
+					x(2).netheriteCasing()
+					x(1).ironBlock()
+					x(0).powerInput()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.TOP, shape = Stairs.Shape.STRAIGHT))
+				}
+				y(0) {
+					x(2).netheriteCasing()
+					x(1).ironBlock()
+					x(0).anyGlass()
+					x(-1).ironBlock()
+					x(-2).anyStairs(PrepackagedPreset.stairs(FORWARD, Bisected.Half.BOTTOM, shape = Stairs.Shape.STRAIGHT))
+				}
 			}
 		}
 	}
@@ -107,7 +166,7 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 		z: Int,
 		structureDirection: BlockFace,
 	): AdvancedShipFactoryEntity {
-		return AdvancedShipFactoryEntity(data, manager, x, y, z, world, structureDirection)
+		return AdvancedShipFactoryEntity(data, this, manager, x, y, z, world, structureDirection)
 	}
 
 	override fun onSignInteract(sign: Sign, player: Player, event: PlayerInteractEvent) {
@@ -116,15 +175,18 @@ object AdvancedShipFactoryMultiblock : AbstractShipFactoryMultiblock<AdvancedShi
 
 	class AdvancedShipFactoryEntity(
 		data: PersistentMultiblockData,
+		multiblock: AdvancedShipFactoryParent,
 		manager: MultiblockManager,
 		x: Int,
 		y: Int,
 		z: Int,
 		world: World,
 		structureDirection: BlockFace
-	) : ShipFactoryEntity(data, AdvancedShipFactoryMultiblock, manager, world, x, y, z, structureDirection), PoweredMultiblockEntity {
+	) : ShipFactoryEntity(data, multiblock, manager, world, x, y, z, structureDirection), PoweredMultiblockEntity {
 		override val maxPower: Int = 300_000
 		override val powerStorage: PowerStorage = loadStoredPower(data)
+
+		val mergeEnd = createLinkage(2, -1, 1, RelativeFace.RIGHT, BazaarTerminalMultiblock.BazaarTerminalMultiblockEntity::class)
 
 		override val displayHandler: TextDisplayHandler = DisplayHandlers.newMultiblockSignOverlay(
 			this,
