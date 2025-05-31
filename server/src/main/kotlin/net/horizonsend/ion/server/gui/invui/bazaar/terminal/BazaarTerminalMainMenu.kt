5cd6e2a04c71d3f2ff9f11c767c19f07275b7b8b
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-05-31 11:53:08 -0500

    save at 4 buttons

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/terminal/BazaarTerminalMainMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/terminal/BazaarTerminalMainMenu.kt
@@ -26,25 +26,74 @@ class BazaarTerminalMainMenu(
 	override fun buildWindow(): Window {
 		val gui = Gui.normal()
 			.setStructure(
-				". . . . . . w w w ",
-				". . . . . . w w w ",
-				". . . . . . w w w ",
-				". . . . . . d d d ",
-				". . . . . . d d d ",
-				". . . . . . d d d "
+				". . . w w w p p p ",
+				". . . w w w p p p ",
+				". . . w w w p p p ",
+				". . . f f f d d d ",
+				". . . f f f d d d ",
+				". . . f f f d d d "
 			)
+			.addIngredient('f', fulfillButton)
+			.addIngredient('p', purchaseButton)
+			.addIngredient('w', withdrawOrderButton)
 			.addIngredient('d', depositButton)
-			.addIngredient('w', withdrawButton)
 			.build()
 
 		return normalWindow(gui)
 	}
 
+	override fun buildTitle(): Component {
+		val text = GuiText("Bazaar Terminal Main Menu")
+			.addBackground()
+			.setGuiIconOverlay(
+				". . . . . . . . . ",
+				"l c r . f . . p . ",
+				". . . . . . . . . ",
+				". . . . . . . . . ",
+				". . . . w . . d . ",
+				". . . . . . . . . "
+			)
+			.addIcon('l', GuiIcon.textInputBoxLeft())
+			.addIcon('c', GuiIcon.textInputBoxCenter())
+			.addIcon('r', GuiIcon.textInputBoxRight())
+			.add(Component.text("Owner"), line = 1, horizontalShift = 1)
+
+		if (terminalMultiblockEntity.isWithdrawAvailable())
+			text.addIcon('f', GuiIcon.withdrawIcon(NamedTextColor.YELLOW, true))
+		else text.addIcon('f', GuiIcon.withdrawIcon(NamedTextColor.GRAY, true))
+
+		if (terminalMultiblockEntity.isWithdrawAvailable())
+			text.addIcon('p', GuiIcon.withdrawIcon(WITHDRAW_COLOR, true))
+		else text.addIcon('p', GuiIcon.withdrawIcon(NamedTextColor.GRAY, true))
+
+		if (terminalMultiblockEntity.isDepositAvailable())
+			text.addIcon('w', GuiIcon.depositIcon(NamedTextColor.RED, true))
+		else text.addIcon('w', GuiIcon.depositIcon(NamedTextColor.GRAY, true))
+
+		if (terminalMultiblockEntity.isDepositAvailable())
+			text.addIcon('d', GuiIcon.depositIcon(DEPOSIT_COLOR, true))
+		else text.addIcon('d', GuiIcon.depositIcon(NamedTextColor.GRAY, true))
+
+		return text.build()
+	}
+
+	private val fulfillButton = FeedbackLike.withHandler({
+		if (terminalMultiblockEntity.isDepositAvailable())
+			GuiItem.EMPTY.makeItem(Component.text("Fulfill Buy Orders"))
+		else GuiItem.EMPTY.makeItem(Component.text("Buy Order Fulfillment Not Available"))
+			.updateLore(listOf(Component.text("Buy order fulfillment is not available outside of trade cities", HE_MEDIUM_GRAY)))
+
+	}) { _, _ -> handleFulfill() }
+
+	private fun handleFulfill() {
+		println("Fulfill")
+	}
+
 	private val depositButton = FeedbackLike.withHandler({
 		if (terminalMultiblockEntity.isDepositAvailable())
-			GuiItem.EMPTY.makeItem(Component.text("Deposit Items"))
-		else GuiItem.EMPTY.makeItem(Component.text("Item deposit not available"))
-			.updateLore(listOf(Component.text("Item deposit not available outside of trade cities", HE_MEDIUM_GRAY)))
+			GuiItem.EMPTY.makeItem(Component.text("Deposit Items To Your Sell Orders"))
+		else GuiItem.EMPTY.makeItem(Component.text("Item deposit Not Available"))
+			.updateLore(listOf(Component.text("Item deposit is not available outside of trade cities", HE_MEDIUM_GRAY)))
 
 	}) { _, _ -> handleDeposit() }
 
@@ -54,38 +103,27 @@ class BazaarTerminalMainMenu(
 		BazaarBulkDepositMenu(viewer, terminalMultiblockEntity).openGui(this)
 	}
 
-	private val withdrawButton = ItemProvider {
+	private val purchaseButton = ItemProvider {
 		if (terminalMultiblockEntity.isWithdrawAvailable())
-			GuiItem.EMPTY.makeItem(Component.text("Withdraw Items"))
+			GuiItem.EMPTY.makeItem(Component.text("Purchase Items"))
+		else GuiItem.EMPTY.makeItem(Component.text("Item Purchase not available"))
+			.updateLore(listOf(Component.text("The left merge port is occupying the withdraw capabilities.", HE_MEDIUM_GRAY)))
+
+	}.makeGuiButton { _, _ -> handlePurchase() }
+
+	private fun handlePurchase() {
+		println("Purchase")
+	}
+
+	private val withdrawOrderButton = ItemProvider {
+		if (terminalMultiblockEntity.isWithdrawAvailable())
+			GuiItem.EMPTY.makeItem(Component.text("Withdraw Items From Fulfilled Orders"))
 			else GuiItem.EMPTY.makeItem(Component.text("Item withdraw not available"))
 				.updateLore(listOf(Component.text("The left merge port is occupying the withdraw capabilities.", HE_MEDIUM_GRAY)))
 
-	}.makeGuiButton { _, _ -> handleWithdraw() }
+	}.makeGuiButton { _, _ -> handleWithdrawOrder() }
 
-	private fun handleWithdraw() {
+	private fun handleWithdrawOrder() {
 		println("Withdraw")
-	}
-
-	override fun buildTitle(): Component {
-		val text = GuiText("Bazaar Terminal Main Menu")
-			.addBackground()
-			.setGuiIconOverlay(
-				". . . . . . . . . ",
-				". . . . . . . w . ",
-				". . . . . . . . . ",
-				". . . . . . . . . ",
-				". . . . . . . d . ",
-				". . . . . . . . . "
-			)
-
-		if (terminalMultiblockEntity.isWithdrawAvailable())
-			text.addIcon('w', GuiIcon.withdrawIcon(WITHDRAW_COLOR, true))
-			else text.addIcon('w', GuiIcon.withdrawIcon(NamedTextColor.GRAY, true))
-
-		if (terminalMultiblockEntity.isDepositAvailable())
-			text.addIcon('d', GuiIcon.depositIcon(DEPOSIT_COLOR, true))
-			else text.addIcon('d', GuiIcon.depositIcon(NamedTextColor.GRAY, true))
-
-		return text.build()
 	}
 }
