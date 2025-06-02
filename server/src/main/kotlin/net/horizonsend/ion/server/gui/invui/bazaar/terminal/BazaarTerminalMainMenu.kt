6e1d996ad0966505e53ab8ec2e7628b82d2d0756
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-02 00:46:58 -0500

    Finish up most of the terminal main menu

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/terminal/BazaarTerminalMainMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/bazaar/terminal/BazaarTerminalMainMenu.kt
@@ -1,26 +1,38 @@
 package net.horizonsend.ion.server.gui.invui.bazaar.terminal
 
+import net.horizonsend.ion.common.database.schema.economy.BazaarItem
+import net.horizonsend.ion.common.database.schema.economy.BazaarOrder
+import net.horizonsend.ion.common.database.schema.misc.SLPlayer
+import net.horizonsend.ion.common.database.schema.nations.Settlement
+import net.horizonsend.ion.common.database.slPlayerId
 import net.horizonsend.ion.common.utils.text.clip
 import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
 import net.horizonsend.ion.common.utils.text.gui.icons.GuiIcon
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.common.utils.text.template
 import net.horizonsend.ion.server.features.economy.bazaar.Bazaars
+import net.horizonsend.ion.server.features.economy.city.TradeCities
+import net.horizonsend.ion.server.features.economy.city.TradeCityType
 import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiText
+import net.horizonsend.ion.server.features.multiblock.type.DisplayNameMultilblock.Companion.getDisplayName
 import net.horizonsend.ion.server.features.multiblock.type.economy.BazaarTerminalMultiblock
+import net.horizonsend.ion.server.features.nations.region.Regions
+import net.horizonsend.ion.server.features.nations.region.types.RegionTerritory
 import net.horizonsend.ion.server.gui.invui.InvUIWindowWrapper
 import net.horizonsend.ion.server.gui.invui.bazaar.getMenuTitleName
 import net.horizonsend.ion.server.gui.invui.utils.buttons.FeedbackLike
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
+import net.horizonsend.ion.server.miscellaneous.utils.slPlayerId
 import net.horizonsend.ion.server.miscellaneous.utils.updateLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.NamedTextColor.WHITE
 import net.kyori.adventure.text.format.TextColor
-import org.bukkit.Material
 import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemStack
+import org.litote.kmongo.and
+import org.litote.kmongo.eq
 import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.window.Window
@@ -29,21 +41,43 @@ class BazaarTerminalMainMenu(
 	viewer: Player,
 	private val terminalMultiblockEntity: BazaarTerminalMultiblock.BazaarTerminalMultiblockEntity
 ) : InvUIWindowWrapper(viewer, async = true) {
+	private var saleCount: Int = 0
+	private var orderCount: Int = 0
+	private var ownerName: String = "Unknown"
+
+	override fun firstTimeSetup() {
+		saleCount = BazaarItem.count(and(BazaarItem::seller eq viewer.slPlayerId, BazaarItem::cityTerritory eq cityData?.territoryId)).toInt()
+		orderCount = BazaarOrder.count(and(BazaarOrder::player eq viewer.slPlayerId, BazaarOrder::cityTerritory eq cityData?.territoryId)).toInt()
+
+		val owner = terminalMultiblockEntity.owner
+		ownerName = if (owner != null) SLPlayer.getName(owner.slPlayerId) ?: "Unknown" else "Unclaimed"
+	}
+
 	override fun buildWindow(): Window {
 		val gui = Gui.normal()
 			.setStructure(
-				". . . . . . . . . ",
-				". . . b b b d d d ",
-				". . . b b b d d d ",
-				". . . . . . . . . ",
-				". . . r r r f f f ",
-				". . . r r r f f f "
+				"c c c . . . . . . ",
+				"o o o b b b d d d ",
+				"l l l b b b d d d ",
+				"S S S . . . . . . ",
+				"O O O r r r f f f ",
+				"s . . r r r f f f "
 			)
+
 			.addIngredient('b', buyButton)
 			.addIngredient('d', depositButton)
 
 			.addIngredient('r', recieveButton)
 			.addIngredient('f', fulfillButton)
+
+			.addIngredient('c', territoryButtons)
+			.addIngredient('o', orderBackingItem)
+			.addIngredient('l', saleBackingItem)
+			.addIngredient('S', statusBackingItem)
+			.addIngredient('O', ownerBackingItem)
+
+			.addIngredient('s', settingsButton)
+
 			.build()
 
 		return normalWindow(gui)
@@ -91,14 +125,67 @@ class BazaarTerminalMainMenu(
 			.add(getMenuTitleName(text("Orders", WHITE)), alignment = GuiText.TextAlignment.CENTER, line = 7, horizontalShift = 96 + 12 + 5)
 			.build()
 
-		val title = ItemStack(Material.WAXED_WEATHERED_COPPER).displayName()
-
 		val textBody = GuiText("", guiWidth = 48)
-			.add(getMenuTitleName(title.clip(48, true)))
+			.add(text("City:"), line = 0)
+			.add(getMenuTitleName(text(territoryName, WHITE)).clip(48, true), line = 1)
+			.add(text("Orders:"), line = 2)
+			.add(getOrderText(), line = 3)
+			.add(text("Sale Items:"), line = 4)
+			.add(getSaleText(), line = 5)
+			.add(text("Status:"), line = 6)
+			.add(getStatusText(), line = 7)
+			.add(text("Owner:"), line = 8)
+			.add(getOwnerName(), line = 9)
 			.build()
 
 		return ofChildren(text.build(), buttonLabels1, buttonLabels2, textBody)
 	}
+
+	private fun getSaleText(): Component {
+		return getMenuTitleName(text(saleCount, WHITE))
+	}
+
+	private val saleBackingItem get() = GuiItem.EMPTY.makeItem(text("Sold Items in This Territory"))
+		.updateLore(listOf(
+			template(text("You are selling {0} items in {1}.", HE_MEDIUM_GRAY), saleCount, cityData?.displayName)
+		))
+
+	private fun getOrderText(): Component {
+		return getMenuTitleName(text(orderCount, WHITE))
+	}
+
+	private val orderBackingItem get() = GuiItem.EMPTY.makeItem(text("Item Orders in This Territory"))
+		.updateLore(listOf(
+			template(text("You have ordered {0} items at {1}.", HE_MEDIUM_GRAY), orderCount, cityData?.displayName)
+		))
+
+	private fun getStatusText(): Component {
+		val string = if (terminalMultiblockEntity.mergeEnd?.get() != null) "Merged" else "Single"
+		return getMenuTitleName(text(string, WHITE))
+	}
+
+	private val statusBackingItem get() = GuiItem.EMPTY.makeItem(text("Multiblock Merger Status"))
+		.updateLore(listOf(
+			template(text(if (terminalMultiblockEntity.mergeEnd?.get() != null) "This multiblock is merged with {0}." else "This multiblock is not merged with any other.", HE_MEDIUM_GRAY), terminalMultiblockEntity.mergeEnd?.get()?.multiblock?.getDisplayName())
+		))
+
+	private fun getOwnerName(): Component {
+		return getMenuTitleName(text(ownerName, WHITE).clip(48, true))
+	}
+
+	private val ownerBackingItem get() = GuiItem.EMPTY.makeItem(text("Multiblock Ownership"))
+		.updateLore(listOf(
+			template(text("This multiblock is owned by {0}", HE_MEDIUM_GRAY), ownerName)
+		))
+
+	private val cityData = Regions.findFirstOf<RegionTerritory>(terminalMultiblockEntity.location)?.let(TradeCities::getIfCity)
+	private val territoryName = cityData?.displayName ?: "Unknown"
+
+	private val territoryButtons = GuiItem.EMPTY.makeItem(text("Current Trade City"))
+		.updateLore(listOf(
+			text(territoryName, HE_MEDIUM_GRAY),
+			template(text("Status: {0}", HE_MEDIUM_GRAY), if (cityData?.type == TradeCityType.SETTLEMENT) Settlement.findOnePropById(cityData.settlementId, Settlement::cityState)?.name ?: "Unregistered" else if (cityData != null) "Active" else "Unregistered")
+		))
 
 	private val fulfillButton = FeedbackLike.withHandler({
 		if (terminalMultiblockEntity.isDepositAvailable())
@@ -111,7 +198,7 @@ class BazaarTerminalMainMenu(
 		val cityCheck = Bazaars.checkInValidCity(viewer)
 		if (!cityCheck.isSuccess()) return fulfillButton.updateWith(cityCheck)
 
-		println("Fulfill")
+		println("Fulfill") //TODO
 	}
 
 	private val depositButton = FeedbackLike.withHandler({
@@ -136,7 +223,7 @@ class BazaarTerminalMainMenu(
 	}.makeGuiButton { _, _ -> handlePurchase() }
 
 	private fun handlePurchase() {
-		println("Purchase")
+		println("Purchase") //TODO
 	}
 
 	private val recieveButton = ItemProvider {
@@ -148,6 +235,8 @@ class BazaarTerminalMainMenu(
 	}.makeGuiButton { _, _ -> handleWithdrawOrder() }
 
 	private fun handleWithdrawOrder() {
-		println("Withdraw")
+		println("Withdraw") //TODO
 	}
+
+	private val settingsButton = GuiItem.GEAR //TODO
 }
