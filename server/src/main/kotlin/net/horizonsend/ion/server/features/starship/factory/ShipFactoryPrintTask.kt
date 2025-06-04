ff46b7c26bb7a7c6059a34b89f506e5f267c6d2f
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-04 02:00:55 -0500

    Ship factory cleanup, start on bazaar integration

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/factory/ShipFactoryPrintTask.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/factory/ShipFactoryPrintTask.kt
@@ -8,11 +8,14 @@ import net.horizonsend.ion.common.database.schema.starships.Blueprint
 import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.extensions.userError
+import net.horizonsend.ion.common.utils.input.InputResult
 import net.horizonsend.ion.common.utils.miscellaneous.roundToHundredth
 import net.horizonsend.ion.common.utils.text.colors.HEColorScheme
 import net.horizonsend.ion.common.utils.text.formatPaginatedMenu
 import net.horizonsend.ion.common.utils.text.ofChildren
+import net.horizonsend.ion.common.utils.text.template
 import net.horizonsend.ion.common.utils.text.toComponent
+import net.horizonsend.ion.common.utils.text.toCreditComponent
 import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.configuration.ConfigurationFiles
 import net.horizonsend.ion.server.features.multiblock.MultiblockEntities
@@ -35,6 +38,7 @@ import net.horizonsend.ion.server.miscellaneous.utils.setNMSBlockData
 import net.horizonsend.ion.server.miscellaneous.utils.withdrawMoney
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor.DARK_GRAY
+import net.kyori.adventure.text.format.NamedTextColor.GREEN
 import net.kyori.adventure.text.format.NamedTextColor.RED
 import net.kyori.adventure.text.format.NamedTextColor.WHITE
 import net.starlegacy.javautil.SignUtils.SignData
@@ -103,6 +107,7 @@ class ShipFactoryPrintTask(
 
 		if (availableCredits <= 0.0) {
 			player.userError("You don't have enough credits to print!")
+			gui?.enableButton?.updateWith(InputResult.FailureReason(listOf(text("You don't have enough credits to print!", RED))))
 			disable()
 		}
 
@@ -134,94 +139,32 @@ class ShipFactoryPrintTask(
 				IonServer.slF4JLogger.warn("$blockData has no print item!")
 				continue
 			}
+
 			val requiredAmount = StarshipFactories.getRequiredAmount(blockData)
 
-			val replaceableTag = registryAccess().getRegistry(RegistryKey.BLOCK).getTag(BlockTypeTagKeys.REPLACEABLE)
-			val replaceable = replaceableTag.contains(TypedKey.create(RegistryKey.BLOCK, worldBlockData.material.asBlockType()!!.key))
-
-			// Whether to skip the obstruction check because it replaceable and that setting is enabled
-			val isAllowedReplaceable = replaceable && settings.overrideReplaceableBlocks
-
-			// Whether to skip the obstruction check because it water and that setting is enabled
-			val isAllowedWater = worldBlockData.material == Material.WATER && settings.placeBlocksUnderwater
-
-			// If it is air then it can be placed.
-			// If it is not air, AND not replaceable (if replaceables are marked as obstructing), OR in water (if water placement is not allowed)
-			// then placement is obstructed
-			if (!worldBlockData.material.isAir) {
-				// Air is replacable, so the check should only be done if it is not air
-				if (!isAllowedReplaceable && !isAllowedWater) {
-					// Continue if it should just be marked as complete, not missing
-					if (settings.markObstrcutedBlocksAsComplete) continue
-
-					// Mark missing
-					skippedBlocks++
-					markItemMissing(printItem, requiredAmount)
-
-					// Move onto next block
-					continue
-				}
-			}
-
-			val price = ShipFactoryMaterialCosts.getPrice(blockData)
+			if (checkObstruction(printItem, blockData, requiredAmount)) continue
 
 			var toBreak = false
 
-			val anyAvailable = areResourcesAvailable(availableItems, printItem, requiredAmount) { result: Boolean, resources ->
-				if (result && availableCredits >= price) {
-					// Mark as available to print
-					toPrint.add(key)
-					usedPower += 10
-
-					if (entity is AdvancedShipFactoryParent.AdvancedShipFactoryEntity) {
-						val power = entity.powerStorage.getPower()
-						if (power < usedPower) {
-							entity.statusManager.setStatus(text("Insufficient Power!", RED))
-							player.userError("Ship factory has insufficient power")
-							toBreak = true
-							disable()
-							return@areResourcesAvailable
-						}
-					}
-
-					// Decrement available resources
-					resources.amount.addAndGet(-requiredAmount)
-
-					// If it were missing before, and is not now, it was likely added to the inventory
-					if (missingMaterials.containsKey(printItem)) {
-						val atomic = missingMaterials[printItem]
-
-						if (atomic != null) {
-							if (atomic.get() >= requiredAmount) atomic.addAndGet(-requiredAmount)
-						}
-					}
-
-					availableCredits -= price
-					consumedCredits += price
-
-					return@areResourcesAvailable
-				}
-
-				if (availableCredits < price) {
-					entity.statusManager.setStatus(text("Insufficient Credits!", RED))
-					player.userError("Insufficient Credits!")
-					toBreak = true
-				}
-
-				markItemMissing(printItem, requiredAmount)
+			val breakLoop = {
+				toBreak = true
+				disable()
 			}
 
+			val consumeCredits = { credits: Double -> consumedCredits += credits; usedPower += 10 }
+
+			checkResource(key, availableItems, printItem, blockData, requiredAmount, availableCredits, toPrint, consumeCredits, breakLoop)
 			if (toBreak) break
-
-			if (!anyAvailable) {
-				markItemMissing(printItem, requiredAmount)
-			}
 		}
 
 		if (toPrint.isEmpty()) {
 			if (missingMaterials.isNotEmpty()) {
 				sendMissing(missingMaterials)
 				player.information("Printing consumed C${consumedCredits.roundToHundredth()}")
+				gui?.enableButton?.updateWith(InputResult.FailureReason(listOf(
+					text("Missing Materials!", RED),
+					template(text("Printing consumed {0}", GREEN), consumedCredits.toCreditComponent())
+				)))
 			}
 
 			entity.disable()
@@ -238,6 +181,10 @@ class ShipFactoryPrintTask(
 
 			player.success("Ship factory has finished printing.")
 			player.information("Printing consumed C${consumedCredits.roundToHundredth()}")
+			gui?.enableButton?.updateWith(InputResult.SuccessReason(listOf(
+				text("Ship factory has finished printing.", GREEN),
+				template(text("Printing consumed {0}", GREEN), consumedCredits.toCreditComponent())
+			)))
 			entity.disable()
 		} else {
 			updateStatus()
@@ -349,22 +296,108 @@ class ShipFactoryPrintTask(
 		return items
 	}
 
-	private fun areResourcesAvailable(
-		availableItems: Map<PrintItem, AvailableItemInformation>,
+	private fun checkObstruction(
 		printItem: PrintItem,
-		requiredAmount: Int,
-		resultConsumer: (Boolean, AvailableItemInformation) -> Unit = { _, _ -> }
+		worldBlockData: BlockData,
+		requiredAmount: Int
 	): Boolean {
-		val information = availableItems[printItem] ?: return false
+		val replaceableTag = registryAccess().getRegistry(RegistryKey.BLOCK).getTag(BlockTypeTagKeys.REPLACEABLE)
+		val replaceable = replaceableTag.contains(TypedKey.create(RegistryKey.BLOCK, worldBlockData.material.asBlockType()!!.key))
 
-		if (information.amount.get() < requiredAmount) {
-			resultConsumer.invoke(false, information)
-			return true
+		// Whether to skip the obstruction check because it replaceable and that setting is enabled
+		val isAllowedReplaceable = replaceable && settings.overrideReplaceableBlocks
+
+		// Whether to skip the obstruction check because it water and that setting is enabled
+		val isAllowedWater = worldBlockData.material == Material.WATER && settings.placeBlocksUnderwater
+
+		// If it is air then it can be placed.
+		// If it is not air, AND not replaceable (if replaceables are marked as obstructing), OR in water (if water placement is not allowed)
+		// then placement is obstructed
+		if (!worldBlockData.material.isAir) {
+			// Air is replacable, so the check should only be done if it is not air
+			if (!isAllowedReplaceable && !isAllowedWater) {
+				// Continue if it should just be marked as complete, not missing
+				if (settings.markObstrcutedBlocksAsComplete) return false
+
+				// Mark missing
+				skippedBlocks++
+				markItemMissing(printItem, requiredAmount)
+
+				// Move onto next block
+				return false
+			}
 		}
 
-		resultConsumer.invoke(true, information)
 		return true
 	}
+
+	private fun checkResource(
+		position: BlockKey,
+		availableItems: Map<PrintItem, AvailableItemInformation>,
+		printItem: PrintItem,
+		printItemBlockData: BlockData,
+		requiredAmount: Int,
+		availableCredits: Double,
+		toPrint: MutableList<BlockKey>,
+		consumeResourcesFunction: (Double) -> Unit,
+		breakLoopFunction: () -> Unit
+	) {
+		val price = ShipFactoryMaterialCosts.getPrice(printItemBlockData)
+
+		if (availableCredits < price) {
+			gui?.enableButton?.updateWith(InputResult.FailureReason(listOf(text("Insufficent Credits!", RED))))
+			entity.statusManager.setStatus(text("Insufficient Credits!", RED))
+			player.userError("Insufficient Credits!")
+
+			breakLoopFunction.invoke()
+			disable()
+			return
+		}
+
+		val resourceInformation = availableItems[printItem]
+
+		if (resourceInformation == null) {
+			markItemMissing(printItem, requiredAmount)
+			// Don't break loop
+			return
+		}
+
+		if (resourceInformation.amount.get() < requiredAmount) {
+			markItemMissing(printItem, requiredAmount - resourceInformation.amount.get())
+			// Don't break loop
+			return
+		}
+
+		toPrint.add(position)
+
+		if (entity is AdvancedShipFactoryParent.AdvancedShipFactoryEntity) {
+			val power = entity.powerStorage.getPower()
+			if (power < toPrint.size * 10) {
+				gui?.enableButton?.updateWith(InputResult.FailureReason(listOf(text("Insufficient Power!", RED))))
+				entity.statusManager.setStatus(text("Insufficient Power!", RED))
+				player.userError("Ship factory has insufficient power")
+
+				breakLoopFunction.invoke()
+				disable()
+				return
+			}
+		}
+
+		resourceInformation.amount.addAndGet(-requiredAmount)
+
+		if (missingMaterials.containsKey(printItem)) {
+			val atomic = missingMaterials[printItem]
+
+			if (atomic != null) {
+				if (atomic.get() >= requiredAmount) atomic.addAndGet(-requiredAmount)
+			}
+		}
+
+		consumeResourcesFunction.invoke(price)
+		consumedCredits += price
+	}
+
+	private data class AvailableItemInformation(val amount: AtomicInteger, val references: MutableList<ItemReference>)
 
 	private fun sendMissing(missingMaterials: MutableMap<PrintItem, AtomicInteger>) {
 		missingMaterialsCache[player.uniqueId] = missingMaterials.mapValues { it.value.get() }
@@ -391,26 +424,24 @@ class ShipFactoryPrintTask(
 		player.userError("Use <italic><underlined><click:run_command:/shipfactory listmissing all>/shipfactory listmissing all</click></italic> to list all missing materials in one message.")
 	}
 
-	companion object {
-		fun consumeItemFromReferences(references: Collection<ItemReference>, amount: Int): Int {
-			var remaining = amount
+	private fun consumeItemFromReferences(references: Collection<ItemReference>, amount: Int): Int {
+		var remaining = amount
 
-			for (reference in references) {
-				val item = reference.get() ?: continue
-				val stackAmount = item.amount
+		for (reference in references) {
+			val item = reference.get() ?: continue
+			val stackAmount = item.amount
 
-				if (remaining >= stackAmount) {
-					remaining -= stackAmount
-					reference.inventory.setItem(reference.index, null)
-				} else {
-					val toRemove = minOf(stackAmount, remaining)
-					item.amount -= toRemove
-					remaining -= toRemove
-				}
+			if (remaining >= stackAmount) {
+				remaining -= stackAmount
+				reference.inventory.setItem(reference.index, null)
+			} else {
+				val toRemove = minOf(stackAmount, remaining)
+				item.amount -= toRemove
+				remaining -= toRemove
 			}
-
-			return remaining
 		}
+
+		return remaining
 	}
 
 	private fun markItemMissing(printItem: PrintItem, amount: Int): Int {
