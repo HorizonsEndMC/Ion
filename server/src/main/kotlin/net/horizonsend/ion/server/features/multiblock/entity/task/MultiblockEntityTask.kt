e11e00aaa7d43061fa6c35cd275220e2ad060c06
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-05 15:10:02 -0500

    Further organize and cleanup

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/task/MultiblockEntityTask.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/entity/task/MultiblockEntityTask.kt
@@ -3,6 +3,8 @@ package net.horizonsend.ion.server.features.multiblock.entity.task
 import net.horizonsend.ion.server.features.multiblock.entity.MultiblockEntity
 
 interface MultiblockEntityTask<T: MultiblockEntity> {
+	var isDisabled: Boolean
+
 	val taskEntity: T
 
 	fun disable()
