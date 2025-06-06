f989b418e0ac1a4b75c032f798172cc0925a8caa
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-06 00:55:25 -0500

    And left and right variants of mergeable ship factories and bazaar terminals, add double merge variant of the ship factory

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/MultiblockRegistration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/MultiblockRegistration.kt
@@ -392,7 +392,9 @@ object MultiblockRegistration : IonServerComponent() {
 		// Machine
 		registerMultiblock(ShipFactoryMultiblock)
 		registerMultiblock(AdvancedShipFactoryMultiblock)
-		registerMultiblock(AdvancedShipFactoryParent.AdvancedShipFactoryMergeable)
+		registerMultiblock(AdvancedShipFactoryParent.AdvancedShipFactoryMergeableRight)
+		registerMultiblock(AdvancedShipFactoryParent.AdvancedShipFactoryMergeableLeft)
+		registerMultiblock(AdvancedShipFactoryParent.AdvancedShipFactoryMergeableDouble)
 
 		registerMultiblock(DecomposerMultiblock)
 
@@ -407,8 +409,9 @@ object MultiblockRegistration : IonServerComponent() {
 		registerMultiblock(MediumTractorBeamMultiblock)
 		registerMultiblock(LargeTractorBeamMultiblock)
 
-		registerMultiblock(BazaarTerminalMultiblock.BazaarTerminalStandardMultiblock)
-		registerMultiblock(BazaarTerminalMultiblock.BazaarTerminalMergeableMultiblock)
+		registerMultiblock(BazaarTerminalMultiblock.BazaarTerminalMultiblockStandard)
+		registerMultiblock(BazaarTerminalMultiblock.BazaarTerminalMultiblockMergeableRight)
+		registerMultiblock(BazaarTerminalMultiblock.BazaarTerminalMultiblockMergeableLeft)
 
 		registerMultiblock(AntiAirCannonBaseMultiblock)
 	}
