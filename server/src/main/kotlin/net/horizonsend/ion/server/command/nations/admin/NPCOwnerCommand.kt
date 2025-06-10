6c9cad0fbca34ac7a01effa2ec05e20c15364152
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-10 13:42:09 -0500

    Rental area management commands

--- server/src/main/kotlin/net/horizonsend/ion/server/command/nations/admin/NPCOwnerCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/nations/admin/NPCOwnerCommand.kt
@@ -1,23 +1,47 @@
 package net.horizonsend.ion.server.command.nations.admin
 
+import co.aikar.commands.InvalidCommandArgument
+import co.aikar.commands.PaperCommandManager
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
+import com.sk89q.worldedit.regions.CuboidRegion
+import net.horizonsend.ion.common.database.schema.economy.StationRentalArea
+import net.horizonsend.ion.common.database.schema.misc.SLPlayer
 import net.horizonsend.ion.common.database.schema.nations.NPCTerritoryOwner
 import net.horizonsend.ion.common.database.schema.nations.spacestation.NPCSpaceStation
+import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.common.extensions.success
 import net.horizonsend.ion.common.utils.text.isAlphanumeric
+import net.horizonsend.ion.server.IonServer
 import net.horizonsend.ion.server.command.SLCommand
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.cube
+import net.horizonsend.ion.server.miscellaneous.utils.runnable
 import org.bukkit.Color
+import org.bukkit.Particle
 import org.bukkit.World
 import org.bukkit.entity.Player
 import org.litote.kmongo.and
+import org.litote.kmongo.combine
 import org.litote.kmongo.eq
 import org.litote.kmongo.setValue
+import java.util.Date
+import java.util.concurrent.TimeUnit
 
 @CommandPermission("nations.npcowner")
 @CommandAlias("npcowner")
 internal object NPCOwnerCommand : SLCommand() {
+	override fun onEnable(manager: PaperCommandManager) {
+		manager.commandCompletions.registerAsyncCompletion("npcStations") { NPCSpaceStation.all().map(NPCSpaceStation::name) }
+		manager.commandCompletions.registerAsyncCompletion("rentalAreas") { context ->
+			val stationName = context.getContextValueByName(String::class.java, "stationName") ?: throw InvalidCommandArgument("Station not specified!")
+			val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq stationName) ?: throw InvalidCommandArgument("Station not found!")
+			StationRentalArea.find(StationRentalArea::station eq station._id).map(StationRentalArea::name).toList()
+		}
+	}
+
 	private fun validateTerritoryName(name: String) {
 		failIf(!name.isAlphanumeric()) { "Name must be alphanumeric" }
 
@@ -91,6 +115,7 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station delete")
+	@CommandCompletion("@npcStations")
 	fun onDeleteStation(sender: Player, name: String) = asyncCommand(sender) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 		NPCSpaceStation.delete(station._id)
@@ -98,6 +123,7 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station set color")
+	@CommandCompletion("@npcStations")
 	fun onSetStationColor(sender: Player, name: String, red: Int, green: Int, blue: Int) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 
@@ -108,6 +134,7 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station set radius")
+	@CommandCompletion("@npcStations")
 	fun onSetStationRadius(sender: Player, name: String, value: Int) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 
@@ -116,7 +143,8 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station set description")
-	fun onSetStationDescription(sender: Player, name: String, description: String,) {
+	@CommandCompletion("@npcStations")
+	fun onSetStationDescription(sender: Player, name: String, description: String) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 
 		NPCSpaceStation.updateById(station._id, setValue(NPCSpaceStation::dynmapDescription, description))
@@ -124,6 +152,7 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station set protected")
+	@CommandCompletion("@npcStations")
 	fun onSetStationProtection(sender: Player, name: String, protected: Boolean) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 
@@ -132,14 +161,77 @@ internal object NPCOwnerCommand : SLCommand() {
 	}
 
 	@Subcommand("station set location")
+	@CommandCompletion("@npcStations")
 	fun onSetStationLocation(sender: Player, name: String, world: World, x: Int, z: Int) {
 		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq name) ?: fail { "Station $name not found!" }
 
-		NPCSpaceStation.updateById(station._id, and(
+		NPCSpaceStation.updateById(station._id, combine(
 			setValue(NPCSpaceStation::world, world.name),
 			setValue(NPCSpaceStation::x, x),
 			setValue(NPCSpaceStation::z, z),
 		))
 		sender.success("Set the location of $name!")
+	}
+
+	@Subcommand("station rentalarea create")
+	@CommandCompletion("@npcStations")
+	fun onSetRentalAreaStation(sender: Player, stationName: String, regionName: String, world: World, x: Int, z: Int, rent: Double) {
+		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq stationName) ?: fail { "Station $stationName not found!" }
+
+		val selection = requireSelection(sender)
+		if (selection !is CuboidRegion) fail { "You must make a cuboid selection!" }
+
+		val minPoint = Vec3i(selection.minimumPoint.x(), selection.minimumPoint.y(), selection.minimumPoint.z())
+		val maxPoint = Vec3i(selection.maximumPoint.x(), selection.maximumPoint.y(), selection.maximumPoint.z())
+
+		StationRentalArea.create(regionName, station._id, sender.world.name, minPoint, maxPoint, rent)
+		sender.information("Created rental area $regionName in station $stationName")
+	}
+
+	@Subcommand("station rentalarea outline")
+	@CommandCompletion("@npcStations @rentalAreas")
+	fun outline(sender: Player, stationName: String, regionName: String, durationSeconds: Long) {
+		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq stationName) ?: fail { "Station $stationName not found!" }
+		val rentalArea = StationRentalArea.findOne(and(StationRentalArea::station eq station._id, StationRentalArea::station eq station._id)) ?: fail { "Rental area $regionName not found!" }
+
+		val points = cube(Vec3i(rentalArea.minPoint).toLocation(sender.world), Vec3i(rentalArea.maxPoint).toLocation(sender.world).add(1.0, 1.0, 1.0))
+
+		val startTime = System.currentTimeMillis()
+
+		runnable {
+			if ((System.currentTimeMillis() - startTime) > TimeUnit.SECONDS.toMillis(durationSeconds) || !sender.isOnline) {
+				cancel()
+				return@runnable
+			}
+
+			for (point in points) {
+				// I can't think of a better way to do this
+				sender.spawnParticle(Particle.SOUL_FIRE_FLAME, point, 1, 0.0, 0.0, 0.0, 0.0)
+			}
+		}.runTaskTimerAsynchronously(IonServer, 10L, 10L)
+	}
+
+	@Subcommand("station rentalarea clearowner")
+	@CommandCompletion("@npcStations @rentalAreas")
+	fun clearOwner(sender: Player, stationName: String, regionName: String) {
+		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq stationName) ?: fail { "Station $stationName not found!" }
+		val rentalArea = StationRentalArea.findOne(and(StationRentalArea::station eq station._id, StationRentalArea::station eq station._id)) ?: fail { "Rental area $regionName not found!" }
+
+		StationRentalArea.removeOwner(rentalArea._id)
+	}
+
+	@Subcommand("station rentalarea info")
+	@CommandCompletion("@npcStations @rentalAreas @nothing")
+	fun rentalAreaInfo(sender: Player, stationName: String, regionName: String) {
+		val station = NPCSpaceStation.findOne(NPCSpaceStation::name eq stationName) ?: fail { "Station $stationName not found!" }
+		val rentalArea = StationRentalArea.findOne(and(StationRentalArea::station eq station._id, StationRentalArea::station eq station._id)) ?: fail { "Rental area $regionName not found!" }
+
+		sender.information("Station: $stationName")
+		sender.information("Rental area: $regionName")
+		sender.information("Min Point: ${rentalArea.minPoint}")
+		sender.information("Rent: ${rentalArea.rent}")
+		sender.information("Owner: ${rentalArea.owner?.let(SLPlayer::getName)}")
+		sender.information("Rent Balance: ${rentalArea.rentBalance}")
+		sender.information("Rent last charged: ${Date(rentalArea.rentLastCharged)}")
 	}
 }
