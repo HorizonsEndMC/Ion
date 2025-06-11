84afbb05d3e0116db7a0562e4fc904f98a2f9b97
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-11 15:52:02 -0500

    Claiming and abandonment functionality

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/misc/util/ConfirmationMenu.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/misc/util/ConfirmationMenu.kt
@@ -6,6 +6,7 @@ import net.horizonsend.ion.server.features.gui.GuiItem
 import net.horizonsend.ion.server.features.gui.GuiText
 import net.horizonsend.ion.server.gui.CommonGuiWrapper
 import net.horizonsend.ion.server.gui.invui.InvUIWindowWrapper
+import net.horizonsend.ion.server.gui.invui.utils.buttons.FeedbackLike
 import net.horizonsend.ion.server.gui.invui.utils.buttons.makeGuiButton
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor.GREEN
@@ -13,8 +14,15 @@ import net.kyori.adventure.text.format.NamedTextColor.RED
 import org.bukkit.entity.Player
 import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.window.Window
+import java.util.function.Supplier
 
-class ConfirmationMenu(parentWindow: CommonGuiWrapper?, viewer: Player, val title: GuiText, private val onConfirm: () -> Unit) : InvUIWindowWrapper(viewer, async = true) {
+class ConfirmationMenu(
+	parentWindow: CommonGuiWrapper?,
+	viewer: Player,
+	val title: GuiText,
+	val confirmationFallbackLore: Supplier<List<Component>> = Supplier { listOf() },
+	private val onConfirm: FeedbackLike.() -> Unit,
+) : InvUIWindowWrapper(viewer, async = true) {
 	override fun buildWindow(): Window {
 		val gui = Gui.normal()
 			.setStructure(
@@ -51,19 +59,22 @@ class ConfirmationMenu(parentWindow: CommonGuiWrapper?, viewer: Player, val titl
 		if (parentWindow == null) GuiItem.EMPTY.makeItem(Component.text("Go back")).makeGuiButton { _, _ -> viewer.closeInventory() }
 		else GuiItem.EMPTY.makeItem(Component.text("Go back to previous menu")).makeGuiButton { _, _ -> parentWindow.openGui() }
 
-	private val confirmButton = GuiItem.EMPTY.makeItem(Component.text("Confirm")).makeGuiButton { _, _ -> onConfirm.invoke() }
+	private val confirmButton = FeedbackLike.withHandler(GuiItem.EMPTY.makeItem(Component.text("Confirm")), fallbackLoreProvider = confirmationFallbackLore) { _, _ -> runConfimration()}
+	private fun runConfimration() {
+		onConfirm.invoke(confirmButton)
+	}
 
 	companion object {
-		fun promptConfirmation(viewer: Player, prompt: GuiText, onConfirm: () -> Unit) {
-			ConfirmationMenu(null, viewer, prompt, onConfirm).openGui()
+		fun promptConfirmation(viewer: Player, prompt: GuiText, confirmationFallbackLore: Supplier<List<Component>> = Supplier { listOf() },onConfirm: FeedbackLike.() -> Unit) {
+			ConfirmationMenu(null, viewer, prompt, confirmationFallbackLore, onConfirm).openGui()
 		}
 
-		fun promptConfirmation(viewer: Player, window: CommonGuiWrapper, prompt: GuiText, onConfirm: () -> Unit) {
-			ConfirmationMenu(window, viewer, prompt, onConfirm).openGui()
+		fun promptConfirmation(viewer: Player, window: CommonGuiWrapper, prompt: GuiText, confirmationFallbackLore: Supplier<List<Component>> = Supplier { listOf() },onConfirm: FeedbackLike.() -> Unit) {
+			ConfirmationMenu(window, viewer, prompt, confirmationFallbackLore, onConfirm).openGui()
 		}
 
-		fun promptConfirmation(window: InvUIWindowWrapper, prompt: GuiText, onConfirm: () -> Unit) {
-			ConfirmationMenu(window, window.viewer, prompt, onConfirm).openGui()
+		fun promptConfirmation(window: InvUIWindowWrapper, prompt: GuiText, confirmationFallbackLore: Supplier<List<Component>> = Supplier { listOf() },onConfirm: FeedbackLike.() -> Unit) {
+			ConfirmationMenu(window, window.viewer, prompt, confirmationFallbackLore, onConfirm).openGui()
 		}
 	}
 }
