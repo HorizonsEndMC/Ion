39de06da33c3dbd52780a21f11a82e63ddbe4b7a
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-13 14:36:48 -0500

    Half of the messaging system

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxyComponent.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/IonProxyComponent.kt
@@ -1,5 +1,6 @@
 package net.horizonsend.ion.proxy
 
 import net.horizonsend.ion.common.IonComponent
+import net.horizonsend.ion.proxy.features.cache.Listener
 
-abstract class IonProxyComponent : IonComponent()
+abstract class IonProxyComponent : IonComponent(), Listener
