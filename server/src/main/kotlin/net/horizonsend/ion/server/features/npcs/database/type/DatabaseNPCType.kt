309a156c398f0b8aedc42d5ca264c5c83aa2ef8c
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-17 12:47:37 -0500

    Get functional & add management commands

--- server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/database/type/DatabaseNPCType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/npcs/database/type/DatabaseNPCType.kt
@@ -2,10 +2,14 @@ package net.horizonsend.ion.server.features.npcs.database.type
 
 import net.citizensnpcs.api.npc.NPC
 import net.horizonsend.ion.common.utils.configuration.Configuration
+import net.horizonsend.ion.server.features.npcs.database.DatabaseNPCs
 import net.horizonsend.ion.server.features.npcs.database.UniversalNPCWrapper
 import net.horizonsend.ion.server.features.npcs.database.metadata.UniversalNPCMetadata
+import net.horizonsend.ion.server.miscellaneous.utils.Skins
 import net.kyori.adventure.text.Component
+import org.bukkit.Location
 import org.bukkit.entity.Player
+import java.util.function.Consumer
 import kotlin.reflect.KClass
 
 interface DatabaseNPCType<T: UniversalNPCMetadata> {
@@ -29,4 +33,17 @@ interface DatabaseNPCType<T: UniversalNPCMetadata> {
 	fun handleClick(player: Player, npc: NPC, metaData: T)
 
 	fun canUseType(player: Player, metaData: T): Boolean
+	fun canManage(player: Player, wrapper: UniversalNPCWrapper<*, T>): Boolean
+
+	fun getDefaultMetaData(): T
+
+	fun createNew(location: Location, skinData: Skins.SkinData) {
+		DatabaseNPCs.create(location, this, getDefaultMetaData(), skinData)
+	}
+
+	fun createNew(player: Player, skinData: Skins.SkinData) {
+		DatabaseNPCs.create(player, player.location, this, getDefaultMetaData(), skinData)
+	}
+
+	fun manage(player: Player, managed: UniversalNPCWrapper<*, T>, newMetaDataConsumer: Consumer<T>) {}
 }
