7e4bd25ba3e83a177a151c776bc07d1a46a47767
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-19 01:20:17 -0500

    delete ship when purchased, give seller money, notify of purchases

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/dealers/PlayerCreatedDealerShip.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/dealers/PlayerCreatedDealerShip.kt
@@ -2,23 +2,35 @@ package net.horizonsend.ion.server.features.starship.dealers
 
 import com.sk89q.worldedit.extent.clipboard.Clipboard
 import net.horizonsend.ion.common.database.Oid
+import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
 import net.horizonsend.ion.common.database.schema.starships.PlayerSoldShip
+import net.horizonsend.ion.common.database.uuid
 import net.horizonsend.ion.common.utils.text.colors.HEColorScheme.Companion.HE_MEDIUM_GRAY
 import net.horizonsend.ion.common.utils.text.gson
+import net.horizonsend.ion.common.utils.text.gui.sendDepositMessage
 import net.horizonsend.ion.common.utils.text.template
 import net.horizonsend.ion.common.utils.text.toCreditComponent
+import net.horizonsend.ion.server.features.economy.bazaar.Bazaars
+import net.horizonsend.ion.server.features.misc.ServerInboxes
+import net.horizonsend.ion.server.features.nations.region.Regions
+import net.horizonsend.ion.server.features.nations.region.types.RegionNPCSpaceStation
+import net.horizonsend.ion.server.features.nations.region.types.RegionTerritory
 import net.horizonsend.ion.server.features.starship.StarshipType
 import net.horizonsend.ion.server.miscellaneous.utils.actualType
+import net.horizonsend.ion.server.miscellaneous.utils.depositMoney
 import net.horizonsend.ion.server.miscellaneous.utils.loadClipboard
 import net.horizonsend.ion.server.miscellaneous.utils.updateDisplayName
 import net.horizonsend.ion.server.miscellaneous.utils.updateLore
 import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.time.Duration
 import java.util.Date
 
 class PlayerCreatedDealerShip(
 	val id: Oid<PlayerSoldShip>,
+	val seller: SLPlayerId,
 	val className: String,
 	displayName: Component,
 	val description: List<Component>,
@@ -48,9 +60,33 @@ class PlayerCreatedDealerShip(
 			.updateLore(lore)
 	}
 
+	override fun onPurchase(purchaser: Player) {
+		PlayerSoldShip.delete(id)
+
+		val name: String = when (val region = Regions.find(purchaser.location).firstOrNull()) {
+			is RegionNPCSpaceStation -> region.name
+			is RegionTerritory -> Bazaars.cityName(region)
+			else -> "UNKNOWN"
+		}
+
+		val message = template(
+			Component.text("{0} purchased a {1} class starship at {2} for {3}!", HE_MEDIUM_GRAY),
+			purchaser.name,
+			className,
+			name,
+			price.toCreditComponent()
+		)
+
+		ServerInboxes.sendServerMessage(seller, Component.text("Ship Purchased!"), message)
+
+		Bukkit.getOfflinePlayer(seller.uuid).depositMoney(price)
+		Bukkit.getPlayer(seller.uuid)?.let { sendDepositMessage(it, price) }
+	}
+
 	companion object {
 		fun create(ship: PlayerSoldShip) = PlayerCreatedDealerShip(
 			id = ship._id,
+			seller = ship.owner,
 			className = ship.className,
 			displayName = gson.deserialize(ship.name),
 			description = ship.description?.map(gson::deserialize) ?: listOf(),
