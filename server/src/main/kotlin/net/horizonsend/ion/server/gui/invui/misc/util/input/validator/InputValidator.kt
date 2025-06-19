342935e881e0de46015c682db2c354f474c940ee
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-19 15:11:50 -0500

    Add manage button to the NPC gui, add ability to change name

--- server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/misc/util/input/validator/InputValidator.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/gui/invui/misc/util/input/validator/InputValidator.kt
@@ -2,4 +2,8 @@ package net.horizonsend.ion.server.gui.invui.misc.util.input.validator
 
 fun interface InputValidator<T : Any> {
 	fun isValid(input: String): ValidatorResult<T>
+
+	companion object {
+		fun alwaysTrue(): InputValidator<String> = InputValidator { ValidatorResult.ValidatorSuccessSingleEntry(it) }
+	}
 }
