7e47b06204ef0c7bacb796c354f909d36faa1245
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-19 15:49:29 -0500

    Add title line, change skin button from the mange GUI

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/gui/custom/settings/button/general/NullableComponentSupplierConsumerInputButton.kt
@@ -0,0 +1,55 @@
+package net.horizonsend.ion.server.features.gui.custom.settings.button.general
+
+import net.horizonsend.ion.common.utils.text.clip
+import net.horizonsend.ion.common.utils.text.plainText
+import net.horizonsend.ion.common.utils.text.restrictedMiniMessageSerializer
+import net.horizonsend.ion.common.utils.text.template
+import net.horizonsend.ion.server.features.gui.GuiItem
+import net.horizonsend.ion.server.features.gui.custom.settings.SettingsPageGui
+import net.horizonsend.ion.server.gui.invui.misc.util.input.TextInputMenu.Companion.anvilInputText
+import net.horizonsend.ion.server.gui.invui.misc.util.input.validator.ValidatorResult
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor.BLUE
+import net.kyori.adventure.text.format.NamedTextColor.RED
+import org.bukkit.entity.Player
+import xyz.xenondevs.invui.gui.PagedGui
+import java.util.function.Consumer
+import java.util.function.Supplier
+
+class NullableComponentSupplierConsumerInputButton(
+	valueSupplier: Supplier<Component?>,
+	valueConsumer: Consumer<Component?>,
+	private val inputDescription: Component = Component.empty(),
+	name: Component,
+	buttonDescription: String,
+	icon: GuiItem,
+	defaultValue: Component
+) : SupplierConsumerButton<Component?>(valueSupplier, valueConsumer, name, buttonDescription, icon, defaultValue) {
+	override fun getSecondLine(player: Player): Component {
+		val value = getState(player)
+		return template(text("Current: {0}", BLUE), value?.clip(100))
+	}
+
+	override fun handleClick(clicker: Player, oldValue: Component?, gui: PagedGui<*>, parent: SettingsPageGui, newValueConsumer: Consumer<Component?>) {
+		clicker.anvilInputText(
+			prompt = text("Enter new value"),
+			description = inputDescription,
+			backButtonHandler = { parent.openGui() },
+			inputValidator = {
+				val deserialized = restrictedMiniMessageSerializer.deserializeOrNull(it)
+
+				if (deserialized != null && deserialized.plainText().isEmpty()) return@anvilInputText ValidatorResult.ValidatorSuccessSingleEntry(Component.empty())
+
+				if (deserialized != null) ValidatorResult.ValidatorSuccessSingleEntry(deserialized)
+				else ValidatorResult.FailureResult(text("Could not deserialize component!", RED))
+			},
+			componentTransformer = { it },
+			handler = { _, (_, result) ->
+				val formattted = if (result == Component.empty()) null else result.result
+				newValueConsumer.accept(formattted)
+				parent.openGui()
+			}
+		)
+	}
+}
