4cc2aa35bbb188ae20339c9f68ef7559fb27a420
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-29 16:55:06 -0500

    Generalize cache factories to cache more than nodes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/util/BlockBasedCacheFactory.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/nodes/util/BlockBasedCacheFactory.kt
@@ -5,7 +5,6 @@ import net.horizonsend.ion.server.core.registration.IonRegistryKey
 import net.horizonsend.ion.server.core.registration.registries.CustomBlockRegistry.Companion.customBlock
 import net.horizonsend.ion.server.features.custom.blocks.CustomBlock
 import net.horizonsend.ion.server.features.transport.manager.holders.CacheHolder
-import net.horizonsend.ion.server.features.transport.nodes.types.Node
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toBlockKey
 import net.horizonsend.ion.server.miscellaneous.utils.getBlockDataSafe
@@ -16,8 +15,8 @@ import org.bukkit.block.Block
 import org.bukkit.block.data.BlockData
 import kotlin.reflect.KClass
 
-class NodeCacheFactory private constructor(private val materialHandlers: Multimap<Material, MaterialHandler<*>>) {
-	fun cache(block: Block, holder: CacheHolder<*>): Node? {
+class BlockBasedCacheFactory<T : Any> private constructor(private val materialHandlers: Multimap<Material, MaterialHandler<T, *>>) {
+	fun cache(block: Block, holder: CacheHolder<*>): T? {
 		val type = block.getTypeSafe() ?: return null
 
 		val forMaterial = materialHandlers[type] ?: return null
@@ -28,84 +27,84 @@ class NodeCacheFactory private constructor(private val materialHandlers: Multima
 		return filtered.firstNotNullOfOrNull { handler -> handler.construct(blockData, toBlockKey(block.x, block.y, block.z), holder) }
 	}
 
-	class Builder {
-		val materialHandlers = multimapOf<Material, MaterialHandler<*>>()
+	class Builder<T : Any> {
+		val materialHandlers = multimapOf<Material, MaterialHandler<T, *>>()
 
-		inline fun <reified T: BlockData> addDataHandler(material: Material, noinline constructor: (T, BlockKey, CacheHolder<*>) -> Node?): Builder {
-			this.materialHandlers[material].add(MaterialHandler(T::class, constructor))
+		inline fun <reified B: BlockData> addDataHandler(material: Material, noinline constructor: (B, BlockKey, CacheHolder<*>) -> T?): Builder<T> {
+			this.materialHandlers[material].add(MaterialHandler(B::class, constructor))
 			return this
 		}
 
-		inline fun <reified T: BlockData> addDataHandler(customBlock: IonRegistryKey<CustomBlock, out CustomBlock>, noinline constructor: (T, BlockKey, CacheHolder<*>) -> Node?): Builder {
+		inline fun <reified B: BlockData> addDataHandler(customBlock: IonRegistryKey<CustomBlock, out CustomBlock>, noinline constructor: (B, BlockKey, CacheHolder<*>) -> T?): Builder<T> {
 			val blockData = customBlock.getValue().blockData
-			require(blockData is T)
+			require(blockData is B)
 
-			return addDataHandler<T>(blockData.material) { data, lng, holder ->
+			return addDataHandler<B>(blockData.material) { data, lng, holder ->
 				if (data.customBlock != customBlock) return@addDataHandler null
 				constructor.invoke(data, lng, holder)
 			}
 		}
 
-		inline fun <reified T: BlockData> addDataHandler(materials: Iterable<Material>, noinline constructor: (T, BlockKey, CacheHolder<*>) -> Node): Builder {
-			for (material in materials) this.materialHandlers[material].add(MaterialHandler(T::class, constructor))
+		inline fun <reified B: BlockData> addDataHandler(materials: Iterable<Material>, noinline constructor: (B, BlockKey, CacheHolder<*>) -> T): Builder<T> {
+			for (material in materials) this.materialHandlers[material].add(MaterialHandler(B::class, constructor))
 			return this
 		}
 
-		inline fun <reified T: BlockData> addDataHandler(vararg materials: Material, noinline constructor: (T, BlockKey, CacheHolder<*>) -> Node?): Builder {
-			for (material in materials) this.materialHandlers[material].add(MaterialHandler(T::class, constructor))
+		inline fun <reified B: BlockData> addDataHandler(vararg materials: Material, noinline constructor: (B, BlockKey, CacheHolder<*>) -> T?): Builder<T> {
+			for (material in materials) this.materialHandlers[material].add(MaterialHandler(B::class, constructor))
 			return this
 		}
 
-		fun addSimpleNode(materials: Iterable<Material>, constructor: (BlockKey, Material, CacheHolder<*>) -> Node): Builder {
+		fun addSimpleNode(materials: Iterable<Material>, constructor: (BlockKey, Material, CacheHolder<*>) -> T): Builder<T> {
 			for (material in materials) this.materialHandlers[material].add(MaterialHandler(BlockData::class) { data, key, holder -> constructor(key, data.material, holder) })
 			return this
 		}
 
-		fun addSimpleNode(vararg materials: Material, constructor: (BlockKey, Material, CacheHolder<*>) -> Node): Builder {
+		fun addSimpleNode(vararg materials: Material, constructor: (BlockKey, Material, CacheHolder<*>) -> T): Builder<T> {
 			for (material in materials) this.materialHandlers[material].add(MaterialHandler(BlockData::class) { data, key, holder -> constructor(key, data.material, holder) })
 			return this
 		}
 
-		fun addSimpleNode(material: Material, constructor: (BlockKey, CacheHolder<*>) -> Node): Builder {
+		fun addSimpleNode(material: Material, constructor: (BlockKey, CacheHolder<*>) -> T): Builder<T> {
 			this.materialHandlers[material].add(MaterialHandler(BlockData::class) { _, key, holder -> constructor(key, holder) })
 			return this
 		}
 
-		fun addSimpleNode(material: Material, node: Node): Builder {
+		fun addSimpleNode(material: Material, node: T): Builder<T> {
 			this.materialHandlers[material].add(MaterialHandler(BlockData::class) { _, _, _ -> node })
 			return this
 		}
 
-		inline fun <reified T: BlockData> addSimpleNode(customBlock: IonRegistryKey<CustomBlock, out CustomBlock>, node: Node): Builder {
-			return addDataHandler<T>(customBlock.getValue().blockData.material) { data, lng, _ ->
+		inline fun <reified B: BlockData> addSimpleNode(customBlock: IonRegistryKey<CustomBlock, out CustomBlock>, node: T): Builder<T> {
+			return addDataHandler<B>(customBlock.getValue().blockData.material) { data, lng, _ ->
 				if (data.customBlock?.key != customBlock) return@addDataHandler null
 				node
 			}
 		}
 
-		fun addSimpleNode(materials: Iterable<Material>, node: Node): Builder {
+		fun addSimpleNode(materials: Iterable<Material>, node: T): Builder<T> {
 			for (material in materials) this.materialHandlers[material].add(MaterialHandler(BlockData::class) { _, _, _ -> node })
 			return this
 		}
 
-		fun addSimpleNode(vararg material: Material, node: Node) = addSimpleNode(material.toSet(), node)
+		fun addSimpleNode(vararg material: Material, node: T) = addSimpleNode(material.toSet(), node)
 
-		fun build(): NodeCacheFactory {
-			return NodeCacheFactory(materialHandlers)
+		fun build(): BlockBasedCacheFactory<T> {
+			return BlockBasedCacheFactory(materialHandlers)
 		}
 	}
 
-	class MaterialHandler<T: BlockData>(val blockDataClass: KClass<T>, val constructor: (T, BlockKey, CacheHolder<*>) -> Node?) {
+	class MaterialHandler<T : Any, B: BlockData>(val blockDataClass: KClass<B>, val constructor: (B, BlockKey, CacheHolder<*>) -> T?) {
 		@Synchronized
-		fun construct(blockData: BlockData, key: BlockKey, holder: CacheHolder<*>): Node? {
+		fun construct(blockData: BlockData, key: BlockKey, holder: CacheHolder<*>): T? {
 			@Suppress("UNCHECKED_CAST")
-			return runCatching { constructor.invoke(blockData as T, key, holder) }
+			return runCatching { constructor.invoke(blockData as B, key, holder) }
 				.onFailure { exception -> exception.printStackTrace() }
 				.getOrNull()
 		}
 	}
 
 	companion object {
-		fun builder(): Builder = Builder()
+		fun <T : Any> builder(): Builder<T> = Builder<T>()
 	}
 }
