5aedf1e9467eb1c24e9c7421076c34ef7555b651
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-30 14:14:56 -0500

    Rudimentary transfer ability

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/FluidGraphManager.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/FluidGraphManager.kt
@@ -3,9 +3,17 @@ package net.horizonsend.ion.server.features.transport.manager.graph
 import net.horizonsend.ion.server.features.transport.manager.TransportManager
 import net.horizonsend.ion.server.features.transport.manager.graph.fluid.FluidGraph
 import net.horizonsend.ion.server.features.transport.manager.graph.fluid.FluidNode
+import net.horizonsend.ion.server.features.transport.manager.graph.fluid.FluidNode.Input
+import net.horizonsend.ion.server.features.transport.nodes.util.BlockBasedCacheFactory
+import org.bukkit.Material
 import java.util.UUID
 
 class FluidGraphManager(manager: TransportManager<*>) : GraphManager<FluidNode, FluidGraph>(manager) {
+	override val cacheFactory: BlockBasedCacheFactory<FluidNode, GraphManager<FluidNode, FluidGraph>> = BlockBasedCacheFactory.builder<FluidNode, GraphManager<FluidNode, FluidGraph>>()
+		.addSimpleNode(Material.COPPER_GRATE) { pos, _, holder -> FluidNode.RegularPipe(pos) }
+		.addSimpleNode(Material.FLETCHING_TABLE) { pos, _, holder -> Input(pos) }
+		.build()
+
 	override fun createGraph(): FluidGraph {
 		return FluidGraph(UUID.randomUUID(), this)
 	}
