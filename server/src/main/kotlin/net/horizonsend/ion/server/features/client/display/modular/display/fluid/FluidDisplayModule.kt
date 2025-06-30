68fe6345aca384d5d328334fb5c170b344178623
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-06-30 00:47:17 -0500

    add relative face capabilities to displays, use the chemical processor multi for testing

--- server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/fluid/FluidDisplayModule.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/client/display/modular/display/fluid/FluidDisplayModule.kt
@@ -4,6 +4,7 @@ import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.server.features.client.display.modular.TextDisplayHandler
 import net.horizonsend.ion.server.features.client.display.modular.display.DisplayModule
 import net.horizonsend.ion.server.features.multiblock.entity.type.fluids.storage.FluidStorageContainer
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
@@ -14,8 +15,9 @@ abstract class FluidDisplayModule(
     offsetLeft: Double,
     offsetUp: Double,
     offsetBack: Double,
-    scale: Float
-) : DisplayModule(handler, offsetLeft, offsetUp, offsetBack, scale) {
+    scale: Float,
+	relativeFace: RelativeFace = RelativeFace.FORWARD,
+) : DisplayModule(handler, offsetLeft, offsetUp, offsetBack, scale, relativeFace) {
 	private val updateHandler: (FluidStorageContainer) -> Unit = {
 		runUpdates()
 	}
