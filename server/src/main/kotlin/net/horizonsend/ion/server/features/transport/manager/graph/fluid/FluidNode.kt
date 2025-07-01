5932026dee6b1c29056a82aa155f3e4b228fb725
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-01 14:13:03 -0500

    check graph integrity

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/fluid/FluidNode.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/fluid/FluidNode.kt
@@ -3,39 +3,54 @@ package net.horizonsend.ion.server.features.transport.manager.graph.fluid
 import net.horizonsend.ion.server.features.transport.manager.graph.TransportNodeGraph
 import net.horizonsend.ion.server.features.transport.nodes.graph.GraphNode
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.BlockKey
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
+import net.horizonsend.ion.server.miscellaneous.utils.getBlockIfLoaded
+import org.bukkit.Material
+import kotlin.properties.Delegates
 
 interface FluidNode : GraphNode {
 	val volume: Double
+	val graph: FluidGraph
 
 	class RegularPipe(override val location: BlockKey) : FluidNode {
+		override var graph: FluidGraph by Delegates.notNull<FluidGraph>(); private set
 		override val volume: Double = 100.0
 
-		override fun isIntact(): Boolean {
-			return true
+		override fun isIntact(): Boolean? {
+			val world = graph.manager.transportManager.getWorld()
+			val globalVec3i = graph.manager.transportManager.getGlobalCoordinate(toVec3i(location))
+			val block = getBlockIfLoaded(world, globalVec3i.x, globalVec3i.y, globalVec3i.z) ?: return null
+
+			return block.type == Material.COPPER_GRATE
 		}
 
 		override fun setGraph(graph: TransportNodeGraph<*>) {
-			TODO("Not yet implemented")
+			this.graph = graph as FluidGraph
 		}
 
 		override fun getGraph(): TransportNodeGraph<*> {
-			TODO("Not yet implemented")
+			return graph
 		}
 	}
 
 	class Input(override val location: BlockKey) : FluidNode {
+		override var graph: FluidGraph by Delegates.notNull<FluidGraph>(); private set
 		override val volume: Double = 100.0
 
-		override fun isIntact(): Boolean {
-			return true
+		override fun isIntact(): Boolean? {
+			val world = graph.manager.transportManager.getWorld()
+			val globalVec3i = graph.manager.transportManager.getGlobalCoordinate(toVec3i(location))
+			val block = getBlockIfLoaded(world, globalVec3i.x, globalVec3i.y, globalVec3i.z) ?: return null
+
+			return block.type == Material.FLETCHING_TABLE
 		}
 
 		override fun setGraph(graph: TransportNodeGraph<*>) {
-			TODO("Not yet implemented")
+			this.graph = graph as FluidGraph
 		}
 
 		override fun getGraph(): TransportNodeGraph<*> {
-			TODO("Not yet implemented")
+			return graph
 		}
 	}
 }
