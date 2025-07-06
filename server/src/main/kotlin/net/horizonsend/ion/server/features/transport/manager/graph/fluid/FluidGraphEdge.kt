53a034c4123edfe52ba23dee5f8544bdacb17f25
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-06 13:11:06 -0500

    store edge direction

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/fluid/FluidGraphEdge.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/manager/graph/fluid/FluidGraphEdge.kt
@@ -2,6 +2,10 @@ package net.horizonsend.ion.server.features.transport.manager.graph.fluid
 
 import net.horizonsend.ion.server.features.transport.nodes.graph.GraphEdge
 import net.horizonsend.ion.server.features.transport.nodes.graph.TransportNode
+import net.horizonsend.ion.server.miscellaneous.utils.ADJACENT_BLOCK_FACES
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.toVec3i
+import org.bukkit.block.BlockFace
 
 class FluidGraphEdge(
 	override val nodeOne: TransportNode,
@@ -9,4 +13,10 @@ class FluidGraphEdge(
 ) : GraphEdge {
 	/** returns the net flow between the two nodes. If positive, it is towards node two, if negative, towards node one. */
 	var netFlow: Double = 0.0
+
+	val direction: BlockFace by lazy {
+		val direction = toVec3i(nodeTwo.location).minus(toVec3i(nodeOne.location))
+		val facesByMod = ADJACENT_BLOCK_FACES.plus(BlockFace.SELF).associateBy { face -> Vec3i(face.modX, face.modY, face.modZ) }
+		facesByMod[direction]!!
+	}
 }
