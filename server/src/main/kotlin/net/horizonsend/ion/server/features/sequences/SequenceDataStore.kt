120701665f0a4442cf30d415b2cf66b56f8b5534
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-10 13:36:01 -0500

    Maintain a key / value data store for sequences

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequenceDataStore.kt
@@ -0,0 +1,18 @@
+package net.horizonsend.ion.server.features.sequences
+
+import java.util.Optional
+import java.util.UUID
+
+class SequenceDataStore(player: UUID) {
+	val keyedData = mutableMapOf<String, Any?>()
+
+	operator fun <T : Any> get(key: String): Optional<T> {
+		if (!keyedData.containsKey(key)) return Optional.empty<T>()
+		@Suppress("UNCHECKED_CAST")
+		return Optional.ofNullable(keyedData[key] as? T)
+	}
+
+	operator fun <T : Any> set(key: String, value: T) {
+		keyedData[key] = value
+	}
+}
