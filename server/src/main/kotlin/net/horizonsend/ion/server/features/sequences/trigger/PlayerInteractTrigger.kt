66e87a68d9fdfd486cb7efa3ff662c6d705939ed
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-10 14:04:07 -0500

    move triggers to top level & rework the trigger logic flow

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/PlayerInteractTrigger.kt
@@ -0,0 +1,19 @@
+package net.horizonsend.ion.server.features.sequences.trigger
+
+import net.horizonsend.ion.server.features.sequences.trigger.PlayerInteractTrigger.InteractTriggerSettings
+import net.horizonsend.ion.server.miscellaneous.utils.listen
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerInteractEvent
+
+object PlayerInteractTrigger : SequenceTriggerType<InteractTriggerSettings>() {
+	override fun setupChecks() {
+		listen<PlayerInteractEvent> { checkPhaseTriggers(it.player) }
+	}
+
+	class InteractTriggerSettings(
+	) : TriggerSettings() {
+		override fun shouldProceed(player: Player): Boolean {
+			return true
+		}
+	}
+}
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/PlayerMovementTrigger.kt
@@ -0,0 +1,44 @@
+package net.horizonsend.ion.server.features.sequences.trigger
+
+import net.horizonsend.ion.server.features.sequences.trigger.PlayerMovementTrigger.MovementTriggerSettings
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.cube
+import net.horizonsend.ion.server.miscellaneous.utils.debugAudience
+import net.horizonsend.ion.server.miscellaneous.utils.listen
+import org.bukkit.Particle
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.util.BoundingBox
+
+object PlayerMovementTrigger : SequenceTriggerType<MovementTriggerSettings>() {
+	override fun setupChecks() {
+		listen<PlayerMoveEvent> { checkPhaseTriggers(it.player) }
+	}
+
+	class MovementTriggerSettings(
+		val predicates: List<PlayerLocationPredicate>
+	) : TriggerSettings() {
+		override fun shouldProceed(player: Player): Boolean {
+			return predicates.all { predicate -> predicate.check(player) }
+		}
+	}
+
+	fun interface PlayerLocationPredicate {
+		fun check(player: Player): Boolean
+	}
+
+	fun inBoundingBox(box: BoundingBox) = PlayerLocationPredicate {
+		val cube = cube(box.min, box.max)
+		@Suppress("OverrideOnly")
+		debugAudience.audiences().filterIsInstance<Player>().forEach { player -> cube.forEach { cubePoint -> player.spawnParticle(Particle.SOUL_FIRE_FLAME, cubePoint.x, cubePoint.y, cubePoint.z, 1, 0.0, 0.0, 0.0, 0.0) } }
+
+		return@PlayerLocationPredicate box.contains(it.location.toVector())
+	}
+
+	fun lookingAtBoundingBox(box: BoundingBox) = PlayerLocationPredicate { player ->
+		val cube = cube(box.min, box.max)
+		@Suppress("OverrideOnly")
+		debugAudience.audiences().filterIsInstance<Player>().forEach { player -> cube.forEach { cubePoint -> player.spawnParticle(Particle.SOUL_FIRE_FLAME, cubePoint.x, cubePoint.y, cubePoint.z, 1, 0.0, 0.0, 0.0, 0.0) } }
+
+		return@PlayerLocationPredicate box.rayTrace(player.eyeLocation.toVector(), player.location.direction, 10.0) != null
+	}
+}
