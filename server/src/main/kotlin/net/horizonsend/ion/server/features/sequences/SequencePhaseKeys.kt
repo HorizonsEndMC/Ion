9353f597960043a1c4352aa6ca3ee2c0e41515cf
Gutin <57606752+gutin1@users.noreply.github.com>
2025-07-09 22:25:30 -0700

    move to sequence keys to allow back tracking of branches

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequencePhaseKeys.kt
@@ -0,0 +1,23 @@
+package net.horizonsend.ion.server.features.sequences
+
+object SequencePhaseKeys {
+	val keys: MutableList<SequencePhaseKey> = mutableListOf()
+	val byString: MutableMap<String, SequencePhaseKey> = mutableMapOf()
+
+	class SequencePhaseKey(val key: String) {
+		fun getValue(): SequencePhase {
+			return SequenceManager.getPhaseByKey(this)
+		}
+	}
+
+	val TUTORIAL_START = register("TUTORIAL_START")
+	val TUTORIAL_TWO = register("TUTORIAL_TWO")
+	val TUTORIAL_END = register("TUTORIAL_END")
+
+	fun register(key: String): SequencePhaseKey {
+		val phaseKey = SequencePhaseKey(key)
+		keys.add(phaseKey)
+		byString[key] = phaseKey
+		return phaseKey
+	}
+}
