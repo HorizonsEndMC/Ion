81892bccb3224f3714e5a4a70587dd54204d17f3
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-11 14:19:09 -0500

    Start on implementing tutorial script

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequencePhaseKeys.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequencePhaseKeys.kt
@@ -10,6 +10,13 @@ object SequencePhaseKeys {
 		}
 	}
 
+	val REAL_TUTORIAL_START = register("REAL_TUTORIAL_START")
+	val EXIT_CRYOPOD_ROOM = register("EXIT_CRYOPOD_ROOM")
+	val BRANCH_LOOK_OUTSIDE = register("LOOK_OUTSIDE")
+	val BROKEN_ELEVATOR = register("BROKEN_ELEVATOR")
+	val LOOK_AT_TRACTOR = register("LOOK_AT_TRACTOR")
+	val USED_TRACTOR_BEAM = register("USED_TRACTOR_BEAM")
+
 	val TUTORIAL_START = register("TUTORIAL_START")
 	val TUTORIAL_TWO = register("TUTORIAL_TWO")
 	val CHERRY_TEST_BRANCH = register("TUTORIAL_BRANCH")
