8d8626deca98b4eb02e774ad7a11f7f83083824e
Gutin <57606752+gutin1@users.noreply.github.com>
2025-07-13 12:52:48 -0700

    Support for multiple concurrent sequences and save / load sequence data

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/UsedTractorBeamTrigger.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/UsedTractorBeamTrigger.kt
@@ -1,18 +1,19 @@
 package net.horizonsend.ion.server.features.sequences.trigger
 
 import net.horizonsend.ion.server.event.multiblock.PlayerUseTractorBeamEvent
+import net.horizonsend.ion.server.features.sequences.SequenceManager.getCurrentSequences
 import net.horizonsend.ion.server.features.sequences.trigger.UsedTractorBeamTrigger.TractorBeamTriggerSettings
 import net.horizonsend.ion.server.miscellaneous.utils.listen
 import org.bukkit.entity.Player
 
 object UsedTractorBeamTrigger : SequenceTriggerType<TractorBeamTriggerSettings>() {
 	override fun setupChecks() {
-		listen<PlayerUseTractorBeamEvent> { checkPhaseTriggers(it.player) }
+		listen<PlayerUseTractorBeamEvent> { for (sequenceKey in getCurrentSequences(it.player)) { checkPhaseTriggers(it.player, sequenceKey) } }
 	}
 
 	class TractorBeamTriggerSettings(
 	) : TriggerSettings() {
-		override fun shouldProceed(player: Player, callingTrigger: SequenceTriggerType<*>): Boolean {
+		override fun shouldProceed(player: Player, sequenceKey: String, callingTrigger: SequenceTriggerType<*>): Boolean {
 			return callingTrigger == UsedTractorBeamTrigger
 		}
 	}
