e4911d5164c473adc72ddcd03d3ff402845aa545
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-07-14 00:51:47 -0500

    allow multiple triggers with different results

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/SequenceTrigger.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/SequenceTrigger.kt
@@ -2,20 +2,23 @@ package net.horizonsend.ion.server.features.sequences.trigger
 
 import net.horizonsend.ion.server.core.registration.IonRegistryKey
 import net.horizonsend.ion.server.features.sequences.Sequence
+import net.horizonsend.ion.server.features.sequences.SequenceManager
+import net.horizonsend.ion.server.features.sequences.phases.SequencePhase
 import org.bukkit.entity.Player
 
-class SequenceTrigger<T : SequenceTriggerType.TriggerSettings>(val type: SequenceTriggerType<T>, private val settings: T) {
-	private var onTriggered: SequenceTrigger<T>.(Player) -> Unit = {}
-
-	fun setTriggerResult(result: SequenceTrigger<T>.(Player) -> Unit) {
-		onTriggered = result
-	}
-
+class SequenceTrigger<T : SequenceTriggerType.TriggerSettings>(val type: SequenceTriggerType<T>, private val settings: T, private var triggerResult: (Player) -> Unit) {
 	fun trigger(player: Player) {
-		onTriggered.invoke(this, player)
+		triggerResult.invoke(player)
 	}
 
 	fun shouldProceed(player: Player, sequenceKey: IonRegistryKey<Sequence, out Sequence>, callingTrigger: SequenceTriggerType<*>): Boolean {
 		return settings.shouldProceed(player, sequenceKey, callingTrigger)
+	}
+
+	companion object {
+		fun startPhase(phaseKey: IonRegistryKey<SequencePhase, out SequencePhase>): (Player) -> Unit = {
+			val phase = phaseKey.getValue()
+			SequenceManager.startPhase(it, phase.sequenceKey, phase.phaseKey)
+		}
 	}
 }
