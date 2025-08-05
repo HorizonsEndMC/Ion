7e7dfa76f7d4d778a0af57afbd76e03f169d8792
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-08-05 13:59:41 -0500

    add back other messages

--- server/src/main/kotlin/net/horizonsend/ion/server/features/ai/faction/AIFaction.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/ai/faction/AIFaction.kt
@@ -484,7 +484,13 @@ class AIFaction private constructor(
 				"betrayal" to "<red>Dammit, I knew I shouldn't have trusted you "
 			)
 			.addSmackMessages(
+				"<white>Please no, I've done nothing wrong!",
+				"<white>Spare me; this ship is all I have!",
+				"<white>My friends will avenge me!",
 				"<white>I'm calling the {0} Defense Patrol! ",
+				"<white>Shields are down!",
+				"<white>Hull integrity critical!",
+				"<white>Engines compromised!"
 			)
 			.addFleeMessages(
 				"<white>Mayday, mayday, going down!" to true,
