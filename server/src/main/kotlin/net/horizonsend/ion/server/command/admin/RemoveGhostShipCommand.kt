6406649fb4191cd30a10b7599b4b1497aea9b652
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-08-10 16:04:11 -0500

    Improve removeghostship

--- server/src/main/kotlin/net/horizonsend/ion/server/command/admin/RemoveGhostShipCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/admin/RemoveGhostShipCommand.kt
@@ -3,11 +3,16 @@ package net.horizonsend.ion.server.command.admin
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
+import net.horizonsend.ion.common.database.Oid
+import net.horizonsend.ion.common.database.schema.starships.StarshipData
+import net.horizonsend.ion.common.extensions.information
 import net.horizonsend.ion.common.utils.text.ofChildren
 import net.horizonsend.ion.common.utils.text.toComponent
 import net.horizonsend.ion.server.features.starship.DeactivatedPlayerStarships
 import net.horizonsend.ion.server.features.starship.PilotedStarships
+import net.horizonsend.ion.server.features.starship.StarshipComputers
 import net.horizonsend.ion.server.miscellaneous.utils.Tasks
+import net.horizonsend.ion.server.miscellaneous.utils.bukkitWorld
 import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockBreakEvent
 
@@ -19,24 +24,37 @@ object RemoveGhostShipCommand : net.horizonsend.ion.server.command.SLCommand() {
 	fun onDeleteGhostShip(sender: Player) = asyncCommand(sender) {
 		val selection = requireSelection(sender)
 
+		val visited = mutableSetOf<Oid<out StarshipData>>()
+
 		for (pos in selection) {
 			val x = pos.x()
 			val y = pos.y()
 			val z = pos.z()
 
-			val ship = DeactivatedPlayerStarships[sender.world, x, y, z] ?: continue
+			val ship = DeactivatedPlayerStarships.getContaining(sender.world, x, y, z) ?: continue
 
-			Tasks.sync {
+			if (visited.contains(ship._id)) continue
+
+			@Suppress("DEPRECATION") val block = ship.bukkitWorld().getBlockAtKey(ship.blockKey)
+			if (block.type == StarshipComputers.COMPUTER_TYPE) {
+				visited.add(ship._id)
+				continue
+			}
+
+			Tasks.getSyncBlocking {
 				val event = BlockBreakEvent(sender.world.getBlockAt(x, y, z), sender).callEvent()
-				if (!event) return@sync
+				if (!event) return@getSyncBlocking
 
 				val name = PilotedStarships.getDisplayName(ship)
 
-				DeactivatedPlayerStarships.removeState(ship)
-				ship.companion().remove(ship._id)
+				DeactivatedPlayerStarships.destroyAsync(ship) {
+					sender.sendMessage(ofChildren("Removed ".toComponent(), name))
+				}
 
-				sender.sendMessage(ofChildren("Removed ".toComponent(), name))
+				visited.add(ship._id)
 			}
 		}
+
+		sender.information("Removed ${visited.size} ships.")
 	}
 }
