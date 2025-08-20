a279cf62999e8e1e250140ee60bd7731b60c11e1
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-08-20 11:26:51 -0500

    Rewrite cache since dbobjectcache wasn't being properly cleaned up

--- server/src/main/kotlin/net/horizonsend/ion/server/features/cache/ChestShopCache.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/cache/ChestShopCache.kt
@@ -1,60 +1,88 @@
 package net.horizonsend.ion.server.features.cache
 
-import com.googlecode.cqengine.index.hash.HashIndex
-import com.googlecode.cqengine.query.QueryFactory
-import com.googlecode.cqengine.resultset.ResultSet
-import com.mongodb.client.model.changestream.ChangeStreamDocument
+import com.google.common.collect.HashBasedTable
 import net.horizonsend.ion.common.database.Oid
 import net.horizonsend.ion.common.database.boolean
-import net.horizonsend.ion.common.database.cache.DbObjectCache
+import net.horizonsend.ion.common.database.cache.ManualCache
 import net.horizonsend.ion.common.database.document
 import net.horizonsend.ion.common.database.double
 import net.horizonsend.ion.common.database.get
 import net.horizonsend.ion.common.database.nullable
+import net.horizonsend.ion.common.database.oid
 import net.horizonsend.ion.common.database.schema.economy.ChestShop
-import net.horizonsend.ion.common.database.schema.misc.SLPlayerId
 import net.horizonsend.ion.common.database.slPlayerId
 import net.horizonsend.ion.common.database.string
 import net.horizonsend.ion.common.utils.DBVec3i
+import net.horizonsend.ion.common.utils.set
 import net.horizonsend.ion.server.features.economy.chestshops.ChestShops
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
+import net.kyori.adventure.key.Key
+import java.util.concurrent.ConcurrentHashMap
 
-object ChestShopCache : DbObjectCache<ChestShop, Oid<ChestShop>>(ChestShop.Companion) {
-	override val idAttribute = attribute(ChestShop::_id)
+object ChestShopCache : ManualCache() {
+	private val byLocation = HashBasedTable.create</* World Key */ Key, Vec3i, ChestShop>()
+	private val locationMap = ConcurrentHashMap<Oid<ChestShop>, Pair<Key, Vec3i>>()
+	private val byId = ConcurrentHashMap<Oid<ChestShop>, ChestShop>()
 
-	private val ownerAttribute = attribute(ChestShop::owner)
-	private val locationAttribute = attribute(ChestShop::location)
-	private val worldAttribute = attribute(ChestShop::world)
+	override fun load() {
+		byId.clear()
+		byLocation.clear()
 
-	override fun addExtraIndexes() {
-		cache.addIndex(HashIndex.onAttribute(ownerAttribute))
-		cache.addIndex(HashIndex.onAttribute(locationAttribute))
-		cache.addIndex(HashIndex.onAttribute(worldAttribute))
+		for (shop in ChestShop.all()) {
+			cache(shop)
+		}
+
+		ChestShop.watchInserts { change ->
+			val id: Oid<ChestShop> = change.oid
+
+			synced {
+				cache(change.fullDocument!!)
+			}
+
+			val cached = this[id]
+			ChestShops.updateSign(cached)
+		}
+
+		ChestShop.watchDeletes { change ->
+			val id: Oid<ChestShop> = change.oid
+
+			synced {
+				byId.remove(id)
+				val (worldKey, location) = locationMap.remove(id) ?: return@synced
+				byLocation.remove(worldKey, location)
+			}
+		}
+
+		ChestShop.watchUpdates { change ->
+			synced {
+				val id: Oid<ChestShop> = change.oid
+				val cached = this[id]
+
+				change[ChestShop::world]?.let { cached.world = it.string() }
+				change[ChestShop::location]?.let { cached.location = it.document<DBVec3i>() }
+				change[ChestShop::owner]?.let { cached.owner = it.slPlayerId() }
+				change[ChestShop::soldItem]?.let { bson -> cached.soldItem = bson.nullable()?.string() }
+				change[ChestShop::price]?.let { bson -> cached.price = bson.double() }
+				change[ChestShop::selling]?.let { bson -> cached.selling = bson.boolean() }
+
+				ChestShops.updateSign(cached)
+			}
+		}
 	}
 
-	override fun update(cached: ChestShop, change: ChangeStreamDocument<ChestShop>) {
-		change[ChestShop::world]?.let { cached.world = it.string() }
-		change[ChestShop::location]?.let { cached.location = it.document<DBVec3i>() }
-		change[ChestShop::owner]?.let { cached.owner = it.slPlayerId() }
-		change[ChestShop::soldItem]?.let { bson -> cached.soldItem = bson.nullable()?.string() }
-		change[ChestShop::price]?.let { bson -> cached.price = bson.double() }
-		change[ChestShop::selling]?.let { bson -> cached.selling = bson.boolean() }
+	fun cache(shop: ChestShop) {
+		byId[shop._id] = shop
 
-		ChestShops.updateSign(cached)
+		val worldKey = Key.key(shop.world)
+		val vec3i = Vec3i(shop.location)
+
+		byLocation[worldKey, vec3i] = shop
+		locationMap[shop._id] = worldKey to vec3i
 	}
 
-	override fun onInsert(cached: ChestShop) {
-		ChestShops.updateSign(cached)
-	}
+	operator fun get(id: Oid<ChestShop>): ChestShop = byId[id] ?: error("Chest shop $id is not cached!")
 
-	fun getByOwner(id: SLPlayerId): ResultSet<ChestShop> {
-		return cache.retrieve(QueryFactory.equal(ownerAttribute, id))
-	}
-
-	fun getByLocation(world: String, location: DBVec3i): ChestShop? {
-		return cache.retrieve(
-            QueryFactory.and(
-			QueryFactory.equal(locationAttribute, location),
-			QueryFactory.equal(worldAttribute, world)
-		)).firstOrNull()
+	fun getByLocation(world: Key, location: DBVec3i): ChestShop? {
+		return byLocation[world, location]
 	}
 }
