8e4ff2f1333bb0a9f7853be0c9ba32f7d7c03217
kwazedilla <kwazedilla@gmail.com>
2025-08-30 11:24:13 -0700

    add signature registries

--- server/src/main/kotlin/net/horizonsend/ion/server/core/registration/IonRegistries.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/core/registration/IonRegistries.kt
@@ -10,6 +10,8 @@ import net.horizonsend.ion.server.core.registration.registries.FluidTypeRegistry
 import net.horizonsend.ion.server.core.registration.registries.ItemModRegistry
 import net.horizonsend.ion.server.core.registration.registries.Registry
 import net.horizonsend.ion.server.core.registration.registries.TransportNetworkNodeTypeRegistry
+import net.horizonsend.ion.server.core.registration.registries.SignatureRegistry
+import net.horizonsend.ion.server.core.registration.registries.SignatureSpawnerRegistry
 import net.horizonsend.ion.server.features.multiblock.crafting.MultiblockRecipeRegistry
 
 object IonRegistries : IonComponent() {
@@ -32,6 +34,8 @@ object IonRegistries : IonComponent() {
 	val MULTIBLOCK_RECIPE = register(MultiblockRecipeRegistry())
 	val TRANSPORT_NETWORK_NODE_TYPE = register(TransportNetworkNodeTypeRegistry())
 	val FLUID_PROPERTY_TYPE = register(FluidPropertyTypeRegistry())
+	val SIGNATURE_SPAWNERS = register(SignatureSpawnerRegistry())
+	val SIGNATURES = register(SignatureRegistry())
 
 	private fun <T : Registry<*>> register(registry: T): T {
 		byId[registry.id] = registry
