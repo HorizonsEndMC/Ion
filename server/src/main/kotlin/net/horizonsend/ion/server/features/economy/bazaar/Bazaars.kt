24db456d3d4a52e19a3e561810d0ce0609d03b65
kwazedilla <kwazedilla@gmail.com>
2025-09-01 08:39:24 -0700

    ensure buy order fulfillment does not exceed current buy order stock

--- server/src/main/kotlin/net/horizonsend/ion/server/features/economy/bazaar/Bazaars.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/economy/bazaar/Bazaars.kt
@@ -642,7 +642,7 @@ object Bazaars : IonServerComponent() {
 		val result = FutureInputResult()
 
 		Tasks.sync {
-			val count = takePlayerItemsOfType(inventory, itemReference, limit)
+			val count = takePlayerItemsOfType(inventory, itemReference, minOf(orderDocument.stock, limit))
 
 			if (count == 0) {
 				result.complete(
