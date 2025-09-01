98c24e202d247a2fc69ba15f88100019913dfb30
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-09-01 12:50:25 -0500

    add param to iotype to make them more distinct

--- server/src/main/kotlin/net/horizonsend/ion/server/features/transport/inputs/IOType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/transport/inputs/IOType.kt
@@ -5,14 +5,14 @@ import net.horizonsend.ion.server.features.transport.inputs.IOPort.RegisteredMet
 import net.horizonsend.ion.server.features.transport.inputs.IOPort.Simple
 import kotlin.reflect.KClass
 
-sealed class IOType<T : IOPort>(val clazz: KClass<T>) {
-	data object POWER : IOType<Simple>(Simple::class)
-	data object FLUID : IOType<RegisteredMetaDataInput<FluidInputMetadata>>(RegisteredMetaDataInput::class as KClass<RegisteredMetaDataInput<FluidInputMetadata>>)
-	data object E2 : IOType<Simple>(Simple::class)
+sealed class IOType<T : IOPort>(val displayName: String, val clazz: KClass<T>) {
+	data object POWER : IOType<Simple>("POWER", Simple::class)
+	data object FLUID : IOType<RegisteredMetaDataInput<FluidInputMetadata>>("FLUID", RegisteredMetaDataInput::class as KClass<RegisteredMetaDataInput<FluidInputMetadata>>)
+	data object E2 : IOType<Simple>("E2", Simple::class)
 
 	companion object {
 		val types = arrayOf<IOType<*>>(POWER, FLUID, E2)
-		val byName = mapOf("POWER" to POWER, "FLUID" to FLUID, "E2" to E2)
+		val byName get()  = mapOf("POWER" to POWER, "FLUID" to FLUID, "E2" to E2)
 		operator fun get(name: String) : IOType<*>? = byName[name]
 	}
 }
