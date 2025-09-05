ecfdd44d755ca9aac56ed609963894a233508ec7
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-09-05 13:56:35 -0500

    Clean up commands & add one to end phase fully

--- server/src/main/kotlin/net/horizonsend/ion/server/command/starship/StarshipDebugCommand.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/command/starship/StarshipDebugCommand.kt
@@ -12,9 +12,6 @@ import net.horizonsend.ion.server.command.SLCommand
 import net.horizonsend.ion.server.features.ai.module.targeting.TargetingModule
 import net.horizonsend.ion.server.features.client.display.ClientDisplayEntities.highlightBlock
 import net.horizonsend.ion.server.features.misc.UnusedSoldShipPurge
-import net.horizonsend.ion.server.features.sequences.SequenceKeys
-import net.horizonsend.ion.server.features.sequences.SequenceManager
-import net.horizonsend.ion.server.features.sequences.phases.SequencePhaseKeys
 import net.horizonsend.ion.server.features.starship.DeactivatedPlayerStarships
 import net.horizonsend.ion.server.features.starship.active.ActiveStarships
 import net.horizonsend.ion.server.features.starship.control.controllers.ai.AIController
@@ -261,10 +258,5 @@ object StarshipDebugCommand : SLCommand() {
 
 		ship.forecastEnabled = !ship.forecastEnabled
 		sender.information("Toggled forecast for $shipIdentifier to ${ship.forecastEnabled}")
-	}
-
-	@Subcommand("tutorialTest")
-	fun startTutorial(sender: Player) {
-		SequenceManager.startPhase(sender, SequenceKeys.TUTORIAL, SequencePhaseKeys.TUTORIAL_START)
 	}
 }
