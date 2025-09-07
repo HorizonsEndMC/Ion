61630fb3e3901085d3a6200771345c0ecedc9914
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-09-07 17:10:05 -0500

    Add optional pilot offset for sold ships, ability to spawn one unchecked, add blindness transition

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/dealers/DealerShip.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/dealers/DealerShip.kt
@@ -4,6 +4,7 @@ import com.sk89q.worldedit.extent.clipboard.Clipboard
 import net.horizonsend.ion.common.utils.text.gui.sendWithdrawMessage
 import net.horizonsend.ion.server.features.starship.Starship
 import net.horizonsend.ion.server.features.starship.StarshipType
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.horizonsend.ion.server.miscellaneous.utils.withdrawMoney
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
@@ -13,7 +14,8 @@ abstract class DealerShip(
 	open val displayName: Component,
 	open val price: Double,
 	open val protectionCanBypass: Boolean,
-	open val starshipType: StarshipType
+	open val starshipType: StarshipType,
+	open val pilotOffset: Vec3i = Vec3i(0, 0, 0)
 ) {
 	abstract fun getClipboard(): Clipboard
 	abstract fun getIcon(): ItemStack
