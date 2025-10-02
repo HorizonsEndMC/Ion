f9cff40d01eb4ce7856f3844b09e41c95753127d
Gutin <57606752+Gutin1@users.noreply.github.com>
2025-10-02 18:13:31 -0500

    convert asteroid density to noise field

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/feature/meta/asteroid/noise/NoiseWrapper.kt
+++
@@ -1,7 +0,0 @@
-package net.horizonsend.ion.server.features.world.generation.feature.meta.asteroid.noise
-
-import kotlin.random.Random
-
-interface NoiseWrapper {
-	fun setSeed(random: Random) {}
-}
