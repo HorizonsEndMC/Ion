23273325c32e737563f51136ab71f56eaa135d03
kwazedilla <kwazedilla@gmail.com>
2025-10-07 00:31:47 -0700

    boids

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/BoidProjectile.kt
@@ -0,0 +1,72 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
+
+import net.horizonsend.ion.server.configuration.starship.StarshipBoidProjectileBalancing
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.source.ProjectileSource
+import net.kyori.adventure.text.Component
+import org.bukkit.Location
+import org.bukkit.damage.DamageType
+import org.bukkit.util.Vector
+
+abstract class BoidProjectile<B : StarshipBoidProjectileBalancing>(
+    source: ProjectileSource,
+    name: Component,
+    loc: Location,
+    dir: Vector,
+    shooter: Damager,
+    damageType: DamageType,
+    val otherBoids: MutableList<SimpleProjectile<B>>
+) : SimpleProjectile<B>(source, name, loc, dir, shooter, damageType) {
+    val separationDistance: Double get() = balancing.separationDistance
+    val separationFactor: Double get() = balancing.separationFactor
+    val visibleDistance: Double get() = balancing.visibleDistance
+    val alignFactor: Double get() = balancing.alignFactor
+    val centerFactor: Double get() = balancing.centerFactor
+
+    init {
+        otherBoids.add(this)
+    }
+
+    override fun tick() {
+        var neighboringBoids = 0
+        val separationVector = Vector(0.0, 0.0, 0.0)
+        val alignVector = Vector(0.0, 0.0, 0.0)
+        val averagePosition = Vector(0.0, 0.0, 0.0)
+
+        for (boid in otherBoids) {
+            // Ensure that this boid does not try to modify itself
+            if (boid === this) continue
+
+            if (location.distanceSquared(boid.location) < separationDistance) {
+                // Steer away from other boids
+                separationVector.x = boid.location.x - location.x
+                separationVector.y = boid.location.y - location.y
+                separationVector.z = boid.location.z - location.z
+            }
+
+            if (location.distanceSquared(boid.location) < visibleDistance) {
+                // Align with nearby boids
+                neighboringBoids++
+                alignVector.x += boid.direction.x
+                alignVector.y += boid.direction.y
+                alignVector.z += boid.direction.z
+
+                // Used to get average center of nearby boids
+                averagePosition.add(boid.location.toVector())
+            }
+        }
+
+        if (neighboringBoids > 0) {
+            alignVector.multiply(1 / neighboringBoids)
+            averagePosition.multiply(1 / neighboringBoids)
+        }
+
+        val oldDirection = direction.clone()
+        direction.add(separationVector.multiply(separationFactor))
+        direction.add(alignVector.clone().subtract(oldDirection).multiply(alignFactor))
+        direction.add(averagePosition.clone().subtract(location.toVector()).multiply(centerFactor))
+        direction.normalize().multiply(speed)
+
+        super.tick()
+    }
+}
