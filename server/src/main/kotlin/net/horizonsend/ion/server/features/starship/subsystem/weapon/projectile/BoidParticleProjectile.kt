c9d289fdd86eb4d7fb6d945e929a070fb3d802be
kwazedilla <kwazedilla@gmail.com>
2025-10-07 17:06:40 -0700

    boid test weapon

---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/BoidParticleProjectile.kt
@@ -0,0 +1,31 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
+
+import net.horizonsend.ion.server.configuration.starship.StarshipBoidProjectileBalancing
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.source.ProjectileSource
+import net.kyori.adventure.text.Component
+import org.bukkit.Location
+import org.bukkit.damage.DamageType
+import org.bukkit.util.Vector
+
+abstract class BoidParticleProjectile<T: StarshipBoidProjectileBalancing>(
+    source: ProjectileSource,
+    name: Component,
+    loc: Location,
+    dir: Vector,
+    shooter: Damager,
+    otherBoids: MutableList<BoidProjectile<*>>,
+    damageType: DamageType
+) : BoidProjectile<T>(source, name, loc, dir, shooter, otherBoids, damageType) {
+    override fun moveVisually(oldLocation: Location, newLocation: Location, travel: Double) {
+        for (i in 0 until travel.toInt()) {
+            val x = location.x + direction.x * i
+            val y = location.y + direction.y * i
+            val z = location.z + direction.z * i
+            val force = i % 3 == 0
+            spawnParticle(x, y, z, force)
+        }
+    }
+
+    protected abstract fun spawnParticle(x: Double, y: Double, z: Double, force: Boolean)
+}
---
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/TestBoidProjectile.kt
@@ -0,0 +1,28 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
+
+import net.horizonsend.ion.server.configuration.starship.TestBoidCannonBalancing
+import net.horizonsend.ion.server.features.starship.damager.Damager
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.source.ProjectileSource
+import net.kyori.adventure.text.Component
+import org.bukkit.Color
+import org.bukkit.Location
+import org.bukkit.Particle
+import org.bukkit.damage.DamageType
+import org.bukkit.util.Vector
+
+class TestBoidProjectile(
+    source: ProjectileSource,
+    name: Component,
+    loc: Location,
+    dir: Vector,
+    shooter: Damager,
+    otherBoids: MutableList<BoidProjectile<*>>,
+    damageType: DamageType
+) : BoidParticleProjectile<TestBoidCannonBalancing.TestBoidCannonProjectileBalancing>(source, name, loc, dir, shooter, otherBoids, damageType) {
+    override fun spawnParticle(x: Double, y: Double, z: Double, force: Boolean) {
+        val particle = Particle.DUST
+        val dustOptions = Particle.DustOptions(Color.AQUA, 1f)
+
+        location.world.spawnParticle(particle, x, y, z, 1, 0.0, 0.0, 0.0, 0.0, dustOptions, force)
+    }
+}
