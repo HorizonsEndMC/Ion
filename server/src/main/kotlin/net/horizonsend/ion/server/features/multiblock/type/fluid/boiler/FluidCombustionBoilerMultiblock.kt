13040be0cf7ed3dec925170c296f10e8c2442d3d
Clayton <57606752+Gutin1@users.noreply.github.com>
2025-10-30 18:33:50 -0500

    effects improvements

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/boiler/FluidCombustionBoilerMultiblock.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/fluid/boiler/FluidCombustionBoilerMultiblock.kt
@@ -22,9 +22,11 @@ import net.horizonsend.ion.server.features.transport.inputs.IOData
 import net.horizonsend.ion.server.features.transport.inputs.IOPort
 import net.horizonsend.ion.server.features.transport.inputs.IOType
 import net.horizonsend.ion.server.miscellaneous.registrations.persistence.NamespacedKeys
+import net.horizonsend.ion.server.miscellaneous.utils.axis
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.RelativeFace
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
+import org.bukkit.Axis
 import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.World
@@ -416,8 +418,8 @@ object FluidCombustionBoilerMultiblock : BoilerMultiblock<FluidBoilerEntity>() {
 		val pollutionStorage = FluidStorageContainer(data, "pollution_out", text("Pollution Output"), NamespacedKeys.key("pollution_out"), 100_000.0, FluidRestriction.Unlimited)
 
 		override fun IOData.Builder.registerAdditionalIO(): IOData.Builder =
-				addPort(IOType.FLUID, 0, -1, 0) { IOPort.RegisteredMetaDataInput(this@FluidBoilerEntity, FluidPortMetadata(connectedStore = fuelStorage, inputAllowed = true, outputAllowed = false)) }
-				.addPort(IOType.FLUID, 0, 1, 6) { IOPort.RegisteredMetaDataInput(this@FluidBoilerEntity, FluidPortMetadata(connectedStore = pollutionStorage, inputAllowed = false, outputAllowed = true)) }
+			addPort(IOType.FLUID, 0, -1, 0) { IOPort.RegisteredMetaDataInput(this@FluidBoilerEntity, FluidPortMetadata(connectedStore = fuelStorage, inputAllowed = true, outputAllowed = false)) }
+			.addPort(IOType.FLUID, 0, 1, 6) { IOPort.RegisteredMetaDataInput(this@FluidBoilerEntity, FluidPortMetadata(connectedStore = pollutionStorage, inputAllowed = false, outputAllowed = true)) }
 
 		override val displayHandler: TextDisplayHandler = DisplayHandlers.newMultiblockSignOverlay(this,
 			{ ComplexFluidDisplayModule(handler = it, container = fluidInput, title = text("Input"), offsetLeft = 3.5, offsetUp = 1.15, offsetBack = -4.0 + 0.39, scale = 0.7f, RelativeFace.RIGHT) },
@@ -471,9 +473,11 @@ object FluidCombustionBoilerMultiblock : BoilerMultiblock<FluidBoilerEntity>() {
 			val location = getBlockRelative(0, 0, 3).location.toCenterLocation()
 
 			repeat(2) {
-				val offsetX = Random.nextDouble(-1.5, 1.5)
-				val offsetY = Random.nextDouble(-0.5, 0.5)
-				val offsetZ = Random.nextDouble(-1.5, 1.5)
+				val xRange = if (structureDirection.axis == Axis.X) 2.5 else 1.5
+				val offsetX = Random.nextDouble(-xRange, xRange)
+				val offsetY = Random.nextDouble(-0.45, 0.45)
+				val zRange = if (structureDirection.axis == Axis.Z) 2.5 else 1.5
+				val offsetZ = Random.nextDouble(-zRange, zRange)
 
 				world.spawnParticle(Particle.FLAME, location.x + offsetX, location.y + offsetY, location.z + offsetZ, 1, 0.0, 0.0, 0.0, 0.0, null)
 			}
