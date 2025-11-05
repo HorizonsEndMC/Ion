fa21e282524ad1cf8de8bbe335317b29b00214e4
Gutin1 <57606752+gutin1@users.noreply.github.com>
2025-11-05 10:28:00 -0800

    Move to only relative coordinates

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/SequenceTriggerType.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/SequenceTriggerType.kt
@@ -2,6 +2,7 @@ package net.horizonsend.ion.server.features.sequences.trigger
 
 import net.horizonsend.ion.server.core.registration.IonRegistryKey
 import net.horizonsend.ion.server.features.sequences.Sequence
+import net.horizonsend.ion.server.features.sequences.SequenceContext
 import net.horizonsend.ion.server.features.sequences.SequenceManager
 import org.bukkit.entity.Player
 
@@ -10,10 +11,11 @@ abstract class SequenceTriggerType<T : SequenceTriggerType.TriggerSettings> {
 
 	protected fun checkPhaseTriggers(player: Player, sequenceKey: IonRegistryKey<Sequence, out Sequence>) {
 		val currentPhase = SequenceManager.getCurrentPhase(player, sequenceKey)?.getValue() ?: return
+		val context = SequenceManager.getSequenceData(player, sequenceKey).context
 
 		// Find all triggers for children on the current phase
 		for (trigger in currentPhase.triggers) {
-			if (!trigger.shouldProceed(player, sequenceKey, this@SequenceTriggerType)) continue
+			if (!trigger.shouldProceed(player, sequenceKey, this@SequenceTriggerType, context)) continue
 
 			trigger.trigger(player)
 			break
@@ -21,7 +23,7 @@ abstract class SequenceTriggerType<T : SequenceTriggerType.TriggerSettings> {
 	}
 
 	abstract class TriggerSettings() {
-		abstract fun shouldProceed(player: Player, sequenceKey: IonRegistryKey<Sequence, out Sequence>, callingTrigger: SequenceTriggerType<*>): Boolean
+		abstract fun shouldProceed(player: Player, sequenceKey: IonRegistryKey<Sequence, out Sequence>, callingTrigger: SequenceTriggerType<*>, context: SequenceContext): Boolean
 	}
 
 	override fun toString(): String {
