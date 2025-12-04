3628f55650e107764c16e5781a7d96b1234b3aac
Gutin1 <57606752+Gutin1@users.noreply.github.com>
2025-12-04 17:58:34 -0600

    remove old generation system

--- server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/miscellaneous/registrations/Components.kt
@@ -96,7 +96,6 @@ import net.horizonsend.ion.server.features.waypoint.WaypointManager
 import net.horizonsend.ion.server.features.world.IonWorld
 import net.horizonsend.ion.server.features.world.data.DataFixers
 import net.horizonsend.ion.server.features.world.environment.Environments
-import net.horizonsend.ion.server.features.world.generation.WorldGenerationManager
 import net.horizonsend.ion.server.features.world.generation.feature.nms.NMSStructureIntegration
 import net.horizonsend.ion.server.listener.misc.WorldEditListener
 import net.horizonsend.ion.server.listener.packets.PacketListeners
@@ -233,6 +232,5 @@ val components: List<IonComponent> = listOf(
 	ServerInboxes,
 	ServerMutesHook,
 	ChestShops,
-	Environments,
-	WorldGenerationManager,
+	Environments
 )
