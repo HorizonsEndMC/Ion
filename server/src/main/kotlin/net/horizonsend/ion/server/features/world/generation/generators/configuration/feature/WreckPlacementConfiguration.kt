22bd38b87e3171240a92530d61af1d5a0d750b66
Gutin1 <57606752+Gutin1@users.noreply.github.com>
2025-12-04 18:41:22 -0600

    More permanent density map

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/feature/WreckPlacementConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/feature/WreckPlacementConfiguration.kt
@@ -10,23 +10,49 @@ import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 import net.minecraft.world.level.ChunkPos
 import org.bukkit.World
 import kotlin.random.Random
+import kotlin.random.asJavaRandom
 
 @Serializable
-class WreckPlacementConfiguration() : FeaturePlacementConfiguration<WreckMetaData> {
+class WreckPlacementConfiguration(val density: Double) : FeaturePlacementConfiguration<WreckMetaData> {
 	// Placed after asteroids & other terrain features
 	override val placementPriority: Int = 1000
 
 	override fun getFeatureKey(): IonRegistryKey<GeneratedFeature<*>, GeneratedFeature<WreckMetaData>> = WorldGenerationFeatureKeys.WRECK
 
-
 	override fun generatePlacements(world: World, chunk: ChunkPos, random: Random): List<Pair<Vec3i, WreckMetaData>> {
+		if (!random.nextBoolean()) return emptyList()
+
+		val stDev = density * 4.0
+
+		val count = random.asJavaRandom()
+			.nextGaussian(density, stDev)
+			.plus(stDev)
+			.coerceAtLeast(0.0)
+			.toInt()
+
+		val list = mutableListOf<Pair<Vec3i, WreckMetaData>>()
+
 		val chunkStartX = chunk.x.shl(4)
 		val chunkStartZ = chunk.z.shl(4)
 
-		if (!random.nextBoolean()) return emptyList()
+		repeat(count) {
+			val x = chunkStartX + random.nextInt(0, 15)
+			val z = chunkStartZ + random.nextInt(0, 15)
 
-		return listOf(
-			Vec3i(chunkStartX, 128, chunkStartZ) to WreckMetaData(random.nextLong(), WreckStructureKeys.TUTORIAL_ESCAPE_POD) //TODO
-		)
+			val meta = generateMetaData(random, world, x, z)
+
+			val y = random.nextInt(
+				world.minHeight + meta.structureId.getValue().minPoint.y,
+				world.maxHeight - meta.structureId.getValue().maxPoint.y
+			)
+
+			list.add(Vec3i(x, y, z) to meta)
+		}
+
+		return list
+	}
+
+	private fun generateMetaData(random: Random, world: World, x: Int, z: Int): WreckMetaData {
+		return WreckMetaData(random.nextLong(), WreckStructureKeys.TUTORIAL_ESCAPE_POD)
 	}
 }
