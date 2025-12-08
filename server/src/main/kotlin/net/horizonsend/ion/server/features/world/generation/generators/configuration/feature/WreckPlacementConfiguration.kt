af75547170fad756f5e29c9dfdb11ebcb9f29dcc
Gutin1 <57606752+Gutin1@users.noreply.github.com>
2025-12-08 13:48:59 -0600

    implement random rotation of wrecks

--- server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/feature/WreckPlacementConfiguration.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/world/generation/generators/configuration/feature/WreckPlacementConfiguration.kt
@@ -7,7 +7,9 @@ import net.horizonsend.ion.server.core.registration.keys.WreckStructureKeys
 import net.horizonsend.ion.server.features.world.generation.feature.GeneratedFeature
 import net.horizonsend.ion.server.features.world.generation.feature.meta.wreck.WreckMetaData
 import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
+import net.horizonsend.ion.server.miscellaneous.utils.randomEntry
 import net.minecraft.world.level.ChunkPos
+import net.minecraft.world.level.block.Rotation
 import org.bukkit.World
 import kotlin.random.Random
 import kotlin.random.asJavaRandom
@@ -41,9 +43,11 @@ class WreckPlacementConfiguration(val density: Double) : FeaturePlacementConfigu
 
 			val meta = generateMetaData(random, world, x, z)
 
+			val (minPoint, maxPoint) = meta.structureId.getValue().getExtents(meta)
+
 			val y = random.nextInt(
-				world.minHeight + meta.structureId.getValue().minPoint.y,
-				world.maxHeight - meta.structureId.getValue().maxPoint.y
+				world.minHeight + minPoint.y,
+				world.maxHeight - maxPoint.y
 			)
 
 			list.add(Vec3i(x, y, z) to meta)
@@ -53,6 +57,7 @@ class WreckPlacementConfiguration(val density: Double) : FeaturePlacementConfigu
 	}
 
 	private fun generateMetaData(random: Random, world: World, x: Int, z: Int): WreckMetaData {
-		return WreckMetaData(random.nextLong(), WreckStructureKeys.TUTORIAL_ESCAPE_POD)
+		val rotation = Rotation.entries.randomEntry()
+		return WreckMetaData(random.nextLong(), WreckStructureKeys.TUTORIAL_ESCAPE_POD, rotation) //TODO
 	}
 }
