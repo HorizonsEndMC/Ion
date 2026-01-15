5b401966e1024ba64282ec9f719cc6b360f3cd2b
CrystalzzzDeez <billcypher269@gmail.com>
2026-01-15 02:15:41 +0000

    fix coreforge recipes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CoreRecipes.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/industry/CoreRecipes.kt
@@ -1,10 +1,10 @@
 package net.horizonsend.ion.server.features.multiblock.type.industry
 
-import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.ATAVUM
-import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.SCORDITE
+import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.ATAVUM_BLOCK
+import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.SCORDITE_BLOCK
 import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.URANIUM_BLOCK
-import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.VANADIUM
-import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.ZIRCON
+import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.VANADIUM_BLOCK
+import net.horizonsend.ion.server.core.registration.keys.CustomItemKeys.ZIRCON_BLOCK
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
@@ -13,45 +13,45 @@ object CoreRecipes {
 		ItemStack(Material.IRON_BLOCK) to 32,
 		URANIUM_BLOCK.getValue().constructItemStack() to 32,
 		ItemStack(Material.REDSTONE_BLOCK) to 8,
-		SCORDITE.getValue().constructItemStack() to 8
+		SCORDITE_BLOCK.getValue().constructItemStack() to 8
 	)
 	val smallReactorRecipe: Map<ItemStack, Int> = mapOf(
 		ItemStack(Material.IRON_BLOCK) to 48,
 		URANIUM_BLOCK.getValue().constructItemStack() to 48,
 		ItemStack(Material.REDSTONE_BLOCK) to 48,
-		SCORDITE.getValue().constructItemStack() to 16,
-		VANADIUM.getValue().constructItemStack() to 4
+		SCORDITE_BLOCK.getValue().constructItemStack() to 16,
+		VANADIUM_BLOCK.getValue().constructItemStack() to 4
 	)
 	val mediumReactorRecipe: Map<ItemStack, Int> = mapOf(
 		ItemStack(Material.IRON_BLOCK) to 48,
 		URANIUM_BLOCK.getValue().constructItemStack() to 48,
 		ItemStack(Material.REDSTONE_BLOCK) to 48,
-		SCORDITE.getValue().constructItemStack() to 32,
-		VANADIUM.getValue().constructItemStack() to 8,
-		ZIRCON.getValue().constructItemStack() to 4
+		SCORDITE_BLOCK.getValue().constructItemStack() to 32,
+		VANADIUM_BLOCK.getValue().constructItemStack() to 8,
+		ZIRCON_BLOCK.getValue().constructItemStack() to 4
 	)
 	val largeReactorRecipe: Map<ItemStack, Int> = mapOf(
 		ItemStack(Material.IRON_BLOCK) to 64,
 		URANIUM_BLOCK.getValue().constructItemStack() to 64,
 		ItemStack(Material.REDSTONE_BLOCK) to 64,
-		SCORDITE.getValue().constructItemStack() to 32,
-		VANADIUM.getValue().constructItemStack() to 12,
-		ZIRCON.getValue().constructItemStack() to 8,
-		ATAVUM.getValue().constructItemStack() to 2
+		SCORDITE_BLOCK.getValue().constructItemStack() to 32,
+		VANADIUM_BLOCK.getValue().constructItemStack() to 12,
+		ZIRCON_BLOCK.getValue().constructItemStack() to 8,
+		ATAVUM_BLOCK.getValue().constructItemStack() to 2
 	)
 	val cruiserReactorRecipe: Map<ItemStack, Int> = mapOf(
 		ItemStack(Material.IRON_BLOCK) to 1280,
 		URANIUM_BLOCK.getValue().constructItemStack() to 640,
 		ItemStack(Material.REDSTONE_BLOCK) to 320,
-		SCORDITE.getValue().constructItemStack() to 8,
-		VANADIUM.getValue().constructItemStack() to 2
+		SCORDITE_BLOCK.getValue().constructItemStack() to 8,
+		VANADIUM_BLOCK.getValue().constructItemStack() to 2
 	)
 	val battlecruiserReactorRecipe: Map<ItemStack, Int> = mapOf(
 		ItemStack(Material.IRON_BLOCK) to 1600,
 		URANIUM_BLOCK.getValue().constructItemStack() to 960,
 		ItemStack(Material.REDSTONE_BLOCK) to 640,
-		SCORDITE.getValue().constructItemStack() to 16,
-		VANADIUM.getValue().constructItemStack() to 4,
-		ATAVUM.getValue().constructItemStack() to 2
+		SCORDITE_BLOCK.getValue().constructItemStack() to 16,
+		VANADIUM_BLOCK.getValue().constructItemStack() to 4,
+		ATAVUM_BLOCK.getValue().constructItemStack() to 2
 	)
 }
