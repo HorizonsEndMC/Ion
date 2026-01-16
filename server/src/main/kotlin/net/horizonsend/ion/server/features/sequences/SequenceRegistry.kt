a38aeb554d95379bc90286a2f04506841926bc8d
kwazedilla <kwazedilla@gmail.com>
2026-01-15 16:06:09 -0800

    un-merge scripted-tutorial

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequenceRegistry.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/SequenceRegistry.kt
@@ -1,14 +1,16 @@
 package net.horizonsend.ion.server.features.sequences
 
+import net.horizonsend.ion.server.configuration.ConfigurationFiles
 import net.horizonsend.ion.server.core.registration.keys.KeyRegistry
 import net.horizonsend.ion.server.core.registration.keys.RegistryKeys
 import net.horizonsend.ion.server.core.registration.registries.Registry
 import net.horizonsend.ion.server.features.sequences.phases.SequencePhaseKeys
+import net.horizonsend.ion.server.miscellaneous.utils.coordinates.Vec3i
 
 class SequenceRegistry : Registry<Sequence>(RegistryKeys.SEQUENCE) {
 	override fun getKeySet(): KeyRegistry<Sequence> = SequenceKeys
 
 	override fun boostrap() {
-		register(SequenceKeys.TUTORIAL, Sequence(SequenceKeys.TUTORIAL, SequencePhaseKeys.TUTORIAL_START))
+		register(SequenceKeys.TUTORIAL, object : Sequence(SequenceKeys.TUTORIAL, SequencePhaseKeys.TUTORIAL_START) { override fun getOrigin(): Vec3i = Vec3i(ConfigurationFiles.serverConfiguration().tutorialOrigin) })
 	}
 }
