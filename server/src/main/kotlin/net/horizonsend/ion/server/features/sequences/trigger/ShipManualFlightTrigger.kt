9af4a5db3e6eb73933cacc4ad7435895055db79e
kwazedilla <kwazedilla@gmail.com>
2026-04-13 00:23:26 -0700

    WIP: starship movement trigger

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/ShipManualFlightTrigger.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/ShipManualFlightTrigger.kt
@@ -1,6 +1,5 @@
 package net.horizonsend.ion.server.features.sequences.trigger
 
-import net.horizonsend.ion.server.features.sequences.trigger.PlayerMovementTrigger.PlayerLocationPredicate
 import net.horizonsend.ion.server.features.sequences.trigger.ShipManualFlightTrigger.ShiftFlightTriggerSettings
 import net.horizonsend.ion.server.features.starship.event.movement.StarshipTranslateEvent
 import net.horizonsend.ion.server.features.starship.movement.TranslateMovement
@@ -16,9 +15,14 @@ object ShipManualFlightTrigger : SequenceTriggerType<ShiftFlightTriggerSettings>
 		}
 	}
 
-	class ShiftFlightTriggerSettings(val predicates: List<PlayerLocationPredicate> = listOf()) : TriggerSettings() {
+	class ShiftFlightTriggerSettings(val movementPredicate: (Player, TranslateMovement) -> Boolean = { _, _ -> true }) : TriggerSettings() {
 		override fun shouldProceed(player: Player, context: TriggerContext): Boolean {
-			return context.callingTrigger == ShipManualFlightTrigger
+			val event = context.event
+			if (event is StarshipTranslateEvent) {
+				return movementPredicate.invoke(player, event.movement)
+			}
+
+			return context.callingTrigger == ShipRotateTrigger
 		}
 	}
 }
