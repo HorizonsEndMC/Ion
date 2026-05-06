867b03e403a79d48933152e5357374099d2e2e87
kwazedilla <kwazedilla@gmail.com>
2026-05-05 17:09:07 -0700

    change world trigger

--- server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/PlayerInteractTrigger.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/sequences/trigger/PlayerInteractTrigger.kt
@@ -1,16 +1,16 @@
 package net.horizonsend.ion.server.features.sequences.trigger
 
-import net.horizonsend.ion.server.features.sequences.trigger.PlayerInteractTrigger.InteractTriggerSettings
+import net.horizonsend.ion.server.features.sequences.trigger.PlayerInteractTrigger.PlayerInteractTriggerSettings
 import net.horizonsend.ion.server.miscellaneous.utils.listen
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
 
-object PlayerInteractTrigger : SequenceTriggerType<InteractTriggerSettings>() {
+object PlayerInteractTrigger : SequenceTriggerType<PlayerInteractTriggerSettings>() {
 	override fun setupChecks() {
 		listen<PlayerInteractEvent> { checkAllSequences(it.player, it) }
 	}
 
-	class InteractTriggerSettings(
+	class PlayerInteractTriggerSettings(
 	) : TriggerSettings() {
 		override fun shouldProceed(player: Player, context: TriggerContext): Boolean {
 			return context.callingTrigger == PlayerInteractTrigger
