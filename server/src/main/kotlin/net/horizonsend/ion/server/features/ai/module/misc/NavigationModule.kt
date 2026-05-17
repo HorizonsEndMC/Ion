b3e72e29d09bfc474a073338f27e7d21c077cfb3
CrystalzzzDeez <billcypher269@gmail.com>
2026-05-17 02:41:03 +0100

    remove ai jumping

--- server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/misc/NavigationModule.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/ai/module/misc/NavigationModule.kt
@@ -55,10 +55,12 @@ class NavigationModule(
 
 	override fun tick() {
 		validateNavigation()
+		// Here lies the worst feature to ever exist, goodbye ai jumping:
+
 		//only try to use hyperdrive when navigation calls for it and after navigation has been evaluated
-		if (navigate && hyperdriveNavigate && !triggerUpdate && ticks % 20 == 0) attemptHyperdrive() //tick every second
+		//if (navigate && hyperdriveNavigate && !triggerUpdate && ticks % 20 == 0) attemptHyperdrive() //tick every second
 		//interrupt hyperdrive if not necessary to prevent oscillations
-		if (!hyperdriveNavigate && Hyperspace.isWarmingUp(starship)) Hyperspace.interruptWarmup(starship)
+		//if (!hyperdriveNavigate && Hyperspace.isWarmingUp(starship)) Hyperspace.interruptWarmup(starship)
 
 		ticks++
 		if (ticks % tickRate != 0 && !triggerUpdate) return
