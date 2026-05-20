3d40eed71b9505a562c4f9d9e770903006414a3d
CrystalzzzDeez <billcypher269@gmail.com>
2026-05-20 03:15:17 +0100

    fix2

--- server/src/main/kotlin/net/horizonsend/ion/server/features/misc/DominionWorldDB.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/misc/DominionWorldDB.kt
@@ -1,6 +1,9 @@
 package net.horizonsend.ion.server.features.misc
 
 import net.horizonsend.ion.common.database.schema.nations.DominionTerritory
+import net.horizonsend.ion.common.database.schema.nations.NPCTerritoryOwner
+import net.horizonsend.ion.common.database.schema.nations.Territory
+import net.horizonsend.ion.common.database.schema.nations.TradeWorldTerritory
 import net.horizonsend.ion.server.core.IonServerComponent
 import net.horizonsend.ion.server.features.world.IonWorld
 import net.horizonsend.ion.server.features.world.WorldFlag
@@ -13,6 +16,17 @@ object DominionWorldDB: IonServerComponent() {
 			val worldName = ionWorld.world.name
 			if (DominionTerritory.findByWorld(worldName) == null) {
 				DominionTerritory.create(worldName, worldName)
+			}
+
+			if (ionWorld.hasFlag(WorldFlag.DOMINION_TRADE_WORLD)) {
+				if (TradeWorldTerritory.findByWorld(worldName) == null) {
+					// Create backing Territory with empty polygon
+					val territoryId = Territory.create(worldName, worldName, ByteArray(0))
+					// Create NPCTerritoryOwner so TradeCities picks it up
+					NPCTerritoryOwner.create(territoryId, worldName, 0xFFFFFF, true)
+					// Create TradeWorldTerritory linking everything
+					TradeWorldTerritory.create(worldName, worldName, 0xFFFFFF, territoryId)
+				}
 			}
 		}
 	}
