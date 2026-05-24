c12812eceda17b2feab42259db5374e38a3503d7
kwazedilla <kwazedilla@gmail.com>
2026-05-24 01:21:59 -0700

    disable most bazaar monitoring

--- server/src/main/kotlin/net/horizonsend/ion/server/features/player/EventLogger.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/player/EventLogger.kt
@@ -84,6 +84,7 @@ object EventLogger : IonServerComponent() {
 			)
 		}
 
+		/*
 		listen<BazaarDepositItemToSellOrderEvent> { event ->
 			val player = event.player
 			val itemString = event.itemString
@@ -161,6 +162,7 @@ object EventLogger : IonServerComponent() {
 				"**sold to a buy order on bazaar**: $amount of $item at $city (rev: $subtotal; tax: $tax; total: $total)"
 			)
 		}
+		 */
 
 		listen<BazaarCollectItemFromBuyOrderEvent> { event ->
 			val player = event.player
