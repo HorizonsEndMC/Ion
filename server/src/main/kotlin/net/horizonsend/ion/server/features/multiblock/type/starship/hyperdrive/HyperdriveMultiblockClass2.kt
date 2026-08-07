305522bb1e10ae8d00ffc0503ba4ab0ae86eb5a2
Sciath <67932943+SciathGH@users.noreply.github.com>
2026-08-07 17:15:11 +0100

    Added the Hyper-Hyperdrive. Same stats as a tier 4 hyperdrive. Only 1 inventory however.

--- server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/starship/hyperdrive/HyperdriveMultiblockClass2.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/multiblock/type/starship/hyperdrive/HyperdriveMultiblockClass2.kt
@@ -20,7 +20,7 @@ object HyperdriveMultiblockClass2 : HyperdriveMultiblock() {
 	override val hyperdriveClass = 2
 
 	override fun MultiblockShape.buildStructure() {
-		addHoppers(this)
+		addFuelInventories(this)
 
 		z(+0) {
 			y(-1) {
@@ -47,7 +47,7 @@ object HyperdriveMultiblockClass2 : HyperdriveMultiblock() {
 		}
 	}
 
-	override fun buildHopperOffsets() = listOf(
+	override fun buildFuelInventoryOffsets() = listOf(
 		Vec3i(x = -1, y = -1, z = +1), // left hopper
 		Vec3i(x = +1, y = -1, z = +1) // right hopper
 	)
